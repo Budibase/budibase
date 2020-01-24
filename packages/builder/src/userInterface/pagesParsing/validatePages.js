c2d99e173b70526f8250ebe6fc46b412ba87e907
pngwn <hello@pngwn.io>
2020-01-24 16:39:25 +0000

    Remove unused validateProps module and tests

--- packages/builder/src/userInterface/pagesParsing/validatePages.js
+++ packages/builder/src/userInterface/pagesParsing/validatePages.js
@@ -1,7 +1,6 @@
-import { recursivelyValidate } from "./validateProps";
-import { 
-    isString, 
-    keys, 
+import {
+    isString,
+    keys,
     flatten,
     isArray,
     map,
@@ -15,12 +14,12 @@ export const validatePage = (page, getComponent) => {
     const error = message => errors.push(message);
 
     const noIndex = !page.index;
-    if(noIndex) {
+    if (noIndex) {
         error("Page does not define an index member");
     }
 
-    if(!page.appBody 
-        || !isString(page.appBody) 
+    if (!page.appBody
+        || !isString(page.appBody)
         || !page.appBody.endsWith(".json")) {
         error("App body must be set toa valid JSON file");
     }
@@ -28,7 +27,7 @@ export const validatePage = (page, getComponent) => {
     /* Commenting this for now
     * index is a load of static members just now, but maybe useful
     for pageLayout props (which is just a pipe dream at time of writing)
-    const indexHtmlErrors = noIndex 
+    const indexHtmlErrors = noIndex
         ? []
         : pipe(
             recursivelyValidate(page.index, getComponent), [
@@ -44,17 +43,17 @@ export const validatePages = (pages, getComponent) => {
     let errors = [];
     const error = message => errors.push(message);
 
-    if(!pages.main) {
+    if (!pages.main) {
         error("must have a 'main' page");
     }
 
-    if(!pages.unauthenticated) {
+    if (!pages.unauthenticated) {
         error("must have a 'unauthenticated' (login) page");
     }
 
-    if(!pages.componentLibraries 
-      || !isArray(pages.componentLibraries) 
-      || pages.componentLibraries.length === 0) {
+    if (!pages.componentLibraries
+        || !isArray(pages.componentLibraries)
+        || pages.componentLibraries.length === 0) {
 
         error("componentLibraries must be set to a non-empty array of strings");
     }
