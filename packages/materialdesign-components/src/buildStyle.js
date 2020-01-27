ee5a4e8c962b29242152cbbd8065d8f3ccf65eaf
Michael Shanks <mike@budibase.com>
2020-01-27 10:59:56 +0000

    MD components library - empty proj (#61)
    Added a shell Material Design components packages

---
+++ packages/materialdesign-components/src/buildStyle.js
@@ -0,0 +1,9 @@
+export const buildStyle = (styles) => {
+    let str = "";
+    for(let s in styles) {
+        if(styles[s]) {
+            str += `${s}: ${styles[s]}; `
+        }
+    }
+    return str;
+}
