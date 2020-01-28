1d386644c2565a0cb713ae96186ab821e3a42491
Conor Mack <cmckeown101@gmail.com>
2020-01-28 10:03:45 +0000

    Beginning the button component and general setup

--- packages/materialdesign-components/rollup.config.js
+++ packages/materialdesign-components/rollup.config.js
@@ -1,20 +1,36 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import svelte from "rollup-plugin-svelte";
+import postcss from "rollup-plugin-postcss";
+import resolve from "rollup-plugin-node-resolve";
+
+const postcssOptions = () => ({
+  extensions: [".scss", ".sass"],
+  extract: false,
+  minimize: true,
+  use: [
+    [
+      "sass",
+      {
+        includePaths: ["./node_modules"]
+      }
+    ]
+  ]
+});
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ 
-			file: "dist/index.js", 
-			format: 'esm', 
-			name:"budibaseStandardComponents",
-			sourcemap: "inline"
-		}
-	],
-	plugins: [
-		svelte({
-			hydratable:true
-		}),
-		resolve()
-	]
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+      name: "budibaseStandardComponents",
+      sourcemap: "inline"
+    }
+  ],
+  plugins: [
+    svelte({
+      hydratable: true
+    }),
+    resolve(),
+    postcss(postcssOptions())
+  ]
 };
--- packages/materialdesign-components/src/index.js
+++ packages/materialdesign-components/src/index.js
@@ -1,1 +1,2 @@
-export {default as h1} from  "./H1.svelte";
+export { default as h1 } from "./H1.svelte";
+export { default as button } from "./Button.svelte";
