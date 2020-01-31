7ae29a603072a9d00981a0ad569614829ea77cd4
Michael Shanks <mike@budibase.com>
2020-01-31 23:11:50 +0000

    #24 - Control Flow (#79)
    * removed binding references to array type
    
    * refactored initialiseChildren into seperate file
    
    * render function, with code blocks - tested simple cases
    
    * few mores tests for control flow
    
    * md components - getting TestApp to work
    
    * new render wrapper - bug fix
    
    * client: providing access to component root elements
    
    * code editor working
    
    * code editor improvements

---
+++ packages/builder/src/builderStore/buildCodeForScreens.js
@@ -0,0 +1,35 @@
+
+
+const buildCodeForSingleScreen = (screen) => {
+    let code = "";
+    const walkProps = (props) => {
+        if(props._code && props._code.trim().length > 0) {
+            code += buildComponentCode(props)
+        }
+        
+        if(!props._children) return;
+        
+        for(let child of props._children) {
+            walkProps(child);
+        }
+    }
+
+    walkProps(screen.props);
+
+    return code;
+}
+
+export const buildCodeForScreens = screens => {
+    let allfunctions = ""; 
+    for(let screen of screens) {
+        allfunctions += buildCodeForSingleScreen(screen);
+    }
+    
+    return (`return ({ ${allfunctions} });`);
+}
+
+const buildComponentCode = (componentProps) => 
+`"${componentProps._id}" : (render, context) => {
+${componentProps._code}
+},
+`;
