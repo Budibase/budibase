936bce711e58983aee3442969ca2f3929f4b270a
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-02-10 16:58:20 +0000

    state binding v1

--- packages/server/utilities/builder/index.js
+++ packages/server/utilities/builder/index.js
@@ -139,4 +139,73 @@ const getComponents = async (appPath, pages, lib) => {
   return { components, generators }
 }
 
+/**
+ * buildStateOrigins
+ *
+ * Builds an object that details all the bound state in the application, and what updates it. 
+ *
+ * @param screenDefinition - the screen definition metadata.
+ * @returns {Object} an object with the client state values and how they are managed. 
+ */
+const buildStateOrigins = screenDefinition => {
+  const origins = {};
+
+  function traverse(propValue) {
+    for (let key in propValue) {
+      if (!Array.isArray(propValue[key])) continue;
+
+      if (key === "_children") propValue[key].forEach(traverse);
+
+      for (let element of propValue[key]) {
+        if (element["##eventHandlerType"] === "Set State") origins[element.parameters.path] = element;
+      }
+    }
+  }
+
+  traverse(screenDefinition.props);
+
+  return origins;
+};
+
+const fetchscreens = async (appPath, relativePath = "") => {
+  const currentDir = join(appPath, "components", relativePath)
+
+  const contents = await readdir(currentDir)
+
+  const components = []
+
+  for (let item of contents) {
+    const itemRelativePath = join(relativePath, item)
+    const itemFullPath = join(currentDir, item)
+    const stats = await stat(itemFullPath)
+
+    if (stats.isFile()) {
+      if (!item.endsWith(".json")) continue
+
+      const component = await readJSON(itemFullPath)
+
+      component.name = itemRelativePath
+        .substring(0, itemRelativePath.length - 5)
+        .replace(/\\/g, "/")
+
+      component.props = component.props || {}
+
+      component.stateOrigins = buildStateOrigins(component);
+
+      components.push(component)
+    } else {
+      const childComponents = await fetchscreens(
+        appPath,
+        join(relativePath, item)
+      )
+
+      for (let c of childComponents) {
+        components.push(c)
+      }
+    }
+  }
+
+  return components
+}
+
 module.exports.getComponents = getComponents
