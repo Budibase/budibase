0af15bd7d89bd80b4fd68757d735b68429643192
Conor_Mack <36074859+Conor-Mack@users.noreply.github.com>
2020-02-12 12:32:46 +0000

    Completed checkbox and form field. Changes to test app (#91)
    * Completed checkbox and form field component with changes around how test app renders components
    
    * Tidyup

--- packages/materialdesign-components/src/index.js
+++ packages/materialdesign-components/src/index.js
@@ -1,3 +1,14 @@
-export { default as h1 } from "./H1.svelte"
-export { default as icon } from "./Icon.svelte"
+import "@material/theme/mdc-theme.scss";
+
 export { button } from "./Button"
+export { default as icon } from "./Icon.svelte"
+export { textfield } from "./Textfield"
+export * from "./Typography"
+export { checkbox } from "./Checkbox"
+
+// import { Button } from "./Button";
+// import Icon from "./Icon.svelte";
+// import { Textfield } from "./Textfield";
+// export { Button, Icon, Textfield };
+// export * from "./Typography";
+// export { Checkbox } from "./Checkbox";
