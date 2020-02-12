c7830352e0acdc8a8d0bca7df4e82926832dcf92
Conor Mack <cmckeown101@gmail.com>
2020-02-12 11:37:40 +0000

    Completed checkbox and form field component with changes around how test app renders components

--- packages/materialdesign-components/src/Test/createApp.js
+++ packages/materialdesign-components/src/Test/createApp.js
@@ -1,17 +1,24 @@
 import { createApp } from "@budibase/client/src/createApp"
 import components from "./testComponents"
 import packageJson from "../../package.json"
-
-export default async () => {
+import { rootComponent } from "./rootComponent"
+export default async props => {
   delete components._lib
-
   const componentLibraries = {}
   componentLibraries[packageJson.name] = components
-
+  componentLibraries["testcomponents"] = {
+    rootComponent: rootComponent(window)
+  }
   const appDef = { hierarchy: {}, actions: {} }
   const user = { name: "yeo", permissions: [] }
-
-  var app = createApp(window.document, componentLibraries, appDef, user, {})
-
-  return app
+  const { initialisePage } = createApp(
+    window.document,
+    componentLibraries,
+    { appRootPath: "" },
+    appDef,
+    user,
+    {},
+    []
+  )
+  return initialisePage
 }
--- packages/materialdesign-components/src/Test/props.js
+++ packages/materialdesign-components/src/Test/props.js
@@ -1,5 +1,3 @@
-const getComponent = comp => `@budibase/materialdesign-components/${comp}`;
-
 
 export const props = {
   h1: {
@@ -8,9 +6,9 @@ export const props = {
     text: "Im a big header",
   },
   overline: {
-    _component: getComponent`Overline`,
+    _component: "@budibase/materialdesign-components/Overline",
     _children: [],
-    text: "A wee Overline",
+    text: "Im a wee overline",
   },
   button: {
     _component: "@budibase/materialdesign-components/button",
@@ -36,7 +34,61 @@ export const props = {
     label: "First",
     colour: "secondary",
     textarea: true,
-    fullwidth:true,
+    fullwidth: true,
     helperText: "Add Surname",
-    useCharCounter: true
+    useCharCounter: true,
+  },
+  checkbox: {
+    _component: "@budibase/materialdesign-components/checkbox",
+    _children: [],
+    id: "test-check",
+    label: "Check Yo Self",
+  }
 }
+
+// export const props = {
+//   h1: {
+//     _component: "@budibase/materialdesign-components/H1",
+//     _children: [],
+//     text: "Im a big header",
+//   },
+//   overline: {
+//     _component: "@budibase/materialdesign-components/Overline",
+//     _children: [],
+//     text: "A wee Overline",
+//   },
+//   button: {
+//     _component: "@budibase/materialdesign-components/button",
+//     _children: [],
+//     variant: "raised",
+//     colour: "secondary",
+//     size: "large",
+//     href: "",
+//     icon: "alarm_on",
+//     trailingIcon: true,
+//     fullwidth: false,
+//     text: "I am button",
+//     disabled: false,
+//   },
+//   icon: {
+//     _component: "@budibase/materialdesign-components/icon",
+//     _children: [],
+//     icon: "",
+//   },
+//   textfield: {
+//     _component: "@budibase/materialdesign-components/textfield",
+//     _children: [],
+//     label: "First",
+//     colour: "secondary",
+//     textarea: true,
+//     fullwidth: true,
+//     helperText: "Add Surname",
+//     useCharCounter: true,
+//   },
+//   checkbox: {
+//     _component: "@budibase/materialdesign-components/checkbox",
+//     _children: [],
+//     id: "test-check",
+//     label: "Check Yo Self",
+//   }
+// }
