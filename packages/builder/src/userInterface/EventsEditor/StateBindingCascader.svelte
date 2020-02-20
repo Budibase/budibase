412aa6739f92cd1fc63336f7e7103a08ba406da5
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-02-20 20:19:24 +0000

    state management bindings

---
+++ packages/builder/src/userInterface/EventsEditor/StateBindingCascader.svelte
@@ -0,0 +1,33 @@
+<script>
+  import IconButton from "../../common/IconButton.svelte"
+  import PlusButton from "../../common/PlusButton.svelte"
+  import Select from "../../common/Select.svelte"
+  import Input from "../../common/Input.svelte"
+  import StateBindingControl from "../StateBindingControl.svelte"
+  import { find, map, keys, reduce, keyBy } from "lodash/fp"
+  import { pipe, userWithFullAccess } from "../../common/core"
+  import {
+    EVENT_TYPE_MEMBER_NAME,
+    allHandlers,
+  } from "../../common/eventHandlers"
+  import { store } from "../../builderStore"
+
+  export let parameter
+</script>
+
+<div class="handler-option">
+  <span>{parameter.name}</span>
+  <Input on:change value={parameter.value} />
+</div>
+
+<style>
+  .handler-option {
+    display: flex;
+    flex-direction: column;
+  }
+
+  span {
+    font-size: 12px;
+    margin-bottom: 5px;
+  }
+</style>
