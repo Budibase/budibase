395dcd1cc2a5e5d6b6a54b7fc4859fe6f4bb6356
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-02-21 18:02:02 +0000

    finished state management ui for v2

--- packages/builder/src/common/Select.svelte
+++ packages/builder/src/common/Select.svelte
@@ -23,11 +23,9 @@
 
   select {
     display: block;
-    font-size: 14px;
     font-family: sans-serif;
     font-weight: 500;
     color: #163057;
-    line-height: 1.3;
     padding: 1em 2.6em 0.9em 1.4em;
     width: 100%;
     max-width: 100%;
@@ -38,7 +36,7 @@
     appearance: none;
     background: #fff;
     border: 1px solid #ccc;
-    height: 50px;
+    height: 35px;
   }
 
   .arrow {
--- packages/builder/src/userInterface/EventsEditor/StateBindingCascader.svelte
+++ packages/builder/src/userInterface/EventsEditor/StateBindingCascader.svelte
@@ -11,19 +11,60 @@
     allHandlers,
   } from "../../common/eventHandlers"
   import { store } from "../../builderStore"
+  import StateBindingOptions from "../PropertyCascader/StateBindingOptions.svelte"
+  import { ArrowDownIcon } from "../../common/Icons/"
 
   export let parameter
+  export let onChange
+
+  let isOpen = false
 </script>
 
 <div class="handler-option">
   <span>{parameter.name}</span>
-  <Input on:change value={parameter.value} />
+  <div class="handler-input">
+    <Input on:change={onChange} value={parameter.value} />
+    <button on:click={() => (isOpen = !isOpen)}>
+      <div class="icon" style={`transform: rotate(${isOpen ? 0 : 90}deg);`}>
+        <ArrowDownIcon size={36} />
+      </div>
+    </button>
+    {#if isOpen}
+      <StateBindingOptions
+        onSelect={option => {
+          onChange(option)
+          isOpen = false
+        }} />
+    {/if}
+  </div>
 </div>
 
 <style>
+  button {
+    cursor: pointer;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    background: rgba(249, 249, 249, 1);
+
+    font-size: 1.6rem;
+    font-weight: 700;
+    color: rgba(22, 48, 87, 1);
+    margin-left: 5px;
+  }
+
+  .icon {
+    width: 24px;
+  }
+
   .handler-option {
     display: flex;
     flex-direction: column;
+  }
+  
+  .handler-input {
+    position: relative;
+    display: flex;
   }
 
   span {
