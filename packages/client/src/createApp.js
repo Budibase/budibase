1657beb8fce845fe79edcba44520bbd5e8a98352
Michael Shanks <mike@budibase.com>
2020-02-24 11:15:08 +0000

    bugfix - client - approotPath not passed correctly

--- packages/client/src/createApp.js
+++ packages/client/src/createApp.js
@@ -32,6 +32,7 @@ export const createApp = (
         uiFunctions,
         onScreenSlotRendered: () => {},
         routeTo,
+        appRootPath: frontendDefinition.appRootPath,
       })
       const getAttchChildrenParams = attachChildrenParams(stateManager)
       screenSlotNode.props._children = [screen.props]
@@ -78,6 +79,7 @@ export const createApp = (
     componentLibraries,
     uiFunctions,
     onScreenSlotRendered,
+    appRootPath: frontendDefinition.appRootPath,
     // seems weird, but the routeTo variable may not be available at this point
     routeTo: url => routeTo(url),
   })
