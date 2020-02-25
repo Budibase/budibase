38f3a3b2b75b5c2ec4a1727c8d810628884bca3f
Conor Mack <cmckeown101@gmail.com>
2020-02-25 14:55:44 +0000

    Changing list to use Children for items

--- packages/materialdesign-components/src/List/index.js
+++ packages/materialdesign-components/src/List/index.js
@@ -1,2 +1,3 @@
-import "./_style.scss";
-export { default as List } from "./List.svelte";
+import "./_style.scss"
+export { default as List } from "./List.svelte"
+export { default as ListItem } from "./ListItem.svelte"
