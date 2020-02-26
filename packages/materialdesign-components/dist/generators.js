56d51014d45c50578970cd86fcfa87cdc4548e23
Michael Shanks <mike@budibase.com>
2020-02-26 22:18:14 +0000

    prep for NPM publish

--- packages/materialdesign-components/dist/generators.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdG9ycy5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
