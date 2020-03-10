00336cecc9ae473584968833926eaec114973c73
Michael Shanks <mike@budibase.com>
2020-03-10 22:50:34 +0000

    diffHierarchy started

---
+++ packages/core/src/templateApi/upgradeData.js
@@ -0,0 +1,17 @@
+ /*
+const changeActions = {
+  rebuildIndex: indexNodeKey => ({
+    type: "rebuildIndex",
+    indexNodeKey,
+  }),
+  reshardRecords: recordNodeKey => ({
+    type: "reshardRecords",
+    recordNodeKey,
+  }),
+  deleteRecords: recordNodeKey => ({
+    type: "reshardRecords",
+    recordNodeKey,
+  }),
+  renameRecord
+}
+*/
