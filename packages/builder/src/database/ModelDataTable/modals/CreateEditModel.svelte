38d762386213ec8fe40888dee025b98346d75916
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-03-17 00:20:13 +0000

    modal restructuring

--- packages/builder/src/database/ModelDataTable/modals/CreateEditModel.svelte
+++ packages/builder/src/database/ModelDataTable/modals/CreateEditModel.svelte
@@ -1,6 +1,8 @@
 <script>
-  import Modal from "../../common/Modal.svelte"
-  import ActionButton from "../../common/ActionButton.svelte"
+  import Modal from "../../../common/Modal.svelte"
+  import ActionButton from "../../../common/ActionButton.svelte"
+  import ModelView from "../../ModelView.svelte"
+  import ActionsHeader from "../../ActionsHeader.svelte"
   import * as api from "./api"
 
   export let modalOpen = false
@@ -13,11 +15,13 @@
 <Modal {onClosed} bind:isOpen={modalOpen} title={'Record'}>
   <h4 class="budibase__title--4">Create / Edit Model</h4>
   <div class="actions">
-    <ActionButton alert on:click={onClosed}>Cancel</ActionButton>
+    <ModelView />
+    <ActionsHeader />
+    <!-- <ActionButton alert on:click={onClosed}>Cancel</ActionButton>
     <ActionButton
       disabled={false}
       on:click={() => api.createNewRecord(recordInfo)}>
       Save
-    </ActionButton>
+    </ActionButton> -->
   </div>
 </Modal>
--- packages/builder/src/database/ModelDataTable/modals/index.js
+++ packages/builder/src/database/ModelDataTable/modals/index.js
@@ -1,2 +1,3 @@
 export { default as DeleteRecordModal } from "./DeleteRecord.svelte";
 export { default as CreateEditRecordModal } from "./CreateEditRecord.svelte";
+export { default as CreateEditModelModal } from "./CreateEditModel.svelte";
