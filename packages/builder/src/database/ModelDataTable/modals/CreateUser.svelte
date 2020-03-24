57c3598d9f6792ea3348b0377297af0fedfd5fb2
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-03-24 16:17:10 +0000

    user creation

--- packages/builder/src/database/ModelDataTable/modals/CreateUser.svelte
+++ packages/builder/src/database/ModelDataTable/modals/CreateUser.svelte
@@ -1,30 +1,51 @@
 <script>
   import Modal from "../../../common/Modal.svelte"
-  import { store } from "../../../builderStore"
+  import { store, backendUiStore } from "../../../builderStore"
   import ActionButton from "../../../common/ActionButton.svelte"
-  // import * as api from "../api"
+  import * as api from "../api"
 
   export let onClosed
 
-  let userName
+  let username
+  let password
+  let accessLevels = []
+
+  $: valid = username && password && accessLevels.length
+  $: currentAppInfo = {
+    appname: $store.appname,
+    instanceId: $backendUiStore.selectedDatabase.id,
+  }
 
   async function createUser() {
-    // const response = await api.createUser($store.appname, userName)
-    // store.createUserForInstance(response)
+    const response = await api.createUser(
+      {
+        username,
+        password,
+        accessLevels,
+        enabled: true,
+      },
+      currentAppInfo
+    )
     onClosed()
   }
 </script>
 
-<section>
-  User Name
-  <input class="uk-input" type="text" bind:value={userName} />
+<form class="uk-form-stacked">
+  <label class="uk-form-label" for="form-stacked-text">Username</label>
+  <input class="uk-input" type="text" bind:value={username} />
+  <label class="uk-form-label" for="form-stacked-text">Password</label>
+  <input class="uk-input" type="password" bind:value={password} />
+  <label class="uk-form-label" for="form-stacked-text">Access Levels</label>
+  <select multiple bind:value={accessLevels}>
+    {#each $store.accessLevels.levels as level}
+      <option value={level.name}>{level.name}</option>
+    {/each}
+  </select>
   <footer>
     <ActionButton alert on:click={onClosed}>Cancel</ActionButton>
-    <ActionButton disabled={!userName} on:click={createUser}>
-      Save
-    </ActionButton>
+    <ActionButton disabled={!valid} on:click={createUser}>Save</ActionButton>
   </footer>
-</section>
+</form>
 
 <style>
   footer {
