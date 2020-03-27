ca026e6017d91e1a32ea2ab3a70601eeb672bd0b
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-03-27 14:49:43 +0000

    write access levels

--- packages/server/middleware/routeHandlers/upgradeData.js
+++ packages/server/middleware/routeHandlers/upgradeData.js
@@ -1,6 +1,9 @@
 const StatusCodes = require("../../utilities/statusCodes")
 
 module.exports = async ctx => {
+  const accessLevels = ctx.request.body.accessLevels
+  accessLevels.version = 0 
+  await ctx.instance.authApi.saveAccessLevels(accessLevels)
   await ctx.instance.templateApi.upgradeData(ctx.request.body.newHierarchy)
   ctx.response.status = StatusCodes.OK
 }
