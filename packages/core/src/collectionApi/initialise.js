946c79df53aa29c315ac6bebb4df5d16f8b8396d
Michael Shanks <mike@budibase.com>
2020-03-28 06:39:22 +0000

    bugfix: clearing sessions on backend update

--- packages/core/src/collectionApi/initialise.js
+++ packages/core/src/collectionApi/initialise.js
@@ -6,7 +6,7 @@ import {
 } from "../templateApi/hierarchy"
 import { $, allTrue, joinKey } from "../common"
 
-const ensureCollectionIsInitialised = async (datastore, node, dir) => {
+export const ensureCollectionIsInitialised = async (datastore, node, dir) => {
   if (!(await datastore.exists(dir))) {
     await datastore.createFolder(dir)
     await datastore.createFolder(joinKey(dir, node.nodeId))
