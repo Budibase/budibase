a657076397b725b4cc8c210636b1fdb65134f727
Michael Shanks <mike@budibase.com>
2020-04-02 16:37:22 +0100

    "Master Layout" > "Page Layout" .. and moved above screens

--- packages/builder/src/components/userInterface/UserInterfaceRoot.svelte
+++ packages/builder/src/components/userInterface/UserInterfaceRoot.svelte
@@ -1,7 +1,7 @@
 <script>
   import ComponentsHierarchy from "./ComponentsHierarchy.svelte"
   import ComponentsHierarchyChildren from "./ComponentsHierarchyChildren.svelte"
-  import MasterLayout from "./MasterLayout.svelte"
+  import PageLayout from "./PageLayout.svelte"
   import PagesList from "./PagesList.svelte"
   import { store } from "builderStore"
   import IconButton from "components/common/IconButton.svelte"
@@ -55,6 +55,10 @@
 
     <div class="border-line" />
 
+    <PageLayout layout={$store.pages[$store.currentPageName]} />
+
+    <div class="border-line" />
+
     <div class="components-list-container">
       <div class="nav-group-header">
         <span class="components-nav-header" style="margin-top: 0;">
@@ -67,7 +71,6 @@
         </div>
       </div>
       <div class="nav-items-container">
-        <MasterLayout layout={$store.pages[$store.currentPageName]} />
         <ComponentsHierarchy screens={$store.screens} />
       </div>
     </div>
