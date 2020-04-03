0aa41d924d80a4b8073df3b0caf02bc75ec871b5
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-04-03 16:15:53 +0100

    restructure server routes

---
+++ packages/server/middleware/routes/index.js
@@ -0,0 +1,14 @@
+const screenRoutes = require("./screens");
+const pageRoutes = require("./pages");
+const componentRoutes = require("./components");
+const userRoutes = require("./user");
+const appsRoutes = require("./apps");
+const authenticatedRoutes = require("./authenticated");
+
+module.exports = {
+  screenRoutes,
+  pageRoutes,
+  componentRoutes,
+  authRoutes,
+  appsRoutes
+};
