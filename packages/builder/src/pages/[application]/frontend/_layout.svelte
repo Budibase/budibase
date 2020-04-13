67e223f777820ad529aaf36283853fbcbda7be48
kevmodrome <kevin@kevmodro.me>
2020-04-13 12:59:46 +0200

    makes sure the frontend selects a database when navigated to

--- packages/builder/src/pages/[application]/frontend/_layout.svelte
+++ packages/builder/src/pages/[application]/frontend/_layout.svelte
@@ -1,1 +1,22 @@
+<script>
+  import { store, backendUiStore } from "builderStore"
+  import { goto } from "@sveltech/routify"
+  import { onMount } from "svelte"
+
+  $: instances = $store.appInstances
+  $: views = $store.hierarchy.indexes
+
+  async function selectDatabase(database) {
+    backendUiStore.actions.records.select(null)
+    backendUiStore.actions.views.select(views[0])
+    backendUiStore.actions.database.select(database)
+  }
+
+  onMount(async () => {
+    if ($store.appInstances.length > 0 && !$backendUiStore.database) {
+      await selectDatabase($store.appInstances[0])
+    }
+  })
+</script>
+
 <slot />
