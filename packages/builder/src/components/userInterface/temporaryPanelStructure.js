12067af0db5651fa097f1b597a993a805498991f
kevmodrome <kevin@kevmodro.me>
2020-04-17 09:16:03 +0200

    adds basic tab view to the components pane

---
+++ packages/builder/src/components/userInterface/temporaryPanelStructure.js
@@ -0,0 +1,154 @@
+export default {
+  categories: [
+    {
+      name: 'Basic',
+      components: [
+        {
+          component: 'Text',
+          icon: 'Text',
+          type: [
+            {
+              _component: '@budibase/standard-components/header',
+              name: 'Headline',
+              icon: 'headline',
+              props: {
+                type: {
+                  type: 'options',
+                  options: [
+                    'h1',
+                    'h2'
+                  ],
+                  'default': 'h1'
+                }
+              }
+            },
+            {
+              _component: '@budibase/standard-components/text',
+              name: 'Paragraph',
+              icon: 'paragraph'
+            }
+          ]
+        },
+        {
+          component: 'Button',
+          icon: 'Text',
+          type: [
+            {
+              _component: '@budibase/standard-components/header',
+              name: 'Headline',
+              icon: 'headline',
+              props: {
+                type: {
+                  type: 'options',
+                  options: [
+                    'h1',
+                    'h2'
+                  ],
+                  'default': 'h1'
+                }
+              }
+            },
+            {
+              _component: '@budibase/standard-components/text',
+              name: 'Paragraph',
+              icon: 'paragraph'
+            }
+          ]
+        },
+      ]
+    },
+    {
+      name: 'Form',
+      components: [
+        {
+          component: 'Text',
+          icon: 'Text',
+          type: [
+            {
+              _component: '@budibase/standard-components/header',
+              name: 'Headline',
+              icon: 'headline',
+              props: {
+                type: {
+                  type: 'options',
+                  options: [
+                    'h1',
+                    'h2'
+                  ],
+                  'default': 'h1'
+                }
+              }
+            },
+            {
+              _component: '@budibase/standard-components/text',
+              name: 'Paragraph',
+              icon: 'paragraph'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Blocks',
+      components: [
+        {
+          component: 'Text',
+          icon: 'Text',
+          type: [
+            {
+              _component: '@budibase/standard-components/header',
+              name: 'Headline',
+              icon: 'headline',
+              props: {
+                type: {
+                  type: 'options',
+                  options: [
+                    'h1',
+                    'h2'
+                  ],
+                  'default': 'h1'
+                }
+              }
+            },
+            {
+              _component: '@budibase/standard-components/text',
+              name: 'Paragraph',
+              icon: 'paragraph'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Data',
+      components: [
+        {
+          component: 'Text',
+          icon: 'Text',
+          type: [
+            {
+              _component: '@budibase/standard-components/header',
+              name: 'Headline',
+              icon: 'headline',
+              props: {
+                type: {
+                  type: 'options',
+                  options: [
+                    'h1',
+                    'h2'
+                  ],
+                  'default': 'h1'
+                }
+              }
+            },
+            {
+              _component: '@budibase/standard-components/text',
+              name: 'Paragraph',
+              icon: 'paragraph'
+            }
+          ]
+        }
+      ]
+    },
+  ]
+}
