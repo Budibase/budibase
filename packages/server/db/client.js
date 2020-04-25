b88044278e793a47db8c422b86f064ab0d958fa4
Michael Shanks <mike@budibase.com>
2020-04-25 08:35:34 +0100

    cli init now working...

--- packages/server/db/client.js
+++ packages/server/db/client.js
@@ -1,14 +1,14 @@
 const PouchDB = require("pouchdb")
-require("pouchdb-all-dbs")(PouchDB)
+const allDbs = require("pouchdb-all-dbs")
 
 module.exports = () => {
   const COUCH_DB_URL =
     process.env.COUCH_DB_URL || "http://admin:password@localhost:5984"
   const DATABASE_TYPE = process.env.DATABASE_TYPE || "couch"
 
-  PouchDB.defaults({
+  const pouch =  PouchDB.defaults({
     prefix: COUCH_DB_URL,
   })
-
-  return PouchDB
+  allDbs(pouch)
+  return pouch
 }
