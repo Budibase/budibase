8ad10976d3f03c2225a04873409014534e9703b3
Conor_Mack <cmckeown101@gmail.com>
2020-05-05 10:02:10 +0100

    Updates to naming conventions

--- packages/builder/src/components/userInterface/PropertyGroup.svelte
+++ packages/builder/src/components/userInterface/PropertyGroup.svelte
@@ -1,6 +1,6 @@
 <script>
-  export let title = ""
-  export let content = {}
+  export let name = ""
+  export let properties = {}
   export let componentInstance = {}
   export let componentDefinition = {}
 
@@ -8,24 +8,24 @@
 
   const propExistsOnComponentDef = prop => prop in componentDefinition.props
 
-  const capitalize = title => title[0].toUpperCase() + title.slice(1)
+  const capitalize = name => name[0].toUpperCase() + name.slice(1)
 
-  $: propertyKeys = Object.keys(content)
+  $: propertyKeys = Object.keys(properties)
   $: icon = show ? "ri-arrow-down-s-fill" : "ri-arrow-right-s-fill"
 </script>
 
 <div class="property-group-container" on:click={() => (show = !show)}>
-  <div class="property-group-title">
+  <div class="property-group-name">
     <div class="icon">
       <i class={icon} />
     </div>
-    <div class="title">{capitalize(title)}</div>
+    <div class="name">{capitalize(name)}</div>
   </div>
   <div class="property-panel" class:show>
     <ul>
       {#each propertyKeys as key}
         <!-- {#if propExistsOnComponentDef(key)} -->
-        <li>{content[key].displayName || capitalize(key)}</li>
+        <li>{properties[key].label || capitalize(key)}</li>
         <!-- {/if} -->
       {/each}
     </ul>
@@ -42,7 +42,7 @@
     padding: 5px;
   }
 
-  .property-group-title {
+  .property-group-name {
     cursor: pointer;
     flex: 0 0 20px;
     display: flex;
@@ -54,7 +54,7 @@
     text-align: center;
   }
 
-  .title {
+  .name {
     flex: 1;
     text-align: left;
   }
