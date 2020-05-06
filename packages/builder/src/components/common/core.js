0af5f12405ee84e836eadffc353e41b360a15518
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-05-06 20:49:21 +0100

    removed core library

--- packages/builder/src/components/common/core.js
+++ packages/builder/src/components/common/core.js
@@ -1,5 +1,3 @@
-import { common } from "../../../../core/src"
+import { flow } from "lodash/fp";
 
-export const pipe = common.$
-
-export const events = common.eventsList
+export const pipe = (arg, funcs) => flow(funcs)(arg)
--- packages/core/src/templateApi/getBehaviourSources.js
+++
@@ -1,3 +0,0 @@
-export const getBehaviourSources = async datastore => {
-  await datastore.loadFile("/.config/behaviourSources.js")
-}
