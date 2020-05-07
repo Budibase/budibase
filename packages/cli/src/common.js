0f7292f30e4fd44c1033e61086c5af5b0fd96b86
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-05-07 22:46:27 +0100

    final tidy up

--- packages/cli/src/common.js
+++ packages/cli/src/common.js
@@ -4,18 +4,5 @@ const { homedir } = require("os")
 module.exports.serverFileName = relativePath =>
   resolve(__dirname, "..", "node_modules", "@budibase", "server", relativePath)
 
-// module.exports.getAppContext = async ({ configName, masterIsCreated }) => {
-//   if (configName) {
-//     if (!configName.endsWith(".js")) {
-//       configName = `config.${configName}.js`
-//     }
-//   } else {
-//     configName = "config.js"
-//   }
-
-//   const config = require(resolve(cwd(), configName))()
-//   return await buildAppContext(config, masterIsCreated)
-// }
-
 module.exports.xPlatHomeDir = dir =>
   dir.startsWith("~") ? join(homedir(), dir.substring(1)) : dir
