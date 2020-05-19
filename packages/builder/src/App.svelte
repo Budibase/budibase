97af667e2f5cfe95524a17c8e3b813ad82b45c18
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-05-19 15:45:09 +0100

    routify config for electron

--- packages/builder/src/App.svelte
+++ packages/builder/src/App.svelte
@@ -2,7 +2,7 @@
   import Modal from "svelte-simple-modal"
   import { onMount } from "svelte"
   import { Router, basepath } from "@sveltech/routify"
-  import { routes } from "@sveltech/routify/tmp/routes"
+  import { routes } from "../routify/routes"
   import { store, initialise } from "builderStore"
   import AppNotification, {
     showAppNotification,
