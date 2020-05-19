d65acca512eb15a996205bf42507063c7735e11e
kevmodrome <kevin@kevmodro.me>
2020-05-19 15:19:32 +0200

    upgrades routify to latest version

--- packages/builder/src/pages/[application]/_layout.svelte
+++ packages/builder/src/pages/[application]/_layout.svelte
@@ -2,7 +2,7 @@
   import { store } from "builderStore"
 
   import { fade } from "svelte/transition"
-  import { isActive, goto, context } from "@sveltech/routify"
+  import { isActive, goto, layout } from "@sveltech/routify"
 
   import { SettingsIcon, PreviewIcon } from "components/common/Icons/"
   import IconButton from "components/common/IconButton.svelte"
@@ -23,8 +23,6 @@
       throw new Error(pkg)
     }
   }
-  $: ({ component } = $context)
-  $: list = component.parent.children.filter(child => child.isIndexable)
 </script>
 
 <div class="root">
@@ -38,12 +36,12 @@
       </button>
 
       <!-- This gets all indexable subroutes and sticks them in the top nav. -->
-      {#each list as { path, prettyName, children, meta }}
+      {#each $layout.children as { path, title }}
         <span
           class:active={$isActive(path)}
           class="topnavitem"
           on:click={() => $goto(path)}>
-          {prettyName}
+          {title}
         </span>
       {/each}
       <!-- <IconButton icon="home"
