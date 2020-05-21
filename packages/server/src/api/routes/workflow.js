d4d46cea8e945a1253f8a27309bbdaf96ebfa27f
Michael Shanks <mike@budibase.com>
2020-05-21 14:57:24 +0100

    auth for workflow endpoints

--- packages/server/src/api/routes/workflow.js
+++ packages/server/src/api/routes/workflow.js
@@ -1,13 +1,19 @@
 const Router = require("@koa/router")
 const controller = require("../controllers/workflow")
+const authorized = require("../../middleware/authorized")
+const { BUILDER } = require("../../utilities/accessLevels")
 
 const router = Router()
 
 router
-  .get("/api/:instanceId/workflows", controller.fetch)
-  .get("/api/:instanceId/workflows/:id", controller.find)
-  .post("/api/:instanceId/workflows", controller.create)
-  .put("/api/:instanceId/workflows/:id", controller.update)
-  .delete("/api/:instanceId/workflows/:id/:rev", controller.destroy)
+  .get("/api/:instanceId/workflows", authorized(BUILDER), controller.fetch)
+  .get("/api/:instanceId/workflows/:id", authorized(BUILDER), controller.find)
+  .post("/api/:instanceId/workflows", authorized(BUILDER), controller.create)
+  .put("/api/:instanceId/workflows/:id", authorized(BUILDER), controller.update)
+  .delete(
+    "/api/:instanceId/workflows/:id/:rev",
+    authorized(BUILDER),
+    controller.destroy
+  )
 
 module.exports = router
