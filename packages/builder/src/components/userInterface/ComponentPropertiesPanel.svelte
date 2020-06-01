d4b2fb57365318069a5d16f5e73e8afe1087ffde
Michael Shanks <mike@budibase.com>
2020-06-01 22:04:32 +0100

    bugfix: right nav scrolling

--- packages/builder/src/components/userInterface/ComponentPropertiesPanel.svelte
+++ packages/builder/src/components/userInterface/ComponentPropertiesPanel.svelte
@@ -105,7 +105,9 @@
     display: flex;
     flex-direction: column;
     overflow-x: hidden;
+    overflow-y:  hidden;
     padding: 20px;
+    box-sizing: border-box;
   }
 
   .title > div:nth-child(1) {
@@ -120,5 +122,7 @@
   .component-props-container {
     margin-top: 20px;
     flex: 1 1 auto;
+    min-height: 0;
+    overflow-y: auto;
   }
 </style>
