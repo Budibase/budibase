60424c6f0e2aa335de42c35d5587050a92c28bfc
kevmodrome <kevin@kevmodro.me>
2020-06-03 14:36:39 +0200

    adds options to navbar component

--- packages/builder/src/components/userInterface/temporaryPanelStructure.js
+++ packages/builder/src/components/userInterface/temporaryPanelStructure.js
@@ -349,7 +349,13 @@ export default {
             "A component for handling the navigation within your app.",
           icon: "ri-navigation-fill",
           children: [],
-          properties: { design: { ...all } },
+          properties: {
+            design: { ...all },
+            settings: [
+              { label: "Logo URL", key: "logoUrl", control: Input },
+              { label: "Title", key: "title", control: Input },
+              { label: "Background", key: "backgroundColor", control: Input }],
+          },
         },
       ],
     },
