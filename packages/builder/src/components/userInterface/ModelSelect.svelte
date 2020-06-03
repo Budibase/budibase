2f6e0f14aeb74d34ea16f86301c2443e2a3ec6ba
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-06-03 16:10:03 +0100

    fix dataform

--- packages/builder/src/components/userInterface/ModelSelect.svelte
+++ packages/builder/src/components/userInterface/ModelSelect.svelte
@@ -7,7 +7,7 @@
 <div class="uk-margin block-field">
   <div class="uk-form-controls">
     <select class="budibase__input" on:change {value}>
-      <option value=""></option>
+      <option value="" />
       {#each $backendUiStore.models as model}
         <option value={model._id}>{model.name}</option>
       {/each}
