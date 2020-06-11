202abaa13eff9deb78c4b56a35ce99735cabf49e
kevmodrome <kevin@kevmodro.me>
2020-06-11 12:04:31 +0200

    rework tests to use commands for DRYness

--- packages/builder/cypress/integration/createModel.spec.js
+++ packages/builder/cypress/integration/createModel.spec.js
@@ -1,73 +1,24 @@
 context('Create Model', () => {
 
-    beforeEach(() => {
+    before(() => {
         cy.visit('localhost:4001/_builder')
+        // https://on.cypress.io/type
+        cy.createApp('Model App', 'Model App Description')
     })
 
     // https://on.cypress.io/interacting-with-elements
-
     it('should create a new model', () => {
-        // https://on.cypress.io/type
-        cy.get('.banner-button')
-            .click()
-            .get('input[name="name"]')
-            .type('My Cool Application').should('have.value', 'My Cool Application')
 
-        cy.get('textarea[name="description"]')
-            .type('This is a description').should('have.value', 'This is a description')
+        cy.createModel('dog', 'name', 'age')
 
-        cy.contains('Save').click()
-
-        // Enter model name
-        cy.get('.budibase__input')
-            .type('dog')
-
-        // Add new field
-        cy.get('.new-field')
-            .click()
-
-        // Enter field name
-        cy.get('.budibase__input').first()
-            .type('name')
-
-        // Save
-        cy.contains('Save').click()
-
-
-        // Add new field
-        cy.get('.new-field')
-            .click()
-
-        // Enter field name
-        cy.get('.budibase__input').first()
-            .type('age')
-
-        cy.get('select').select('number')
-
-        // Save
-        cy.contains('Save').click()
-
-        cy.contains('age').should('exist')
-
-        // Save
-        cy.contains('Save').click()
-
+        // Check if model exists
         cy.get('.title').should('have.text', 'dog')
     })
     it('should add a record', () => {
+        // Page needs to be reloaded for some reason, cookie might be remove between tests?
+        cy.reload()
 
-        // Open just created app
-        cy.get(':nth-child(1) > .card-footer > .app-button')
-            .click()
-
-        // Open add record modal
-        cy.get('.button')
-            .click()
-
-        cy.get(':nth-child(1) > .uk-input').type('bob').get(':nth-child(2) > .uk-input').type('15')
-
-        // Save
-        cy.contains('Save').click()
+        cy.addRecord('bob', '15')
 
         cy.contains('bob').should('have.text', 'bob')
     })
---
+++ packages/builder/cypress/integration/createUser.spec.js
@@ -0,0 +1,19 @@
+context('Create Model', () => {
+
+    before(() => {
+        cy.visit('localhost:4001/_builder')
+        // https://on.cypress.io/type
+        cy.createApp('User App', 'This app is used to test user creation')
+    })
+
+    // https://on.cypress.io/interacting-with-elements
+    it('should create a user', () => {
+        // Close Model modal that shows up after creating an app
+        cy.get('.close').click()
+
+        cy.createUser('bbuser', 'test', 'ADMIN')
+
+        // Check to make sure user was created!
+        cy.contains('bbuser').should('have.text', 'bbuser')
+    })
+})
