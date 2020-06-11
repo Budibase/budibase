3ee5111d6f28db4600c3a00988f083389bfc80d5
kevmodrome <kevin@kevmodro.me>
2020-06-11 12:04:31 +0200

    rework tests to use commands for DRYness

--- packages/builder/cypress/integration/createApp.spec.js
+++ packages/builder/cypress/integration/createApp.spec.js
@@ -8,18 +8,10 @@ context('Create Application', () => {
 
     it('should create a new application', () => {
         // https://on.cypress.io/type
-        cy.get('.banner-button')
-            .click()
-            .get('input[name="name"]')
-            .type('My Cool Application').should('have.value', 'My Cool Application')
-
-        cy.get('textarea[name="description"]')
-            .type('This is a description').should('have.value', 'This is a description')
-
-        cy.contains('Save').click()
+        cy.createApp('My Cool App', 'This is a description')
 
         cy.visit('localhost:4001/_builder')
 
-        cy.contains('My Cool Application').should('exist')
+        cy.contains('My Cool App').should('exist')
     })
 })
--- packages/builder/cypress/support/commands.js
+++ packages/builder/cypress/support/commands.js
@@ -23,3 +23,62 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createApp", (name, description) => {
+    cy.get('.banner-button')
+        .click()
+        .get('input[name="name"]')
+        .type(name).should('have.value', name)
+
+    cy.get('textarea[name="description"]')
+        .type(description).should('have.value', description)
+
+    cy.contains('Save').click()
+})
+Cypress.Commands.add("createModel", (modelName, firstField, secondField) => {
+    // Enter model name
+    cy.get('.budibase__input')
+        .type(modelName)
+
+    // Add 'name' field
+    cy.get('.new-field')
+        .click()
+    cy.get('.budibase__input').first()
+        .type(firstField)
+    cy.contains('Save').click()
+
+
+    // Add 'age' field
+    cy.get('.new-field')
+        .click()
+    cy.get('.budibase__input').first()
+        .type(secondField)
+    cy.get('select').select('number')
+    cy.contains('Save').click()
+    cy.contains(secondField).should('exist')
+
+    // Save model
+    cy.contains('Save').click()
+})
+Cypress.Commands.add("addRecord", (firstField, secondField) => {
+    cy.contains('Create new record')
+        .click()
+
+    cy.get(':nth-child(1) > .uk-input').type(firstField).get(':nth-child(2) > .uk-input').type(secondField)
+
+    // Save
+    cy.contains('Save').click()
+})
+
+Cypress.Commands.add("createUser", (username, password, level) => {
+    // Create User
+    cy.get('.nav-group-header > .ri-add-line')
+        .click()
+
+    cy.get(':nth-child(2) > .uk-input').type(username)
+    cy.get(':nth-child(3) > .uk-input').type(password)
+    cy.get('.uk-select').select(level)
+
+    // Save
+    cy.contains('Save').click()
+})
