5d46c572b82ea6c389909ca71b77a61f0c6bc8b7
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-06-15 17:08:59 +0100

    lint :sparkles:

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,13 +1,13 @@
 const { resolve } = require("path")
 
 async function runServer() {
-  const budibaseDir = "~/.budibase";
+  const budibaseDir = "~/.budibase"
 
   process.env.BUDIBASE_DIR = resolve(budibaseDir)
 
-  const server = await require("./app")();
+  const server = await require("./app")()
   server.on("close", () => console.log("Server Closed"))
   console.log(`Budibase running on ${JSON.stringify(server.address())}`)
 }
 
-runServer();
+runServer()
