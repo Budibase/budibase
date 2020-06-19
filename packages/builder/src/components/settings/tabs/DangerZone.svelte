c9a49e4d4bbf0406fd9df7302fad587d0b988e66
kevmodrome <kevin@kevmodro.me>
2020-06-19 12:51:02 +0200

    bump bbui and add DangerZone design

--- packages/builder/src/components/settings/tabs/DangerZone.svelte
+++ packages/builder/src/components/settings/tabs/DangerZone.svelte
@@ -1,1 +1,43 @@
-Danger
+<script>
+  import { Input, TextArea, Button } from "@budibase/bbui"
+  let value = ""
+  let loading = false
+
+  const deleteApp = () => {
+    loading = true
+    // Do stuff here to delete app!
+    // Navigate to start
+  }
+</script>
+
+<div class="background">
+  <Input
+    on:change={e => (value = e.target.value)}
+    on:input={e => (value = e.target.value)}
+    thin
+    disabled={loading}
+    placeholder="Enter your name"
+    label="Type DELETE into the textbox, then click the following button to
+    delete your web app:" />
+
+  <Button
+    disabled={value !== 'DELETE' || loading}
+    primary
+    wide
+    on:click={deleteApp}>
+    Delete Entire Web App
+  </Button>
+</div>
+
+<style>
+  .background {
+    display: grid;
+    grid-gap: 16px;
+    border-radius: 5px;
+    background-color: var(--light-grey);
+    padding: 12px 12px 18px 12px;
+  }
+  .background :global(button) {
+    max-width: 100%;
+  }
+</style>
