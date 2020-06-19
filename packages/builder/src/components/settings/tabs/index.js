482d791b96d087d66cb6a14b37d8dae63eb15aa9
kevmodrome <kevin@kevmodro.me>
2020-06-19 09:14:03 +0200

    Sets up tabbed modal for settings

---
+++ packages/builder/src/components/settings/tabs/index.js
@@ -0,0 +1,5 @@
+export { default as General } from './General.svelte';
+export { default as Integrations } from './Integrations.svelte';
+export { default as Permissions } from './Permissions.svelte';
+export { default as Users } from './Users.svelte';
+export { default as DangerZone } from './DangerZone.svelte';
