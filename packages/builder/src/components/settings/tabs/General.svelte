310b41c4b32135167c915795d5e82f69b7e64b57
kevmodrome <kevin@kevmodro.me>
2020-06-19 12:09:27 +0200

    adds General tab design and updates bbui

--- packages/builder/src/components/settings/tabs/General.svelte
+++ packages/builder/src/components/settings/tabs/General.svelte
@@ -1,1 +1,24 @@
-General
+<script>
+  import { Input, TextArea, Button } from "@budibase/bbui"
+</script>
+
+<div class="container">
+  <div class="background">
+    <Input thin edit placeholder="Enter your name" label="Name" />
+  </div>
+  <div class="background">
+    <TextArea thin edit placeholder="Enter your name" label="Name" />
+  </div>
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-gap: 16px;
+  }
+  .background {
+    border-radius: 5px;
+    background-color: var(--light-grey);
+    padding: 12px 12px 18px 12px;
+  }
+</style>
