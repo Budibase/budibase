849fcfe8192ea446253293e12d928a01706c1d58
Michael Shanks <mike@budibase.com>
2020-06-19 07:04:55 +0100

    client - should recognise new cookie format

--- packages/client/src/render/getAppId.js
+++ packages/client/src/render/getAppId.js
@@ -2,8 +2,10 @@ export const getAppId = cookie => {
   const base64Token = cookie
     .split(";")
     .find(c => c.trim().startsWith("budibase:token"))
-    .split("=")[1]
+    .substring(lengthOfKey)
 
   const user = JSON.parse(atob(base64Token.split(".")[1]))
   return user.appId
 }
+
+const lengthOfKey = "budibase:token=".length
