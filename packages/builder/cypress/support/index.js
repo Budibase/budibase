a1df978b80b312a35e17be45842fa86222633497
Michael Shanks <mike@budibase.com>
2020-06-22 10:53:17 +0100

    cypress - retain "builder:token" cookie between tests

--- packages/builder/cypress/support/index.js
+++ packages/builder/cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import "./cookies"
 import "./commands"
 
 // Alternatively you can use CommonJS syntax:
