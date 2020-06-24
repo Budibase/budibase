1c63a93234b541d1bac524e7f8c31680f7d8166a
kevmodrome <kevin@kevmodro.me>
2020-06-24 15:04:01 +0200

    more work on settings modal

---
+++ packages/builder/src/components/settings/TabTitle.svelte
@@ -0,0 +1,13 @@
+<h3>
+  <slot />
+</h3>
+
+<style>
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    color: var(--ink);
+    margin-top: var(--space-lg);
+    margin-bottom: var(--space-md);
+  }
+</style>
