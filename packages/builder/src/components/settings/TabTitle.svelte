106b28ced68feb0bda80d6e4bb699d06cf9fbfb8
kevmodrome <kevin@kevmodro.me>
2020-06-25 12:41:22 +0200

    change spacing var to be hardcoded

--- packages/builder/src/components/settings/TabTitle.svelte
+++ packages/builder/src/components/settings/TabTitle.svelte
@@ -7,7 +7,7 @@
     font-size: 18px;
     font-weight: bold;
     color: var(--ink);
-    margin-top: var(--space-lg);
-    margin-bottom: var(--space-md);
+    margin-top: 40px;
+    margin-bottom: 20px;
   }
 </style>
