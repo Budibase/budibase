68e1441b83fe7de4c88a0fee7bb65b17ed9b2735
kevmodrome <kevin@kevmodro.me>
2020-06-25 14:25:17 +0200

    adds functionality to check list of users and add users in settings modal

---
+++ packages/builder/src/components/settings/UserRow.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Input, Select, Button } from "@budibase/bbui"
+  export let user
+</script>
+
+<div class="inputs">
+  <Input thin bind:value={user.username} name="Name" placeholder="Username" />
+  <Select bind:value={user.accessLevelId} thin>
+    <option value="ADMIN">Admin</option>
+    <option value="POWER_USER">Power User</option>
+  </Select>
+  <Button on:click>Edit</Button>
+</div>
+
+<style>
+  .inputs {
+    display: grid;
+    justify-items: stretch;
+    grid-gap: 18px;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+</style>
