cb0492886b1b06192635e2b6854cf027eb3c1255
Michael Shanks <mike@budibase.com>
2020-07-03 14:44:25 +0100

    hiding the `<EmptyModel />` for now

--- packages/builder/src/pages/[application]/backend/model/[selectedModel]/index.svelte
+++ packages/builder/src/pages/[application]/backend/model/[selectedModel]/index.svelte
@@ -23,9 +23,7 @@
   }
 </script>
 
-{#if selectedModel.schema && Object.keys(selectedModel.schema).length === 0}
-  <EmptyModel />
-{:else if $backendUiStore.selectedDatabase._id && selectedModel.name}
+{#if $backendUiStore.selectedDatabase._id && selectedModel.name}
   <ModelDataTable />
 {:else}
   <i style="color: var(--grey-4)">create your first table to start building</i>
