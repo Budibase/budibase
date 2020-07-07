2204f3589c5ee0d62b4900c88bebe1ef3b1e0908
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-07-07 20:17:09 +0100

    tidy up

--- packages/server/deploy.sh
+++
@@ -1,3 +0,0 @@
-docker build -t prod-budi-app-server .
-docker tag prod-budi-app-server:latest 545012296077.dkr.ecr.eu-west-1.amazonaws.com/prod-budi-app-server:latest
-docker push 545012296077.dkr.ecr.eu-west-1.amazonaws.com/prod-budi-app-server:latest 
