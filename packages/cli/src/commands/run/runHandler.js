fccfc3fd5c13460b5f60a20490a9d42428a3587b
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-07-10 16:17:04 +0100

    beta bugfixes

--- packages/cli/src/commands/run/runHandler.js
+++ packages/cli/src/commands/run/runHandler.js
@@ -1,13 +1,13 @@
 const { xPlatHomeDir } = require("../../common")
 const { resolve } = require("path")
 
-module.exports = ({ dir }) => {
+module.exports = async ({ dir }) => {
   dir = xPlatHomeDir(dir)
   process.env.BUDIBASE_DIR = resolve(dir)
   require("dotenv").config({ path: resolve(dir, ".env") })
 
   // dont make this a variable or top level require
-  // ti will cause environment module to be loaded prematurely
+  // it will cause environment module to be loaded prematurely
   return require("@budibase/server/src/app")().then(server => {
     server.on("close", () => console.log("Server Closed"))
     console.log(`Budibase running on ${JSON.stringify(server.address())}`)
