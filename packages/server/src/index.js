2673acd46e6304719554432e367a5a666bd61eef
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-07-10 16:17:04 +0100

    beta bugfixes

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,13 +1,22 @@
-const { resolve } = require("path")
+const { resolve, join } = require("path")
+const { homedir } = require("os")
 
 async function runServer() {
-  const budibaseDir = "~/.budibase"
+  const budibaseDir = join(homedir(), ".budibase"); 
+  process.env.BUDIBASE_DIR = budibaseDir 
 
-  process.env.BUDIBASE_DIR = resolve(budibaseDir)
+  require("dotenv").config({ path: resolve(budibaseDir, ".env") })
 
   const server = await require("./app")()
+
   server.on("close", () => console.log("Server Closed"))
   console.log(`Budibase running on ${JSON.stringify(server.address())}`)
 }
 
 runServer()
+
+
+process.on("SIGINT", function() {
+  console.log("Shutting down server.." );
+  process.exit(1);
+});
--- packages/server/src/middleware/authenticated.js
+++ packages/server/src/middleware/authenticated.js
@@ -21,6 +21,7 @@ module.exports = async (ctx, next) => {
     try {
       const jwtPayload = jwt.verify(builderToken, ctx.config.jwtSecret)
       ctx.isAuthenticated = jwtPayload.accessLevelId === BUILDER_LEVEL_ID
+      ctx.isBuilder = true
       ctx.user = {
         ...jwtPayload,
         accessLevel: await getAccessLevel(
