e227512448829b12d87b2e946ad8ba5d4bbce9c8
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-07-14 09:10:01 +0100

    fix path when running in electron

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,12 +1,15 @@
 const { resolve, join } = require("path")
 const { homedir } = require("os")
 const { app } = require("electron")
+const fixPath = require("fix-path")
 
 async function runServer() {
   const homeDir = app ? app.getPath("home") : homedir()
 
   const budibaseDir = join(homeDir, ".budibase")
   process.env.BUDIBASE_DIR = budibaseDir
+
+  fixPath()
 
   require("dotenv").config({ path: resolve(budibaseDir, ".env") })
 
