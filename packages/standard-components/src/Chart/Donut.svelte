cae50603aed34b5cd75fce049b19f41398b5e3cb
cmack <conoramck@gmail.com>
2020-07-20 16:25:50 +0100

    WIP: Work so far

---
+++ packages/standard-components/src/Chart/Donut.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { colors } from "britecharts"
+  import Chart, { colorSchemas } from "./Chart"
+
+  let type = "donut"
+
+  export let colorSchema = colorSchemas.britecharts
+  export let height = 200
+  export let width = 200
+  export let margin = { top: 0, right: 0, bottom: 0, left: 0 }
+
+  export let externalRadius = 25
+  export let hasFixedHighlightedSlice = false
+  export let hasLastHoverSliceHighlighted = false
+  export let internalRadius = 25
+  export let isAnimated = true
+  export let radiusHoverOffset = 0
+</script>
+
+<Chart {type} {...$$props} />
