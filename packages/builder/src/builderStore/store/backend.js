32cfc40fd41018bcbaf3bf613aaa42dc3d2e687c
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-07-21 10:24:09 +0100

    ensure table name is not persisted between apps

--- packages/builder/src/builderStore/store/backend.js
+++ packages/builder/src/builderStore/store/backend.js
@@ -2,23 +2,24 @@ import { writable } from "svelte/store"
 import { cloneDeep } from "lodash/fp"
 import api from "../api"
 
-export const getBackendUiStore = () => {
-  const INITIAL_BACKEND_UI_STATE = {
-    models: [],
-    views: [],
-    users: [],
-    selectedDatabase: {},
-    selectedModel: {},
-    draftModel: {},
-    tabs: {
-      SETUP_PANEL: "SETUP",
-      NAVIGATION_PANEL: "NAVIGATE",
-    },
-  }
+const INITIAL_BACKEND_UI_STATE = {
+  models: [],
+  views: [],
+  users: [],
+  selectedDatabase: {},
+  selectedModel: {},
+  draftModel: {},
+  tabs: {
+    SETUP_PANEL: "SETUP",
+    NAVIGATION_PANEL: "NAVIGATE",
+  },
+}
 
-  const store = writable(INITIAL_BACKEND_UI_STATE)
+export const getBackendUiStore = () => {
+  const store = writable({ ...INITIAL_BACKEND_UI_STATE })
 
   store.actions = {
+    reset: () => store.set({ ...INITIAL_BACKEND_UI_STATE }),
     database: {
       select: async db => {
         const modelsResponse = await api.get(`/api/models`)
@@ -78,7 +79,6 @@ export const getBackendUiStore = () => {
         }
 
         const SAVE_MODEL_URL = `/api/models`
-        console.log(updatedModel)
         const response = await api.post(SAVE_MODEL_URL, updatedModel)
         const savedModel = await response.json()
         await store.actions.models.fetch()
