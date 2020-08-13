764a09118c44f98ea041bdf3521c74205b82df94
Michael Shanks <mike@budibase.com>
2020-08-13 14:02:15 +0100

    bugfix: dnd over screenslot was throwing err

--- packages/builder/src/builderStore/store/index.js
+++ packages/builder/src/builderStore/store/index.js
@@ -25,6 +25,7 @@ import {
   saveScreenApi as _saveScreenApi,
   regenerateCssForCurrentScreen,
   generateNewIdsForComponent,
+  getComponentDefinition,
 } from "../storeUtils"
 export const getStore = () => {
   const initial = {
@@ -76,9 +77,6 @@ export const getStore = () => {
 }
 
 export default getStore
-
-export const getComponentDefinition = (state, name) =>
-  name.startsWith("##") ? getBuiltin(name) : state.components[name]
 
 const setPackage = (store, initial) => async pkg => {
   const [main_screens, unauth_screens] = await Promise.all([
