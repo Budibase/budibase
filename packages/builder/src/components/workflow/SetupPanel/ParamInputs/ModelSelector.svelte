cea2007b7c45594efa48dfaa463d8d8f36551bd0
Victoria Sloan <victoriasloan@gmail.com>
2020-08-30 14:49:25 +0100

    removing uk-form-controls from datepicker, componentselector, modelselector & recordselector

--- packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
+++ packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
@@ -5,12 +5,10 @@
 </script>
 
 <div class="bb-margin-xl block-field">
-  <div class="uk-form-controls">
-    <select class="budibase__input" bind:value>
-      <option value="" />
-      {#each $backendUiStore.models as model}
-        <option value={model}>{model.name}</option>
-      {/each}
-    </select>
-  </div>
+  <select class="budibase__input" bind:value>
+    <option value="" />
+    {#each $backendUiStore.models as model}
+      <option value={model}>{model.name}</option>
+    {/each}
+  </select>
 </div>
--- packages/builder/src/components/workflow/SetupPanel/ParamInputs/RecordSelector.svelte
+++ packages/builder/src/components/workflow/SetupPanel/ParamInputs/RecordSelector.svelte
@@ -6,20 +6,18 @@
 </script>
 
 <div class="bb-margin-xl block-field">
-  <div class="uk-form-controls">
-    <select class="budibase__input" bind:value={value.model}>
-      {#each $backendUiStore.models as model}
-        <option value={model}>{model.name}</option>
-      {/each}
-    </select>
-  </div>
+  <select class="budibase__input" bind:value={value.model}>
+    {#each $backendUiStore.models as model}
+      <option value={model}>{model.name}</option>
+    {/each}
+  </select>
 </div>
 
 {#if value.model}
   <div class="bb-margin-xl block-field">
     <label class="uk-form-label fields">Fields</label>
     {#each Object.keys(value.model.schema) as field}
-      <div class="uk-form-controls bb-margin-xl">
+      <div class="bb-margin-xl">
         <Input bind:value={value[field]} label={field} />
       </div>
     {/each}
