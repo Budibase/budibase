e41bdf73729c4f3fd0c2e1c2a7bd750da28c7cba
Martin McKeaveney <martin@budibase.com>
2020-09-09 17:04:49 +0100

    date picker component (#591)
    * date picker component
    
    * use bbui date picker in standard components
    
    * using bbui component

---
+++ packages/standard-components/src/DatePicker.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Flatpickr from "svelte-flatpickr"
+  import { DatePicker } from "@budibase/bbui"
+
+  export let placeholder
+  export let value
+
+  export let _bb
+
+  function handleChange(event) {
+    const [fullDate, dateStr, instance] = event.detail
+    if (_bb) {
+      _bb.setBinding("value", fullDate)
+    }
+  }
+</script>
+
+<DatePicker {placeholder} on:change={handleChange} {value} />
