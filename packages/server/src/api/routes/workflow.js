ad48b9fdd204a254ec5aa8fa90d3896b75c46902
mike12345567 <me@michaeldrury.co.uk>
2020-09-10 17:46:34 +0100

    Adding joi validation to the workflow update and create to make sure structure is adhered to, this is built through a curried middleware, to validate on headers, body etc add multiple different validator middlewares to your endpoint.

--- packages/server/src/api/routes/workflow.js
+++ packages/server/src/api/routes/workflow.js
@@ -1,9 +1,40 @@
 const Router = require("@koa/router")
 const controller = require("../controllers/workflow")
 const authorized = require("../../middleware/authorized")
+const joiValidator = require("../../middleware/joi-validator")
 const { BUILDER } = require("../../utilities/accessLevels")
+const Joi = require("joi")
 
 const router = Router()
+
+// prettier-ignore
+function generateStepSchema(allowStepTypes) {
+  return Joi.object({
+    stepId: Joi.string().required(),
+    id: Joi.string().required(),
+    description: Joi.string().required(),
+    name: Joi.string().required(),
+    tagline: Joi.string().required(),
+    icon: Joi.string().required(),
+    params: Joi.object(),
+    // TODO: validate args a bit more deeply
+    args: Joi.object(),
+    type: Joi.string().required().valid(...allowStepTypes),
+  }).unknown(true)
+}
+
+// prettier-ignore
+const workflowValidator = joiValidator.body(Joi.object({
+  live: Joi.bool(),
+  id: Joi.string().required(),
+  rev: Joi.string().required(),
+  name: Joi.string().required(),
+  type: Joi.string().valid("workflow").required(),
+  definition: Joi.object({
+    steps: Joi.array().required().items(generateStepSchema(["ACTION", "LOGIC"])),
+    trigger: generateStepSchema(["TRIGGER"]).required(),
+  }).required().unknown(true),
+}).unknown(true))
 
 router
   .get("/api/workflows/trigger/list", authorized(BUILDER), controller.getTriggerList)
@@ -11,8 +42,8 @@ router
   .get("/api/workflows/logic/list", authorized(BUILDER), controller.getLogicList)
   .get("/api/workflows", authorized(BUILDER), controller.fetch)
   .get("/api/workflows/:id", authorized(BUILDER), controller.find)
-  .put("/api/workflows", authorized(BUILDER), controller.update)
-  .post("/api/workflows", authorized(BUILDER), controller.create)
+  .put("/api/workflows", authorized(BUILDER), workflowValidator, controller.update)
+  .post("/api/workflows", authorized(BUILDER), workflowValidator, controller.create)
   .post("/api/workflows/:id/trigger", controller.trigger)
   .delete("/api/workflows/:id/:rev", authorized(BUILDER), controller.destroy)
 
