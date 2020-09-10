77f32c8a0fd53e22a5a400fdb7cbda1c943ead4f
mike12345567 <me@michaeldrury.co.uk>
2020-09-10 15:00:21 +0100

    Major update to server workflow backend, implementing a very basic queue API in between triggers and orchestration and then moving everything around to keep it generally more concise, creating a directory to handle all workflow related matters as they do not pertain to the API (running of workflows is a completely separate operation).

---
+++ packages/server/src/workflows/triggers.js
@@ -0,0 +1,32 @@
+const CouchDB = require("../db")
+const emitter = require("../events/index")
+const InMemoryQueue = require("./queue/inMemoryQueue")
+
+let workflowQueue = new InMemoryQueue()
+
+async function queueRelevantWorkflows(event, eventType) {
+  const db = new CouchDB(event.instanceId)
+  const workflowsToTrigger = await db.query("database/by_workflow_trigger", {
+    key: [eventType],
+    include_docs: true,
+  })
+
+  const workflows = workflowsToTrigger.rows.map(wf => wf.doc)
+  for (let workflow of workflows) {
+    workflowQueue.add({ workflow, event })
+  }
+}
+
+emitter.on("record:save", async function(event) {
+  await queueRelevantWorkflows(event, "record:save")
+})
+
+emitter.on("record:delete", async function(event) {
+  await queueRelevantWorkflows(event, "record:delete")
+})
+
+module.exports.externalTrigger = async function(workflow, params) {
+  workflowQueue.add({ workflow, event: params })
+}
+
+module.exports.workflowQueue = workflowQueue
