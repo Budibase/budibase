4d118b6eed513457cfb8afe26dea73537c26db0c
Andrew Kingston <aptkingston@ssl-mail.com>
2020-09-10 07:35:24 +0100

    Fix arrow not transitioning out causing jumps when removing workflow blocks

--- packages/builder/src/components/workflow/WorkflowBuilder/flowchart/Arrow.svelte
+++ packages/builder/src/components/workflow/WorkflowBuilder/flowchart/Arrow.svelte
@@ -1,4 +1,9 @@
+<script>
+  import { fade } from "svelte/transition"
+</script>
+
 <svg
+  transition:fade
   width="9"
   height="75"
   viewBox="0 0 9 75"
@@ -7,3 +12,9 @@
   <path d="M5.0625 70H9L4.5 75L0 70H3.9375V65H5.0625V70Z" fill="#ADAEC4" />
   <rect x="4" width="1" height="65" fill="#ADAEC4" />
 </svg>
+
+<style>
+  svg {
+    margin: 8px 0;
+  }
+</style>
