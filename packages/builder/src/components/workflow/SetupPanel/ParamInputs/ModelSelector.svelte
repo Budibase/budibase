7ef5d8a9b8a10ad3f3a5cd029a00ef6395f3a152
Andrew Kingston <aptkingston@ssl-mail.com>
2020-09-16 13:55:28 +0100

    Update builder to support new block definitions with JSON schema

--- packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
+++ packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
@@ -2,19 +2,10 @@
   import { backendUiStore } from "builderStore"
 
   export let value
-  $: modelId = value ? value._id : ""
-
-  function onChange(e) {
-    value = $backendUiStore.models.find(model => model._id === e.target.value)
-  }
 </script>
 
 <div class="block-field">
-  <select
-    class="budibase__input"
-    value={modelId}
-    on:blur={onChange}
-    on:change={onChange}>
+  <select class="budibase__input" bind:value>
     <option value="">Choose an option</option>
     {#each $backendUiStore.models as model}
       <option value={model._id}>{model.name}</option>
