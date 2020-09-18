6ac5e792b5a9eb9bb805b6596fa24431d6c91e36
mike12345567 <me@michaeldrury.co.uk>
2020-09-18 16:50:52 +0100

    Initial work towards async block loading, it will pull the manifest.json from a specified bucket and load packages in async when it does not have them - this will need another feature for when running in the cloud to pull all packages it doesn't currently have to the install directory (EFS).

--- packages/server/src/workflows/actions.js
+++ packages/server/src/workflows/actions.js
@@ -2,14 +2,20 @@ const sendEmail = require("./steps/sendEmail")
 const saveRecord = require("./steps/saveRecord")
 const deleteRecord = require("./steps/deleteRecord")
 const createUser = require("./steps/createUser")
+const environment = require("../environment")
+const download = require("download")
+const fetch = require("node-fetch")
+const path = require("path")
+const Sentry = require("@sentry/node")
 
+const AUTOMATION_MANIFEST = "manifest.json"
+const AUTOMATION_BUNDLE = "bundle.js"
 const BUILTIN_ACTIONS = {
   SEND_EMAIL: sendEmail.run,
   SAVE_RECORD: saveRecord.run,
   DELETE_RECORD: deleteRecord.run,
   CREATE_USER: createUser.run,
 }
-
 const BUILTIN_DEFINITIONS = {
   SEND_EMAIL: sendEmail.definition,
   SAVE_RECORD: saveRecord.definition,
@@ -17,11 +23,54 @@ const BUILTIN_DEFINITIONS = {
   CREATE_USER: createUser.definition,
 }
 
+let MANIFEST = null
+
+async function downloadPackage(name, version, pathToInstall) {
+  await download(
+    `${environment.AUTOMATION_BUCKET}/${name}/${version}/${AUTOMATION_BUNDLE}`,
+    pathToInstall
+  )
+  return require(path.join(pathToInstall, AUTOMATION_BUNDLE))
+}
+
 module.exports.getAction = async function(actionName) {
   if (BUILTIN_ACTIONS[actionName] != null) {
     return BUILTIN_ACTIONS[actionName]
   }
-  // TODO: load async actions here
+  // env setup to get async packages
+  if (!MANIFEST || !MANIFEST.packages || !MANIFEST.packages[actionName]) {
+    return null
+  }
+  let pkg = MANIFEST.packages[actionName]
+  let toInstall = path.join(
+    environment.AUTOMATION_DIRECTORY,
+    pkg.stepId,
+    pkg.version
+  )
+  try {
+    return require(path.join(toInstall, AUTOMATION_BUNDLE))
+  } catch (err) {
+    return downloadPackage(pkg.stepId, pkg.version, toInstall)
+  }
 }
 
+module.exports.init = async function() {
+  // env setup to get async packages
+  try {
+    if (environment.AUTOMATION_DIRECTORY && environment.AUTOMATION_BUCKET) {
+      let response = await fetch(
+        `${environment.AUTOMATION_BUCKET}/${AUTOMATION_MANIFEST}`
+      )
+      MANIFEST = await response.json()
+    }
+  } catch (err) {
+    Sentry.captureException(err)
+  }
+  module.exports.DEFINITIONS =
+    MANIFEST && MANIFEST.packages
+      ? Object.assign(MANIFEST.packages, BUILTIN_DEFINITIONS)
+      : BUILTIN_DEFINITIONS
+}
+
+module.exports.DEFINITIONS = BUILTIN_DEFINITIONS
 module.exports.BUILTIN_DEFINITIONS = BUILTIN_DEFINITIONS
