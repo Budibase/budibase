b5f42384e3c4cb60afb51ce971e2b6b012bb9603
mike12345567 <me@michaeldrury.co.uk>
2020-09-22 10:12:17 +0100

    Updating GH actions to use Node 12 LTS to be inline with Node version used in Docker.

--- packages/server/src/workflows/index.js
+++ packages/server/src/workflows/index.js
@@ -1,21 +1,21 @@
 const triggers = require("./triggers")
 const environment = require("../environment")
-//const workerFarm = require("worker-farm")
+const workerFarm = require("worker-farm")
 const singleThread = require("./thread")
 
-//let workers = workerFarm(require.resolve("./thread"))
+let workers = workerFarm(require.resolve("./thread"))
 
-// function runWorker(job) {
-//   return new Promise((resolve, reject) => {
-//     workers(job, err => {
-//       if (err) {
-//         reject(err)
-//       } else {
-//         resolve()
-//       }
-//     })
-//   })
-// }
+function runWorker(job) {
+  return new Promise((resolve, reject) => {
+    workers(job, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 /**
  * This module is built purely to kick off the worker farm and manage the inputs/outputs
@@ -23,9 +23,9 @@ const singleThread = require("./thread")
 module.exports.init = function() {
   triggers.workflowQueue.process(async job => {
     if (environment.BUDIBASE_ENVIRONMENT === "PRODUCTION") {
-      //await runWorker(job)
-      await singleThread(job)
+      await runWorker(job)
     } else {
+      console.log("Testing standard thread")
       await singleThread(job)
     }
   })
