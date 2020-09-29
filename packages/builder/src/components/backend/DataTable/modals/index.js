db47e5c5813881be25b34ae697a3733f22251374
Andrew Kingston <aptkingston@ssl-mail.com>
2020-09-29 13:52:26 +0100

    Bump BBUI version

--- packages/builder/src/components/backend/DataTable/modals/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CreateEditRecordModal } from "./CreateEditRecord.svelte"
-export { default as CreateEditColumnModal } from "./CreateEditColumn.svelte"
-export { default as RecordFieldControlModal } from "./RecordFieldControl.svelte"
