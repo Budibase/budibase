2aff68b24598821d2582342534f6b2eec324fcdd
mike12345567 <me@michaeldrury.co.uk>
2020-10-02 12:37:46 +0100

    Adding jsdoc to the db utils and removing the views, this is a breaking change for existing apps.

--- packages/server/src/db/utils.js
+++ packages/server/src/db/utils.js
@@ -1,3 +1,5 @@
+const newid = require("./newid")
+
 const DocumentTypes = {
   MODEL: "model",
   RECORD: "record",
@@ -12,6 +14,15 @@ exports.DocumentTypes = DocumentTypes
 
 const UNICODE_MAX = "\ufff0"
 
+/**
+ * If creating DB allDocs/query params with only a single input this can be used, this
+ * is usually the case for top level documents like models, automations, users etc.
+ * @param {string} docType The type of document which input params are being built for, e.g. user,
+ * link, app, model and so on.
+ * @param {string|null} input The input if looking for a particular entry.
+ * @param {object} otherProps Add any other properties onto the request, e.g. include_docs.
+ * @returns {object} Parameters which can then be used with an allDocs request.
+ */
 function singleInputParams(docType, input, otherProps = {}) {
   if (input == null) {
     input = ""
@@ -23,17 +34,32 @@ function singleInputParams(docType, input, otherProps = {}) {
   }
 }
 
-exports.getModelParams = (modelId, otherProps = {}) => {
+/**
+ * Gets parameters for retrieving models, this is a utility function for the singleInputParams function.
+ */
+exports.getModelParams = (modelId = null, otherProps = {}) => {
   return singleInputParams(DocumentTypes.MODEL, modelId, otherProps)
 }
 
-exports.generateModelID = modelId => {
-  return `${DocumentTypes.MODEL}:${modelId}`
+/**
+ * Generates a new model ID.
+ * @returns {string} The new model ID which the model doc can be stored under.
+ */
+exports.generateModelID = () => {
+  return `${DocumentTypes.MODEL}:${newid()}`
 }
 
-exports.getRecordParams = (modelId, recordId, otherProps = {}) => {
-  if (modelId == null && recordId != null) {
-    throw "Cannot build key for record ID without a model ID"
+/**
+ * Gets the DB allDocs/query params for retrieving a record.
+ * @param {string} modelId The model in which the records have been stored.
+ * @param {string|null} recordId The ID of the record which is being specifically queried for. This can be
+ * left null to get all the records in the model.
+ * @param {object} otherProps Any other properties to add to the request.
+ * @returns {object} Parameters which can then be used with an allDocs request.
+ */
+exports.getRecordParams = (modelId, recordId = null, otherProps = {}) => {
+  if (modelId == null) {
+    throw "Cannot build params for records without a model ID"
   }
   const endOfKey = recordId == null ? `${modelId}:` : `${modelId}:${recordId}`
   return {
@@ -43,44 +69,86 @@ exports.getRecordParams = (modelId, recordId, otherProps = {}) => {
   }
 }
 
-exports.generateRecordID = (modelId, recordId) => {
-  return `${DocumentTypes.RECORD}:${modelId}:${recordId}`
+/**
+ * Gets a new record ID for the specified model.
+ * @param {string} modelId The model which the record is being created for.
+ * @returns {string} The new ID which a record doc can be stored under.
+ */
+exports.generateRecordID = modelId => {
+  return `${DocumentTypes.RECORD}:${modelId}:${newid()}`
 }
 
-exports.getUserParams = (username, otherProps = {}) => {
+/**
+ * Gets parameters for retrieving users, this is a utility function for the singleInputParams function.
+ */
+exports.getUserParams = (username = null, otherProps = {}) => {
   return singleInputParams(DocumentTypes.USER, username, otherProps)
 }
 
+/**
+ * Generates a new user ID based on the passed in username.
+ * @param {string} username The username which the ID is going to be built up of.
+ * @returns {string} The new user ID which the user doc can be stored under.
+ */
 exports.generateUserID = username => {
   return `${DocumentTypes.USER}:${username}`
 }
 
-exports.getAutomationParams = (automationId, otherProps = {}) => {
+/**
+ * Gets parameters for retrieving automations, this is a utility function for the singleInputParams function.
+ */
+exports.getAutomationParams = (automationId = null, otherProps = {}) => {
   return singleInputParams(DocumentTypes.AUTOMATION, automationId, otherProps)
 }
 
-exports.generateAutomationID = automationId => {
-  return `${DocumentTypes.AUTOMATION}:${automationId}`
+/**
+ * Generates a new automation ID.
+ * @returns {string} The new automation ID which the automation doc can be stored under.
+ */
+exports.generateAutomationID = () => {
+  return `${DocumentTypes.AUTOMATION}:${newid()}`
 }
 
-// for now link records still have their own view as we can walk multiple directions
+/**
+ * Generates a new link doc ID. This is currently not usable with the alldocs call,
+ * instead a view is built to make walking to tree easier.
+ * @param {string} modelId1 The ID of the linker model.
+ * @param {string} modelId2 The ID of the linked model.
+ * @param {string} recordId1 The ID of the linker record.
+ * @param {string} recordId2 The ID of the linked record.
+ * @returns {string} The new link doc ID which the automation doc can be stored under.
+ */
 exports.generateLinkID = (modelId1, modelId2, recordId1, recordId2) => {
   return `${DocumentTypes.AUTOMATION}:${modelId1}:${modelId2}:${recordId1}:${recordId2}`
 }
 
-exports.generateAppID = appId => {
-  return `${DocumentTypes.APP}:${appId}`
+/**
+ * Generates a new app ID.
+ * @returns {string} The new app ID which the app doc can be stored under.
+ */
+exports.generateAppID = () => {
+  return `${DocumentTypes.APP}:${newid()}`
 }
 
-exports.getAppParams = (appId, otherProps = {}) => {
+/**
+ * Gets parameters for retrieving apps, this is a utility function for the singleInputParams function.
+ */
+exports.getAppParams = (appId = null, otherProps = {}) => {
   return singleInputParams(DocumentTypes.APP, appId, otherProps)
 }
 
-exports.generateAccessLevelID = accessLevelId => {
-  return `${DocumentTypes.ACCESS_LEVEL}:${accessLevelId}`
+/**
+ * Generates a new access level ID.
+ * @returns {string} The new access level ID which the access level doc can be stored under.
+ */
+exports.generateAccessLevelID = () => {
+  return `${DocumentTypes.ACCESS_LEVEL}:${newid()}`
 }
 
-exports.getAccessLevelParams = (accessLevelId, otherProps) => {
+/**
+ * Gets parameters for retrieving an access level, this is a utility function for the singleInputParams function.
+ */
+exports.getAccessLevelParams = (accessLevelId = null, otherProps = {}) => {
   return singleInputParams(
     DocumentTypes.ACCESS_LEVEL,
     accessLevelId,
