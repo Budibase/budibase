fb5503d10ec4ce443ae7717f4fa011743c5a06bb
Michael Shanks <mike@budibase.com>
2020-10-07 22:30:51 +0100

    RecordDetail & NewRecord now baseComponents

---
+++ packages/standard-components/src/NewRecord.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from "svelte"
+  import { isEmpty } from "lodash/fp"
+
+  export let _bb
+  export let model
+
+  let record = {}
+
+  $: {
+    record.modelId = model
+  }
+
+  let target
+
+  onMount(async () => {
+    if (!isEmpty(record) && record.model) {
+      _bb.attachChildren(target, {
+        context: record,
+      })
+    }
+  })
+</script>
+
+<section bind:this={target} />
