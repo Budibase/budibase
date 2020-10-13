14af18d27b1584dcc9f3ed88224a6776118587f7
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-10-13 16:33:45 +0100

    fix issues from eslint plugin prettier

--- packages/builder/src/components/settings/tabs/Users.svelte
+++ packages/builder/src/components/settings/tabs/Users.svelte
@@ -73,7 +73,7 @@
     <Label extraSmall grey>Current Users</Label>
     {#await fetchUsersPromise}
       Loading...
-    {:then [object Object]}
+    {:then users}
       <ul>
         {#each users as user}
           <li>
@@ -83,7 +83,7 @@
           <li>No Users found</li>
         {/each}
       </ul>
-    {:catch [object Object]}
+    {:catch error}
       Something went wrong when trying to fetch users. Please refresh (CMD + R /
       CTRL + R) the page and try again.
     {/await}
