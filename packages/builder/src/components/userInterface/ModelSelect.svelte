5af85541ecbbee028cdd7a5bcf0291e14bf71ae5
Andrew Kingston <aptkingston@ssl-mail.com>
2020-10-13 08:26:13 +0100

    Fix ModelSelect styles in frontend settings

--- packages/builder/src/components/userInterface/ModelSelect.svelte
+++ packages/builder/src/components/userInterface/ModelSelect.svelte
@@ -5,9 +5,22 @@
   export let value
 </script>
 
-<Select thin secondary wide on:change {value}>
-  <option value="" />
-  {#each $backendUiStore.models as model}
-    <option value={model._id}>{model.name}</option>
-  {/each}
-</Select>
+<div>
+  <Select thin secondary wide on:change {value}>
+    <option value="">Choose a table</option>
+    {#each $backendUiStore.models as model}
+      <option value={model._id}>{model.name}</option>
+    {/each}
+  </Select>
+</div>
+
+<style>
+  div {
+    flex: 1 1 auto;
+    display: flex;
+    flex-direction: row;
+  }
+  div :global(> *) {
+    flex: 1 1 auto;
+  }
+</style>
