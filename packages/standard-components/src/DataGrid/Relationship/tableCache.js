0b5f516485608c7e5182eb593c7214ec85ed39d2
Andrew Kingston <aptkingston@ssl-mail.com>
2020-10-15 11:08:56 +0100

    Add support for rendering real relationships inside grid

---
+++ packages/standard-components/src/DataGrid/Relationship/tableCache.js
@@ -0,0 +1,21 @@
+import api from "../../api"
+
+let cache = {}
+
+async function fetchTable(id) {
+  const FETCH_TABLE_URL = `/api/tables/${id}`
+  const response = await api.get(FETCH_TABLE_URL)
+  return await response.json()
+}
+
+export default async function getTable(tableId) {
+  if (!tableId) {
+    return null
+  }
+  if (!cache[tableId]) {
+    console.log("cache miss for " + tableId)
+    cache[tableId] = fetchTable(tableId)
+    cache[tableId] = await cache[tableId]
+  }
+  return await cache[tableId]
+}
