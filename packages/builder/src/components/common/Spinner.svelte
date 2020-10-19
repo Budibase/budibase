c319054c30b1e5e06a15c8a62ad6c97b165fc1ea
mike12345567 <me@michaeldrury.co.uk>
2020-10-19 15:33:26 +0100

    Fixing some issues seen with cloudfront and adding a bit more UI around the deployment history as pending can be happening for some time.

--- packages/builder/src/components/common/Spinner.svelte
+++ packages/builder/src/components/common/Spinner.svelte
@@ -4,4 +4,12 @@
   export let size = "60"
 </script>
 
+<div class="spinner-container">
 <Circle {size} color="#000000" unit="px" />
+</div>
+
+<style>
+  .spinner-container {
+    display: block;
+  }
+</style>
