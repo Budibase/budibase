cecf00a09e7bb4c4f8ba77e5a793967ae4f7bbd1
Michael Shanks <mike@budibase.com>
2020-10-22 21:59:40 +0100

    Feedback - added Modal after deploy

---
+++ packages/builder/src/components/userInterface/Feedback/FeedbackNavLink.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { FeedbackIcon } from "components/common/Icons/"
+  import { Popover } from "@budibase/bbui"
+  import FeedbackIframe from "./FeedbackIframe.svelte"
+
+  let iconContainer
+  let popover
+</script>
+
+<span class="container" bind:this={iconContainer} on:click={popover.show}>
+  <FeedbackIcon />
+</span>
+<Popover bind:this={popover} anchor={iconContainer} align="right">
+  <FeedbackIframe on:finished={popover.hide} />
+</Popover>
+
+<style>
+  .container {
+    cursor: pointer;
+    color: var(--grey-7);
+    margin: 0 20px 0 0;
+    font-weight: 500;
+    font-size: 1rem;
+    height: 100%;
+    display: flex;
+    flex: 1;
+    align-items: center;
+    box-sizing: border-box;
+  }
+
+  .container:hover {
+    color: var(--ink);
+    font-weight: 500;
+  }
+</style>
