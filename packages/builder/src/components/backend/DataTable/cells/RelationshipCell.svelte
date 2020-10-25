4549e8e5b27bcd478504d43222f3ea6d5d34d7ae
Martin McKeaveney <martin@budibase.com>
2020-10-25 17:05:11 +0000

    reinstating relationships

--- packages/builder/src/components/backend/DataTable/cells/RelationshipCell.svelte
+++ packages/builder/src/components/backend/DataTable/cells/RelationshipCell.svelte
@@ -1,80 +1,28 @@
 <script>
-  import { onMount } from "svelte"
-  import { goto, params } from "@sveltech/routify"
-  import api from "builderStore/api"
-  import { getTable } from "./tableCache"
-
   export let columnName
   export let row
+  export let selectRelationship
 
   $: count =
     row && columnName && Array.isArray(row[columnName])
       ? row[columnName].length
       : 0
-  let linkedRows = []
-  let displayColumn
-
-  onMount(async () => {
-    linkedRows = await fetchLinkedRowsData(row, columnName)
-    if (linkedRows && linkedRows.length) {
-      const table = await getTable(linkedRows[0].tableId)
-      if (table && table.primaryDisplay) {
-        displayColumn = table.primaryDisplay
-      }
-    }
-  })
-
-  async function fetchLinkedRowsData(row, columnName) {
-    if (!row || !row._id) {
-      return []
-    }
-    const QUERY_URL = `/api/${row.tableId}/${row._id}/enrich`
-    const response = await api.get(QUERY_URL)
-    const enrichedRow = await response.json()
-    return enrichedRow[columnName]
-  }
-
-  // TODO: wire up
-  function drillIntoRelationship(row, fieldName) {
-    if (!row?.[fieldName]?.length) {
-      return
-    }
-    $goto(
-      `/${$params.application}/data/table/${tableId}/relationship/${row._id}/${fieldName}`
-    )
-  }
 </script>
 
-<div class="container">
-  {#if linkedRows && linkedRows.length && displayColumn}
-    {#each linkedRows as linkedRow}
-      {#if linkedRow[displayColumn] != null && linkedRow[displayColumn] !== ''}
-        <div class="linked-row">{linkedRow[displayColumn]}</div>
-      {/if}
-    {/each}
-  {:else}{count} related row(s){/if}
+<div
+  class:link={count}
+  on:click={() => selectRelationship(row, columnName)}>
+  {count}
+  related row(s)
 </div>
 
 <style>
-  .container {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: var(--spacing-xs);
-    width: 100%;
+  .link {
+    text-decoration: underline;
   }
 
-  /* This styling is opinionated to ensure these always look consistent */
-  .linked-row {
-    color: white;
-    background-color: #616161;
-    border-radius: var(--border-radius-xs);
-    padding: var(--spacing-xs) var(--spacing-s) calc(var(--spacing-xs) + 1px)
-      var(--spacing-s);
-    line-height: 1;
-    font-size: 0.8em;
-    font-family: var(--font-sans);
-    font-weight: 500;
+  .link:hover {
+    color: var(--grey-6);
+    cursor: pointer;
   }
 </style>
--- packages/builder/src/components/backend/DataTable/cells/cellRenderers.js
+++ packages/builder/src/components/backend/DataTable/cells/cellRenderers.js
@@ -6,7 +6,6 @@ import RelationshipDisplay from "./RelationshipCell.svelte"
 const renderers = {
   attachment: attachmentRenderer,
   link: linkedRowRenderer,
-  boolean: booleanRenderer,
 }
 
 export function getRenderer(schema, editable) {
@@ -30,33 +29,6 @@ export function editRowRenderer(params) {
   return container
 }
 
-/* eslint-disable no-unused-vars */
-function booleanRenderer(options, constraints) {
-  return params => {
-    let container = document.createElement("input")
-
-    // TODO: implement
-
-    return container
-
-    // const toggle = e => {
-    //   params.value = !params.value
-    //   params.setValue(e.currentTarget.checked)
-    // }
-    // let input = document.createElement("input")
-    // input.style.display = "grid"
-    // input.style.placeItems = "center"
-    // input.style.height = "100%"
-    // input.type = "checkbox"
-    // input.checked = params.value
-    // if (editable) {
-    //   input.addEventListener("click", toggle)
-    // } else {
-    //   input.disabled = true
-    // }
-    // return input
-  }
-}
 
 /* eslint-disable no-unused-vars */
 function attachmentRenderer(options, constraints, editable) {
@@ -82,11 +54,10 @@ function attachmentRenderer(options, constraints, editable) {
 }
 
 /* eslint-disable no-unused-vars */
-function linkedRowRenderer(options, constraints, editable) {
+function linkedRowRenderer() {
   return params => {
     let container = document.createElement("div")
     container.style.display = "grid"
-    container.style.placeItems = "center"
     container.style.height = "100%"
 
     new RelationshipDisplay({
@@ -94,6 +65,7 @@ function linkedRowRenderer(options, constraints, editable) {
       props: {
         row: params.data,
         columnName: params.column.colId,
+        selectRelationship: params.selectRelationship
       },
     })
 
