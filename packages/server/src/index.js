9b0dd682ad8817d17d5969ceceeff9202f034334
mike12345567 <me@michaeldrury.co.uk>
2020-10-26 17:58:01 +0000

    Fixing typo.

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -7,7 +7,7 @@ const fs = require("fs")
 async function runServer() {
   if (isDev() && !fs.existsSync(budibaseTempDir())) {
     console.error(
-      "Please run a build before attempting to run server indepdently to fill 'tmp' directory."
+      "Please run a build before attempting to run server independently to fill 'tmp' directory."
     )
     process.exit(-1)
   }
