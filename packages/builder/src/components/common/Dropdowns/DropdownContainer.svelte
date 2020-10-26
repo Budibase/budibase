16de23eea905c33f59ae88981a53cc3c8682f985
Andrew Kingston <aptkingston@ssl-mail.com>
2020-10-26 11:00:34 +0000

    Add new common components for dropdown styles

---
+++ packages/builder/src/components/common/Dropdowns/DropdownContainer.svelte
@@ -0,0 +1,13 @@
+<div class="dropdown-container">
+  <slot />
+</div>
+
+<style>
+  .dropdown-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+    padding: var(--spacing-s) 0;
+  }
+</style>
