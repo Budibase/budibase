e22157231337d4782d5beaaf91ff6d234c2716df
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-10-28 10:21:24 +0000

    remove budibase cli

--- packages/cli/src/common.js
+++
@@ -1,8 +0,0 @@
-const { resolve, join } = require("path")
-const { homedir } = require("os")
-
-module.exports.serverFileName = relativePath =>
-  resolve(__dirname, "..", "node_modules", "@budibase", "server", relativePath)
-
-module.exports.xPlatHomeDir = dir =>
-  dir.startsWith("~") ? join(homedir(), dir.substring(1)) : dir
