ffc2c7bd5fddd966b1a05751a9d26e47c507da9f
Martin McKeaveney <martin@budibase.com>
2020-10-28 21:27:37 +0000

    adding cloud check to dev check

--- packages/server/src/utilities/index.js
+++ packages/server/src/utilities/index.js
@@ -2,6 +2,7 @@ exports.wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 exports.isDev = () => {
   return (
+    !process.env.CLOUD &&
     process.env.NODE_ENV !== "production" &&
     process.env.NODE_ENV !== "jest" &&
     process.env.NODE_ENV !== "cypress"
