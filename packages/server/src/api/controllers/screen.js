dc12752c6b399b3b514725b96605aa7a498d5dfd
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-11-01 15:32:54 +0000

    started creating APIs for screens

--- packages/server/src/api/controllers/screen.js
+++ packages/server/src/api/controllers/screen.js
@@ -3,9 +3,28 @@
  * currently managed as part of the pages API, please look in api/routes/page.js
  * for routes and controllers.
  */
+const CouchDB = require("../../db")
 
 exports.fetch = async ctx => {
   ctx.throw(501)
+}
+
+exports.create = async ctx => {
+  const db = new CouchDB(ctx.user.appId)
+  const screen = {
+    // name: ctx.request.body.name,
+    // _rev: ctx.request.body._rev,
+    // permissions: ctx.request.body.permissions || [],
+    // _id: generateAccessLevelID(),
+    // type: "accesslevel",
+  }
+
+  const response = await db.put(screen)
+  ctx.body = {
+    ...screen,
+    ...response,
+  }
+  ctx.message = `Screen '${screen.name}' created successfully.`
 }
 
 exports.save = async ctx => {
