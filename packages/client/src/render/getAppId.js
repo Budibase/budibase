0c81516662255a4759401d350898e688cd375ab3
mike12345567 <me@michaeldrury.co.uk>
2020-11-02 20:14:10 +0000

    Some more re-work, more testing needed to auth stuff.

--- packages/client/src/render/getAppId.js
+++ packages/client/src/render/getAppId.js
@@ -1,14 +1,3 @@
-export const parseAppIdFromCookie = docCookie => {
-  const cookie =
-    docCookie.split(";").find(c => c.trim().startsWith("budibase:token")) ||
-    docCookie.split(";").find(c => c.trim().startsWith("builder:token"))
-
-  if (!cookie) return location.pathname.replace(/\//g, "")
-
-  const base64Token = cookie.substring(lengthOfKey)
-
-  const user = JSON.parse(atob(base64Token.split(".")[1]))
-  return user.appId
+export const getAppIdFromPath = () => {
+  return location.pathname.split("/")[1]
 }
-
-const lengthOfKey = "budibase:token=".length
