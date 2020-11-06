eca97f6f5783c2774f1363737be78ea1a6f28d6b
mike12345567 <me@michaeldrury.co.uk>
2020-11-06 21:32:03 +0000

    Linting.

--- packages/client/src/render/getAppId.js
+++ packages/client/src/render/getAppId.js
@@ -2,8 +2,10 @@ const COOKIE_SEPARATOR = ";"
 const APP_PREFIX = "app_"
 const KEY_VALUE_SPLIT = "="
 
-export const getAppId = (allCookies) => {
-  const cookie = allCookies.split(COOKIE_SEPARATOR).find(cookie => cookie.trim().startsWith("budibase:currentapp"))
+export const getAppId = allCookies => {
+  const cookie = allCookies
+    .split(COOKIE_SEPARATOR)
+    .find(cookie => cookie.trim().startsWith("budibase:currentapp"))
   let appId = location.pathname.split("/")[1]
   appId = appId && appId.startsWith(APP_PREFIX) ? appId : undefined
   if (!appId && cookie && cookie.split(KEY_VALUE_SPLIT).length === 2) {
