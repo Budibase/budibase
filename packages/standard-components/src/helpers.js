e24a89af56a8ebf513c7655d9ba228eb2d5caa7c
Andrew Kingston <aptkingston@ssl-mail.com>
2020-11-11 14:26:33 +0000

    Tidy up standard components

--- packages/standard-components/src/helpers.js
+++ packages/standard-components/src/helpers.js
@@ -1,1 +1,49 @@
-export const capitalise = s => s.substring(0, 1).toUpperCase() + s.substring(1)
+/**
+ * Capitalises a string.
+ *
+ * @param string
+ * @returns {string}
+ */
+export const capitalise = string => {
+  return string.substring(0, 1).toUpperCase() + string.substring(1)
+}
+
+/**
+ * Builds a style string from a style object.
+ *
+ * @param styles
+ * @returns {string}
+ */
+export const buildStyle = styles => {
+  let str = ""
+  Object.entries(styles).forEach(([style, value]) => {
+    if (style && value) {
+      str += `${style}: ${value}; `
+    }
+  })
+  return str
+}
+
+/**
+ * Svelte action to set CSS variables on a DOM node.
+ *
+ * @param node
+ * @param props
+ */
+export const cssVars = (node, props) => {
+  Object.entries(props).forEach(([key, value]) => {
+    node.style.setProperty(`--${key}`, value)
+  })
+
+  return {
+    update(new_props) {
+      Object.entries(new_props).forEach(([key, value]) => {
+        node.style.setProperty(`--${key}`, value)
+        delete props[key]
+      })
+
+      Object.keys(props).forEach(name => node.style.removeProperty(`--${name}`))
+      props = new_props
+    },
+  }
+}
