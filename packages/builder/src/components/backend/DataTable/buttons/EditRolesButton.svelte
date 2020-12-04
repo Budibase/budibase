c346630dfe056bfab882a12def9fd82989c2ad93
Andrew Kingston <aptkingston@ssl-mail.com>
2020-12-04 08:27:42 +0000

    Add modal for editing user roles

---
+++ packages/builder/src/components/backend/DataTable/buttons/EditRolesButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { TextButton as Button, Modal } from "@budibase/bbui"
+  import EditRolesModal from "../modals/EditRoles.svelte"
+
+  let modal
+</script>
+
+<div>
+  <Button text small on:click={modal.show}>
+    <i class="ri-lock-line" />
+    Edit Roles
+  </Button>
+</div>
+<Modal bind:this={modal}>
+  <EditRolesModal />
+</Modal>
+
+<style>
+  i {
+    margin-right: var(--spacing-xs);
+    font-size: var(--font-size-s);
+  }
+</style>
