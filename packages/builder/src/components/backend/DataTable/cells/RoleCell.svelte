495427082fa093629174b38bb5cb548333bf0c3d
Andrew Kingston <aptkingston@ssl-mail.com>
2020-12-08 14:03:47 +0000

    Add custom renderer for user roles to show pretty names

---
+++ packages/builder/src/components/backend/DataTable/cells/RoleCell.svelte
@@ -0,0 +1,43 @@
+<!-- Module scoped cache of saved role options -->
+<script context="module">
+  import builderApi from "builderStore/api"
+
+  let cachedRoles
+
+  async function getRoles(force = false) {
+    if (cachedRoles && !force) {
+      return await cachedRoles
+    }
+    cachedRoles = new Promise(resolve => {
+      console.log("HIT API")
+      builderApi
+        .get("/api/roles")
+        .then(response => response.json())
+        .then(resolve)
+    })
+    return await cachedRoles
+  }
+</script>
+
+<script>
+  export let roleId
+
+  let roleName
+  $: getRole()
+
+  async function getRole() {
+    // Try to find a matching role
+    let roles = await getRoles()
+    let role = roles.find(role => role._id === roleId)
+
+    // If we didn't find a matching role, try updating the cached results
+    if (!role) {
+      let roles = await getRoles(true)
+      let role = roles.find(role => role._id === roleId)
+    }
+    role = roles.find(role => role._id === roleId)
+    roleName = role?.name ?? "Unknown role"
+  }
+</script>
+
+<div>{roleName}</div>
