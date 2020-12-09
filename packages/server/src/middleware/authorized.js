1cbe3771abe1a8cc4df11f8308342945a0a61c6e
mike12345567 <me@michaeldrury.co.uk>
2020-12-09 17:04:05 +0000

    Adding in self hosted API key.

--- packages/server/src/middleware/authorized.js
+++ packages/server/src/middleware/authorized.js
@@ -7,7 +7,7 @@ const {
   doesHavePermission,
 } = require("../utilities/security/permissions")
 const env = require("../environment")
-const { apiKeyTable } = require("../db/dynamoClient")
+const { getAPIKey } = require("../utilities/security/apikey")
 const { AuthTypes } = require("../constants")
 
 const ADMIN_ROLES = [BUILTIN_ROLE_IDS.ADMIN, BUILTIN_ROLE_IDS.BUILDER]
@@ -21,9 +21,7 @@ module.exports = (permType, permLevel = null) => async (ctx, next) => {
   }
   if (env.CLOUD && ctx.headers["x-api-key"] && ctx.headers["x-instanceid"]) {
     // api key header passed by external webhook
-    const apiKeyInfo = await apiKeyTable.get({
-      primary: ctx.headers["x-api-key"],
-    })
+    const apiKeyInfo = await getAPIKey(ctx.headers["x-api-key"])
 
     if (apiKeyInfo) {
       ctx.auth = {
