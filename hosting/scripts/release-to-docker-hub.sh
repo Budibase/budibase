f1c7a46d37d92a9b4cd5131d2f7ac367fa153e39
mike12345567 <me@michaeldrury.co.uk>
2021-01-07 11:38:37 +0000

    Adding a build method which can push docker images if you are logged in to docker CLI and making the main docker compose file use the pushed images instead.

---
+++ hosting/scripts/release-to-docker-hub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pushd ../build
+docker-compose build --force app-service
+docker-compose build --force worker-service
+docker tag build_app-service budibase/budibase-apps:latest
+docker push budibase/budibase-apps
+docker tag build_worker-service budibase/budibase-worker:latest
+docker push budibase/budibase-worker
+popd
