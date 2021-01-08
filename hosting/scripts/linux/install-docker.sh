b4a3f2282c40524f8857f4af1177f535f2e348ed
mike12345567 <me@michaeldrury.co.uk>
2021-01-08 10:53:20 +0000

    Changing scripts directory around a bit for hosting, make it clear shell scripts are for unix/linux.

---
+++ hosting/scripts/linux/install-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
