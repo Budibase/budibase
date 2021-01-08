711e44a3b8bc1689a3ad14307585b72f0107b841
mike12345567 <me@michaeldrury.co.uk>
2021-01-08 11:59:42 +0000

    Updating envoy to handle API request, proxy to app-service, doesn't have to come from /app/api.

--- hosting/scripts/linux/install-docker.sh
+++ hosting/scripts/linux/install-docker.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+echo "**** WARNING - not for production environments ****"
+# warning this is a convience script, for production installations install docker 
+# properly for your environment!
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
