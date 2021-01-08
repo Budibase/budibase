a878d7eb40edeeb442482507266002f8cdf948c7
Michael Shanks <mike@budibase.com>
2021-01-08 17:25:06 +0000

    Trigger Automation from frontend actions

--- packages/builder/src/components/userInterface/EventsEditor/actions/index.js
+++ packages/builder/src/components/userInterface/EventsEditor/actions/index.js
@@ -1,6 +1,7 @@
 import NavigateTo from "./NavigateTo.svelte"
 import SaveRow from "./SaveRow.svelte"
 import DeleteRow from "./DeleteRow.svelte"
+import TriggerAutomation from "./TriggerAutomation.svelte"
 
 // defines what actions are available, when adding a new one
 // the component is the setup panel for the action
@@ -19,5 +20,9 @@ export default [
   {
     name: "Navigate To",
     component: NavigateTo,
+  },
+  {
+    name: "Trigger Automation",
+    component: TriggerAutomation,
   },
 ]
