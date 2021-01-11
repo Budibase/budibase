5aa7edda642aef37808a4919454a1f13f6d9ed31
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-01-11 15:34:43 +0000

    use dynamic input in query

--- packages/client/src/api/datasources.js
+++ packages/client/src/api/datasources.js
@@ -1,9 +1,11 @@
+import { get } from "svelte/store"
 import { fetchTableData } from "./tables"
 import { fetchViewData } from "./views"
 import { fetchRelationshipData } from "./relationships"
 import { executeQuery } from "./queries"
 import { enrichRows } from "./rows"
 import { enrichDataBindings } from "../utils/enrichDataBinding"
+import { bindingStore } from "../store/binding"
 
 /**
  * Fetches all rows for a particular Budibase data source.
@@ -21,12 +23,13 @@ export const fetchDatasource = async (datasource, dataContext) => {
   } else if (type === "view") {
     rows = await fetchViewData(datasource)
   } else if (type === "query") {
-    console.log("Query Datasource", datasource)
-    console.log("Data Context", dataContext)
-    // TODO: You left here
-    const parameters = enrichDataBindings(datasource.queryParams, dataContext)
-    console.log("PARSED PARAMS", parameters)
-    return await executeQuery({ _id: datasource._id, parameters })
+    const bindings = get(bindingStore)
+    const fullContext = {
+      ...bindings,
+      ...dataContext,
+    }
+    const parameters = enrichDataBindings(datasource.queryParams, fullContext)
+    return await executeQuery({ queryId: datasource._id, parameters })
   } else if (type === "link") {
     const row = dataContext[datasource.providerId]
     rows = await fetchRelationshipData({
