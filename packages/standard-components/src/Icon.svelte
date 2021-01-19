7f405e7dd9d18ffd54a5e7b347b8146d511d560f
kevmodrome <kevin@kevmodro.me>
2021-01-19 13:42:49 +0100

    retains icons design panel options while being able to change color in settings panel

--- packages/standard-components/src/Icon.svelte
+++ packages/standard-components/src/Icon.svelte
@@ -1,15 +1,16 @@
 <script>
   import { getContext } from "svelte"
 
-  // Add this back once we can define specific design options to expose
-  // const { styleable } = getContext("sdk")
-  // const component = getContext("component")
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
 
   export let icon = ""
   export let size = "fa-lg"
   export let color = "#000"
 </script>
 
-<i
-  style={`color: ${color};`}
-  class={`${icon} ${size}`} />
+<div style="color: {color}; display: contents">
+  <i
+  use:styleable={$component.styles}
+  class="{icon} {size}" />
+</div>
