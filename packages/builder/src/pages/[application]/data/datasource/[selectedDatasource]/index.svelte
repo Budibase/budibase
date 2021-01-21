ada6611b0391fdd3a46cfabbb25ad6299ae3c0e6
Joe <49767913+joebudi@users.noreply.github.com>
2021-01-21 14:57:11 +0000

    External data source config ui
    I've improved the spacing on this screen and added an additional element (query list) which should improve UX. This list is not complete. Is required dyanmic data. Currently it holds static placement text in order to support the design.

--- packages/builder/src/pages/[application]/data/datasource/[selectedDatasource]/index.svelte
+++ packages/builder/src/pages/[application]/data/datasource/[selectedDatasource]/index.svelte
@@ -1,6 +1,6 @@
 <script>
   import { goto } from "@sveltech/routify"
-  import { Button, Spacer, Icon, TextButton } from "@budibase/bbui"
+  import { Button, Spacer, Icon } from "@budibase/bbui"
   import { backendUiStore } from "builderStore"
   import { notifier } from "builderStore/store/notifications"
   import IntegrationConfigForm from "components/backend/DatasourceNavigator/TableIntegrationMenu/IntegrationConfigForm.svelte"
@@ -17,28 +17,152 @@
 </script>
 
 {#if datasource}
-  <TextButton text small on:click={() => $goto('../new')}>
-    <Icon name="filter" />
-    Create Query
-  </TextButton>
   <section>
-    <h4>{datasource.name}: Configuration</h4>
-    <IntegrationConfigForm integration={datasource.config} />
     <Spacer medium />
-    <footer>
-      <Button blue wide on:click={saveDatasource}>Save</Button>
-    </footer>
+    <header>
+      <h3 class="section-title">{datasource.name}</h3>
+    </header>
+    <Spacer extraLarge />
+    <div class="container">
+      <div class="config-header">
+        <h5>Configuration</h5>
+        <Button secondary on:click={saveDatasource}>Save</Button>
+      </div>
+      <Spacer medium />
+      <IntegrationConfigForm integration={datasource.config} />
+    </div>
+    <Spacer extraLarge />
+    <div class="container">
+      <div class="query-header">
+        <h5>Queries</h5>
+        <Button blue on:click={() => $goto('../new')}>Create Query</Button>
+      </div>
+      <Spacer extraLarge />
+      <div class="query-list">
+        <div class="query-list-item">
+          <p class="query-name">The Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4000 records</p>
+          <p>→</p>
+        </div>
+        <div class="query-list-item">
+          <p class="query-name">The Second Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4,000,000 records</p>
+          <p>→</p>
+        </div>
+        <div class="query-list-item">
+          <p class="query-name">The Second Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4,000,000 records</p>
+          <p>→</p>
+        </div>
+        <div class="query-list-item">
+          <p class="query-name">The Second Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4,000,000 records</p>
+          <p>→</p>
+        </div>
+        <div class="query-list-item">
+          <p class="query-name">The Second Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4,000,000 records</p>
+          <p>→</p>
+        </div>
+        <div class="query-list-item">
+          <p class="query-name">The Second Long Query Name</p>
+          <p>Read</p>
+          <p>SQL</p>
+          <p>4,000,000 records</p>
+          <p>→</p>
+        </div>
+      </div>
+      <Spacer medium />
+    </div>
   </section>
+
 {/if}
 
 <style>
-  h4 {
-    margin-top: var(--spacing-xl);
-    margin-bottom: var(--spacing-s);
+  h3 {
+    margin: 0;
   }
   section {
-    background: var(--background);
+    margin: 0 auto;
+    width: 800px;
+  }
+
+  header {
+    margin: 0 0 var(--spacing-xs) 0;
+  }
+
+  .section-title {
+    text-transform: capitalize;
+  }
+
+  .config-header {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0 var(--spacing-xs) 0;
+  }
+
+  .container {
     border-radius: var(--border-radius-m);
-    padding: var(--spacing-xl);
+    background: var(--background);
+    padding: var(--layout-s);
+    margin: 0 auto;
+  }
+
+  h5 {
+    margin: 0 !important;
+  }
+
+  .query-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 0 var(--spacing-s) 0;
+  }
+
+  .query-list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-m);
+  }
+
+  .query-list-item {
+    border-radius: var(--border-radius-m);
+    background: var(--background);
+    border: var(--border-grey);
+    display: grid;
+    grid-template-columns: 2fr 0.75fr 0.75fr 1fr 20px;
+    align-items: center;
+    padding: var(--spacing-m) var(--layout-xs);
+    gap: var(--layout-xs);
+    transition: 200ms background ease;
+  }
+
+  .query-list-item:hover {
+    background: var(--grey-1);
+    cursor: pointer;
+  }
+
+  p {
+    font-size: var(--font-size-xs);
+    color: var(--grey-8);
+  }
+
+  .query-name {
+    color: var(--ink);
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-size: var(--font-size-s);
   }
 </style>
