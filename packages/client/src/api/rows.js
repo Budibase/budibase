2034fb8646fcb9e18bd530e1b36ec7ced34945c5
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-01-25 12:44:44 +0100

    adds success notification messages to row API

--- packages/client/src/api/rows.js
+++ packages/client/src/api/rows.js
@@ -1,3 +1,4 @@
+import { notificationStore } from "../store/notification"
 import API from "./api"
 import { fetchTableDefinition } from "./tables"
 
@@ -15,42 +16,50 @@ export const fetchRow = async ({ tableId, rowId }) => {
  * Creates a row in a table.
  */
 export const saveRow = async row => {
-  return await API.post({
+  const res = await API.post({
     url: `/api/${row.tableId}/rows`,
     body: row,
   })
+  notificationStore.success("Row saved")
+  return res
 }
 
 /**
  * Updates a row in a table.
  */
 export const updateRow = async row => {
-  return await API.patch({
+  const res = await API.patch({
     url: `/api/${row.tableId}/rows/${row._id}`,
     body: row,
   })
+  notificationStore.success("Row updated")
+  return res
 }
 
 /**
  * Deletes a row from a table.
  */
 export const deleteRow = async ({ tableId, rowId, revId }) => {
-  return await API.del({
+  const res = await API.del({
     url: `/api/${tableId}/rows/${rowId}/${revId}`,
   })
+  notificationStore.success("Row deleted")
+  return res
 }
 
 /**
  * Deletes many rows from a table.
  */
 export const deleteRows = async ({ tableId, rows }) => {
-  return await API.post({
+  const res = await API.post({
     url: `/api/${tableId}/rows`,
     body: {
       rows,
       type: "delete",
     },
   })
+  notificationStore.success(`${rows.length} rows deleted.`)
+  return res
 }
 
 /**
