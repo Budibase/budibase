8713105b62d13370c642a326dfa1ba3603533642
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-01-25 12:55:29 +0100

    changes notification handling from catch-all to specific messages per action

--- packages/client/src/api/automations.js
+++ packages/client/src/api/automations.js
@@ -1,10 +1,15 @@
+import { notificationStore } from "../store/notification"
 import API from "./api"
 /**
  * Executes an automation. Must have "App Action" trigger.
  */
 export const triggerAutomation = async (automationId, fields) => {
-  return await API.post({
+  const res = await API.post({
     url: `/api/automations/${automationId}/trigger`,
     body: { fields },
   })
+  res.error
+    ? notificationStore.danger("En error has occured")
+    : notificationStore.success("Automation triggered.")
+  return res
 }
