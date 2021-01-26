bfd24571b2e2dedb4859ddbd8bee96ef714ccf60
Andrew Kingston <aptkingston@ssl-mail.com>
2021-01-26 09:40:04 +0000

    Fix client API URLs not being cleaned properly

--- packages/client/src/api/api.js
+++ packages/client/src/api/api.js
@@ -77,7 +77,7 @@ const makeCachedApiCall = async params => {
 const requestApiCall = method => async params => {
   const { url, cache = false } = params
   const fixedUrl = `/${url}`.replace("//", "/")
-  const enrichedParams = { ...params, method, fixedUrl }
+  const enrichedParams = { ...params, method, url: fixedUrl }
   return await (cache ? makeCachedApiCall : makeApiCall)(enrichedParams)
 }
 
