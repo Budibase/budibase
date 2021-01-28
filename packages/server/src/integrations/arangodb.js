2f167d8245172f738819a9b44cab924e6155263a
sovlookup <805408477@qq.com>
2021-01-28 22:20:15 +0800

    add arangodb integration

--- packages/server/src/integrations/arangodb.js
+++ packages/server/src/integrations/arangodb.js
@@ -52,9 +52,7 @@ class ArangoDBIntegration {
   async read(query) {
     try {
       const result = await this.client.query(query.sql)
-      let rl = []
-      await result.forEach(r => rl.push(r))
-      return rl
+      return result.forEach(r => rl.push(r))
     } catch (err) {
       console.error("Error querying arangodb", err.message)
       throw err
@@ -69,9 +67,7 @@ class ArangoDBIntegration {
       const result = await this.client.query(
         aql`INSERT ${query.json} INTO ${clc} LET n = NEW RETURN NEW`
       )
-      let rl = []
-      await result.forEach(r => rl.push(r))
-      return rl
+      return result.forEach(r => rl.push(r))
     } catch (err) {
       console.error("Error querying arangodb", err.message)
       throw err
