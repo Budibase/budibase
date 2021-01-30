a1f09b0a1a249d1021be593ab395021afe5e85a9
Michael Drury <me@michaeldrury.co.uk>
2021-01-30 02:54:52 +0000

    Some rollup fixes - the handlebars-helper library needs dynamic requires removed from it, likely best to fork it and do this.

--- packages/string-templates/rollup.config.js
+++ packages/string-templates/rollup.config.js
@@ -1,23 +1,36 @@
-import commonjs from "rollup-plugin-commonjs"
+import commonjs from "@rollup/plugin-commonjs"
 import resolve from "rollup-plugin-node-resolve"
 import builtins from "rollup-plugin-node-builtins"
 import globals from "rollup-plugin-node-globals"
 import json from "@rollup/plugin-json"
+import replace from "@rollup/plugin-replace"
 import { terser } from "rollup-plugin-terser"
 
 const production = !process.env.ROLLUP_WATCH
 export default {
-  input: "src/index.js",
+  input: "src/esIndex.js",
   output: [
     {
       sourcemap: true,
-      format: "es",
+      format: "esm",
       file: "./dist/bundle.js",
       name: "templates",
       exports: "named",
     },
   ],
   plugins: [
+    // this replacement is a crazy hack to fix an issue that
+    // rollup has with the handlebars-helper package
+    // if we don't do this then the browser will always error
+    // with the isNumber function being unavailable
+    replace({
+      include: [
+        "node_modules/handlebars-helpers/lib/**",
+        "node_modules/handlebar-utils/lib/**",
+      ],
+      "utils.isNumber(": "!isNaN(",
+      "isNumber(": "!isNaN(",
+    }),
     resolve({
       mainFields: ["module", "main"],
       preferBuiltins: true,
