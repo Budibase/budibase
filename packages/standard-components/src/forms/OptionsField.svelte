b65673b77a03f0cf4f7b9bdf6409fd1a90d3a537
Andrew Kingston <aptkingston@ssl-mail.com>
2021-02-01 13:15:35 +0000

    Add reltationship field. Add common picker component to dedupe options and relationship fields. Add better animation and position to picker dropdowns

--- packages/standard-components/src/forms/OptionsField.svelte
+++ packages/standard-components/src/forms/OptionsField.svelte
@@ -3,6 +3,7 @@
   import "@spectrum-css/popover/dist/index-vars.css"
   import "@spectrum-css/menu/dist/index-vars.css"
   import SpectrumField from "./SpectrumField.svelte"
+  import Picker from "./Picker.svelte"
 
   export let field
   export let label
@@ -17,6 +18,7 @@
   $: options = fieldSchema?.constraints?.inclusion ?? []
   $: placeholderText = placeholder || "Choose an option"
   $: isNull = $fieldState?.value == null || $fieldState?.value === ""
+  $: fieldText = isNull ? placeholderText : $fieldState?.value
 
   const selectOption = value => {
     fieldApi.setValue(value)
@@ -26,70 +28,14 @@
 
 <SpectrumField {field} {label} bind:fieldState bind:fieldApi bind:fieldSchema>
   {#if fieldState}
-    <button
-      id={$fieldState.fieldId}
-      class="spectrum-Picker"
-      class:is-invalid={!$fieldState.valid}
-      class:is-open={open}
-      aria-haspopup="listbox"
-      on:click={() => (open = true)}>
-      <span class="spectrum-Picker-label" class:is-placeholder={isNull}>
-        {isNull ? placeholderText : $fieldState.value}
-      </span>
-      {#if !$fieldState.valid}
-        <svg
-          class="spectrum-Icon spectrum-Icon--sizeM spectrum-Picker-validationIcon"
-          focusable="false"
-          aria-hidden="true"
-          aria-label="Folder">
-          <use xlink:href="#spectrum-icon-18-Alert" />
-        </svg>
-      {/if}
-      <svg
-        class="spectrum-Icon spectrum-UIIcon-ChevronDown100 spectrum-Picker-menuIcon"
-        focusable="false"
-        aria-hidden="true">
-        <use xlink:href="#spectrum-css-icon-Chevron100" />
-      </svg>
-    </button>
-    <div
-      class="spectrum-Popover spectrum-Popover--bottom spectrum-Picker-popover"
-      class:is-open={open}
-      style="z-index: 1;">
-      <ul class="spectrum-Menu" role="listbox">
-        <li
-          class="spectrum-Menu-item"
-          class:is-selected={isNull}
-          role="option"
-          aria-selected="true"
-          tabindex="0"
-          on:click={() => selectOption(null)}>
-          <span class="spectrum-Menu-itemLabel">{placeholderText}</span>
-          <svg
-            class="spectrum-Icon spectrum-UIIcon-Checkmark100 spectrum-Menu-checkmark spectrum-Menu-itemIcon"
-            focusable="false"
-            aria-hidden="true">
-            <use xlink:href="#spectrum-css-icon-Checkmark100" />
-          </svg>
-        </li>
-        {#each options as option}
-          <li
-            class="spectrum-Menu-item"
-            class:is-selected={option === $fieldState.value}
-            role="option"
-            aria-selected="true"
-            tabindex="0"
-            on:click={() => selectOption(option)}>
-            <span class="spectrum-Menu-itemLabel">{option}</span>
-            <svg
-              class="spectrum-Icon spectrum-UIIcon-Checkmark100 spectrum-Menu-checkmark spectrum-Menu-itemIcon"
-              focusable="false"
-              aria-hidden="true">
-              <use xlink:href="#spectrum-css-icon-Checkmark100" />
-            </svg>
-          </li>
-        {/each}
-      </ul>
-    </div>
+    <Picker
+      bind:open
+      {fieldState}
+      {fieldText}
+      {options}
+      isPlaceholder={isNull}
+      placeholderOption={placeholderText}
+      isOptionSelected={option => option === $fieldState.value}
+      onSelectOption={selectOption} />
   {/if}
 </SpectrumField>
