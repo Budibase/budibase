8d8cdcd732f90bcf86e8410ffc543d1f835f3fea
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-02-05 10:55:54 +0000

    app export with space in name

--- packages/builder/src/components/start/AppCard.svelte
+++ packages/builder/src/components/start/AppCard.svelte
@@ -14,7 +14,9 @@
   async function exportApp() {
     appExportLoading = true
     try {
-      download(`/api/backups/export?appId=${_id}&appname=${name}`)
+      download(
+        `/api/backups/export?appId=${_id}&appname=${encodeURIComponent(name)}`
+      )
       notifier.success("App Export Complete.")
     } catch (err) {
       console.error(err)
@@ -30,13 +32,13 @@
   <Spacer medium />
   <div class="card-footer">
     <TextButton text medium blue href="/_builder/{_id}">
-      Open
-      {name}
-      →
+      Open {name} →
     </TextButton>
     {#if appExportLoading}
       <Spinner size="10" />
-    {:else}<i class="ri-folder-download-line" on:click={exportApp} />{/if}
+    {:else}
+      <i class="ri-folder-download-line" on:click={exportApp} />
+    {/if}
   </div>
 </div>
 
