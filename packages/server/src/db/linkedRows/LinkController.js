383c2a6c0a82a6ffbe2dcc306ed8796fdf202f14
mike12345567 <me@michaeldrury.co.uk>
2021-02-09 13:18:59 +0000

    Quick fix to make sure all relationships link to a valid, existing doc.

--- packages/server/src/db/linkedRows/LinkController.js
+++ packages/server/src/db/linkedRows/LinkController.js
@@ -138,6 +138,13 @@ class LinkController {
         // iterate through the link IDs in the row field, see if any don't exist already
         for (let linkId of rowField) {
           if (linkId && linkId !== "" && linkDocIds.indexOf(linkId) === -1) {
+            // first check the doc we're linking to exists
+            try {
+              await this._db.get(linkId)
+            } catch (err) {
+              // skip links that don't exist
+              continue
+            }
             operations.push(
               new LinkDocument(
                 table._id,
