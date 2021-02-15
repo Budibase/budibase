87f0a2cd6752f60074b5bf25a368f9c5c61ca9a9
mike12345567 <me@michaeldrury.co.uk>
2021-02-15 19:01:15 +0000

    Fixing issue with linked rows not handling uniqueness correctly when links between tables are using fieldnames for uniqueness.

--- packages/server/src/db/linkedRows/index.js
+++ packages/server/src/db/linkedRows/index.js
@@ -118,8 +118,12 @@ exports.attachLinkInfo = async (appId, rows) => {
     // have to get unique as the previous table query can
     // return duplicates, could be querying for both tables in a relation
     const linkVals = getUniqueByProp(
-      responses.filter(el => el.thisId === row._id),
-      "id"
+      responses
+        // find anything that matches the row's ID we are searching for
+        .filter(el => el.thisId === row._id)
+        // create a unique ID which we can use for getting only unique ones
+        .map(el => ({ ...el, unique: el.id + el.fieldName })),
+      "unique"
     )
     for (let linkVal of linkVals) {
       // work out which link pertains to this row
