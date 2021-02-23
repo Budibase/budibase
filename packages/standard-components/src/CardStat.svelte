d0c40f060ceb44b71ec7689f44276a50948cc8b4
Andrew Kingston <aptkingston@ssl-mail.com>
2021-02-23 10:15:40 +0000

    Update card stat to preserve white space

--- packages/standard-components/src/CardStat.svelte
+++ packages/standard-components/src/CardStat.svelte
@@ -20,7 +20,6 @@
   .container {
     min-width: 260px;
     width: max-content;
-    max-height: 170px;
     border: 1px solid var(--grey-3);
     border-radius: 0.3rem;
     color: var(--blue);
@@ -31,6 +30,7 @@
     color: #9e9e9e;
     font-weight: 500;
     margin: 1rem 1.5rem 0.5rem 1.5rem;
+    white-space: pre-wrap;
   }
 
   .value {
@@ -38,6 +38,7 @@
     font-weight: 500;
     margin: 0 1.5rem 1.5rem 1.5rem;
     color: inherit;
+    white-space: pre-wrap;
   }
 
   .label {
@@ -45,5 +46,6 @@
     font-weight: 400;
     color: #9e9e9e;
     margin: 1rem 1.5rem;
+    white-space: pre-wrap;
   }
 </style>
