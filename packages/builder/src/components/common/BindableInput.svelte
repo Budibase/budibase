fb1eb55ba2f485f4be02f570a0b0a9433b286f7a
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-02-23 16:43:03 +0100

    work on converting popover to drawer

--- packages/builder/src/components/common/BindableInput.svelte
+++ packages/builder/src/components/common/BindableInput.svelte
@@ -1,44 +1,64 @@
 <script>
+  import BindingPanel from "../automation/SetupPanel/AutomationBindingPanel.svelte"
+  import { runtimeToReadableBinding, readableToRuntimeBinding } from "builderStore/dataBinding"
+  import { Body, Button, Input, Icon, Drawer } from "@budibase/bbui"
+  
   import { createEventDispatcher } from "svelte"
-  import GenericBindingPopover from "../automation/SetupPanel/GenericBindingPopover.svelte"
-  import { Input, Icon } from "@budibase/bbui"
-
   const dispatch = createEventDispatcher()
-
+  
+  export let value = ""
   export let bindings = []
-  export let value
+  export let thin = true
+  export let label = ""
+  export let title = "Bindings"
+  export let placeholder
 
-  let anchor
-  let popover = undefined
-  let enrichedValue
-  let inputProps
 
-  // Extract all other props to pass to input component
-  $: {
-    let { bindings, ...otherProps } = $$props
-    inputProps = otherProps
+  let bindingDrawer
+
+  $: tempValue = value
+  $: readableValue = runtimeToReadableBinding(bindings, value)
+
+  const handleClose = () => {
+    onChange(tempValue)
+    bindingDrawer.hide()
   }
-  $: value && dispatch("change", value)
+
+  const onChange = value => {
+    dispatch("change", readableToRuntimeBinding(bindings, value))
+  }
 </script>
 
-<div class="container" bind:this={anchor}>
-  <Input {...inputProps} bind:value />
-  <div class="icon" on:click={popover.show}>
-    <Icon name="edit" />
+<div class="control">
+  <Input
+    {thin}
+    {label}
+    value={readableValue}
+    on:change={event => onChange(event.target.value)}
+    {placeholder} />
+  <div class="icon" on:click={bindingDrawer.show}>
+    <Icon name="lightning" />
   </div>
 </div>
-<GenericBindingPopover
-  {anchor}
-  {bindings}
-  bind:value
-  bind:popover
-  align="right" />
+<Drawer bind:this={bindingDrawer} {title}>
+  <div slot="description">
+    <Body extraSmall grey>
+      Add the objects on the left to enrich your text.
+    </Body>
+  </div>
+  <heading slot="buttons">
+    <Button thin blue on:click={handleClose}>Save</Button>
+  </heading>
+  <div slot="body">
+    <BindingPanel
+      {value}
+      close={handleClose}
+      on:update={event => (tempValue = event.detail)}
+      {bindings} />
+  </div>
+</Drawer>
 
 <style>
-  .container {
-    position: relative;
-  }
-
   .icon {
     right: 2px;
     top: 5px;
