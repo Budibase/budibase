80f86720ac8aa08360114de2636018ef272b92e7
Andrew Kingston <aptkingston@ssl-mail.com>
2021-02-26 09:58:11 +0000

    Add background image component

---
+++ packages/standard-components/src/BackgroundImage.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { getContext } from "svelte"
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+
+  export let url = ""
+  $: style = url ? `background-image: url("${url}")` : ""
+</script>
+
+<div class="outer" use:styleable={$component.styles}>
+  <div class="inner" {style} />
+</div>
+
+<style>
+  .outer {
+    position: relative;
+  }
+
+  .inner {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top center;
+  }
+</style>
