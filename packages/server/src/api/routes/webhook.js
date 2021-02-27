2e7410bc717bdbb4aa08e5b3bafbdb919c05a663
mike12345567 <me@michaeldrury.co.uk>
2021-02-27 10:45:01 +0000

    Linting.

--- packages/server/src/api/routes/webhook.js
+++ packages/server/src/api/routes/webhook.js
@@ -2,11 +2,7 @@ const Router = require("@koa/router")
 const controller = require("../controllers/webhook")
 const authorized = require("../../middleware/authorized")
 const joiValidator = require("../../middleware/joi-validator")
-const {
-  BUILDER,
-  PermissionTypes,
-  PermissionLevels,
-} = require("../../utilities/security/permissions")
+const { BUILDER } = require("../../utilities/security/permissions")
 const Joi = require("joi")
 
 const router = Router()
