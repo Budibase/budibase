e4a29047359c5dddd3af0a1feb2781a31f6b80b1
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-01 13:24:57 +0100

    minor changes to options

--- packages/client/src/utils/transition.js
+++ packages/client/src/utils/transition.js
@@ -5,7 +5,7 @@ const transitions = new Map([
   ["fade", { tn: fade, opt: {} }],
   ["blur", { tn: blur, opt: {} }],
   ["slide", { tn: slide, opt: {} }],
-  ["fly", { tn: fly, opt: { y: 30 } }],
+  ["fly", { tn: fly, opt: { y: 80 } }],
 ])
 	
 export default function transition(node, {type, options = {}}) {
