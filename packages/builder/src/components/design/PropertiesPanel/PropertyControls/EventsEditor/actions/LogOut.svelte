9382a0a5679a0f45eccff59f1edeb923ac2ae3f9
Andrew Kingston <aptkingston@ssl-mail.com>
2021-03-01 14:56:30 +0000

    Remove obsolete code from log out button action component

--- packages/builder/src/components/design/PropertiesPanel/PropertyControls/EventsEditor/actions/LogOut.svelte
+++ packages/builder/src/components/design/PropertiesPanel/PropertyControls/EventsEditor/actions/LogOut.svelte
@@ -1,13 +1,3 @@
-<script>
-  import { getBindableProperties } from "builderStore/dataBinding"
-  import { currentAsset, store } from "builderStore"
-
-  export let parameters
-
-  let bindingDrawer
-  $: bindings = getBindableProperties($currentAsset, $store.selectedComponentId)
-</script>
-
 <div class="root">This action doesn't require any additional settings.</div>
 
 <style>
