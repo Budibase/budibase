39e2d78317c5fea3b3d651b41224955fe9e31e42
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-03-04 17:55:24 +0000

    refactor view tests

--- packages/server/src/api/routes/tests/view.spec.js
+++ packages/server/src/api/routes/tests/view.spec.js
@@ -1,65 +1,67 @@
-const {
-  createApplication,
-  createTable,
-  supertest,
-  defaultHeaders,
-  getDocument
-} = require("./couchTestUtils")
+const TestConfig = require("./utilities/TestConfiguration");
 
 describe("/views", () => {
-  let request
-  let server
-  let app
-  let appId
-  let table
-
-  const createView = async (config = {
-    name: "TestView",
-    field: "Price",
-    calculation: "stats",
-    tableId: table._id
-  }) => 
-    await request
-    .post(`/api/views`)
-    .send(config)
-    .set(defaultHeaders(appId))
-    .expect('Content-Type', /json/)
-    .expect(200)
-
-  const createRow = async row => request
-    .post(`/api/${table._id}/rows`)
-    .send(row)
-    .set(defaultHeaders(appId))
-    .expect('Content-Type', /json/)
-    .expect(200)
+  let request;
+  let server;
+  let app;
+  let config;
+  let appId;
+  let table;
 
   beforeAll(async () => {
-    ({ request, server } = await supertest())
-  })
+    config = new TestConfig();
+    request = config.request;
+  });
 
   beforeEach(async () => {
-    app = await createApplication(request)
-    appId = app.instance._id
-  })
+    app = await config.init();
+    appId = app.instance._id;
+  });
 
   afterAll(() => {
-    server.close()
-  })
+    config.end();
+  });
 
   describe("create", () => {
     beforeEach(async () => {
-      table = await createTable(request, appId);
-    })
+      table = await config.createTable();
+    });
 
     it("returns a success message when the view is successfully created", async () => {
-      const res = await createView()
-      expect(res.res.statusMessage).toEqual("View TestView saved successfully.");
-    })
+      const res = await request
+        .post(`/api/views`)
+        .send({
+          name: "TestView",
+          field: "Price",
+          calculation: "stats",
+          tableId: table._id,
+        })
+        .set(config.defaultHeaders())
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(res.res.statusMessage).toEqual(
+        "View TestView saved successfully."
+      );
+    });
 
     it("updates the table row with the new view metadata", async () => {
-      const res = await createView()
-      expect(res.res.statusMessage).toEqual("View TestView saved successfully.");
-      const updatedTable = await getDocument(appId, table._id)
+      const res = await request
+        .post(`/api/views`)
+        .send({
+          name: "TestView",
+          field: "Price",
+          calculation: "stats",
+          tableId: table._id,
+        })
+        .set(config.defaultHeaders())
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(res.res.statusMessage).toEqual(
+        "View TestView saved successfully."
+      );
+      const updatedTable = await config.getTable(table._id);
       expect(updatedTable.views).toEqual({
         TestView: {
           field: "Price",
@@ -88,88 +90,98 @@ describe("/views", () => {
             field: {
               type: "string",
             },
-          }
-        }
+          },
+        },
       });
-    })
+    });
   });
 
   describe("fetch", () => {
     beforeEach(async () => {
-      table = await createTable(request, appId);
+      table = await config.createTable();
     });
 
     it("returns only custom views", async () => {
-      await createView()
+      await config.createView({
+        name: "TestView",
+        field: "Price",
+        calculation: "stats",
+        tableId: table._id,
+      });
       const res = await request
         .get(`/api/views`)
-        .set(defaultHeaders(appId))
-        .expect('Content-Type', /json/)
-        .expect(200)
-      expect(res.body.length).toBe(1)
-      expect(res.body.find(({ name }) => name === "TestView")).toBeDefined()
-    })
+        .set(config.defaultHeaders())
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body.length).toBe(1);
+      expect(res.body.find(({ name }) => name === "TestView")).toBeDefined();
+    });
   });
 
   describe("query", () => {
     beforeEach(async () => {
-      table = await createTable(request, appId);
+      table = await config.createTable();
     });
 
     it("returns data for the created view", async () => {
-      await createView()
-      await createRow({
+      await config.createView({
+        name: "TestView",
+        field: "Price",
+        calculation: "stats",
         tableId: table._id,
-        Price: 1000
-      })
-      await createRow({
+      });
+      await config.createRow({
         tableId: table._id,
-        Price: 2000
-      })
-      await createRow({
+        Price: 1000,
+      });
+      await config.createRow({
         tableId: table._id,
-        Price: 4000
-      })
+        Price: 2000,
+      });
+      await config.createRow({
+        tableId: table._id,
+        Price: 4000,
+      });
       const res = await request
         .get(`/api/views/TestView?calculation=stats`)
-        .set(defaultHeaders(appId))
-        .expect('Content-Type', /json/)
-        .expect(200)
-      expect(res.body.length).toBe(1)
-      expect(res.body).toMatchSnapshot()
-    })
+        .set(config.defaultHeaders())
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body.length).toBe(1);
+      expect(res.body).toMatchSnapshot();
+    });
 
     it("returns data for the created view using a group by", async () => {
-      await createView({
+      await config.createView({
         calculation: "stats",
         name: "TestView",
         field: "Price",
         groupBy: "Category",
-        tableId: table._id
-      })
-      await createRow({
+        tableId: table._id,
+      });
+      await config.createRow({
         tableId: table._id,
         Price: 1000,
-        Category: "One"
-      })
-      await createRow({
+        Category: "One",
+      });
+      await config.createRow({
         tableId: table._id,
         Price: 2000,
-        Category: "One"
-      })
-      await createRow({
+        Category: "One",
+      });
+      await config.createRow({
         tableId: table._id,
         Price: 4000,
-        Category: "Two"
-      })
+        Category: "Two",
+      });
       const res = await request
         .get(`/api/views/TestView?calculation=stats&group=Category`)
-        .set(defaultHeaders(appId))
-        .expect('Content-Type', /json/)
-        .expect(200)
-      
-      expect(res.body.length).toBe(2)
-      expect(res.body).toMatchSnapshot()
-    })
+        .set(config.defaultHeaders())
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(res.body.length).toBe(2);
+      expect(res.body).toMatchSnapshot();
+    });
   });
 });
