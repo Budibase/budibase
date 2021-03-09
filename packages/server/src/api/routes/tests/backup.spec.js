61110be9d0a184266989bfa837f9b5468190296f
mike12345567 <me@michaeldrury.co.uk>
2021-03-09 16:28:41 +0000

    Adding test cases for backup and integration.

---
+++ packages/server/src/api/routes/tests/backup.spec.js
@@ -0,0 +1,32 @@
+const { checkBuilderEndpoint } = require("./utilities/TestFunctions")
+const setup = require("./utilities")
+
+describe("/backups", () => {
+  let request = setup.getRequest()
+  let config = setup.getConfig()
+
+  afterAll(setup.afterAll)
+
+  beforeEach(async () => {
+    await config.init()
+  })
+
+  describe("exportAppDump", () => {
+    it("should be able to export app", async () => {
+      const res = await request
+        .get(`/api/backups/export?appId=${config.getAppId()}`)
+        .set(config.defaultHeaders())
+        .expect(200)
+      expect(res.text).toBeDefined()
+      expect(res.text.includes(`"db_name":"${config.getAppId()}"`)).toEqual(true)
+    })
+
+    it("should apply authorization to endpoint", async () => {
+      await checkBuilderEndpoint({
+        config,
+        method: "GET",
+        url: `/api/backups/export?appId=${config.getAppId()}`,
+      })
+    })
+  })
+})
