799168c6b86a35d3df129a1dc3050da0359ad5ee
mike12345567 <me@michaeldrury.co.uk>
2021-03-12 15:32:16 +0000

    Adding in all the automation step tests, beginning work on the general automation tests.

---
+++ packages/server/src/automations/tests/automation.spec.js
@@ -0,0 +1,14 @@
+const automation = require("../index")
+const usageQuota = require("../../utilities/usageQuota")
+
+jest.mock("../../utilities/usageQuota")
+
+describe("Check the primary input functions to automations", () => {
+  it("should be able to init in builder", async () => {
+
+  })
+
+  it("should be able to init in cloud", async () => {
+    
+  })
+})
