ce59aab7f87753d61d436ea3186229fe930d87a1
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-15 12:53:44 +0100

    wip: work on routing in automation tab

--- packages/builder/src/pages/[application]/automate/index.svelte
+++ packages/builder/src/pages/[application]/automate/index.svelte
@@ -1,5 +1,21 @@
 <script>
+  import { onMount } from 'svelte'
+  import { automationStore } from "builderStore"
   import AutomationBuilder from "components/automation/AutomationBuilder/AutomationBuilder.svelte"
+
+  onMount(async () => {
+    console.log('Automation Store: ', $automationStore)
+    console.log('Automation Store Methods: ', automationStore)
+    // navigate to first automation in list, if not already selected
+    // if (
+    //   !$leftover &&
+    //   $backendUiStore.tables.length > 0 &&
+    //   (!$backendUiStore.selectedTable || !$backendUiStore.selectedTable._id)
+    // ) {
+    //   // this file routes as .../tables/index, so, go up one.
+    //   $goto(`../${$backendUiStore.tables[0]._id}`)
+    // }
+  })
 </script>
 
 <AutomationBuilder />
