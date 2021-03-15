8b9271e5b763988ae5787b3368c59f5c5da8bd13
mike12345567 <me@michaeldrury.co.uk>
2021-03-15 20:43:08 +0000

    Removing unused code that was causing some issues with API key lookup and validation in self hosted setups.

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -9,7 +9,6 @@ const env = require("./environment")
 const eventEmitter = require("./events")
 const automations = require("./automations/index")
 const Sentry = require("@sentry/node")
-const selfhost = require("./selfhost")
 
 const app = new Koa()
 
@@ -66,11 +65,8 @@ module.exports = server.listen(env.PORT || 0, async () => {
   console.log(`Budibase running on ${JSON.stringify(server.address())}`)
   env._set("PORT", server.address().port)
   eventEmitter.emitPort(env.PORT)
-  automations.init()
-  // only init the self hosting DB info in the Pouch, not needed in self hosting prod
-  if (!env.CLOUD) {
-    await selfhost.init()
-  }
+
+  await automations.init()
 })
 
 process.on("uncaughtException", err => {
