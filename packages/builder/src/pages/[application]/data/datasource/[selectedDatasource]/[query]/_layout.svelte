4b303845493dbb7e55ee9ced24f920f7c927437f
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-15 10:26:59 +0100

    data switcher now shows correct tab

---
+++ packages/builder/src/pages/[application]/data/datasource/[selectedDatasource]/[query]/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  // import { params } from "@sveltech/routify"
+  // import { backendUiStore } from "builderStore"
+
+  // if ($params.selectedDatasource) {
+  //   const datasource = $backendUiStore.datasources.find(
+  //     m => m._id === $params.selectedDatasource
+  //   )
+  //   if (datasource) {
+  //     backendUiStore.actions.datasources.select(datasource._id)
+  //   }
+  // }
+</script>
+
+<slot />
