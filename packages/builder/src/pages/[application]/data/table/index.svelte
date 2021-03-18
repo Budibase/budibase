fdc1e4489627d79bcba6023c94f464f2914267ab
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-18 10:46:21 +0100

    removes some logic and fixes table/integration select when switching tabs

--- packages/builder/src/pages/[application]/data/table/index.svelte
+++ packages/builder/src/pages/[application]/data/table/index.svelte
@@ -1,19 +1,10 @@
 <script>
   import { backendUiStore } from "builderStore"
-  import { goto, leftover } from "@sveltech/routify"
+  import { goto } from "@sveltech/routify"
   import { onMount } from "svelte"
 
   onMount(async () => {
-    // navigate to first table in list, if not already selected
-    // and this is the final url (i.e. no selectedTable)
-    if (
-      !$leftover &&
-      $backendUiStore.tables.length > 0 &&
-      (!$backendUiStore.selectedTable || !$backendUiStore.selectedTable._id)
-    ) {
-      // this file routes as .../tables/index, so, go up one.
-      $goto(`../${$backendUiStore.tables[0]._id}`)
-    }
+    $backendUiStore.tables.length > 0 && $goto(`../${$backendUiStore.tables[0]._id}`)
   })
 </script>
 
