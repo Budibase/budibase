4b3891f68ac18ea5d143c5827bdcdfd66e44ffdb
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-22 09:59:22 +0100

    moves permissions to a separate store

---
+++ packages/builder/src/builderStore/store/backend/index.js
@@ -0,0 +1,1 @@
+export { permissions } from "./permissions"
---
+++ packages/builder/src/builderStore/store/backend/permissions.js
@@ -0,0 +1,29 @@
+import { writable } from "svelte/store"
+import api from "../../api"
+
+function createPermissionStore() {
+  const { subscribe, set } = writable([])
+
+  return {
+    subscribe,
+    fetchLevels: async () => {
+      const response = await api.get("/api/permission/levels")
+      const json = await response.json()
+      set(json)
+    },
+    forResource: async resourceId => {
+      const response = await api.get(`/api/permission/${resourceId}`)
+      const json = await response.json()
+      return json
+    },
+    save: async ({ role, resource, level }) => {
+      const response = await api.post(
+        `/api/permission/${role}/${resource}/${level}`
+      )
+      const json = await response.json()
+      return json
+    },
+  }
+}
+
+export const permissions = createPermissionStore()
