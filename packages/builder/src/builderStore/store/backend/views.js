071522c43f1015e06df024db8e2b1ba2f17bd584
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-23 12:04:11 +0100

    split out views to separate store

---
+++ packages/builder/src/builderStore/store/backend/views.js
@@ -0,0 +1,49 @@
+import { writable, get } from "svelte/store"
+import { tables } from "./"
+import api from "../../api"
+
+function createViewsStore() {
+  const { subscribe, set, update } = writable({
+      list: [],
+      selected: null
+  })
+
+  return {
+    subscribe,
+    select: view => {
+        update(state => ({
+            ...state,
+            selected: view,
+        }))
+        tables.select()
+    },
+    delete: async view => {
+        await api.delete(`/api/views/${view}`)
+        await tables.fetch()
+    },
+    save: async view => {
+        const response = await api.post(`/api/views`, view)
+        const json = await response.json()
+
+        const viewMeta = {
+            name: view.name,
+            ...json,
+        }
+
+        update(state => {
+            const viewTable = get(tables).list.find(
+                table => table._id === view.tableId
+            )
+
+            if (view.originalName) delete viewTable.views[view.originalName]
+            viewTable.views[view.name] = viewMeta
+
+            state.tables = state.tables
+            state.selectedView = viewMeta
+            return state
+        })
+    },
+  }
+}
+
+export const views = createViewsStore()
