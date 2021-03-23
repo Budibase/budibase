cefd3b96432bcd56dd2be91d96e720d69b46a969
Maurits Lourens <maurits.lourens@gmail.com>
2021-03-23 20:32:22 +0100

    fix build by running yarn format

--- packages/server/src/electron.js
+++ packages/server/src/electron.js
@@ -8,7 +8,7 @@ const initialiseBudibase = require("./utilities/initialiseBudibase")
 const { budibaseAppsDir } = require("./utilities/budibaseDir")
 const { openNewGitHubIssue, debugInfo } = require("electron-util")
 const eventEmitter = require("./events")
-const isMac = process.platform === 'darwin';
+const isMac = process.platform === "darwin"
 const budibaseDir = budibaseAppsDir()
 const envFile = join(budibaseDir, ".env")
 
@@ -90,108 +90,109 @@ async function startApp() {
 
   const template = [
     // { role: 'appMenu' }
-    ...(isMac ? [{
-      label: app.name,
-      submenu: [
-        { role: 'about' },
-        { type: 'separator' },
-        { role: 'services' },
-        { type: 'separator' },
-        { role: 'hide' },
-        { role: 'hideothers' },
-        { role: 'unhide' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]
-    }] : []),
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideothers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
     // { role: 'fileMenu' }
     {
-      label: 'File',
-      submenu: [
-        isMac ? { role: 'close' } : { role: 'quit' }
-      ]
+      label: "File",
+      submenu: [isMac ? { role: "close" } : { role: "quit" }],
     },
     // { role: 'editMenu' }
     {
-      label: 'Edit',
+      label: "Edit",
       submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        ...(isMac ? [
-          { role: 'pasteAndMatchStyle' },
-          { role: 'delete' },
-          { role: 'selectAll' },
-          { type: 'separator' },
-          {
-            label: 'Speech',
-            submenu: [
-              { role: 'startSpeaking' },
-              { role: 'stopSpeaking' }
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              },
             ]
-          }
-        ] : [
-          { role: 'delete' },
-          { type: 'separator' },
-          { role: 'selectAll' }
-        ])
-      ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+      ],
     },
     // { role: 'viewMenu' }
     {
-      label: 'View',
+      label: "View",
       submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
+        { role: "reload" },
+        { role: "forceReload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
     },
     // { role: 'windowMenu' }
     {
-      label: 'Window',
+      label: "Window",
       submenu: [
-        { role: 'minimize' },
-        { role: 'zoom' },
-        ...(isMac ? [
-          { type: 'separator' },
-          { role: 'front' },
-          { type: 'separator' },
-          { role: 'window' }
-        ] : [
-          { role: 'close' }
-        ])
-      ]
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" },
+            ]
+          : [{ role: "close" }]),
+      ],
     },
     {
-      role: 'help',
+      role: "help",
       submenu: [
         {
-          label: 'Learn More',
-          click: () => shell.openExternal('https://www.budibase.com/')
+          label: "Learn More",
+          click: () => shell.openExternal("https://www.budibase.com/"),
         },
         {
-          label: 'Documentation',
-          click: () => shell.openExternal('https://docs.budibase.com/')
+          label: "Documentation",
+          click: () => shell.openExternal("https://docs.budibase.com/"),
         },
         {
-          label: 'Search issues',
-          click: () => shell.openExternal('https://github.com/Budibase/budibase/issues')
+          label: "Search issues",
+          click: () =>
+            shell.openExternal("https://github.com/Budibase/budibase/issues"),
         },
         {
-          label: 'Community discussions',
-          click: () => shell.openExternal('https://github.com/Budibase/budibase/discussions')
+          label: "Community discussions",
+          click: () =>
+            shell.openExternal(
+              "https://github.com/Budibase/budibase/discussions"
+            ),
         },
-      ]
-    }
+      ],
+    },
   ]
 
   const menu = Menu.buildFromTemplate(template)
