22fb849ad2f859dd13c62b29736a0e5e7997bab6
Andrew Kingston <aptkingston@ssl-mail.com>
2021-03-24 18:45:55 +0000

    Increase table attachment preview heights

--- packages/standard-components/src/table/AttachmentRenderer.svelte
+++ packages/standard-components/src/table/AttachmentRenderer.svelte
@@ -14,19 +14,21 @@
 
 <style>
   img {
-    height: 24px;
-    max-width: 50px;
+    height: 32px;
+    max-width: 64px;
   }
   .file {
-    height: 24px;
+    height: 32px;
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
     padding: 0 8px;
-    color: var(--spectrum-global-color-gray-500);
-    border: 1px solid var(--spectrum-global-color-gray-500);
+    color: var(--spectrum-global-color-gray-800);
+    border: 1px solid var(--spectrum-global-color-gray-300);
     border-radius: 2px;
     text-transform: uppercase;
+    font-weight: 500;
+    font-size: 11px;
   }
 </style>
