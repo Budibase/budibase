f17e2478e2e73004a9efa82c01256c6c9e1fd0f1
Andrew Kingston <aptkingston@ssl-mail.com>
2021-03-25 16:17:56 +0000

    Fix crash generating bindable properties for an empty data provider ID

--- packages/builder/src/builderStore/dataBinding.js
+++ packages/builder/src/builderStore/dataBinding.js
@@ -366,10 +366,7 @@ function bindingReplacement(bindableProperties, textWithBindings, convertTo) {
  * {{ literal [componentId] }}
  */
 function extractLiteralHandlebarsID(value) {
-  if (!value) {
-    return null
-  }
-  return value.match(/{{\s*literal[\s\[]+([a-fA-F0-9]+)[\s\]]*}}/)[1]
+  return value?.match(/{{\s*literal[\s\[]+([a-fA-F0-9]+)[\s\]]*}}/)?.[1]
 }
 
 /**
