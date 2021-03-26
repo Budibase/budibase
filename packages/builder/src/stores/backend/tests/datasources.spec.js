ed2d541a77f204aa1693ab3a80714f3172df559f
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-03-26 15:43:55 +0100

    add some temporary testing files

--- packages/builder/src/stores/backend/tests/datasources.spec.js
+++ packages/builder/src/stores/backend/tests/datasources.spec.js
@@ -1,24 +1,33 @@
 import { get } from 'svelte/store'
+import api from 'builderStore/api'
+
+jest.mock('builderStore/api');
+
 import { createDatasourcesStore } from "../datasources"
 
-const mockApi = {
-  post: () => ({}),
-  get: () => ({}),
-  patch: () => ({}),
-  delete: () => ({}),
-  put: () => ({}),
-}
-
-
 describe("Automation Data Object", () => {
-  let store
+  let store = createDatasourcesStore()
 
   beforeEach(() => {
-    store = createDatasourcesStore(mockApi)
+    store.init()
   })
 
   it("Inits properly", () => {
+
     const value = get(store)
-    expect(value).toBe(true)
+    expect(value).toEqual({ list: [], selected: null})
+  })
+
+  it("Fetch returns and updates store", async () => {
+    api.get.mockReturnValueOnce({ json: () => 'some-cool-value'})
+
+    store.fetch()
+
+    
+    
+    expect(api.get).toBeCalledWith(`/api/datasources`)
+    
+
+    // expect(get(store)).toEqual({ list: [], selected: null})
   })
 })
