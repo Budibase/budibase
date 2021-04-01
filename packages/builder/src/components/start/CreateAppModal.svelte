7c76a264673c1df545ff65ae51fa98508584db20
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-01 18:00:46 +0100

    Update to support new backend table structure

--- packages/builder/src/components/start/CreateAppModal.svelte
+++ packages/builder/src/components/start/CreateAppModal.svelte
@@ -1,11 +1,7 @@
 <script>
   import { writable, get as svelteGet } from "svelte/store"
   import { notifier } from "builderStore/store/notifications"
-  import {
-    store,
-    automationStore,
-    hostingStore,
-  } from "builderStore"
+  import { store, automationStore, hostingStore } from "builderStore"
   import { string, object } from "yup"
   import api, { get } from "builderStore/api"
   import Form from "@svelteschool/svelte-forms"
@@ -23,7 +19,6 @@
   const createAppStore = writable({ currentStep: 0, values: {} })
 
   export let template
-
 
   const infoValidation = {
     applicationName: string().required("Your application must have a name."),
@@ -149,7 +144,6 @@
       )
       const pkg = await applicationPkg.json()
       if (applicationPkg.ok) {
-        // backendUiStore.actions.reset()
         await store.actions.initialise(pkg)
         await automationStore.actions.fetch()
       } else {
