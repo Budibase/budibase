5bf33c5d6bb6bf3da630584f632e121540dab568
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-01 16:09:16 +0100

    Fix string-templates and client buindling for ESM

--- packages/string-templates/rollup.config.js
+++ packages/string-templates/rollup.config.js
@@ -2,19 +2,21 @@ import commonjs from "@rollup/plugin-commonjs"
 import resolve from "rollup-plugin-node-resolve"
 import json from "@rollup/plugin-json"
 import { terser } from "rollup-plugin-terser"
-import polyfillNode from "rollup-plugin-polyfill-node"
+import builtins from "rollup-plugin-node-builtins"
+import globals from "rollup-plugin-node-globals"
 
 const production = !process.env.ROLLUP_WATCH
 
 const plugins = [
   resolve({
-    browser: true,
     preferBuiltins: true,
+    browser: true,
   }),
   commonjs(),
-  polyfillNode(),
-  production && terser(),
+  globals(),
+  builtins(),
   json(),
+  production && terser(),
 ]
 
 export default [
