2ccdf549242d35a9a66feb50ef81dc88c9074ba5
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-07 11:16:48 +0100

    Use node_modules to fetch component manifest in test env

--- packages/server/src/utilities/fileSystem/index.js
+++ packages/server/src/utilities/fileSystem/index.js
@@ -165,13 +165,7 @@ exports.downloadTemplate = async (type, name) => {
 exports.getComponentLibraryManifest = async (appId, library) => {
   const filename = "manifest.json"
   /* istanbul ignore next */
-  // when testing in cypress and so on we need to get the package
-  // as the environment may not be fully fleshed out for dev or prod
-  if (env.isTest()) {
-    const lib = library.split("/")[1]
-    const path = require.resolve(library).split(lib)[0]
-    return require(join(path, lib, filename))
-  } else if (env.isDev()) {
+  if (env.isTest() || env.isDev()) {
     const path = join(
       NODE_MODULES_PATH,
       "@budibase",
