87b1924470d2f4b0ac16f5b2c755dd0f559ab6e8
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-04-07 11:33:16 +0100

    groundwork for budibase auth lib

---
+++ packages/auth/src/index.js
@@ -0,0 +1,31 @@
+const passport = require("koa-passport")
+const LocalStrategy = require("passport-local").Strategy
+const JwtStrategy = require("passport-jwt").Strategy
+// const GoogleStrategy = require("passport-google-oauth").Strategy
+const CouchDB = require("./db")
+const { StaticDatabases } = require("./db/utils")
+const { jwt, local, google } = require("./middleware")
+const hashing = require("./hashing")
+
+// Strategies
+passport.use(new LocalStrategy(local.options, local.authenticate))
+passport.use(new JwtStrategy(jwt.options, jwt.authenticate))
+// passport.use(new GoogleStrategy(google.options, google.authenticate))
+
+passport.serializeUser((user, done) => done(null, user))
+
+passport.deserializeUser(async (user, done) => {
+  const db = new CouchDB(StaticDatabases.USER.name)
+
+  try {
+    const user = await db.get(user._id)
+    return done(null, user)
+  } catch (err) {
+    console.error("User not found", err)
+    return done(null, false, { message: "User not found" })
+  }
+})
+
+// exports.hashing = hashing
+
+module.exports = passport
--- packages/worker/src/api/routes/admin/index.js
+++ packages/worker/src/api/routes/admin/index.js
@@ -1,15 +1,16 @@
 const Router = require("@koa/router")
+const passport = require("@budibase/auth")
 const controller = require("../../controllers/admin")
 const authController = require("../../controllers/admin/auth")
-const authorized = require("../../../middleware/authorized")
+const authenticated = require("../../../middleware/authenticated")
 
 const router = Router()
 
 router
-  .post("/api/admin/users", authorized, controller.userSave)
+  .post("/api/admin/users", authenticated, controller.userSave)
   .post("/api/admin/authenticate", authController.authenticate)
-  .delete("/api/admin/users/:email", authorized, controller.userDelete)
-  .get("/api/admin/users", authorized, controller.userFetch)
-  .get("/api/admin/users/:email", authorized, controller.userFind)
+  .delete("/api/admin/users/:email", authenticated, controller.userDelete)
+  .get("/api/admin/users", passport.authenticate("jwt"), controller.userFetch)
+  .get("/api/admin/users/:email", authenticated, controller.userFind)
 
 module.exports = router
