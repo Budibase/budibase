7f4250dca211e8dcc690d1f25e626ff8822c8fcb
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-08 13:26:45 +0200

    wip: bbui modal to spectrum conversion

--- packages/builder/src/pages/builder/index.svelte
+++ packages/builder/src/pages/builder/index.svelte
@@ -76,6 +76,7 @@
 
   <AppList />
 </div>
+<div id="modal-container" />
 
 <Modal bind:this={modal} padding={false} width="600px" on:hide={closeModal}>
   <CreateAppModal {hasKey} {template} />
@@ -120,5 +121,9 @@
   .button-group {
     display: flex;
     flex-direction: row;
+  }
+
+  #modal-container {
+    position: absolute;
   }
 </style>
