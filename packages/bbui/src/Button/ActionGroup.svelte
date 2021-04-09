a8f5d257eb7afa5aff3686336fad33c6080774be
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-09 13:48:55 +0200

    adds ActionGroup component

---
+++ packages/bbui/src/Button/ActionGroup.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "@spectrum-css/actiongroup/dist/index-vars.css"
+    export let vertical;
+    export let justified;
+    export let quiet;
+    export let compact;
+
+    // Attaches a spectrum-ActionGroup-item class to buttons inside the div
+    function group(element) {
+        const buttons = Array.from(element.getElementsByTagName('button'))
+        buttons.forEach(button => {
+            button.classList.add('spectrum-ActionGroup-item')
+        })
+    }
+</script>
+
+<div use:group class="spectrum-ActionGroup 
+    {vertical && 'spectrum-ActionGroup--vertical'} 
+    {justified && 'spectrum-ActionGroup--justified'} 
+    {quiet && 'spectrum-ActionGroup--quiet'} 
+    {compact && 'spectrum-ActionGroup--compact'}">
+    <slot />
+</div>
