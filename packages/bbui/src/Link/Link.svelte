428ee85e1350ef60519578516efd7105b4bd4656
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-09 14:44:48 +0200

    adds Link component to bbui

---
+++ packages/bbui/src/Link/Link.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "@spectrum-css/link/dist/index-vars.css"
+
+    export let href = "#"
+    export let size = "M"
+    export let quiet = false;
+    export let primary = false;
+    export let secondary = false;
+    export let overBackground = false
+</script>
+
+<a {href} class="spectrum-Link 
+    spectrum-Link--size{size}
+    {primary && 'spectrum-Link--primary'}
+    {secondary && 'spectrum-Link--secondary'}
+    {overBackground && 'spectrum-Link--overBackground'}
+    {quiet && 'spectrum-Link--quiet'}"><slot /></a>
