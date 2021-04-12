5c3fcb053d728fa9e87f3f5a61555e219899ca7f
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-12 16:26:26 +0200

    Implements Menu component

--- packages/bbui/src/Menu/Section.svelte
+++ packages/bbui/src/Menu/Section.svelte
@@ -2,8 +2,8 @@
     export let heading
 </script>
 <li role="presentation">
-<span class="spectrum-Menu-sectionHeading" id="menu-heading-category-1"  aria-hidden="true">{heading}</span>
-    <ul class="spectrum-Menu" role="group" aria-labelledby="menu-heading-category-1">
+<span class="spectrum-Menu-sectionHeading">{heading}</span>
+    <ul class="spectrum-Menu" role="group">
         <slot />
     </ul>
 </li>
