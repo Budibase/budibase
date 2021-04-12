d2a3dc4a2f6f5ca263465459d826243934323c87
mike12345567 <me@michaeldrury.co.uk>
2021-04-12 11:53:37 +0100

    Some fixes for automations, removing password obsfucation in create user step as not useful and making update row work with correct row ID.

--- packages/server/src/automations/steps/createUser.js
+++ packages/server/src/automations/steps/createUser.js
@@ -23,7 +23,6 @@ module.exports.definition = {
         },
         password: {
           type: "string",
-          customType: "password",
           title: "Password",
         },
         roleId: {
