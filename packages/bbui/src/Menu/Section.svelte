b947bac525fb62c333f92570c6be8d30e54231ec
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-12 11:24:03 +0200

    wip menu component

--- packages/bbui/src/Menu/Section.svelte
+++ packages/bbui/src/Menu/Section.svelte
@@ -1,15 +1,9 @@
-<span class="spectrum-Menu-sectionHeading"><slot /></span>
+<script>
+    export let heading
+</script>
 <li role="presentation">
-<span class="spectrum-Menu-sectionHeading" id="menu-heading-category-1"  aria-hidden="true">Section Heading</span>
+<span class="spectrum-Menu-sectionHeading" id="menu-heading-category-1"  aria-hidden="true">{heading}</span>
     <ul class="spectrum-Menu" role="group" aria-labelledby="menu-heading-category-1">
-        <li class="spectrum-Menu-item" role="menuitem" tabindex="0">
-            <span class="spectrum-Menu-itemLabel">Action 1</span>
-        </li>
-        <li class="spectrum-Menu-item" role="menuitem" tabindex="0">
-            <span class="spectrum-Menu-itemLabel">Action 2</span>
-        </li>
-        <li class="spectrum-Menu-item" role="menuitem" tabindex="0">
-            <span class="spectrum-Menu-itemLabel">Action 3</span>
-        </li>
+        <slot />
     </ul>
 </li>
