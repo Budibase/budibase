698c9830565a2fdab621e952a6dddd43375fbfba
mike12345567 <me@michaeldrury.co.uk>
2021-04-12 15:54:14 +0100

    Updating fetch self to get the global user as well as local metadata.

---
+++ packages/server/src/utilities/users.js
@@ -0,0 +1,21 @@
+const CouchDB = require("../../db")
+const {
+  generateUserMetadataID,
+  getEmailFromUserMetadataID,
+} = require("../db/utils")
+const { getGlobalUsers } = require("../../utilities/workerRequests")
+
+exports.getFullUser = async ({ ctx, email, userId }) => {
+  if (!email) {
+    email = getEmailFromUserMetadataID(userId)
+  }
+  const db = new CouchDB(ctx.appId)
+  const global = await getGlobalUsers(ctx, ctx.appId, email)
+  const user = await db.get(generateUserMetadataID(email))
+  return {
+    ...global,
+    ...user,
+    // make sure the ID is always a local ID, not a global one
+    _id: generateUserMetadataID(email),
+  }
+}
