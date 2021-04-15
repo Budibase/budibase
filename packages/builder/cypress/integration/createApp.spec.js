fdfb9dafb244327c0e9092ecbefdc13980c645c7
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-04-15 17:36:58 +0100

    fix cypress tests, only login if cookie not present

--- packages/builder/cypress/integration/createApp.spec.js
+++ packages/builder/cypress/integration/createApp.spec.js
@@ -1,6 +1,5 @@
 context("Create an Application", () => {
   it("should create a new application", () => {
-    cy.login()
     cy.createTestApp()
     cy.visit(`localhost:${Cypress.env("PORT")}/builder`)
     cy.contains("Cypress Tests").should("exist")
