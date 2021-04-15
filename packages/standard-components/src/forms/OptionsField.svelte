13e5c5ad61348502b5b877bd2b1df20b429fbcbb
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-15 11:51:18 +0100

    Update StringField and OptionsField to use BBUI components

--- packages/standard-components/src/forms/OptionsField.svelte
+++ packages/standard-components/src/forms/OptionsField.svelte
@@ -1,6 +1,6 @@
 <script>
+  import { Select } from "@budibase/bbui/internal"
   import Field from "./Field.svelte"
-  import Picker from "./Picker.svelte"
 
   export let field
   export let label
@@ -10,18 +10,6 @@
   let fieldState
   let fieldApi
   let fieldSchema
-
-  // Picker state
-  let open = false
-  $: options = fieldSchema?.constraints?.inclusion ?? []
-  $: placeholderText = placeholder || "Choose an option"
-  $: isNull = $fieldState?.value == null || $fieldState?.value === ""
-  $: fieldText = isNull ? placeholderText : $fieldState?.value
-
-  const selectOption = value => {
-    fieldApi.setValue(value)
-    open = false
-  }
 </script>
 
 <Field
@@ -33,14 +21,13 @@
   bind:fieldApi
   bind:fieldSchema>
   {#if fieldState}
-    <Picker
-      bind:open
-      {fieldState}
-      {fieldText}
-      {options}
-      isPlaceholder={isNull}
-      placeholderOption={placeholderText}
-      isOptionSelected={option => option === $fieldState.value}
-      onSelectOption={selectOption} />
+    <Select
+      value={$fieldState.value}
+      fieldId={$fieldState.fieldId}
+      disabled={$fieldState.disabled}
+      error={$fieldState.error}
+      options={fieldSchema?.constraints?.inclusion ?? []}
+      {placeholder}
+      on:change={e => fieldApi.setValue(e.detail)} />
   {/if}
 </Field>
