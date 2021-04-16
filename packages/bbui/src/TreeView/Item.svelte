8aa6e66be27043775b382ca42140e349aa1e06f3
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-16 16:17:26 +0200

    wip: tree component

---
+++ packages/bbui/src/TreeView/Item.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let selected = false;
+    export let open = false;
+    export let title;
+    export let icon;
+</script>
+
+<li class:is-selected={selected} class:is-open={open} class="spectrum-TreeView-item">
+    <a class="spectrum-TreeView-itemLink" href="#">
+        {#if $$slots.default}
+            <svg class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-TreeView-itemIndicator" focusable="false" aria-hidden="true">
+                <use xlink:href="#spectrum-css-icon-Chevron100" />
+            </svg>
+            {#if icon}
+                <svg class="spectrum-TreeView-itemIcon spectrum-Icon spectrum-Icon--sizeM" focusable="false" aria-hidden="true" aria-label="Layers">
+                    <use xlink:href="#spectrum-icon-18-{icon}" />
+                </svg>
+            {/if}
+            <span class="spectrum-TreeView-itemLabel">{title}</span>
+            <ul class="spectrum-TreeView">
+                <slot />
+            </ul>
+        {:else}
+            {#if icon}
+                <svg class="spectrum-TreeView-itemIcon spectrum-Icon spectrum-Icon--sizeM" focusable="false" aria-hidden="true" aria-label="Layers">
+                    <use xlink:href="#spectrum-icon-18-{icon}" />
+                </svg>
+            {/if}
+            <span class="spectrum-TreeView-itemLabel">{title}</span>
+        {/if}
+    </a>
+</li>
---
+++ packages/bbui/src/TreeView/Tree.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "@spectrum-css/treeview/dist/index-vars.css"
+
+    export let quiet = false
+    export let width;
+</script>
+
+<ul class:spectrum-TreeView--quiet={quiet} class="spectrum-TreeView" style="width: {width}">
+    <slot />
+</ul>
