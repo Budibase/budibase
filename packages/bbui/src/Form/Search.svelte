5b7f58d7b4152a6f4eda7ecc6471291140e44e68
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-20 14:24:14 +0100

    Add search component and update screen list panel

--- packages/bbui/src/Form/Search.svelte
+++ packages/bbui/src/Form/Search.svelte
@@ -1,25 +1,27 @@
-<form class="spectrum-Search">
-  <div class="spectrum-Textfield">
-    <svg
-      class="spectrum-Icon spectrum-Icon--sizeM spectrum-Textfield-icon"
-      focusable="false"
-      aria-hidden="true">
-      <use xlink:href="#spectrum-icon-18-Magnify" />
-    </svg>
-    <input
-      type="search"
-      placeholder="Search"
-      name="search"
-      value=""
-      class="spectrum-Textfield-input spectrum-Search-input"
-      autocomplete="off" />
-  </div>
-  <button type="reset" class="spectrum-ClearButton spectrum-Search-clearButton">
-    <svg
-      class="spectrum-Icon spectrum-UIIcon-Cross75"
-      focusable="false"
-      aria-hidden="true">
-      <use xlink:href="#spectrum-css-icon-Cross75" />
-    </svg>
-  </button>
-</form>
+<script>
+  import Field from "./Field.svelte"
+  import Search from "./Core/Search.svelte"
+  import { createEventDispatcher } from "svelte"
+
+  export let value = null
+  export let label = null
+  export let labelPosition = "above"
+  export let placeholder = null
+  export let disabled = false
+
+  const dispatch = createEventDispatcher()
+  const onChange = e => {
+    value = e.detail
+    dispatch("change", e.detail)
+  }
+</script>
+
+<Field {label} {labelPosition} {disabled}>
+  <Search
+    {disabled}
+    {value}
+    {placeholder}
+    on:change={onChange}
+    on:click
+    on:input />
+</Field>
