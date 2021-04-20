9b477c6f70c6a3549120a6bf411654e1521b2dff
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-04-20 13:13:15 +0100

    lint

--- packages/worker/src/api/controllers/admin/index.js
+++ packages/worker/src/api/controllers/admin/index.js
@@ -1,10 +1,6 @@
 const users = require("./users")
 const groups = require("./groups")
 
-exports.initialise = async function(ctx) {
-  // create the ALL users group
-}
-
 module.exports = {
   users,
   groups,
