7b9377ea1ed91121e1f7f27a6db589ce76e0f4c9
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-20 16:44:08 +0200

    clean up Tree Item component

--- packages/bbui/src/TreeView/Item.svelte
+++ packages/bbui/src/TreeView/Item.svelte
@@ -5,12 +5,14 @@
     export let icon;
 </script>
 
-<li class:is-selected={selected} class:is-open={open} class="spectrum-TreeView-item">
-    {#if $$slots.default}
-        <a class="spectrum-TreeView-itemLink" href="#">
-            <svg class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-TreeView-itemIndicator" focusable="false" aria-hidden="true">
-                <use xlink:href="#spectrum-css-icon-Chevron100" />
-            </svg>
+<li
+    class:is-selected={selected} class:is-open={open} class="spectrum-TreeView-item">
+        <a on:click class="spectrum-TreeView-itemLink" href="#">
+            {#if $$slots.default}
+                <svg class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-TreeView-itemIndicator" focusable="false" aria-hidden="true">
+                    <use xlink:href="#spectrum-css-icon-Chevron100" />
+                </svg>
+            {/if}
             {#if icon}
                 <svg class="spectrum-TreeView-itemIcon spectrum-Icon spectrum-Icon--sizeM" focusable="false" aria-hidden="true" aria-label="Layers">
                     <use xlink:href="#spectrum-icon-18-{icon}" />
@@ -18,17 +20,9 @@
             {/if}
             <span class="spectrum-TreeView-itemLabel">{title}</span>
         </a>
-        <ul class="spectrum-TreeView">
-            <slot />
-        </ul>
-    {:else}
-        <a class="spectrum-TreeView-itemLink" href="#">
-            {#if icon}
-                <svg class="spectrum-TreeView-itemIcon spectrum-Icon spectrum-Icon--sizeM" focusable="false" aria-hidden="true" aria-label="Layers">
-                    <use xlink:href="#spectrum-icon-18-{icon}" />
-                </svg>
-            {/if}
-            <span class="spectrum-TreeView-itemLabel">{title}</span>
-        </a>
-    {/if}
+        {#if $$slots.default}
+            <ul class="spectrum-TreeView">
+                <slot />
+            </ul>
+        {/if}
 </li>
