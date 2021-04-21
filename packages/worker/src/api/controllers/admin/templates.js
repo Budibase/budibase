e85b7682e0f4b0d529680d81f28353e86599a2eb
mike12345567 <me@michaeldrury.co.uk>
2021-04-21 18:15:57 +0100

    Initial CRUD interface for templates.

--- packages/worker/src/api/controllers/admin/templates.js
+++ packages/worker/src/api/controllers/admin/templates.js
@@ -1,25 +1,75 @@
-// const { generateTemplateID, getTemplateParams, StaticDatabases } = require("@budibase/auth").db
-// const { CouchDB } = require("../../../db")
+const { generateTemplateID, getTemplateParams, StaticDatabases } = require("@budibase/auth").db
+const { CouchDB } = require("../../../db")
+const { TemplatePurposePretty } = require("../../../constants")
 
-// const GLOBAL_DB = StaticDatabases.GLOBAL.name
+const GLOBAL_DB = StaticDatabases.GLOBAL.name
+const GLOBAL_OWNER = "global"
+
+async function getTemplates({ ownerId, type, id } = {}) {
+  const db = new CouchDB(GLOBAL_DB)
+  const response = await db.allDocs(
+    getTemplateParams(ownerId, id, {
+      include_docs: true,
+    })
+  )
+  let templates = response.rows.map(row => row.doc)
+  if (type) {
+    templates = templates.filter(template => template.type === type)
+  }
+  return templates
+}
 
 exports.save = async ctx => {
-  // const db = new CouchDB(GLOBAL_DB)
-  // const id = generateTemplateID()
-  ctx.body = {}
+  const db = new CouchDB(GLOBAL_DB)
+  const type = ctx.params.type
+  let template = ctx.request.body
+  if (!template.ownerId) {
+    template.ownerId = GLOBAL_OWNER
+  }
+  if (!template._id) {
+    template._id = generateTemplateID(template.ownerId)
+  }
+
+  const response = await db.put({
+    ...template,
+    type,
+  })
+  ctx.body = {
+    ...template,
+    _rev: response.rev,
+  }
+}
+
+exports.definitions = async ctx => {
+  ctx.body = {
+    purpose: TemplatePurposePretty
+  }
 }
 
 exports.fetch = async ctx => {
-  // const db = new CouchDB(GLOBAL_DB)
-  ctx.body = {}
+  ctx.body = await getTemplates()
+}
+
+exports.fetchByType = async ctx => {
+  ctx.body = await getTemplates({
+    type: ctx.params.type,
+  })
+}
+
+exports.fetchByOwner = async ctx => {
+  ctx.body = await getTemplates({
+    ownerId: ctx.params.ownerId,
+  })
 }
 
 exports.find = async ctx => {
-  // const db = new CouchDB(GLOBAL_DB)
-  ctx.body = {}
+  ctx.body = await getTemplates({
+    id: ctx.params.id,
+  })
 }
 
 exports.destroy = async ctx => {
-  // const db = new CouchDB(GLOBAL_DB)
+  // TODO
+  const db = new CouchDB(GLOBAL_DB)
   ctx.body = {}
 }
--- packages/worker/src/constants/index.js
+++ packages/worker/src/constants/index.js
@@ -6,3 +6,29 @@ exports.UserStatus = {
 exports.Groups = {
   ALL_USERS: "all_users",
 }
+
+exports.TemplateTypes = {
+  EMAIL: "email",
+}
+
+exports.TemplatePurpose = {
+  PASSWORD_RECOVERY: "password_recovery",
+  INVITATION: "invitation",
+  CUSTOM: "custom",
+}
+
+exports.TemplatePurposePretty = [
+  {
+    name: "Password Recovery",
+    value: exports.TemplatePurpose.PASSWORD_RECOVERY
+  },
+  {
+    name: "New User Invitation",
+    value: exports.TemplatePurpose.INVITATION,
+  },
+  {
+    name: "Custom",
+    value: exports.TemplatePurpose.CUSTOM,
+  }
+]
+
