c6f0b868a0b67bc3a2f61f677cb3186d966f2dc6
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 14:09:56 +0200

    implements ClearButton

---
+++ packages/bbui/src/ClearButton/ClearButton.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let disabled;
+</script>
+
+<button on:click class="spectrum-ClearButton" {disabled}>
+    <svg class="spectrum-Icon spectrum-UIIcon-Cross75" focusable="false" aria-hidden="true">
+        <use xlink:href="#spectrum-css-icon-Cross75" />
+    </svg>
+</button>
