b9f5868fb3056186a2c04e596ca3d3ad9ce4533f
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 14:51:17 +0200

    fix Menu component

--- packages/bbui/src/Menu/Item.svelte
+++ packages/bbui/src/Menu/Item.svelte
@@ -4,12 +4,10 @@
 </script>
 
 <li on:click|preventDefault class="spectrum-Menu-item" class:is-disabled={disabled} role="menuitem" tabindex="0">
-    <span class="spectrum-Menu-itemLabel">
-        {#if icon}
-            <svg class="spectrum-Icon spectrum-Icon--sizeM spectrum-Menu-itemIcon" focusable="false" aria-hidden="true" aria-label={icon}>
-                <use xlink:href="#spectrum-icon-18-{icon}"></use>
-            </svg>
-        {/if}
-        <span class="spectrum-Menu-itemLabel"><slot /></span>
-    </span>
+    {#if icon}
+        <svg class="spectrum-Icon spectrum-Icon--sizeM spectrum-Menu-itemIcon" focusable="false" aria-hidden="true" aria-label={icon}>
+            <use xlink:href="#spectrum-icon-18-{icon}"></use>
+        </svg>
+    {/if}
+    <span class="spectrum-Menu-itemLabel"><slot /></span>
 </li>
