4afe779eb45572b368485807c798ec418516f88f
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 14:19:10 +0200

    fix ButtonGroup not adding classes to children

--- packages/bbui/src/ButtonGroup/ButtonGroup.svelte
+++ packages/bbui/src/ButtonGroup/ButtonGroup.svelte
@@ -1,7 +1,15 @@
 <script>
+    import "@spectrum-css/buttongroup/dist/index-vars.css"
     export let vertical = false
+
+    function group(element) {
+        const buttons = Array.from(element.getElementsByTagName('button'))
+        buttons.forEach(button => {
+            button.classList.add('spectrum-ButtonGroup-item')
+        })
+    }
 </script>
 
-<div class="spectrum-ButtonGroup" class:spectrum-ButtonGroup--vertical={vertical}>
+<div use:group class="spectrum-ButtonGroup" class:spectrum-ButtonGroup--vertical={vertical}>
     <slot />
 </div>
