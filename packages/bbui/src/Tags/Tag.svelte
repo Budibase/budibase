1f608ad7619a688a0620e2cfb4f269a75d51339a
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 13:53:49 +0200

    wip: initial tags

---
+++ packages/bbui/src/Tags/Tag.svelte
@@ -0,0 +1,11 @@
+<div class="spectrum-Tags" role="list" aria-label="list">
+    <div class="spectrum-Tags-item" role="listitem">
+      <span class="spectrum-Tags-itemLabel">Tag 1</span>
+    </div>
+    <div class="spectrum-Tags-item is-invalid" role="listitem">
+      <span class="spectrum-Tags-itemLabel">Tag 2</span>
+    </div>
+    <div class="spectrum-Tags-item is-disabled" role="listitem">
+      <span class="spectrum-Tags-itemLabel">Tag 2</span>
+    </div>
+  </div>
