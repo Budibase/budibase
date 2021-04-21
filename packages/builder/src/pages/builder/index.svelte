1888fbacca5036f0abe1263dbdc9f2e922c2f5d9
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 13:55:38 +0200

    remove old Switcher import

--- packages/builder/src/pages/builder/index.svelte
+++ packages/builder/src/pages/builder/index.svelte
@@ -3,7 +3,7 @@
   import AppList from "components/start/AppList.svelte"
   import { get } from "builderStore/api"
   import CreateAppModal from "components/start/CreateAppModal.svelte"
-  import { Button, Heading, Modal, Spacer, Divider } from "@budibase/bbui"
+  import { Button, Heading, Modal, Spacer, Tags, Tag } from "@budibase/bbui"
   import TemplateList from "components/start/TemplateList.svelte"
   import analytics from "analytics"
   import Banner from "/assets/orange-landscape.png"
@@ -75,6 +75,13 @@
   <!-- <TemplateList onSelect={selectTemplate} /> -->
 
   <AppList />
+
+  <Tags>
+    <Tag>Test</Tag>
+    <Tag>Avatar</Tag>
+    <Tag>Error</Tag>
+    <Tag>Disabled</Tag>
+  </Tags>
 </div>
 
 <Modal bind:this={modal} padding={false} width="600px" on:hide={closeModal}>
