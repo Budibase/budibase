bade192b6d5cb5d2946fa1c4af809c168270f536
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-21 14:09:36 +0200

    implements Avatar component

---
+++ packages/bbui/src/Avatar/Avatar.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "@spectrum-css/avatar/dist/index-vars.css"
+    export let url = "";
+    export let disabled = false;
+</script>
+
+<img class:is-disabled={disabled} class="spectrum-Avatar" src={url} alt="Avatar">
