f6e25c3792d9f29d833f1b976411b784a074a82a
mike12345567 <me@michaeldrury.co.uk>
2021-04-23 13:10:37 +0100

    putting together the bulk of the email sending/config options.

--- packages/worker/src/api/routes/admin/configs.js
+++ packages/worker/src/api/routes/admin/configs.js
@@ -6,11 +6,52 @@ const { Configs } = require("../../../constants")
 
 const router = Router()
 
+function smtpValidation() {
+  return Joi.object({
+    port: Joi.number().required(),
+    host: Joi.string().required(),
+    from: Joi.string().email().required(),
+    secure: Joi.boolean().optional(),
+    selfSigned: Joi.boolean().optional(),
+    auth: Joi.object({
+      type: Joi.string().valid("login", "oauth2", null),
+      user: Joi.string().required(),
+      pass: Joi.string().valid("", null),
+    }).optional(),
+  }).unknown(true)
+}
+
+function settingValidation() {
+  return Joi.object({
+    url: Joi.string().valid("", null),
+    logoUrl: Joi.string().valid("", null),
+    company: Joi.string().required(),
+  }).unknown(true)
+}
+
+function googleValidation() {
+  return Joi.object({
+    clientID: Joi.string().required(),
+    clientSecret: Joi.string().required(),
+    callbackURL: Joi.string().required(),
+  }).unknown(true)
+}
+
 function buildConfigSaveValidation() {
   // prettier-ignore
   return joiValidator.body(Joi.object({
     type: Joi.string().valid(...Object.values(Configs)).required(),
-  }).required().unknown(true))
+    config: Joi.alternatives()
+      .conditional("type", {
+        switch: [
+          { is: Configs.SMTP, then: smtpValidation() },
+          { is: Configs.SETTINGS, then: settingValidation() },
+          { is: Configs.ACCOUNT, then: Joi.object().unknown(true) },
+          { is: Configs.GOOGLE, then: googleValidation() }
+        ],
+      }),
+    }),
+  )
 }
 
 router
