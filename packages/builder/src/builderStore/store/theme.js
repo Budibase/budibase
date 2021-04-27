e89c51deef736e0cbda49c1f25aa02f3b80ee2ff
Andrew Kingston <aptkingston@ssl-mail.com>
2021-04-27 13:15:49 +0100

    Add support for all 4 spectrum themes in builder!

--- packages/builder/src/builderStore/store/theme.js
+++ packages/builder/src/builderStore/store/theme.js
@@ -3,14 +3,25 @@ import { localStorageStore } from "./localStorage"
 export const getThemeStore = () => {
   const themeElement = document.documentElement
   const initialValue = {
-    darkMode: true,
+    theme: "darkest",
+    options: ["lightest", "light", "dark", "darkest"],
   }
   const store = localStorageStore("bb-theme", initialValue)
 
-  // Update theme when store changes
-  store.subscribe(theme => {
-    themeElement.classList.toggle("spectrum--darkest", theme.darkMode)
-    themeElement.classList.toggle("spectrum--lightest", !theme.darkMode)
+  // Update theme class when store changes
+  store.subscribe(state => {
+    // Handle any old local storage values - this can be removed after the update
+    if (state.darkMode !== undefined) {
+      store.set(initialValue)
+      return
+    }
+
+    state.options.forEach(option => {
+      themeElement.classList.toggle(
+        `spectrum--${option}`,
+        option === state.theme
+      )
+    })
   })
 
   return store
