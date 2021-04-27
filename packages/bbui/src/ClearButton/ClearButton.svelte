19052a66a5269af06edc756316481e789a0eaf3b
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-27 16:23:42 +0200

    rework ClearButton

--- packages/bbui/src/ClearButton/ClearButton.svelte
+++ packages/bbui/src/ClearButton/ClearButton.svelte
@@ -1,10 +1,19 @@
 <script>
-    export let s = false;
-    export let disabled;
+  export let small = false
+  export let disabled
 </script>
 
-<button on:click class="spectrum-ClearButton" class:spectrum-ClearButton--small={s} {disabled}>
-    <svg class="spectrum-Icon spectrum-UIIcon-Cross75" focusable="false" aria-hidden="true">
-        <use xlink:href="#spectrum-css-icon-Cross75" />
-    </svg>
+<button
+  on:click
+  class="spectrum-ClearButton"
+  class:spectrum-ClearButton--small={small}
+  {disabled}
+>
+  <svg
+    class="spectrum-Icon spectrum-UIIcon-Cross75"
+    focusable="false"
+    aria-hidden="true"
+  >
+    <use xlink:href="#spectrum-css-icon-Cross75" />
+  </svg>
 </button>
