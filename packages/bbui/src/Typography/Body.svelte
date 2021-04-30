8ecdbc9bf4b709956371e579461d545d99c52a91
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-04-30 13:33:50 +0200

    convert Heading to new API

--- packages/bbui/src/Typography/Body.svelte
+++ packages/bbui/src/Typography/Body.svelte
@@ -1,11 +1,13 @@
 <script>
-    import "@spectrum-css/typography/dist/index-vars.css"
-    
-    export let size = "M";
-    export let serif = false;
+  import "@spectrum-css/typography/dist/index-vars.css"
+
+  export let size = "M"
+  export let serif = false
 </script>
 
-<p class="spectrum-Body class:spectrum-Body--size{size}"
-    class:spectrum-Body--serif={serif}
-    <slot />
+<p
+  class="spectrum-Body class:spectrum-Body--size{size}"
+  class:spectrum-Body--serif={serif}
+>
+  <slot />
 </p>
