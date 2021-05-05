c381f08b4aa003952efd3df4ad06e31a42947f4f
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-05 17:21:35 +0100

    fix styling

--- packages/builder/src/pages/portal/oauth/index.svelte
+++ packages/builder/src/pages/portal/oauth/index.svelte
@@ -59,27 +59,34 @@
 </script>
 
 <section class="container">
-  <Heading size="M">OAuth</Heading>
-  <Body size="S">
-    Every budibase app comes with basic authentication (email/password)
-    included. You can add additional authentication methods from the options
-    below.
-  </Body>
+  <header>
+    <Heading size="M">OAuth</Heading>
+    <Body size="S">
+      Every budibase app comes with basic authentication (email/password)
+      included. You can add additional authentication methods from the options
+      below.
+    </Body>
+  </header>
   <Divider />
   {#if google}
-    <Layout gap="S">
-      <Heading size="S">
-        <GoogleLogo />
-        Google
-      </Heading>
-      {#each ConfigFields.Google as field}
-        <div class="form-row">
-          <Label>{field}</Label>
-          <Input bind:value={google.config[field]} />
-        </div>
-      {/each}
-    </Layout>
-    <Button primary on:click={() => save(google)}>Save</Button>
+    <div class="config-form">
+      <Layout gap="S">
+        <Heading size="S">
+          <span>
+            <GoogleLogo />
+            Google
+          </span>
+        </Heading>
+        {#each ConfigFields.Google as field}
+          <div class="form-row">
+            <Label>{field}</Label>
+            <Input bind:value={google.config[field]} />
+          </div>
+        {/each}
+      </Layout>
+      <Button primary on:click={() => save(google)}>Save</Button>
+    </div>
+    <Divider />
   {/if}
 </section>
 
@@ -88,9 +95,24 @@
     margin: 60px 320px;
   }
 
+  .config-form {
+    margin-top: 42px;
+    margin-bottom: 42px;
+  }
+
   .form-row {
     display: grid;
     grid-template-columns: 20% 1fr;
     grid-gap: var(--spacing-l);
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: var(--spacing-s);
+  }
+
+  header {
+    margin-bottom: 42px;
   }
 </style>
