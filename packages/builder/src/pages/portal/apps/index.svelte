99a02eb94d98445bc6f5d5176fe2cc3bfb8d0b64
Andrew Kingston <aptkingston@ssl-mail.com>
2021-05-06 10:22:36 +0100

    Add skeleton for apps page as example of full width page

---
+++ packages/builder/src/pages/portal/apps/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Heading, Layout } from "@budibase/bbui"
+</script>
+
+<Layout noPadding>
+  <div>
+    <Heading>Apps</Heading>
+  </div>
+  <div class="appList">
+    {#each new Array(10) as _}
+      <div class="app" />
+    {/each}
+  </div>
+</Layout>
+
+<style>
+  .appList {
+    display: grid;
+    grid-gap: 50px;
+    grid-template-columns: repeat(auto-fill, 300px);
+  }
+  .app {
+    height: 130px;
+    border-radius: 4px;
+    background-color: var(--spectrum-global-color-gray-200);
+  }
+</style>
