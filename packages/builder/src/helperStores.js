b0e20eddc3145f64829ab4d18e1288d328f03e6b
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-05-07 14:30:51 +0200

    creates a custom helper store for fetching data

---
+++ packages/builder/src/helperStores.js
@@ -0,0 +1,20 @@
+import { writable } from 'svelte/store'
+import api from "builderStore/api"
+
+export function fetchData (url) {
+	const store = writable({status: 'LOADING', data: {}, error: {}})
+	
+	async function get() {
+		store.update(u => ({...u, status: 'SUCCESS'}))
+		try {
+			const response = await api.get(url)
+            store.set({data: await response.json(), status: 'SUCCESS'})
+		} catch(e) {
+            store.set({data: {}, error: e, status: 'ERROR'})
+		}
+	}
+	
+	get()
+	
+	return [store, get]
+}
