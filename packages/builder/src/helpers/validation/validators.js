548c56cb00d7fb7221358695f90a1135976e83de
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-05-11 18:19:32 +0200

    change exports

--- packages/builder/src/helpers/validation/validators.js
+++ packages/builder/src/helpers/validation/validators.js
@@ -1,12 +1,7 @@
-function emailValidator (value) {
+export function emailValidator (value) {
     return (value && !!value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
   }
 
-function requiredValidator (value) {
+export function requiredValidator (value) {
     return (value !== undefined && value !== null && value !== '') || 'This field is required'
   }
-
-export {
-  emailValidator,
-  requiredValidator
-}
