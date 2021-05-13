48006408f14674e2d5e15cd1a2677425d09c1751
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-13 17:33:31 +0100

    publish button

--- packages/builder/src/constants/index.js
+++ packages/builder/src/constants/index.js
@@ -11,7 +11,7 @@ export const FrontendTypes = {
 
 export const AppStatus = {
   DEV: "dev",
-  DEPLOYED: "deployed"
+  PUBLISHED: "published"
 }
 
 // fields on the user table that cannot be edited
