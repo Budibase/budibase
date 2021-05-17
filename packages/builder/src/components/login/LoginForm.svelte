3641ee5e2ce64ff2e3e41c8574f93ce4d0cc669f
mike12345567 <me@michaeldrury.co.uk>
2021-05-17 13:47:23 +0100

    Work in progress, moving login form around a bit.

--- packages/builder/src/components/login/LoginForm.svelte
+++ packages/builder/src/components/login/LoginForm.svelte
@@ -2,11 +2,13 @@
   import { goto } from "@roxi/routify"
   import {
     notifications,
-    Link,
     Input,
     Modal,
     ModalContent,
+    Button,
+    Divider,
   } from "@budibase/bbui"
+  import GoogleButton from "./GoogleButton.svelte"
   import { auth } from "stores/backend"
 
   let username = ""
@@ -25,35 +27,66 @@
       notifications.error("Invalid credentials")
     }
   }
+
+  async function forgot() {
+
+  }
 </script>
 
 <Modal fixed>
   <ModalContent
     size="M"
     title="Log In"
-    onConfirm={login}
-    confirmText="Log In"
     showCancelButton={false}
+    showConfirmButton={false}
     showCloseIcon={false}
   >
     <Input label="Email" bind:value={username} />
     <Input label="Password" type="password" on:change bind:value={password} />
-    <div class="footer" slot="footer">
-      <Link target="_blank" href="/api/admin/auth/google">
-        Sign In With Google
-      </Link>
+    <div class="login-buttons">
+      <Button secondary on:click={forgot}>Forgot Password?</Button>
+      <Button cta on:click={login}>Login</Button>
+    </div>
+    <div class="or-divide">
+      <Divider noGrid />
+      <div class="or-label">
+        OR
+      </div>
+    </div>
+    <div class="sso">
+      <GoogleButton />
     </div>
   </ModalContent>
 </Modal>
 
 <style>
-  .footer {
+  .sso {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .login-buttons {
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
     align-items: center;
   }
-  .footer :global(a) {
-    margin-right: var(--spectrum-global-dimension-static-size-200);
+  .login-buttons :global(>*:not(:last-child)) {
+    margin-right: var(--spacing-xl);
+  }
+  .or-divide {
+    position: relative;
+  }
+  .or-label {
+    position: absolute;
+    top: calc(50% - 2px);
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    background-color: var(--background);
+    padding: 0 var(--spacing-xl);
+    font-weight: 500;
+    font-size: var(--font-size-s);
+    color: var(--spectrum-global-color-gray-600);
   }
 </style>
