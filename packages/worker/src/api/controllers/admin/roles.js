dd4a963c3e5916a022c6abebc4c7087a5fd6bd95
mike12345567 <me@michaeldrury.co.uk>
2021-05-17 14:20:19 +0100

    Updating role system to never think about the dev app.

--- packages/worker/src/api/controllers/admin/roles.js
+++ packages/worker/src/api/controllers/admin/roles.js
@@ -1,17 +1,24 @@
 const { getAllRoles } = require("@budibase/auth/roles")
-const { getAllApps } = require("@budibase/auth/db")
+const { getAllApps, getDeployedAppID } = require("@budibase/auth/db")
 
 exports.fetch = async ctx => {
   // always use the dev apps as they'll be most up to date (true)
   const apps = await getAllApps(true)
   const promises = []
   for (let app of apps) {
+    // use dev app IDs
     promises.push(getAllRoles(app._id))
   }
   const roles = await Promise.all(promises)
   const response = {}
   for (let app of apps) {
-    response[app._id] = roles.shift()
+    const deployedAppId = getDeployedAppID(app._id)
+    response[deployedAppId] = {
+      roles: roles.shift(),
+      name: app.name,
+      version: app.version,
+      url: app.url,
+    }
   }
   ctx.body = response
 }
