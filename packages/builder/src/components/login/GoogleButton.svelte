db87d1d62145a50d12935e112d42bea4e61bc4d1
mike12345567 <me@michaeldrury.co.uk>
2021-05-17 13:47:23 +0100

    Work in progress, moving login form around a bit.

---
+++ packages/builder/src/components/login/GoogleButton.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { Link } from "@budibase/bbui"
+  import GoogleLogo from "/assets/google-logo.png"
+</script>
+
+<div class="outer">
+  <Link target="_blank" href="/api/admin/auth/google">
+    <div class="inner">
+      <img src={GoogleLogo} alt="google icon" />
+      <p>Sign in with Google</p>
+    </div>
+  </Link>
+</div>
+
+<style>
+  .inner {
+    display: flex;
+    flex-direction: row;
+    background-color: #4285f4;
+    border-radius: 3px;
+    padding-right: 12px;
+  }
+
+  .inner img {
+    width: 24px;
+    margin: 1px 14px 1px 1px;
+    border-radius: 2px;
+    background-color: #fff;
+    padding: 8px;
+  }
+
+  .inner p {
+    margin: 12px 0 0;
+  }
+
+  .outer :global(a) {
+    text-decoration: none;
+    font-weight: bold;
+    color: #fff;
+  }
+</style>
