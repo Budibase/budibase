f7c05d063137b5a8467989237888c71e0e8456be
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-05-17 10:10:51 +0200

    wip: roles fetch

--- packages/builder/src/pages/builder/portal/manage/users/[userId].svelte
+++ packages/builder/src/pages/builder/portal/manage/users/[userId].svelte
@@ -33,6 +33,13 @@
   let selectedApp
 
   const request = fetchData(`/api/admin/users/${userId}`)
+  const roles = fetchData(
+    `/api/admin/roles/app_5a72d9b923504765852338e614a72c85`
+  )
+
+  $: console.log($apps)
+
+  $: console.log($roles)
 
   async function deleteUser() {
     const res = await users.del(userId)
@@ -62,8 +69,10 @@
       <Body
         >Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis porro
         ut nesciunt ipsam perspiciatis aliquam et hic minus alias beatae. Odit
-        veritatis quos quas laborum magnam tenetur perspiciatis ex hic.</Body
-      >
+        veritatis quos quas laborum magnam tenetur perspiciatis ex hic.
+      </Body>
+      {JSON.stringify($roles.data, null, 2)}
+      <Body />
     </Layout>
   </div>
   <Divider size="S" />
