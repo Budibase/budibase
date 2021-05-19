ba1ee6b033617eaabff54012483097bd7e5faa42
Andrew Kingston <aptkingston@ssl-mail.com>
2021-05-19 13:18:58 +0100

    Add modal to let users change their own password, without endpoint for now

---
+++ packages/builder/src/pages/builder/apps/_components/ChangePasswordModal.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { ModalContent, Body, notifications } from "@budibase/bbui"
+  import PasswordRepeatInput from "components/common/users/PasswordRepeatInput.svelte"
+
+  let password
+  let error
+
+  const updatePassword = async () => {
+    // Update password
+  }
+</script>
+
+<ModalContent
+  title="Change password"
+  confirmText="Update password"
+  onConfirm={updatePassword}
+  disabled={error}
+>
+  <Body size="S">Enter your new password below.</Body>
+  <PasswordRepeatInput bind:password bind:error />
+</ModalContent>
