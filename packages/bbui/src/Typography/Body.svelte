54d16a3bf3cc22f72bb116e5e5f39ce904b9802b
Andrew Kingston <aptkingston@ssl-mail.com>
2021-05-19 09:02:51 +0100

    Remove margin in BBUI body component

--- packages/bbui/src/Typography/Body.svelte
+++ packages/bbui/src/Typography/Body.svelte
@@ -3,27 +3,14 @@
 
   export let size = "M"
   export let serif = false
-  export let noPadding = false
   export let weight = 400
   export let textAlign = "left"
 </script>
 
 <p
   style="font-weight:{weight};text-align:{textAlign};"
-  class:noPadding
   class="spectrum-Body spectrum-Body--size{size}"
   class:spectrum-Body--serif={serif}
 >
   <slot />
 </p>
-
-<style>
-  p {
-    margin-top: 0.75em;
-    margin-bottom: 0.75em;
-  }
-  .noPadding {
-    padding: 0;
-    margin: 0;
-  }
-</style>
