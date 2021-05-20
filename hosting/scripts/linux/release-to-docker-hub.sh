f6f32b98ba919a5dd0cf093db4698d18fe3c097c
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-20 12:46:31 +0100

    testing CI pipeline

--- hosting/scripts/linux/release-to-docker-hub.sh
+++ hosting/scripts/linux/release-to-docker-hub.sh
@@ -10,13 +10,13 @@ docker-compose build --force worker-service
 
 echo "Tagging images with SHA: $GITHUB_SHA and version: $BUDIBASE_VERSION"
 
-docker tag build_app-service budibase/budibase-apps:$tag
-docker tag build_worker-service budibase/budibase-worker:$tag
+docker tag build_app-service budibase/apps:$tag
+docker tag build_worker-service budibase/worker:$tag
 
 # Tag with git sha
-docker tag build_app-service budibase/budibase-apps:$GITHUB_SHA
-docker tag build_worker-service budibase/budibase-worker:$GITHUB_SHA
+docker tag build_app-service budibase/apps:$GITHUB_SHA
+docker tag build_worker-service budibase/worker:$GITHUB_SHA
 
-docker push budibase/budibase-apps
-docker push budibase/budibase-worker
+docker push budibase/apps
+docker push budibase/worker
 popd
