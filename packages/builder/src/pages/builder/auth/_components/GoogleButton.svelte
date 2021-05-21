c06edd8b44291412b62ba43126469f68d88af7f6
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-21 14:45:56 +0100

    enter to login, google auth only show if configured

--- packages/builder/src/pages/builder/auth/_components/GoogleButton.svelte
+++ packages/builder/src/pages/builder/auth/_components/GoogleButton.svelte
@@ -1,24 +1,32 @@
 <script>
+  import { onMount } from "svelte"
   import { ActionButton } from "@budibase/bbui"
   import GoogleLogo from "/assets/google-logo.png"
+
+  let show = false
+
+  async function fetchConfig() {
+    const googleResponse = await api.get(
+      `/api/admin/configs/${ConfigTypes.Google}`
+    )
+    const googleDoc = await googleResponse.json()
+
+    if (googleDoc._id) show = true 
+  }
 </script>
 
-<ActionButton>
-  <a target="_blank" href="/api/admin/auth/google">
-    <div class="inner">
-      <img src={GoogleLogo} alt="google icon" />
-      <p>Sign in with Google</p>
-    </div>
-  </a>
-</ActionButton>
+{#if show}
+  <ActionButton>
+    <a target="_blank" href="/api/admin/auth/google">
+      <div class="inner">
+        <img src={GoogleLogo} alt="google icon" />
+        <p>Sign in with Google</p>
+      </div>
+    </a>
+  </ActionButton>
+{/if}
 
 <style>
-  .outer {
-    border: 1px solid #494949;
-    border-radius: 4px;
-    width: 100%;
-    background-color: var(--background-alt);
-  }
   .inner {
     display: flex;
     flex-direction: row;
