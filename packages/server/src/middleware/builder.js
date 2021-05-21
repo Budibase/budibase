aa51bf7f07a0cb8c9ef66e8dfd2cc37eeebff031
mike12345567 <me@michaeldrury.co.uk>
2021-05-21 15:03:28 +0100

    Switching logic for lazy evaluation.

--- packages/server/src/middleware/builder.js
+++ packages/server/src/middleware/builder.js
@@ -44,7 +44,7 @@ async function updateAppUpdatedAt(ctx) {
   const appId = ctx.appId
   // if debouncing skip this update
   // get methods also aren't updating
-  if ((await checkDebounce(appId)) || ctx.method === "GET") {
+  if (ctx.method === "GET" || (await checkDebounce(appId))) {
     return
   }
   const db = new CouchDB(appId)
