35e361717c2665a81c23c1297ced243c738db500
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-23 15:40:00 +0100

    make attachments downloadable and images clickable

---
+++ packages/bbui/src/Tooltip/Tooltip.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "@spectrum-css/tooltip/dist/index-vars.css"
+
+  export let direction = "top"
+  export let text = ""
+</script>
+
+<span class="u-tooltip-showOnHover tooltip">
+  <slot />
+  <div class={`spectrum-Tooltip spectrum-Tooltip--${direction}`}>
+    <span class="spectrum-Tooltip-label">{text}</span>
+    <span class="spectrum-Tooltip-tip" />
+  </div>
+</span>
