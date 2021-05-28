f348f1862123933434457eb85831fe4600caca19
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-28 12:44:29 +0100

    go straight to app if you only have one published app

--- packages/builder/src/pages/builder/portal/index.svelte
+++ packages/builder/src/pages/builder/portal/index.svelte
@@ -1,4 +1,18 @@
 <script>
-  import { redirect } from "@roxi/routify"
-  $redirect("./apps")
+  import { onMount } from "svelte"
+  import { redirect, goto } from "@roxi/routify"
+  import { apps, organisation, auth } from "stores/portal"
+  import { AppStatus } from "constants"
+
+  onMount(async () => {
+    await apps.load()
+    // Skip the portal if you only have one app
+    if (!$auth.isBuilder && $apps.filter(app => app.status === AppStatus.DEPLOYED).length === 1) {
+      // window.location = `/${publishedApps[0].prodId}`
+      $redirect(`/${publishedApps[0].prodId}`)
+    } else {
+      $goto("./apps")
+    }
+  })
+
 </script>
