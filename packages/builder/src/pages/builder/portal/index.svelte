587e6d36a11e2ee258e6ab199c1ca5cdb7d775ba
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-05-28 14:06:18 +0100

    tidy up

--- packages/builder/src/pages/builder/portal/index.svelte
+++ packages/builder/src/pages/builder/portal/index.svelte
@@ -1,18 +1,4 @@
 <script>
-  import { onMount } from "svelte"
-  import { redirect, goto } from "@roxi/routify"
-  import { apps, organisation, auth } from "stores/portal"
-  import { AppStatus } from "constants"
-
-  onMount(async () => {
-    await apps.load()
-    // Skip the portal if you only have one app
-    if (!$auth.isBuilder && $apps.filter(app => app.status === AppStatus.DEPLOYED).length === 1) {
-      // window.location = `/${publishedApps[0].prodId}`
-      $redirect(`/${publishedApps[0].prodId}`)
-    } else {
-      $goto("./apps")
-    }
-  })
-
+  import { redirect } from "@roxi/routify"
+  $redirect("./apps")
 </script>
