0166c48d40d2c36224405780d0f6440f57eb51c8
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-01 20:22:56 +0100

    removing superfluous controllers

--- packages/worker/src/api/routes/admin/updates.js
+++
@@ -1,9 +0,0 @@
-const Router = require("@koa/router")
-const controller = require("../../controllers/admin/updates")
-const adminOnly = require("../../../middleware/adminOnly")
-
-const router = Router()
-
-router.get("/api/admin/update", adminOnly, controller.updateSystem)
-
-module.exports = router
