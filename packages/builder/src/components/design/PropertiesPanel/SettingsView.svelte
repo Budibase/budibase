ebdbbdc094f3c90ff5c2ebd4542c9646e3a23108
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-06-04 08:57:18 +0200

    wip layout component

--- packages/builder/src/components/design/PropertiesPanel/SettingsView.svelte
+++ packages/builder/src/components/design/PropertiesPanel/SettingsView.svelte
@@ -15,6 +15,7 @@
   import FieldSelect from "./PropertyControls/FieldSelect.svelte"
   import MultiFieldSelect from "./PropertyControls/MultiFieldSelect.svelte"
   import SchemaSelect from "./PropertyControls/SchemaSelect.svelte"
+  import ModalSelect from "./PropertyControls/ModalSelect.svelte"
   import EventsEditor from "./PropertyControls/EventsEditor"
   import FilterEditor from "./PropertyControls/FilterEditor/FilterEditor.svelte"
   import { IconSelect } from "./PropertyControls/IconSelect"
@@ -69,6 +70,7 @@
     field: FieldSelect,
     multifield: MultiFieldSelect,
     schema: SchemaSelect,
+    modal: ModalSelect,
     filter: FilterEditor,
     "field/string": StringFieldSelect,
     "field/number": NumberFieldSelect,
