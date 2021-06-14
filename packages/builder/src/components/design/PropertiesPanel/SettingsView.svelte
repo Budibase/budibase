7566a84aebe25b5923c7e7f9f88bfaf2d1ff838a
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-06-14 07:34:42 +0200

    missed files that were not commited on friday

--- packages/builder/src/components/design/PropertiesPanel/SettingsView.svelte
+++ packages/builder/src/components/design/PropertiesPanel/SettingsView.svelte
@@ -15,7 +15,8 @@
   import FieldSelect from "./PropertyControls/FieldSelect.svelte"
   import MultiFieldSelect from "./PropertyControls/MultiFieldSelect.svelte"
   import SchemaSelect from "./PropertyControls/SchemaSelect.svelte"
-  import ModalSelect from "./PropertyControls/ModalSelect.svelte"
+  import SectionSelect from "./PropertyControls/SectionSelect.svelte"
+  import NavigationSelect from "./PropertyControls/NavigationSelect.svelte"
   import EventsEditor from "./PropertyControls/EventsEditor"
   import FilterEditor from "./PropertyControls/FilterEditor/FilterEditor.svelte"
   import { IconSelect } from "./PropertyControls/IconSelect"
@@ -70,7 +71,8 @@
     field: FieldSelect,
     multifield: MultiFieldSelect,
     schema: SchemaSelect,
-    modal: ModalSelect,
+    section: SectionSelect,
+    navigationSelect: NavigationSelect,
     filter: FilterEditor,
     "field/string": StringFieldSelect,
     "field/number": NumberFieldSelect,
