1a94ef5039ea6ad86cf2373442ac6a12edd4024d
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-16 17:50:22 +0100

    end to end data providers and opinionated sql

--- packages/server/src/integrations/plus/postgres.js
+++ packages/server/src/integrations/plus/postgres.js
@@ -62,11 +62,11 @@
 //     "select * from information_schema.columns where table_schema = 'public'"
 
 //   PRIMARY_KEYS_SQL = `
-//     select tc.table_schema, tc.table_name, kc.column_name as primary_key 
+//     select tc.table_schema, tc.table_name, kc.column_name as primary_key
 //     from information_schema.table_constraints tc
-//     join 
-//       information_schema.key_column_usage kc on kc.table_name = tc.table_name 
-//       and kc.table_schema = tc.table_schema 
+//     join
+//       information_schema.key_column_usage kc on kc.table_name = tc.table_name
+//       and kc.table_schema = tc.table_schema
 //       and kc.constraint_name = tc.constraint_name
 //     where tc.constraint_type = 'PRIMARY KEY';
 //   `
