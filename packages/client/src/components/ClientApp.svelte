5a6a40a80c2ac7d96c7bf5131a3e4bc5fc22a8be
Andrew Kingston <aptkingston@ssl-mail.com>
2021-06-17 13:20:29 +0100

    Key client layout component to ensure fully remounting when swapping layouts in the builder preview

--- packages/client/src/components/ClientApp.svelte
+++ packages/client/src/components/ClientApp.svelte
@@ -65,7 +65,9 @@
   >
     <Provider key="user" data={$authStore} {actions}>
       <div id="app-root">
-        <Component instance={$screenStore.activeLayout.props} />
+        {#key $screenStore.activeLayout._id}
+          <Component instance={$screenStore.activeLayout.props} />
+        {/key}
       </div>
       <NotificationDisplay />
       <!-- Key block needs to be outside the if statement or it breaks -->
