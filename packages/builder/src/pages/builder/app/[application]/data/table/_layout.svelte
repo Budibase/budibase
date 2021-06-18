44dda78ea4406c5d10b24a6e8e1f57140584f6ab
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-18 17:57:01 +0100

    fix condition in layout

--- packages/builder/src/pages/builder/app/[application]/data/table/_layout.svelte
+++ packages/builder/src/pages/builder/app/[application]/data/table/_layout.svelte
@@ -8,7 +8,8 @@
     // and this is the final url (i.e. no selectedTable)
     if (
       !$leftover &&
-      $tables.list.length > 0(!$tables.selected || !$tables.selected._id)
+      $tables.list.length > 0 && 
+      (!$tables.selected || !$tables.selected._id)
     ) {
       $goto(`./${$tables.list[0]._id}`)
     }
