9deeef2d9edd3f61266b9ad5bf565ea59aa480e0
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-21 16:25:12 +0100

    add develop tag and update docker push script

--- hosting/scripts/linux/release-to-docker-hub.sh
+++ hosting/scripts/linux/release-to-docker-hub.sh
@@ -5,12 +5,12 @@ tag=${tag:-latest}
 
 echo "Tagging images with SHA: $GITHUB_SHA and tag: $tag"
 
-docker tag app-service budibase/apps:$tag
-docker tag worker-service budibase/worker:$tag
+docker tag app-service:$tag budibase/apps:$tag
+docker tag worker-service:$tag budibase/worker:$tag
 
 # Tag with git sha
-docker tag app-service budibase/apps:$GITHUB_SHA
-docker tag worker-service budibase/worker:$GITHUB_SHA
+docker tag app-service:$tag budibase/apps:$GITHUB_SHA
+docker tag worker-service:$tag budibase/worker:$GITHUB_SHA
 
 docker push budibase/apps
 docker push budibase/worker
