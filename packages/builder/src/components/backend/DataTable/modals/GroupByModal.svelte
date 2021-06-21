5f3336531f3078b71783691a4a536be8e26891aa
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-21 13:55:34 +0100

    finally fixing cypress tests

--- packages/builder/src/components/backend/DataTable/modals/GroupByModal.svelte
+++ packages/builder/src/components/backend/DataTable/modals/GroupByModal.svelte
@@ -5,7 +5,7 @@
 
   export let view = {}
 
-  $: viewTable = $tables.list.find(({ _id }) => _id === $views.selected.tableId)
+  $: viewTable = $tables.list.find(({ _id }) => _id === $views.selected?.tableId)
   $: fields =
     viewTable &&
     Object.entries(viewTable.schema)
