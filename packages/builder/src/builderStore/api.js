dee180dc7ca785f945251e654f5343fb7ada3e39
mike12345567 <me@michaeldrury.co.uk>
2021-06-22 13:05:15 +0100

    Fixing inactivity issue in builder as well #1773.

--- packages/builder/src/builderStore/api.js
+++ packages/builder/src/builderStore/api.js
@@ -14,6 +14,8 @@ const apiCall =
     })
     if (resp.status === 403) {
       removeCookie(Cookies.Auth)
+      // reload after removing cookie, go to login
+      location.reload()
     }
     return resp
   }
