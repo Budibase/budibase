46c14fafc5da7f032a8dfcb199fff269e54c97b7
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-06-23 14:29:40 +0100

    started on opinionated relationships

--- packages/builder/src/pages/builder/app/[application]/data/datasource/[selectedDatasource]/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/data/datasource/[selectedDatasource]/index.svelte
@@ -1,16 +1,19 @@
 <script>
   import { goto, beforeUrlChange } from "@roxi/routify"
-  import { Button, Heading, Body, Divider, Layout } from "@budibase/bbui"
+  import { Button, Heading, Body, Divider, Layout, Modal } from "@budibase/bbui"
   import { datasources, integrations, queries, tables } from "stores/backend"
   import { notifications } from "@budibase/bbui"
   import IntegrationConfigForm from "components/backend/DatasourceNavigator/TableIntegrationMenu/IntegrationConfigForm.svelte"
+  import CreateEditRelationship from "./CreateEditRelationship.svelte"
   import ICONS from "components/backend/DatasourceNavigator/icons"
   import { capitalise } from "helpers"
 
   let unsaved = false
+  let relationshipModal
 
   $: datasource = $datasources.list.find(ds => ds._id === $datasources.selected)
   $: integration = datasource && $integrations[datasource.source]
+  $: plusTables = datasource?.plus ? Object.values(datasource.entities) : []
 
   async function saveDatasource() {
     try {
@@ -48,6 +51,10 @@
     unsaved = true
   }
 
+  function openRelationshipModal() {
+    relationshipModal.show()
+  }
+
   $beforeUrlChange(() => {
     if (unsaved) {
       notifications.error(
@@ -58,6 +65,10 @@
     return true
   })
 </script>
+
+<Modal bind:this={relationshipModal}>
+  <CreateEditRelationship save={saveDatasource} tables={plusTables} />
+</Modal>
 
 {#if datasource && integration}
   <section>
@@ -100,19 +111,43 @@
           having to write any queries at all.
         </Body>
         <div class="query-list">
-          {#if datasource.entities}
-            {#each Object.keys(datasource.entities) as entity}
-              <div
-                class="query-list-item"
-                on:click={() => onClickTable(datasource.entities[entity])}
-              >
-                <p class="query-name">{entity}</p>
-                <p>Primary Key: {datasource.entities[entity].primary}</p>
-                <p>→</p>
-              </div>
-            {/each}
-          {/if}
+          {#each plusTables as table}
+            <div
+              class="query-list-item"
+              on:click={() => onClickTable(table)}
+            >
+              <p class="query-name">{table.name}</p>
+              <p>Primary Key: {table.primary}</p>
+              <p>→</p>
+            </div>
+          {/each}
         </div>
+
+                <Divider />
+        <div class="query-header">
+          <Heading size="S">Relationships</Heading>
+          <Button primary on:click={openRelationshipModal}>Create Relationship</Button>
+        </div>
+          <Body>
+            Tell budibase how your tables are related to get even more smart features.
+          </Body>
+          <div class="query-list">
+              {#each plusTables as table}
+                {#each Object.keys(table) as column}
+                  {#if table[column].type === "link"}
+                    <div
+                      class="query-list-item"
+                      on:click={() => onClickTable(table[column])}
+                    >
+                      <p class="query-name">{table[column].name}</p>
+                      <p>Primary Key: {table[column].primary}</p>
+                      <p>→</p>
+                    </div>
+                  {/if}
+                {/each}
+              {/each}
+          </div>
+
       {/if}
       <Divider />
       <div class="query-header">
