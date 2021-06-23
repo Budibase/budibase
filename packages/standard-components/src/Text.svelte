d0ba754a02a5cdb92910a00860750c224cafd91a
Andrew Kingston <aptkingston@ssl-mail.com>
2021-06-23 14:21:37 +0100

    Add new typography settings, add new settings bar dropdowns, add settings bar to text components

--- packages/standard-components/src/Text.svelte
+++ packages/standard-components/src/Text.svelte
@@ -5,14 +5,37 @@
   const component = getContext("component")
 
   export let text
+  export let color
+  export let align
+  export let bold
+  export let italic
+  export let underline
+  export let size
 
   $: placeholder = $builderStore.inBuilder && !text
   $: componentText = $builderStore.inBuilder
     ? text || "Placeholder text"
     : text || ""
+
+  // Add color styles to main styles object, otherwise the styleable helper
+  // overrides the color when it's passed as inline style.
+  $: styles = {
+    ...$component.styles,
+    normal: {
+      ...$component.styles?.normal,
+      color,
+    },
+  }
 </script>
 
-<p use:styleable={$component.styles} class:placeholder>
+<p
+  use:styleable={styles}
+  class:placeholder
+  class:bold
+  class:italic
+  class:underline
+  class="align--{align || 'left'} size--{size || 'M'}"
+>
   {componentText}
 </p>
 
@@ -21,9 +44,38 @@
     display: inline-block;
     white-space: pre-wrap;
   }
-
   .placeholder {
     font-style: italic;
     color: var(--grey-6);
+  }
+  .bold {
+    font-weight: 600;
+  }
+  .italic {
+    font-style: italic;
+  }
+  .underline {
+    text-decoration: underline;
+  }
+  .size--S {
+    font-size: 14px;
+  }
+  .size--M {
+    font-size: 16px;
+  }
+  .size--L {
+    font-size: 18px;
+  }
+  .align--left {
+    text-align: left;
+  }
+  .align--center {
+    text-align: center;
+  }
+  .align--right {
+    text-align: right;
+  }
+  .align-justify {
+    text-align: justify;
   }
 </style>
