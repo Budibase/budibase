cae1a204114499c15ff2758cae561de9f6da8c78
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-07-01 21:24:23 +0100

    preventing relationship overrides

--- packages/server/src/integrations/postgres.ts
+++ packages/server/src/integrations/postgres.ts
@@ -135,7 +135,10 @@ module PostgresModule {
      * Fetches the tables from the postgres table and assigns them to the datasource.
      * @param {*} datasourceId - datasourceId to fetch
      */
-    async buildSchema(datasourceId: string) {
+    async buildSchema(
+      datasourceId: string,
+      entities: Record<string, Table>
+    ) {
       let tableKeys: { [key: string]: string[] } = {}
       try {
         const primaryKeysResponse = await this.client.query(
@@ -166,6 +169,20 @@ module PostgresModule {
             primary: tableKeys[tableName] || ["id"],
             name: tableName,
             schema: {},
+          }
+
+          // add the existing relationships from the entities if they exist, to prevent them from being overridden
+          if (entities) {
+            const existingTableSchema = entities[tableName].schema 
+            for (let key in existingTableSchema) {
+              if (existingTableSchema[key].type === "link") {
+                tables[tableName].schema[key] = existingTableSchema[key]
+              }
+            }
+            console.log({
+              existingTableSchema,
+              tables
+            })
           }
         }
 
