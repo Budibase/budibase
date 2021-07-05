c10e2da4f58e045ea2f07237598ffc02aae25f8d
mike12345567 <me@michaeldrury.co.uk>
2021-07-05 17:33:36 +0100

    Fixing issue with TS definitions.

--- packages/server/src/integrations/utils.ts
+++ packages/server/src/integrations/utils.ts
@@ -33,13 +33,13 @@ export function generateRowIdField(keyProps: any[] = []) {
 }
 
 // should always return an array
-export function breakRowIdField(_id: string) {
+export function breakRowIdField(_id: string): any[] {
   if (!_id) {
-    return null
+    return []
   }
   // have to replace on the way back as we swapped out the double quotes
   // when encoding, but JSON can't handle the single quotes
-  const decoded = decodeURIComponent(_id).replace(/'/g, '"')
+  const decoded: string = decodeURIComponent(_id).replace(/'/g, '"')
   const parsed = JSON.parse(decoded)
   return Array.isArray(parsed) ? parsed : [parsed]
 }
