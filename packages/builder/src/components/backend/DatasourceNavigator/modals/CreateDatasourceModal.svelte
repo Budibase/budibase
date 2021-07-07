b8928077e2afebd84442ac57ac9ef36087015f4c
mike12345567 <me@michaeldrury.co.uk>
2021-07-07 17:12:13 +0100

    Making it possible to create an internal table from the plus symbol.

--- packages/builder/src/components/backend/DatasourceNavigator/modals/CreateDatasourceModal.svelte
+++ packages/builder/src/components/backend/DatasourceNavigator/modals/CreateDatasourceModal.svelte
@@ -2,14 +2,20 @@
   import { goto } from "@roxi/routify"
   import { datasources } from "stores/backend"
   import { notifications } from "@budibase/bbui"
-  import { Input, Label, ModalContent } from "@budibase/bbui"
+  import { Input, Label, ModalContent, Modal, Context } from "@budibase/bbui"
   import TableIntegrationMenu from "../TableIntegrationMenu/index.svelte"
+  import CreateTableModal from "components/backend/TableNavigator/modals/CreateTableModal.svelte"
   import analytics from "analytics"
+  import { getContext } from "svelte"
 
-  let error = ""
+  const modalContext = getContext(Context.Modal)
 
+  let tableModal
   let name
+  let error = ""
   let integration
+
+  $: checkOpenModal(integration && integration.type === "BUDIBASE")
 
   function checkValid(evt) {
     const datasourceName = evt.target.value
@@ -20,6 +26,12 @@
       return
     }
     error = ""
+  }
+
+  function checkOpenModal(isInternal) {
+    if (isInternal) {
+      tableModal.show()
+    }
   }
 
   async function saveDatasource() {
@@ -40,6 +52,9 @@
   }
 </script>
 
+<Modal bind:this={tableModal} on:hide={modalContext.hide}>
+  <CreateTableModal bind:name />
+</Modal>
 <ModalContent
   title="Create Datasource"
   size="L"
