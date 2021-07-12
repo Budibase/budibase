7aca56f019b9a781d3d2ae16fb6ad7fefdd78002
mike12345567 <me@michaeldrury.co.uk>
2021-07-12 11:10:11 +0100

    Adding float type to MySQL.

--- packages/server/scripts/integrations/mysql/init.sql
+++ packages/server/scripts/integrations/mysql/init.sql
@@ -3,6 +3,7 @@ USE main;
 CREATE TABLE Persons (
     PersonID int NOT NULL AUTO_INCREMENT,
     CreatedAt datetime,
+    Age float,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
@@ -18,6 +19,6 @@ CREATE TABLE Tasks (
         FOREIGN KEY(PersonID)
 	    REFERENCES Persons(PersonID)
 );
-INSERT INTO Persons (FirstName, LastName, Address, City, CreatedAt) VALUES ('Mike', 'Hughes', '123 Fake Street', 'Belfast', '2021-01-19 03:14:07');
+INSERT INTO Persons (FirstName, LastName, Age, Address, City, CreatedAt) VALUES ('Mike', 'Hughes', 28.2, '123 Fake Street', 'Belfast', '2021-01-19 03:14:07');
 INSERT INTO Tasks (PersonID, TaskName) VALUES (1, 'assembling');
 INSERT INTO Tasks (PersonID, TaskName) VALUES (1, 'processing');
