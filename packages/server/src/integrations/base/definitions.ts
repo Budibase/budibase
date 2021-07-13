7e5c4be14f33223bd3fa2627290d13b24553c68f
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-07-13 17:11:11 +0100

    fix postgres query array value

--- packages/server/src/integrations/base/definitions.ts
+++ packages/server/src/integrations/base/definitions.ts
@@ -99,9 +99,11 @@ export interface QueryJson {
 
 export interface SqlQuery {
   sql: string
-  bindings?: {
-    [key: string]: any
-  }
+  bindings?:
+    | string[]
+    | {
+        [key: string]: any
+      }
 }
 
 export interface QueryOptions {
