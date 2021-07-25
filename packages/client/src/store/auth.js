de9f60c986e2e4ef1953a5a0b24aa32f4c8e3f2a
Andrew Kingston <aptkingston@ssl-mail.com>
2021-07-25 13:47:28 +0100

    Redirect to login screen on client logout to prevent viewing public pages if they exist

--- packages/client/src/store/auth.js
+++ packages/client/src/store/auth.js
@@ -1,19 +1,8 @@
 import * as API from "../api"
 import { writable } from "svelte/store"
-import { initialise } from "./initialise"
-import { routeStore } from "./routes"
 
 const createAuthStore = () => {
   const store = writable(null)
-
-  const goToDefaultRoute = () => {
-    // Setting the active route forces an update of the active screen ID,
-    // even if we're on the same URL
-    routeStore.actions.setActiveRoute("/")
-
-    // Navigating updates the URL to reflect this route
-    routeStore.actions.navigate("/")
-  }
 
   // Fetches the user object if someone is logged in and has reloaded the page
   const fetchUser = async () => {
@@ -22,10 +11,8 @@ const createAuthStore = () => {
   }
 
   const logOut = async () => {
-    store.set(null)
     window.document.cookie = `budibase:auth=; budibase:currentapp=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`
-    await initialise()
-    goToDefaultRoute()
+    window.location = "/builder/auth/login"
   }
 
   return {
