88c0a635a09ae7b320ea8a8a59a50f9f359990b4
Michael Drury <me@michaeldrury.co.uk>
2021-07-25 12:07:25 +0100

    Adding back logout functionality.

--- packages/client/src/store/auth.js
+++ packages/client/src/store/auth.js
@@ -1,8 +1,19 @@
 import * as API from "../api"
 import { writable } from "svelte/store"
+import { initialise } from "./initialise"
 
 const createAuthStore = () => {
   const store = writable(null)
+
+  const goToDefaultRoute = () => {
+    // Setting the active route forces an update of the active screen ID,
+    // even if we're on the same URL
+    routeStore.actions.setActiveRoute("/")
+
+    // Navigating updates the URL to reflect this route
+    routeStore.actions.navigate("/")
+  }
+
 
   // Fetches the user object if someone is logged in and has reloaded the page
   const fetchUser = async () => {
@@ -10,9 +21,16 @@ const createAuthStore = () => {
     store.set(user)
   }
 
+  const logOut = async () => {
+    store.set(null)
+    window.document.cookie = `budibase:auth=; budibase:currentapp=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+    await initialise()
+    goToDefaultRoute()
+  }
+
   return {
     subscribe: store.subscribe,
-    actions: { fetchUser },
+    actions: { fetchUser, logOut },
   }
 }
 
