a279e5e5044f0078eebcb0b92cdfaa955184b8bf
mike12345567 <me@michaeldrury.co.uk>
2021-07-26 13:40:50 +0100

    Removing un-necessary log statements.

--- packages/builder/src/pages/builder/portal/manage/email/index.svelte
+++ packages/builder/src/pages/builder/portal/manage/email/index.svelte
@@ -92,7 +92,6 @@
     }
     loading = false
     requireAuth = smtpConfig.config.auth != null
-    console.log(requireAuth)
     // always attach the auth for the forms purpose -
     // this will be removed later if required
     if (!smtpDoc.config.auth) {
