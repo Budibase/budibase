efe6fe15e250099bc81538cd3358d71e1a554bc5
Andrew Kingston <aptkingston@ssl-mail.com>
2021-08-04 14:38:20 +0100

    Fix parsing of numbers when assigning a default value to a number field

--- packages/standard-components/src/forms/NumberField.svelte
+++ packages/standard-components/src/forms/NumberField.svelte
@@ -2,10 +2,17 @@
   import StringField from "./StringField.svelte"
 
   export let defaultValue
+
+  const parseNumber = val => {
+    if (val == null) {
+      return null
+    }
+    return isNaN(val) ? null : parseFloat(val)
+  }
 </script>
 
 <StringField
   {...$$props}
   type="number"
-  defaultValue={isNaN(defaultValue) ? null : parseFloat(defaultValue)}
+  defaultValue={parseNumber(defaultValue)}
 />
