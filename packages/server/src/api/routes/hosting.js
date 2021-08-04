b35cab476a3c8522b56a769ed03d288ca9c91cea
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-08-04 20:33:39 +0100

    remove self host check from apps call

--- packages/server/src/api/routes/hosting.js
+++ packages/server/src/api/routes/hosting.js
@@ -1,7 +1,6 @@
 const Router = require("@koa/router")
 const controller = require("../controllers/hosting")
 const authorized = require("../../middleware/authorized")
-const selfhost = require("../../middleware/selfhost")
 const { BUILDER } = require("@budibase/auth/permissions")
 
 const router = Router()
@@ -9,6 +8,6 @@ const router = Router()
 router
   .get("/api/hosting/urls", authorized(BUILDER), controller.fetchUrls)
   // this isn't risky, doesn't return anything about apps other than names and URLs
-  .get("/api/hosting/apps", selfhost, controller.getDeployedApps)
+  .get("/api/hosting/apps", controller.getDeployedApps)
 
 module.exports = router
