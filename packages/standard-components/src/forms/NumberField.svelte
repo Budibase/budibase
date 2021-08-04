bbf9f425558714d9413bc0c05a36717953a9ccc1
Andrew Kingston <aptkingston@ssl-mail.com>
2021-08-04 14:33:37 +0100

    Add default value for number field

--- packages/standard-components/src/forms/NumberField.svelte
+++ packages/standard-components/src/forms/NumberField.svelte
@@ -1,5 +1,11 @@
 <script>
   import StringField from "./StringField.svelte"
+
+  export let defaultValue
 </script>
 
-<StringField {...$$props} type="number" />
+<StringField
+  {...$$props}
+  type="number"
+  defaultValue={isNaN(defaultValue) ? null : parseFloat(defaultValue)}
+/>
