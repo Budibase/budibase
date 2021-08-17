8e1e016c66fe9192dbf5416260b5013dfd46a003
Martin McKeaveney <martinmckeaveney@gmail.com>
2021-08-17 07:16:43 +0100

    Revert "Feature/couchdb auth"

--- packages/auth/src/environment.js
+++ packages/auth/src/environment.js
@@ -9,8 +9,6 @@ function isTest() {
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   COUCH_DB_URL: process.env.COUCH_DB_URL,
-  COUCH_DB_USERNAME: process.env.COUCH_DB_USER,
-  COUCH_DB_PASSWORD: process.env.COUCH_DB_PASSWORD,
   SALT_ROUNDS: process.env.SALT_ROUNDS,
   REDIS_URL: process.env.REDIS_URL,
   REDIS_PASSWORD: process.env.REDIS_PASSWORD,
