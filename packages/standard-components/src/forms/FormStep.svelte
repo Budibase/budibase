c44d0a684be1050ff2d2f0acbf80b75c8359ef9e
Andrew Kingston <aptkingston@ssl-mail.com>
2021-08-18 14:58:58 +0100

    Support multiple component context exports and export additional context from forms

---
+++ packages/standard-components/src/forms/FormStep.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { getContext } from "svelte"
+  import Placeholder from "../Placeholder.svelte"
+
+  export let step
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+  const formContext = getContext("form")
+</script>
+
+{#if !formContext}
+  <Placeholder text="Form steps need to be wrapped in a form" />
+{:else}
+  <div use:styleable={$component.styles}>
+    <slot />
+  </div>
+{/if}
