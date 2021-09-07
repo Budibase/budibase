3d68b396476af016b4b59441f13748e73dfb064f
Andrew Kingston <aptkingston@ssl-mail.com>
2021-09-07 10:05:24 +0100

    Fix test for loading component library

--- packages/server/src/utilities/fileSystem/index.js
+++ packages/server/src/utilities/fileSystem/index.js
@@ -178,6 +178,7 @@ exports.getComponentLibraryManifest = async (appId, library) => {
   // when testing in cypress and so on we need to get the package
   // as the environment may not be fully fleshed out for dev or prod
   if (env.isTest()) {
+    library = library.replace("standard-components", "client")
     const lib = library.split("/")[1]
     const path = require.resolve(library).split(lib)[0]
     return require(join(path, lib, filename))
