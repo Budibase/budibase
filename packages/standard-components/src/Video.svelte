0a78b5ffeec73877f92a6dcfe6c1e26051ae935b
蠢丶 <zzchun12826@gmail.com>
2021-09-08 18:31:25 +0800

    add video component

---
+++ packages/standard-components/src/Video.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { getContext } from "svelte"
+  import Placeholder from "./Placeholder.svelte"
+
+  const { styleable, builderStore } = getContext("sdk")
+  const component = getContext("component")
+
+  export let url
+  export let autoplay
+</script>
+
+{#if url}
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video
+    src={url}
+    autoplay={$autoplay}
+    alt={$component.name}
+    use:styleable={$component.styles}
+    controls
+  />
+{:else if $builderStore.inBuilder}
+  <div
+    class="placeholder"
+    use:styleable={{ ...$component.styles, empty: true }}
+  >
+    <Placeholder />
+  </div>
+{/if}
+
+<style>
+  .placeholder {
+    display: grid;
+    place-items: center;
+  }
+</style>
