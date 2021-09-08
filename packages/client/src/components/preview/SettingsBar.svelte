802cdc48c86ea38103289a69886d659ab8502fca
Andrew Kingston <aptkingston@ssl-mail.com>
2021-09-08 09:40:02 +0100

    Update position of indicators in preview to clip better

--- packages/client/src/components/preview/SettingsBar.svelte
+++ packages/client/src/components/preview/SettingsBar.svelte
@@ -26,8 +26,10 @@
     const id = $builderStore.selectedComponentId
     const parent = document.getElementsByClassName(id)?.[0]
     const element = parent?.childNodes?.[0]
+    const device = document.getElementById("device-root")
     if (element && self) {
       // Batch reads to minimize reflow
+      const deviceBounds = device.getBoundingClientRect()
       const elBounds = element.getBoundingClientRect()
       const width = self.offsetWidth
       const height = self.offsetHeight
@@ -35,8 +37,15 @@
 
       // Vertically, always render above unless no room, then render inside
       let newTop = elBounds.top + scrollY - verticalOffset - height
+      if (newTop < deviceBounds.top - 50) {
+        newTop = deviceBounds.top - 50
+      }
       if (newTop < 0) {
         newTop = 0
+      }
+      const deviceBottom = deviceBounds.top + deviceBounds.height
+      if (newTop > deviceBottom - 44) {
+        newTop = deviceBottom - 44
       }
 
       // Horizontally, try to center first.
