689065d2b87e43563c31fe7f5cd4811c3ff0e29e
Rory Powell <rory.codes@gmail.com>
2021-09-14 14:18:13 +0100

    Redirect to account portal login page in the cloud

--- packages/worker/src/api/controllers/system/environment.js
+++ packages/worker/src/api/controllers/system/environment.js
@@ -3,6 +3,7 @@ const env = require("../../../environment")
 exports.fetch = async ctx => {
   ctx.body = {
     multiTenancy: !!env.MULTI_TENANCY,
-    cloud: !!env.SELF_HOSTED,
+    cloud: !(env.SELF_HOSTED === "1"),
+    accountPortalUrl: env.ACCOUNT_PORTAL_URL,
   }
 }
