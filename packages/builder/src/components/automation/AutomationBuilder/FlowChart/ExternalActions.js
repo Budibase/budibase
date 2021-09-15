f141647e72369e84acc418ad932463f8e94bf69e
Peter Clement <peter@budibase.com>
2021-09-15 09:57:06 +0100

    incorrect file name

--- packages/builder/src/components/automation/AutomationBuilder/FlowChart/ExternalActions.js
+++ packages/builder/src/components/automation/AutomationBuilder/FlowChart/ExternalActions.js
@@ -2,7 +2,7 @@ import DiscordLogo from "assets/discord.svg"
 import ZapierLogo from "assets/zapier.png"
 import IntegromatLogo from "assets/integromat.png"
 import SlackLogo from "assets/slack.svg"
-import n8nlogo from "assets/n8nlogo.png"
+import n8nlogo from "assets/n8n.png"
 
 export const externalActions = {
   zapier: { name: "zapier", icon: ZapierLogo },
