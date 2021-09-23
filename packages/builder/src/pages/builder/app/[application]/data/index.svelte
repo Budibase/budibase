468a7caa07386f2aef45f22b0d124eea8e416cd0
Peter Clement <peter@budibase.com>
2021-09-23 22:02:35 +0100

    fix flow of wizard

--- packages/builder/src/pages/builder/app/[application]/data/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/data/index.svelte
@@ -1,13 +1,25 @@
 <script>
+  import { goto } from "@roxi/routify"
+  import { onMount } from "svelte"
   import CreateDatasourceModal from "components/backend/DatasourceNavigator/modals/CreateDatasourceModal.svelte"
   import { datasources } from "stores/backend"
 
+  let modal
   $: setupComplete =
     $datasources.list.find(x => (x._id = "bb_internal")).entities.length > 1 ||
-    $datasources.list > 1
+    $datasources.list.length >= 1
+  $: console.log($datasources.list.length >= 1)
+  $: console.log(
+    $datasources.list.find(x => (x._id = "bb_internal")).entities.length > 1
+  )
+  onMount(() => {
+    if (!setupComplete) {
+      modal.show()
+    } else {
+      $goto("./table")
+    }
+  })
 </script>
 
-{#if !setupComplete}
-  <CreateDatasourceModal />
-{/if}
+<CreateDatasourceModal bind:modal />
 <!-- routify:options index=false -->
