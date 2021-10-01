8decbc20d5034b4d6604525e8a0d93efaa4fff98
mike12345567 <me@michaeldrury.co.uk>
2021-10-01 15:00:11 +0100

    Fixing issue with created by/updated by on public forms, also fixing a small issue with analytics ping generating a 404.

--- packages/server/src/api/routes/analytics.js
+++ packages/server/src/api/routes/analytics.js
@@ -3,7 +3,8 @@ const controller = require("../controllers/analytics")
 
 const router = Router()
 
-router.get("/api/analytics", controller.isEnabled)
-router.post("/api/analytics/ping", controller.endUserPing)
+router
+  .get("/api/analytics", controller.isEnabled)
+  .post("/api/analytics/ping", controller.endUserPing)
 
 module.exports = router
