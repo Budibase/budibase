2f669e7b26ba350af6c0c14bff7eb96afaa3b680
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-10-06 14:33:21 +0100

    Redesign UX, get rid of welcome screen

--- packages/builder/src/components/start/TemplateList.svelte
+++ packages/builder/src/components/start/TemplateList.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Heading, Body, Layout } from "@budibase/bbui"
+  import { Heading, Layout, Icon } from "@budibase/bbui"
   import Spinner from "components/common/Spinner.svelte"
   import api from "builderStore/api"
 
@@ -21,17 +21,24 @@
   {:then templates}
     <div class="templates">
       {#each templates as template}
-        <Layout gap="XS" noPadding>
-          <img
-            alt="template"
-            on:click={() => onSelect(template)}
-            src={template.image}
-            width="100%"
-          />
+        <div class="template" on:click={() => onSelect(template)}>
+          <div
+            class="background-icon"
+            style={`background: ${template.background};`}
+          >
+            <Icon name={template.icon} />
+          </div>
           <Heading size="XS">{template.name}</Heading>
-          <Body size="S" black>{template.description}</Body>
-        </Layout>
+          <p class="detail">{template?.category?.toUpperCase()}</p>
+        </div>
       {/each}
+      <div class="template start-from-scratch" on:click={() => onSelect(null)}>
+        <div class="background-icon" style={`background: var(--background);`}>
+          <Icon name="Add" />
+        </div>
+        <Heading size="XS">Start from scratch</Heading>
+        <p class="detail">BLANK</p>
+      </div>
     </div>
   {:catch err}
     <h1 style="color:red">{err}</h1>
@@ -42,14 +49,41 @@
   .templates {
     display: grid;
     width: 100%;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: var(--layout-m);
+    grid-gap: var(--spacing-m);
+    grid-template-columns: 1fr;
     justify-content: start;
-    margin-top: var(--layout-s);
+    margin-top: 15px;
   }
 
-  img {
-    margin-bottom: var(--layout-s);
+  .background-icon {
+    padding: 10px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 18px;
+    color: white;
+  }
+
+  .template {
+    height: 60px;
+    display: grid;
+    grid-gap: var(--layout-m);
+    grid-template-columns: 5% 1fr 15%;
+    border: 1px solid #494949;
+    align-items: center;
     cursor: pointer;
+    border-radius: 4px;
+    background: #1a1a1a;
+    padding: 8px 16px;
+  }
+
+  .detail {
+    text-align: right;
+  }
+
+  .start-from-scratch {
+    background: var(--spectrum-global-color-gray-50);
+    margin-top: 20px;
   }
 </style>
