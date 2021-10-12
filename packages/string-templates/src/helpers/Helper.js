d6a1e3d248e474658512e2dbb139cd545e1935d6
Andrew Kingston <aptkingston@ssl-mail.com>
2021-10-12 15:40:01 +0100

    Correctly handle JS nullish values by removing forced fallback value of HBS helper

--- packages/string-templates/src/helpers/Helper.js
+++ packages/string-templates/src/helpers/Helper.js
@@ -1,14 +1,20 @@
 class Helper {
-  constructor(name, fn) {
+  constructor(name, fn, useValueFallback = true) {
     this.name = name
     this.fn = fn
+    this.useValueFallback = useValueFallback
   }
 
   register(handlebars) {
     // wrap the function so that no helper can cause handlebars to break
     handlebars.registerHelper(this.name, (value, info) => {
-      const context = info?.data?.root
-      return this.fn(value, context || {}) || value
+      const context = info?.data?.root || {}
+      const result = this.fn(value, context)
+      if (result == null) {
+        return this.useValueFallback ? value : null
+      } else {
+        return result
+      }
     })
   }
 
