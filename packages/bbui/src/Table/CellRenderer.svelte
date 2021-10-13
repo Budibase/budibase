4c0e2d3d07b8e41b5a5d9f25e40572ffbb9f13d2
Peter Clement <peter@budibase.com>
2021-10-13 14:14:08 +0100

    remove uneeded log

--- packages/bbui/src/Table/CellRenderer.svelte
+++ packages/bbui/src/Table/CellRenderer.svelte
@@ -25,7 +25,6 @@
     array: ArrayRenderer,
     internal: InternalRenderer,
   }
-  console.log(value)
 
   $: type = schema?.type ?? "string"
   $: customRenderer = customRenderers?.find(x => x.column === schema?.name)
