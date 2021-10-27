8b8c923541b48157f7f65ff2d7a14fdc6f9e5051
Rory Powell <rory.codes@gmail.com>
2021-10-27 17:21:29 +0100

    Update tests

--- packages/server/__mocks__/pg.ts
+++ packages/server/__mocks__/pg.ts
@@ -21,15 +21,18 @@ module PgMock {
   function Pool() {
   }
 
+  const on = jest.fn()
   Pool.prototype.query = query
   Pool.prototype.connect = jest.fn(() => {
     // @ts-ignore
     return new Client()
   })
+  Pool.prototype.on = on
 
   pg.Client = Client
   pg.Pool = Pool
   pg.queryMock = query
+  pg.on = on
 
   module.exports = pg
 }
