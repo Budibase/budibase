35f0384b171db1ebdf1c7277a33062b6fc642062
Peter Clement <peter@budibase.com>
2021-11-11 12:55:04 +0000

    removing log

--- packages/builder/src/components/design/NavigationPanel/ScreenNameModal.svelte
+++ packages/builder/src/components/design/NavigationPanel/ScreenNameModal.svelte
@@ -4,12 +4,11 @@
   export let modal
   export let navigationSelectionModal
   export let screenName
-  $: console.log(name)
 </script>
 
 <ModalContent
   size="M"
-  title={"hello"}
+  title={"Enter name"}
   confirmText={"Continue"}
   onCancel={() => modal.show()}
   onConfirm={() => navigationSelectionModal.show()}
