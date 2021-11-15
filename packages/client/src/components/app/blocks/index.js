7a1b22ef5f3485e9a4b33a33c7af165d60e00c1a
Andrew Kingston <aptkingston@ssl-mail.com>
2021-11-15 18:07:51 +0000

    Rename data block to repeater block

--- packages/client/src/components/app/blocks/index.js
+++ packages/client/src/components/app/blocks/index.js
@@ -1,3 +1,3 @@
 export { default as tableblock } from "./TableBlock.svelte"
 export { default as cardsblock } from "./CardsBlock.svelte"
-export { default as datablock } from "./DataBlock.svelte"
+export { default as repeaterblock } from "./RepeaterBlock.svelte"
