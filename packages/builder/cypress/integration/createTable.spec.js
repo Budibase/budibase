87800109262b9d89f540982329ab6d8109d072b3
Mitch-Budibase <mitch@budibase.com>
2021-11-15 15:25:58 +0000

    New Tests (User Roles, Query Level Transformers, Data Sources etc.)
    New tests:
    -Query Level Transformers
    -Table pagination
    -User Roles
    -Data Sources (correct config, incorrect config, Wizard)
    
    Also:
    -New Commands to support
    Testing
    -Cypress Updated

--- packages/builder/cypress/integration/createTable.spec.js
+++ packages/builder/cypress/integration/createTable.spec.js
@@ -30,7 +30,6 @@ context("Create a Table", () => {
     cy.contains("Save Column").click()
     cy.contains("nameupdated ").should("contain", "nameupdated")
   })
-
   
   it("edits a row", () => {
     cy.contains("button", "Edit").click({ force: true })
@@ -46,6 +45,36 @@ context("Create a Table", () => {
     cy.contains("Delete 1 row(s)").click()
     cy.get(".spectrum-Modal").contains("Delete").click()
     cy.contains("RoverUpdated").should("not.exist")
+  })
+  
+  it("Adds 15 rows and checks pagination", () => {
+    // 10 rows per page, 15 rows should create 2 pages within table
+    const totalRows = 16
+    for (let i = 1; i < totalRows; i++){
+      cy.addRow([i])
+    }
+    cy.wait(1000)
+    cy.get(".spectrum-Pagination").within(() => {
+      cy.get(".spectrum-ActionButton").eq(1).click()
+    })
+    cy.get(".spectrum-Pagination").within(() => {
+      cy.get(".spectrum-Body--secondary").contains("Page 2")
+    })
+  })
+  
+  it("Deletes rows and checks pagination", () => {
+    // Delete rows, removing second page of rows from table
+    const deleteRows = 5
+    cy.get(".spectrum-Checkbox-input").check({ force: true })
+    cy.get(".spectrum-Table-body")
+    cy.contains("Delete 5 row(s)").click()
+    cy.get(".spectrum-Modal").contains("Delete").click()
+    cy.wait(1000)
+    
+    // Confirm table only has one page
+    cy.get(".spectrum-Pagination").within(() => {
+      cy.get(".spectrum-ActionButton").eq(1).should('not.be.enabled')
+    })
   })
 
   it("deletes a column", () => {
