c7c4bd607f170ef0e6b98d657c4647ac0f38a111
Mitch-Budibase <mitch@budibase.com>
2021-11-18 13:47:41 +0000

    Env variables & data files
    Changes as per PR
    -Created files for transformer queries - tests updated accordingly
    -Updates for Env variables and usage
    -Changes surrounding App Rename testing

--- packages/builder/cypress/integration/createApp.spec.js
+++ packages/builder/cypress/integration/createApp.spec.js
@@ -2,7 +2,7 @@ context("Create an Application", () => {
   it("should create a new application", () => {
     cy.login()
     cy.createTestApp()
-    cy.visit(`localhost:${Cypress.env("PORT")}/builder`)
+    cy.visit(`${Cypress.config().baseUrl}/builder`)
     cy.contains("Cypress Tests").should("exist")
   })
 })
---
+++ packages/builder/cypress/support/queryLevelTransformerFunction.js
@@ -0,0 +1,11 @@
+const breweries = data
+const totals = {}
+
+for (let brewery of breweries)
+    {const state = brewery.state
+    if (totals[state] == null)
+        {totals[state] = 1}
+         else 
+         {totals[state]++}}
+const entries = Object.entries(totals)
+return entries.map(([state, count]) => ({state, count}))
