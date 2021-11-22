e91a8a175e0e49d3951055673f8c38e9452631ef
Peter Clement <peter@budibase.com>
2021-11-22 09:43:57 +0000

    fix broken automation test

--- packages/builder/cypress/integration/createAutomation.spec.js
+++ packages/builder/cypress/integration/createAutomation.spec.js
@@ -22,9 +22,9 @@ context("Create a automation", () => {
     cy.get(".spectrum-Picker-label").click()
     cy.wait(500)
     cy.contains("dog").click()
-
+    cy.wait(2000)
     // Create action
-    cy.contains("Add Action").click()
+    cy.get(".block > .spectrum-Icon").click()
     cy.get(".modal-inner-wrapper").within(() => {
       cy.wait(1000)
       cy.contains("Create Row").trigger('mouseover').click().click()
