d5a5f4d22746f361cc735f38561a7dccfbb4f52e
Martin McKeaveney <martin@shogunsystems.co.uk>
2021-11-22 18:25:57 +0100

    adding version metadata to docker build

--- hosting/scripts/linux/release-to-docker-hub.sh
+++ hosting/scripts/linux/release-to-docker-hub.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 tag=$1
-production=$2
 
 if [[ ! "$tag" ]]; then
 	echo "No tag present. You must pass a tag to this script"
@@ -12,12 +11,6 @@ echo "Tagging images with tag: $tag"
 
 docker tag app-service budibase/apps:$tag
 docker tag worker-service budibase/worker:$tag
-
-if [[ "$production" ]]; then
-	echo "Production Deployment. Tagging latest.."
-	docker tag app-service budibase/apps:latest
-	docker tag worker-service budibase/worker:latest
-fi
 
 docker push --all-tags budibase/apps 
 docker push --all-tags budibase/worker
