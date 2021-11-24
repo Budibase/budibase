7e65edfc5a9c0ed7f860c0a92f4dee89053598b9
Peter Clement <peter@budibase.com>
2021-11-24 19:10:10 +0000

    refactor screen templates to include table name

--- packages/builder/src/builderStore/store/screenTemplates/rowListScreen.js
+++ packages/builder/src/builderStore/store/screenTemplates/rowListScreen.js
@@ -10,6 +10,7 @@ export default function (tables) {
       name: `${table.name} - List`,
       create: () => createScreen(table),
       id: ROW_LIST_TEMPLATE,
+      table: table.name
     }
   })
 }
