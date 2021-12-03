5d1efb3024ec1e2c74f3d8201a42069fee75f5b1
Rory Powell <rory.codes@gmail.com>
2021-12-03 11:14:22 +0000

    Add import to existing DS and add cookie support to curl import

--- packages/server/src/api/controllers/query/import/sources/curl.ts
+++ packages/server/src/api/controllers/query/import/sources/curl.ts
@@ -27,6 +27,17 @@ const parseBody = (curl: any) => {
   return undefined
 }
 
+const parseCookie = (curl: any) => {
+  if (curl.cookies){
+    return Object.entries(curl.cookies).reduce((acc, entry) => {
+      const [key, value] = entry
+      return acc + `${key}=${value}; `
+    }, "")
+  }
+
+  return null
+}
+
 /**
  * Curl
  * https://curl.se/docs/manpage.html
@@ -60,6 +71,11 @@ export class Curl extends ImportSource {
     const queryString = url.search
     const headers = this.curl.headers
     const requestBody = parseBody(this.curl)
+
+    const cookieHeader = parseCookie(this.curl)
+    if (cookieHeader) {
+      headers["Cookie"] = cookieHeader
+    }
 
     const query = this.constructQuery(
       datasourceId,
