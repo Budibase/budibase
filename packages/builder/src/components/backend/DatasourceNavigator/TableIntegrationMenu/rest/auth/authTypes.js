01ab892a00db82d2ceb1bfe9470b22d13df67e88
Rory Powell <rory.codes@gmail.com>
2021-12-09 11:39:44 +0000

    Modal integration

---
+++ packages/builder/src/components/backend/DatasourceNavigator/TableIntegrationMenu/rest/auth/authTypes.js
@@ -0,0 +1,15 @@
+export const AUTH_TYPES = {
+  BEARER: "bearer",
+  BASIC: "basic",
+}
+
+export const AUTH_TYPE_LABELS = [
+  {
+    label: "Bearer Token",
+    value: AUTH_TYPES.BEARER,
+  },
+  {
+    label: "Basic Auth",
+    value: AUTH_TYPES.BASIC,
+  },
+]
