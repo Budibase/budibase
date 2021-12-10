dc7d22e5cb0ad5024640b510e64b52415345b7b3
Andrew Kingston <aptkingston@ssl-mail.com>
2021-12-10 14:18:01 +0000

    Add support for binding forms to deep json fields

--- packages/builder/src/builderStore/dataBinding.js
+++ packages/builder/src/builderStore/dataBinding.js
@@ -220,23 +220,6 @@ const getProviderContextBindings = (asset, dataProviders) => {
         schema = info.schema
         table = info.table
 
-        // Check for any JSON fields so we can add any top level properties
-        let jsonAdditions = {}
-        Object.keys(schema).forEach(fieldKey => {
-          const fieldSchema = schema[fieldKey]
-          if (fieldSchema.type === "json") {
-            const jsonSchema = convertJSONSchemaToTableSchema(fieldSchema, {
-              squashObjects: true,
-            })
-            Object.keys(jsonSchema).forEach(jsonKey => {
-              jsonAdditions[`${fieldKey}.${jsonKey}`] = {
-                type: jsonSchema[jsonKey].type,
-              }
-            })
-          }
-        })
-        schema = { ...schema, ...jsonAdditions }
-
         // For JSON arrays, use the array name as the readable prefix.
         // Otherwise use the table name
         if (datasource.type === "jsonarray") {
@@ -483,6 +466,26 @@ export const getSchemaForDatasource = (asset, datasource, isForm = false) => {
       } else {
         schema = cloneDeep(table.schema)
       }
+    }
+
+    // Check for any JSON fields so we can add any top level properties
+    if (schema) {
+      let jsonAdditions = {}
+      Object.keys(schema).forEach(fieldKey => {
+        const fieldSchema = schema[fieldKey]
+        if (fieldSchema?.type === "json") {
+          const jsonSchema = convertJSONSchemaToTableSchema(fieldSchema, {
+            squashObjects: true,
+          })
+          Object.keys(jsonSchema).forEach(jsonKey => {
+            jsonAdditions[`${fieldKey}.${jsonKey}`] = {
+              type: jsonSchema[jsonKey].type,
+              nestedJSON: true,
+            }
+          })
+        }
+      })
+      schema = { ...schema, ...jsonAdditions }
     }
 
     // Add _id and _rev fields for certain types
