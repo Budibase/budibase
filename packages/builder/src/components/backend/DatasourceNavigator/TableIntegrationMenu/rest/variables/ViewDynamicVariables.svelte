f499320b477e0ad6daf4db0bcfd6f1ad0a846a23
Rory Powell <rory.codes@gmail.com>
2021-12-14 17:05:17 +0000

    Link datasource dynamic variables table to query

--- packages/builder/src/components/backend/DatasourceNavigator/TableIntegrationMenu/rest/variables/ViewDynamicVariables.svelte
+++ packages/builder/src/components/backend/DatasourceNavigator/TableIntegrationMenu/rest/variables/ViewDynamicVariables.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Body, Table } from "@budibase/bbui"
   import { onMount } from "svelte"
+  import { queries as queriesStore } from "stores/backend"
+  import { goto } from "@roxi/routify"
 
   export let datasource
   export let queries
@@ -11,6 +13,12 @@
     name: "",
     value: "",
     query: "",
+  }
+
+  const onClick = dynamicVariable => {
+    const queryId = dynamicVariable.queryId
+    queriesStore.select({ _id: queryId })
+    $goto(`./${queryId}`)
   }
 
   /**
@@ -31,6 +39,7 @@
 </script>
 
 <Table
+  on:click={({ detail }) => onClick(detail)}
   schema={dynamicVariableSchema}
   data={dynamicVariables}
   allowEditColumns={false}
