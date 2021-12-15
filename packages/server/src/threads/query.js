d1127f32202441b6f2245cadc1950506cdebe58f
mike12345567 <me@michaeldrury.co.uk>
2021-12-15 11:25:52 +0000

    Fixing an issue with automations throwing a Pouch error due to the auth library database not being setup.

--- packages/server/src/threads/query.js
+++ packages/server/src/threads/query.js
@@ -1,6 +1,4 @@
-// when thread starts, make sure it is recorded
-const env = require("../environment")
-env.setInThread()
+require("./utils").threadSetup()
 const ScriptRunner = require("../utilities/scriptRunner")
 const { integrations } = require("../integrations")
 
