01b72b6b71fbce85fd79f2079f9d9cfd530a2b9e
mike12345567 <me@michaeldrury.co.uk>
2021-12-15 12:26:29 +0000

    Don't run change if not threading.

--- packages/server/src/threads/utils.js
+++ packages/server/src/threads/utils.js
@@ -3,6 +3,10 @@ const CouchDB = require("../db")
 const { init } = require("@budibase/auth")
 
 exports.threadSetup = () => {
+  // don't run this if not threading
+  if (env.isTest() || env.DISABLE_THREADING) {
+    return
+  }
   // when thread starts, make sure it is recorded
   env.setInThread()
   init(CouchDB)
