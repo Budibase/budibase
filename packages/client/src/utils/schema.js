6e3f217fbbaef0db6501229e968ac0d3912696e3
Andrew Kingston <aptkingston@ssl-mail.com>
2022-01-04 15:02:43 +0000

    Add support for nested provider, field and jsonarray datasource types in new data fetch model

--- packages/client/src/utils/schema.js
+++ packages/client/src/utils/schema.js
@@ -1,83 +1,53 @@
-import {
-  convertJSONSchemaToTableSchema,
-  getJSONArrayDatasourceSchema,
-} from "builder/src/builderStore/jsonUtils"
-import { fetchTableDefinition } from "api"
+import { convertJSONSchemaToTableSchema } from "builder/src/builderStore/jsonUtils"
 import TableFetch from "./fetch/TableFetch.js"
 import ViewFetch from "./fetch/ViewFetch.js"
 import QueryFetch from "./fetch/QueryFetch.js"
+import RelationshipFetch from "./fetch/RelationshipFetch.js"
+import NestedProviderFetch from "./fetch/NestedProviderFetch.js"
+import FieldFetch from "./fetch/FieldFetch.js"
+import JSONArrayFetch from "./fetch/JSONArrayFetch.js"
 
 /**
  * Fetches the schema of any kind of datasource.
+ * All datasource fetch classes implement their own functionality to get the
+ * schema of a datasource of their respective types.
  */
 export const fetchDatasourceSchema = async datasource => {
-  const type = datasource?.type
-  let schema
-
-  // Nested providers should already have exposed their own schema
-  if (type === "provider") {
-    schema = datasource.value?.schema
-  }
-
-  // Field sources have their schema statically defined
-  if (type === "field") {
-    if (datasource.fieldType === "attachment") {
-      schema = {
-        url: {
-          type: "string",
-        },
-        name: {
-          type: "string",
-        },
-      }
-    } else if (datasource.fieldType === "array") {
-      schema = {
-        value: {
-          type: "string",
-        },
-      }
-    }
-  }
-
-  // JSON arrays need their table definitions fetched.
-  // We can then extract their schema as a subset of the table schema.
-  if (type === "jsonarray") {
-    const table = await fetchTableDefinition(datasource.tableId)
-    schema = getJSONArrayDatasourceSchema(table?.schema, datasource)
-  }
-
-  // All normal datasource schema can use their corresponding implementations
-  // in the data fetch classes
   const handler = {
     table: TableFetch,
-    link: TableFetch,
     view: ViewFetch,
     query: QueryFetch,
-  }[type]
-  if (handler) {
-    const definition = await handler.getDefinition(datasource)
-    schema = handler.getSchema(datasource, definition)
+    link: RelationshipFetch,
+    provider: NestedProviderFetch,
+    field: FieldFetch,
+    jsonarray: JSONArrayFetch,
+  }[datasource?.type]
+  if (!handler) {
+    return null
+  }
+
+  // Get the datasource definition and then schema
+  const definition = await handler.getDefinition(datasource)
+  const schema = handler.getSchema(datasource, definition)
+  if (!schema) {
+    return null
   }
 
   // Check for any JSON fields so we can add any top level properties
-  if (schema) {
-    let jsonAdditions = {}
-    Object.keys(schema).forEach(fieldKey => {
-      const fieldSchema = schema[fieldKey]
-      if (fieldSchema?.type === "json") {
-        const jsonSchema = convertJSONSchemaToTableSchema(fieldSchema, {
-          squashObjects: true,
-        })
-        Object.keys(jsonSchema).forEach(jsonKey => {
-          jsonAdditions[`${fieldKey}.${jsonKey}`] = {
-            type: jsonSchema[jsonKey].type,
-            nestedJSON: true,
-          }
-        })
-      }
-    })
-    return { ...schema, ...jsonAdditions }
-  }
-
-  return null
+  let jsonAdditions = {}
+  Object.keys(schema).forEach(fieldKey => {
+    const fieldSchema = schema[fieldKey]
+    if (fieldSchema?.type === "json") {
+      const jsonSchema = convertJSONSchemaToTableSchema(fieldSchema, {
+        squashObjects: true,
+      })
+      Object.keys(jsonSchema).forEach(jsonKey => {
+        jsonAdditions[`${fieldKey}.${jsonKey}`] = {
+          type: jsonSchema[jsonKey].type,
+          nestedJSON: true,
+        }
+      })
+    }
+  })
+  return { ...schema, ...jsonAdditions }
 }
