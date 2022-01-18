56ef16665eb85ce9079d6810d5b5b0fd820e586b
Andrew Kingston <andrew@kingston.dev>
2022-01-18 13:20:06 +0000

    Transform the output of JS expressions to be actual types rather than strings

--- packages/string-templates/src/processors/postprocessor.js
+++ packages/string-templates/src/processors/postprocessor.js
@@ -36,6 +36,11 @@ module.exports.processors = [
         return value === "true"
       case "object":
         return JSON.parse(value)
+      case "js_result":
+        // We use the literal helper to process the result of JS expressions
+        // as we want to be able to return any types.
+        // We wrap the value in an abject to be able to use undefined properly.
+        return JSON.parse(value).data
     }
     return value
   }),
