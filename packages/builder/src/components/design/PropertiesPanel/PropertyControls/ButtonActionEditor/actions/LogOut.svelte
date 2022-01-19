8be175843a0ba7e2b5ac6c2714ffdeefb3312450
Andrew Kingston <andrew@kingston.dev>
2022-01-19 11:22:04 +0000

    Add return URL setting to log out button action

--- packages/builder/src/components/design/PropertiesPanel/PropertyControls/ButtonActionEditor/actions/LogOut.svelte
+++ packages/builder/src/components/design/PropertiesPanel/PropertyControls/ButtonActionEditor/actions/LogOut.svelte
@@ -1,13 +1,38 @@
 <script>
-  import { Body } from "@budibase/bbui"
+  import { Label, Body, Layout } from "@budibase/bbui"
+  import DrawerBindableInput from "components/common/bindings/DrawerBindableInput.svelte"
+
+  export let parameters
+  export let bindings = []
 </script>
 
 <div class="root">
-  <Body size="S">This action doesn't require any additional settings.</Body>
+  <Layout noPadding gap="M">
+    <Body size="S">
+      Please enter what URL you would like to be redirected to after logging
+      out. If you dont' enter a value, you'll be redirected to the login screen.
+    </Body>
+    <div class="content">
+      <Label small>Return URL</Label>
+      <DrawerBindableInput
+        title="Return URL"
+        value={parameters.returnUrl}
+        on:change={value => (parameters.returnUrl = value.detail)}
+        {bindings}
+      />
+    </div>
+  </Layout>
 </div>
 
 <style>
   .root {
+    max-width: 400px;
     margin: 0 auto;
+  }
+  .content {
+    display: grid;
+    align-items: center;
+    gap: var(--spacing-m);
+    grid-template-columns: auto 1fr;
   }
 </style>
