5c6a9cf654726af81c8a9443cdbdba0b2a96b0ff
Andrew Kingston <andrew@kingston.dev>
2022-01-24 15:19:21 +0000

    Filter out an S3 datasources which contain custom endpoints when selecting the datsource to use with the S3 upload component

--- packages/builder/src/components/design/PropertiesPanel/PropertyControls/S3DataSourceSelect.svelte
+++ packages/builder/src/components/design/PropertiesPanel/PropertyControls/S3DataSourceSelect.svelte
@@ -5,7 +5,7 @@
   export let value = null
 
   $: dataSources = $datasources.list
-    .filter(ds => ds.source === "S3")
+    .filter(ds => ds.source === "S3" && !ds.config?.endpoint)
     .map(ds => ({
       label: ds.name,
       value: ds._id,
