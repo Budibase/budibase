5a88c89baf329d9bd614e6d50fff57a6b5869174
Peter Clement <peter@budibase.com>
2022-02-11 15:37:43 +0000

    changing bindings key

--- packages/client/src/utils/enrichDataBinding.js
+++ packages/client/src/utils/enrichDataBinding.js
@@ -24,5 +24,8 @@ export const enrichDataBinding = async (input, context) => {
  * Props are deeply cloned so that no mutation is done to the source object.
  */
 export const enrichDataBindings = (props, context) => {
+  console.log(props)
+  console.log(context)
+
   return processObjectSync(cloneDeep(props), context)
 }
