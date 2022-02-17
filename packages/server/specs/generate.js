23bd6d39e2a5da230ea5243542c26d3268a2c05c
mike12345567 <me@michaeldrury.co.uk>
2022-02-17 19:55:37 +0000

    Adding a structure for breaking up examples and parameters.

--- packages/server/specs/generate.js
+++ packages/server/specs/generate.js
@@ -1,6 +1,8 @@
 const swaggerJsdoc = require("swagger-jsdoc")
 const { join } = require("path")
 const { writeFileSync } = require("fs")
+const { row, table } = require("./examples")
+const { tableId } = require("./parameters")
 
 const VARIABLES = {}
 
@@ -23,45 +25,12 @@ const options = {
       },
     ],
     components: {
+      parameters: {
+        tableId: tableId,
+      },
       examples: {
-        row: {
-          value: {
-            _id: "ro_ta_5b1649e42a5b41dea4ef7742a36a7a70_e6dc7e38cf1343b2b56760265201cda4",
-            type: "row",
-            tableId: "ta_5b1649e42a5b41dea4ef7742a36a7a70",
-            name: "Mike",
-            age: 30,
-            relationship: [
-              {
-                primaryDisplay: "Joe",
-                _id: "ro_ta_...",
-              },
-            ],
-          },
-        },
-        table: {
-          value: {
-            _id: "ta_5b1649e42a5b41dea4ef7742a36a7a70",
-            name: "People",
-            schema: {
-              name: {
-                type: "string",
-                name: "name",
-              },
-              age: {
-                type: "number",
-                name: "age",
-              },
-              relationship: {
-                type: "link",
-                name: "relationship",
-                tableId: "ta_...",
-                fieldName: "relatedColumn",
-                relationshipType: "many-to-many",
-              },
-            },
-          },
-        },
+        row: row,
+        table: table,
       },
     },
   },
