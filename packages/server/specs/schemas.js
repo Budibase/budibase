604bad89fd66471dd46e0c1b75a157b855f45870
mike12345567 <me@michaeldrury.co.uk>
2022-02-18 18:06:58 +0000

    Adding a schema for tables.

--- packages/server/specs/schemas.js
+++ packages/server/specs/schemas.js
@@ -1,3 +1,5 @@
+const { FieldTypes } = require("../src/constants")
+
 exports.row = {
   description: "The row to be created/updated, based on the table schema.",
   type: "object",
@@ -9,5 +11,28 @@ exports.row = {
       { type: "array" },
       { type: "boolean" },
     ],
+  },
+}
+
+exports.table = {
+  description: "The table to be created/updated.",
+  type: "object",
+  properties: {
+    name: {
+      description: "The name of the table",
+      type: "string",
+    },
+    schema: {
+      type: "object",
+      additionalProperties: {
+        type: "object",
+        properties: {
+          type: {
+            type: "string",
+            enum: Object.values(FieldTypes),
+          },
+        },
+      },
+    },
   },
 }
