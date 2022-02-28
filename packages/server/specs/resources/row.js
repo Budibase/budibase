7b3318a19e30c22593472e8489cd04ee810551d6
mike12345567 <me@michaeldrury.co.uk>
2022-02-28 12:54:32 +0000

    Switching the basic single row read to use the enrich endpoint.

--- packages/server/specs/resources/row.js
+++ packages/server/specs/resources/row.js
@@ -24,9 +24,39 @@ const row = {
   ],
 }
 
+const enrichedRow = {
+  _id: "ro_ta_5b1649e42a5b41dea4ef7742a36a7a70_e6dc7e38cf1343b2b56760265201cda4",
+  name: "eg",
+  tableId: "ta_5b1649e42a5b41dea4ef7742a36a7a70",
+  type: "row",
+  relationship: [
+    {
+      _id: "ro_ta_users_us_8f3d717147d74d759d8cef5b6712062f",
+      name: "Joe",
+      tableId: "ta_users",
+      internal: [
+        {
+          _id: "ro_ta_5b1649e42a5b41dea4ef7742a36a7a70_e6dc7e38cf1343b2b56760265201cda4",
+          primaryDisplay: "eg",
+        },
+      ],
+    },
+  ],
+}
+
 const rowSchema = {
   description: "The row to be created/updated, based on the table schema.",
   type: "object",
+  properties: {
+    _id: {
+      description: "The ID of the row.",
+      type: "string",
+    },
+    tableId: {
+      description: "The ID of the table this row comes from.",
+      type: "string",
+    },
+  },
   additionalProperties: {
     oneOf: [
       { type: "string" },
@@ -46,6 +76,11 @@ module.exports = new Resource()
     row: {
       value: {
         row: row,
+      },
+    },
+    enrichedRow: {
+      value: {
+        row: enrichedRow,
       },
     },
     rows: {
