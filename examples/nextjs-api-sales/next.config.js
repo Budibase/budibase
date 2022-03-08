5162a3ee172cac028106fbde06950de02183f1e2
mike12345567 <me@michaeldrury.co.uk>
2022-03-08 20:17:07 +0000

    Adding in pagination to next frontend.

--- examples/nextjs-api-sales/next.config.js
+++ examples/nextjs-api-sales/next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     includePaths: [join(__dirname, "styles")]
   },
   serverRuntimeConfig: {
-    apiKey: "",
-    appName: "",
+    apiKey: "bf4d86af933b5ac0af0fdbe4bf7d89ff-f929752a1eeaafb00f4b5e3325097d51a44fe4b39f22ed857923409cc75414b379323a25ebfb4916",
+    appName: "sales",
     host: "http://localhost:10000"
   }
 }
--- examples/nextjs-api-sales/pages/api/sales.ts
+++ examples/nextjs-api-sales/pages/api/sales.ts
@@ -1,13 +1,15 @@
 import getConfig from "next/config"
-import fetch from "node-fetch"
-import { App, AppSearch, RowSearch } from "../../definitions"
+import {App, AppSearch, Table, TableSearch} from "../../definitions"
 
 const { serverRuntimeConfig } = getConfig()
 const apiKey = serverRuntimeConfig["apiKey"]
 const appName = serverRuntimeConfig["appName"]
 const host = serverRuntimeConfig["host"]
 
-async function makeCall(method: string, url: string, opts?: { body?: any, appId?: string } = {}): Promise<any> {
+let APP: App | null = null
+let TABLES: { [key: string]: Table } = {}
+
+async function makeCall(method: string, url: string, opts?: { body?: any, appId?: string }): Promise<any> {
   const fetchOpts: any = {
     method,
     headers: {
@@ -18,49 +20,96 @@ async function makeCall(method: string, url: string, opts?: { body?: any, appId?
     fetchOpts.headers["x-budibase-app-id"] = opts.appId
   }
   if (opts?.body) {
-    fetchOpts.body = JSON.stringify(opts?.body)
+    fetchOpts.body = typeof opts.body !== "string" ? JSON.stringify(opts.body) : opts.body
     fetchOpts.headers["Content-Type"] = "application/json"
   }
-  const response = await fetch(`${host}/public/v1/${url}`, fetchOpts)
-  if (response.status === 200) {
+  const finalUrl = `${host}/api/public/v1/${url}`
+  const response = await fetch(finalUrl, fetchOpts)
+  if (response.ok) {
     return response.json()
   } else {
-    throw new Error(await response.text())
+    const error = await response.text()
+    console.error("Budibase server error - ", error)
+    throw new Error(error)
   }
 }
 
 async function getApp(): Promise<App> {
+  if (APP) {
+    return APP
+  }
   const apps: AppSearch = await makeCall("post", "applications/search", {
     body: {
       name: appName,
     }
   })
-  if (!Array.isArray(apps?.data)) {
-    throw new Error("Fatal error, no apps found.")
-  }
   const app = apps.data.find((app: App) => app.name === appName)
   if (!app) {
     throw new Error("Could not find app, please make sure app name in config is correct.")
   }
+  APP = app
   return app
 }
 
+async function findTable(appId: string, tableName: string): Promise<Table> {
+  if (TABLES[tableName]) {
+    return TABLES[tableName]
+  }
+  const tables: TableSearch = await makeCall("post", "tables/search", {
+    body: {
+      name: tableName,
+    },
+    appId,
+  })
+  const table = tables.data.find((table: Table) => table.name === tableName)
+  if (!table) {
+    throw new Error("Could not find table, please make sure your app is configured with the Postgres datasource correctly.")
+  }
+  TABLES[tableName] = table
+  return table
+}
+
 async function getSales(req: any) {
+  const { page } = req.query
   const { _id: appId } = await getApp()
+  const table = await findTable(appId, "sales")
+  return await makeCall("post", `tables/${table._id}/rows/search`, {
+    appId,
+    body: {
+      limit: 10,
+      sort: {
+        type: "string",
+        order: "ascending",
+        column: "sale_id",
+      },
+      paginate: true,
+      bookmark: parseInt(page),
+    }
+  })
 }
 
 async function saveSale(req: any) {
   const { _id: appId } = await getApp()
+  const table = await findTable(appId, "sales")
+  return await makeCall("post", `tables/${table._id}/rows`, {
+    body: req.body,
+    appId,
+  })
 }
 
 export default async function handler(req: any, res: any) {
   let response: any = {}
-  if (req.method === "POST") {
-    response = await saveSale(req)
-  } else if (req.method === "GET") {
-    response = await getSales(req)
-  } else {
-    res.status(404)
+  try {
+    if (req.method === "POST") {
+      response = await saveSale(req)
+    } else if (req.method === "GET") {
+      response = await getSales(req)
+    } else {
+      res.status(404)
+      return
+    }
+    res.status(200).json(response)
+  } catch (err: any) {
+    res.status(400).send(err)
   }
-  res.status(200).json(response)
 }
