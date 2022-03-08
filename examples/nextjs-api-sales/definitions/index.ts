4312e53464a775a4e891aca1d96e5653b326b7d3
mike12345567 <me@michaeldrury.co.uk>
2022-03-08 20:17:07 +0000

    Adding in pagination to next frontend.

--- examples/nextjs-api-sales/definitions/index.ts
+++ examples/nextjs-api-sales/definitions/index.ts
@@ -1,7 +1,7 @@
 import { components } from "./openapi"
 
 export type App = components["schemas"]["applicationOutput"]["data"]
-export type AppSearch = {
-  data: App[]
-}
+export type Table = components["schemas"]["tableOutput"]["data"]
+export type TableSearch = components["schemas"]["tableSearch"]
+export type AppSearch = components["schemas"]["applicationSearch"]
 export type RowSearch = components["schemas"]["searchOutput"]
