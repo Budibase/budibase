c51352c9bf65f6d42dceed1adbfa2412dfd4aa47
mike12345567 <me@michaeldrury.co.uk>
2022-03-09 15:00:36 +0000

    Final version of example, has save form and table of sales, supporting relationships and pagination all from Postgres.

--- examples/nextjs-api-sales/pages/index.tsx
+++ examples/nextjs-api-sales/pages/index.tsx
@@ -1,61 +1,81 @@
 import type { NextPage } from "next"
 import styles from "../styles/home.module.css"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
+import Notifications from "../components/notifications"
 
 const Home: NextPage = () => {
   const [sales, setSales] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
+  const [loaded, setLoaded] = useState(false)
 
-  const getSales = async (page: Number = 1) => {
+  const getSales = useCallback(async (page: Number = 1) => {
     let url = "/api/sales"
     if (page) {
       url += `?page=${page}`
     }
     const response = await fetch(url)
     if (!response.ok) {
-      throw new Error(await response.text())
+      const error = await response.text()
+      Notifications.error(error, "Failed to get sales")
+      return
     }
     const sales = await response.json()
     // @ts-ignore
     setCurrentPage(page)
     return setSales(sales.data)
-  }
+  }, [])
 
-  const saveSale = async () => {
-    const response = await fetch("/api/sales", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
-    })
-    if (!response.ok) {
-      throw new Error(await response.text())
-    }
-  }
-
-  const goToNextPage = async () => {
+  const goToNextPage = useCallback(async () => {
     await getSales(currentPage + 1)
-  }
+  }, [currentPage, getSales])
 
-  const goToPrevPage = async () => {
+  const goToPrevPage = useCallback(async () => {
     if (currentPage > 1) {
       await getSales(currentPage - 1)
     }
-  }
+  }, [currentPage, getSales])
 
   useEffect(() => {
-    getSales().catch(() => {
+    getSales().then(() => {
+      setLoaded(true)
+    }).catch(() => {
       setSales([])
     })
   }, [])
 
+  if (!loaded) {
+    return null
+  }
+
   return (
     <div className={styles.container}>
-      <h1 className="subtitle">Sales</h1>
-      <div>{sales.map((sale: any) => <p key={sale.sale_id}>{sale.sale_id}</p>)}</div>
-      <button onClick={goToPrevPage}>Prev Page</button>
-      <button onClick={goToNextPage}>Next Page</button>
+      <div className={styles.tableSection}>
+        <h1 className="subtitle">Sales</h1>
+        <div className={styles.table}>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Sale ID</th>
+                <th>name</th>
+                <th>Sold by</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sales.map((sale: any) =>
+                <tr key={sale.sale_id}>
+                  <th>{sale.sale_id}</th>
+                  <th>{sale.sale_name}</th>
+                  <th>{sale.sales_people?.map((person: any) => person.primaryDisplay)[0]}</th>
+                </tr>
+              )}
+            </tbody>
+          </table>
+          <div className={styles.buttons}>
+            <button className="button" onClick={goToPrevPage}>Prev Page</button>
+            <button className="button" onClick={goToNextPage}>Next Page</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
