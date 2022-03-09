05352703b9d8105103075f2ef587962abd23411a
mike12345567 <me@michaeldrury.co.uk>
2022-03-09 15:00:36 +0000

    Final version of example, has save form and table of sales, supporting relationships and pagination all from Postgres.

--- examples/nextjs-api-sales/db/init.sql
+++ examples/nextjs-api-sales/db/init.sql
@@ -1,23 +1,21 @@
 CREATE TABLE IF NOT EXISTS sales_people (
-  person_id INT NOT NULL,
-  name varchar(200) NOT NULL,
-  PRIMARY KEY (person_id)
+  person_id SERIAL PRIMARY KEY,
+  name varchar(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sales (
-  sale_id INT NOT NULL,
+  sale_id SERIAL PRIMARY KEY,
   sale_name varchar(200) NOT NULL,
   sold_by INT,
-  PRIMARY KEY (sale_id),
   CONSTRAINT sold_by_fk
     FOREIGN KEY(sold_by)
       REFERENCES sales_people(person_id)
 );
 
-INSERT INTO sales_people
-select id, concat('Sales person ', id)
+INSERT INTO sales_people (name)
+select 'Salesperson ' || id
 FROM GENERATE_SERIES(1, 50) as id;
 
-INSERT INTO sales
-select id, concat('Sale ', id), floor(random() * 50 + 1)::int
+INSERT INTO sales (sale_name, sold_by)
+select 'Sale ' || id, floor(random() * 50 + 1)::int
 FROM GENERATE_SERIES(1, 200) as id;
--- examples/nextjs-api-sales/pages/_app.tsx
+++ examples/nextjs-api-sales/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/global.sass"
 import type { AppProps } from "next/app"
+import Layout from "../components/layout"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
