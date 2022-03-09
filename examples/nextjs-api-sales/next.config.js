418c00c52a087edd5801af297611c16d8b804125
mike12345567 <me@michaeldrury.co.uk>
2022-03-09 16:31:08 +0000

    Some quick updates, cleaning up codebase.

--- examples/nextjs-api-sales/next.config.js
+++ examples/nextjs-api-sales/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     includePaths: [join(__dirname, "styles")]
   },
   serverRuntimeConfig: {
-    apiKey: "bf4d86af933b5ac0af0fdbe4bf7d89ff-f929752a1eeaafb00f4b5e3325097d51a44fe4b39f22ed857923409cc75414b379323a25ebfb4916",
+    apiKey: "",
     appName: "sales",
     host: "http://localhost:10000"
   }
--- examples/nextjs-api-sales/pages/api/salespeople.ts
+++ examples/nextjs-api-sales/pages/api/salespeople.ts
@@ -1,4 +1,4 @@
-import { getApp, findTable, makeCall } from "../../components/utils"
+import { getApp, findTable, makeCall } from "../../utilities"
 
 async function getSalespeople() {
   const { _id: appId } = await getApp()
