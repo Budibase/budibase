6c6b7fe411bc6672454706e71850a18cbf446f25
Andrew Kingston <andrew@kingston.dev>
2022-04-07 12:23:17 +0100

    Fix endless encoding loop with screen URLs

--- packages/builder/src/builderStore/store/screenTemplates/utils/sanitizeUrl.js
+++ packages/builder/src/builderStore/store/screenTemplates/utils/sanitizeUrl.js
@@ -2,9 +2,14 @@ export default function (url) {
   return url
     .split("/")
     .map(part => {
-      // if parameter, then use as is
-      if (part.startsWith(":")) return part
-      return encodeURIComponent(part.replace(/ /g, "-"))
+      part = decodeURIComponent(part)
+
+      // If parameter, then use as is
+      if (!part.startsWith(":")) {
+        part = encodeURIComponent(part)
+      }
+
+      return part.replace(/ /g, "-")
     })
     .join("/")
     .toLowerCase()
