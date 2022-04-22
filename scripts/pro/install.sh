fcc8db9e349e00f7e384f2871f68ed88ce65d71d
Rory Powell <rory.codes@gmail.com>
2022-04-22 11:45:45 +0100

    Skip pro install on community forks

--- scripts/pro/install.sh
+++ scripts/pro/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-if [[ -z "${CI}" ]]; then
+if [[ -z "${CI}" ]] then
   echo 'Cannot run insall.sh unless in CI'
   exit 0
 fi
@@ -11,14 +11,21 @@ BASE_BRANCH=$2
 cd ../
 echo "Cloning pro repo..."
 git clone https://$PERSONAL_ACCESS_TOKEN@github.com/Budibase/budibase-pro.git
-cd budibase-pro
 
-# Try to checkout the matching pro branch
-git checkout $BRANCH
-# Try to checkout the matching pro base (master or develop) branch
-git checkout $BASE_BRANCH
-# If neither branch exists continue with default branch 'develop'
-git pull
+# Community forks won't be able to clone the pro repo as they can't access secrets
+# Skip the rest of the installation and rely on npm version instead
+# This should be ok as forks will not rely on pro changes
+if [ -d "budibase-pro" ] 
+then
+  cd budibase-pro
 
-echo "Initializing pro repo..."
-yarn setup
+  # Try to checkout the matching pro branch
+  git checkout $BRANCH
+  # Try to checkout the matching pro base (master or develop) branch
+  git checkout $BASE_BRANCH
+  # If neither branch exists continue with default branch 'develop'
+  git pull
+
+  echo "Initializing pro repo..."
+  yarn setup
+fi
