3ac278b77e50fb2c740aa871ec4d9aaa3169b660
Andrew Kingston <andrew@kingston.dev>
2022-04-26 16:37:12 +0100

    Improve UI and state management around adding new components

--- packages/builder/src/pages/builder/app/[application]/design/components/[screenId]/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/components/[screenId]/index.svelte
@@ -1,11 +1,14 @@
 <script>
-  import { selectedScreen } from "builderStore"
+  import { selectedScreen, selectedComponent } from "builderStore"
   import { onMount } from "svelte"
   import { redirect } from "@roxi/routify"
 
   onMount(() => {
-    if ($selectedScreen) {
-      // Select the screen slot if a screen exists
+    if ($selectedComponent) {
+      // Navigate to the selected component if one exists
+      $redirect(`./${$selectedComponent._id}`)
+    } else if ($selectedScreen) {
+      // Otherwise the screen slot if a screen exists
       $redirect(`./${$selectedScreen.props._id}`)
     } else {
       // Otherwise go up so we can select a new valid screen
