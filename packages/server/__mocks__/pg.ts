a5bdf70cf73f7cac5b4c7525742286956480e027
Martin McKeaveney <martinmckeaveney@gmail.com>
2022-04-26 17:12:47 +0100

    renamable buckets, started firebase tests, onboarding modal copy update

--- packages/server/__mocks__/pg.ts
+++ packages/server/__mocks__/pg.ts
@@ -17,18 +17,9 @@ module PgMock {
   Client.prototype.connect = jest.fn()
   Client.prototype.release = jest.fn()
 
-  function Pool() {}
-
   const on = jest.fn()
-  Pool.prototype.query = query
-  Pool.prototype.connect = jest.fn(() => {
-    // @ts-ignore
-    return new Client()
-  })
-  Pool.prototype.on = on
 
   pg.Client = Client
-  pg.Pool = Pool
   pg.queryMock = query
   pg.on = on
 
