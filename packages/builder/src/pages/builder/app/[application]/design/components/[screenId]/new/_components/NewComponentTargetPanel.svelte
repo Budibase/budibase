0f8eee4db9cc648c3479a4febb9d2faf83ce5698
Andrew Kingston <andrew@kingston.dev>
2022-04-26 16:37:12 +0100

    Improve UI and state management around adding new components

--- packages/builder/src/pages/builder/app/[application]/design/components/[screenId]/new/_components/NewComponentTargetPanel.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/components/[screenId]/new/_components/NewComponentTargetPanel.svelte
@@ -1,10 +1,17 @@
 <script>
   import SettingsPanel from "components/design/settings/SettingsPanel.svelte"
   import { Body, Layout } from "@budibase/bbui"
+  import { selectedComponent, selectedScreen, store } from "builderStore"
+
+  $: componentDefinition = store.actions.components.getDefinition(
+    $selectedComponent?._component
+  )
+  $: isScreen = $selectedComponent?._id === $selectedScreen?.props._id
+  $: title = isScreen ? "Screen" : $selectedComponent?._instanceName
 </script>
 
-<SettingsPanel title="Screen" icon="WebPage">
+<SettingsPanel {title} icon={componentDefinition?.icon}>
   <Layout paddingX="L" paddingY="XL">
-    <Body>The component you add will be placed inside Screen</Body>
+    <Body>The component you add will be placed inside {title}</Body>
   </Layout>
 </SettingsPanel>
