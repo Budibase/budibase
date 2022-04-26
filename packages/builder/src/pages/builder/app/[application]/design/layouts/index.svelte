01bfb862766040afa4a3949048a26cda44fc74ec
Andrew Kingston <andrew@kingston.dev>
2022-04-26 14:24:10 +0100

    Add empty navigation panels for navigation and layouts tabs

--- packages/builder/src/pages/builder/app/[application]/design/layouts/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/layouts/index.svelte
@@ -1,1 +1,5 @@
-Layouts
+<script>
+  import NavigationPanel from "components/design/navigation/NavigationPanel.svelte"
+</script>
+
+<NavigationPanel title="Layouts" />
--- packages/builder/src/pages/builder/app/[application]/design/navigation/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/navigation/index.svelte
@@ -1,1 +1,5 @@
-Navigation
+<script>
+  import NavigationPanel from "components/design/navigation/NavigationPanel.svelte"
+</script>
+
+<NavigationPanel title="Navigation" />
