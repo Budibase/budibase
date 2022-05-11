75535caef1594223059f31f00993a0031d309fc9
Dean <deanhannigan@gmail.com>
2022-05-11 19:37:12 +0100

    Cypress test fixes and a correction for the version update behaviour

--- packages/builder/src/pages/builder/portal/overview/_components/SettingsTab.svelte
+++ packages/builder/src/pages/builder/portal/overview/_components/SettingsTab.svelte
@@ -13,6 +13,7 @@
   import clientPackage from "@budibase/client/package.json"
   import VersionModal from "components/deploy/VersionModal.svelte"
   import UpdateAppModal from "components/start/UpdateAppModal.svelte"
+  import { AppStatus } from "constants"
 
   export let app
 
@@ -25,85 +26,93 @@
   $: appUrl = `${window.origin}/app${app?.url}`
   $: lockedBy = app?.lockedBy
   $: lockedByYou = $auth.user.email === lockedBy?.email
+  $: appDeployed = app.status === AppStatus.DEPLOYED
 </script>
 
-<div class="version-tab">
+<div class="settings-tab">
   <Page wide={false}>
     <Layout gap="XL" noPadding>
-      <Layout gap="XS" noPadding>
-        <Heading size="S">Name and Url</Heading>
-        <Divider />
-        <Body>
-          <div class="app-details">
-            <div class="app-name">
-              <div class="name-title detail-title">Name</div>
-              <div class="name">{app?.name}</div>
+      <span class="details-section">
+        <Layout gap="XS" noPadding>
+          <Heading size="S">Name and Url</Heading>
+          <Divider />
+          <Body>
+            <div class="app-details">
+              <div class="app-name">
+                <div class="name-title detail-title">Name</div>
+                <div class="name">{app?.name}</div>
+              </div>
+              <div class="app-url">
+                <div class="url-title detail-title">Url Path</div>
+                <div class="url">{appUrl}</div>
+              </div>
             </div>
-            <div class="app-url">
-              <div class="url-title detail-title">Url Path</div>
-              <div class="url">{appUrl}</div>
+            <div class="page-action">
+              <Button
+                cta
+                secondary
+                on:click={() => {
+                  updatingModal.show()
+                }}
+                disabled={appDeployed}
+              >
+                Edit
+              </Button>
             </div>
-          </div>
-          <div class="page-action">
-            <Button
-              cta
-              secondary
-              on:click={() => {
-                updatingModal.show()
-              }}
-              disabled={lockedBy && !lockedByYou}
-            >
-              Edit
-            </Button>
-          </div>
-        </Body>
-      </Layout>
-      <Layout gap="XS" noPadding>
-        <Heading size="S">App Version</Heading>
-        <Divider />
-        <Body>
-          {#if updateAvailable}
+          </Body>
+        </Layout>
+      </span>
+      <span class="version-section">
+        <Layout gap="XS" noPadding>
+          <Heading size="S">App Version</Heading>
+          <Divider />
+          <Body>
+            {#if updateAvailable}
+              <p class="version-status">
+                The app is currently using version
+                <strong>{$store.version}</strong>
+                but version <strong>{clientPackage.version}</strong> is available.
+              </p>
+            {:else}
+              <p class="version-status">
+                The app is currently using version
+                <strong>{$store.version}</strong>. You're running the latest!
+              </p>
+            {/if}
             <p>
-              The app is currently using version <strong>{app?.version}</strong>
-              but version <strong>{clientPackage.version}</strong> is available.
+              Updates can contain new features, performance improvements and bug
+              fixes.
             </p>
-          {:else}
+            <div class="page-action">
+              <Button
+                cta
+                on:click={versionModal.show()}
+                disabled={!updateAvailable}>Update App</Button
+              >
+            </div>
+          </Body>
+        </Layout>
+      </span>
+      <span class="selfhost-section">
+        <Layout gap="XS" noPadding>
+          <Heading size="S">Self-host Budibase</Heading>
+          <Divider />
+          <Body>
             <p>
-              The app is currently using version <strong>{app?.version}</strong
-              >. You're running the latest!
+              Self-host Budibase for free to get unlimited apps and more - and
+              it only takes a few minutes!
             </p>
-          {/if}
-          <p>
-            Updates can contain new features, performance improvements and bug
-            fixes.
-          </p>
-          <div class="page-action">
-            <Button
-              cta
-              on:click={versionModal.show()}
-              disabled={!updateAvailable}>Update App</Button
-            >
-          </div>
-        </Body>
-      </Layout>
-      <Layout gap="XS" noPadding>
-        <Heading size="S">Self-host Budibase</Heading>
-        <Divider />
-        <Body>
-          <p>
-            Self-host Budibase for free to get unlimited apps and more - and it
-            only takes a few minutes!
-          </p>
-          <div class="page-action">
-            <Button
-              cta
-              on:click={() => {
-                window.open(selfHostPath, "_blank")
-              }}>Self-host Budibase</Button
-            >
-          </div>
-        </Body>
-      </Layout>
+            <div class="page-action">
+              <Button
+                cta
+                on:click={() => {
+                  window.open(selfHostPath, "_blank")
+                }}>Self-host Budibase</Button
+              >
+            </div>
+          </Body>
+        </Layout>
+      </span>
     </Layout>
     <VersionModal bind:this={versionModal} hideIcon={true} />
     <Modal bind:this={updatingModal} padding={false} width="600px">
