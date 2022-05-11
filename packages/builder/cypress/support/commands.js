7fc7c5a2fc21d4b97d2722077f5b72d941819727
Mitch-Budibase <mitch@budibase.com>
2022-05-11 17:23:05 +0100

    Update Commnds.js
    Specifically updating the createTestApp function
    - Creates a 'home' screen when a test app is being created

--- packages/builder/cypress/support/commands.js
+++ packages/builder/cypress/support/commands.js
@@ -144,6 +144,7 @@ Cypress.Commands.add("createTestApp", () => {
   const appName = "Cypress Tests"
   cy.deleteApp(appName)
   cy.createApp(appName, "This app is used for Cypress testing.")
+  cy.createScreen("home")
 })
 
 Cypress.Commands.add("createTestTableWithData", () => {
