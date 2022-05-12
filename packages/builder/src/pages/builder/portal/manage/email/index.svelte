bcf3e1ef1d7efa8b44142b09468a3aef48d4fd8e
Mel O'Hagan <mel@budibase.com>
2022-05-12 16:57:54 +0100

    Allow SMTP config to be cleared

--- packages/builder/src/pages/builder/portal/manage/email/index.svelte
+++ packages/builder/src/pages/builder/portal/manage/email/index.svelte
@@ -68,6 +68,26 @@
     }
   }
 
+  async function clearSmtp() {
+    // Delete the SMTP config
+    try {
+      await API.deleteConfig({
+        id: smtpConfig._id,
+        rev: smtpConfig._rev,
+      })
+      smtpConfig = {
+        config: {},
+      }
+      await admin.getChecklist()
+      notifications.success(`Settings cleared`)
+      analytics.captureEvent(Events.SMTP.SAVED)
+    } catch (error) {
+      notifications.error(
+        `Failed to clear email settings, reason: ${error?.message || "Unknown"}`
+      )
+    }
+  }
+
   async function fetchSmtp() {
     loading = true
     try {
@@ -156,8 +176,9 @@
         </div>
       {/if}
     </Layout>
-    <div>
+    <div class="spectrum-ButtonGroup spectrum-Settings-buttonGroup">
       <Button cta on:click={saveSmtp}>Save</Button>
+      <Button secondary on:click={clearSmtp}>Clear</Button>
     </div>
     <Divider />
     <Layout gap="XS" noPadding>
@@ -185,5 +206,9 @@
     grid-template-columns: 120px 1fr;
     grid-gap: var(--spacing-l);
     align-items: center;
+  }
+  .spectrum-Settings-buttonGroup {
+    gap: var(--spectrum-global-dimension-static-size-200);
+    align-items: flex-end;
   }
 </style>
