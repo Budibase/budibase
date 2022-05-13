b14440065c599845727b892656f4b6b80930cad5
Andrew Kingston <andrew@kingston.dev>
2022-05-13 10:14:52 +0100

    Clear screen validation errors when changing screen

--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/screens/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/screens/index.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { selectedScreen } from "builderStore"
   import ScreenListPanel from "./_components/ScreenListPanel.svelte"
   import ScreenSettingsPanel from "./_components/ScreenSettingsPanel.svelte"
 </script>
 
 <ScreenListPanel />
-<ScreenSettingsPanel />
+{#key $selectedScreen?._id}
+  <ScreenSettingsPanel />
+{/key}
