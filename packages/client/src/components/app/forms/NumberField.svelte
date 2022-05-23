c7f5e9124737db0f16c222a4105fca865c1db060
Mel O'Hagan <mel@budibase.com>
2022-05-23 17:39:13 +0100

    Default value for currency

--- packages/client/src/components/app/forms/NumberField.svelte
+++ packages/client/src/components/app/forms/NumberField.svelte
@@ -1,11 +1,15 @@
 <script>
   import StringField from "./StringField.svelte"
+  import currency from "currency.js"
 
   export let defaultValue
 
   const parseNumber = val => {
     if (val == null) {
       return null
+    }
+    if (typeof val === "string") {
+      return currency(val).value
     }
     return isNaN(val) ? null : parseFloat(val)
   }
