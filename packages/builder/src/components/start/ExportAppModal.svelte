a0735efbb78b814a7fe53790a042667e30bd60ce
Peter Clement <peter@budibase.com>
2022-05-25 09:26:10 +0100

    Export app without rows

---
+++ packages/builder/src/components/start/ExportAppModal.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { ModalContent, Toggle } from "@budibase/bbui"
+
+  export let app
+  let includeRows = true
+
+  const exportApp = () => {
+    const id = app.deployed ? app.prodId : app.devId
+    const appName = encodeURIComponent(app.name)
+    window.location = `/api/backups/export?appId=${id}&appname=${appName}&includeRows=${includeRows}`
+  }
+</script>
+
+<ModalContent title={"Export"} confirmText={"Export"} onConfirm={exportApp}>
+  <Toggle text="Include Data" bind:value={includeRows} />
+</ModalContent>
--- packages/server/src/api/controllers/backup.js
+++ packages/server/src/api/controllers/backup.js
@@ -1,9 +1,11 @@
 const { streamBackup } = require("../../utilities/fileSystem")
 
 exports.exportAppDump = async function (ctx) {
-  const { appId } = ctx.query
+  let { appId, includeRows } = ctx.query
   const appName = decodeURI(ctx.query.appname)
+  includeRows = includeRows === "true"
   const backupIdentifier = `${appName}-export-${new Date().getTime()}.txt`
   ctx.attachment(backupIdentifier)
-  ctx.body = await streamBackup(appId)
+
+  ctx.body = await streamBackup(appId, includeRows)
 }
