bd737cc1ca05af741120c1a59511a1271e465b1a
Peter Clement <peter@budibase.com>
2022-06-06 15:17:14 +0100

    negate export button

--- packages/server/src/api/controllers/backup.js
+++ packages/server/src/api/controllers/backup.js
@@ -1,11 +1,11 @@
 const { streamBackup } = require("../../utilities/fileSystem")
 
 exports.exportAppDump = async function (ctx) {
-  let { appId, includeRows } = ctx.query
+  let { appId, excludeRows } = ctx.query
   const appName = decodeURI(ctx.query.appname)
-  includeRows = includeRows === "true"
+  excludeRows = excludeRows === "true"
   const backupIdentifier = `${appName}-export-${new Date().getTime()}.txt`
   ctx.attachment(backupIdentifier)
 
-  ctx.body = await streamBackup(appId, includeRows)
+  ctx.body = await streamBackup(appId, excludeRows)
 }
