5b58981a53734dba27770285761e908bc1494803
Martin McKeaveney <martinmckeaveney@gmail.com>
2022-06-07 16:40:43 +0100

    start of beta panel

---
+++ packages/builder/src/components/beta/BetaTab.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Body, Button, Layout, Heading } from "@budibase/bbui"
+  import { CookieUtils } from "@budibase/frontend-core"
+
+  function openFeedbackApp() {
+    window.open(`https://bb.budibase.app/betauifeedback`)
+  }
+
+  function revertToOldBuilder() {
+    CookieUtils.removeCookie("beta:new_design_ui")
+    window.location.reload()
+  }
+</script>
+
+<Layout>
+  <Heading size="M">Beta Builder</Heading>
+  <Body>
+    Your account has been given access to the budibase beta program. We would
+    love to hear what you think and get your feedback and suggestions on the new
+    Design UI.
+  </Body>
+  <Button cta on:click={openFeedbackApp}>Give Feedback</Button>
+  <Button secondary on:click={revertToOldBuilder}
+    >Revert back to old builder</Button
+  >
+</Layout>
