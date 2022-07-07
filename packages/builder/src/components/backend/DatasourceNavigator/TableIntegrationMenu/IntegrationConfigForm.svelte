8592f79eaaacae58cedfbdc420c49f8ffdb8a9ca
Maurits Lourens <maurits.lourens@nedap.com>
2022-07-07 23:06:57 +0200

    6538 - add validation to datasource config modal

--- packages/builder/src/components/backend/DatasourceNavigator/TableIntegrationMenu/IntegrationConfigForm.svelte
+++ packages/builder/src/components/backend/DatasourceNavigator/TableIntegrationMenu/IntegrationConfigForm.svelte
@@ -10,10 +10,14 @@
   import KeyValueBuilder from "components/integration/KeyValueBuilder.svelte"
   import { capitalise } from "helpers"
   import { IntegrationTypes } from "constants/backend"
+  import { createValidationStore } from "helpers/validation/yup"
+  import { createEventDispatcher } from "svelte"
 
   export let datasource
   export let schema
   export let creating
+  const validation = createValidationStore()
+  const dispatch = createEventDispatcher()
 
   function filter([key, value]) {
     if (!value) {
@@ -30,6 +34,17 @@
   $: configKeys = Object.entries(schema || {})
     .filter(el => filter(el))
     .map(([key]) => key)
+
+  // setup the validation for each required field
+  $: configKeys.forEach(key => {
+    if (schema[key].required) {
+      validation.addValidatorType(key, schema[key].type, schema[key].required)
+    }
+  })
+  // run the validation whenever the config changes
+  $: validation.check(config)
+  // dispatch the validation result
+  $: dispatch("valid", $validation.valid)
 
   let addButton
 
@@ -79,6 +94,9 @@
             type={schema[configKey].type}
             on:change
             bind:value={config[configKey]}
+            on:blur={($validation.touched[configKey] = true)}
+            error={$validation.touched[configKey] &&
+              $validation.errors[configKey]}
           />
         </div>
       {:else}
@@ -88,6 +106,9 @@
             type={schema[configKey].type}
             on:change
             bind:value={config[configKey]}
+            on:blur={($validation.touched[configKey] = true)}
+            error={$validation.touched[configKey] &&
+              $validation.errors[configKey]}
           />
         </div>
       {/if}
