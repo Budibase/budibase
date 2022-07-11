22541721c1b053cd6a79bc04abe5d4424de5e3be
Maurits Lourens <maurits.lourens@nedap.com>
2022-07-11 15:59:32 +0200

    Pass a title property with untruncated text to the RadioGroup

--- packages/builder/src/components/backend/DataTable/modals/CreateEditColumn.svelte
+++ packages/builder/src/components/backend/DataTable/modals/CreateEditColumn.svelte
@@ -474,6 +474,7 @@
         options={relationshipOptions}
         getOptionLabel={option => option.name}
         getOptionValue={option => option.value}
+        getOptionTitle={option => option.alt}
       />
     {/if}
     <Input
