24cfc17a7827aabf368231c4e20271db79c27d11
Dean <deanhannigan@gmail.com>
2022-07-13 12:30:58 +0100

    Updated action categories, adding a logic specific entry for the conditional step. Shift the action text above the button.

--- packages/builder/src/components/design/settings/controls/ButtonActionEditor/ButtonActionEditor.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/ButtonActionEditor.svelte
@@ -76,8 +76,9 @@
   } set`
 </script>
 
-<ActionButton on:click={openDrawer}>Define actions</ActionButton>
 <div class="action-count">{actionText}</div>
+<ActionButton on:click={openDrawer}>Define actions</ActionButton>
+
 <Drawer bind:this={drawer} title={"Actions"}>
   <svelte:fragment slot="description">
     Define what actions to run.
@@ -94,6 +95,7 @@
 
 <style>
   .action-count {
-    padding-top: var(--spacing-s);
+    padding-bottom: var(--spacing-s);
+    font-weight: 600;
   }
 </style>
