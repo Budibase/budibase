25dc032a3efa73e99c28b3f1cc4c6dfd882d0b25
Jonny McCullagh <jonny.mccullagh@redbranch.net>
2022-07-18 11:16:33 +0100

    WAzure App Service image to docker registry

--- hosting/scripts/build-target-paths.sh
+++ hosting/scripts/build-target-paths.sh
@@ -3,15 +3,15 @@
 echo ${TARGETBUILD} > /buildtarget.txt
 if [[ "${TARGETBUILD}" = "aas" ]]; then
     # Azure AppService uses /home for persisent data & SSH on port 2222
-    mkdir -p /home/budibase/{minio,couchdb}
-    mkdir -p  /home/budibase/couchdb/data
-    chown -R couchdb:couchdb /home/budibase/couchdb/
+    mkdir -p /home/{search,minio,couch}
+    mkdir -p /home/couch/{dbs,views}
+    chown -R couchdb:couchdb /home/couch/
     apt update
     apt-get install -y openssh-server
-    sed -i 's#dir=/opt/couchdb/data/search#dir=/home/budibase/couchdb/data/search#' /opt/clouseau/clouseau.ini
-    sed -i 's#/minio/minio server /minio &#/minio/minio server /home/budibase/minio &#' /runner.sh
-    sed -i 's#database_dir = ./data#database_dir = /home/budibase/couchdb/data#' /opt/couchdb/etc/default.ini
-    sed -i 's#view_index_dir = ./data#view_index_dir = /home/budibase/couchdb/data#' /opt/couchdb/etc/default.ini
+    sed -i 's#dir=/opt/couchdb/data/search#dir=/home/search#' /opt/clouseau/clouseau.ini
+    sed -i 's#/minio/minio server /minio &#/minio/minio server /home/minio &#' /runner.sh
+    sed -i 's#database_dir = ./data#database_dir = /home/couch/dbs#' /opt/couchdb/etc/default.ini
+    sed -i 's#view_index_dir = ./data#view_index_dir = /home/couch/views#' /opt/couchdb/etc/default.ini
     sed -i "s/#Port 22/Port 2222/" /etc/ssh/sshd_config
     /etc/init.d/ssh restart
 fi
