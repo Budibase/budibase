ea1722dfd224d888f555a044e4f4791e6dec9e06
Andrew Kingston <andrew@kingston.dev>
2022-07-25 12:07:11 +0100

    Update class names

--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/components/[componentId]/new/_components/NewComponentPanel.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/components/[componentId]/new/_components/NewComponentPanel.svelte
@@ -151,7 +151,7 @@
   })
 </script>
 
-<div class="new-component" transition:fly|local={{ x: 260, duration: 300 }}>
+<div class="container" transition:fly|local={{ x: 260, duration: 300 }}>
   <Panel
     title="Add component"
     showCloseButton
@@ -222,7 +222,7 @@
 </div>
 
 <style>
-  .new-component {
+  .container {
     position: fixed;
     right: 0;
     z-index: 1;
