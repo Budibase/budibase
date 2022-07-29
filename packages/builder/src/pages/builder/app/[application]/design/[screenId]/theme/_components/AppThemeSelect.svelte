df759ac20a7a81dd6c888378b6ea95c61e197098
Andrew Kingston <andrew@kingston.dev>
2022-07-29 11:53:07 +0100

    Offer custom themes for client apps and improve theme panel

--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/theme/_components/AppThemeSelect.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/theme/_components/AppThemeSelect.svelte
@@ -1,20 +1,11 @@
 <script>
-  import { notifications, Slider, Icon } from "@budibase/bbui"
+  import { notifications } from "@budibase/bbui"
   import { store } from "builderStore"
+  import { Constants } from "@budibase/frontend-core"
 
-  const ThemeOptions = [
-    "spectrum--darkest",
-    "spectrum--dark",
-    "spectrum--light",
-    "spectrum--lightest",
-  ]
-
-  $: themeIndex = ThemeOptions.indexOf($store.theme) ?? 2
-
-  const onChangeTheme = async e => {
+  const onChangeTheme = async theme => {
     try {
-      const theme = ThemeOptions[e.detail] ?? ThemeOptions[2]
-      await store.actions.theme.save(theme)
+      await store.actions.theme.save(`spectrum--${theme}`)
     } catch (error) {
       notifications.error("Error updating theme")
     }
@@ -22,26 +13,52 @@
 </script>
 
 <div class="container">
-  <Icon name="Moon" />
-  <Slider
-    min={0}
-    max={3}
-    step={1}
-    value={themeIndex}
-    on:change={onChangeTheme}
-  />
-  <Icon name="Light" />
+  {#each Constants.Themes as theme}
+    <div
+      class="theme"
+      class:selected={`spectrum--${theme.class}` === $store.theme}
+      on:click={() => onChangeTheme(theme.class)}
+    >
+      <div
+        style="background: {theme.preview}"
+        class="color spectrum--{theme.class}"
+      />
+      {theme.name}
+    </div>
+  {/each}
 </div>
 
 <style>
-  div {
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+    gap: var(--spacing-xs);
+  }
+  .color {
+    width: 20px;
+    height: 20px;
+    border-radius: 50px;
+    background: var(--spectrum-global-color-gray-200);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .theme {
+    border-radius: 4px;
+    padding: var(--spacing-s) var(--spacing-m);
     display: flex;
     flex-direction: row;
-    justify-content: flex-start;
     align-items: center;
-    gap: var(--spacing-m);
+    gap: var(--spacing-xl);
+    transition: background 130ms ease-out;
+    font-weight: 600;
+    color: var(--spectrum-global-color-gray-900);
   }
-  div :global(.spectrum-Form-item) {
-    flex: 1 1 auto;
+  .theme:hover {
+    cursor: pointer;
+  }
+  .theme.selected,
+  .theme:hover {
+    background: var(--spectrum-global-color-gray-50);
   }
 </style>
