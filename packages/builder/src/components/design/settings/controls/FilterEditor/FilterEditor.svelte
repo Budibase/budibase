5eeb2ea6006d9fded675b10da3856a1b452d51e4
mike12345567 <me@michaeldrury.co.uk>
2022-08-02 18:34:58 +0100

    Updating the filter system to allow adding multiple filter properties of the same name at once, as well as enabling the use of the allOr property from within the UI - resolves an old issue #2585.

--- packages/builder/src/components/design/settings/controls/FilterEditor/FilterEditor.svelte
+++ packages/builder/src/components/design/settings/controls/FilterEditor/FilterEditor.svelte
@@ -8,21 +8,73 @@
   import FilterDrawer from "./FilterDrawer.svelte"
   import { currentAsset } from "builderStore"
 
+  const QUERY_START_REGEX = /\d[0-9]*:/g
   const dispatch = createEventDispatcher()
 
   export let value = []
   export let componentInstance
   export let bindings = []
 
-  let drawer
-  let tempValue = value || []
+  let drawer,
+    toSaveFilters = null,
+    allOr,
+    initialAllOr
 
+  $: initialFilters = correctFilters(value || [])
   $: dataSource = getDatasourceForProvider($currentAsset, componentInstance)
   $: schema = getSchemaForDatasource($currentAsset, dataSource)?.schema
   $: schemaFields = Object.values(schema || {})
 
-  const saveFilter = async () => {
-    dispatch("change", tempValue)
+  function addNumbering(filters) {
+    let count = 1
+    for (let value of filters) {
+      if (value.field && value.field?.match(QUERY_START_REGEX) == null) {
+        value.field = `${count++}:${value.field}`
+      }
+    }
+    return filters
+  }
+
+  function correctFilters(filters) {
+    const corrected = []
+    for (let filter of filters) {
+      let field = filter.field
+      if (filter.operator === "allOr") {
+        initialAllOr = allOr = true
+        continue
+      }
+      if (
+        typeof filter.field === "string" &&
+        filter.field.match(QUERY_START_REGEX) != null
+      ) {
+        const parts = field.split(":")
+        const number = parts[0]
+        // it's the new format, remove number
+        if (!isNaN(parseInt(number))) {
+          parts.shift()
+          field = parts.join(":")
+        }
+      }
+      corrected.push({
+        ...filter,
+        field,
+      })
+    }
+    return corrected
+  }
+
+  async function saveFilter() {
+    if (!toSaveFilters && allOr !== initialAllOr) {
+      toSaveFilters = initialFilters
+    }
+    const filters = toSaveFilters?.filter(filter => filter.operator !== "allOr")
+    if (allOr && filters) {
+      filters.push({ operator: "allOr" })
+    }
+    // only save if anything was updated
+    if (filters) {
+      dispatch("change", addNumbering(filters))
+    }
     notifications.success("Filters saved.")
     drawer.hide()
   }
@@ -33,8 +85,12 @@
   <Button cta slot="buttons" on:click={saveFilter}>Save</Button>
   <FilterDrawer
     slot="body"
-    bind:filters={tempValue}
+    filters={initialFilters}
     {bindings}
     {schemaFields}
+    bind:allOr
+    on:change={event => {
+      toSaveFilters = event.detail
+    }}
   />
 </Drawer>
--- packages/server/src/integrations/base/utils.ts
+++ packages/server/src/integrations/base/utils.ts
@@ -1,22 +1,30 @@
 import { QueryJson } from "../../definitions/datasource"
 import { Datasource } from "../../definitions/common"
+const { integrations } = require("../index")
 
-module DatasourceUtils {
-  const { integrations } = require("../index")
+const QUERY_START_REGEX = /\d[0-9]*:/g
 
-  export async function makeExternalQuery(
-    datasource: Datasource,
-    json: QueryJson
-  ) {
-    const Integration = integrations[datasource.source]
-    // query is the opinionated function
-    if (Integration.prototype.query) {
-      const integration = new Integration(datasource.config)
-      return integration.query(json)
-    } else {
-      throw "Datasource does not support query."
-    }
+export async function makeExternalQuery(
+  datasource: Datasource,
+  json: QueryJson
+) {
+  const Integration = integrations[datasource.source]
+  // query is the opinionated function
+  if (Integration.prototype.query) {
+    const integration = new Integration(datasource.config)
+    return integration.query(json)
+  } else {
+    throw "Datasource does not support query."
   }
+}
 
-  module.exports.makeExternalQuery = makeExternalQuery
+export function removeKeyNumbering(key: any): string {
+  if (typeof key === "string" && key.match(QUERY_START_REGEX) != null) {
+    const parts = key.split(":")
+    // remove the number
+    parts.shift()
+    return parts.join(":")
+  } else {
+    return key
+  }
 }
