f5f510a482424a18f0e2e02d2828093ac3dcb637
Andrew Kingston <andrew@kingston.dev>
2022-08-04 15:33:51 +0100

    Fix multiple issues in user details page

--- packages/builder/src/components/settings/UserGroupPicker.svelte
+++ packages/builder/src/components/settings/UserGroupPicker.svelte
@@ -16,7 +16,7 @@
     return list.map(item => {
       return {
         ...item,
-        selected: selected.find(x => x._id === item._id) != null,
+        selected: selected.find(x => x === item._id) != null,
       }
     })
   }
