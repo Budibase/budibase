0b04dc51720832d15fb78ede8ca330dcf4a16ff6
Andrew Kingston <andrew@kingston.dev>
2022-08-05 10:37:35 +0100

    Use core icon component for app rows

--- packages/bbui/src/Icon/IconAvatar.svelte
+++ packages/bbui/src/Icon/IconAvatar.svelte
@@ -24,8 +24,8 @@
     border-radius: 50%;
   }
   .icon :global(.spectrum-Icon) {
-    width: 20px;
-    height: 20px;
+    width: 22px;
+    height: 22px;
   }
   .icon.filled :global(.spectrum-Icon) {
     width: 16px;
