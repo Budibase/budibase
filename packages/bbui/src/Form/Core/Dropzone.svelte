df620fb9f729bf79512242cd6a30267e34d7bbb4
Mel O'Hagan <mel@budibase.com>
2022-08-12 11:43:39 +0100

    Delete the selected file only

--- packages/bbui/src/Form/Core/Dropzone.svelte
+++ packages/bbui/src/Form/Core/Dropzone.svelte
@@ -95,8 +95,10 @@
       "change",
       value.filter((x, idx) => idx !== selectedImageIdx)
     )
+    await deleteAttachments(
+      value.filter((x, idx) => idx === selectedImageIdx).map(item => item.key)
+    )
     selectedImageIdx = 0
-    await deleteAttachments(value.map(item => item.key))
   }
 
   function navigateLeft() {
