af7ce241ffc64ca803bb635ecab13faf11b4130e
poirazis <poirazis@gmail.com>
2022-08-14 20:27:10 +0300

    Component Defined in Manifest

---
+++ packages/client/src/components/app/ProgressBar.svelte
@@ -0,0 +1,15 @@
+<script></script>
+  import { getContext } from "svelte"
+
+  const component = getContext("component")
+
+  export let size = "M"
+  export let progress = "50" 
+  export let label
+
+  $: size = size?.toLowerCase()
+  $: if (!label) ? label = "Progress ..." : label = label
+
+</script>
+
+<div>Hello !</div>
