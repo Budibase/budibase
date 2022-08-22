8a2f35b3f0cccbfbf85b878f5f3b33cec350f376
Andrew Kingston <andrew@kingston.dev>
2022-08-22 11:00:51 +0100

    Add action for showing a custom notification

---
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/ShowNotification.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { Select, Label } from "@budibase/bbui"
+  import { onMount } from "svelte"
+  import DrawerBindableInput from "components/common/bindings/DrawerBindableInput.svelte"
+
+  export let parameters
+
+  const types = [
+    {
+      label: "Success",
+      value: "success",
+    },
+    {
+      label: "Warning",
+      value: "warning",
+    },
+    {
+      label: "Error",
+      value: "error",
+    },
+    {
+      label: "Info",
+      value: "info",
+    },
+  ]
+
+  onMount(() => {
+    if (!parameters.type) {
+      parameters.type = "success"
+    }
+  })
+</script>
+
+<div class="root">
+  <Label small>Type</Label>
+  <Select bind:value={parameters.type} options={types} placeholder={null} />
+  <Label small>Message</Label>
+  <DrawerBindableInput
+    value={parameters.message}
+    on:change={e => (parameters.message = e.detail)}
+  />
+</div>
+
+<style>
+  .root {
+    display: grid;
+    column-gap: var(--spacing-l);
+    row-gap: var(--spacing-s);
+    grid-template-columns: 60px 1fr;
+    align-items: center;
+    max-width: 400px;
+    margin: 0 auto;
+  }
+</style>
