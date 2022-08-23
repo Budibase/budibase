0a2ad8f87981d4460da108806c14236583338840
Andrew Kingston <andrew@kingston.dev>
2022-08-23 14:58:56 +0100

    Remove form block and rename form block plus to form block

--- packages/client/src/components/app/blocks/FormBlock.svelte
+++ packages/client/src/components/app/blocks/FormBlock.svelte
@@ -2,8 +2,9 @@
   import { getContext } from "svelte"
   import BlockComponent from "../../BlockComponent.svelte"
   import Block from "../../Block.svelte"
-  import { Heading, Layout } from "@budibase/bbui"
+  import { Layout } from "@budibase/bbui"
   import Placeholder from "../Placeholder.svelte"
+  import { makePropSafe as safe } from "@budibase/string-templates"
 
   export let actionType
   export let dataSource
@@ -12,14 +13,12 @@
   export let fields
   export let labelPosition
   export let title
-  export let showPrimaryButton
-  export let primaryButtonOnClick
-  export let primaryButtonText
-  export let showSecondaryButton
-  export let secondaryButtonOnClick
-  export let secondaryButtonText
+  export let showSaveButton
+  export let showDeleteButton
+  export let rowId
+  export let actionUrl
 
-  const { styleable, fetchDatasourceSchema } = getContext("sdk")
+  const { styleable, fetchDatasourceSchema, builderStore } = getContext("sdk")
   const component = getContext("component")
   const FieldTypeToComponentMap = {
     string: "stringfield",
@@ -35,7 +34,65 @@
   }
 
   let schema
+  let formId
+  let providerId
+  let repeaterId
+
   $: fetchSchema(dataSource)
+  $: onSave = [
+    {
+      "##eventHandlerType": "Save Row",
+      parameters: {
+        providerId: formId,
+        tableId: dataSource?.tableId,
+      },
+    },
+    {
+      "##eventHandlerType": "Close Screen Modal",
+    },
+    {
+      "##eventHandlerType": "Navigate To",
+      parameters: {
+        url: actionUrl,
+      },
+    },
+  ]
+  $: onDelete = [
+    {
+      "##eventHandlerType": "Delete Row",
+      parameters: {
+        confirm: true,
+        tableId: dataSource?.tableId,
+        rowId: `{{ ${safe(repeaterId)}.${safe("_id")} }}`,
+        revId: `{{ ${safe(repeaterId)}.${safe("_rev")} }}`,
+      },
+    },
+    {
+      "##eventHandlerType": "Close Screen Modal",
+    },
+    {
+      "##eventHandlerType": "Navigate To",
+      parameters: {
+        url: actionUrl,
+      },
+    },
+  ]
+  $: filter = [
+    {
+      field: "_id",
+      operator: "equal",
+      type: "string",
+      value: rowId,
+      valueType: "binding",
+    },
+  ]
+  // If we're using an "update" form, use the real data provider. If we're
+  // using a create form, we just want a fake array so that our repeater
+  // will actually render the form, but data doesn't matter.
+  $: dataProvider =
+    actionType === "Update"
+      ? `{{ literal ${safe(providerId)} }}`
+      : { rows: [{}] }
 
   const fetchSchema = async () => {
     schema = (await fetchDatasourceSchema(dataSource)) || {}
@@ -54,57 +111,85 @@
   <div use:styleable={$component.styles}>
     {#if fields?.length}
       <BlockComponent
-        type="form"
-        props={{ actionType, dataSource, size, disabled }}
-        context="form"
+        type="dataprovider"
+        context="provider"
+        bind:id={providerId}
+        props={{
+          dataSource,
+          filter,
+          limit: rowId ? 1 : $builderStore.inBuilder ? 1 : 0,
+          paginate: false,
+        }}
       >
-        <Layout noPadding gap="M">
-          <div class="title" class:with-text={!!title}>
-            <Heading>{title || ""}</Heading>
-            <div class="buttons">
-              {#if showSecondaryButton}
-                <BlockComponent
-                  type="button"
-                  props={{
-                    text: secondaryButtonText,
-                    onClick: secondaryButtonOnClick,
-                    quiet: true,
-                    type: "secondary",
-                  }}
-                />
-              {/if}
-              {#if showPrimaryButton}
-                <BlockComponent
-                  type="button"
-                  props={{
-                    text: primaryButtonText,
-                    onClick: primaryButtonOnClick,
-                    type: "cta",
-                  }}
-                />
-              {/if}
-            </div>
-          </div>
-          <BlockComponent type="fieldgroup" props={{ labelPosition }}>
-            {#each fields as field}
-              {#if getComponentForField(field)}
-                <BlockComponent
-                  type={getComponentForField(field)}
-                  props={{
-                    field,
-                    label: field,
-                    placeholder: field,
-                    disabled,
-                  }}
-                />
-              {/if}
-            {/each}
+        <BlockComponent
+          type="repeater"
+          context="repeater"
+          bind:id={repeaterId}
+          props={{
+            dataProvider,
+            noRowsMessage: "We couldn't find a row to display",
+          }}
+        >
+          <BlockComponent
+            type="form"
+            props={{
+              actionType,
+              dataSource,
+              size,
+              disabled: disabled || actionType === "View",
+            }}
+            context="form"
+            bind:id={formId}
+          >
+            <Layout noPadding gap="M">
+              <div class="title" class:with-text={!!title}>
+                <BlockComponent type="heading" props={{ text: title || "" }} />
+                <div class="buttons">
+                  {#if showDeleteButton && actionType === "Update"}
+                    <BlockComponent
+                      type="button"
+                      props={{
+                        text: "Delete",
+                        onClick: onDelete,
+                        quiet: true,
+                        type: "secondary",
+                      }}
+                    />
+                  {/if}
+                  {#if showSaveButton && actionType !== "View"}
+                    <BlockComponent
+                      type="button"
+                      props={{
+                        text: "Save",
+                        onClick: onSave,
+                        type: "cta",
+                      }}
+                    />
+                  {/if}
+                </div>
+              </div>
+              <BlockComponent type="fieldgroup" props={{ labelPosition }}>
+                {#each fields as field}
+                  {#if getComponentForField(field)}
+                    <BlockComponent
+                      type={getComponentForField(field)}
+                      props={{
+                        field,
+                        label: field,
+                        placeholder: field,
+                        disabled,
+                      }}
+                    />
+                  {/if}
+                {/each}
+              </BlockComponent>
+            </Layout>
           </BlockComponent>
-        </Layout>
+        </BlockComponent>
       </BlockComponent>
     {:else}
       <Placeholder
-        text="Choose your schema and add some fields to your form to get started"
+        text="Choose your table and add some fields to your form to get started"
       />
     {/if}
   </div>
