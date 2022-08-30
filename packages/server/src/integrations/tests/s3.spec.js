6a1dd4b0aa3fca508512302c7006821d547be4fc
Mel O'Hagan <mel@budibase.com>
2022-08-30 16:56:56 +0100

    Create WIP

--- packages/server/src/integrations/tests/s3.spec.js
+++ packages/server/src/integrations/tests/s3.spec.js
@@ -16,7 +16,7 @@ describe("S3 Integration", () => {
   })
 
   it("calls the read method with the correct params", async () => {
-    const response = await config.integration.read({ 
+    await config.integration.read({ 
       bucket: "test",
       delimiter: "/",
       marker: "file.txt",
@@ -29,6 +29,36 @@ describe("S3 Integration", () => {
       Marker: "file.txt",
       MaxKeys: 999,
       Prefix: "directory/"
+    })
+  })
+
+  it("calls the create method with the correct params", async () => {
+    await config.integration.create({ 
+      bucket: "test",
+      location: "af-south-1",
+      grantFullControl: "me",
+      grantRead: "him",
+      grantReadAcp: "her",
+      grantWrite: "she",
+      grantWriteAcp: "he",
+      objectLockEnabledForBucket: true
+    }, {
+      acl: "private",
+      objectOwnership: "BucketOwnerPreferred"
+    })
+    expect(config.integration.client.createBucket).toHaveBeenCalledWith({
+      Bucket: "test",
+      CreateBucketConfiguration: {
+        LocationConstraint: "af-south-1"
+      },
+      GrantFullControl: "me",
+      GrantRead: "him",
+      GrantReadAcp: "her",
+      GrantWrite: "she",
+      GrantWriteAcp: "he",
+      ObjectLockEnabledForBucket: true,
+      ACL: "private",
+      ObjectOwnership: "BucketOwnerPreferred"
     })
   })
 })
