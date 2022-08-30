b86bd5bf034148ba87b082b72e9f54fbfcaa7368
Andrew Kingston <andrew@kingston.dev>
2022-08-30 10:53:58 +0100

    Add setting button for ejecting blocks. Update manifest to show info beside relevant settings

---
+++ packages/builder/src/components/design/settings/controls/EjectBlockButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { ActionButton } from "@budibase/bbui"
+  import { store } from "builderStore"
+  import ConfirmDialog from "components/common/ConfirmDialog.svelte"
+
+  export let componentInstance
+
+  let confirmDialog
+</script>
+
+<div>
+  <ActionButton secondary on:click={confirmDialog.show}>
+    Eject block
+  </ActionButton>
+</div>
+<ConfirmDialog
+  bind:this={confirmDialog}
+  title="Eject block"
+  body="Ejecting a block breaks it down into multiple components. Are you sure you want to eject this block?"
+  onOk={() =>
+    store.actions.components.requestEjectBlock(componentInstance?._id)}
+  okText="Eject block"
+/>
