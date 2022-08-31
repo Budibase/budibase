b8f2a5feeb88b670dffca50ce0d76ca20534d412
Hendrik-Huels <112501447+Hendrik-Huels@users.noreply.github.com>
2022-08-31 17:00:02 +0200

    Update testing.sh

--- hosting/utils/testing.sh
+++ hosting/utils/testing.sh
@@ -12,9 +12,9 @@ dockerInstalled
 
 source "${BASH_SOURCE%/*}/hosting.properties"
 
-opts="-e MINIO_ACCESS_KEY=$minio_access_key -e MINIO_SECRET_KEY=$minio_secret_key"
+opts="-e MINIO_ROOT_USER=$minio_access_key -e MINIO_ROOT_PASSWORD=$minio_secret_key"
 if [ -n "$minio_secret_key_old" ] && [ -n "$minio_access_key_old" ]; then
-    opts="$opts -e MINIO_SECRET_KEY_OLD=$minio_secret_key_old -e MINIO_ACCESS_KEY_OLD=$minio_access_key_old"
+    opts="$opts -e MINIO_ROOT_PASSWORD_OLD=$minio_secret_key_old -e MINIO_ROOT_USER_OLD=$minio_access_key_old"
 fi
 
 docker run -p $minio_port:$minio_port $opts -v /mnt/data:/data minio/minio server /data
