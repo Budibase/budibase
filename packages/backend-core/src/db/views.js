395a846328310805467f6b638344043681f23372
Martin McKeaveney <martinmckeaveney@gmail.com>
2022-09-02 16:55:55 +0100

    fixing circular dep issue

--- packages/backend-core/src/db/views.js
+++ packages/backend-core/src/db/views.js
@@ -4,7 +4,7 @@ const {
   DeprecatedViews,
   SEPARATOR,
 } = require("./utils")
-const { getGlobalDB } = require("../tenancy")
+const { getGlobalDB } = require("../context")
 const { StaticDatabases } = require("./constants")
 const { doWithDB } = require("./")
 
