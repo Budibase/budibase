9c28fac0bf78fffdadf124250435ae0993dc6f49
Hervé de CHAVIGNY <vevedh@gmail.com>
2022-09-04 17:12:07 -0400

    Stable datasource ADLDAP v0 -update icon

---
+++ packages/builder/src/components/backend/DatasourceNavigator/icons/ADLDAP.svelte
@@ -0,0 +1,135 @@
+<script>
+  export let width = "100"
+  export let height = "100"
+</script>
+
+<svg
+  {width}
+  {height}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+  <linearGradient
+    id="a"
+    gradientUnits="userSpaceOnUse"
+    x1="15"
+    x2="28"
+    y1="13"
+    y2="26"
+  >
+    <stop offset="0" stop-color="#292c2f" />
+    <stop offset="1" stop-opacity="0" />
+  </linearGradient>
+  <linearGradient
+    id="b"
+    gradientUnits="userSpaceOnUse"
+    x1="26"
+    x2="26"
+    y1="20.999999"
+    y2="7"
+  >
+    <stop offset="0" stop-color="#31363b" />
+    <stop offset="1" stop-color="#4d4d4d" />
+  </linearGradient>
+  <linearGradient
+    id="c"
+    gradientTransform="translate(0 -1)"
+    gradientUnits="userSpaceOnUse"
+    x1="4"
+    x2="8"
+    xlink:href="#i"
+    y1="19"
+    y2="23"
+  />
+  <linearGradient
+    id="d"
+    gradientTransform="translate(0 -8)"
+    gradientUnits="userSpaceOnUse"
+    x1="4"
+    x2="8"
+    xlink:href="#i"
+    y1="19"
+    y2="23"
+  />
+  <linearGradient id="e">
+    <stop offset="0" />
+    <stop offset="1" stop-opacity="0" />
+  </linearGradient>
+  <linearGradient
+    id="f"
+    gradientUnits="userSpaceOnUse"
+    x1="3.999"
+    x2="8"
+    xlink:href="#e"
+    y1="4"
+    y2="8"
+  />
+  <linearGradient
+    id="g"
+    gradientUnits="userSpaceOnUse"
+    x1="28"
+    x2="28"
+    y1="30"
+    y2="2"
+  >
+    <stop offset="0" stop-color="#997657" />
+    <stop offset="1" stop-color="#d4ba94" />
+  </linearGradient>
+  <linearGradient
+    id="h"
+    gradientTransform="matrix(.66667 0 0 16 -25.794 -199.679)"
+    gradientUnits="userSpaceOnUse"
+    x1="44.691"
+    x2="44.691"
+    y1="14.355"
+    y2="12.605"
+  >
+    <stop offset="0" stop-color="#433a35" />
+    <stop offset="1" stop-color="#6c5548" />
+  </linearGradient>
+  <linearGradient
+    id="i"
+    gradientTransform="matrix(.70710678 -.70710678 .70710678 .70710678 -664.006239 -60.060868)"
+    gradientUnits="userSpaceOnUse"
+    x1="432.711"
+    x2="432.711"
+    xlink:href="#e"
+    y1="531.791"
+    y2="544.519"
+  />
+  <path d="m4 2h24v28h-24z" fill="url(#g)" />
+  <path d="m4 29h24v1h-24z" fill="#232629" opacity=".6" />
+  <path d="m4 2v28h4v-1h-3v-1h3v-26h-3z" fill="url(#h)" />
+  <path d="m4 28h24v1h-24z" fill="#c5cfd3" />
+  <path d="m4 27h24v1h-24z" fill="#232629" opacity=".4" />
+  <path d="m28 6h2v4h-2z" fill="#2ecc71" />
+  <path d="m28 12h2v4h-2z" fill="#2980b9" />
+  <path d="m28 18h2v4h-2z" fill="#da4453" />
+  <circle cx="6" cy="21" fill="#232629" r="1" />
+  <circle cx="6" cy="14" fill="#232629" r="1" />
+  <path d="m6.333 20h-2.333l4 4 1.333-1z" fill="url(#c)" opacity=".2" />
+  <path
+    d="m4.5 19a2.5 3 0 0 0 -2.5 3 2.5 3 0 0 0 2 2.938v-1.055a1.5 2 0 0 1 -1-1.883 1.5 2 0 0 1 1.5-2 1.5 2 0 0 1 1.5 2 1 1 0 0 0 .938-.654 2.5 3 0 0 0 -2.438-2.346z"
+    fill="#7f8c8d"
+  />
+  <path d="m6.333 13h-2.333l4 4 1.332-1z" fill="url(#d)" opacity=".2" />
+  <path
+    d="m4.5 12a2.5 3 0 0 0 -2.5 3 2.5 3 0 0 0 2 2.938v-1.055a1.5 2 0 0 1 -1-1.883 1.5 2 0 0 1 1.5-2 1.5 2 0 0 1 1.5 2 1 1 0 0 0 .938-.654 2.5 3 0 0 0 -2.438-2.346z"
+    fill="#7f8c8d"
+  />
+  <circle cx="6" cy="7" fill="#232629" r="1" />
+  <path d="m6.333 6h-2.333l4 4 1.332-1z" fill="url(#f)" opacity=".2" />
+  <path
+    d="m4.5 5a2.5 3 0 0 0 -2.5 3 2.5 3 0 0 0 2 2.938v-1.055a1.5 2 0 0 1 -1-1.883 1.5 2 0 0 1 1.5-2 1.5 2 0 0 1 1.5 2 1 1 0 0 0 .938-.654 2.5 3 0 0 0 -2.438-2.346z"
+    fill="#7f8c8d"
+  />
+  <path
+    d="m11 21 6 6h11v-11.2466l-7.8767-7.8767-2.1233-.8767-3 3 .8768 2.1232 2.1232 2.1232-3 .7536-3 3z"
+    fill="url(#a)"
+    opacity=".2"
+  />
+  <path
+    d="m18 7c-1.662 0-3 1.3379999-3 3 0 1.662 1.338 3 3 3s3-1.338 3-3c0-1.6620001-1.338-3-3-3zm0 1c1.108 0 2 .892 2 2s-.892 2-2 2-2-.892-2-2 .892-2 2-2zm0 6c-3.878 0-7 3.122-7 7h7 7c0-3.878-3.122-7-7-7zm-.04883 1a5.98 5.98 0 0 1 .04883 0 5.98 5.98 0 0 1 5.910156 5h-5.910156-5.910156a5.98 5.98 0 0 1 5.861328-5z"
+    fill="url(#b)"
+  />
+</svg>
