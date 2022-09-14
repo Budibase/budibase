46c1590c8385d5006670ae2dc93a9200d3c9a2bb
mike12345567 <me@michaeldrury.co.uk>
2022-09-14 17:35:46 +0100

    Adding types for plugin events, specifically for plugin init, import and deletion, this is across the CLI and the server.

--- packages/types/src/sdk/events/datasource.ts
+++ packages/types/src/sdk/events/datasource.ts
@@ -3,14 +3,17 @@ import { BaseEvent } from "./event"
 export interface DatasourceCreatedEvent extends BaseEvent {
   datasourceId: string
   source: string
+  custom: boolean
 }
 
 export interface DatasourceUpdatedEvent extends BaseEvent {
   datasourceId: string
   source: string
+  custom: boolean
 }
 
 export interface DatasourceDeletedEvent extends BaseEvent {
   datasourceId: string
   source: string
+  custom: boolean
 }
