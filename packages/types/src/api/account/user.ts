f72c53b461bc11c416f0fe06c3c121d12588529d
Rory Powell <rory.codes@gmail.com>
2022-09-14 11:32:11 +0100

    Remove active user pricing and custom invoicing

--- packages/types/src/api/account/user.ts
+++ packages/types/src/api/account/user.ts
@@ -1,12 +1,3 @@
-export interface PutAccountUser {
-  createdAt: number
-}
-
-export interface PutAccountUserResponse {
-  userId: string
-  createdAt: number
-}
-
 export interface PostAccountUserActivity {
   timestamp: number
 }
--- packages/types/src/documents/account/user.ts
+++ packages/types/src/documents/account/user.ts
@@ -1,14 +1,3 @@
-export interface SaveAccountUser {
-  accountId: string
-  userId: string
-  createdAt: number
-  deletedAt?: number
-}
-
-export interface AccountUser extends SaveAccountUser {
-  tenantId: string // for backwards compatibility with the accounts table. Really this is a USER#<id>
-}
-
 export interface CreateAccountUserActivity {
   accountId: string
   userId: string
