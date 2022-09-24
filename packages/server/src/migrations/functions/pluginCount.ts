e3f4e744abe4eec43476ac25617a6fbe48445e3f
Michael Drury <me@michaeldrury.co.uk>
2022-09-24 16:55:22 +0100

    PR comments.

--- packages/server/src/migrations/functions/pluginCount.ts
+++ packages/server/src/migrations/functions/pluginCount.ts
@@ -1,12 +1,7 @@
 import { tenancy, logging } from "@budibase/backend-core"
 import { plugins } from "@budibase/pro"
-import env from "../../environment"
 
 export const run = async () => {
-  // only a self hosted op
-  if (!env.SELF_HOSTED) {
-    return
-  }
   try {
     await tenancy.doInTenant(tenancy.DEFAULT_TENANT_ID, async () => {
       await plugins.checkPluginQuotas()
