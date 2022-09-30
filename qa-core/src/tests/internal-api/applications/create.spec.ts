c532c7388da668b792f8bf9c40011b3e145be14d
Martin McKeaveney <martinmckeaveney@gmail.com>
2022-09-30 15:03:38 +0100

    create app tests

--- qa-core/src/tests/internal-api/applications/create.spec.ts
+++ qa-core/src/tests/internal-api/applications/create.spec.ts
@@ -2,6 +2,7 @@ import TestConfiguration from "../../../config/internal-api/TestConfiguration"
 import { Application } from "@budibase/server/api/controllers/public/mapping/types"
 import InternalAPIClient from "../../../config/internal-api/TestConfiguration/InternalAPIClient"
 import generateApp from "../../../config/internal-api/fixtures/applications"
+import generator from "../../../config/generator"
 
 describe("Internal API - /applications endpoints", () => {
   const api = new InternalAPIClient()
@@ -36,5 +37,46 @@ describe("Internal API - /applications endpoints", () => {
     const [response, app] = await config.applications.create(generateApp())
     expect(response).toHaveStatusCode(200)
     expect(app._id).toBeDefined()
+  })
+
+  it("POST - Create an application from a template", async () => {
+    const appName = generator.word()
+    const [response, app] = await config.applications.create({
+      name: appName,
+      url: `/${generator.word()}`,
+      useTemplate: true,
+      templateName: "Car Rental Admin Panel",
+      templateKey: "app/car-rental-admin-panel",
+      templateFile: undefined
+    })
+    expect(response).toHaveStatusCode(200)
+    expect(app.appId).toBeDefined()
+    const [_, appPackage] = await config.applications.getAppPackage(app.appId as string)
+    expect(appPackage.application.appId).toBe(app.appId)
+    expect(appPackage.application.name).toBe(appName)
+  })
+
+  it("POST - Publish app from template", async () => {
+    const appUrl = `/${generator.word()}`
+    const [response, app] = await config.applications.create({
+      name: generator.word(),
+      url: appUrl,
+      useTemplate: true,
+      templateName: "Car Rental Admin Panel",
+      templateKey: "app/car-rental-admin-panel",
+      templateFile: undefined
+    })
+    expect(response).toHaveStatusCode(200)
+    expect(app.appId).toBeDefined()
+
+    config.applications.api.appId = app.appId
+
+    const [publishResponse, json] = await config.applications.publish()
+    expect(publishResponse).toHaveStatusCode(200)
+    expect(json).toEqual({
+      _id: expect.any(String),
+      appUrl,
+      status: "SUCCESS"
+    })
   })
 })
