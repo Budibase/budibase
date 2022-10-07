ed948e6ae7cc920982f83d78cc02a031e38732cd
Rory Powell <rory.codes@gmail.com>
2022-10-07 13:57:10 +0100

    Move trigger logic inside pro

--- packages/types/src/api/account/license.ts
+++ packages/types/src/api/account/license.ts
@@ -3,3 +3,9 @@ import { QuotaUsage } from "../../documents"
 export interface GetLicenseRequest {
   quotaUsage: QuotaUsage
 }
+
+export interface QuotaTriggeredRequest {
+  percentage: number
+  name: string
+  resetDate?: string
+}
