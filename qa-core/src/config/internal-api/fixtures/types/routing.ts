6e083c85a4c1e3e403850fab213f04a920f5e32f
Rory Powell <rory.codes@gmail.com>
2022-10-12 10:25:02 +0100

    Fix linting

--- qa-core/src/config/internal-api/fixtures/types/routing.ts
+++ qa-core/src/config/internal-api/fixtures/types/routing.ts
@@ -1,17 +1,17 @@
 export interface RouteConfig {
-    routes: Record<string, Route>
+  routes: Record<string, Route>
 }
 
 export interface Route {
-    subpaths: Record<string, Subpath>
+  subpaths: Record<string, Subpath>
 }
 
 export interface Subpath {
-    screens: ScreenRouteConfig
+  screens: ScreenRouteConfig
 }
 
 export interface ScreenRouteConfig {
-    BASIC?: string
-    POWER?: string
-    ADMIN?: string
+  BASIC?: string
+  POWER?: string
+  ADMIN?: string
 }
