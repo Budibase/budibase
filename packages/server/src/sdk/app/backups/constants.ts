a2d12ef0a6f948e68c9c69c6a6b5bfa4f07498ed
mike12345567 <me@michaeldrury.co.uk>
2022-10-12 17:34:17 +0100

    Adding first pass of attachments updating.

--- packages/server/src/sdk/app/backups/constants.ts
+++ packages/server/src/sdk/app/backups/constants.ts
@@ -1,4 +1,4 @@
-import { ATTACHMENT_DIR as attachmentDir } from "../../constants"
+import { ATTACHMENT_DIR as attachmentDir } from "../../../constants"
 export const DB_EXPORT_FILE = "db.txt"
 export const ATTACHMENT_DIR = attachmentDir
 export const GLOBAL_DB_EXPORT_FILE = "global.txt"
