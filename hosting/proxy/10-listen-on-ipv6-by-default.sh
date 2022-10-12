9b830c5b19417f2ed5e5359025d068ce788ca52d
Jonny McCullagh <jonny.mccullagh@redbranch.net>
2022-10-12 14:54:25 +0100

    remove ipv6 conf from nginx proxy if ipv6 not enabled

--- hosting/proxy/10-listen-on-ipv6-by-default.sh
+++ hosting/proxy/10-listen-on-ipv6-by-default.sh
@@ -8,7 +8,7 @@ NGINX_CONF_FILE="/etc/nginx/nginx.conf"
 DEFAULT_CONF_FILE="/etc/nginx/conf.d/default.conf"
 
 # check if we have ipv6 available
-if [ ! -f "/proc/net/if_jonnyinet6" ]; then
+if [ ! -f "/proc/net/if_inet6" ]; then
     # ipv6 not available so delete lines from nginx conf
     if [ -f "$NGINX_CONF_FILE" ]; then
         sed -i '/listen  \[::\]/d' $NGINX_CONF_FILE
