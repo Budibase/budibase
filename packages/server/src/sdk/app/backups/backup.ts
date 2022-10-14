1373630b33af7739e55c6e79fe424f580616e431
mike12345567 <me@michaeldrury.co.uk>
2022-10-14 20:10:44 +0100

    Minor fixes after testing manual backup system.

--- packages/server/src/sdk/app/backups/backup.ts
+++ packages/server/src/sdk/app/backups/backup.ts
@@ -10,30 +10,32 @@ export async function init() {
     const appId = job.data.appId,
       trigger = job.data.trigger,
       name = job.data.name
-    const createdAt = new Date().toISOString()
-    const tarPath = await exportApp(appId, { tar: true })
-    let filename = `${appId}/backup-${createdAt}.tar.gz`
-    // add the tenant to the bucket path if backing up within a multi-tenant environment
-    if (env.MULTI_TENANCY) {
-      const tenantId = tenancy.getTenantIDFromAppID(appId)
-      filename = `${tenantId}/${filename}`
-    }
-    const bucket = objectStore.ObjectStoreBuckets.BACKUPS
-    const metadata = {
-      appId,
-      createdAt,
-      trigger,
-      name,
-    }
-    await objectStore.upload({
-      path: tarPath,
-      type: "application/gzip",
-      bucket,
-      filename,
-      metadata,
+    const tenantId = tenancy.getTenantIDFromAppID(appId)
+    await tenancy.doInTenant(tenantId, async () => {
+      const createdAt = new Date().toISOString()
+      const tarPath = await exportApp(appId, { tar: true })
+      let filename = `${appId}/backup-${createdAt}.tar.gz`
+      // add the tenant to the bucket path if backing up within a multi-tenant environment
+      if (env.MULTI_TENANCY) {
+        filename = `${tenantId}/${filename}`
+      }
+      const bucket = objectStore.ObjectStoreBuckets.BACKUPS
+      const metadata = {
+        appId,
+        createdAt,
+        trigger,
+        name,
+      }
+      await objectStore.upload({
+        path: tarPath,
+        type: "application/gzip",
+        bucket,
+        filename,
+        metadata,
+      })
+      await backups.storeAppBackupMetadata(filename, metadata)
+      // clear up the tarball after uploading it
+      fs.rmSync(tarPath)
     })
-    await backups.storeAppBackupMetadata(filename, metadata)
-    // clear up the tarball after uploading it
-    fs.rmSync(tarPath)
   })
 }
