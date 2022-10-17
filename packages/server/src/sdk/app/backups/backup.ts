07d4d9ee4db8e0700d870b3c5b277e81d2c02a68
mike12345567 <me@michaeldrury.co.uk>
2022-10-17 19:42:36 +0100

    Reformatting types to allow queue to be used for import and export.

--- packages/server/src/sdk/app/backups/backup.ts
+++ packages/server/src/sdk/app/backups/backup.ts
@@ -5,37 +5,41 @@ import { Job } from "bull"
 import fs from "fs"
 import env from "../../../environment"
 
-export async function init() {
-  await backups.addAppBackupProcessor(async (job: Job) => {
-    const appId = job.data.appId,
-      trigger = job.data.trigger,
-      name = job.data.name
-    const tenantId = tenancy.getTenantIDFromAppID(appId)
-    await tenancy.doInTenant(tenantId, async () => {
-      const createdAt = new Date().toISOString()
-      const tarPath = await exportApp(appId, { tar: true })
-      let filename = `${appId}/backup-${createdAt}.tar.gz`
-      // add the tenant to the bucket path if backing up within a multi-tenant environment
-      if (env.MULTI_TENANCY) {
-        filename = `${tenantId}/${filename}`
-      }
-      const bucket = objectStore.ObjectStoreBuckets.BACKUPS
-      const metadata = {
-        appId,
-        createdAt,
-        trigger,
-        name,
-      }
-      await objectStore.upload({
-        path: tarPath,
-        type: "application/gzip",
-        bucket,
-        filename,
-        metadata,
-      })
-      await backups.storeAppBackupMetadata(filename, metadata)
-      // clear up the tarball after uploading it
-      fs.rmSync(tarPath)
+async function importProcessor(job: Job) {}
+
+async function exportProcessor(job: Job) {
+  const appId = job.data.appId,
+    trigger = job.data.trigger,
+    name = job.data.name
+  const tenantId = tenancy.getTenantIDFromAppID(appId)
+  await tenancy.doInTenant(tenantId, async () => {
+    const createdAt = new Date().toISOString()
+    const tarPath = await exportApp(appId, { tar: true })
+    let filename = `${appId}/backup-${createdAt}.tar.gz`
+    // add the tenant to the bucket path if backing up within a multi-tenant environment
+    if (env.MULTI_TENANCY) {
+      filename = `${tenantId}/${filename}`
+    }
+    const bucket = objectStore.ObjectStoreBuckets.BACKUPS
+    const metadata = {
+      appId,
+      createdAt,
+      trigger,
+      name,
+    }
+    await objectStore.upload({
+      path: tarPath,
+      type: "application/gzip",
+      bucket,
+      filename,
+      metadata,
     })
+    await backups.storeAppBackupMetadata(filename, metadata)
+    // clear up the tarball after uploading it
+    fs.rmSync(tarPath)
   })
+}
+
+export async function init() {
+  await backups.addAppBackupProcessors(importProcessor, exportProcessor)
 }
