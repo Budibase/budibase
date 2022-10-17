5ba7100e486829df3efc59245487fcaa4a3eb0fc
Pedro Silva <pedro@budibase.com>
2022-10-17 18:20:40 +0100

    Add Table endpoints

--- qa-core/src/config/internal-api/TestConfiguration/index.ts
+++ qa-core/src/config/internal-api/TestConfiguration/index.ts
@@ -1,14 +1,17 @@
 import ApplicationApi from "./applications"
 import AuthApi from "./auth"
 import InternalAPIClient from "./InternalAPIClient"
+import TablesApi from "./tables"
 
 export default class TestConfiguration<T> {
   applications: ApplicationApi
   auth: AuthApi
   context: T
+  tables: TablesApi
 
   constructor(apiClient: InternalAPIClient) {
     this.applications = new ApplicationApi(apiClient)
+    this.tables = new TablesApi(apiClient)
     this.auth = new AuthApi(apiClient)
     this.context = <T>{}
   }
