34754f47ce085afe9355675a26780c457d28699d
Peter Clement <peter@budibase.com>
2022-10-18 19:00:19 +0100

    add apis and svelte store

---
+++ packages/builder/src/stores/portal/backups.js
@@ -0,0 +1,40 @@
+import { writable } from "svelte/store"
+import { API } from "api"
+
+export function createBackupsStore() {
+  const { subscribe, set } = writable([])
+
+  async function load() {
+    set([
+      {
+        trigger: "PUBLISH",
+        name: "A Backup",
+        date: "1665407451",
+        userId: "Peter Clement",
+        contents: [
+          { datasources: ["datasource1", "datasource2"] },
+          { screens: ["screen1", "screen2"] },
+          { automations: ["automation1", "automation2"] },
+        ],
+      },
+    ])
+  }
+
+  async function searchBackups(appId) {
+    return API.searchBackups(appId)
+  }
+
+  async function createManualBackup(appId, name) {
+    let resp = API.createManualBackup(appId, name)
+    return resp
+  }
+
+  return {
+    subscribe,
+    load,
+    createManualBackup,
+    searchBackups,
+  }
+}
+
+export const backups = createBackupsStore()
