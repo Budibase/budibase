d138d5ebaa1bc81b3b7f65cbc053679b6ca41046
Peter Clement <peter@budibase.com>
2022-10-20 16:10:09 +0100

    add ability tofilter by range

--- packages/bbui/src/Form/DatePicker.svelte
+++ packages/bbui/src/Form/DatePicker.svelte
@@ -18,7 +18,13 @@
   const dispatch = createEventDispatcher()
 
   const onChange = e => {
-    value = e.detail
+    if (range) {
+      // Flatpickr cant take two dates and work out what to display, needs to be provided a string.
+      // Like - "Date1 to Date2". Hence passing in that specifically from the array
+      value = e.detail[1]
+    } else {
+      value = e.detail
+    }
     dispatch("change", e.detail)
   }
 </script>
