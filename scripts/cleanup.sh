012a007116f3fc3c524a54b212c930d3dd33ff8b
tomamplius <thomas@lgy.fr>
2022-10-21 21:09:03 +0200

    use egrep to replace grep + awk

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-KEEP="dist package.json yarn.lock client builder build pm2.config.js docker_run.sh"
-ls | grep -v $(echo ${KEEP} | awk '{split($0,a," ");for (i in a) printf "-e ^"a[i]"$ "}') | xargs rm -fr 
+KEEP="dist|package.json|yarn.lock|client|builder|build|pm2.config.js|docker_run.sh"
+echo "Removing unneeded build files:"
+ls | egrep -v $KEEP | xargs rm -rf
 NODE_ENV=production yarn
