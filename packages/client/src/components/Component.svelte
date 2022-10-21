67b6821b376b377bd76a77e729e351fced116030
Andrew Kingston <andrew@kingston.dev>
2022-10-21 16:52:46 +0100

    Fix components being selected when starting dragging

--- packages/client/src/components/Component.svelte
+++ packages/client/src/components/Component.svelte
@@ -446,7 +446,7 @@
   const scrollIntoView = () => {
     // Don't scroll into view if we selected this component because we were
     // starting dragging on it
-    if (get(builderStore).dragging) {
+    if (get(dndIsDragging)) {
       return
     }
     const node = document.getElementsByClassName(id)?.[0]?.children[0]
