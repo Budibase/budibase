0b7ca494183c0e7e4eb16c22005570d697a95467
Rory Powell <rory.codes@gmail.com>
2022-10-25 09:02:55 +0100

    Fix build failure around PouchDB type

--- qa-core/src/config/internal-api/TestConfiguration/rows.ts
+++ qa-core/src/config/internal-api/TestConfiguration/rows.ts
@@ -3,26 +3,26 @@ import { Row } from "@budibase/types"
 import InternalAPIClient from "./InternalAPIClient"
 
 export default class RowsApi {
-    api: InternalAPIClient
+  api: InternalAPIClient
 
-    constructor(apiClient: InternalAPIClient) {
-        this.api = apiClient
-    }
+  constructor(apiClient: InternalAPIClient) {
+    this.api = apiClient
+  }
 
-    async getAll(tableId: string): Promise<[Response, Row[]]> {
-        const response = await this.api.get(`/${tableId}/rows`)
-        const json = await response.json()
-        return [response, json]
-    }
-    async add(tableId: string, body: any): Promise<[Response, Row]> {
-        const response = await this.api.post(`/${tableId}/rows`, { body })
-        const json = await response.json()
-        return [response, json]
-    }
+  async getAll(tableId: string): Promise<[Response, Row[]]> {
+    const response = await this.api.get(`/${tableId}/rows`)
+    const json = await response.json()
+    return [response, json]
+  }
+  async add(tableId: string, body: any): Promise<[Response, Row]> {
+    const response = await this.api.post(`/${tableId}/rows`, { body })
+    const json = await response.json()
+    return [response, json]
+  }
 
-    async delete(tableId: string, body: any): Promise<[Response, Row[]]> {
-        const response = await this.api.del(`/${tableId}/rows/`, { body })
-        const json = await response.json()
-        return [response, json]
-    }
+  async delete(tableId: string, body: any): Promise<[Response, Row[]]> {
+    const response = await this.api.del(`/${tableId}/rows/`, { body })
+    const json = await response.json()
+    return [response, json]
+  }
 }
