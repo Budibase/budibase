80d4220cb5dccaab5b3b48faa13f26eeb15e6dae
mike12345567 <me@michaeldrury.co.uk>
2022-10-26 18:50:40 +0100

    Adding backup queue to bullboard if enabled.

--- packages/server/src/automations/bullboard.js
+++ packages/server/src/automations/bullboard.js
@@ -3,6 +3,7 @@ const { BullAdapter } = require("@bull-board/api/bullAdapter")
 const { KoaAdapter } = require("@bull-board/koa")
 const { queue } = require("@budibase/backend-core")
 const automation = require("../threads/automation")
+const { backups } = require("@budibase/pro")
 
 let automationQueue = queue.createQueue(
   queue.JobQueue.AUTOMATION,
@@ -11,9 +12,13 @@ let automationQueue = queue.createQueue(
 
 const PATH_PREFIX = "/bulladmin"
 
-exports.init = () => {
+exports.init = async () => {
   // Set up queues for bull board admin
+  const backupQueue = await backups.getBackupQueue()
   const queues = [automationQueue]
+  if (backupQueue) {
+    queues.push(backupQueue)
+  }
   const adapters = []
   const serverAdapter = new KoaAdapter()
   for (let queue of queues) {
