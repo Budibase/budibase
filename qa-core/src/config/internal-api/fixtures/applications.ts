4fde16d55b8c4b5acda9d594b24ccb8849606ae1
Pedro Silva <pedro@budibase.com>
2022-11-09 17:05:05 +0000

    Fix duplicated name failure

--- qa-core/src/config/internal-api/fixtures/applications.ts
+++ qa-core/src/config/internal-api/fixtures/applications.ts
@@ -1,11 +1,12 @@
 import generator from "../../generator"
 import { Application } from "@budibase/server/api/controllers/public/mapping/types"
 
+
 const generate = (
   overrides: Partial<Application> = {}
 ): Partial<Application> => ({
-  name: generator.word(),
-  url: `/${generator.word()}`,
+  name: generator.word() + generator.hash(),
+  url: `/${generator.word() + generator.hash()}`,
   ...overrides,
 })
 
