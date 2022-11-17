12a9ed6dcb7883a4aedeb80f7e77b5d90f071dfa
mike12345567 <me@michaeldrury.co.uk>
2022-11-17 23:09:49 +0000

    Adding type to triggered.

--- packages/types/src/sdk/events/backup.ts
+++ packages/types/src/sdk/events/backup.ts
@@ -1,14 +1,13 @@
 import { BaseEvent } from "./event"
-import { AppBackupTrigger } from "../../documents/app"
+import { AppBackupTrigger, AppBackupType } from "../../documents"
 
 export interface AppBackupRestoreEvent extends BaseEvent {
   appId: string
-  backupName: string
   backupCreatedAt: string
 }
 
 export interface AppBackupTriggeredEvent extends BaseEvent {
   appId: string
   trigger: AppBackupTrigger
-  backupName?: string
+  type: AppBackupType
 }
