f385d7a6a9d8fa9cac2bc9441666701d365be99b
Mel O'Hagan <mel@budibase.com>
2022-11-21 16:24:24 +0000

    Refactor

--- packages/builder/src/components/design/settings/controls/ColumnEditor/BasicColumnEditor.svelte
+++ packages/builder/src/components/design/settings/controls/ColumnEditor/BasicColumnEditor.svelte
@@ -1,13 +1,10 @@
 <script>
   import ColumnEditor from "./ColumnEditor.svelte"
-  import { createEventDispatcher } from "svelte"
-
-  const dispatch = createEventDispatcher()
 </script>
 
 <ColumnEditor
   {...$$props}
-  on:change={e => dispatch("change", e.detail)}
+  on:change
   allowCellEditing={false}
   subject="Dynamic Filter"
 />
