ae15690741481c1c438ec8642a244b63f3fe47d4
Andrew Kingston <andrew@kingston.dev>
2022-11-24 14:12:40 +0000

    Add WIP spreadsheet

---
+++ packages/client/src/components/app/spreadsheet/OptionsCell.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let value
+  export let schema
+
+  const colors = [
+    "rgb(207, 223, 255)",
+    "rgb(208, 240, 253)",
+    "rgb(194, 245, 233)",
+    "rgb(209, 247, 196)",
+    "rgb(255, 234, 182)",
+    "rgb(254, 226, 213)",
+    "rgb(255, 220, 229)",
+    "rgb(255, 218, 246)",
+    "rgb(237, 226, 254)",
+  ]
+
+  $: idx = schema?.constraints?.inclusion?.indexOf(value)
+  $: color = value && idx === -1 ? null : colors[idx % colors.length]
+</script>
+
+<div style="--color: {color}" class:valid={!!color}>
+  {value || ""}
+</div>
+
+<style>
+  div {
+    padding: 0 8px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  div.valid {
+    margin: 0 8px;
+    padding: 2px 8px;
+    background: var(--color);
+    border-radius: 8px;
+    color: #333;
+  }
+</style>
---
+++ packages/client/src/components/app/spreadsheet/TextCell.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let value
+</script>
+
+<div class="text-cell">
+  {value || ""}
+</div>
+
+<style>
+  .text-cell {
+    padding: 0 8px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+</style>
