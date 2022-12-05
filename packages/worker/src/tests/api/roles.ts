195121e71f9ad1160661271bc64be8197521b7c1
Peter Clement <peter@budibase.com>
2022-12-05 09:23:22 +0000

    more tests

--- packages/worker/src/tests/api/roles.ts
+++ packages/worker/src/tests/api/roles.ts
@@ -9,7 +9,24 @@ export class RolesAPI extends TestAPI {
   get = (opts?: TestAPIOpts) => {
     return this.request
       .get(`/api/global/roles`)
-      .set(opts?.headers ? opts.headers : this.config.defaultHeaders())
+      .set(this.config.defaultHeaders())
+      .expect("Content-Type", /json/)
+      .expect(opts?.status ? opts.status : 200)
+  }
+
+  find = (appId: string, opts?: TestAPIOpts) => {
+    return this.request
+      .get(`/api/global/roles/${appId}`)
+      .set(this.config.defaultHeaders())
+      .expect("Content-Type", /json/)
+      .expect(opts?.status ? opts.status : 200)
+  }
+
+  remove = (appId: string, opts?: TestAPIOpts) => {
+    return this.request
+      .delete(`/api/global/roles/${appId}`)
+      .set(this.config.defaultHeaders())
+      .expect("Content-Type", /json/)
       .expect(opts?.status ? opts.status : 200)
   }
 }
