efbc3f1e921969a0404e4ec1ca1f9cfd770c91ce
mike12345567 <me@michaeldrury.co.uk>
2022-12-13 17:14:08 +0000

    Updating dev compose to use test image.

--- hosting/tests/runner.sh
+++ hosting/tests/runner.sh
@@ -2,7 +2,7 @@
 
 redis-server --requirepass $REDIS_PASSWORD > /dev/stdout 2>&1 &
 /bbcouch-runner.sh &
-/minio/minio server ${DATA_DIR}/minio > /dev/stdout 2>&1 &
+/minio/minio server ${DATA_DIR}/minio --console-address ":9001" > /dev/stdout 2>&1 &
 
 echo "Test environment started..."
 sleep infinity
