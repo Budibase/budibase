f86d43a59eb7c4fadd2c0f5b3bae16ea9d182bc5
Rory Powell <rory.codes@gmail.com>
2022-12-16 12:51:16 +0000

    Environment variable type coercion fix

--- packages/server/src/environment.ts
+++ packages/server/src/environment.ts
@@ -108,12 +108,12 @@ const environment = {
 }
 
 // threading can cause memory issues with node-ts in development
-if (isDev() && module.exports.DISABLE_THREADING == null) {
+if (isDev() && environment.DISABLE_THREADING == null) {
   environment._set("DISABLE_THREADING", "1")
 }
 
 // clean up any environment variable edge cases
-for (let [key, value] of Object.entries(module.exports)) {
+for (let [key, value] of Object.entries(environment)) {
   // handle the edge case of "0" to disable an environment variable
   if (value === "0") {
     // @ts-ignore
