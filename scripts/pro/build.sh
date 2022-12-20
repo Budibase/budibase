1a0987caaff515b6f1b6ce81e42120555912ffe9
Rory Powell <rory.codes@gmail.com>
2022-12-20 12:51:54 +0000

    Only build pro after bootstrapping and after backend-core build
    This protects against the scenario of a breaking change in backend-core
    that is relied on by the pro package.
    
    Introducing to fix the current build as the published backend-core package is corrupted.
    This change ensures the local filsystem version of core will be used.

---
+++ scripts/pro/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is designed for building the pro repo after the backend-core has completed.
+# This ensures that any changes in core that are required by pro are done in the correct order.
+
+set -e
+
+if [[ -d "../../../budibase-pro" ]]; then
+  echo "Building pro"
+  cd "../../../budibase-pro"
+  yarn build
+  cd -
+fi
