7526bc83879b80cb96101dbd59e3d1c6667b196d
Andrew Kingston <andrew@kingston.dev>
2022-12-21 11:52:25 +0000

    Update user details page to use tables and match designs

---
+++ packages/builder/src/pages/builder/portal/users/users/_components/RemoveGroupTableRenderer.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { ActionButton } from "@budibase/bbui"
+  import { getContext } from "svelte"
+
+  export let value
+
+  const groupContext = getContext("groups")
+
+  const onClick = e => {
+    e.stopPropagation()
+    groupContext.removeGroup(value)
+  }
+</script>
+
+<ActionButton size="S" on:click={onClick}>Remove</ActionButton>
