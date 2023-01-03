c2dab0a60bccf6c6abfc1cabbcd0d542df1a0d8a
Dean <deanhannigan@gmail.com>
2023-01-03 16:04:11 +0000

    Refactored the create/edit column UI, fixed auto column validation issues and some other bugs

--- packages/builder/src/pages/builder/app/[application]/data/table/[selectedTable]/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/data/table/[selectedTable]/index.svelte
@@ -1,9 +1,48 @@
 <script>
   import TableDataTable from "components/backend/DataTable/DataTable.svelte"
   import { tables, database } from "stores/backend"
+  import { InlineAlert } from "@budibase/bbui"
+
+  const verifyAutocolumns = table => {
+    // Check for duplicates
+    return Object.values(table?.schema || {}).reduce((acc, fieldSchema) => {
+      console.log(fieldSchema)
+      if (!fieldSchema.autocolumn || !fieldSchema.subtype) {
+        return acc
+      }
+      let fieldKey = fieldSchema.tableId
+        ? `${fieldSchema.tableId}-${fieldSchema.subtype}`
+        : fieldSchema.subtype
+      acc[fieldKey] = acc[fieldKey] || []
+      acc[fieldKey].push(fieldSchema)
+      return acc
+    }, {})
+  }
+
+  $: autoColumnStatus = verifyAutocolumns($tables?.selected)
+  $: duplicates = Object.values(autoColumnStatus).reduce((acc, status) => {
+    if (status.length > 1) {
+      acc = [...acc, ...status]
+    }
+    return acc
+  }, [])
+  $: invalidColumnText = duplicates.map(entry => {
+    return `${entry.name} (${entry.subtype})`
+  })
 </script>
 
 {#if $database?._id && $tables?.selected?.name}
+  {#if duplicates?.length}
+    <div class="alert-wrap">
+      <InlineAlert
+        error
+        header="Schema Invalid"
+        message={`There are duplicate auto column types defined in this schema. Please delete the duplicate entries where appropriate: 
+      - ${invalidColumnText.join(", ")}
+      This validation will be enforced at a later date.`}
+      />
+    </div>
+  {/if}
   <TableDataTable />
 {:else}<i>Create your first table to start building</i>{/if}
 
@@ -12,5 +51,12 @@
     font-size: var(--font-size-m);
     color: var(--grey-5);
     margin-top: 2px;
+  }
+  .alert-wrap {
+    display: flex;
+    width: 100%;
+  }
+  .alert-wrap :global(> *) {
+    flex: 1;
   }
 </style>
