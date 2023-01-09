7378d21fea565c388759ba4964cfcf58039738b8
Peter Clement <peter@budibase.com>
2023-01-09 16:48:39 +0000

    environment variables ui boilerplate

---
+++ packages/builder/src/stores/portal/envVars.js
@@ -0,0 +1,20 @@
+import { writable } from "svelte/store"
+import { API } from "api"
+
+export function createEnvVarsStore() {
+    const { subscribe, set, update } = writable([])
+
+    async function load() {
+        const envVars = await API.fetchEnvVars()
+
+        let testVars = ['blah', 'blah123']
+        set(testVars)
+    }
+
+    return {
+        subscribe,
+        load,
+    }
+}
+
+export const envVars = createEnvVarsStore()
---
+++ packages/frontend-core/src/api/envVars.js
@@ -0,0 +1,11 @@
+export const buildEnvironmentVariableEndpoints = API => ({
+    /**
+     * Fetches a list of environment variables
+     */
+    fetchEnvVars: async () => {
+        return await API.get({
+            url: `/api/env/variables`,
+            json: false,
+        })
+    }
+})
