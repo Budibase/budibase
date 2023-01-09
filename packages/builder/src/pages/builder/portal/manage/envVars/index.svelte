715df0d9131efe5dc36edfcf05077eb08bbc182e
Peter Clement <peter@budibase.com>
2023-01-09 16:48:39 +0000

    environment variables ui boilerplate

---
+++ packages/builder/src/pages/builder/portal/manage/envVars/index.svelte
@@ -0,0 +1,39 @@
+<script>
+  import {
+    Layout,
+    Heading,
+    Body,
+    Button,
+    Select,
+    Divider,
+    Modal,
+    Search,
+  } from "@budibase/bbui"
+
+  import { onMount } from "svelte"
+
+  onMount(async () => {
+    await envVars.load()
+  })
+</script>
+
+<Layout noPadding>
+  <Layout gap="XS" noPadding>
+    <Heading size="M">Envrironment Variables</Heading>
+    <Body
+      >Add and manage environment variable for development and production</Body
+    >
+  </Layout>
+  <Divider size="S" />
+  <Layout noPadding>
+    {#each $envVars as envVar}
+      <Layout gap="XS" noPadding>
+        <Heading size="S">{envVar}</Heading>
+      </Layout>
+      <Divider size="S" />
+    {/each}
+  </Layout>
+</Layout>
+
+<style>
+</style>
