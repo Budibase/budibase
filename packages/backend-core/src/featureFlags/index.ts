49e30b1780f0dd6475c0687dea9dc6a46458e663
Gerard Burns <gjburns9@hotmail.com>
2023-01-13 17:03:14 +0000

    pr feedback

--- packages/backend-core/src/featureFlags/index.ts
+++ packages/backend-core/src/featureFlags/index.ts
@@ -1,7 +1,8 @@
 import env from "../environment"
 import * as tenancy from "../tenancy"
 
-/** * Read the TENANT_FEATURE_FLAGS env var and return an array of features flags for each tenant.
+/**
+ * Read the TENANT_FEATURE_FLAGS env var and return an array of features flags for each tenant.
  * The env var is formatted as:
  *  tenant1:feature1:feature2,tenant2:feature1
  */
@@ -35,7 +36,7 @@ export function isEnabled(featureFlag: string) {
 }
 
 export function getTenantFeatureFlags(tenantId: string) {
-  const flags = ["USER_GROUPS"]
+  const flags = []
 
   if (TENANT_FEATURE_FLAGS) {
     const globalFlags = TENANT_FEATURE_FLAGS["*"]
