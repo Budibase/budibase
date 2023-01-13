fc5f6ca53050ede5e7e46454808a8a00d884648f
mike12345567 <me@michaeldrury.co.uk>
2023-01-13 19:53:46 +0000

    Adding the ability to store environment variables to context, to make them more easily available/re-usable, as well as exposing them to queries.

--- packages/server/src/sdk/index.ts
+++ packages/server/src/sdk/index.ts
@@ -3,6 +3,7 @@ import { default as tables } from "./app/tables"
 import { default as automations } from "./app/automations"
 import { default as applications } from "./app/applications"
 import { default as datasources } from "./app/datasources"
+import { default as queries } from "./app/queries"
 import { default as rows } from "./app/rows"
 import { default as users } from "./users"
 
@@ -14,6 +15,7 @@ const sdk = {
   rows,
   users,
   datasources,
+  queries,
 }
 
 // default export for TS
