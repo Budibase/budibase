029453eab538b890ffad3a918328c8e1ea35140a
Adria Navarro <adria@revityapp.com>
2023-01-18 16:46:40 +0000

    Retrieve row test

--- packages/server/src/integration-test/row.spec.ts
+++ packages/server/src/integration-test/row.spec.ts
@@ -5,10 +5,13 @@ import {
 } from "../api/routes/public/tests/utils"
 
 import * as setup from "../api/routes/tests/utilities"
-import { Datasource, FieldType, SourceName } from "@budibase/types"
+import { Datasource, FieldType, SourceName, Table } from "@budibase/types"
 
 const config = setup.getConfig()
-let apiKey, makeRequest: MakeRequestResponse, postgresDatasource: Datasource
+let apiKey,
+  makeRequest: MakeRequestResponse,
+  postgresDatasource: Datasource,
+  postgresTable: Table
 
 beforeEach(async () => {
   await config.init()
@@ -31,50 +34,56 @@ beforeEach(async () => {
   })
 
   makeRequest = generateMakeRequest(apiKey)
+
+  postgresTable = await config.createTable({
+    name: faker.lorem.word(),
+    schema: {
+      name: {
+        name: "name",
+        type: FieldType.STRING,
+        constraints: {
+          presence: true,
+        },
+      },
+      description: {
+        name: "description",
+        type: FieldType.STRING,
+      },
+      value: {
+        name: "value",
+        type: FieldType.NUMBER,
+      },
+    },
+    sourceId: postgresDatasource._id,
+  })
 })
 
 afterAll(async () => {
   await config.end()
 })
 
+function createRandomRow() {
+  return {
+    name: faker.name.fullName(),
+    description: faker.lorem.paragraphs(),
+    value: +faker.random.numeric(),
+  }
+}
+
 describe("row api", () => {
   describe("create a row", () => {
     test("Given than no row exists, adding a new rows persists it", async () => {
-      const tableName = faker.lorem.word()
+      const newRow = createRandomRow()
 
-      const table = await config.createTable({
-        name: tableName,
-        schema: {
-          name: {
-            name: "name",
-            type: FieldType.STRING,
-            constraints: {
-              presence: true,
-            },
-          },
-          description: {
-            name: "description",
-            type: FieldType.STRING,
-          },
-          value: {
-            name: "value",
-            type: FieldType.NUMBER,
-          },
-        },
-        sourceId: postgresDatasource._id,
-      })
-
-      const newRow = {
-        name: faker.name.fullName(),
-        description: faker.lorem.paragraphs(),
-        value: +faker.random.numeric(),
-      }
-
-      const res = await makeRequest("post", `/tables/${table._id}/rows`, newRow)
+      const res = await makeRequest(
+        "post",
+        `/tables/${postgresTable._id}/rows`,
+        newRow
+      )
 
       expect(res.status).toBe(200)
 
-      const persistedRows = await config.getRows(table._id!)
+      const persistedRows = await config.getRows(postgresTable._id!)
       expect(persistedRows).toHaveLength(1)
       expect(persistedRows).toEqual([
         expect.objectContaining({
@@ -85,51 +94,39 @@ describe("row api", () => {
     })
 
     test("Given than no row exists, multiple rows can be persisted", async () => {
-      const tableName = faker.lorem.word()
-
-      const table = await config.createTable({
-        name: tableName,
-        schema: {
-          name: {
-            name: "name",
-            type: FieldType.STRING,
-            constraints: {
-              presence: true,
-            },
-          },
-          description: {
-            name: "description",
-            type: FieldType.STRING,
-          },
-          value: {
-            name: "value",
-            type: FieldType.NUMBER,
-          },
-        },
-        sourceId: postgresDatasource._id,
-      })
-
       const numberOfRows = 10
-      const newRows = Array(numberOfRows).fill({
-        name: faker.name.fullName(),
-        description: faker.lorem.paragraphs(),
-        value: +faker.random.numeric(),
-      })
+      const newRows = Array(numberOfRows).fill(createRandomRow())
 
       for (const newRow of newRows) {
         const res = await makeRequest(
           "post",
-          `/tables/${table._id}/rows`,
+          `/tables/${postgresTable._id}/rows`,
           newRow
         )
         expect(res.status).toBe(200)
       }
 
-      const persistedRows = await config.getRows(table._id!)
+      const persistedRows = await config.getRows(postgresTable._id!)
       expect(persistedRows).toHaveLength(numberOfRows)
       expect(persistedRows).toEqual(
         expect.arrayContaining(newRows.map(expect.objectContaining))
       )
+    })
+  })
+
+  describe("Retrieve a row", () => {
+    test("Given than a table have a single row, the row can be retrieved successfully", async () => {
+      const rowData = createRandomRow()
+      const row = await config.createRow(rowData)
+
+      const res = await makeRequest(
+        "get",
+        `/tables/${postgresTable._id}/rows/${row._id}`
+      )
+
+      expect(res.status).toBe(200)
+
+      expect(res.body.data).toEqual(expect.objectContaining(rowData))
     })
   })
 })
