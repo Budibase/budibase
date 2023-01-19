2a2466fbda9d8088624b2c12c603fe7764b9a784
Andrew Kingston <andrew@kingston.dev>
2023-01-19 16:09:40 +0000

    More fixes (#9387)
    * Mark filters as nested settings to support persisting bindings post block ejection
    
    * Add support for searching on linked fields in blocks
    
    * Fix multi-select pickers closing on every click
    
    * Target spectrum root element by default when rendering popovers, and allow customisation via context for client library
    
    * Don't send up invalid HBS expressions when filtering on dates in blocks with empty date value
    
    * Move profile above theme in user dropdown

--- packages/bbui/src/Form/Core/Picker.svelte
+++ packages/bbui/src/Form/Core/Picker.svelte
@@ -45,7 +45,9 @@
     getOptionLabel
   )
 
-  const onClick = () => {
+  const onClick = e => {
+    e.preventDefault()
+    e.stopPropagation()
     dispatch("click")
     if (readonly) {
       return
@@ -88,7 +90,6 @@
   class:is-open={open}
   aria-haspopup="listbox"
   on:click={onClick}
-  use:clickOutside={() => (open = false)}
   bind:this={button}
 >
   {#if fieldIcon}
@@ -130,14 +131,17 @@
 <Popover
   anchor={button}
   align="left"
-  portalTarget={document.documentElement}
   bind:this={popover}
   {open}
   on:close={() => (open = false)}
   useAnchorWidth={!autoWidth}
   maxWidth={autoWidth ? 400 : null}
 >
-  <div class="popover-content" class:auto-width={autoWidth}>
+  <div
+    class="popover-content"
+    class:auto-width={autoWidth}
+    use:clickOutside={() => (open = false)}
+  >
     {#if autocomplete}
       <Search
         value={searchTerm}
