ab084d54c981d8184595dde7968d99176cba2bc6
Gerard Burns <gjburns9@hotmail.com>
2023-01-19 17:31:30 +0000

    New Help Menu

--- packages/builder/src/App.svelte
+++ packages/builder/src/App.svelte
@@ -3,7 +3,7 @@
   import { routes } from "../.routify/routes"
   import { NotificationDisplay, BannerDisplay } from "@budibase/bbui"
   import { parse, stringify } from "qs"
-  import HelpIcon from "components/common/HelpIcon.svelte"
+  import HelpMenu from "components/common/HelpMenu.svelte"
   import LicensingOverlays from "components/portal/licensing/LicensingOverlays.svelte"
 
   const queryHandler = { parse, stringify }
@@ -15,7 +15,7 @@
 <LicensingOverlays />
 <Router {routes} config={{ queryHandler }} />
 <div class="modal-container" />
-<HelpIcon />
+<HelpMenu />
 
 <style>
   .modal-container {
---
+++ packages/builder/src/components/common/FontAwesomeIcon.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  import { dom, library } from "@fortawesome/fontawesome-svg-core"
+  import {
+    faEnvelope,
+    faXmark,
+    faBook,
+    faPlay,
+    faLock,
+  } from "@fortawesome/free-solid-svg-icons"
+  import { faGithub, faDiscord } from "@fortawesome/free-brands-svg-icons"
+
+  library.add(faXmark, faBook, faPlay, faLock, faGithub, faDiscord, faEnvelope)
+  dom.watch()
+</script>
+
+<script>
+  export let name
+</script>
+
+<i class={name} />
