f696b5f021876d40a9f46d63c393c6de466a2d2f
Andrew Kingston <andrew@kingston.dev>
2023-01-20 08:58:47 +0000

    Revert changes to builder files for testing

--- packages/builder/src/pages/builder/auth/_components/GoogleButton.svelte
+++ packages/builder/src/pages/builder/auth/_components/GoogleButton.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { FancyButton } from "@budibase/bbui"
+  import { ActionButton } from "@budibase/bbui"
   import GoogleLogo from "assets/google-logo.png"
   import { auth, organisation } from "stores/portal"
 
@@ -10,12 +10,31 @@
 </script>
 
 {#if show}
-  <FancyButton
-    on:click={() => {
-      window.open(`/api/global/auth/${tenantId}/google`, "_blank")
-    }}
-    icon={GoogleLogo}
+  <ActionButton
+    on:click={() =>
+      window.open(`/api/global/auth/${tenantId}/google`, "_blank")}
   >
-    Log in with Google
-  </FancyButton>
+    <div class="inner">
+      <img src={GoogleLogo} alt="google icon" />
+      <p>Sign in with Google</p>
+    </div>
+  </ActionButton>
 {/if}
+
+<style>
+  .inner {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding-top: var(--spacing-xs);
+    padding-bottom: var(--spacing-xs);
+  }
+  .inner img {
+    width: 18px;
+    margin: 3px 10px 3px 3px;
+  }
+  .inner p {
+    margin: 0;
+  }
+</style>
