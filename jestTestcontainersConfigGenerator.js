53fa5d99b261408724895832f762da3019553412
Adria Navarro <adria@revityapp.com>
2023-01-24 11:02:58 +0000

    Use docker-compose dev for dev testing

--- jestTestcontainersConfigGenerator.js
+++ jestTestcontainersConfigGenerator.js
@@ -1,12 +1,26 @@
-module.exports = env => ({
-  devEnv: {
-    image: "budibase/dependencies",
-    tag: "latest",
-    ports: [6379, 5984, 9000],
-    env,
-    wait: {
-      type: "text",
-      text: "Test environment started...",
-    },
-  },
-})
+
+
+module.exports = dependenciesEnv => {
+  if (process.env.DEV_TOOLS) {
+    return {
+      dockerCompose: {
+        composeFilePath: `${__dirname}/hosting`,
+        composeFile: 'docker-compose.dev.yaml',
+        startupTimeout: 10000,
+      }
+    }
+  }
+
+  return {
+    devEnv: {
+      image: "budibase/dependencies",
+      tag: "latest",
+      ports: [6379, 5984, 9000],
+      env: dependenciesEnv,
+      wait: {
+        type: "text",
+        text: "Test environment started...",
+      },
+    }
+  }
+}
--- packages/server/jest-testcontainers-config.js
+++ packages/server/jest-testcontainers-config.js
@@ -1,6 +1,6 @@
 const { join } = require("path")
 const { parsed: env } = require("dotenv").config({
-  path: join(__dirname, ".env.test"),
+  path: join(__dirname, "..", "..", "hosting", ".env"),
 })
 
 const jestTestcontainersConfigGenerator = require("../../jestTestcontainersConfigGenerator")
