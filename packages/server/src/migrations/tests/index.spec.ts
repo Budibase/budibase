9bcb33285cdc33e14e23e6f73ba1e0e01ed97eea
adrinr <adria@budibase.com>
2023-01-25 11:51:08 +0000

    Fix timespan issues

--- packages/server/src/migrations/tests/index.spec.ts
+++ packages/server/src/migrations/tests/index.spec.ts
@@ -10,8 +10,9 @@ import * as structures from "../../tests/utilities/structures"
 import { MIGRATIONS } from "../"
 import * as helpers from "./helpers"
 
-const { mocks } = require("@budibase/backend-core/tests")
-const timestamp = mocks.date.MOCK_DATE.toISOString()
+import tk from "timekeeper"
+const timestamp = new Date().toISOString()
+tk.freeze(timestamp)
 
 const clearMigrations = async () => {
   const dbs = [context.getDevAppDB(), context.getProdAppDB()]
@@ -23,6 +24,10 @@ const clearMigrations = async () => {
 }
 
 jest.setTimeout(10000)
+
+afterAll(() => {
+  tk.reset()
+})
 
 describe("migrations", () => {
   const config = new TestConfig()
