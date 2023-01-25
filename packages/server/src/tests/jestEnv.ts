067462f458199b859e470e4f075c8ee2d90eee32
Adria Navarro Redo <adria@budibase.com>
2023-01-25 17:11:37 +0000

    Use multitenancy for tests

--- packages/server/src/tests/jestEnv.ts
+++ packages/server/src/tests/jestEnv.ts
@@ -3,6 +3,7 @@ import { tmpdir } from "os"
 
 env._set("SELF_HOSTED", "1")
 env._set("NODE_ENV", "jest")
+env._set("MULTI_TENANCY", "1")
 // @ts-ignore
 env._set("BUDIBASE_DIR", tmpdir("budibase-unittests"))
 env._set("LOG_LEVEL", "silent")
