33f162800001fd9d14bb921488ebc322ee30110c
Adria Navarro Redo <adria@budibase.com>
2023-01-26 14:18:56 +0000

    Fix issues with parallel tests and self/cloud modes

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -1,8 +1,8 @@
-import { Config } from "@jest/types"
+import { Config } from "jest"
 import * as fs from "fs"
 const preset = require("ts-jest/jest-preset")
 
-const config: Config.InitialOptions = {
+const configSettings = {
   ...preset,
   preset: "@trendyol/jest-testcontainers",
   setupFiles: ["./src/tests/jestEnv.ts"],
@@ -20,18 +20,34 @@ const config: Config.InitialOptions = {
 
 if (!process.env.CI) {
   // use sources when not in CI
-  config.moduleNameMapper = {
+  configSettings.moduleNameMapper = {
     "@budibase/backend-core/(.*)": "<rootDir>/../backend-core/$1",
     "@budibase/backend-core": "<rootDir>/../backend-core/src",
     "@budibase/types": "<rootDir>/../types/src",
   }
   // add pro sources if they exist
   if (fs.existsSync("../../../budibase-pro")) {
-    config.moduleNameMapper["@budibase/pro"] =
+    configSettings.moduleNameMapper["@budibase/pro"] =
       "<rootDir>/../../../budibase-pro/packages/pro/src"
   }
 } else {
   console.log("Running tests with compiled dependency sources")
+}
+
+const config: Config = {
+  projects: [
+    {
+      ...configSettings,
+      displayName: "sequential test",
+      testMatch: ["<rootDir>/**/*.seq.spec.[jt]s"],
+      runner: "jest-serial-runner",
+    },
+    {
+      ...configSettings,
+      displayName: "parallel test",
+      testMatch: ["<rootDir>/**/!(*.seq).spec.[jt]s"],
+    },
+  ],
 }
 
 export default config
