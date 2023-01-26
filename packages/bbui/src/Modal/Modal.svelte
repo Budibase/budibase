ff654e1d25213b6c544d73d248260418a56af1bd
Andrew Kingston <andrew@kingston.dev>
2023-01-26 18:14:26 +0000

    Fix modals not closing in design section when escape is pressed

--- packages/bbui/src/Modal/Modal.svelte
+++ packages/bbui/src/Modal/Modal.svelte
@@ -1,7 +1,7 @@
 <script>
   import "@spectrum-css/modal/dist/index-vars.css"
   import "@spectrum-css/underlay/dist/index-vars.css"
-  import { createEventDispatcher, setContext, tick } from "svelte"
+  import { createEventDispatcher, setContext, tick, onMount } from "svelte"
   import { fade, fly } from "svelte/transition"
   import Portal from "svelte-portal"
   import Context from "../context"
@@ -38,6 +38,7 @@
   }
 
   function handleKey(e) {
+    console.log("MODAL", e.key)
     if (visible && e.key === "Escape") {
       cancel()
     }
@@ -62,9 +63,14 @@
   }
 
   setContext(Context.Modal, { show, hide, cancel })
-</script>
 
-<svelte:window on:keydown={handleKey} />
+  onMount(() => {
+    document.addEventListener("keydown", handleKey)
+    return () => {
+      document.removeEventListener("keydown", handleKey)
+    }
+  })
+</script>
 
 {#if inline}
   {#if visible}
