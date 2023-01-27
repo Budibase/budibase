722267e588400c9acaf59b7e8be14f0ddb724e16
adrinr <adria@budibase.com>
2023-01-27 12:36:52 +0000

    Add module name mapper back

--- packages/worker/jest.config.ts
+++ packages/worker/jest.config.ts
@@ -1,10 +1,7 @@
 import { Config } from "@jest/types"
 import * as fs from "fs"
-const preset = require("ts-jest/jest-preset")
 
 const config: Config.InitialOptions = {
-  ...preset,
-  preset: "@trendyol/jest-testcontainers",
   testEnvironment: "node",
   setupFiles: ["./src/tests/jestEnv.ts"],
   setupFilesAfterEnv: ["./src/tests/jestSetup.ts"],
@@ -13,6 +10,24 @@ const config: Config.InitialOptions = {
   transform: {
     "^.+\\.ts?$": "@swc/jest",
   },
+}
+
+if (!process.env.CI) {
+  // use sources when not in CI
+  config.moduleNameMapper = {
+    "@budibase/backend-core/(.*)": "<rootDir>/../backend-core/$1",
+    "@budibase/backend-core": "<rootDir>/../backend-core/src",
+    "@budibase/types": "<rootDir>/../types/src",
+  }
+  // add pro sources if they exist
+  if (fs.existsSync("../../../budibase-pro")) {
+    config.moduleNameMapper["@budibase/pro/(.*)"] =
+      "<rootDir>/../../../budibase-pro/packages/pro/$1"
+    config.moduleNameMapper["@budibase/pro"] =
+      "<rootDir>/../../../budibase-pro/packages/pro/src"
+  }
+} else {
+  console.log("Running tests with compiled dependency sources")
 }
 
 export default config
