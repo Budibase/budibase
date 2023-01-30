08a2e77871d66d7db2fb1f0e8e1e9ec0a5a4a0fd
adrinr <adria@budibase.com>
2023-01-30 18:18:23 +0000

    Fix jest.setup

--- packages/backend-core/tests/jestSetup.ts
+++ packages/backend-core/tests/jestSetup.ts
@@ -2,13 +2,15 @@ import env from "../src/environment"
 
 const globalSafe = global as any
 
-env._set("COUCH_DB_PORT", globalSafe.__TESTCONTAINERS_DEVENV_PORT_5984__)
+env._set(
+  "COUCH_DB_PORT",
+  globalSafe["__TESTCONTAINERS_COUCHDB-SERVICE-1_PORT_5984__"]
+)
 env._set(
   "COUCH_DB_URL",
-  `http://${globalSafe.__TESTCONTAINERS_DEVENV_IP__}:${globalSafe.__TESTCONTAINERS_DEVENV_PORT_5984__}`
+  `http://${globalSafe["__TESTCONTAINERS_COUCHDB-SERVICE-1_IP__"]}:${globalSafe["__TESTCONTAINERS_COUCHDB-SERVICE-1_PORT_5984__"]}`
 )
-
 env._set(
   "MINIO_URL",
-  `http://${globalSafe.__TESTCONTAINERS_DEVENV_IP__}:${globalSafe.__TESTCONTAINERS_DEVENV_PORT_9000__}`
+  `http://${globalSafe["__TESTCONTAINERS_MINIO-SERVICE-1_IP__"]}:${globalSafe["__TESTCONTAINERS_MINIO-SERVICE-1_PORT_9000__"]}`
 )
