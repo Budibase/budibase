1dabf71f687f1f1f64421f9389365d33b090be8f
adrinr <adria@budibase.com>
2023-01-30 13:35:09 +0000

    Run conflicting tests in sequential mode

--- packages/backend-core/jest.config.ts
+++ packages/backend-core/jest.config.ts
@@ -1,7 +1,7 @@
-import { Config } from "@jest/types"
+import { Config } from "jest"
 const preset = require("ts-jest/jest-preset")
 
-const config: Config.InitialOptions = {
+const configSettings = {
   ...preset,
   preset: "@trendyol/jest-testcontainers",
   setupFiles: ["./tests/jestEnv.ts"],
@@ -15,11 +15,26 @@ const config: Config.InitialOptions = {
 
 if (!process.env.CI) {
   // use sources when not in CI
-  config.moduleNameMapper = {
+  configSettings.moduleNameMapper = {
     "@budibase/types": "<rootDir>/../types/src",
   }
 } else {
   console.log("Running tests with compiled dependency sources")
+}
+
+const config: Config = {
+  projects: [
+    {
+      ...configSettings,
+      displayName: "sequential test",
+      testMatch: ["<rootDir>/**/*.seq.spec.[jt]s"],
+      runner: "jest-serial-runner",
+    },
+    {
+      ...configSettings,
+      testMatch: ["<rootDir>/**/!(*.seq).spec.[jt]s"],
+    },
+  ],
 }
 
 export default config
