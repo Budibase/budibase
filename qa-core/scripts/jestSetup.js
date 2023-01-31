71db2420b5ce4686ac485882429450fccf02d21c
Pedro Silva <pedro@budibase.com>
2023-01-31 17:40:00 +0000

    Remove console log

--- qa-core/scripts/jestSetup.js
+++ qa-core/scripts/jestSetup.js
@@ -12,10 +12,8 @@ const MOCK_DATE = new Date("2020-01-01T00:00:00.000Z")
 const tk = require("timekeeper")
 tk.freeze(MOCK_DATE)
 
-/*
 if (!process.env.DEBUG) {
   global.console.log = jest.fn() // console.log are ignored in tests
 }
-*/
 
 jest.setTimeout(10000)
