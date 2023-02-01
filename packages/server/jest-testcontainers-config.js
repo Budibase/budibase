83d06121d78f9ea1fff1f6713a3f0ebc36b6cfc2
adrinr <adria@budibase.com>
2023-02-01 11:49:25 +0000

    Remove redundant env from jestTestcontainersConfigGenerator

--- packages/server/jest-testcontainers-config.js
+++ packages/server/jest-testcontainers-config.js
@@ -1,8 +1,8 @@
 const { join } = require("path")
-const { parsed: env } = require("dotenv").config({
+require("dotenv").config({
   path: join(__dirname, "..", "..", "hosting", ".env"),
 })
 
 const jestTestcontainersConfigGenerator = require("../../jestTestcontainersConfigGenerator")
 
-module.exports = jestTestcontainersConfigGenerator(env)
+module.exports = jestTestcontainersConfigGenerator()
