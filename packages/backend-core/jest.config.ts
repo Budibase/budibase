9b3cd812b7ea9f0ecd28670079ece4ea33d26ac9
adrinr <adria@budibase.com>
2023-02-01 10:57:05 +0000

    Fix coverage conflicts issues

--- packages/backend-core/jest.config.ts
+++ packages/backend-core/jest.config.ts
@@ -1,40 +1,40 @@
 import { Config } from "jest"
 const preset = require("ts-jest/jest-preset")
 
-const configSettings = {
+const testContainersSettings = {
   ...preset,
   preset: "@trendyol/jest-testcontainers",
   setupFiles: ["./tests/jestEnv.ts"],
   setupFilesAfterEnv: ["./tests/jestSetup.ts"],
-  collectCoverageFrom: ["src/**/*.{js,ts}"],
-  coverageReporters: ["lcov", "json", "clover"],
   transform: {
     "^.+\\.ts?$": "@swc/jest",
   },
 }
 
-if (!process.env.CI) {
-  // use sources when not in CI
-  configSettings.moduleNameMapper = {
-    "@budibase/types": "<rootDir>/../types/src",
-  }
-} else {
-  console.log("Running tests with compiled dependency sources")
-}
-
 const config: Config = {
   projects: [
     {
-      ...configSettings,
+      ...testContainersSettings,
       displayName: "sequential test",
       testMatch: ["<rootDir>/**/*.seq.spec.[jt]s"],
       runner: "jest-serial-runner",
     },
     {
-      ...configSettings,
+      ...testContainersSettings,
       testMatch: ["<rootDir>/**/!(*.seq).spec.[jt]s"],
     },
   ],
+  collectCoverageFrom: ["src/**/*.{js,ts}"],
+  coverageReporters: ["lcov", "json", "clover"],
+}
+
+if (!process.env.CI) {
+  // use sources when not in CI
+  config.moduleNameMapper = {
+    "@budibase/types": "<rootDir>/../types/src",
+  }
+} else {
+  console.log("Running tests with compiled dependency sources")
 }
 
 export default config
--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -1,12 +1,28 @@
 import { Config } from "jest"
+
 import * as fs from "fs"
 const preset = require("ts-jest/jest-preset")
 
-const configSettings = {
+const testContainersSettings = {
   ...preset,
   preset: "@trendyol/jest-testcontainers",
   setupFiles: ["./src/tests/jestEnv.ts"],
   setupFilesAfterEnv: ["./src/tests/jestSetup.ts"],
+}
+
+const config: Config = {
+  projects: [
+    {
+      ...testContainersSettings,
+      displayName: "sequential test",
+      testMatch: ["<rootDir>/**/*.seq.spec.[jt]s"],
+      runner: "jest-serial-runner",
+    },
+    {
+      ...testContainersSettings,
+      testMatch: ["<rootDir>/**/!(*.seq).spec.[jt]s"],
+    },
+  ],
   collectCoverageFrom: [
     "src/**/*.{js,ts}",
     // The use of coverage with couchdb view functions breaks tests
@@ -20,33 +36,18 @@ const configSettings = {
 
 if (!process.env.CI) {
   // use sources when not in CI
-  configSettings.moduleNameMapper = {
+  config.moduleNameMapper = {
     "@budibase/backend-core/(.*)": "<rootDir>/../backend-core/$1",
     "@budibase/backend-core": "<rootDir>/../backend-core/src",
     "@budibase/types": "<rootDir>/../types/src",
   }
   // add pro sources if they exist
   if (fs.existsSync("../../../budibase-pro")) {
-    configSettings.moduleNameMapper["@budibase/pro"] =
+    config.moduleNameMapper["@budibase/pro"] =
       "<rootDir>/../../../budibase-pro/packages/pro/src"
   }
 } else {
   console.log("Running tests with compiled dependency sources")
-}
-
-const config: Config = {
-  projects: [
-    {
-      ...configSettings,
-      displayName: "sequential test",
-      testMatch: ["<rootDir>/**/*.seq.spec.[jt]s"],
-      runner: "jest-serial-runner",
-    },
-    {
-      ...configSettings,
-      testMatch: ["<rootDir>/**/!(*.seq).spec.[jt]s"],
-    },
-  ],
 }
 
 export default config
