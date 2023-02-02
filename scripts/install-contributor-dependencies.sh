4b9d1780866b772b0ecd4fd069dc86b108e2cb0f
adrinr <adria@budibase.com>
2023-02-02 12:36:25 +0000

    Add setup in md

---
+++ scripts/install-contributor-dependencies.sh
@@ -0,0 +1,23 @@
+# Install brew
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install and setup asdf
+if ! command -v asdf &> /dev/null
+then
+    brew install asdf
+
+    if test -f ~/.bashrc; then
+        echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.bashrc
+    fi
+
+    if test -f ~/.zshrc; then
+        echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
+    fi
+fi
+
+# Install ASDF Plugins
+asdf plugin add nodejs
+asdf plugin add python
