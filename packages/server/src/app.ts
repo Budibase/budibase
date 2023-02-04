97035642838a4f6b57b18083f137905f757b92ea
Adria Navarro <adria@revityapp.com>
2023-02-04 20:30:12 +0000

    Add onClose back in tests

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -63,22 +63,22 @@ initialiseWebsockets(server)
 let shuttingDown = false,
   errCode = 0
 
-if (!env.isTest()) {
-  server.on("close", async () => {
-    // already in process
-    if (shuttingDown) {
-      return
-    }
-    shuttingDown = true
-    console.log("Server Closed")
-    await automations.shutdown()
-    await redis.shutdown()
-    await events.shutdown()
-    await Thread.shutdown()
-    api.shutdown()
+server.on("close", async () => {
+  // already in process
+  if (shuttingDown) {
+    return
+  }
+  shuttingDown = true
+  console.log("Server Closed")
+  await automations.shutdown()
+  await redis.shutdown()
+  events.shutdown()
+  await Thread.shutdown()
+  api.shutdown()
+  if (!env.isTest()) {
     process.exit(errCode)
-  })
-}
+  }
+})
 
 export default server.listen(env.PORT || 0, async () => {
   await startup(app, server)
