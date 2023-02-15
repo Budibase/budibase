1ed4f4b8feea5d2d06b719a97b81d2db9895fa17
mike12345567 <me@michaeldrury.co.uk>
2023-02-15 12:37:32 +0000

    Update on audit logs, getting the full write flow from events configured.

--- packages/types/src/documents/global/auditLogs.ts
+++ packages/types/src/documents/global/auditLogs.ts
@@ -1,6 +1,8 @@
 import { Document } from "../document"
 import { Event } from "../../sdk"
 
+export const AuditLogSystemUser = "SYSTEM"
+
 export interface AuditLogDoc extends Document {
   appId?: string
   event: Event
