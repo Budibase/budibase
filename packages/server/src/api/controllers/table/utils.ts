654c348e4e75bff6098542d16a90a5b670ce73f3
Andrew Kingston <andrew@kingston.dev>
2023-02-20 18:46:06 +0000

    Generate inclusion schema when importing multiselect columns

--- packages/server/src/api/controllers/table/utils.ts
+++ packages/server/src/api/controllers/table/utils.ts
@@ -111,7 +111,8 @@ export function importToRows(data: any, table: any, user: any = {}) {
     for ([fieldName, schema] of Object.entries(table.schema)) {
       // check whether the options need to be updated for inclusion as part of the data import
       if (
-        schema.type === FieldTypes.OPTIONS &&
+        (schema.type === FieldTypes.OPTIONS ||
+          schema.type === FieldTypes.ARRAY) &&
         row[fieldName] &&
         (!schema.constraints.inclusion ||
           schema.constraints.inclusion.indexOf(row[fieldName]) === -1)
