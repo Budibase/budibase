e350f6b166ca69edfd9dfb24e37f7e47e824f812
adrinr <adria@budibase.com>
2023-02-22 11:02:32 +0000

    Undo literal changes

--- packages/server/src/api/controllers/row/ExternalRequest.ts
+++ packages/server/src/api/controllers/row/ExternalRequest.ts
@@ -316,7 +316,7 @@ export class ExternalRequest {
             key: otherKey,
             [thisKey]: breakRowIdField(relationship)[0],
             // leave the ID for enrichment later
-            [otherKey]: `{{ literal [${table.name}.${tablePrimary}] }}`,
+            [otherKey]: `{{ literal ${tablePrimary} }}`,
           })
         })
       }
