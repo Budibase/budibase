e8bfd79663d4c5a137b6dc6b88f3e1f3e7dd4761
Mel O'Hagan <mel@budibase.com>
2023-02-24 15:43:48 +0000

    Support navigate to link in new tab

--- packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/NavigateTo.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/NavigateTo.svelte
@@ -7,16 +7,20 @@
 </script>
 
 <div class="root">
-  <Label small>Screen</Label>
+  <Label small>Screen or URL</Label>
   <DrawerBindableInput
     title="Destination URL"
-    placeholder="/screen"
+    placeholder="/route"
     value={parameters.url}
     on:change={value => (parameters.url = value.detail)}
     {bindings}
   />
   <div />
   <Checkbox text="Open screen in modal" bind:value={parameters.peek} />
+  <Checkbox
+    text="Open external link in new tab"
+    bind:value={parameters.externalNewTab}
+  />
 </div>
 
 <style>
@@ -24,7 +28,7 @@
     display: grid;
     align-items: center;
     gap: var(--spacing-m);
-    grid-template-columns: auto 1fr;
+    grid-template-columns: auto;
     max-width: 400px;
     margin: 0 auto;
   }
