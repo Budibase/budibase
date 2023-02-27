65646ba01bce7608f6feb6c07b67876ea4ee8b07
mike12345567 <me@michaeldrury.co.uk>
2023-02-27 11:00:13 +0000

    Further PR comments.

--- packages/backend-core/src/events/index.ts
+++ packages/backend-core/src/events/index.ts
@@ -3,7 +3,6 @@ export * as processors from "./processors"
 export * as analytics from "./analytics"
 export { default as identification } from "./identification"
 export * as backfillCache from "./backfill"
-export { isAudited } from "./events"
 
 import { processors } from "./processors"
 
