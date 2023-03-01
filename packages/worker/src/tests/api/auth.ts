c83684ed830d8577b843ddd398b4dd8d9600c1db
Rory Powell <rory.codes@gmail.com>
2023-03-01 14:49:44 +0000

    Prevent showing user exists for password disabled actions

--- packages/worker/src/tests/api/auth.ts
+++ packages/worker/src/tests/api/auth.ts
@@ -61,11 +61,13 @@ export class AuthAPI extends TestAPI {
 
     let code: string | undefined
     if (res.status === 200) {
-      const emailCall = sendMailMock.mock.calls[0][0]
-      const parts = emailCall.html.split(
-        `http://localhost:10000/builder/auth/reset?code=`
-      )
-      code = parts[1].split('"')[0].split("&")[0]
+      if (sendMailMock.mock.calls.length) {
+        const emailCall = sendMailMock.mock.calls[0][0]
+        const parts = emailCall.html.split(
+          `http://localhost:10000/builder/auth/reset?code=`
+        )
+        code = parts[1].split('"')[0].split("&")[0]
+      }
     }
 
     return { code, res }
