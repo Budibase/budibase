518b77d3e0a30d2a00c75904b6f9db2949e640b6
adrinr <adria@budibase.com>
2023-03-06 13:10:38 +0100

    Fix tests

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -14,6 +14,7 @@ const baseConfig: Config.InitialProjectOptions = {
   moduleNameMapper: {
     "@budibase/backend-core/(.*)": "<rootDir>/../backend-core/$1",
     "@budibase/backend-core": "<rootDir>/../backend-core/src",
+    "@budibase/data-utils": "<rootDir>/../data-utils/src",
     "@budibase/types": "<rootDir>/../types/src",
   },
 }
