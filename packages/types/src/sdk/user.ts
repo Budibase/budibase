f622c84ebc3209e2a70acecb52bd546c21c99e94
Dean <deanhannigan@gmail.com>
2023-03-07 16:39:26 +0000

    Review feedback

--- packages/types/src/sdk/user.ts
+++ packages/types/src/sdk/user.ts
@@ -1,11 +1,3 @@
-export interface UpdateSelf {
-  firstName?: string
-  lastName?: string
-  password?: string
-  forceResetPassword?: boolean
-  onboardedAt?: string
-}
-
 export interface SaveUserOpts {
   hashPassword?: boolean
   requirePassword?: boolean
