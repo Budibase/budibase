48dc408197314cb232c73a83226fb8c44eea9b45
Michael Drury <me@michaeldrury.co.uk>
2023-03-08 13:35:26 +0000

    Revert "pinning MinIO versions"

--- scripts/install-minio.sh
+++ scripts/install-minio.sh
@@ -2,9 +2,9 @@
 if [[ $TARGETARCH == arm* ]] ;
 then
   echo "INSTALLING ARM64 MINIO"
-  wget https://dl.min.io/server/minio/release/linux-arm64/archive/minio.RELEASE.2022-10-24T18-35-07Z
+  wget https://dl.min.io/server/minio/release/linux-arm64/minio
 else
   echo "INSTALLING AMD64 MINIO"
-  wget https://dl.min.io/server/minio/release/linux-amd64/minio.RELEASE.2022-10-24T18-35-07Z
+  wget https://dl.min.io/server/minio/release/linux-amd64/minio
 fi
 chmod +x minio
