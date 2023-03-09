63afd34eb26eccb0b2acd76349cb9304a3af5772
Adria Navarro <adria@revityapp.com>
2023-03-09 08:50:26 +0000

    Bug - BUDI-6068 filters do not work for google sheets (#9886)
    * Add data-utils with filters
    
    * Create data-utils
    
    * Add data-utils to compiled code
    
    * Reuse constants
    
    * Fix tests
    
    * Rename package to shared-core
    
    * Namespace export shared-core
    
    * Rely on rollup to bundle shared-core
    
    * Revert "Rely on rollup to bundle shared-core"
    
    This reverts commit e8b5a2bb9a5f5cfe41b5540737ac3ab64677cf1c.
    
    * Updating version and removing private.
    
    * Update version
    
    * Increment versions.
    
    * Implement sort
    
    * Enabling sort
    
    * v2.3.21-alpha.2
    
    * Fix build
    
    * Improve readability
    
    * Move deepGet to shared helper
    
    * Better type usage
    
    * Fix types
    
    * Configure types
    
    * Fix vite refs
    
    * Add dep
    
    * Fixing depencencies on client
    
    * Add missing dev command
    
    * Fix loading issues
    
    * Update versions to latest
    
    * Multiple es6 and commonjs configs
    
    * Config
    
    * Use local packages on rollup for client
    
    * Change shared-core and types entry points
    
    ---------
    
    Co-authored-by: mike12345567 <me@michaeldrury.co.uk>

--- packages/bbui/src/helpers.js
+++ packages/bbui/src/helpers.js
@@ -1,3 +1,6 @@
+import { helpers } from "@budibase/shared-core"
+export const deepGet = helpers.deepGet
+
 /**
  * Generates a DOM safe UUID.
  * Starting with a letter is important to make it DOM safe.
@@ -39,30 +42,6 @@ export const hashString = string => {
     hash = hash & hash // Convert to 32bit integer
   }
   return hash.toString()
-}
-
-/**
- * Gets a key within an object. The key supports dot syntax for retrieving deep
- * fields - e.g. "a.b.c".
- * Exact matches of keys with dots in them take precedence over nested keys of
- * the same path - e.g. getting "a.b" from { "a.b": "foo", a: { b: "bar" } }
- * will return "foo" over "bar".
- * @param obj the object
- * @param key the key
- * @return {*|null} the value or null if a value was not found for this key
- */
-export const deepGet = (obj, key) => {
-  if (!obj || !key) {
-    return null
-  }
-  if (Object.prototype.hasOwnProperty.call(obj, key)) {
-    return obj[key]
-  }
-  const split = key.split(".")
-  for (let i = 0; i < split.length; i++) {
-    obj = obj?.[split[i]]
-  }
-  return obj
 }
 
 /**
