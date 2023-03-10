f8cebeba4eb2278089b86d7537c97c05ff515602
adrinr <adria@budibase.com>
2023-03-10 11:05:25 +0100

    Add create types

--- packages/types/src/api/web/global/scim/users.ts
+++ packages/types/src/api/web/global/scim/users.ts
@@ -30,3 +30,54 @@ export interface ScimListResponse {
   startIndex: number
   itemsPerPage: number
 }
+
+export interface ScimUserRequest {
+  schemas: [
+    "urn:ietf:params:scim:schemas:core:2.0:User",
+    "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
+  ]
+  externalId: string
+  userName: string
+  active: boolean
+  emails: [
+    {
+      primary: true
+      type: "work"
+      value: string
+    }
+  ]
+  meta: {
+    resourceType: "User"
+  }
+  name: {
+    formatted: string
+    familyName: string
+    givenName: string
+  }
+  roles: []
+}
+
+export interface ScimUserResponse {
+  schemas: ["urn:ietf:params:scim:schemas:core:2.0:User"]
+  id: string
+  externalId: string
+  meta: {
+    resourceType: "User"
+    created: string
+    lastModified: string
+  }
+  userName: string
+  name: {
+    formatted: string
+    familyName: string
+    givenName: string
+  }
+  active: boolean
+  emails: [
+    {
+      value: string
+      type: "work"
+      primary: true
+    }
+  ]
+}
