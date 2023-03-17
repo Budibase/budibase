c7c3f8f1b72e4c449c1bf5913b6f39ffb1b3a432
adrinr <adria@budibase.com>
2023-03-17 10:11:28 +0100

    Add types

--- packages/types/src/api/web/global/scim/users.ts
+++ packages/types/src/api/web/global/scim/users.ts
@@ -1,4 +1,5 @@
 import { ScimResource, ScimMeta, ScimPatchOperation } from "scim-patch"
+import { ScimListResponse } from "./shared"
 
 export interface ScimUserResponse extends ScimResource {
   schemas: ["urn:ietf:params:scim:schemas:core:2.0:User"]
@@ -52,14 +53,6 @@ export interface ScimCreateUserRequest {
 export interface ScimUpdateRequest {
   schemas: ["urn:ietf:params:scim:api:messages:2.0:PatchOp"]
   Operations: ScimPatchOperation[]
-}
-
-interface ScimListResponse<T> {
-  schemas: ["urn:ietf:params:scim:api:messages:2.0:ListResponse"]
-  totalResults: number
-  Resources: T[]
-  startIndex: number
-  itemsPerPage: number
 }
 
 export interface ScimUserListResponse
