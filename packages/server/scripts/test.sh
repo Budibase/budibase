849a593e5d47c86aa359e0e8b98b27c61a13b792
mike12345567 <me@michaeldrury.co.uk>
2023-03-21 19:52:06 +0000

    Updating Jest to latest 29.5.0 and then fixing snapshots to work with this - also setting worker memory limits for Node 16.10+ - where a leak can occur.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -3,10 +3,10 @@
 if [[ -n $CI ]]
 then
   # --runInBand performs better in ci where resources are limited
-  echo "jest --coverage --runInBand"
-  jest --coverage --runInBand
+  echo "jest --coverage --runInBand --forceExit --workerIdleMemoryLimit=1000MB"
+  jest --coverage --runInBand --forceExit --workerIdleMemoryLimit=1000MB
 else
   # --maxWorkers performs better in development
-  echo "jest --coverage --maxWorkers=2"
-  jest --coverage --maxWorkers=2
+  echo "jest --coverage --maxWorkers=2 --forceExit --workerIdleMemoryLimit=1000MB"
+  jest --coverage --maxWorkers=2 --forceExit --workerIdleMemoryLimit=1000MB
 fi
