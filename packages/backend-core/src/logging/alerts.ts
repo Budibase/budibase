4c0f3dd1cd7d97ca69289cee4f3ea6501e949f18
Rory Powell <rory.codes@gmail.com>
2023-03-21 11:04:11 +0000

    Improve bb-alert logging

--- packages/backend-core/src/logging/alerts.ts
+++ packages/backend-core/src/logging/alerts.ts
@@ -8,11 +8,7 @@ export function logAlert(message: string, e?: any) {
   if (e && NonErrors.includes(e.name) && isSuppressed(e)) {
     return
   }
-  let errorJson = ""
-  if (e) {
-    errorJson = ": " + JSON.stringify(e, Object.getOwnPropertyNames(e))
-  }
-  console.error(`bb-alert: ${message} ${errorJson}`)
+  console.error(`bb-alert: ${message}`, e)
 }
 
 export function logAlertWithInfo(
