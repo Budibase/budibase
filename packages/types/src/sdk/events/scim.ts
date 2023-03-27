f2ce5ccd556016b454198b05e8ef9da178fa826e
adrinr <adria@budibase.com>
2023-03-27 15:28:03 +0100

    Clean code

--- packages/types/src/sdk/events/scim.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEvent } from "./event"
-
-export interface ScimUserUpdatedEvent extends BaseEvent {
-  userId: string
-}
-export interface ScimUserDeletedEvent extends BaseEvent {
-  userId: string
-}
