e223c311c540307c9e69c66e77dc5328954265e0
Michael Drury <me@michaeldrury.co.uk>
2023-03-27 21:29:38 +0100

    Removing test script changes.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -3,10 +3,10 @@
 if [[ -n $CI ]]
 then
   # --runInBand performs better in ci where resources are limited
-  echo "jest --coverage --runInBand --forceExit --workerIdleMemoryLimit=1000MB"
-  jest --coverage --runInBand --forceExit --workerIdleMemoryLimit=1000MB
+  echo "jest --coverage --runInBand"
+  jest --coverage --runInBand
 else
   # --maxWorkers performs better in development
-  echo "jest --coverage --maxWorkers=2 --forceExit --workerIdleMemoryLimit=1000MB"
-  jest --coverage --maxWorkers=2 --forceExit --workerIdleMemoryLimit=1000MB
+  echo "jest --coverage --maxWorkers=2"
+  jest --coverage --maxWorkers=2
 fi
