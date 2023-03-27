42caca0f6677f1f6b085eae4317ecbfd0539cc90
adrinr <adria@budibase.com>
2023-03-27 11:10:07 +0100

    Use new scim context functions

--- packages/backend-core/src/context/mainContext.ts
+++ packages/backend-core/src/context/mainContext.ts
@@ -214,6 +214,13 @@ export function doInEnvironmentContext(
   return newContext(updates, task)
 }
 
+export function doInScimContext(task: any) {
+  const updates: ContextMap = {
+    scimCall: true,
+  }
+  return newContext(updates, task)
+}
+
 export function getEnvironmentVariables() {
   const context = Context.get()
   if (!context.environmentVariables) {
@@ -269,4 +276,10 @@ export function getDevAppDB(opts?: any): Database {
     throw new Error("Unable to retrieve dev DB - no app ID.")
   }
   return getDB(conversions.getDevelopmentAppID(appId), opts)
+}
+
+export function isScimCall(): boolean {
+  const context = Context.get()
+  const scimCall = context?.scimCall
+  return !!scimCall
 }
