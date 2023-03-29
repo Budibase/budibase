310c516e89e131cd4c862f73bef275e7537f7d6d
mike12345567 <me@michaeldrury.co.uk>
2023-03-29 14:19:54 +0100

    Adding a check for the tenant ID - to make sure it is a real tenant before forcing the user out of the platform based on it.

--- packages/worker/src/tests/api/tenants.ts
+++ packages/worker/src/tests/api/tenants.ts
@@ -2,8 +2,10 @@ import TestConfiguration from "../TestConfiguration"
 import { TestAPI, TestAPIOpts } from "./base"
 
 export class TenantAPI extends TestAPI {
+  config: TestConfiguration
   constructor(config: TestConfiguration) {
     super(config)
+    this.config = config
   }
 
   delete = (tenantId: string, opts?: TestAPIOpts) => {
@@ -11,5 +13,12 @@ export class TenantAPI extends TestAPI {
       .delete(`/api/system/tenants/${tenantId}`)
       .set(opts?.headers)
       .expect(opts?.status ? opts.status : 204)
+  }
+
+  info = (tenantId: string) => {
+    return this.request
+      .get(`/api/system/tenants/${tenantId}/info`)
+      .set(this.config.defaultHeaders())
+      .expect(200)
   }
 }
