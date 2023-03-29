c7ad0ba7499679775f3223c8cb268bdd2c445dd0
adrinr <adria@budibase.com>
2023-03-29 17:02:56 +0100

    Test user groups

--- packages/server/src/sdk/users/tests/utils.spec.ts
+++ packages/server/src/sdk/users/tests/utils.spec.ts
@@ -1,4 +1,7 @@
-import { db } from "@budibase/backend-core"
+import { db, roles } from "@budibase/backend-core"
+import { structures } from "@budibase/backend-core/tests"
+import { sdk as proSdk } from "@budibase/pro"
+
 import TestConfiguration from "../../../tests/utilities/TestConfiguration"
 import { rawUserMetadata, syncGlobalUsers } from "../utils"
 
@@ -58,6 +61,40 @@ describe("syncGlobalUsers", () => {
           _id: db.generateUserMetadataID(user._id),
         })
       )
+    })
+  })
+
+  it("app users are removed when removed from the tenant", async () => {
+    await config.doInTenant(async () => {
+      const group = await proSdk.groups.save(structures.userGroups.userGroup())
+      const user1 = await config.createUser({ admin: false, builder: false })
+      const user2 = await config.createUser({ admin: false, builder: false })
+      await proSdk.groups.addUsers(group.id, [user1._id, user2._id])
+
+      await config.doInContext(config.appId, async () => {
+        await syncGlobalUsers()
+        expect(await rawUserMetadata()).toHaveLength(1)
+
+        await proSdk.groups.updateGroupApps(group.id, {
+          appsToAdd: [
+            { appId: config.prodAppId!, roleId: roles.BUILTIN_ROLE_IDS.BASIC },
+          ],
+        })
+        await syncGlobalUsers()
+
+        const metadata = await rawUserMetadata()
+        expect(metadata).toHaveLength(3)
+        expect(metadata).toContainEqual(
+          expect.objectContaining({
+            _id: db.generateUserMetadataID(user1._id),
+          })
+        )
+        expect(metadata).toContainEqual(
+          expect.objectContaining({
+            _id: db.generateUserMetadataID(user2._id),
+          })
+        )
+      })
     })
   })
 })
