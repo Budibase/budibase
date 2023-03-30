36b4e76fd42554120d1136f6ca04ded7c69a17c9
mike12345567 <me@michaeldrury.co.uk>
2023-03-30 17:15:49 +0100

    Some final updates to get event spyOn working again.

--- packages/backend-core/tests/core/utilities/mocks/index.ts
+++ packages/backend-core/tests/core/utilities/mocks/index.ts
@@ -5,5 +5,5 @@ export const accounts = jest.mocked(_accounts)
 export * as date from "./date"
 export * as licenses from "./licenses"
 export { default as fetch } from "./fetch"
-import "./posthog"
 import "./events"
+import "./posthog"
