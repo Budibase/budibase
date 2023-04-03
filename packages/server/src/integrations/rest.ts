017afd20b9b4533f84fe26d5bd105c029d55fedf
mike12345567 <me@michaeldrury.co.uk>
2023-04-03 15:36:34 +0100

    Fixing syntax error and linting.

--- packages/server/src/integrations/rest.ts
+++ packages/server/src/integrations/rest.ts
@@ -152,9 +152,9 @@ class RestIntegration implements IntegrationBase {
         }
         raw = rawXml
       } else if (contentType.includes("application/pdf")) {
-        data = await response.arrayBuffer(); // Save PDF as ArrayBuffer
-        raw = Buffer.from(data); 
-      else {
+        data = await response.arrayBuffer() // Save PDF as ArrayBuffer
+        raw = Buffer.from(data)
+      } else {
         data = await response.text()
         raw = data
       }
