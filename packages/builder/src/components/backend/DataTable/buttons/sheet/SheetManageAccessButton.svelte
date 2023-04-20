6bf6d158490eb899ea09fb9fbae43927cbe1ee51
Andrew Kingston <andrew@kingston.dev>
2023-04-20 08:17:07 +0100

    Rename sheet to grid

--- packages/builder/src/components/backend/DataTable/buttons/sheet/SheetManageAccessButton.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import ManageAccessButton from "../ManageAccessButton.svelte"
-  import { getContext } from "svelte"
-
-  const { config } = getContext("sheet")
-</script>
-
-<ManageAccessButton resourceId={$config.tableId} />
