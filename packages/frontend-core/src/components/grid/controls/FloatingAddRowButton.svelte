eabd57d20b1e2812c577c7f35fb725e09d275648
Andrew Kingston <andrew@kingston.dev>
2023-04-21 10:26:55 +0100

    Add FAB for adding rows and inline row creation

---
+++ packages/frontend-core/src/components/grid/controls/FloatingAddRowButton.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Icon } from "@budibase/bbui"
+  import { getContext } from "svelte"
+
+  const { dispatch } = getContext("grid")
+</script>
+
+<div class="floating-add-row" on:click={() => dispatch("add-row-inline")}>
+  <Icon name="Add" size="L" color="white" />
+</div>
+
+<style>
+  .floating-add-row {
+    position: absolute;
+    bottom: 32px;
+    left: 32px;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    background: var(--spectrum-global-color-blue-400);
+    display: grid;
+    place-items: center;
+  }
+  .floating-add-row:hover {
+    background: var(--spectrum-global-color-blue-500);
+    cursor: pointer;
+  }
+</style>
