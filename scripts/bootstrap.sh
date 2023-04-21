33ba0062e454686f9af1561d2837e84cb85a18aa
adrinr <adria@budibase.com>
2023-04-21 18:16:12 +0100

    Clean scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,10 @@
+node scripts/syncProPackage.js
+yarn
+
 if [ -d "packages/pro/packages" ]; then
   cd packages/pro
 
   yarn
   lerna bootstrap
   yarn setup
-else
-  node scripts/updateLocalPro.js 
-  yarn
 fi
