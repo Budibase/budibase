5482125cae2f0aaf297c7dd056934e872364bff1
Mel O'Hagan <mel@budibase.com>
2023-04-25 11:31:33 +0100

    Unit tests WIP

--- packages/builder/src/stores/backend/tests/tables.test.js
+++ packages/builder/src/stores/backend/tests/tables.test.js
@@ -1,6 +1,7 @@
 import { it, expect, describe, beforeEach, vi } from "vitest"
 import { createTablesStore } from "../tables"
-import { writable, get } from "svelte/store"
+import { writable, get, derived } from "svelte/store"
+import { API } from "api"
 
 vi.mock("api", () => {
   return {
@@ -8,6 +9,7 @@ vi.mock("api", () => {
       getTables: vi.fn(),
       fetchTableDefinition: vi.fn(),
       saveTable: vi.fn(),
+      deleteTable: vi.fn(),
     },
   }
 })
@@ -16,7 +18,7 @@ vi.mock("stores/backend", () => {
   return { datasources: vi.fn() }
 })
 
-// explict mock that is overwritten later so that the singleton admin store doesn't throw an error when partially mocked
+// explict mock that is overwritten later
 vi.mock("svelte/store", () => {
   return {
     writable: vi.fn(() => ({
@@ -24,7 +26,10 @@ vi.mock("svelte/store", () => {
       update: vi.fn(),
     })),
     get: vi.fn(),
-    derived: vi.fn(),
+    derived: vi.fn(() => ({
+      subscribe: vi.fn(),
+      update: vi.fn(),
+    })),
   }
 })
 
@@ -35,12 +40,15 @@ describe("tables store", () => {
     ctx.writableReturn = { update: vi.fn(), subscribe: vi.fn() }
     writable.mockReturnValue(ctx.writableReturn)
 
+    ctx.derivedReturn = { update: vi.fn(), subscribe: vi.fn() }
+    derived.mockReturnValue(ctx.derivedReturn)
+
     ctx.returnedStore = createTablesStore()
   })
 
   it("returns the created store", ctx => {
     expect(ctx.returnedStore).toEqual({
-      subscribe: expect.toBe(ctx.writableReturn.subscribe),
+      subscribe: expect.toBe(ctx.derivedReturn.subscribe),
       init: expect.toBeFunc(),
       fetch: expect.toBeFunc(),
       fetchTable: expect.toBeFunc(),
@@ -52,4 +60,169 @@ describe("tables store", () => {
       updateTable: expect.toBeFunc(),
     })
   })
+
+  describe("fetch", () => {
+    it("calls getTables and updates the store", async ctx => {
+      const listOfTables = ["T1", "T2"]
+      API.getTables.mockReturnValue(listOfTables)
+
+      const state = {
+        foo: "foo",
+      }
+      await ctx.returnedStore.fetch()
+
+      expect(API.getTables).toHaveBeenCalledTimes(1)
+      expect(ctx.writableReturn.update.calls[0][0](state)).toEqual({
+        foo: "foo",
+        list: listOfTables,
+      })
+    })
+  })
+
+  describe("fetchTable", () => {
+    it("calls fetchTableDefinition and updates a specific table in the store", async ctx => {
+      const tableId = "TABLE_ID"
+      const table = { _id: tableId, name: "NEW" }
+      API.fetchTableDefinition.mockReturnValue(table)
+
+      const state = {
+        list: [{
+            _id: "T1",
+            name: "OLD_1"
+          }, {
+            _id: tableId,
+            name: "OLD_2"
+          }, {
+            _id: "T3",
+            name: "OLD_3"
+          }
+        ],
+      }
+      await ctx.returnedStore.fetchTable(tableId)
+
+      expect(API.fetchTableDefinition).toHaveBeenCalledTimes(1)
+      expect(API.fetchTableDefinition).toHaveBeenCalledWith(tableId)
+
+      expect(ctx.writableReturn.update.calls[0][0](state)).toEqual({
+        list: [{
+          _id: "T1",
+          name: "OLD_1"
+        }, {
+          _id: tableId,
+          name: "NEW"
+        }, {
+          _id: "T3",
+          name: "OLD_3"
+        }
+      ],
+      })
+    })
+  })
+
+  describe("select", () => {
+    it("updates the store with the selected table id", async ctx => {
+      const tableId = "TABLE_ID"
+      const state = {
+        foo: "foo",
+      }
+      await ctx.returnedStore.select(tableId)
+
+      expect(ctx.writableReturn.update.calls[0][0](state)).toEqual({
+        foo: "foo",
+        selectedTableId: tableId,
+      })
+    })
+  })
+
+  // describe("deleteTable", () => {
+  //   it("calls deleteTable and does a store fetch", async ctx => {
+  //     const table = {
+  //       _id: "TABLE_ID",
+  //       _rev: "REV"
+  //     }
+  //     API.deleteTable.mockReturnValue()
+  //     ctx.returnedStore.fetch.mockImplementation(() => {})
+
+  //     await ctx.returnedStore.deleteTable(table)
+
+  //     expect(API.deleteTable).toHaveBeenCalledTimes(1)
+  //     expect(API.deleteTable).toHaveBeenCalledWith({
+  //       tableId: "TABLE_ID",
+  //       tableRev: "REV"
+  //     })
+  //     expect(fetch).toHaveBeenCalledTimes(1)
+  //   })
+  // })
+
+  describe("updateTable", () => {
+    beforeEach(async ctx => {
+      get.mockImplementation(() => {
+        return {
+            list: [{
+              _id: "T1",
+              name: "OLD_1"
+            }, {
+              _id: "T2",
+              name: "OLD_2"
+            }, {
+              _id: "T3",
+              name: "OLD_3"
+            }
+          ]
+        }
+      })
+    })
+    it("gets a specific table in the store and overwrites it with a new table object", async ctx => {
+      const table = {
+        _id: "T3",
+        _rev: "REV",
+        type: "TYPE_FROM_TABLE",
+        name: "NEW",
+        extra: "ADD_PROP"
+      }
+      const state = {
+        list: [{
+            _id: "T1",
+            name: "OLD_1"
+          }, {
+            _id: "T2",
+            name: "OLD_2"
+          }, {
+            _id: "T3",
+            name: "OLD_3",
+            type: "TYPE_FROM_STATE",
+          }
+        ]
+      }
+
+      await ctx.returnedStore.updateTable(table)
+
+      expect(ctx.writableReturn.update.calls[0][0](state)).toEqual({
+          list: [{
+            _id: "T1",
+            name: "OLD_1"
+          }, {
+            _id: "T2",
+            name: "OLD_2"
+          }, {
+            _id: "T3",
+            _rev: "REV",
+            name: "NEW",
+            extra: "ADD_PROP",
+            type: "TYPE_FROM_STATE"
+          }
+        ]
+      })
+    })
+
+    it("returns early and does not update state if the table id is not found", async ctx => {
+      const table = {
+        _id: "NOT_FOUND",
+      }
+      await ctx.returnedStore.updateTable(table)
+
+      expect(ctx.writableReturn.update.calls.length).toBe(0)
+    })
+  })
+
 })
