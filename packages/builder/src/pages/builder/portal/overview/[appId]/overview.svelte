213a0640a6b63f3bcfcd9521faa2d8ed2966f0b3
Dean <deanhannigan@gmail.com>
2023-04-25 17:00:49 +0100

    Lint

--- packages/builder/src/pages/builder/portal/overview/[appId]/overview.svelte
+++ packages/builder/src/pages/builder/portal/overview/[appId]/overview.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { onMount } from "svelte"
   import DashCard from "components/common/DashCard.svelte"
   import { AppStatus } from "constants"
   import { goto } from "@roxi/routify"
