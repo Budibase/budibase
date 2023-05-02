6e7c78362ea5ee6cc91e37d58f447c45db8c57e4
Adria Navarro <adria@budibase.com>
2023-05-02 12:46:53 +0100

    Return null instead of undefined

--- packages/server/src/utilities/csv.ts
+++ packages/server/src/utilities/csv.ts
@@ -1,19 +1,22 @@
 import csv from "csvtojson"
 
 export async function jsonFromCsvString(csvString: string) {
-  const castedWithEmptyStrings = await csv().fromString(csvString)
-  if (!castedWithEmptyStrings.length) {
-    return []
-  }
+  const castedWithEmptyValues = await csv({ ignoreEmpty: true }).fromString(
+    csvString
+  )
 
   // By default the csvtojson library casts empty values as empty strings. This is causing issues on conversion.
   // ignoreEmpty will remove the key completly if empty, so creating this empty object will ensure we return the values with the keys but empty values
-  const emptyObject = Object.keys(castedWithEmptyStrings[0]).reduce(
-    (p, v) => ({ ...p, [v]: undefined }),
-    {}
-  )
+  const result = await csv({ ignoreEmpty: false }).fromString(csvString)
+  result.forEach((r, i) => {
+    for (const [key] of Object.entries(r).filter(
+      ([key, value]) => value === ""
+    )) {
+      if (castedWithEmptyValues[i][key] === undefined) {
+        r[key] = null
+      }
+    }
+  })
 
-  let result = await csv({ ignoreEmpty: true }).fromString(csvString)
-  result = result.map(r => ({ ...emptyObject, ...r }))
   return result
 }
