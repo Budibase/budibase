62b800b189925c90eb1e41fd38384153b93f5076
Maurits Lourens <maurits.lourens@gmail.com>
2023-05-05 16:27:54 +0200

    replace legacy icon with icon from spectrum-css

--- packages/bbui/src/Form/Core/Dropzone.svelte
+++ packages/bbui/src/Form/Core/Dropzone.svelte
@@ -165,7 +165,7 @@
           {/if}
           {#if !disabled}
             <div class="delete-button" on:click={removeFile}>
-              <i class="ri-delete-bin-line" />
+              <Icon name="Delete" />
             </div>
           {/if}
         </div>
@@ -209,7 +209,7 @@
             {/if}
             {#if !disabled}
               <div class="delete-button" on:click={removeFile}>
-                <i class="ri-delete-bin-line" />
+                <Icon name="Delete" />
               </div>
             {/if}
           </div>
