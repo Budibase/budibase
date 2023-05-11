acc1fb05e6e76813223db97a68e33b5c91f9bd9a
melohagan <101575380+melohagan@users.noreply.github.com>
2023-05-11 11:39:08 +0100

    Update packages/server/src/automations/steps/make.ts
    Co-authored-by: Martin McKeaveney <martin@budibase.com>

--- packages/server/src/automations/steps/make.ts
+++ packages/server/src/automations/steps/make.ts
@@ -79,7 +79,7 @@ export async function run({ inputs }: AutomationStepInput) {
 
   let payload = {}
   try {
-    payload = JSON.parse(body?.value || "{}")
+    payload = body?.value ? JSON.parse(body?.value) : {}
   } catch (err) {
     return {
       httpStatus: 400,
