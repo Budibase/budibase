1ac55241d375135c2c8597d1355d520558996731
Andrew Kingston <andrew@kingston.dev>
2023-05-12 14:33:00 +0100

    Disable command palette and tour for non lock holders and forcefully prevent access to the design and automate pages

--- packages/builder/src/pages/builder/app/[application]/design/_layout.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/_layout.svelte
@@ -1,2 +1,14 @@
+<script>
+  import { store } from "builderStore"
+  import { redirect } from "@roxi/routify"
+
+  // Prevent access for other users than the lock holder
+  $: {
+    if (!$store.hasLock) {
+      $redirect("../data")
+    }
+  }
+</script>
+
 <!-- routify:options index=2 -->
 <slot />
