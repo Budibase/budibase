de70bc9c7694a45d524b359bcebbf39612c732ce
Adria Navarro <adria@budibase.com>
2023-05-15 16:25:12 +0200

    Undo

--- qa-core/src/integrations/validators/postgres.spec.ts
+++ qa-core/src/integrations/validators/postgres.spec.ts
@@ -1,11 +1,7 @@
 import { GenericContainer } from "testcontainers"
-import { Duration, TemporalUnit } from "node-duration"
-
 import postgres from "../../../../packages/server/src/integrations/postgres"
-import { SourceName } from "@budibase/types"
 
 jest.unmock("pg")
-jest.unmock("mssql")
 
 describe("datasource validators", () => {
   describe("postgres", () => {
@@ -51,62 +47,6 @@ describe("datasource validators", () => {
       const result = await integration.testConnection()
       expect(result).toEqual({
         error: 'password authentication failed for user "wrong"',
-      })
-    })
-  })
-
-  describe("mssql", () => {
-    const validator = integrations.getValidator[SourceName.SQL_SERVER]!
-
-    let host: string, port: number
-
-    const password = "Str0Ng_p@ssW0rd!"
-
-    beforeAll(async () => {
-      const container = await new GenericContainer(
-        "mcr.microsoft.com/mssql/server"
-      )
-        .withExposedPorts(1433)
-        .withEnv("ACCEPT_EULA", "Y")
-        .withEnv("MSSQL_SA_PASSWORD", password)
-        .withEnv("MSSQL_PID", "Developer")
-        .withWaitStrategy(Wait.forHealthCheck())
-        .withHealthCheck({
-          test: `/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${password}" -Q "SELECT 1" -b -o /dev/null`,
-          interval: new Duration(1000, TemporalUnit.MILLISECONDS),
-          timeout: new Duration(3, TemporalUnit.SECONDS),
-          retries: 20,
-          startPeriod: new Duration(100, TemporalUnit.MILLISECONDS),
-        })
-        .start()
-
-      host = container.getContainerIpAddress()
-      port = container.getMappedPort(1433)
-    })
-
-    it("test valid connection string", async () => {
-      const result = await validator({
-        user: "sa",
-        password,
-        server: host,
-        port: port,
-        database: "master",
-        schema: "dbo",
-      })
-      expect(result).toBe(true)
-    })
-
-    it("test invalid password", async () => {
-      const result = await validator({
-        user: "sa",
-        password: "wrong_pwd",
-        server: host,
-        port: port,
-        database: "master",
-        schema: "dbo",
-      })
-      expect(result).toEqual({
-        error: "ConnectionError: Login failed for user 'sa'.",
       })
     })
   })
