90a67770e24f3b314a07abb56bfc1c9aba6bd4b5
Adria Navarro <adria@budibase.com>
2023-05-16 13:21:05 +0200

    Test airtable connection

--- packages/server/src/integrations/airtable.ts
+++ packages/server/src/integrations/airtable.ts
@@ -1,12 +1,12 @@
 import {
-  DatasourceFeature,
+  ConnectionInfo,
   DatasourceFieldType,
   Integration,
   IntegrationBase,
   QueryType,
 } from "@budibase/types"
 
-const Airtable = require("airtable")
+import Airtable from "airtable"
 
 interface AirtableConfig {
   apiKey: string
@@ -83,11 +83,34 @@ const SCHEMA: Integration = {
 
 class AirtableIntegration implements IntegrationBase {
   private config: AirtableConfig
-  private client: any
+  private client
 
   constructor(config: AirtableConfig) {
     this.config = config
     this.client = new Airtable(config).base(config.base)
+  }
+
+  async testConnection(): Promise<ConnectionInfo> {
+    const mockTable = Date.now().toString()
+    try {
+      await this.client.makeRequest({
+        path: `/${mockTable}`,
+      })
+
+      return { connected: true }
+    } catch (e: any) {
+      if (
+        e.message ===
+        `Could not find table ${mockTable} in application ${this.config.base}`
+      ) {
+        return { connected: true }
+      }
+
+      return {
+        connected: false,
+        error: e.message as string,
+      }
+    }
   }
 
   async create(query: { table: any; json: any }) {
