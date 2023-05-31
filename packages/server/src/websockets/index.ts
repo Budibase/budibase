16e929bd81c72d07b55eac6cd036690e81cde04a
mike12345567 <me@michaeldrury.co.uk>
2023-05-31 18:09:15 +0100

    Removing checks around websockets as redis properly available in testing now.

--- packages/server/src/websockets/index.ts
+++ packages/server/src/websockets/index.ts
@@ -10,12 +10,9 @@ let gridSocket: GridSocket | undefined
 let builderSocket: BuilderSocket | undefined
 
 export const initialise = (app: Koa, server: http.Server) => {
-  // currently we use ioredis-mock which doesn't work with the websocket system for testing
-  if (!env.isTest()) {
-    clientAppSocket = new ClientAppSocket(app, server)
-    gridSocket = new GridSocket(app, server)
-    builderSocket = new BuilderSocket(app, server)
-  }
+  clientAppSocket = new ClientAppSocket(app, server)
+  gridSocket = new GridSocket(app, server)
+  builderSocket = new BuilderSocket(app, server)
 }
 
 export { clientAppSocket, gridSocket, builderSocket }
