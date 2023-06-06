e1b63842e5e50be106371fd165b704d2a177f9e0
Dean <deanhannigan@gmail.com>
2023-06-06 16:24:30 +0100

    Adding in relocated screen components

---
+++ packages/builder/src/pages/builder/app/[application]/settings/backups/_components/TypeRenderer.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { BackupTrigger } from "constants/backend/backups"
+  export let row
+
+  $: trigger = row?.trigger || "manual"
+  $: type = row?.type || "backup"
+
+  function printTrigger(trig) {
+    let final = "undefined"
+    switch (trig) {
+      case BackupTrigger.PUBLISH:
+        final = "published"
+        break
+      case BackupTrigger.RESTORING:
+        final = "pre-restore"
+        break
+      default:
+        final = trig
+        break
+    }
+    return final.charAt(0).toUpperCase() + final.slice(1)
+  }
+</script>
+
+<div class="cell">
+  {printTrigger(trigger)}
+  {type}
+</div>
+
+<style>
+  .cell {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+</style>
