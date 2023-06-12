81522d0784b24cf29ba9b8a0ae44a70cec5e7100
Adria Navarro <adria@budibase.com>
2023-06-12 17:51:21 +0100

    Clean defaults

--- packages/server/src/api/controllers/backup.ts
+++ packages/server/src/api/controllers/backup.ts
@@ -4,7 +4,7 @@ import { DocumentType } from "../../db/utils"
 import { isQsTrue } from "../../utilities"
 
 export async function exportAppDump(ctx: any) {
-  let { appId, excludeRows = false, encryptPassword = "password" } = ctx.query
+  let { appId, excludeRows = false, encryptPassword } = ctx.query
   // remove the 120 second limit for the request
   ctx.req.setTimeout(0)
   const appName = decodeURI(ctx.query.appname)
