e6455d005b870fb2a690694aebd1ca73a87c22dc
Adria Navarro <adria@budibase.com>
2023-06-13 10:00:01 +0100

    Add encrypt option

--- packages/builder/src/components/start/ExportAppModal.svelte
+++ packages/builder/src/components/start/ExportAppModal.svelte
@@ -1,9 +1,16 @@
 <script>
-  import { ModalContent, Toggle, Body, InlineAlert } from "@budibase/bbui"
+  import {
+    ModalContent,
+    Toggle,
+    Body,
+    InlineAlert,
+    Divider,
+  } from "@budibase/bbui"
 
   export let app
   export let published
-  let excludeRows = false
+  let includeInternalTablesRows = true
+  let encypt = true
 
   $: title = published ? "Export published app" : "Export latest app"
   $: confirmText = published ? "Export published" : "Export latest"
@@ -11,17 +18,21 @@
   const exportApp = () => {
     const id = published ? app.prodId : app.devId
     const appName = encodeURIComponent(app.name)
-    window.location = `/api/backups/export?appId=${id}&appname=${appName}&excludeRows=${excludeRows}`
+    window.location = `/api/backups/export?appId=${id}&appname=${appName}&excludeRows=${!includeInternalTablesRows}`
   }
 </script>
 
 <ModalContent {title} {confirmText} onConfirm={exportApp}>
-  <InlineAlert
-    header="Do not share your budibase application exports publicly as they may contain sensitive information such as database credentials or secret keys."
-  />
-  <Body
-    >Apps can be exported with or without data that is within internal tables -
-    select this below.</Body
-  >
-  <Toggle text="Exclude Rows" bind:value={excludeRows} />
+  <Body>
+    <Toggle
+      text="Export rows from internal tables"
+      bind:value={includeInternalTablesRows}
+    />
+    <Toggle text="Encrypt my export" bind:value={encypt} />
+  </Body>
+  {#if !encypt}
+    <InlineAlert
+      header="Do not share your budibase application exports publicly as they may contain sensitive information such as database credentials or secret keys."
+    />
+  {/if}
 </ModalContent>
