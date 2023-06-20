38be94fa074aceffca92a037cb9a3833f2037154
mike12345567 <me@michaeldrury.co.uk>
2023-06-20 12:36:05 +0100

    Fix for test case.

--- packages/worker/src/api/controllers/system/environment.ts
+++ packages/worker/src/api/controllers/system/environment.ts
@@ -1,14 +1,14 @@
-import { BBContext } from "@budibase/types"
+import { Ctx } from "@budibase/types"
 import env from "../../../environment"
 
-export const fetch = async (ctx: BBContext) => {
+export const fetch = async (ctx: Ctx) => {
   ctx.body = {
     multiTenancy: !!env.MULTI_TENANCY,
+    offlineMode: !!env.OFFLINE_MODE,
     cloud: !env.SELF_HOSTED,
     accountPortalUrl: env.ACCOUNT_PORTAL_URL,
     disableAccountPortal: env.DISABLE_ACCOUNT_PORTAL,
     baseUrl: env.PLATFORM_URL,
-    offlineMode: env.OFFLINE_MODE,
     // in test need to pretend its in production for the UI (Cypress)
     isDev: env.isDev() && !env.isTest(),
   }
