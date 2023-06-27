a8b4322ed80cb9b6e81a2d83b37359b3ae144071
Gerard Burns <gjburns9@hotmail.com>
2023-06-27 14:16:55 +0100

    Update datasource config to use tabs

---
+++ packages/builder/src/pages/builder/app/[application]/data/datasource/[datasourceId]/_components/panels/Tooltip.svelte
@@ -0,0 +1,30 @@
+<script>
+  import FontAwesomeIcon from "components/common/FontAwesomeIcon.svelte"
+  import { Body } from "@budibase/bbui"
+
+  export let title = ""
+  export let href = null
+</script>
+
+<a {href} class="tooltip" target="_blank" rel="noopener noreferrer">
+  <FontAwesomeIcon name="fa-solid fa-question-circle" />
+  <Body weight="bold">{title}</Body>
+</a>
+
+<style>
+  .tooltip {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .tooltip :global(p) {
+    color: var(--grey-9);
+  }
+
+  .tooltip :global(svg) {
+    margin-top: 1px;
+    margin-right: 5px;
+    color: var(--grey-7);
+  }
+</style>
