2ac941a6e3414afa9726a15c583b19d79f553342
Peter Clement <peter@budibase.com>
2023-06-28 09:18:41 +0100

    remove incorrect psql codes

--- packages/server/src/integrations/base/errorMapping.ts
+++ packages/server/src/integrations/base/errorMapping.ts
@@ -5,13 +5,7 @@ const mysqlErrorMessages: Record<number, string> = {
   1049: "The specified database does not exist. Please verify that the database name is correct.",
 }
 
-const postgresErrorMessages: Record<number, string> = {
-  28: "Connection timed out. Please verify that the database host and port are correct, and that the database is accepting TCP/IP connections.",
-  3: "Connection timed out. Please verify that the database host and port are correct, and that the database is accepting TCP/IP connections.",
-  4: "Connection refused. Please verify that the database host and port are correct, and that the database is accepting TCP/IP connections.",
-  1017: "Access denied for the specified user. User does not have the necessary privileges or the provided credentials are incorrect. Please verify the credentials, and ensure that the user has appropriate permissions.",
-  1049: "The specified database does not exist. Please verify that the database name is correct.",
-}
+const postgresErrorMessages: Record<number, string> = {}
 
 const sqlServerErrorMessages: Record<number, string> = {}
 
