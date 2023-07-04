c1e9bc3c81b70325928f5bbae60c518c3d28e872
mike12345567 <me@michaeldrury.co.uk>
2023-07-04 11:59:56 +0100

    PR comments.

--- packages/backend-core/src/db/errors.ts
+++ packages/backend-core/src/db/errors.ts
@@ -1,9 +1,14 @@
-export function is409Error(error: any) {
+export function checkErrorCode(error: any, code: number) {
+  const stringCode = code.toString()
   if (typeof error === "object") {
-    return error.status === 409 || error.message?.includes("409")
+    return error.status === code || error.message?.includes(stringCode)
   } else if (typeof error === "number") {
-    return error === 409
+    return error === code
   } else if (typeof error === "string") {
-    return error.includes("409")
+    return error.includes(stringCode)
   }
+}
+
+export function isDocumentConflictError(error: any) {
+  return checkErrorCode(error, 409)
 }
