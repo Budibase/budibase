aba069d33314bc429b811267d68170c8ca287b98
Gerard Burns <Ghrehh@users.noreply.github.com>
2023-07-04 16:52:20 +0100

    Fix google sheets import (#11131)

--- packages/builder/src/components/backend/Datasources/TableImportSelection/tableSelectionStore.js
+++ packages/builder/src/components/backend/Datasources/TableImportSelection/tableSelectionStore.js
@@ -10,7 +10,11 @@ export const createTableSelectionStore = (integration, datasource) => {
 
   datasources.getTableNames(datasource).then(tableNames => {
     tableNamesStore.set(tableNames)
-    selectedTableNamesStore.set(tableNames.filter(t => datasource.entities[t]))
+
+    selectedTableNamesStore.set(
+      tableNames.filter(tableName => datasource.entities[tableName])
+    )
+
     loadingStore.set(false)
   })
 
