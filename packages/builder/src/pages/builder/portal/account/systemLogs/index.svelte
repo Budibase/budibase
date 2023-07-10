eb0923e76db625d0e4659660a4a5a3702834c4c4
Adria Navarro <adria@budibase.com>
2023-07-10 17:48:19 +0200

    Renames

--- packages/builder/src/pages/builder/portal/account/systemLogs/index.svelte
+++ packages/builder/src/pages/builder/portal/account/systemLogs/index.svelte
@@ -10,7 +10,7 @@
   async function download() {
     loading = true
     try {
-      await downloadStream(await API.getServerLogs())
+      await downloadStream(await API.getSystemLogs())
     } finally {
       loading = false
     }
