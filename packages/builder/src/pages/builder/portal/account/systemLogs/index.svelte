fd0018c1bc74cae2e5112c7ae1b91d459ab04fc3
Adria Navarro <adria@budibase.com>
2023-07-10 13:57:27 +0200

    Download logs section

---
+++ packages/builder/src/pages/builder/portal/account/systemLogs/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Layout, Body, Button } from "@budibase/bbui"
+  import { downloadStream } from "@budibase/frontend-core"
+
+  import { API } from "api"
+
+  async function download() {
+    downloadStream(await API.getServerLogs())
+  }
+</script>
+
+<Layout noPadding>
+  <Body>Download your latest logs to share with the Budibase team</Body>
+  <div class="download-button">
+    <Button cta on:click={download}>Download system logs</Button>
+  </div>
+</Layout>
