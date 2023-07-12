203c0f112c82baf517553c262032b12666eadfd5
Adria Navarro <adria@budibase.com>
2023-07-12 16:13:00 +0200

    Implement and test create

--- packages/server/src/api/controllers/view/views.ts
+++ packages/server/src/api/controllers/view/views.ts
@@ -1,6 +1,11 @@
 import sdk from "../../../sdk"
-import { Ctx } from "@budibase/types"
+import { Ctx, ViewV2 } from "@budibase/types"
 
 export async function fetch(ctx: Ctx) {
-  ctx.body = await sdk.views.get()
+  ctx.body = await sdk.views.get(ctx.params.viewId)
+}
+
+export async function save(ctx: Ctx<ViewV2>) {
+  const view = ctx.request.body
+  ctx.body = await sdk.views.save(view)
 }
---
+++ packages/server/src/sdk/app/views/index.ts
@@ -0,0 +1,25 @@
+import { context } from "@budibase/backend-core"
+import { ViewV2 } from "@budibase/types"
+import { generateViewID } from "../../../db/utils"
+
+export async function get(viewId: string) {
+  const db = context.getAppDB()
+  const result = await db.get(viewId)
+  return result
+}
+
+export async function save(view: ViewV2) {
+  const db = context.getAppDB()
+
+  const response = await db.put(
+    {
+      _id: generateViewID(),
+      ...view,
+    },
+    {}
+  )
+  return {
+    _id: response.id,
+    _rev: response.rev,
+  }
+}
