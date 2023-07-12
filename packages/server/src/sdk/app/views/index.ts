e2c1a549def4cd29ff41fb255fa91170f1474f10
Adria Navarro <adria@budibase.com>
2023-07-12 17:02:54 +0200

    Find by table

--- packages/server/src/sdk/app/views/index.ts
+++ packages/server/src/sdk/app/views/index.ts
@@ -5,12 +5,25 @@ import {
   context,
 } from "@budibase/backend-core"
 import { ViewV2 } from "@budibase/types"
-import { generateViewID } from "../../../db/utils"
+import * as utils from "../../../db/utils"
 
 export async function fetch() {
   const db = context.getAppDB()
 
   const startKey = `${DocumentType.VIEW}${SEPARATOR}`
+  const response = await db.allDocs({
+    startkey: startKey,
+    endkey: `${startKey}${UNICODE_MAX}`,
+    include_docs: true,
+  })
+
+  return response.rows.map(r => r.doc)
+}
+
+export async function findByTable(tableId: string) {
+  const db = context.getAppDB()
+
+  const startKey = utils.viewIDPrefix(tableId)
   const response = await db.allDocs({
     startkey: startKey,
     endkey: `${startKey}${UNICODE_MAX}`,
@@ -31,7 +44,7 @@ export async function save(view: ViewV2) {
 
   const response = await db.put(
     {
-      _id: generateViewID(),
+      _id: utils.generateViewID(view.tableId),
       ...view,
     },
     {}
