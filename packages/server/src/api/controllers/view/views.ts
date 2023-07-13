deaed2416d59e0996e42e38f642e6501b8f5be3c
Adria Navarro <adria@budibase.com>
2023-07-13 11:36:26 +0200

    Simplifying endpoints

--- packages/server/src/api/controllers/view/views.ts
+++ packages/server/src/api/controllers/view/views.ts
@@ -2,22 +2,24 @@ import sdk from "../../../sdk"
 import { Ctx, ViewV2 } from "@budibase/types"
 
 export async function fetch(ctx: Ctx) {
-  ctx.body = { views: await sdk.views.fetch() }
+  const { tableId } = ctx.query
+
+  if (tableId && typeof tableId !== "string") {
+    ctx.throw(400, "tableId type is not valid")
+  }
+
+  const views = tableId
+    ? await sdk.views.findByTable(tableId)
+    : await sdk.views.fetch()
+
+  ctx.body = { views }
 }
 
 export async function find(ctx: Ctx) {
-  const { tableId, viewId } = ctx.params
+  const { viewId } = ctx.params
 
   const result = await sdk.views.get(viewId)
-  if (result?.tableId !== tableId) {
-    ctx.throw(404)
-  }
   ctx.body = result
-}
-
-export async function findByTable(ctx: Ctx) {
-  const { tableId } = ctx.params
-  ctx.body = { views: await sdk.views.findByTable(tableId) }
 }
 
 export async function save(ctx: Ctx<ViewV2>) {
