3326d061f19ebcf4b46e76ff3a92ec5e14ca58ea
Rory Powell <rory.codes@gmail.com>
2023-07-14 11:44:05 +0100

    core structure updates

--- packages/backend-core/tests/core/utilities/structures/documents/platform/installation.ts
+++ packages/backend-core/tests/core/utilities/structures/documents/platform/installation.ts
@@ -1,9 +1,11 @@
 import { generator } from "@budibase/backend-core/tests"
 import { Installation } from "@budibase/types"
+import * as db from "../../db"
 
 export function install(): Installation {
   return {
     _id: "install",
+    _rev: db.rev(),
     installId: generator.guid(),
     version: generator.string()
   }
