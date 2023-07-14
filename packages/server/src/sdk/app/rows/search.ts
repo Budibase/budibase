c9633943b2c5d19ec6980be85a5971fa8341d88a
Adria Navarro <adria@budibase.com>
2023-07-14 17:04:59 +0200

    Moving files and functions

---
+++ packages/server/src/sdk/app/rows/search.ts
@@ -0,0 +1,38 @@
+import { Ctx, SearchFilters } from "@budibase/types"
+import { isExternalTable } from "../../../integrations/utils"
+import * as internal from "./search/internal"
+import * as external from "./search/external"
+
+export interface SearchParams {
+  tableId: string
+  paginate: boolean
+  query?: SearchFilters
+  bookmark?: number
+  limit: number
+  sort?: string
+  sortOrder?: string
+  sortType?: string
+}
+
+function pickApi(tableId: any) {
+  if (isExternalTable(tableId)) {
+    return external
+  }
+  return internal
+}
+
+export async function search(tableId: string, ctx: Ctx) {
+  return pickApi(tableId).search(ctx)
+}
+
+export async function exportRows(tableId: string, ctx: Ctx) {
+  return pickApi(tableId).exportRows(ctx)
+}
+
+export async function fetch(tableId: string, ctx: Ctx) {
+  return pickApi(tableId).fetch(ctx)
+}
+
+export async function fetchView(tableId: string, ctx: Ctx) {
+  return pickApi(tableId).fetchView(ctx)
+}
---
+++ packages/server/src/sdk/app/rows/search/external.ts
@@ -0,0 +1,152 @@
+import {
+  SortJson,
+  SortDirection,
+  Operation,
+  PaginationJson,
+  IncludeRelationship,
+  Row,
+  Ctx,
+} from "@budibase/types"
+import sdk from "../../../../sdk"
+import { handleRequest } from "../../../../api/controllers/row/external"
+import { breakExternalTableId } from "../../../../integrations/utils"
+import { cleanExportRows } from "../utils"
+import { apiFileReturn } from "../../../../utilities/fileSystem"
+
+export async function search(ctx: Ctx) {
+  const tableId = ctx.params.tableId
+  const { paginate, query, ...params } = ctx.request.body
+  let { bookmark, limit } = params
+  if (!bookmark && paginate) {
+    bookmark = 1
+  }
+  let paginateObj = {}
+
+  if (paginate) {
+    paginateObj = {
+      // add one so we can track if there is another page
+      limit: limit,
+      page: bookmark,
+    }
+  } else if (params && limit) {
+    paginateObj = {
+      limit: limit,
+    }
+  }
+  let sort: SortJson | undefined
+  if (params.sort) {
+    const direction =
+      params.sortOrder === "descending"
+        ? SortDirection.DESCENDING
+        : SortDirection.ASCENDING
+    sort = {
+      [params.sort]: { direction },
+    }
+  }
+  try {
+    const rows = (await handleRequest(Operation.READ, tableId, {
+      filters: query,
+      sort,
+      paginate: paginateObj as PaginationJson,
+      includeSqlRelationships: IncludeRelationship.INCLUDE,
+    })) as Row[]
+    let hasNextPage = false
+    if (paginate && rows.length === limit) {
+      const nextRows = (await handleRequest(Operation.READ, tableId, {
+        filters: query,
+        sort,
+        paginate: {
+          limit: 1,
+          page: bookmark * limit + 1,
+        },
+        includeSqlRelationships: IncludeRelationship.INCLUDE,
+      })) as Row[]
+      hasNextPage = nextRows.length > 0
+    }
+    // need wrapper object for bookmarks etc when paginating
+    return { rows, hasNextPage, bookmark: bookmark + 1 }
+  } catch (err: any) {
+    if (err.message && err.message.includes("does not exist")) {
+      throw new Error(
+        `Table updated externally, please re-fetch - ${err.message}`
+      )
+    } else {
+      throw err
+    }
+  }
+}
+
+export async function exportRows(ctx: Ctx) {
+  const { datasourceId, tableName } = breakExternalTableId(ctx.params.tableId)
+  const format = ctx.query.format as string
+  const { columns } = ctx.request.body
+  const datasource = await sdk.datasources.get(datasourceId!)
+  if (!datasource || !datasource.entities) {
+    ctx.throw(400, "Datasource has not been configured for plus API.")
+  }
+
+  if (ctx.request.body.rows) {
+    ctx.request.body = {
+      query: {
+        oneOf: {
+          _id: ctx.request.body.rows.map((row: string) => {
+            const ids = JSON.parse(
+              decodeURI(row).replace(/'/g, `"`).replace(/%2C/g, ",")
+            )
+            if (ids.length > 1) {
+              ctx.throw(400, "Export data does not support composite keys.")
+            }
+            return ids[0]
+          }),
+        },
+      },
+    }
+  }
+
+  let result = await search(ctx)
+  let rows: Row[] = []
+
+  // Filter data to only specified columns if required
+
+  if (columns && columns.length) {
+    for (let i = 0; i < result.rows.length; i++) {
+      rows[i] = {}
+      for (let column of columns) {
+        rows[i][column] = result.rows[i][column]
+      }
+    }
+  } else {
+    rows = result.rows
+  }
+
+  if (!tableName) {
+    ctx.throw(400, "Could not find table name.")
+  }
+  let schema = datasource.entities[tableName].schema
+  let exportRows = cleanExportRows(rows, schema, format, columns)
+
+  let headers = Object.keys(schema)
+
+  // @ts-ignore
+  const exporter = exporters[format]
+  const filename = `export.${format}`
+
+  // send down the file
+  ctx.attachment(filename)
+  return apiFileReturn(exporter(headers, exportRows))
+}
+
+export async function fetch(ctx: Ctx) {
+  const tableId = ctx.params.tableId
+  return handleRequest(Operation.READ, tableId, {
+    includeSqlRelationships: IncludeRelationship.INCLUDE,
+  })
+}
+
+export async function fetchView(ctx: Ctx) {
+  // there are no views in external datasources, shouldn't ever be called
+  // for now just fetch
+  const split = ctx.params.viewName.split("all_")
+  ctx.params.tableId = split[1] ? split[1] : split[0]
+  return fetch(ctx)
+}
