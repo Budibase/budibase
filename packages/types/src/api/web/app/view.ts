6f563d14bb972329a2d6d15ed76ef8c370dbb5aa
Adria Navarro <adria@budibase.com>
2023-07-18 12:06:35 +0200

    Better typing responses

--- packages/types/src/api/web/app/view.ts
+++ packages/types/src/api/web/app/view.ts
@@ -7,3 +7,8 @@ export interface FetchViewResponse {
 export interface ViewResponse {
   data: ViewV2
 }
+
+export type CreateViewRequest = Omit<
+  ViewV2,
+  "_id" | "_rev" | "createdAt" | "updatedAt"
+>
