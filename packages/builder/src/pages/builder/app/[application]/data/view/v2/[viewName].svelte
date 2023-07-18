5b6af98ad2c4e2107451e853c6725c471d6e825b
Adria Navarro <adria@budibase.com>
2023-07-18 18:32:52 +0200

    ViewV2 page

---
+++ packages/builder/src/pages/builder/app/[application]/data/view/v2/[viewName].svelte
@@ -0,0 +1,22 @@
+<script>
+  import { views } from "stores/backend"
+  import { Grid } from "@budibase/frontend-core"
+  import { API } from "api"
+
+  $: tableId = $views.selected?.id
+</script>
+
+<div class="wrapper">
+  <Grid {API} {tableId} datasourceType="view" showAvatars={false} />
+</div>
+
+<style>
+  .wrapper {
+    flex: 1 1 auto;
+    margin: -28px -40px -40px -40px;
+    display: flex;
+    flex-direction: column;
+    background: var(--background);
+    overflow: hidden;
+  }
+</style>
