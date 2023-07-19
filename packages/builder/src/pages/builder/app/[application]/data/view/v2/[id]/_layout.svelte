16764e66349994b72977e815361036bb845f8132
Adria Navarro <adria@budibase.com>
2023-07-19 11:06:59 +0200

    Fetch

---
+++ packages/builder/src/pages/builder/app/[application]/data/view/v2/[id]/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { views } from "stores/backend"
+  import { syncURLToState } from "helpers/urlStateSync"
+  import * as routify from "@roxi/routify"
+  import { onDestroy } from "svelte"
+  import { store } from "builderStore"
+
+  $: viewName = $views.selectedViewName
+  $: store.actions.websocket.selectResource(viewName)
+
+  const stopSyncing = syncURLToState({
+    urlParam: "id",
+    stateKey: "selectedViewId",
+    validate: id => $views.list?.some(view => view._id === id),
+    update: id => {
+      const view = $views.list.find(v => v._id === id)
+      views.select(view.name)
+    },
+    fallbackUrl: "../",
+    store: views,
+    routify,
+    decode: decodeURIComponent,
+  })
+
+  onDestroy(stopSyncing)
+</script>
+
+<slot />
