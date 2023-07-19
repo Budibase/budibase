e31798c488c09115d287051443d4d8e08826d70f
Adria Navarro <adria@budibase.com>
2023-07-19 15:49:46 +0200

    Remove fetch view

--- packages/server/src/sdk/app/views/index.ts
+++ packages/server/src/sdk/app/views/index.ts
@@ -1,22 +1,8 @@
 import { HTTPError, context } from "@budibase/backend-core"
 import { ViewV2 } from "@budibase/types"
-import * as utils from "../../../db/utils"
+
 import sdk from "../../../sdk"
 import { utils as coreUtils } from "@budibase/backend-core"
-
-export async function get(viewId: string): Promise<ViewV2 | undefined> {
-  const db = context.getAppDB()
-  try {
-    const result = await db.get<ViewV2>(viewId)
-    return result
-  } catch (err: any) {
-    if (err.status === 404) {
-      return undefined
-    }
-
-    throw err
-  }
-}
 
 export async function create(
   tableId: string,
@@ -48,9 +34,6 @@ function isV2(view: object): view is ViewV2 {
 
 export async function remove(tableId: string, viewId: string): Promise<void> {
   const db = context.getAppDB()
-
-  const doc = await sdk.views.get(viewId)
-  await db.remove(viewId, doc!._rev)
 
   const table = await sdk.tables.getTable(tableId)
   const view = Object.values(table.views!).find(v => isV2(v) && v.id === viewId)
