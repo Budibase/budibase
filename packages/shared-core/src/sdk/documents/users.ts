3abe5d4cb21dda1a9cba0f1764499ad96d87f817
mike12345567 <me@michaeldrury.co.uk>
2023-07-20 18:34:12 +0100

    Frontend work to support logging in as an app builder - also making sure when a new app is created that the user is assigned app access to it.

--- packages/shared-core/src/sdk/documents/users.ts
+++ packages/shared-core/src/sdk/documents/users.ts
@@ -3,6 +3,9 @@ import { getProdAppID } from "./applications"
 
 // checks if a user is specifically a builder, given an app ID
 export function isBuilder(user: User | ContextUser, appId?: string) {
+  if (!user) {
+    return false
+  }
   if (user.builder?.global) {
     return true
   } else if (appId && user.builder?.apps?.includes(getProdAppID(appId))) {
@@ -15,6 +18,9 @@ export function isBuilder(user: User | ContextUser, appId?: string) {
 // in future whether someone has admin permissions and whether they are
 // an admin for a specific resource could be separated
 export function isAdmin(user: User | ContextUser) {
+  if (!user) {
+    return false
+  }
   return hasAdminPermissions(user)
 }
 
@@ -22,12 +28,20 @@ export function isAdminOrBuilder(user: User | ContextUser, appId?: string) {
   return isBuilder(user, appId) || isAdmin(user)
 }
 
+// check if they are a builder within an app (not necessarily a global builder)
+export function hasAppBuilderPermissions(user?: User | ContextUser) {
+  if (!user) {
+    return false
+  }
+  return !user.builder?.global && user.builder?.apps?.length !== 0
+}
+
 // checks if a user is capable of building any app
 export function hasBuilderPermissions(user?: User | ContextUser) {
   if (!user) {
     return false
   }
-  return user.builder?.global || user.builder?.apps?.length !== 0
+  return user.builder?.global || hasAppBuilderPermissions(user)
 }
 
 // checks if a user is capable of being an admin
