2dcb1a67a12eb105be4e51e61bc7d9382b88738a
Adria Navarro <adria@budibase.com>
2023-07-21 14:16:43 +0200

    Add service name to the logs only on self-host

--- packages/backend-core/src/logging/pino/logger.ts
+++ packages/backend-core/src/logging/pino/logger.ts
@@ -19,8 +19,14 @@ if (!env.DISABLE_PINO_LOGGER) {
         return { level: label.toUpperCase() }
       },
       bindings: () => {
-        return {
-          service: env.SERVICE_NAME,
+        if (env.SELF_HOSTED) {
+          // "service" is being injected in datadog using the pod names,
+          // so we should leave it blank to allow the default behaviour if it's not running self-hosted
+          return {
+            service: env.SERVICE_NAME,
+          }
+        } else {
+          return {}
         }
       },
     },
