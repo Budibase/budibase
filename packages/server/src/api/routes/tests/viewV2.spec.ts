7a4d399c7db680ae1f846db6144bb7d328d8e7c6
Adria Navarro <adria@budibase.com>
2023-07-25 15:35:48 +0200

    Update test

--- packages/server/src/api/routes/tests/viewV2.spec.ts
+++ packages/server/src/api/routes/tests/viewV2.spec.ts
@@ -94,6 +94,27 @@ describe("/v2/views", () => {
       view = await config.api.viewV2.create({ name: "View A" })
     })
 
+    it("can update an existing view data", async () => {
+      const tableId = config.table!._id!
+      await config.api.viewV2.update({
+        ...view,
+        query: { equal: { newField: "thatValue" } },
+      })
+
+      expect(await config.api.table.get(tableId)).toEqual({
+        ...config.table,
+        views: {
+          [view.name]: {
+            ...view,
+            query: { equal: { newField: "thatValue" } },
+            schema: expect.anything(),
+          },
+        },
+        _rev: expect.any(String),
+        updatedAt: expect.any(String),
+      })
+    })
+
     it("can update an existing view name", async () => {
       const tableId = config.table!._id!
       await config.api.viewV2.update({ ...view, name: "View B" })
