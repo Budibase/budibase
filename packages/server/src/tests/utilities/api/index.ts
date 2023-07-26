77c2ce590c69f6cb56564cae93f4571d6cc86ac1
Adria Navarro <adria@budibase.com>
2023-07-26 12:29:19 +0200

    Use test api for patch

--- packages/server/src/tests/utilities/api/index.ts
+++ packages/server/src/tests/utilities/api/index.ts
@@ -1,13 +1,16 @@
 import TestConfiguration from "../TestConfiguration"
+import { RowAPI } from "./row"
 import { TableAPI } from "./table"
 import { ViewV2API } from "./viewV2"
 
 export default class API {
   table: TableAPI
   viewV2: ViewV2API
+  row: RowAPI
 
   constructor(config: TestConfiguration) {
     this.table = new TableAPI(config)
     this.viewV2 = new ViewV2API(config)
+    this.row = new RowAPI(config)
   }
 }
