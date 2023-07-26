f6e6243c81f47cd4d558e917a7319fa92f1d9ba9
Adria Navarro <adria@budibase.com>
2023-07-26 12:20:06 +0200

    Type patch

--- packages/types/src/api/web/app/rows.ts
+++ packages/types/src/api/web/app/rows.ts
@@ -1,3 +1,9 @@
+import { Row } from "../../../documents"
+
+export interface PatchRowRequest extends Row {}
+
+export interface PatchRowResponse extends Row {}
+
 export interface SearchResponse {
   rows: any[]
 }
