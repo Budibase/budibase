f82c10782fb3da16e8022433d0dc440e686dbdef
Adria Navarro <adria@budibase.com>
2023-07-27 17:14:34 +0200

    Log heap usage

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -7,7 +7,7 @@ then
   export NODE_OPTIONS="--max-old-space-size=4096"
   node ../../node_modules/jest/bin/jest.js --version
   echo "node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js"
-  node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js
+  node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js --logHeapUsage
 else
   # --maxWorkers performs better in development
   echo "jest --coverage --maxWorkers=2 --forceExit"
