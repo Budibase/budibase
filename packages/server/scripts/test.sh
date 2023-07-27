2e1903f95577275e102dfbf26de80e683541f9fb
Adria Navarro <adria@budibase.com>
2023-07-27 17:40:44 +0200

    Log

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -6,7 +6,7 @@ then
   # --runInBand performs better in ci where resources are limited
   export NODE_OPTIONS="--max-old-space-size=4096"
   node ../../node_modules/jest/bin/jest.js --version
-  echo "node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js"
+  echo "node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js --logHeapUsage"
   node --expose-gc --no-compilation-cache ../../node_modules/jest/bin/jest.js --logHeapUsage
 else
   # --maxWorkers performs better in development
