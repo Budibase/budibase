454f832b75ebf3a9b991b0e476aab5eedeeb5b8b
mike12345567 <me@michaeldrury.co.uk>
2023-07-28 12:58:05 +0100

    Updates after running through tests, adding mocking of app builders feature for test cases to work.

--- packages/worker/src/api/routes/global/tests/appBuilder.spec.ts
+++ packages/worker/src/api/routes/global/tests/appBuilder.spec.ts
@@ -1,4 +1,5 @@
 import { TestConfiguration, structures } from "../../../../tests"
+import { mocks } from "@budibase/backend-core/tests"
 import { User } from "@budibase/types"
 
 const MOCK_APP_ID = "app_a"
@@ -24,18 +25,31 @@ describe("/api/global/users/:userId/app/builder", () => {
     return response.body as User
   }
 
+  describe("Confirm pro license", () => {
+    it("should 400 with licensing", async () => {
+      const user = await newUser()
+      const resp = await config.api.users.grantBuilderToApp(
+        user._id!,
+        MOCK_APP_ID,
+        400
+      )
+      expect(resp.body.message).toContain("Feature not enabled")
+    })
+  })
+
   describe("PATCH /api/global/users/:userId/app/:appId/builder", () => {
     it("should be able to grant a user access to a particular app", async () => {
+      mocks.licenses.useAppBuilders()
       const user = await newUser()
       await config.api.users.grantBuilderToApp(user._id!, MOCK_APP_ID)
       const updated = await getUser(user._id!)
-      expect(updated.builder?.appBuilder).toBe(true)
       expect(updated.builder?.apps![0]).toBe(MOCK_APP_ID)
     })
   })
 
   describe("DELETE /api/global/users/:userId/app/:appId/builder", () => {
     it("should allow revoking access", async () => {
+      mocks.licenses.useAppBuilders()
       const user = await newUser()
       await config.api.users.grantBuilderToApp(user._id!, MOCK_APP_ID)
       let updated = await getUser(user._id!)
