317bbdaca53914981c21465ccaab0d09d0968265
Peter Clement <PClmnt@users.noreply.github.com>
2023-07-31 15:28:11 +0100

    Reworked Column Configuration in the data section (#11379)
    * base work for using popover to create and edit columns
    
    * more work to enable editing column in popover
    
    * update styling of column type configs
    
    * add new option picker component
    
    * some updates to how the popover is opened and the new picker
    
    * more updates to support the popover handling correctly
    
    * update the popover to support a custom z index
    
    * some styling around the colour picker
    
    * update naming
    
    * fix lint errors
    
    * fix lint
    
    * update filename
    
    * incremental column numbers based on existing schema
    
    * move func declaration
    
    * add option color object to schema not constraints
    
    * ux / pr comment updates
    
    * undefined var
    
    * fix issue with deleting option
    
    * change background color
    
    * update popove z-index

--- packages/bbui/src/index.js
+++ packages/bbui/src/index.js
@@ -84,7 +84,7 @@ export { default as IconSideNavItem } from "./IconSideNav/IconSideNavItem.svelte
 export { default as Slider } from "./Form/Slider.svelte"
 export { default as Accordion } from "./Accordion/Accordion.svelte"
 export { default as File } from "./Form/File.svelte"
-
+export { default as OptionSelectDnD } from "./OptionSelectDnD/OptionSelectDnD.svelte"
 // Renderers
 export { default as BoldRenderer } from "./Table/BoldRenderer.svelte"
 export { default as CodeRenderer } from "./Table/CodeRenderer.svelte"
