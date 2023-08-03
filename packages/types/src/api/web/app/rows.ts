cb59a2fc9bdeb7d2724a83dd71589d88086eb798
Adria Navarro <adria@budibase.com>
2023-08-03 05:16:10 +0200

    Type search request

--- packages/types/src/api/web/app/rows.ts
+++ packages/types/src/api/web/app/rows.ts
@@ -1,3 +1,4 @@
+import { SearchParams } from "../../../sdk"
 import { Row } from "../../../documents"
 
 export interface PatchRowRequest extends Row {
@@ -8,6 +9,8 @@ export interface PatchRowRequest extends Row {
 
 export interface PatchRowResponse extends Row {}
 
-export interface SearchResponse {
+export interface SearchRowRequest extends SearchParams {}
+
+export interface SearchRowResponse {
   rows: any[]
 }
