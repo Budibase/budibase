32e09cf131ece2eb67e3fb83f3bbb69660690e6a
Adria Navarro <adria@budibase.com>
2023-08-09 12:13:38 +0300

    Lint

--- packages/backend-core/scripts/build.js
+++ packages/backend-core/scripts/build.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const { join } = require("path")
-const fs = require("fs")
 const coreBuild = require("../../../scripts/build")
 
 coreBuild("./src/plugin/index.ts", "./dist/plugins.js")
