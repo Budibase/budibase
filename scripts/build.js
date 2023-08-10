45c896ee542130dbd52faa55c1dc267fd297dcde
Adria Navarro <adria@budibase.com>
2023-08-10 11:48:18 +0300

    Fix build script with no pro locally

--- scripts/build.js
+++ scripts/build.js
@@ -22,7 +22,10 @@ function runBuild(entry, outfile) {
     fs.readFileSync(tsconfig, "utf-8")
   )
 
-  if (!fs.existsSync("../pro/src")) {
+  if (
+    !fs.existsSync("../pro/src") &&
+    tsconfigPathPluginContent.compilerOptions?.paths
+  ) {
     // If we don't have pro, we cannot bundle backend-core.
     // Otherwise, the main context will not be shared between libraries
     delete tsconfigPathPluginContent.compilerOptions.paths[
