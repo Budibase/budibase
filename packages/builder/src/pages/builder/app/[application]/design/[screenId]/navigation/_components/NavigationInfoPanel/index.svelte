bd33c0eade74c1f4fa053a1e1df6839696115c8e
Gerard Burns <gjburns9@hotmail.com>
2023-08-14 13:30:15 +0100

    New Nav component

---
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/navigation/_components/NavigationInfoPanel/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import RightPanel from "components/design/RightPanel.svelte"
+  import CustomizePane from "./CustomizePane.svelte"
+  import SettingsPane from "./SettingsPane.svelte"
+</script>
+
+<RightPanel title="Screen" icon="WebPage">
+  <SettingsPane />
+  <CustomizePane />
+</RightPanel>
