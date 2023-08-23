71e45e0b9678cffb26ddb217d7ebf97be0b1014d
mike12345567 <me@michaeldrury.co.uk>
2023-08-23 15:29:19 +0100

    Updating pro reference.

--- jestTestcontainersConfigGenerator.js
+++ jestTestcontainersConfigGenerator.js
@@ -14,13 +14,3 @@ module.exports = () => {
     }
   }
 }
-
-// module.exports = () => {
-//   return {
-//     dockerCompose: {
-//       composeFilePath: "../../hosting",
-//       composeFile: "docker-compose.test.yaml",
-//       startupTimeout: 10000,
-//     },
-//   }
-// }
