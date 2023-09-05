fa38057fb619b619a01188240929356c8f27bcfd
Adria Navarro <adria@budibase.com>
2023-09-05 15:46:39 +0200

    Keep structure building test dist

--- packages/backend-core/scripts/build.js
+++ packages/backend-core/scripts/build.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
 const coreBuild = require("../../../scripts/build")
+const { exec } = require("child_process")
 
 coreBuild("./src/plugin/index.ts", "./dist/plugins.js")
 coreBuild("./src/index.ts", "./dist/index.js")
-coreBuild("./tests/index.ts", "./dist/tests.js")
+
+const child = exec(
+  `esbuild ./tests/*.[tj]s ./tests/**/*.[tj]s --outdir=./dist/tests`
+)
+child.stdout.on("data", data => {
+  console.log(`stdout: ${data}`)
+})
+child.stderr.on("data", data => {
+  console.error(`stderr: ${data}`)
+})
+child.on("close", code => {
+  console.log(`esbuild process exited with code ${code}`)
+})
