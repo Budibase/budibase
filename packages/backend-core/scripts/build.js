ec8875d55b1c83fc5bccde286694412dae8e7456
Adria Navarro <adria@budibase.com>
2023-09-05 16:19:15 +0200

    Use esbuild js

--- packages/backend-core/scripts/build.js
+++ packages/backend-core/scripts/build.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 const coreBuild = require("../../../scripts/build")
-const { exec } = require("child_process")
 
 coreBuild("./src/plugin/index.ts", "./dist/plugins.js")
 coreBuild("./src/index.ts", "./dist/index.js")
 
-const child = exec(
-  `esbuild ./tests/*.[tj]s ./tests/**/*.[tj]s --outdir=./dist/tests`
-)
-child.stdout.on("data", data => {
-  console.log(`stdout: ${data}`)
-})
-child.stderr.on("data", data => {
-  console.error(`stderr: ${data}`)
-})
-child.on("close", code => {
-  console.log(`esbuild process exited with code ${code}`)
-})
+const esbuild = require("esbuild")
+const glob = require("glob")
+
+const inputFiles = glob.sync("./tests/**/*.[tj]s", { nodir: true })
+const outputDir = "./dist/tests"
+
+esbuild
+  .build({
+    entryPoints: inputFiles,
+    outdir: outputDir,
+  })
+  .catch(() => process.exit(1))
