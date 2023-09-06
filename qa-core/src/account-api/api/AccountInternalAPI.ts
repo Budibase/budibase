b526dc5ca3b8ac0b76b06a5c3102ac48bdefae65
Rory Powell <rory.codes@gmail.com>
2023-09-06 10:38:27 +0100

    Separate license management from license activation

--- qa-core/src/account-api/api/AccountInternalAPI.ts
+++ qa-core/src/account-api/api/AccountInternalAPI.ts
@@ -3,7 +3,6 @@ import {
   AccountAPI,
   LicenseAPI,
   AuthAPI,
-  StripeAPI,
 } from "./apis"
 import { State } from "../../types"
 
@@ -13,13 +12,11 @@ export default class AccountInternalAPI {
   auth: AuthAPI
   accounts: AccountAPI
   licenses: LicenseAPI
-  stripe: StripeAPI
 
   constructor(state: State) {
     this.client = new AccountInternalAPIClient(state)
     this.auth = new AuthAPI(this.client)
     this.accounts = new AccountAPI(this.client)
     this.licenses = new LicenseAPI(this.client)
-    this.stripe = new StripeAPI(this.client)
   }
 }
