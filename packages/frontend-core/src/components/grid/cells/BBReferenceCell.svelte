ae237d3714e92c26c525e4ac8c29f1a16cbaac40
Adria Navarro <adria@budibase.com>
2023-09-15 17:05:38 +0200

    Reference cell

---
+++ packages/frontend-core/src/components/grid/cells/BBReferenceCell.svelte
@@ -0,0 +1,16 @@
+<script>
+  import RelationshipCell from "./RelationshipCell.svelte"
+  import { TableNames } from "constants"
+  // import { FieldSubtype } from "@budibase/types"
+
+  const subtypeToTable = {
+    ["user"]: TableNames.USERS,
+  }
+
+  const schema = {
+    ...$$props.schema,
+    tableId: subtypeToTable[$$props.schema.subtype],
+  }
+</script>
+
+<RelationshipCell {...$$props} {schema} />
