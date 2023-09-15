538f8dc017136355de8817dd91fb6d4d9730e462
Adria Navarro <adria@budibase.com>
2023-09-15 18:52:50 +0200

    Implement search and push

--- packages/frontend-core/src/components/grid/cells/BBReferenceCell.svelte
+++ packages/frontend-core/src/components/grid/cells/BBReferenceCell.svelte
@@ -1,16 +1,36 @@
 <script>
+  import { getContext } from "svelte"
   import RelationshipCell from "./RelationshipCell.svelte"
   import { FieldSubtype } from "@budibase/types"
-  import { TableNames } from "../../../constants"
 
-  const subtypeToTable = {
-    [FieldSubtype.USER]: TableNames.USERS,
-  }
+  const { API } = getContext("grid")
+  const { subtype } = $$props.schema
 
   const schema = {
     ...$$props.schema,
-    tableId: subtypeToTable[$$props.schema.subtype],
+    tableId: "external",
+  }
+
+  async function searchFunction(searchParams) {
+    if (subtype !== FieldSubtype.USER) {
+      throw `Search for '${subtype}' not implemented`
+    }
+
+    const results = await API.searchUsers({
+      ...searchParams,
+    })
+    return {
+      ...results,
+      data: undefined,
+      rows: results.data,
+    }
   }
 </script>
 
-<RelationshipCell {...$$props} {schema} />
+<RelationshipCell
+  {...$$props}
+  {schema}
+  {searchFunction}
+  primaryDisplay={"email"}
+  relationshipType={"many-to-one"}
+/>
