5dbe7f1f3d243d6db71b102596bab9f46b5d3308
Adria Navarro <adria@budibase.com>
2023-09-15 11:21:10 +0200

    Handle ids

--- packages/server/src/utilities/rowProcessor/bbReferenceProcessor.ts
+++ packages/server/src/utilities/rowProcessor/bbReferenceProcessor.ts
@@ -1,6 +1,27 @@
+import { cache } from "@budibase/backend-core"
+import { utils } from "@budibase/shared-core"
 import { FieldSubtype } from "@budibase/types"
 
-export async function processOutputBBReferences(
+export async function processInputBBReferences(
   value: string,
   subtype: FieldSubtype
-) {}
+) {
+  const result = []
+  const ids = value.split(",").map((id: string) => id.trim())
+
+  switch (subtype) {
+    case FieldSubtype.USER:
+      for (const id of ids) {
+        result.push(await cache.user.getUser(id))
+      }
+      break
+    default:
+      utils.unreachable(subtype)
+  }
+
+  if (result.length > 1) {
+    return result
+  }
+
+  return result[0]
+}
