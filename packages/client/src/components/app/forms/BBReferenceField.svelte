2bc96addc28b0fae4866822b4b4c7a2bb4812245
Adria Navarro <adria@budibase.com>
2023-09-20 16:44:26 +0200

    Register BBReferenceField

---
+++ packages/client/src/components/app/forms/BBReferenceField.svelte
@@ -0,0 +1,11 @@
+<script>
+  import RelationshipField from "./RelationshipField.svelte"
+
+  const schema = {
+    ...$$props.schema,
+    // This is not really used, just adding some content to be able to render the relationship cell
+    tableId: "external",
+  }
+</script>
+
+<RelationshipField {...$$props} {schema} datasourceType={"user"} />
