2b01ab16a5741504b3a382fb3e3177b5dddb7e31
Andrew Kingston <andrew@kingston.dev>
2023-10-02 08:27:28 +0100

    Remove log

--- packages/client/src/stores/initialise.js
+++ packages/client/src/stores/initialise.js
@@ -3,7 +3,6 @@ import { appStore } from "./app"
 import { orgStore } from "./org"
 
 export async function initialise() {
-  console.log("initialise!")
   await routeStore.actions.fetchRoutes()
   await appStore.actions.fetchAppDefinition()
   await orgStore.actions.init()
