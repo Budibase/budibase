4d9943579c586d740d80c84e1bea5e96db48370c
Adria Navarro <adria@budibase.com>
2023-10-02 18:18:42 +0200

    Unify dependencies

--- scripts/build-single-image.sh
+++ scripts/build-single-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ${BASH_SOURCE%/*}/updateVersions.sh
 yarn build --scope @budibase/server --scope @budibase/worker
+source ${BASH_SOURCE%/*}/removeWorkspaceDependencies.sh
 docker build -f hosting/single/Dockerfile -t budibase:latest .
 source ${BASH_SOURCE%/*}/resetVersions.sh
