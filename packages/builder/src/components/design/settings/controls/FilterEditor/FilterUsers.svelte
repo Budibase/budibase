0702bc6c336758ad95ff0c5f49027c533f426aca
Adria Navarro <adria@budibase.com>
2023-10-03 13:10:53 +0200

    Sanity filter on change

--- packages/builder/src/components/design/settings/controls/FilterEditor/FilterUsers.svelte
+++ packages/builder/src/components/design/settings/controls/FilterEditor/FilterUsers.svelte
@@ -5,6 +5,7 @@
   import { API } from "api"
 
   export let value = null
+  export let multiple = false
 
   $: fetch = fetchData({
     API,
@@ -17,11 +18,21 @@
   })
 
   $: options = $fetch.rows
+
+  function onChange(e) {
+    const val = e.detail
+    if (!val) {
+      value = val
+    } else {
+      value = Array.isArray(val) ? val : [val]
+    }
+  }
 </script>
 
 <Select
+  value={multiple ? value : value[0]}
   autocomplete
-  bind:value
+  on:change={onChange}
   {options}
   getOptionLabel={option => option.email}
   getOptionValue={option => option._id}
