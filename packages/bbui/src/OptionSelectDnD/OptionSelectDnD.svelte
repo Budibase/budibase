615719398b0604bb6c772609398fc5e03c121521
Mel O'Hagan <mel@budibase.com>
2023-10-03 17:02:53 +0100

    Init options onMount

--- packages/bbui/src/OptionSelectDnD/OptionSelectDnD.svelte
+++ packages/bbui/src/OptionSelectDnD/OptionSelectDnD.svelte
@@ -9,7 +9,6 @@
   export let constraints
   export let optionColors = {}
   let options = []
-  let changed = false
 
   let colorPopovers = []
   let anchors = []
@@ -22,14 +21,6 @@
     "hsla(240, 90%, 75%, 0.3)",
     "hsla(320, 90%, 75%, 0.3)",
   ]
-  $: {
-    if (constraints.inclusion.length && !changed) {
-      options = constraints.inclusion.map(value => ({
-        name: value,
-        id: Math.random(),
-      }))
-    }
-  }
   const removeInput = idx => {
     delete optionColors[options[idx].name]
     constraints.inclusion = constraints.inclusion.filter((e, i) => i !== idx)
@@ -66,7 +57,6 @@
   }
 
   const handleNameChange = (optionName, idx, value) => {
-    changed = true
     constraints.inclusion[idx] = value
     options[idx].name = value
     optionColors[value] = optionColors[optionName]
@@ -82,6 +72,11 @@
     // Initialize anchor arrays on mount, assuming 'options' is already populated
     colorPopovers = constraints.inclusion.map(() => undefined)
     anchors = constraints.inclusion.map(() => undefined)
+
+    options = constraints.inclusion.map(value => ({
+      name: value,
+      id: Math.random(),
+    }))
   })
 </script>
 
