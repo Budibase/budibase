6c46e0287dc30d2978a8ed4a0a6f4fa2b5e2f995
jvcalderon <jose@budibase.com>
2023-10-04 13:27:50 +0200

    Revert "Add support for Posthog's feature flags"
    This reverts commit c5599a666a1e734a7337ff1ccc5e09c86ccc3812.

--- packages/types/src/sdk/featureFlag.ts
+++ packages/types/src/sdk/featureFlag.ts
@@ -1,8 +1,5 @@
 export enum FeatureFlag {
   LICENSING = "LICENSING",
-  // Feature IDs in Posthog
-  PER_CREATOR_PER_USER_PRICE = "18873",
-  PER_CREATOR_PER_USER_PRICE_ALERT = "18530"
 }
 
 export interface TenantFeatureFlags {
