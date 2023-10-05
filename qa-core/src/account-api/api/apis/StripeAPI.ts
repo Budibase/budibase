678033cc8b7276e5823536925cfebc577527f764
Mitch-Budibase <mitch@budibase.com>
2023-10-05 17:39:40 +0100

    License Key - Activate & Manage Tests
    There are two test files, license.activate.spec.ts and license.manage.spec.ts
    
    These test files each contain a test:
    - Creates, activates, and deletes an online license for a self hosted account - license.activate.spec.ts
    - Retrieves plans, creates checkout session, and updates license - license.manage.spec.ts
    
    Updated and created API files
    - StripeAPI
    - LicenseAPI
    - internal-api LicenseAPI
    - index & AccountInternalAPI also updated to reflect API file changes

---
+++ qa-core/src/account-api/api/apis/StripeAPI.ts
@@ -0,0 +1,64 @@
+import AccountInternalAPIClient from "../AccountInternalAPIClient"
+import BaseAPI from "./BaseAPI"
+import { APIRequestOpts } from "../../../types"
+
+export default class StripeAPI extends BaseAPI {
+    client: AccountInternalAPIClient
+
+    constructor(client: AccountInternalAPIClient) {
+        super()
+        this.client = client
+    }
+
+    async createCheckoutSession(
+        priceId: string,
+        opts: APIRequestOpts = { status: 200 }
+    ) {
+        return this.doRequest(() => {
+            return this.client.post(`/api/stripe/checkout-session`, {
+                body: { priceId },
+            })
+        }, opts)
+    }
+
+    async checkoutSuccess(opts: APIRequestOpts = { status: 200 }) {
+        return this.doRequest(() => {
+            return this.client.post(`/api/stripe/checkout-success`)
+        }, opts)
+    }
+
+    async createPortalSession(
+        stripeCustomerId: string,
+        opts: APIRequestOpts = { status: 200 }
+    ) {
+        return this.doRequest(() => {
+            return this.client.post(`/api/stripe/portal-session`, {
+                body: { stripeCustomerId },
+            })
+        }, opts)
+    }
+
+    async linkStripeCustomer(opts: APIRequestOpts = { status: 200 }) {
+        return this.doRequest(() => {
+            return this.client.post(`/api/stripe/link`)
+        }, opts)
+    }
+
+    async getInvoices(opts: APIRequestOpts = { status: 200 }) {
+        return this.doRequest(() => {
+            return this.client.get(`/api/stripe/invoices`)
+        }, opts)
+    }
+
+    async getUpcomingInvoice(opts: APIRequestOpts = { status: 200 }) {
+        return this.doRequest(() => {
+            return this.client.get(`/api/stripe/upcoming-invoice`)
+        }, opts)
+    }
+
+    async getStripeCustomers(opts: APIRequestOpts = { status: 200 }) {
+        return this.doRequest(() => {
+            return this.client.get(`/api/stripe/customers`)
+        }, opts)
+    }
+}
