c87a67a37a4c5c2a95d319ed2ff1b0becb5cf4bd
mike12345567 <me@michaeldrury.co.uk>
2023-10-09 18:18:34 +0100

    Converting user IDs where necessary to global user IDs in the frontend, correcting the null entry when using current user IDs, or other user IDs, as a default value for a user column form type.

--- packages/client/src/components/app/forms/BBReferenceField.svelte
+++ packages/client/src/components/app/forms/BBReferenceField.svelte
@@ -1,9 +1,17 @@
 <script>
   import RelationshipField from "./RelationshipField.svelte"
+  import { sdk } from "@budibase/shared-core"
 </script>
 
 <RelationshipField
   {...$$props}
   datasourceType={"user"}
   primaryDisplay={"email"}
+  valueConversion={value => {
+    if (Array.isArray(value)) {
+      return value.map(val => sdk.users.getGlobalUserID(val))
+    } else {
+      return sdk.users.getGlobalUserID(value)
+    }
+  }}
 />
