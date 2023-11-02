c4ff4eb8bb1255d8e268d9e0c31f03f50379bf96
Conor Webb <conorwebb59@gmail.com>
2023-11-02 10:00:40 +0000

    Updated folder name, fixed ui bug.

--- packages/builder/src/components/design/settings/controls/SchemaSelect.svelte
+++ packages/builder/src/components/design/settings/controls/SchemaSelect.svelte
@@ -1,5 +1,5 @@
 <script>
-  import DataSourceSelect from "./DataSourceSelectItem/DataSourceSelect.svelte"
+  import DataSourceSelect from "./DataSourceSelect/DataSourceSelect.svelte"
 
   const otherSources = [{ name: "Custom", label: "Custom" }]
 </script>
