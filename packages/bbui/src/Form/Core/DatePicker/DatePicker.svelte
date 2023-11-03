78af50bafe9956793ace3f78c3aa77a9f367879b
Andrew Kingston <andrew@kingston.dev>
2023-11-03 19:34:12 +0000

    Use new datepicker except when a range is needed

---
+++ packages/bbui/src/Form/Core/DatePicker/DatePicker.svelte
@@ -0,0 +1,10 @@
+<script>
+  import FlatpickrDatePicker from "./FlatpickrDatePicker.svelte"
+  import SpectrumDatePicker from "./SpectrumDatePicker.svelte"
+</script>
+
+<svelte:component
+  this={$$props.range ? FlatpickrDatePicker : SpectrumDatePicker}
+  {...$$props}
+  on:change
+/>
