c25b8a1448bf44b462fbf0a83268709c6dfdbab3
Andrew Kingston <andrew@kingston.dev>
2023-11-07 14:10:16 +0000

    Expose schema context from grid and allow usage in buttons

--- packages/builder/src/components/design/settings/controls/ButtonConfiguration/ButtonSetting.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonConfiguration/ButtonSetting.svelte
@@ -10,10 +10,27 @@
   export let anchor
   export let removeButton
   export let canRemove
+  export let nested
 
   $: readableText = isJSBinding(item.text)
     ? "(JavaScript function)"
     : runtimeToReadableBinding([...bindings, componentBindings], item.text)
+
+  // If this is a nested setting (for example inside a grid or form block) then
+  // we need to mark all the settings of the actual buttons as nested too, to
+  // allow up to reference context provided by the block
+  const updatedNestedFlags = settings => {
+    if (!nested) {
+      return settings
+    }
+    // Buttons do not currently have any sections, so this works.
+    // We will need to update this in future if the normal button component
+    // gets broken into multiple settings sections
+    return settings?.map(setting => ({
+      ...setting,
+      nested: true,
+    }))
+  }
 </script>
 
 <div class="list-item-body">
@@ -24,6 +41,7 @@
       {componentBindings}
       {bindings}
       on:change
+      parseSettings={updatedNestedFlags}
     />
     <div class="field-label">{readableText || "Button"}</div>
   </div>
