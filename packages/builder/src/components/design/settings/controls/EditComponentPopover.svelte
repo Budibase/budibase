f35daee07f43120554991be35456a31a804df2c1
Dean <deanhannigan@gmail.com>
2023-11-08 16:59:00 +0000

    Added toggle-all functionality to the formblock and update context to the ComponentSettingsSection

--- packages/builder/src/components/design/settings/controls/EditComponentPopover.svelte
+++ packages/builder/src/components/design/settings/controls/EditComponentPopover.svelte
@@ -11,6 +11,7 @@
   export let componentBindings
   export let bindings
   export let parseSettings
+  export let disabled
 
   const draggable = getContext("draggable")
   const dispatch = createEventDispatcher()
@@ -90,6 +91,7 @@
       open = true
     }
   }}
+  {disabled}
 />
 
 <Popover
