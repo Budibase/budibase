d146df5f7367959bccc6d3ce3b02c9098e8e4e4e
Sam Rose <hello@samwho.dev>
2023-11-08 11:53:00 +0000

    Convert user.spec.js to user.spec.ts

--- packages/server/src/api/routes/tests/user.spec.ts
+++ packages/server/src/api/routes/tests/user.spec.ts
@@ -1,6 +1,7 @@
 import { roles, utils } from "@budibase/backend-core"
 import { checkPermissionsEndpoint } from "./utilities/TestFunctions"
 import * as setup from "./utilities"
+import { UserMetadata } from "@budibase/types"
 
 jest.setTimeout(30000)
 
@@ -28,15 +29,13 @@ describe("/users", () => {
     it("returns a list of users from an instance db", async () => {
       await config.createUser({ id: "uuidx" })
       await config.createUser({ id: "uuidy" })
-      const res = await request
-        .get(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .expect("Content-Type", /json/)
-        .expect(200)
 
-      expect(res.body.length).toBe(3)
-      expect(res.body.find(u => u._id === `ro_ta_users_us_uuidx`)).toBeDefined()
-      expect(res.body.find(u => u._id === `ro_ta_users_us_uuidy`)).toBeDefined()
+      const res = await config.api.user.fetch()
+      expect(res.length).toBe(3)
+
+      const ids = res.map(u => u._id)
+      expect(ids).toContain(`ro_ta_users_us_uuidx`)
+      expect(ids).toContain(`ro_ta_users_us_uuidy`)
     })
 
     it("should apply authorization to endpoint", async () => {
@@ -54,86 +53,61 @@ describe("/users", () => {
 
   describe("update", () => {
     it("should be able to update the user", async () => {
-      const user = await config.createUser({ id: `us_update${utils.newid()}` })
+      const user: UserMetadata = await config.createUser({
+        id: `us_update${utils.newid()}`,
+      })
       user.roleId = roles.BUILTIN_ROLE_IDS.BASIC
       delete user._rev
-      const res = await request
-        .put(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .send(user)
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body.ok).toEqual(true)
+      const res = await config.api.user.update(user)
+      expect(res.ok).toEqual(true)
     })
 
     it("should be able to update the user multiple times", async () => {
       const user = await config.createUser()
       delete user._rev
 
-      const res1 = await request
-        .put(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .send({ ...user, roleId: roles.BUILTIN_ROLE_IDS.BASIC })
-        .expect(200)
-        .expect("Content-Type", /json/)
-
-      const res = await request
-        .put(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .send({
-          ...user,
-          _rev: res1.body.rev,
-          roleId: roles.BUILTIN_ROLE_IDS.POWER,
-        })
-        .expect(200)
-        .expect("Content-Type", /json/)
-
-      expect(res.body.ok).toEqual(true)
+      const res1 = await config.api.user.update({
+        ...user,
+        roleId: roles.BUILTIN_ROLE_IDS.BASIC,
+      })
+      const res2 = await config.api.user.update({
+        ...user,
+        _rev: res1.rev,
+        roleId: roles.BUILTIN_ROLE_IDS.POWER,
+      })
+      expect(res2.ok).toEqual(true)
     })
 
     it("should require the _rev field for multiple updates", async () => {
       const user = await config.createUser()
       delete user._rev
 
-      await request
-        .put(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .send({ ...user, roleId: roles.BUILTIN_ROLE_IDS.BASIC })
-        .expect(200)
-        .expect("Content-Type", /json/)
-
-      await request
-        .put(`/api/users/metadata`)
-        .set(config.defaultHeaders())
-        .send({ ...user, roleId: roles.BUILTIN_ROLE_IDS.POWER })
-        .expect(409)
-        .expect("Content-Type", /json/)
+      await config.api.user.update({
+        ...user,
+        roleId: roles.BUILTIN_ROLE_IDS.BASIC,
+      })
+      await config.api.user.update(
+        { ...user, roleId: roles.BUILTIN_ROLE_IDS.POWER },
+        { expectStatus: 409 }
+      )
     })
   })
 
   describe("destroy", () => {
     it("should be able to delete the user", async () => {
       const user = await config.createUser()
-      const res = await request
-        .delete(`/api/users/metadata/${user._id}`)
-        .set(config.defaultHeaders())
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body.message).toBeDefined()
+      const res = await config.api.user.destroy(user._id!)
+      expect(res.message).toBeDefined()
     })
   })
 
   describe("find", () => {
     it("should be able to find the user", async () => {
       const user = await config.createUser()
-      const res = await request
-        .get(`/api/users/metadata/${user._id}`)
-        .set(config.defaultHeaders())
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body._id).toEqual(user._id)
-      expect(res.body.roleId).toEqual(roles.BUILTIN_ROLE_IDS.ADMIN)
-      expect(res.body.tableId).toBeDefined()
+      const res = await config.api.user.find(user._id!)
+      expect(res._id).toEqual(user._id)
+      expect(res.roleId).toEqual(roles.BUILTIN_ROLE_IDS.ADMIN)
+      expect(res.tableId).toBeDefined()
     })
   })
 
@@ -153,59 +127,18 @@ describe("/users", () => {
 
     it("should be able to set a flag on the user", async () => {
       await config.createUser()
-      const res = await request
-        .post(`/api/users/flags`)
-        .set(config.defaultHeaders())
-        .send({ value: "test", flag: "test" })
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body.message).toEqual("Flag set successfully")
+      const res = await config.api.user.setFlag("test", true)
+      expect(res.message).toEqual("Flag set successfully")
     })
   })
 
   describe("getFlags", () => {
     it("should get flags for a specific user", async () => {
-      let flagData = { value: "test", flag: "test" }
       await config.createUser()
-      await request
-        .post(`/api/users/flags`)
-        .set(config.defaultHeaders())
-        .send(flagData)
-        .expect(200)
-        .expect("Content-Type", /json/)
+      await config.api.user.setFlag("test", "test")
 
-      const res = await request
-        .get(`/api/users/flags`)
-        .set(config.defaultHeaders())
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body[flagData.value]).toEqual(flagData.flag)
-    })
-  })
-
-  describe("setFlag", () => {
-    it("should throw an error if a flag is not provided", async () => {
-      await config.createUser()
-      const res = await request
-        .post(`/api/users/flags`)
-        .set(config.defaultHeaders())
-        .send({ value: "test" })
-        .expect(400)
-        .expect("Content-Type", /json/)
-      expect(res.body.message).toEqual(
-        "Must supply a 'flag' field in request body."
-      )
-    })
-
-    it("should be able to set a flag on the user", async () => {
-      await config.createUser()
-      const res = await request
-        .post(`/api/users/flags`)
-        .set(config.defaultHeaders())
-        .send({ value: "test", flag: "test" })
-        .expect(200)
-        .expect("Content-Type", /json/)
-      expect(res.body.message).toEqual("Flag set successfully")
+      const res = await config.api.user.getFlags()
+      expect(res.test).toEqual("test")
     })
   })
 })
