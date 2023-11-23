920c802e1e03fe5d82ced3fb497ef7a7d33d89e0
Gerard Burns <gjburns9@hotmail.com>
2023-11-23 13:44:01 +0000

    git stash

--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/_components/AppPanel.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/_components/AppPanel.svelte
@@ -3,28 +3,42 @@
   import AppPreview from "./AppPreview.svelte"
   import { store, screenHistoryStore } from "builderStore"
   import UndoRedoControl from "components/common/UndoRedoControl.svelte"
+  import { getHorizontalResizeActions } from './resizable';
+
+  const [resizable, resizableHandle] = getHorizontalResizeActions();
 </script>
 
 <div class="app-panel">
-  <div class="header">
-    <div class="header-left">
-      <UndoRedoControl store={screenHistoryStore} />
-    </div>
-    <div class="header-right">
-      {#if $store.clientFeatures.devicePreview}
-        <DevicePreviewSelect />
-      {/if}
-    </div>
-  </div>
   <div class="content">
-    {#key $store.version}
-      <AppPreview />
-    {/key}
+    <div class="header">
+      <div class="header-left">
+        <UndoRedoControl store={screenHistoryStore} />
+      </div>
+      <div class="header-right">
+        {#if $store.clientFeatures.devicePreview}
+          <DevicePreviewSelect />
+        {/if}
+      </div>
+    </div>
+    <div class="content">
+      {#key $store.version}
+        <AppPreview />
+      {/key}
+    </div>
   </div>
+  <div
+    role="separator"
+    class="divider"
+    use:resizableHandle
+  />
 </div>
 
 <style>
   .app-panel {
+    display: flex;
+  }
+
+  .content {
     flex: 1 1 auto;
     overflow-y: auto;
     display: flex;
@@ -51,5 +65,9 @@
   }
   .content {
     flex: 1 1 auto;
+  }
+
+  .divider {
+    g
   }
 </style>
