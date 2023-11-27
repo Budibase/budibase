c7d3355bce391e692a383e8fd171326abcc859fc
Adria Navarro <adria@budibase.com>
2023-11-27 15:40:37 +0100

    Add middleware to queue

---
+++ packages/server/src/appMigrations/index.ts
@@ -0,0 +1,18 @@
+import queue from "./queue"
+import { getAppMigrationMetadata } from "./appMigrationMetadata"
+import { MIGRATIONS } from "./migrations"
+
+const latestMigration = Object.keys(MIGRATIONS).sort().reverse()[0]
+
+export async function checkMissingMigrations(appId: string) {
+  const currentVersion = await getAppMigrationMetadata(appId)
+
+  if (currentVersion < latestMigration) {
+    await queue.add(
+      {
+        appId,
+      }
+      // TODO: idempotency
+    )
+  }
+}
