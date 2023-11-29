3ee59b0e965e45f869ba71ab5270080ccd73cb55
Adria Navarro <adria@budibase.com>
2023-11-29 12:34:50 +0100

    Migrations in a queue

--- packages/server/src/appMigrations/migrations.ts
+++ packages/server/src/appMigrations/migrations.ts
@@ -1,3 +1,8 @@
 // This file should never be manually modified, use `yarn add-app-migration` in order to add a new one
 
-export const MIGRATIONS: Record<string, { migration: () => Promise<void> }> = {}
+export const MIGRATIONS: {
+  migrationId: string
+  migrationFunc: () => Promise<void>
+}[] = [
+  // Migrations will be executed sorted by migrationId
+]
--- packages/server/src/appMigrations/tests/migrations.spec.ts
+++ packages/server/src/appMigrations/tests/migrations.spec.ts
@@ -1,22 +1,19 @@
 import { context } from "@budibase/backend-core"
 import * as setup from "../../api/routes/tests/utilities"
 import { MIGRATIONS } from "../migrations"
-import { runMigration } from "../queue"
 
 describe("migration", () => {
   it("each migration can rerun safely", async () => {
     const config = setup.getConfig()
     await config.init()
 
-    const migrations = Object.keys(MIGRATIONS)
-
     await config.doInContext(config.getAppId(), async () => {
       const db = context.getAppDB()
-      for (const migration of migrations) {
-        await runMigration(migration)
+      for (const migration of MIGRATIONS) {
+        await migration.migrationFunc()
         const docs = await db.allDocs({ include_docs: true })
 
-        await runMigration(migration)
+        await migration.migrationFunc()
         const latestDocs = await db.allDocs({ include_docs: true })
 
         expect(docs).toEqual(latestDocs)
