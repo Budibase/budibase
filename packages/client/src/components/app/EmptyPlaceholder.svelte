0220a16bc39eb771b9bb8160e580d16a6e952f4f
Andrew Kingston <andrew@kingston.dev>
2023-12-07 13:27:09 +0000

    Remove placeholders prompting users to add components inside other components

--- packages/client/src/components/app/EmptyPlaceholder.svelte
+++ packages/client/src/components/app/EmptyPlaceholder.svelte
@@ -10,15 +10,7 @@
 
 {#if $builderStore.inBuilder}
   <div class="component-placeholder">
-    <Icon name="Help" color="var(--spectrum-global-color-blue-600)" />
-    <span
-      class="spectrum-Link"
-      on:click={() => {
-        builderStore.actions.requestAddComponent()
-      }}
-    >
-      Add components inside your {definition?.name || $component.type}
-    </span>
+    {$component.name || definition?.name || "Component"}
   </div>
 {/if}
 
@@ -31,15 +23,5 @@
     color: var(--spectrum-global-color-gray-600);
     font-size: var(--font-size-s);
     gap: var(--spacing-s);
-  }
-
-  /* Common styles for all error states to use */
-  .component-placeholder :global(mark) {
-    background-color: var(--spectrum-global-color-gray-400);
-    padding: 0 4px;
-    border-radius: 2px;
-  }
-  .component-placeholder :global(.spectrum-Link) {
-    cursor: pointer;
   }
 </style>
