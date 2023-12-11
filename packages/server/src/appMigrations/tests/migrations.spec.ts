a2b2669a2fa11b14ca6e2af3731e491ba6c9bd9a
Adria Navarro <adria@budibase.com>
2023-12-11 10:47:53 +0100

    Add app migration test

--- packages/server/src/appMigrations/tests/migrations.spec.ts
+++ packages/server/src/appMigrations/tests/migrations.spec.ts
@@ -1,8 +1,21 @@
 import { context } from "@budibase/backend-core"
 import * as setup from "../../api/routes/tests/utilities"
 import { MIGRATIONS } from "../migrations"
+import { getAppMigrationVersion } from "../appMigrationMetadata"
+import { latestMigration } from ".."
 
-describe("migration", () => {
+describe("migrations", () => {
+  it("new apps are created with the latest app migration version set", async () => {
+    const config = setup.getConfig()
+    await config.init()
+
+    await config.doInContext(config.getAppId(), async () => {
+      const migrationVersion = await getAppMigrationVersion(config.getAppId())
+
+      expect(migrationVersion).toEqual(latestMigration)
+    })
+  })
+
   // These test is checking that each migration is "idempotent".
   // We should be able to rerun any migration, with any rerun not modifiying anything. The code should be aware that the migration already ran
   it("each migration can rerun safely", async () => {
