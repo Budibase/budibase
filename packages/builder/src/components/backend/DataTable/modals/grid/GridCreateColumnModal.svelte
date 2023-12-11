7b2880fbe2a8d91f6642323cd3da2c4c0bad2f54
mike12345567 <me@michaeldrury.co.uk>
2023-12-11 17:27:05 +0000

    Initial work to get relationship column saving working as expecting, sending out events on datasource update for tables, trying to move more to table API where possible.

--- packages/builder/src/components/backend/DataTable/modals/grid/GridCreateColumnModal.svelte
+++ packages/builder/src/components/backend/DataTable/modals/grid/GridCreateColumnModal.svelte
@@ -2,7 +2,13 @@
   import { getContext } from "svelte"
   import CreateEditColumn from "components/backend/DataTable/modals/CreateEditColumn.svelte"
 
-  const { datasource } = getContext("grid")
+  const { datasource, rows } = getContext("grid")
 </script>
 
-<CreateEditColumn on:updatecolumns={datasource.actions.refreshDefinition} />
+<CreateEditColumn
+  on:updatecolumns={async () => {
+    await datasource.actions.refreshDefinition()
+    // Need to find a better way to handle formula columns than just always refreshing
+    rows.actions.refreshData()
+  }}
+/>
