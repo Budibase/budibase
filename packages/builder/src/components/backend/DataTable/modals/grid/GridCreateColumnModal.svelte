ccc811f4bfe39e2a13149c5b167d76d8c0a098b4
mike12345567 <me@michaeldrury.co.uk>
2023-12-12 15:02:40 +0000

    PR comments.

--- packages/builder/src/components/backend/DataTable/modals/grid/GridCreateColumnModal.svelte
+++ packages/builder/src/components/backend/DataTable/modals/grid/GridCreateColumnModal.svelte
@@ -2,13 +2,7 @@
   import { getContext } from "svelte"
   import CreateEditColumn from "components/backend/DataTable/modals/CreateEditColumn.svelte"
 
-  const { datasource, rows } = getContext("grid")
+  const { datasource } = getContext("grid")
 </script>
 
-<CreateEditColumn
-  on:updatecolumns={async () => {
-    await datasource.actions.refreshDefinition()
-    // Need to find a better way to handle formula columns than just always refreshing
-    rows.actions.refreshData()
-  }}
-/>
+<CreateEditColumn on:updatecolumns={datasource.actions.refreshDefinition} />
