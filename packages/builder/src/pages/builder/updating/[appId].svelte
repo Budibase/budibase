9067d38dce059718577f7fee214f7a31495e14c4
Adria Navarro <adria@budibase.com>
2023-12-12 18:38:29 +0100

    Create updating page

---
+++ packages/builder/src/pages/builder/updating/[appId].svelte
@@ -0,0 +1,14 @@
+<script>
+  import Spinner from "components/common/Spinner.svelte"
+</script>
+
+<div class="loading">
+  <Spinner size="20" />
+</div>
+
+<style>
+  .loading {
+    display: flex;
+    justify-content: center;
+  }
+</style>
