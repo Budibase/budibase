546eeb5035e829a2b51a35069e8118781dbd9460
Adria Navarro <adria@budibase.com>
2023-12-19 12:20:05 +0100

    Implement refresh

--- packages/client/src/components/UpdatingApp.svelte
+++ packages/client/src/components/UpdatingApp.svelte
@@ -1,4 +1,30 @@
 <script>
+  import { API } from "../api"
+
+  const timeoutMs = 60000 // 1 minute
+  const loadTime = Date.now()
+
+  async function checkMigrationsFinished() {
+    setTimeout(async () => {
+      const response = await API.getMigrationStatus()
+      if (!response.migrated) {
+        if (loadTime + timeoutMs > Date.now()) {
+          return checkMigrationsFinished()
+        }
+
+        return migrationTimeout()
+      }
+
+      window.location.reload()
+    }, 1000)
+  }
+
+  checkMigrationsFinished()
+
+  function migrationTimeout() {
+    // TODO
+    alert("Something went wrong 💀")
+  }
 </script>
 
 <div class="loading">Updating the system...</div>
