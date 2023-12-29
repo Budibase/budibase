4a1b99a9dc5f1b650a4ffe3cfa7602b51f6eedb3
Adria Navarro <adria@budibase.com>
2023-12-29 13:32:45 +0100

    Add migration

--- packages/server/src/appMigrations/migrations/20231229122514_update_link_documents.ts
+++ packages/server/src/appMigrations/migrations/20231229122514_update_link_documents.ts
@@ -1,5 +1,26 @@
+import { SEPARATOR, context } from "@budibase/backend-core"
+import { allLinkDocs } from "../../db/utils"
+
 const migration = async () => {
-  // Add your migration logic here
+  const linkDocs = await allLinkDocs()
+
+  const docsToUpdate = []
+  for (const linkDoc of linkDocs) {
+    if (linkDoc.tableId) {
+      // It already had the required data
+      continue
+    }
+
+    linkDoc.tableId = [linkDoc.doc1.tableId, linkDoc.doc2.tableId]
+      .sort()
+      .join(SEPARATOR)
+    docsToUpdate.push(linkDoc)
+  }
+
+  if (docsToUpdate.length) {
+    const db = context.getAppDB()
+    await db.bulkDocs(docsToUpdate)
+  }
 }
 
 export default migration
