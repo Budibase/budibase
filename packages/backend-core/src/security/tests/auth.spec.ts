f134c3dc1eca2add7c5c0ec326cddedd36816d86
Adria Navarro <adria@budibase.com>
2024-01-02 12:56:08 +0100

    Add tests

---
+++ packages/backend-core/src/security/tests/auth.spec.ts
@@ -0,0 +1,20 @@
+import { validatePassword } from "../auth"
+
+describe("auth", () => {
+  describe("validatePassword", () => {
+    it("a valid password returns successful", () => {
+      expect(validatePassword("password")).toEqual({ valid: true })
+    })
+
+    it.each([
+      ["undefined", undefined],
+      ["null", null],
+      ["empty", ""],
+    ])("%s returns unsuccessful", (_, password) => {
+      expect(validatePassword(password as string)).toEqual({
+        valid: false,
+        error: "Password invalid. Minimum eight characters",
+      })
+    })
+  })
+})
