5609db3545cadaf01299d96b7a95c6b92b939a1f
Adria Navarro <adria@budibase.com>
2024-01-02 15:16:09 +0100

    Add max limit

--- packages/backend-core/src/security/auth.ts
+++ packages/backend-core/src/security/auth.ts
@@ -1,4 +1,5 @@
 const MIN_LENGTH = 8
+const MAX_LENGTH = 100
 
 export function validatePassword(
   password: string
@@ -7,6 +8,13 @@ export function validatePassword(
     return {
       valid: false,
       error: "Password invalid. Minimum eight characters.",
+    }
+  }
+
+  if (password.length > MAX_LENGTH) {
+    return {
+      valid: false,
+      error: "Password invalid. Maximum hundred characters.",
     }
   }
 
