9da142f17ae27927357ff561aeafefb825fbec08
Adria Navarro <adria@budibase.com>
2024-01-08 12:10:35 +0100

    Clean

--- packages/client/src/components/UpdatingApp.svelte
+++ packages/client/src/components/UpdatingApp.svelte
@@ -1,15 +1,9 @@
 <script>
   import { Updating } from "@budibase/frontend-core"
-  import { API } from "../api"
-
-  async function isMigrationDone() {
-    const response = await API.getMigrationStatus()
-    return response.migrated
-  }
 
   async function onMigrationDone() {
     window.location.reload()
   }
 </script>
 
-<Updating {isMigrationDone} {onMigrationDone} />
+<Updating {onMigrationDone} />
