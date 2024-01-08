d62b2ec961c2fd5f63e765d3f2a8777df0d1d1a9
Adria Navarro <adria@budibase.com>
2024-01-08 13:42:38 +0100

    Pass isMigrationDone

--- packages/builder/src/pages/builder/app/updating/[application].svelte
+++ packages/builder/src/pages/builder/app/updating/[application].svelte
@@ -2,6 +2,13 @@
   import { Updating } from "@budibase/frontend-core"
   import { redirect, params } from "@roxi/routify"
 
+  import { API } from "api"
+
+  async function isMigrationDone() {
+    const response = await API.getMigrationStatus()
+    return response.migrated
+  }
+
   async function onMigrationDone() {
     // For some reason routify params is not stripping the ? properly, so we need to check both with and without ?
     const returnUrl = $params.returnUrl || $params["?returnUrl"]
@@ -9,4 +16,4 @@
   }
 </script>
 
-<Updating {onMigrationDone} />
+<Updating {isMigrationDone} {onMigrationDone} />
