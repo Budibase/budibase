e9b8e923854aa8d288f306afb04b3284064c7acc
Mel O'Hagan <mel@budibase.com>
2024-01-12 19:29:36 +0000

    Bug fix

--- packages/frontend-core/src/fetch/QueryArrayFetch.js
+++ packages/frontend-core/src/fetch/QueryArrayFetch.js
@@ -3,6 +3,9 @@ import { getJSONArrayDatasourceSchema } from "../utils/json"
 
 export default class QueryArrayFetch extends FieldFetch {
   async getDefinition(datasource) {
+    if (!datasource?.tableId) {
+      return null
+    }
     // JSON arrays need their table definitions fetched.
     // We can then extract their schema as a subset of the table schema.
     try {
