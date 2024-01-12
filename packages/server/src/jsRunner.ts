c508a435d63eb27d6dc58d573b821ca012724df0
Sam Rose <hello@samwho.dev>
2024-01-12 15:08:08 +0000

    Isolate and context re-use.

--- packages/server/src/jsRunner.ts
+++ packages/server/src/jsRunner.ts
@@ -1,76 +1,41 @@
 import ivm from "isolated-vm"
 import env from "./environment"
 import { setJSRunner } from "@budibase/string-templates"
-import { context, timers } from "@budibase/backend-core"
+import { context } from "@budibase/backend-core"
 import tracer from "dd-trace"
+import { readFileSync } from "fs"
 
-type TrackerFn = <T>(f: () => T) => T
+const helpersSource = readFileSync(
+  "node_modules/@budibase/string-templates/dist/bundle.mjs",
+  "utf8"
+)
 
 export function init() {
   setJSRunner((js: string, ctx: Record<string, any>) => {
     return tracer.trace("runJS", {}, span => {
-      const perRequestLimit = env.JS_PER_REQUEST_TIME_LIMIT_MS
-      let track: TrackerFn = f => f()
-      if (perRequestLimit) {
-        const bbCtx = tracer.trace("runJS.getCurrentContext", {}, span =>
-          context.getCurrentContext()
-        )
-        if (bbCtx) {
-          if (!bbCtx.jsExecutionTracker) {
-            span?.addTags({
-              createdExecutionTracker: true,
-            })
-            bbCtx.jsExecutionTracker = tracer.trace(
-              "runJS.createExecutionTimeTracker",
-              {},
-              span => timers.ExecutionTimeTracker.withLimit(perRequestLimit)
-            )
-          }
-          span?.addTags({
-            js: {
-              limitMS: bbCtx.jsExecutionTracker.limitMs,
-              elapsedMS: bbCtx.jsExecutionTracker.elapsedMS,
-            },
-          })
-          // We call checkLimit() here to prevent paying the cost of creating
-          // a new VM context below when we don't need to.
-          tracer.trace("runJS.checkLimitAndBind", {}, span => {
-            bbCtx.jsExecutionTracker!.checkLimit()
-            track = bbCtx.jsExecutionTracker!.track.bind(
-              bbCtx.jsExecutionTracker
-            )
-          })
-        }
-      }
-
-      const isolate = new ivm.Isolate({ memoryLimit: 64 })
-      const vm = isolate.createContextSync()
-      const jail = vm.global
-      jail.setSync("global", jail.derefInto())
-
-      for (let key in ctx) {
-        let value
-        if (["alert", "setInterval", "setTimeout"].includes(key)) {
-          value = undefined
-        } else {
-          value = ctx[key]
-        }
-
-        if (typeof value === "function") {
-          js = value.toString() + "\n" + js
-          continue
-        } else {
-          value = new ivm.ExternalCopy(value).copyInto({ release: true })
-        }
-        jail.setSync(key, value)
-      }
-
-      const script = isolate.compileScriptSync(js)
-
-      return track(() => {
-        return script.runSync(vm, {
-          timeout: env.JS_PER_EXECUTION_TIME_LIMIT_MS,
+      const bbCtx = context.getCurrentContext()!
+      if (!bbCtx.jsIsolate) {
+        bbCtx.jsIsolate = new ivm.Isolate({ memoryLimit: 64 })
+        bbCtx.jsContext = bbCtx.jsIsolate.createContextSync()
+        const helpersModule = bbCtx.jsIsolate.compileModuleSync(helpersSource)
+        helpersModule.instantiateSync(bbCtx.jsContext, () => {
+          throw new Error("No imports allowed")
         })
+      }
+
+      const perRequestLimit = env.JS_PER_REQUEST_TIME_LIMIT_MS
+      if (perRequestLimit) {
+        const cpuMs = Number(bbCtx.jsIsolate.cpuTime) / 1e6
+        if (cpuMs > perRequestLimit) {
+          throw new Error(
+            `CPU time limit exceeded (${cpuMs}ms > ${perRequestLimit}ms)`
+          )
+        }
+      }
+
+      const script = bbCtx.jsIsolate.compileScriptSync(js)
+      return script.runSync(bbCtx.jsContext, {
+        timeout: env.JS_PER_EXECUTION_TIME_LIMIT_MS,
       })
     })
   })
