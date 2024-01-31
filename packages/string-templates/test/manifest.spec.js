b96ca1cf54dc31899bd2d8260f19635049f4ad66
Adria Navarro <adria@budibase.com>
2024-01-31 16:15:53 +0100

    Run hbs on server tests

--- packages/string-templates/test/manifest.spec.js
+++ packages/string-templates/test/manifest.spec.js
@@ -15,22 +15,12 @@ jest.mock("@budibase/handlebars-helpers/lib/uuid", () => {
   }
 })
 
-const {
-  processString,
-  convertToJS,
-  processStringSync,
-  encodeJSBinding,
-} = require("../src/index.cjs")
+const { processString } = require("../src/index.cjs")
 
 const tk = require("timekeeper")
-const { getJsHelperList } = require("../src/helpers")
-const { getParsedManifest } = require("./utils")
+const { getParsedManifest, runJsHelpersTests } = require("./utils")
 
 tk.freeze("2021-01-21T12:00:00")
-
-const processJS = (js, context) => {
-  return processStringSync(encodeJSBinding(js), context)
-}
 
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") // $& means the whole matched string
@@ -60,36 +50,5 @@ describe("manifest", () => {
     })
   })
 
-  describe("can be parsed and run as js", () => {
-    const jsHelpers = getJsHelperList()
-    const jsExamples = Object.keys(manifest).reduce((acc, v) => {
-      acc[v] = manifest[v].filter(([key]) => jsHelpers[key])
-      return acc
-    }, {})
-
-    describe.each(Object.keys(jsExamples))("%s", collection => {
-      it.each(
-        jsExamples[collection].filter(
-          ([_, { requiresHbsBody }]) => !requiresHbsBody
-        )
-      )("%s", async (_, { hbs, js }) => {
-        const context = {
-          double: i => i * 2,
-          isString: x => typeof x === "string",
-        }
-
-        const arrays = hbs.match(/\[[^/\]]+\]/)
-        arrays?.forEach((arrayString, i) => {
-          hbs = hbs.replace(new RegExp(escapeRegExp(arrayString)), `array${i}`)
-          context[`array${i}`] = JSON.parse(arrayString.replace(/\'/g, '"'))
-        })
-
-        let convertedJs = convertToJS(hbs)
-
-        let result = processJS(convertedJs, context)
-        result = result.replace(/&nbsp;/g, " ")
-        expect(result).toEqual(js)
-      })
-    })
-  })
+  runJsHelpersTests()
 })
