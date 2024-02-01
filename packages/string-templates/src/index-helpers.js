480476bcfe1143adaa6cf7d03adb779d84ced128
Adria Navarro <adria@budibase.com>
2024-02-01 12:32:39 +0100

    Lint

--- packages/string-templates/src/index-helpers.js
+++ packages/string-templates/src/index-helpers.js
@@ -3,6 +3,7 @@ const { getJsHelperList } = require("./helpers/list")
 const helpers = getJsHelperList()
 module.exports = {
   ...helpers,
-  // point stripProtocol to a unexisting function to be able to declare it on isolated-vm
+  // pointing stripProtocol to a unexisting function to be able to declare it on isolated-vm
+  // eslint-disable-next-line no-undef
   stripProtocol: helpersStripProtocol,
 }
