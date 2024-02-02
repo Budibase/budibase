92fb5b1b3cf977bb57601c28355ab6e3919e0690
Sam Rose <hello@samwho.dev>
2024-02-02 09:51:46 +0000

    Revert "Migrate from `vm` to `isolated-vm`"

--- packages/string-templates/src/helpers/list.js
+++ packages/string-templates/src/helpers/list.js
@@ -1,27 +1,10 @@
-const { date, duration } = require("./date")
+const externalHandlebars = require("./external")
+const helperList = require("@budibase/handlebars-helpers")
 
-// https://github.com/evanw/esbuild/issues/56
-const externalCollections = {
-  math: require("@budibase/handlebars-helpers/lib/math"),
-  array: require("@budibase/handlebars-helpers/lib/array"),
-  number: require("@budibase/handlebars-helpers/lib/number"),
-  url: require("@budibase/handlebars-helpers/lib/url"),
-  string: require("@budibase/handlebars-helpers/lib/string"),
-  comparison: require("@budibase/handlebars-helpers/lib/comparison"),
-  object: require("@budibase/handlebars-helpers/lib/object"),
-  regex: require("@budibase/handlebars-helpers/lib/regex"),
-  uuid: require("@budibase/handlebars-helpers/lib/uuid"),
-}
+let helpers = undefined
 
 const helpersToRemoveForJs = ["sortBy"]
 module.exports.helpersToRemoveForJs = helpersToRemoveForJs
-
-const addedHelpers = {
-  date: date,
-  duration: duration,
-}
-
-let helpers = undefined
 
 module.exports.getJsHelperList = () => {
   if (helpers) {
@@ -29,14 +12,18 @@ module.exports.getJsHelperList = () => {
   }
 
   helpers = {}
-  for (let collection of Object.values(externalCollections)) {
+  let constructed = []
+  for (let collection of externalHandlebars.externalCollections) {
+    constructed.push(helperList[collection]())
+  }
+  for (let collection of constructed) {
     for (let [key, func] of Object.entries(collection)) {
       // Handlebars injects the hbs options to the helpers by default. We are adding an empty {} as a last parameter to simulate it
       helpers[key] = (...props) => func(...props, {})
     }
   }
-  for (let key of Object.keys(addedHelpers)) {
-    helpers[key] = addedHelpers[key]
+  for (let key of Object.keys(externalHandlebars.addedHelpers)) {
+    helpers[key] = externalHandlebars.addedHelpers[key]
   }
 
   for (const toRemove of helpersToRemoveForJs) {
