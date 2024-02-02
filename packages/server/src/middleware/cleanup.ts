ba002f96492fd311cb0396262b4f77c2edb6f8f5
Sam Rose <hello@samwho.dev>
2024-02-02 09:30:33 +0000

    Clean up isolates when a request is finished.

---
+++ packages/server/src/middleware/cleanup.ts
@@ -0,0 +1,16 @@
+import { Ctx } from "@budibase/types"
+import { context } from "@budibase/backend-core"
+
+export default async (ctx: Ctx, next: any) => {
+  const resp = next()
+
+  const current = context.getCurrentContext()
+  if (current?.cleanup) {
+    for (let fn of current.cleanup || []) {
+      await fn()
+    }
+    delete current.cleanup
+  }
+
+  return resp
+}
