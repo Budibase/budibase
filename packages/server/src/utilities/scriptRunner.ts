8dfcc3aeb194ce35f9c667e72134957b7d24f334
Adria Navarro <adria@revityapp.com>
2024-02-02 12:02:09 +0100

    Revert "Revert "Migrate from `vm` to `isolated-vm`""

--- packages/server/src/utilities/scriptRunner.ts
+++ packages/server/src/utilities/scriptRunner.ts
@@ -1,29 +1,63 @@
-import fetch from "node-fetch"
-import { VM, VMScript } from "vm2"
+import ivm from "isolated-vm"
 
 const JS_TIMEOUT_MS = 1000
 
 class ScriptRunner {
-  vm: VM
-  results: { out: string }
-  script: VMScript
+  vm: IsolatedVM
 
   constructor(script: string, context: any) {
     const code = `let fn = () => {\n${script}\n}; results.out = fn();`
-    this.vm = new VM({
-      timeout: JS_TIMEOUT_MS,
-    })
-    this.results = { out: "" }
-    this.vm.setGlobals(context)
-    this.vm.setGlobal("fetch", fetch)
-    this.vm.setGlobal("results", this.results)
-    this.script = new VMScript(code)
+    this.vm = new IsolatedVM({ memoryLimit: 8 })
+    this.vm.context = {
+      ...context,
+      results: { out: "" },
+    }
+    this.vm.code = code
   }
 
   execute() {
-    this.vm.run(this.script)
-    return this.results.out
+    this.vm.runScript()
+    const results = this.vm.getValue("results")
+    return results.out
   }
 }
 
+class IsolatedVM {
+  isolate: ivm.Isolate
+  vm: ivm.Context
+  jail: ivm.Reference
+  script: any
+
+  constructor({ memoryLimit }: { memoryLimit: number }) {
+    this.isolate = new ivm.Isolate({ memoryLimit })
+    this.vm = this.isolate.createContextSync()
+    this.jail = this.vm.global
+    this.jail.setSync("global", this.jail.derefInto())
+  }
+
+  getValue(key: string) {
+    const ref = this.vm.global.getSync(key, { reference: true })
+    const result = ref.copySync()
+    ref.release()
+    return result
+  }
+
+  set context(context: Record<string, any>) {
+    for (let key in context) {
+      this.jail.setSync(key, this.copyRefToVm(context[key]))
+    }
+  }
+
+  set code(code: string) {
+    this.script = this.isolate.compileScriptSync(code)
+  }
+
+  runScript() {
+    this.script.runSync(this.vm, { timeout: JS_TIMEOUT_MS })
+  }
+
+  copyRefToVm(value: Object): ivm.Copy<Object> {
+    return new ivm.ExternalCopy(value).copyInto({ release: true })
+  }
+}
 export default ScriptRunner
