0db1441a5819ae087c7cfb764ffa354e6214650a
Adria Navarro <adria@budibase.com>
2024-02-06 11:35:41 +0100

    Use new loadBundle function

--- packages/server/src/jsRunner/bundles/index.ts
+++ packages/server/src/jsRunner/bundles/index.ts
@@ -2,12 +2,14 @@ import { utils } from "@budibase/shared-core"
 import environment from "../../environment"
 import fs from "fs"
 
-export enum BundleType {
+export const enum BundleType {
   HELPERS = "helpers",
+  BSON = "bson",
 }
 
 const bundleSourceCode = {
   [BundleType.HELPERS]: "../bundles/index-helpers.ivm.bundle.js",
+  [BundleType.BSON]: "../bundles/bson.ivm.bundle.js",
 }
 
 export function loadBundle(type: BundleType) {
@@ -18,6 +20,8 @@ export function loadBundle(type: BundleType) {
   switch (type) {
     case BundleType.HELPERS:
       return require("../bundles/index-helpers.ivm.bundle.js")
+    case BundleType.BSON:
+      return require("../bundles/bson.ivm.bundle.js")
     default:
       utils.unreachable(type)
   }
