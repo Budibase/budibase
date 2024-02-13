813f88485faedf98f22278c01aba4c39df794adc
Peter Clement <peter@budibase.com>
2024-02-13 15:26:22 +0000

    store tenant in cookie for use in account portal

--- packages/builder/src/pages/builder/auth/_layout.svelte
+++ packages/builder/src/pages/builder/auth/_layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import { auth, admin } from "stores/portal"
   import { redirect } from "@roxi/routify"
+  import { CookieUtils } from "@budibase/frontend-core"
 
   // If already authenticated, redirect away from the auth section.
   // Check this onMount rather than a reactive statement to avoid trumping
@@ -9,13 +10,20 @@
     $redirect("../")
   }
 
+  if ($admin.checklist?.branding) {
+    let url = new URL("http://hello.petertest.com:10001/auth/login")
+    let hostname = url.hostname
+    let parts = hostname.split(".")
+    let tenantId = parts[0]
+    CookieUtils.setCookie("tenantId", tenantId, "petertest.com")
+  }
+
   if (
     !$auth.user &&
     $admin.cloud &&
     !$admin.disableAccountPortal &&
     $admin.accountPortalUrl &&
-    !$admin?.checklist?.sso?.checked &&
-    !$admin.checklist.branding
+    !$admin?.checklist?.sso?.checked
   ) {
     window.location.href = $admin.accountPortalUrl
   }
