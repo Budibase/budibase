33d53861064fac6ed10d6365bb1efb7f896ce0db
Adria Navarro <adria@budibase.com>
2024-02-13 14:13:09 +0100

    Replace ivm.module for script

--- packages/server/src/jsRunner/bundles/index-helpers.ts
+++ packages/server/src/jsRunner/bundles/index-helpers.ts
@@ -2,9 +2,8 @@ const {
   getJsHelperList,
 } = require("../../../../string-templates/src/helpers/list.js")
 
-const helpers = getJsHelperList()
-export default {
-  ...helpers,
+export const helpers = {
+  ...getJsHelperList(),
   // pointing stripProtocol to a unexisting function to be able to declare it on isolated-vm
   // @ts-ignore
   // eslint-disable-next-line no-undef
