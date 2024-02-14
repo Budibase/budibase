d15824da7b2bf2213a9cc4544285c4942bd2ab8f
mike12345567 <me@michaeldrury.co.uk>
2024-02-14 15:04:08 +0000

    Updating how default environment variables are handled, so that the defaults are easier to access.

--- packages/server/src/constants/automations.ts
+++ packages/server/src/constants/automations.ts
@@ -1,4 +1,6 @@
 import { Duration } from "@budibase/backend-core"
+import env from "../environment"
 
+const defaults = env.getDefaults()
 export const AUTOMATION_SYNC_TIMEOUT = Duration.fromMinutes(2).toMs()
-export const AUTOMATION_ASYNC_TIMEOUT = Duration.fromSeconds(12).toMs()
+export const AUTOMATION_ASYNC_TIMEOUT = defaults.QUERY_THREAD_TIMEOUT
