335228e8782f9a9e7044c9869250c05d1c2817ba
Michael Drury <me@michaeldrury.co.uk>
2024-02-19 21:08:26 +0000

    isolated VM test cases.

--- packages/server/src/jsRunner/tests/isolatedVM.spec.ts
+++ packages/server/src/jsRunner/tests/isolatedVM.spec.ts
@@ -30,4 +30,57 @@ describe("Test isolated vm directly", () => {
     })
     expect(result).toBe("<p>dddd</p>\n")
   })
+
+  it("handle a mapping case", async () => {
+    const context = {
+      data: {
+        data: {
+          searchProducts: {
+            results: [
+              { imageLinks: ["_S/"] }
+            ]
+          }
+        }
+      }
+    }
+    const result = await compare(`
+      const dataUnnested = data.data.searchProducts.results
+      const emptyLink = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRC3hpq0MXqXssA28Lm5NrzcOYAyr--q3xyg&usqp=CAU"
+      let pImage = emptyLink
+      let sImage = emptyLink
+      let uImage = emptyLink
+      let lImage = emptyLink
+      let b1Image = emptyLink
+      let b2Image = emptyLink
+
+      const dataTransformed = dataUnnested.map(x=> {
+        let imageLinks = x.imageLinks
+        for (let i = 0; i < imageLinks.length; i++){ 
+          if(imageLinks[i].includes("_P/") || imageLinks[i].includes("_p/")){
+            pImage = imageLinks[i]
+          } else if (imageLinks[i].includes("_S/") || imageLinks[i].includes("_s/")){
+            sImage = imageLinks[i]
+          } else if (imageLinks[i].includes("_U/") || imageLinks[i].includes("_u/")){
+            uImage = imageLinks[i]
+          } else if (imageLinks[i].includes("_L/") || imageLinks[i].includes("_l/")){
+            lImage = imageLinks[i]
+          } else if (imageLinks[i].includes("_B/") || imageLinks[i].includes("_b/")){
+            b1Image = imageLinks[i]
+          } else if (imageLinks[i].includes("_B2/") || imageLinks[i].includes("_b2/")){
+            b2Image = imageLinks[i]
+          }
+        }
+
+        const arrangedLinks = [pImage, sImage, uImage, lImage, b1Image, b2Image]
+        x.imageLinks = arrangedLinks
+
+        return x
+      })
+
+      return dataTransformed
+      `, context)
+    expect(result).toBeDefined()
+    expect(result.length).toBe(1)
+    expect(result[0].imageLinks.length).toBe(6)
+  })
 })
