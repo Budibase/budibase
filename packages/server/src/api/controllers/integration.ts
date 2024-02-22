298f8a8176274ec13374f2fa8034d180d89b1d01
Conor Webb <conorwebb59@gmail.com>
2024-02-22 13:32:16 +0000

    Refactored based on feedback. Added a check to find.

--- packages/server/src/api/controllers/integration.ts
+++ packages/server/src/api/controllers/integration.ts
@@ -1,21 +1,37 @@
 import { getDefinition, getDefinitions } from "../../integrations"
 import { BBContext } from "@budibase/types"
 
-export async function fetch(ctx: BBContext) {
+async function fetchDefinitions(ctx: BBContext) {
   try {
     const definitions = await getDefinitions()
     delete definitions.AIRTABLE
     ctx.status = 200
     ctx.body = definitions
+
+    return definitions
   } catch (error) {
     console.error("Error fetching definitions:", error)
     ctx.status = 500
     ctx.body = { error: "Internal server error" }
+    
+    return {}
   }
+}
+
+export async function fetch(ctx: BBContext) {
+  await fetchDefinitions(ctx)
 }
 
 export async function find(ctx: BBContext) {
   const def = await getDefinition(ctx.params.type)
-  ctx.body = def
-  ctx.status = 200
+  if (ctx.params.type in await fetchDefinitions(ctx)) {
+    ctx.body = def;
+    ctx.status = 200;
+  } else {
+    ctx.status = 400;
+    ctx.body = {
+      message: `Cannot find definition '${ctx.params.type}'`,
+      status: 400,
+    };
+  }
 }
