2b6a0538bee801917536ac8ef4d50443e18ab00b
mike12345567 <me@michaeldrury.co.uk>
2024-02-22 17:54:26 +0000

    Setting disabled source types.

--- packages/server/src/api/controllers/integration.ts
+++ packages/server/src/api/controllers/integration.ts
@@ -1,15 +1,19 @@
 import { getDefinition, getDefinitions } from "../../integrations"
 import { SourceName, UserCtx } from "@budibase/types"
 
+const DISABLED_EXTERNAL_INTEGRATIONS = [SourceName.AIRTABLE]
+
 export async function fetch(ctx: UserCtx) {
   const definitions = await getDefinitions()
-  delete definitions.AIRTABLE
+  for (let disabledIntegration of DISABLED_EXTERNAL_INTEGRATIONS) {
+    delete definitions[disabledIntegration]
+  }
   ctx.body = definitions
 }
 
 export async function find(ctx: UserCtx) {
   const sourceType = ctx.params?.type
-  if (sourceType === SourceName.AIRTABLE) {
+  if (DISABLED_EXTERNAL_INTEGRATIONS.indexOf(sourceType) !== -1) {
     ctx.throw(400, `Invalid source type - ${sourceType} is not supported.`)
   }
   ctx.body = await getDefinition(ctx.params.type)
