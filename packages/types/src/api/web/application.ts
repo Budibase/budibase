04aaec69ca3786d162c42d5d257707a244750ebe
Sam Rose <hello@samwho.dev>
2024-02-26 17:28:37 +0000

    Respond to PR feedback.

---
+++ packages/types/src/api/web/application.ts
@@ -0,0 +1,29 @@
+import type { PlanType } from "../../sdk"
+import type { Layout, App } from "../../documents"
+
+export interface CreateAppRequest {
+  name: string
+  url?: string
+  useTemplate?: string
+  templateName?: string
+  templateKey?: string
+  templateFile?: string
+  includeSampleData?: boolean
+  encryptionPassword?: string
+  templateString?: string
+}
+
+export interface FetchAppDefinitionResponse {
+  layouts: Layout[]
+  screens: Screen[]
+  libraries: string[]
+}
+
+export interface FetchAppPackageResponse {
+  application: App
+  licenseType: PlanType
+  screens: Screen[]
+  layouts: Layout[]
+  clientLibPath: string
+  hasLock: boolean
+}
