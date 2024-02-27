4e6467e62422fa4aaa6e4d0ce7346a75aba6f262
Adria Navarro <adria@budibase.com>
2024-02-27 14:26:41 +0100

    Unify scim icons

--- packages/builder/src/pages/builder/portal/users/groups/_components/GroupIcon.svelte
+++ packages/builder/src/pages/builder/portal/users/groups/_components/GroupIcon.svelte
@@ -1,6 +1,5 @@
 <script>
   import { IconAvatar } from "@budibase/bbui"
-  import ScimInfo from "../../_components/SCIMInfo.svelte"
 
   export let group
   export let size = "M"
@@ -8,22 +7,10 @@
 
 <div class="icon-group">
   <IconAvatar icon={group?.icon} background={group?.color} {size} />
-
-  {#if group.scimInfo?.isSync}
-    <div class="scim-sync">
-      <ScimInfo iconSize="XXS" />
-    </div>
-  {/if}
 </div>
 
 <style>
   .icon-group {
     position: relative;
-  }
-
-  .scim-sync {
-    position: absolute;
-    bottom: -4px;
-    right: 0;
   }
 </style>
