83195e19a3298d14cf5cff2fdce774b93f76342c
Sam Rose <hello@samwho.dev>
2024-03-01 14:33:18 +0000

    Migrate QueryAPI

--- packages/server/src/tests/utilities/api/query.ts
+++ packages/server/src/tests/utilities/api/query.ts
@@ -1,60 +1,29 @@
-import TestConfiguration from "../TestConfiguration"
 import {
   Query,
-  QueryPreview,
-  type ExecuteQueryRequest,
-  type ExecuteQueryResponse,
+  ExecuteQueryRequest,
+  ExecuteQueryResponse,
+  PreviewQueryRequest,
+  PreviewQueryResponse,
 } from "@budibase/types"
 import { TestAPI } from "./base"
 
 export class QueryAPI extends TestAPI {
-  constructor(config: TestConfiguration) {
-    super(config)
-  }
-
   create = async (body: Query): Promise<Query> => {
-    const res = await this.request
-      .post(`/api/queries`)
-      .set(this.config.defaultHeaders())
-      .send(body)
-      .expect("Content-Type", /json/)
-
-    if (res.status !== 200) {
-      throw new Error(JSON.stringify(res.body))
-    }
-
-    return res.body as Query
+    return await this._post<Query>(`/api/queries`, { body })
   }
 
   execute = async (
     queryId: string,
     body?: ExecuteQueryRequest
   ): Promise<ExecuteQueryResponse> => {
-    const res = await this.request
-      .post(`/api/v2/queries/${queryId}`)
-      .set(this.config.defaultHeaders())
-      .send(body)
-      .expect("Content-Type", /json/)
-
-    if (res.status !== 200) {
-      throw new Error(JSON.stringify(res.body))
-    }
-
-    return res.body
+    return await this._post<ExecuteQueryResponse>(`/api/queries/${queryId}`, {
+      body,
+    })
   }
 
-  previewQuery = async (queryPreview: QueryPreview) => {
-    const res = await this.request
-      .post(`/api/queries/preview`)
-      .send(queryPreview)
-      .set(this.config.defaultHeaders())
-      .expect("Content-Type", /json/)
-      .expect(200)
-
-    if (res.status !== 200) {
-      throw new Error(JSON.stringify(res.body))
-    }
-
-    return res.body
+  previewQuery = async (queryPreview: PreviewQueryRequest) => {
+    return await this._post<PreviewQueryResponse>(`/api/queries/preview`, {
+      body: queryPreview,
+    })
   }
 }
