da560b8c3ddc194a55e72c3be12f947a2f6728ce
Adria Navarro <adria@budibase.com>
2024-03-05 16:49:45 +0100

    Bulk store test

--- packages/backend-core/src/redis/tests/redis.spec.ts
+++ packages/backend-core/src/redis/tests/redis.spec.ts
@@ -2,9 +2,10 @@ import { generator, structures } from "../../../tests"
 import RedisWrapper from "../redis"
 
 describe("redis", () => {
-  const redis = new RedisWrapper(structures.db.id())
+  let redis: RedisWrapper
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    redis = new RedisWrapper(structures.db.id())
     await redis.init()
   })
 
@@ -16,6 +17,25 @@ describe("redis", () => {
       await redis.store(key, value)
 
       expect(await redis.get(key)).toEqual(value)
+    })
+  })
+
+  describe("bulkStore", () => {
+    it("a basic object can be persisted", async () => {
+      const data = generator
+        .unique(() => generator.word(), 10)
+        .reduce((acc, key) => {
+          acc[key] = generator.word()
+          return acc
+        }, {} as Record<string, string>)
+
+      await redis.bulkStore(data)
+
+      for (const [key, value] of Object.entries(data)) {
+        expect(await redis.get(key)).toEqual(value)
+      }
+
+      expect(await redis.keys("*")).toHaveLength(10)
     })
   })
 })
