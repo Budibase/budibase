8910f39b893413bc35128966dafe1496fe27987d
Conor Webb <126772285+ConorWebb96@users.noreply.github.com>
2024-03-06 16:32:00 +0000

    Added icon to button component, reworked icon display code. (#12624)
    * Added icons to buttons, removed svg code added icon component code.
    
    * Added icon functionality to button group component.
    
    * Added gap to button manifest
    
    * Added gap to button setitngs.
    
    * Added gap setting to ButtonGroup component
    
    * Added the ability to clear the selected icon.
    
    * Added enter search to icon select
    
    * Removed use:styleable as its for the button
    
    * Moved non internal props up
    
    * Fixed broken DynamicFilter component icon
    
    * Updated DynamicFilter icon to a better suited one
    
    ---------
    
    Co-authored-by: melohagan <101575380+melohagan@users.noreply.github.com>

--- packages/builder/src/components/design/settings/controls/IconSelect/IconSelect.svelte
+++ packages/builder/src/components/design/settings/controls/IconSelect/IconSelect.svelte
@@ -139,10 +139,22 @@
         {/each}
       </div>
       <div class="search-input">
-        <div class="input-wrapper">
-          <Input bind:value={searchTerm} thin placeholder="Search Icon" />
+        <div class="input-wrapper" style={`width: ${value ? "425" : "510"}px`}>
+          <Input
+            bind:value={searchTerm}
+            on:keyup={event => {
+              if (event.key === "Enter") {
+                searchForIcon()
+              }
+            }}
+            thin
+            placeholder="Search Icon"
+          />
         </div>
         <Button secondary on:click={searchForIcon}>Search</Button>
+        {#if value}
+          <Button primary on:click={() => (value = null)}>Clear</Button>
+        {/if}
       </div>
       <div class="page-area">
         <div class="pager">
@@ -239,6 +251,7 @@
     flex-flow: row nowrap;
     width: 100%;
     padding-right: 15px;
+    gap: 10px;
   }
   .input-wrapper {
     width: 510px;
