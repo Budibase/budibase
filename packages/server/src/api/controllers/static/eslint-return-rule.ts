923af18573bf0c50777920195dafd7a88cb68691
Dean <deanhannigan@gmail.com>
2024-03-08 12:34:33 +0000

    Added useful example url

--- packages/server/src/api/controllers/static/eslint-return-rule.ts
+++ packages/server/src/api/controllers/static/eslint-return-rule.ts
@@ -17,6 +17,7 @@ export const globalReturn = {
       ReturnStatement: function (node: any) {
         // this doesn't have awareness of the main config
         // https://github.com/eslint/eslint/blob/ba1c1bbc6ba9d57a83d04f450566337d3c3b0448/lib/rules/no-eval.js#L229
+        // https://developers.mews.com/how-to-write-custom-eslint-rules/
         let features = context.parserOptions.ecmaFeatures || {}
         let scope = context.getScope()
         if (scope.block.type === "Program") {
