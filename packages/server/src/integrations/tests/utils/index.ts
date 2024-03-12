ce209a16b3f85fbd6151b2a38cfa9e7976378be0
Sam Rose <hello@samwho.dev>
2024-03-12 10:42:42 +0000

    Get tests running with SQL Server. Need to make them pass next.

--- packages/server/src/integrations/tests/utils/index.ts
+++ packages/server/src/integrations/tests/utils/index.ts
@@ -4,6 +4,7 @@ import { Datasource } from "@budibase/types"
 import * as postgres from "./postgres"
 import * as mongodb from "./mongodb"
 import * as mysql from "./mysql"
+import * as mssql from "./mssql"
 import { StartedTestContainer } from "testcontainers"
 
 jest.setTimeout(30000)
@@ -14,4 +15,4 @@ export interface DatabaseProvider {
   datasource(): Promise<Datasource>
 }
 
-export const databaseTestProviders = { postgres, mongodb, mysql }
+export const databaseTestProviders = { postgres, mongodb, mysql, mssql }
