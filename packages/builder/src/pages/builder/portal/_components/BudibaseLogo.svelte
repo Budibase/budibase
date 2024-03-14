d654e5e883e6661f8310e8ab84ae0d2cd2e177bb
Sam Rose <hello@samwho.dev>
2024-03-14 10:25:41 +0000

    Silence lint warnings.

--- packages/builder/src/pages/builder/portal/_components/BudibaseLogo.svelte
+++ packages/builder/src/pages/builder/portal/_components/BudibaseLogo.svelte
@@ -3,6 +3,8 @@
   import { goto } from "@roxi/routify"
 </script>
 
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions-->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <img src={Logo} alt="Budibase Logo" on:click={() => $goto("./apps")} />
 
 <style>
