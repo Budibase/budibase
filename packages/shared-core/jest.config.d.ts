94a93a5bb6095f9aede280fbc29cf2bdf9346698
Adria Navarro <adria@budibase.com>
2024-03-19 23:40:55 +0100

    Run jest

---
+++ packages/shared-core/jest.config.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    preset: string;
+    testEnvironment: string;
+    transform: {
+        "^.+\\.ts?$": string;
+    };
+    moduleNameMapper: {
+        "@budibase/types": string;
+    };
+};
+export default _default;
