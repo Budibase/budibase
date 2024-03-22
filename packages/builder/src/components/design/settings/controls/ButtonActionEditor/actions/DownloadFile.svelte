5f8f1b868f06733810f12e6f260d5331c7fcd480
Adria Navarro <adria@budibase.com>
2024-03-22 09:49:05 +0100

    Add download file option

---
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/DownloadFile.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Label } from "@budibase/bbui"
+  import DrawerBindableInput from "components/common/bindings/DrawerBindableInput.svelte"
+
+  export let parameters
+  export let bindings = []
+</script>
+
+<div class="root">
+  <Label small>File</Label>
+  <DrawerBindableInput
+    title="State value"
+    {bindings}
+    value={parameters.value}
+    on:change={e => (parameters.value = e.detail)}
+  />
+</div>
+
+<style>
+  .root {
+    display: grid;
+    column-gap: var(--spacing-l);
+    row-gap: var(--spacing-s);
+    grid-template-columns: 60px 1fr;
+    align-items: center;
+    margin: 0 auto;
+  }
+</style>
