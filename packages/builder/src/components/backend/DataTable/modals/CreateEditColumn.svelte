040209448d7b1a85ef605b3c510ae4c9f1bb6184
mike12345567 <me@michaeldrury.co.uk>
2024-03-22 15:52:46 +0000

    Disabling the single attachment column toggle for now - until we add the new type.

--- packages/builder/src/components/backend/DataTable/modals/CreateEditColumn.svelte
+++ packages/builder/src/components/backend/DataTable/modals/CreateEditColumn.svelte
@@ -710,21 +710,6 @@
       thin
       text="Allow multiple users"
     />
-  {:else if editableColumn.type === FieldType.ATTACHMENT}
-    <Toggle
-      value={editableColumn.subtype !== FieldTypeSubtypes.ATTACHMENT.SINGLE &&
-        // Checking config before the subtype was added
-        editableColumn.constraints?.length?.maximum !== 1}
-      on:change={e => {
-        if (!e.detail) {
-          editableColumn.subtype = FieldTypeSubtypes.ATTACHMENT.SINGLE
-        } else {
-          delete editableColumn.subtype
-        }
-      }}
-      thin
-      text="Allow multiple"
-    />
   {/if}
   {#if editableColumn.type === AUTO_TYPE || editableColumn.autocolumn}
     <Select
