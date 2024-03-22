cee11a4da39b6593c4d9e56515dbe14555c6a670
Adria Navarro <adria@budibase.com>
2024-03-22 10:38:09 +0100

    Download config

--- packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/DownloadFile.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/DownloadFile.svelte
@@ -1,17 +1,23 @@
 <script>
   import { Label } from "@budibase/bbui"
+  import { FieldType } from "@budibase/types"
   import DrawerBindableInput from "components/common/bindings/DrawerBindableInput.svelte"
 
   export let parameters
   export let bindings = []
+
+  $: fileBindings = bindings?.filter(
+    b => b.fieldSchema?.type === FieldType.ATTACHMENT
+  )
 </script>
 
 <div class="root">
   <Label small>File</Label>
   <DrawerBindableInput
     title="State value"
-    {bindings}
+    bindings={fileBindings}
     value={parameters.value}
+    allowHelpers={false}
     on:change={e => (parameters.value = e.detail)}
   />
 </div>
