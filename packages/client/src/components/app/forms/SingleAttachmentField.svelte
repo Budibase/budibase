7f8ddd422bc7c34774f3f4318e82fc378995df84
Adria Navarro <adria@budibase.com>
2024-03-27 09:25:37 +0100

    SingleAttachmentField component

---
+++ packages/client/src/components/app/forms/SingleAttachmentField.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { FieldType } from "@budibase/types"
+  import AttachmentField from "./AttachmentField.svelte"
+</script>
+
+<AttachmentField {...$$restProps} type={FieldType.ATTACHMENT_SINGLE} />
