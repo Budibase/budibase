a90a11e6d896e8b7af9bd4b6741d6d7fb3090ad1
Adria Navarro <adria@budibase.com>
2024-03-27 10:21:26 +0100

    Clean components

---
+++ packages/frontend-core/src/components/grid/cells/AttachmentSingleCell.svelte
@@ -0,0 +1,20 @@
+<script>
+  import AttachmentCell from "./AttachmentCell.svelte"
+
+  export let value
+  export let onChange
+
+  $: arrayValue = (!Array.isArray(value) && value ? [value] : value) || []
+
+  $: onFileChange = value => {
+    value = value[0] || null
+    onChange(value)
+  }
+</script>
+
+<AttachmentCell
+  {...$$restProps}
+  maximum={1}
+  value={arrayValue}
+  onChange={onFileChange}
+/>
