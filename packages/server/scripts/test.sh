74e1983e372a32e68c0c73fe9df20dd269afc6e8
Sam Rose <hello@samwho.dev>
2024-04-03 11:55:38 +0100

    Testcontainer debug logging in test-libraries.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
-
 if [[ -n $CI ]]
 then
-  echo "jest --coverage --maxWorkers=4 --forceExit --bail $@"
-  jest --coverage --maxWorkers=2 --forceExit --bail $@
+  export NODE_OPTIONS="--max-old-space-size=4096 --no-node-snapshot $NODE_OPTIONS"
+  echo "jest --coverage --maxWorkers=4 --forceExit --workerIdleMemoryLimit=2000MB --bail $@"
+  jest --coverage --maxWorkers=4 --forceExit --workerIdleMemoryLimit=2000MB --bail $@
 else
+  # --maxWorkers performs better in development
+  export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
   echo "jest --coverage --maxWorkers=2 --forceExit $@"
   jest --coverage --maxWorkers=2 --forceExit $@
 fi
