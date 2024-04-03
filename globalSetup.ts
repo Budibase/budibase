3199f159b593de6b97c75e8e45d3c50a6601cd37
Sam Rose <hello@samwho.dev>
2024-04-03 12:06:49 +0100

    Lock globalSetup to prevent multiple containers starting.

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,36 +1,51 @@
 import { GenericContainer, Wait } from "testcontainers"
+import lockfile from "proper-lockfile"
 
 export default async function setup() {
-  let couchdb = new GenericContainer("budibase/couchdb")
-    .withExposedPorts(5984)
-    .withEnvironment({
-      COUCHDB_PASSWORD: "budibase",
-      COUCHDB_USER: "budibase",
-    })
-    .withCopyContentToContainer([
-      {
-        content: `
+  if (process.env.REUSE_CONTAINERS) {
+    // If you run multiple tests at the same time, it's possible for the CouchDB
+    // shared container to get started multiple times despite having an
+    // identical reuse hash. To avoid that, we do a filesystem-based lock so
+    // that only one globalSetup.ts is running at a time.
+    lockfile.lockSync("globalSetup.lock")
+  }
+
+  try {
+    let couchdb = new GenericContainer("budibase/couchdb")
+      .withExposedPorts(5984)
+      .withEnvironment({
+        COUCHDB_PASSWORD: "budibase",
+        COUCHDB_USER: "budibase",
+      })
+      .withCopyContentToContainer([
+        {
+          content: `
           [log]
           level = warn
         `,
-        target: "/opt/couchdb/etc/local.d/test-couchdb.ini",
-      },
-    ])
-    .withWaitStrategy(
-      Wait.forSuccessfulCommand(
-        "curl http://budibase:budibase@localhost:5984/_up"
-      ).withStartupTimeout(20000)
-    )
+          target: "/opt/couchdb/etc/local.d/test-couchdb.ini",
+        },
+      ])
+      .withWaitStrategy(
+        Wait.forSuccessfulCommand(
+          "curl http://budibase:budibase@localhost:5984/_up"
+        ).withStartupTimeout(20000)
+      )
 
-  if (process.env.REUSE_CONTAINERS) {
-    couchdb = couchdb.withReuse()
+    if (process.env.REUSE_CONTAINERS) {
+      couchdb = couchdb.withReuse()
+    }
+
+    if (process.env.CONTAINER_NAMESPACE) {
+      couchdb = couchdb.withLabels({
+        "org.testcontainers.namespace": process.env.CONTAINER_NAMESPACE,
+      })
+    }
+
+    await couchdb.start()
+  } finally {
+    if (process.env.REUSE_CONTAINERS) {
+      lockfile.unlockSync("globalSetup.lock")
+    }
   }
-
-  if (process.env.CONTAINER_NAMESPACE) {
-    couchdb = couchdb.withLabels({
-      "org.testcontainers.namespace": process.env.CONTAINER_NAMESPACE,
-    })
-  }
-
-  await couchdb.start()
 }
