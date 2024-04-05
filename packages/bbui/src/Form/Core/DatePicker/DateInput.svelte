c6bfe0ff360bc438789e98b3f77f67f3ffe43064
Andrew Kingston <andrew@kingston.dev>
2024-04-05 12:16:42 +0100

    Multiple style fixes

--- packages/bbui/src/Form/Core/DatePicker/DateInput.svelte
+++ packages/bbui/src/Form/Core/DatePicker/DateInput.svelte
@@ -28,6 +28,8 @@
   }
 </script>
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
   bind:this={anchor}
   class:is-disabled={disabled}
