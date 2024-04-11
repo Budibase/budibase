29253124384f3c4aaefaa784aa4a076bf2e0fce3
Gerard Burns <gjburns9@hotmail.com>
2024-04-11 14:40:12 +0100

    wip

--- packages/builder/src/components/design/settings/controls/FieldContext/typography/Line.svelte
+++ packages/builder/src/components/design/settings/controls/FieldContext/typography/Line.svelte
@@ -1,22 +1,38 @@
+<script>
+  export let wrap = false
+</script>
 <div class="line">
   <span class="bullet">•</span>
-  <slot />
+  <div class="content" class:wrap>
+    <slot />
+  </div>
 </div>
 
 <style>
+  .line {
+    background-color: var(--background-alt);
+    color: var(--ink);
+    align-items: flex-start;
+    display: flex;
+
+    margin-bottom: 10px;
+  }
+
   .bullet {
     color: var(--grey-6);
     font-size: 17px;
     display: inline block;
     margin-right: 10px;
+    line-height: 26px;
   }
 
-  .line {
-    background-color: var(--background-alt);
-    color: var(--ink);
-    align-items: center;
+  .content {
     display: flex;
+    min-width: 0;
+    align-items: center;
+  }
 
-    margin-bottom: 10px;
+  .wrap {
+    display: block;
   }
 </style>
