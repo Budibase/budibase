bbeda0879025fabc9e4aa7f39f0f91bd97fd0fe4
Gerard Burns <gjburns9@hotmail.com>
2024-04-14 09:47:16 +0100

    array type info

---
+++ packages/builder/src/components/design/settings/controls/FieldContext/ExplanationModal/subjects/components/JSONProperty.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let name;
+  export let value;
+</script>
+
+<div class="property">
+  <span class="propertyName">
+    <slot name="name">
+      {name}
+    </slot>
+  </span>
+  <span class="propertyDivider">-</span>
+  <pre class="pre propertyValue">
+    <slot>
+      {value}
+    </slot>
+  </pre>
+</div>
+
+<style>
+  .property {
+    margin-bottom: 12px;
+  }
+
+  .propertyName {
+    font-style: italic;
+  }
+
+  .propertyDivider {
+    padding: 4px;
+  }
+
+  .pre {
+    margin: 0;
+    margin-top: 3px;
+    padding: 4px;
+    border-radius: 3px;
+    width: 250px;
+    box-sizing: border-box;
+    background-color: black;
+  }
+</style>
