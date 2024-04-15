12d2982c67a8c50267eedd5078cf1cea4be4bb93
Gerard Burns <gjburns9@hotmail.com>
2024-04-15 07:10:46 +0100

    nicer time

--- packages/builder/src/components/design/settings/controls/FieldContext/ExplanationModal/subjects/DatesAsNumbers.svelte
+++ packages/builder/src/components/design/settings/controls/FieldContext/ExplanationModal/subjects/DatesAsNumbers.svelte
@@ -1,9 +1,40 @@
 <script>
-  import { Subject, Section } from './components'
+  import { onMount } from "svelte"
+  import { Block, Subject, Section } from './components'
+
+  let timestamp = Date.now();
+
+  onMount(() => {
+    let run = true;
+
+    const updateTimeStamp = () => {
+      timestamp = Date.now();
+      console.log(timestamp);
+      if (run) {
+        setTimeout(updateTimeStamp, 200)
+      }
+    }
+
+    updateTimeStamp();
+
+    return () => {
+      run = false;
+    }
+  })
 </script>
 
 <Subject heading="Dates as Numbers">
   <Section>
-    A date can be used in place of a numeric value, but it will be parsed as a UNIX epoch timestamp, which is the number of milliseconds since Jan 1st 1970.
+    A date can be used in place of a numeric value, but it will be parsed as a UNIX epoch timestamp, which is the number of milliseconds since Jan 1st 1970. A more recent Date/Time will be a higher number.
+  </Section>
+
+  <Section>
+    Examples:
+    <br />
+    1st Jan 2000: <Block>946684800000</Block>
+    <br />
+    1st Jan 2020: <Block>1577836800000</Block>
+    <br />
+    Now: <Block>{timestamp}</Block>
   </Section>
 </Subject>
