c153dbf83e6709f307ecfcbb24df82b1b09ef861
Gerard Burns <gjburns9@hotmail.com>
2024-04-22 07:23:30 +0100

    string dates

--- packages/builder/src/components/design/settings/controls/Explanation/DetailsModal/subjects/StringsAsDates.svelte
+++ packages/builder/src/components/design/settings/controls/Explanation/DetailsModal/subjects/StringsAsDates.svelte
@@ -55,7 +55,7 @@
       </ExampleLine>
     </ExampleSection>
   <Section>
-    An <Block>ISO 8601</Block> datetime string. which is the number of milliseconds since Jan 1st 1970. A more recent moment in time will be a higher number.
+    An <Block>ISO 8601</Block> datetime string, which represents an exact moment in time as well as the potentional to store the timezone it occured in. 
   </Section>
     <div class="isoExamples">
       <ExampleSection
@@ -63,21 +63,21 @@
       >
         <ExampleLine>
           <Block>2000-01-01T00:00:00.000Z</Block>
-          <span class="separator">{"->"}</span>
+          <span class="separator">↓</span>
           <Block>
             {(new Date(946684800000).toLocaleString())}
           </Block>
         </ExampleLine>
         <ExampleLine>
           <Block>2000-01-01T00:00:00.000Z</Block>
-          <span class="separator">{"->"}</span>
+          <span class="separator">↓</span>
           <Block>
             {(new Date(1577836800000).toLocaleString())}
           </Block>
         </ExampleLine>
         <ExampleLine>
         <Block>{iso}</Block>
-        <span class="separator">{"->"}</span>
+        <span class="separator">↓</span>
           <Block>Now</Block>
         </ExampleLine>
       </ExampleSection>
@@ -87,9 +87,9 @@
 
 <style>
   .separator {
-    flex-shrink: 0;
     margin: 0 5px;
   }
+
 
   .isoExamples :global(.block) {
     word-break: break-all;
@@ -97,5 +97,8 @@
 
   .isoExamples :global(.exampleLine) {
     align-items: center;
+    flex-direction: column;
+    margin-bottom: 16px;
+    width: 162px;
   }
 </style>
