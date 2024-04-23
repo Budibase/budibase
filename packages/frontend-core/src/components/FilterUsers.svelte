37e519758690f0f37daa1a96de8462d9b006efe4
Dean <deanhannigan@gmail.com>
2024-04-23 12:40:57 +0100

    Fix for regression in api initialisation

--- packages/frontend-core/src/components/FilterUsers.svelte
+++ packages/frontend-core/src/components/FilterUsers.svelte
@@ -1,7 +1,9 @@
 <script>
   import { Select, Multiselect } from "@budibase/bbui"
   import { fetchData } from "@budibase/frontend-core"
-  import { API } from "api"
+  import { createAPIClient } from "../api"
+
+  export let API = createAPIClient()
 
   export let value = null
   export let disabled
