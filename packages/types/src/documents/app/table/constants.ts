2be14deb010dd096149028bdc2e84dde43cf6637
Adria Navarro <adria@budibase.com>
2024-04-26 13:26:14 +0200

    Add test

--- packages/types/src/documents/app/table/constants.ts
+++ packages/types/src/documents/app/table/constants.ts
@@ -1,3 +1,5 @@
+import { FieldType } from "../row"
+
 export enum RelationshipType {
   ONE_TO_MANY = "one-to-many",
   MANY_TO_ONE = "many-to-one",
@@ -30,3 +32,18 @@ export enum BBReferenceFieldSubType {
   /** @deprecated this should not be used anymore, left here in order to support the existing usages */
   USERS = "users",
 }
+
+export type SupportedSqlTypes =
+  | FieldType.STRING
+  | FieldType.BARCODEQR
+  | FieldType.LONGFORM
+  | FieldType.OPTIONS
+  | FieldType.DATETIME
+  | FieldType.NUMBER
+  | FieldType.BOOLEAN
+  | FieldType.FORMULA
+  | FieldType.BIGINT
+  | FieldType.BB_REFERENCE
+  | FieldType.BB_REFERENCE_SINGLE
+  | FieldType.LINK
+  | FieldType.ARRAY
