777b5b5f5396ae451f1015a3a3160c77d5ec47b6
Sam Rose <hello@samwho.dev>
2024-04-26 11:35:23 +0100

    Make REUSE_CONTAINERS the default for running tests.

--- globalSetup.ts
+++ globalSetup.ts
@@ -4,13 +4,11 @@ import lockfile from "proper-lockfile"
 
 export default async function setup() {
   const lockPath = path.resolve(__dirname, "globalSetup.ts")
-  if (process.env.REUSE_CONTAINERS) {
-    // If you run multiple tests at the same time, it's possible for the CouchDB
-    // shared container to get started multiple times despite having an
-    // identical reuse hash. To avoid that, we do a filesystem-based lock so
-    // that only one globalSetup.ts is running at a time.
-    lockfile.lockSync(lockPath)
-  }
+  // If you run multiple tests at the same time, it's possible for the CouchDB
+  // shared container to get started multiple times despite having an
+  // identical reuse hash. To avoid that, we do a filesystem-based lock so
+  // that only one globalSetup.ts is running at a time.
+  lockfile.lockSync(lockPath)
 
   try {
     let couchdb = new GenericContainer("budibase/couchdb:v3.2.1-sqs")
@@ -28,20 +26,15 @@ export default async function setup() {
           target: "/opt/couchdb/etc/local.d/test-couchdb.ini",
         },
       ])
+      .withReuse()
       .withWaitStrategy(
         Wait.forSuccessfulCommand(
           "curl http://budibase:budibase@localhost:5984/_up"
         ).withStartupTimeout(20000)
       )
 
-    if (process.env.REUSE_CONTAINERS) {
-      couchdb = couchdb.withReuse()
-    }
-
     await couchdb.start()
   } finally {
-    if (process.env.REUSE_CONTAINERS) {
-      lockfile.unlockSync(lockPath)
-    }
+    lockfile.unlockSync(lockPath)
   }
 }
