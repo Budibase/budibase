141cdd8fec5e97ae938a284f5b4a272eb215c0eb
Mel O'Hagan <mel@budibase.com>
2024-05-03 15:17:44 +0100

    lint

--- packages/builder/src/components/portal/onboarding/FreeTrialModal.svelte
+++ packages/builder/src/components/portal/onboarding/FreeTrialModal.svelte
@@ -14,21 +14,21 @@
 >
   <h1>Experience all of Budibase with a free 14-day trial</h1>
   <div class="free-trial-text">
-    We've upgraded you to a free 14-day trial that allows you to try all our features
-    before deciding which plan is right for you.
+    We've upgraded you to a free 14-day trial that allows you to try all our
+    features before deciding which plan is right for you.
     <p>
-      At the end of your trial, we'll automatically downgrade you to the Free plan
-      unless you choose to upgrade.
+      At the end of your trial, we'll automatically downgrade you to the Free
+      plan unless you choose to upgrade.
     </p>
   </div>
-  <FreeTrial />  
+  <FreeTrial />
 </ModalContent>
 
 <style>
   h1 {
-    font-size: 26px
+    font-size: 26px;
   }
   .free-trial-text {
-    font-size: 16px
+    font-size: 16px;
   }
 </style>
