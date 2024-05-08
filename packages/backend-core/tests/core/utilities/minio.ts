1dfe14672a803f5a1dc7b8c5aafecd5ee08b90df
Sam Rose <hello@samwho.dev>
2024-05-08 11:40:56 +0100

    Bind to 127.0.0.1 instead of 0.0.0.0

--- packages/backend-core/tests/core/utilities/minio.ts
+++ packages/backend-core/tests/core/utilities/minio.ts
@@ -17,7 +17,7 @@ export async function start(): Promise<void> {
     .start()
 
   const port = container.getMappedPort(9000)
-  env._set("MINIO_URL", `http://0.0.0.0:${port}`)
+  env._set("MINIO_URL", `http://127.0.0.1:${port}`)
 }
 
 export async function stop() {
