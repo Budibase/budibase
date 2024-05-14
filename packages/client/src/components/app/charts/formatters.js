bad7a535dcc2ff74a3e43ce6d4df46cb7eebdcd5
Gerard Burns <gjburns9@hotmail.com>
2024-05-14 09:24:18 +0100

    add clonedeep that copies non-serialisable stuff

--- packages/client/src/components/app/charts/formatters.js
+++
@@ -1,8 +0,0 @@
-const formatters = {
-  ["Default"]: val => val,
-  ["Thousands"]: val => `${Math.round(val / 1000)}K`,
-  ["Millions"]: val => `${Math.round(val / 1000000)}M`,
-  ["Datetime"]: val => new Date(val).toLocaleString(),
-}
-
-export default formatters
