d47195e360e889c583e08589231f1a276ede6669
mike12345567 <me@michaeldrury.co.uk>
2024-05-15 13:37:46 +0100

    Reducing the likelihood of validity check clashing with filename.

--- packages/server/src/integrations/rest.ts
+++ packages/server/src/integrations/rest.ts
@@ -146,7 +146,7 @@ class RestIntegration implements IntegrationBase {
       // the API does not follow the requirements of https://www.ietf.org/rfc/rfc2183.txt
       // all content-disposition headers should be format disposition-type; parameters
       // but some APIs do not provide a type, causing the parse below to fail - add one to fix this
-      if (!contentDisposition.includes(";")) {
+      if (!contentDisposition.includes("; ")) {
         contentDisposition = `attachment; ${contentDisposition}`
       }
       filename =
