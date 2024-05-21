e8fc36c7bc1c16d3362510b5ca34148aa4b3df80
mike12345567 <me@michaeldrury.co.uk>
2024-05-21 18:14:04 +0100

    Support dev build with SQS.

--- scripts/devDocker.sh
+++ scripts/devDocker.sh
@@ -7,6 +7,6 @@ if [ ! -d "./packages/pro/src" ]; then
 fi
 
 yarn build --scope @budibase/server --scope @budibase/worker
-docker-compose -f hosting/docker-compose.build.yaml -f hosting/docker-compose.dev.yaml --env-file hosting/.env up --build --scale proxy-service=0
+DOCKER_BUILDKIT=1 docker compose -f hosting/docker-compose.build.yaml -f hosting/docker-compose.dev.yaml --env-file hosting/.env up --build --scale proxy-service=0
 
 
