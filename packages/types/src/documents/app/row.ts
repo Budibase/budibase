28cdfa525fadb5b59269ec80c01de5086208e9b5
mike12345567 <me@michaeldrury.co.uk>
2024-05-23 15:00:13 +0100

    Making sure all JSON column types are correctly handled.

--- packages/types/src/documents/app/row.ts
+++ packages/types/src/documents/app/row.ts
@@ -118,6 +118,15 @@ export enum FieldType {
   BB_REFERENCE_SINGLE = "bb_reference_single",
 }
 
+export const JsonTypes = [
+  FieldType.ATTACHMENT_SINGLE,
+  FieldType.BB_REFERENCE_SINGLE,
+  FieldType.ATTACHMENTS,
+  FieldType.BB_REFERENCE,
+  FieldType.JSON,
+  FieldType.ARRAY,
+]
+
 export interface RowAttachment {
   size: number
   name: string
