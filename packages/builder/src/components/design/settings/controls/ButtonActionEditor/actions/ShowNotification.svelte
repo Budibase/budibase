5ff1ef6a2c9f10067305bda57996b6efc78ca187
Conor Webb <conorwebb59@gmail.com>
2024-06-05 07:45:19 +0100

    Enforce max duration for auto-dismiss to 2 minutes

--- packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/ShowNotification.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/ShowNotification.svelte
@@ -25,6 +25,8 @@
     },
   ]
 
+  const MAX_DURATION = 120000 // Maximum duration in milliseconds (2 minutes)
+
   onMount(() => {
     if (!parameters.type) {
       parameters.type = "success"
@@ -33,6 +35,14 @@
       parameters.autoDismiss = true
     }
   })
+
+  function handleDurationChange(event) {
+    let newDuration = event.detail
+    if (newDuration > MAX_DURATION) {
+      newDuration = MAX_DURATION
+    }
+    parameters.duration = newDuration
+  }
 </script>
 
 <div class="root">
@@ -54,7 +64,7 @@
       {bindings}
       value={parameters.duration}
       placeholder="3000"
-      on:change={e => (parameters.duration = e.detail)}
+      on:change={handleDurationChange}
     />
   {/if}
 </div>
