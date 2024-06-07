d88095ce03a549df5fc527f2eb5d466f166f9b55
Sam Rose <hello@samwho.dev>
2024-06-07 16:56:07 +0100

    Move some mysql.spec.ts and postgres.spec.ts tests into datasource.spec.ts.

--- packages/server/src/tests/utilities/api/datasource.ts
+++ packages/server/src/tests/utilities/api/datasource.ts
@@ -6,6 +6,7 @@ import {
   UpdateDatasourceRequest,
   QueryJson,
   BuildSchemaFromSourceResponse,
+  FetchDatasourceInfoResponse,
 } from "@budibase/types"
 import { Expectations, TestAPI } from "./base"
 
@@ -79,6 +80,16 @@ export class DatasourceAPI extends TestAPI {
     return await this._post<BuildSchemaFromSourceResponse>(
       `/api/datasources/${id}/schema`,
       {
+        expectations,
+      }
+    )
+  }
+
+  info = async (datasource: Datasource, expectations?: Expectations) => {
+    return await this._post<FetchDatasourceInfoResponse>(
+      `/api/datasources/info`,
+      {
+        body: { datasource },
         expectations,
       }
     )
