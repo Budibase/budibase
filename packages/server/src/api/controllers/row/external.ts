b86501e25eb7ec109377dec7cecc0527caecf80c
Adria Navarro <adria@budibase.com>
2024-06-07 10:54:26 +0200

    Fix

--- packages/server/src/api/controllers/row/external.ts
+++ packages/server/src/api/controllers/row/external.ts
@@ -25,6 +25,7 @@ import {
   outputProcessing,
 } from "../../../utilities/rowProcessor"
 import { cloneDeep } from "lodash"
+import { generateIdForRow } from "./utils"
 
 export async function handleRequest<T extends Operation>(
   operation: T,
@@ -59,7 +60,9 @@ export async function patch(ctx: UserCtx<PatchRowRequest, PatchRowResponse>) {
     id: breakRowIdField(_id),
     row: dataToUpdate,
   })
-  const row = await sdk.rows.external.getRow(tableId, _id, {
+
+  const updatedId = generateIdForRow(dataToUpdate, table)
+  const row = await sdk.rows.external.getRow(tableId, updatedId, {
     relationships: true,
   })
   const enrichedRow = await outputProcessing(table, row, {
