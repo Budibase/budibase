b275ba6707dc13629cad6a9f6295233f0cc2a54b
Sam Rose <hello@samwho.dev>
2024-06-11 15:38:25 +0100

    Try the sync versions?

--- packages/server/src/integrations/tests/utils/index.ts
+++ packages/server/src/integrations/tests/utils/index.ts
@@ -86,7 +86,7 @@ export async function startContainer(container: GenericContainer) {
     fs.writeFileSync(lockPath, "")
   }
 
-  const unlock = await lockfile.lock(lockPath, {
+  const unlock = lockfile.lockSync(lockPath, {
     retries: 10,
   })
 
@@ -94,7 +94,7 @@ export async function startContainer(container: GenericContainer) {
   try {
     startedContainer = await container.start()
   } finally {
-    await unlock()
+    unlock()
   }
 
   const info = testContainerUtils.getContainerById(startedContainer.getId())
