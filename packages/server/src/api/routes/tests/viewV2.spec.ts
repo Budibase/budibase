66ef0cb79afe0c28662bebd62dad36f9a3a3e6ff
mike12345567 <me@michaeldrury.co.uk>
2024-06-20 16:05:03 +0100

    Adding back SQS - wasn't causing a problem.

--- packages/server/src/api/routes/tests/viewV2.spec.ts
+++ packages/server/src/api/routes/tests/viewV2.spec.ts
@@ -28,6 +28,7 @@ import { db, roles } from "@budibase/backend-core"
 
 describe.each([
   ["lucene", undefined],
+  ["sqs", undefined],
   [DatabaseName.POSTGRES, getDatasource(DatabaseName.POSTGRES)],
   [DatabaseName.MYSQL, getDatasource(DatabaseName.MYSQL)],
   [DatabaseName.SQL_SERVER, getDatasource(DatabaseName.SQL_SERVER)],
