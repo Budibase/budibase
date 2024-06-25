27917c78d11adc8d71aef7dd989e6bc94798bb3d
Dean <deanhannigan@gmail.com>
2024-06-25 10:01:32 +0100

    Switch TooltipWrapper to AbsTooltip. Messages were being truncated

--- packages/bbui/src/Tooltip/TooltipWrapper.svelte
+++ packages/bbui/src/Tooltip/TooltipWrapper.svelte
@@ -1,12 +1,10 @@
 <script>
-  import Tooltip from "./Tooltip.svelte"
   import Icon from "../Icon/Icon.svelte"
+  import AbsTooltip from "./AbsTooltip.svelte"
 
   export let tooltip = ""
   export let size = "M"
   export let disabled = true
-
-  let showTooltip = false
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -14,20 +12,15 @@
   <slot />
   {#if tooltip}
     <div class="icon-container">
-      <div
-        class="icon"
-        class:icon-small={size === "M" || size === "S"}
-        on:mouseover={() => (showTooltip = true)}
-        on:mouseleave={() => (showTooltip = false)}
-        on:focus
-      >
-        <Icon name="InfoOutline" size="S" {disabled} />
-      </div>
-      {#if showTooltip}
-        <div class="tooltip">
-          <Tooltip textWrapping={true} direction={"bottom"} text={tooltip} />
+      <AbsTooltip text={tooltip}>
+        <div
+          class="icon"
+          class:icon-small={size === "M" || size === "S"}
+          on:focus
+        >
+          <Icon name="InfoOutline" size="S" {disabled} hoverable />
         </div>
-      {/if}
+      </AbsTooltip>
     </div>
   {/if}
 </div>
@@ -43,14 +36,6 @@
     justify-content: center;
     margin-left: 5px;
     margin-right: 5px;
-  }
-  .tooltip {
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    top: 15px;
-    z-index: 200;
-    width: 160px;
   }
   .icon {
     transform: scale(0.75);
