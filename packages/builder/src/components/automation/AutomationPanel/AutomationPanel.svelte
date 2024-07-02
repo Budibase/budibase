b6f8649c05ec14d2bd28bca7a1c83c4a469cc3a0
Dean <deanhannigan@gmail.com>
2024-07-02 11:36:23 +0100

    Removed debugging line

--- packages/builder/src/components/automation/AutomationPanel/AutomationPanel.svelte
+++ packages/builder/src/components/automation/AutomationPanel/AutomationPanel.svelte
@@ -40,8 +40,6 @@
     return acc
   }, {})
 
-  $: console.log("grouped ", Object.entries(groupedAutomations))
-
   $: showNoResults = searchString && !filteredAutomations.length
 
   onMount(async () => {
