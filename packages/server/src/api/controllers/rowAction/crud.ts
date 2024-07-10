38718968b003079fa5cbfc89b0d8858c95a75971
Adria Navarro <adria@budibase.com>
2024-07-10 13:24:00 +0200

    Basic implementation

--- packages/server/src/api/controllers/rowAction/crud.ts
+++ packages/server/src/api/controllers/rowAction/crud.ts
@@ -1,9 +1,21 @@
+import { CreateRowActionRequest, Ctx, RowAction } from "@budibase/types"
+import sdk from "../../../sdk"
+
 export function find() {
   throw new Error("Function not implemented.")
 }
 
-export function create() {
-  throw new Error("Function not implemented.")
+export async function create(ctx: Ctx<CreateRowActionRequest, RowAction>) {
+  const { tableId } = ctx.params
+
+  const table = await sdk.tables.getTable(tableId)
+  if (!table) {
+    ctx.throw(404)
+  }
+
+  // TODO
+
+  ctx.status = 201
 }
 
 export function update() {
