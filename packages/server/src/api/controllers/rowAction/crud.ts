107bd08e21dfefa5f6f82cb1a7ab41778a727a21
Adria Navarro <adria@budibase.com>
2024-07-10 10:46:15 +0200

    Endpoint scaffolding

---
+++ packages/server/src/api/controllers/rowAction/crud.ts
@@ -0,0 +1,15 @@
+export function find() {
+  throw new Error("Function not implemented.")
+}
+
+export function create() {
+  throw new Error("Function not implemented.")
+}
+
+export function update() {
+  throw new Error("Function not implemented.")
+}
+
+export function remove() {
+  throw new Error("Function not implemented.")
+}
