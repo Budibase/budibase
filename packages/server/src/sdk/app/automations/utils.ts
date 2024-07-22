a6f51caa46c1a028677d6af284bdb4062f649d62
Adria Navarro <adria@budibase.com>
2024-07-22 11:05:46 +0200

    Display rowaction

--- packages/server/src/sdk/app/automations/utils.ts
+++ packages/server/src/sdk/app/automations/utils.ts
@@ -3,6 +3,7 @@ import {
   AutomationActionStepId,
   AutomationBuilderData,
   AutomationTriggerStepId,
+  TableRowActions,
 } from "@budibase/types"
 import sdk from "src/sdk"
 
@@ -25,6 +26,16 @@ export async function getBuilderData(
     return tableNameCache[tableId]
   }
 
+  const rowActionNameCache: Record<string, TableRowActions> = {}
+  async function getRowActionName(tableId: string, rowActionId: string) {
+    if (!rowActionNameCache[tableId]) {
+      const rowActions = await sdk.rowActions.get(tableId)
+      rowActionNameCache[tableId] = rowActions
+    }
+
+    return rowActionNameCache[tableId].actions[rowActionId]?.name
+  }
+
   const result: Record<string, AutomationBuilderData> = {}
   for (const automation of automations) {
     const { trigger } = automation.definition
@@ -34,13 +45,17 @@ export async function getBuilderData(
       continue
     }
 
-    const tableName = await getTableName(trigger.inputs.tableId)
+    const { tableId, rowActionId } = trigger.inputs
+
+    const tableName = await getTableName(tableId)
+
+    const rowActionName = await getRowActionName(tableId, rowActionId)
 
     result[automation._id!] = {
       displayName: `${tableName}: ${automation.name}`,
       triggerInfo: {
         title: "Automation trigger",
-        description: `This trigger is tied to the row action TODO on your ${tableName} table`,
+        description: `This trigger is tied to the row action ${rowActionName} on your ${tableName} table`,
       },
     }
   }
