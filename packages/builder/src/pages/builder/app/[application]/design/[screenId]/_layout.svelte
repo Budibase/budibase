2bc4d3a6da66847aa342afec798230e14eb2f680
Andrew Kingston <andrew@kingston.dev>
2024-08-02 10:00:49 +0100

    Improve more component compatibility with grid layouts

--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/_layout.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/_layout.svelte
@@ -2,11 +2,7 @@
   import AppPanel from "./_components/AppPanel.svelte"
   import * as routify from "@roxi/routify"
   import { syncURLToState } from "helpers/urlStateSync"
-  import {
-    screenStore,
-    selectedScreen,
-    selectedComponent,
-  } from "stores/builder"
+  import { screenStore, selectedScreen } from "stores/builder"
   import { onDestroy } from "svelte"
   import LeftPanel from "./_components/LeftPanel.svelte"
 
