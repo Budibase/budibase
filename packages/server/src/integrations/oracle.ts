e5ae064d2c3366401d03c06346e66e6d0ddc6517
Sam Rose <hello@samwho.dev>
2024-08-06 14:14:42 +0100

    Fix tables showing up multiple times for Oracle in the datasource info endpoint.

--- packages/server/src/integrations/oracle.ts
+++ packages/server/src/integrations/oracle.ts
@@ -423,7 +423,11 @@ class OracleIntegration extends Sql implements DatasourcePlus {
     const columnsResponse = await this.internalQuery<OracleColumnsResponse>({
       sql: OracleIntegration.COLUMNS_SQL,
     })
-    return (columnsResponse.rows || []).map(row => row.TABLE_NAME)
+    const tableNames = new Set<string>()
+    for (const row of columnsResponse.rows || []) {
+      tableNames.add(row.TABLE_NAME)
+    }
+    return Array.from(tableNames)
   }
 
   async testConnection() {
