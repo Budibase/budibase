ac62a9a5a8dbeca7a7b1a7fdb8f4f5630f2355f8
Martin McKeaveney <martinmckeaveney@gmail.com>
2024-08-06 18:46:23 +0100

    authenticated iframe PoC

---
+++ packages/client/src/components/context/EmbedProvider.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Provider from "./Provider.svelte"
+
+  $: data = window.parent?.data
+  $: console.log("parentWindow", data)
+</script>
+
+
+<Provider key="embed" {data}>
+  <slot />
+</Provider>
