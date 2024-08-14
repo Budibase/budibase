385c5f6e99e6a68e0d656baa6f30996dadde13fc
Adria Navarro <adria@budibase.com>
2024-08-14 12:24:49 +0200

    Trim unwanted columns in outputProcessing

--- packages/server/src/api/controllers/row/utils/sqlUtils.ts
+++ packages/server/src/api/controllers/row/utils/sqlUtils.ts
@@ -192,10 +192,10 @@ export function buildSqlFieldList(
   function extractRealFields(table: Table, existing: string[] = []) {
     return Object.entries(table.schema)
       .filter(
-        column =>
-          column[1].type !== FieldType.LINK &&
-          column[1].type !== FieldType.FORMULA &&
-          !existing.find((field: string) => field === column[0])
+        ([columnName, column]) =>
+          column.type !== FieldType.LINK &&
+          column.type !== FieldType.FORMULA &&
+          !existing.find((field: string) => field === columnName)
       )
       .map(column => `${table.name}.${column[0]}`)
   }
