c9bb8e2a2cb00b9f7428720d30707b19251fad30
Andrew Kingston <andrew@kingston.dev>
2024-08-15 16:14:00 +0100

    Add initial new UI for views

--- packages/builder/src/components/backend/TableNavigator/TableNavigator.svelte
+++ packages/builder/src/components/backend/TableNavigator/TableNavigator.svelte
@@ -1,7 +1,5 @@
 <script>
-  import { goto } from "@roxi/routify"
   import TableNavItem from "./TableNavItem/TableNavItem.svelte"
-  import ViewNavItem from "./ViewNavItem/ViewNavItem.svelte"
 
   export let tables
   export let selectTable
@@ -16,18 +14,5 @@
 <div class="hierarchy-items-container">
   {#each sortedTables as table, idx}
     <TableNavItem {table} {idx} on:click={() => selectTable(table._id)} />
-    {#each [...Object.entries(table.views || {})].sort() as [name, view], idx (idx)}
-      <ViewNavItem
-        {view}
-        {name}
-        on:click={() => {
-          if (view.version === 2) {
-            $goto(`./view/v2/${encodeURIComponent(view.id)}`)
-          } else {
-            $goto(`./view/v1/${encodeURIComponent(name)}`)
-          }
-        }}
-      />
-    {/each}
   {/each}
 </div>
