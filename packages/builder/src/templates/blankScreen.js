1473e0312f8c13c761e43dfa5cad9b3bb1419ac4
Andrew Kingston <andrew@kingston.dev>
2024-08-15 10:44:42 +0100

    Update blank screens to use grid layout by default

--- packages/builder/src/templates/blankScreen.js
+++ packages/builder/src/templates/blankScreen.js
@@ -1,7 +1,11 @@
 import { Screen } from "./Screen"
 
 const blankScreen = route => {
-  return new Screen().instanceName("New Screen").route(route).json()
+  return new Screen()
+    .instanceName("New Screen")
+    .customProps({ layout: "grid" })
+    .route(route)
+    .json()
 }
 
 export default blankScreen
