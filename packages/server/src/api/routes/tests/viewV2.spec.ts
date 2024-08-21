cf22205d63b1d34813b08b226dd22422abb898ad
Adria Navarro <adria@budibase.com>
2024-08-21 13:40:17 +0200

    Run tests for lucene as well

--- packages/server/src/api/routes/tests/viewV2.spec.ts
+++ packages/server/src/api/routes/tests/viewV2.spec.ts
@@ -1492,107 +1492,103 @@ describe.each([
         )
       })
 
-      !isLucene &&
-        it("can query on top of the view filters", async () => {
-          await config.api.row.save(table._id!, {
-            one: "foo",
-            two: "bar",
-          })
-          await config.api.row.save(table._id!, {
-            one: "foo2",
-            two: "bar2",
-          })
-          const three = await config.api.row.save(table._id!, {
-            one: "foo3",
-            two: "bar3",
-          })
-
-          const view = await config.api.viewV2.create({
-            tableId: table._id!,
-            name: generator.guid(),
-            query: [
-              {
-                operator: BasicOperator.NOT_EQUAL,
-                field: "two",
-                value: "bar2",
-              },
-            ],
-            schema: {
-              id: { visible: true },
-              one: { visible: false },
-              two: { visible: true },
-            },
-          })
-
-          const response = await config.api.viewV2.search(view.id, {
-            query: {
-              [BasicOperator.NOT_EQUAL]: {
-                two: "bar",
-              },
-              [BasicOperator.NOT_EMPTY]: {
-                two: null,
-              },
-            },
-          })
-          expect(response.rows).toHaveLength(1)
-          expect(response.rows).toEqual(
-            expect.arrayContaining([
-              expect.objectContaining({ _id: three._id }),
-            ])
-          )
+      it("can query on top of the view filters", async () => {
+        await config.api.row.save(table._id!, {
+          one: "foo",
+          two: "bar",
+        })
+        await config.api.row.save(table._id!, {
+          one: "foo2",
+          two: "bar2",
+        })
+        const three = await config.api.row.save(table._id!, {
+          one: "foo3",
+          two: "bar3",
         })
 
-      !isLucene &&
-        it("can query on top of the view filters (using or filters)", async () => {
-          const one = await config.api.row.save(table._id!, {
-            one: "foo",
-            two: "bar",
-          })
-          await config.api.row.save(table._id!, {
-            one: "foo2",
-            two: "bar2",
-          })
-          const three = await config.api.row.save(table._id!, {
-            one: "foo3",
-            two: "bar3",
-          })
-
-          const view = await config.api.viewV2.create({
-            tableId: table._id!,
-            name: generator.guid(),
-            query: [
-              {
-                operator: BasicOperator.NOT_EQUAL,
-                field: "two",
-                value: "bar2",
-              },
-            ],
-            schema: {
-              id: { visible: true },
-              one: { visible: false },
-              two: { visible: true },
+        const view = await config.api.viewV2.create({
+          tableId: table._id!,
+          name: generator.guid(),
+          query: [
+            {
+              operator: BasicOperator.NOT_EQUAL,
+              field: "one",
+              value: "foo2",
             },
-          })
-
-          const response = await config.api.viewV2.search(view.id, {
-            query: {
-              allOr: true,
-              [BasicOperator.NOT_EQUAL]: {
-                two: "bar",
-              },
-              [BasicOperator.NOT_EMPTY]: {
-                two: null,
-              },
-            },
-          })
-          expect(response.rows).toHaveLength(2)
-          expect(response.rows).toEqual(
-            expect.arrayContaining([
-              expect.objectContaining({ _id: one._id }),
-              expect.objectContaining({ _id: three._id }),
-            ])
-          )
+          ],
+          schema: {
+            id: { visible: true },
+            one: { visible: true },
+            two: { visible: true },
+          },
         })
+
+        const response = await config.api.viewV2.search(view.id, {
+          query: {
+            [BasicOperator.EQUAL]: {
+              two: "bar3",
+            },
+            [BasicOperator.NOT_EMPTY]: {
+              two: null,
+            },
+          },
+        })
+        expect(response.rows).toHaveLength(1)
+        expect(response.rows).toEqual(
+          expect.arrayContaining([expect.objectContaining({ _id: three._id })])
+        )
+      })
+
+      it("can query on top of the view filters (using or filters)", async () => {
+        const one = await config.api.row.save(table._id!, {
+          one: "foo",
+          two: "bar",
+        })
+        await config.api.row.save(table._id!, {
+          one: "foo2",
+          two: "bar2",
+        })
+        const three = await config.api.row.save(table._id!, {
+          one: "foo3",
+          two: "bar3",
+        })
+
+        const view = await config.api.viewV2.create({
+          tableId: table._id!,
+          name: generator.guid(),
+          query: [
+            {
+              operator: BasicOperator.NOT_EQUAL,
+              field: "two",
+              value: "bar2",
+            },
+          ],
+          schema: {
+            id: { visible: true },
+            one: { visible: false },
+            two: { visible: true },
+          },
+        })
+
+        const response = await config.api.viewV2.search(view.id, {
+          query: {
+            allOr: true,
+            [BasicOperator.NOT_EQUAL]: {
+              two: "bar",
+            },
+            [BasicOperator.NOT_EMPTY]: {
+              two: null,
+            },
+          },
+        })
+        expect(response.rows).toHaveLength(2)
+        expect(response.rows).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ _id: one._id }),
+            expect.objectContaining({ _id: three._id }),
+          ])
+        )
+      })
 
       isLucene &&
         it.each([true, false])(
