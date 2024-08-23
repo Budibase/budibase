547638a354a5f07b277dec6c03f047c2262816a3
Andrew Kingston <andrew@kingston.dev>
2024-08-23 14:45:45 +0100

    Lint

--- packages/client/src/constants.js
+++ packages/client/src/constants.js
@@ -3,8 +3,7 @@ export { FieldType as FieldTypes } from "@budibase/types"
 export const ActionTypes = {
   ValidateForm: "ValidateForm",
   UpdateFieldValue: "UpdateFieldValue",
-  RefreshDatasource:
-     "RefreshDatasource",
+  RefreshDatasource: "RefreshDatasource",
   AddDataProviderQueryExtension: "AddDataProviderQueryExtension",
   RemoveDataProviderQueryExtension: "RemoveDataProviderQueryExtension",
   SetDataProviderSorting: "SetDataProviderSorting",
