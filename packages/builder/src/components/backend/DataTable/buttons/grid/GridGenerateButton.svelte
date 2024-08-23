495c01c1a2a22d8f480dbec4cdf99bc15ecfbabc
Andrew Kingston <andrew@kingston.dev>
2024-08-23 11:19:56 +0100

    Allow generating screens from the data section

--- packages/builder/src/components/backend/DataTable/buttons/grid/GridGenerateButton.svelte
+++ packages/builder/src/components/backend/DataTable/buttons/grid/GridGenerateButton.svelte
@@ -1,15 +1,24 @@
 <script>
   import { ActionButton, Menu, MenuItem, notifications } from "@budibase/bbui"
   import { getContext } from "svelte"
-  import { automationStore, tables, builderStore } from "stores/builder"
+  import {
+    automationStore,
+    tables,
+    builderStore,
+    viewsV2,
+  } from "stores/builder"
   import { TriggerStepID } from "constants/backend/automations"
   import { goto } from "@roxi/routify"
   import DetailPopover from "components/common/DetailPopover.svelte"
   import MagicWand from "./magic-wand.svg"
+  import { AutoScreenTypes } from "constants"
+  import CreateScreenModal from "pages/builder/app/[application]/design/_components/NewScreen/CreateScreenModal.svelte"
 
   const { datasource } = getContext("grid")
 
   let popover
+  let createScreenModal
+  let isCreatingScreen
 
   $: triggers = $automationStore.blockDefinitions.CREATABLE_TRIGGER
   $: table = $tables.list.find(table => table._id === $datasource.tableId)
@@ -57,6 +66,17 @@
       notifications.error("Error creating automation")
     }
   }
+
+  const startScreenWizard = autoScreenType => {
+    popover.hide()
+    let preSelected
+    if ($datasource.type === "table") {
+      preSelected = $tables.list.find(x => x._id === $datasource.tableId)
+    } else {
+      preSelected = $viewsV2.list.find(x => x.id === $datasource.id)
+    }
+    createScreenModal.show(autoScreenType, preSelected)
+  }
 </script>
 
 <DetailPopover title="Generate" bind:this={popover}>
@@ -88,9 +108,29 @@
       >
         Automation: when row is updated
       </MenuItem>
+      <MenuItem
+        icon="WebPage"
+        on:click={() => {
+          open = false
+          startScreenWizard(AutoScreenTypes.TABLE)
+        }}
+      >
+        CRUD app
+      </MenuItem>
+      <MenuItem
+        icon="WebPage"
+        on:click={() => {
+          open = false
+          startScreenWizard(AutoScreenTypes.FORM)
+        }}
+      >
+        Form
+      </MenuItem>
     </Menu>
   </div>
 </DetailPopover>
+
+<CreateScreenModal bind:this={createScreenModal} />
 
 <style>
   .menu {
