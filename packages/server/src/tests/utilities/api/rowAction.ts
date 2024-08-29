3fcfec0c2d7f27644e03cb6b6c6b50715de13330
Adria Navarro <adria@budibase.com>
2024-08-29 16:11:05 +0200

    Clean

--- packages/server/src/tests/utilities/api/rowAction.ts
+++ packages/server/src/tests/utilities/api/rowAction.ts
@@ -18,8 +18,8 @@ export class RowActionAPI extends TestAPI {
       {
         body: rowAction,
         expectations: {
+          status: 201,
           ...expectations,
-          status: expectations?.status || 201,
         },
         ...config,
       }
@@ -82,7 +82,10 @@ export class RowActionAPI extends TestAPI {
     return await this._post<RowActionResponse>(
       `/api/tables/${tableId}/actions/${rowActionId}/permissions/${viewId}`,
       {
-        expectations,
+        expectations: {
+          status: 200,
+          ...expectations,
+        },
         ...config,
       }
     )
@@ -98,7 +101,10 @@ export class RowActionAPI extends TestAPI {
     return await this._delete<RowActionResponse>(
       `/api/tables/${tableId}/actions/${rowActionId}/permissions/${viewId}`,
       {
-        expectations,
+        expectations: {
+          status: 200,
+          ...expectations,
+        },
         ...config,
       }
     )
