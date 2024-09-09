785ac58d84a189f863c7625b06a99f60196e1d8e
Adria Navarro <adria@budibase.com>
2024-09-09 12:41:59 +0200

    Allow selecting columns in design

---
+++ packages/frontend-core/src/utils/schema.js
@@ -0,0 +1,27 @@
+export function enrichSchemaWithRelColumns(schema) {
+  if (!schema) {
+    return
+  }
+  const result = Object.keys(schema).reduce((acc, c) => {
+    const field = schema[c]
+    acc[c] = field
+
+    if (field.columns) {
+      for (const relColumn of Object.keys(field.columns)) {
+        const relField = field.columns[relColumn]
+        if (!relField.visible) {
+          continue
+        }
+        const name = `${field.name}.${relColumn}`
+        acc[name] = {
+          ...relField,
+          name,
+          related: { field: c, subField: relColumn },
+        }
+      }
+    }
+    return acc
+  }, {})
+
+  return result
+}
