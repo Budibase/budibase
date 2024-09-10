9da84b19f985dc3cefb7877d212f8feb83b9f5a8
Andrew Kingston <andrew@kingston.dev>
2024-09-10 09:11:52 +0100

    Add scaffolding for new role editor modal

---
+++ packages/builder/src/components/backend/DataTable/buttons/RoleEditor.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {
+    Button,
+    Modal,
+    ModalContent,
+    Drawer,
+    DrawerContent,
+  } from "@budibase/bbui"
+
+  let drawer
+
+  $: invalid = false
+
+  const save = async () => {
+    drawer.hide()
+  }
+</script>
+
+<Button secondary icon="UsersLock" on:click on:click={drawer.show}>
+  Role editor
+</Button>
+
+<Drawer
+  bind:this={drawer}
+  title="Role editor"
+  on:drawerHide
+  on:drawerShow
+  forceModal
+>
+  <Button disabled={invalid} cta slot="buttons" on:click={save}>Save</Button>
+  <DrawerContent slot="body">asdasdasd</DrawerContent>
+</Drawer>
