e281250569a9bcd67d9d2e998959682018097b3e
Martin McKeaveney <martinmckeaveney@gmail.com>
2024-09-30 14:00:12 +0100

    ai cron helper E2E

--- packages/builder/src/components/automation/SetupPanel/CronBuilder.svelte
+++ packages/builder/src/components/automation/SetupPanel/CronBuilder.svelte
@@ -1,41 +1,58 @@
 <script>
-  import { Button, Select, Input, Label } from "@budibase/bbui"
+  import { Button, Select, Icon, InlineAlert, Input, Label, Layout, Popover } from "@budibase/bbui"
   import { onMount, createEventDispatcher } from "svelte"
   import { flags } from "stores/builder"
+  import { licensing } from "stores/portal"
+  import { API } from "api"
+
   import { helpers, REBOOT_CRON } from "@budibase/shared-core"
 
   const dispatch = createEventDispatcher()
 
-  export let value
+  export let cronExpression
+
   let error
+  let nextExecutions
 
+  // AI prompt
+  let aiCronPrompt = ""
+  let loadingAICronExpression = false
+
+  $: aiEnabled = $licensing.customAIConfigsEnabled || $licensing.budibaseAIEnabled
   $: {
-    const exists = CRON_EXPRESSIONS.some(cron => cron.value === value)
-    const customIndex = CRON_EXPRESSIONS.findIndex(
-      cron => cron.label === "Custom"
-    )
-
-    if (!exists && customIndex === -1) {
-      CRON_EXPRESSIONS[0] = { label: "Custom", value: value }
-    } else if (exists && customIndex !== -1) {
-      CRON_EXPRESSIONS.splice(customIndex, 1)
+    if (cronExpression) {
+      try {
+        nextExecutions = helpers.cron.getNextExecutionDates(cronExpression).join("\n")
+      } catch (err) {
+        nextExecutions = null
+      }
     }
   }
 
   const onChange = e => {
-    if (value !== REBOOT_CRON) {
+    if (e.detail !== REBOOT_CRON) {
       error = helpers.cron.validate(e.detail).err
     }
-    if (e.detail === value || error) {
+    if (e.detail === cronExpression || error) {
       return
     }
 
-    value = e.detail
+    cronExpression = e.detail
     dispatch("change", e.detail)
   }
 
+  const updatePreset = e => {
+    aiCronPrompt = ""
+    onChange(e)
+  }
+
+  const updateCronExpression = e => {
+    aiCronPrompt = ""
+    cronExpression = null
+    onChange(e)
+  }
+
   let touched = false
-  let presets = false
 
   const CRON_EXPRESSIONS = [
     {
@@ -64,45 +81,129 @@
       })
     }
   })
+
+  async function generateAICronExpression() {
+    loadingAICronExpression = true
+    // make the API call to generate the cron expression
+    const response = await API.generateCronExpression({ prompt: aiCronPrompt })
+    // return it and set it in the field
+    cronExpression = response.message
+    dispatch("change", response.message)
+    loadingAICronExpression = false
+  }
 </script>
 
-<div class="block-field">
+<Layout noPadding gap="S">
+  <Select
+    on:change={updatePreset}
+    value={cronExpression || "Custom"}
+    secondary
+    extraThin
+    label="Use a Preset (Optional)"
+    options={CRON_EXPRESSIONS}
+  />
+  {#if aiEnabled}
+    <div class="cron-ai-generator">
+      <Input
+        bind:value={aiCronPrompt}
+        label="Generate Cron Expression with AI"
+        size="S"
+        placeholder="Run every hour between 1pm to 4pm everyday of the week"
+      />
+      {#if aiCronPrompt}
+        <div
+          class="icon"
+          class:pulsing-text={loadingAICronExpression}
+          on:click={generateAICronExpression}
+        >
+          <Icon size="S" name="MagicWand" />
+        </div>
+      {/if}
+    </div>
+  {/if}
   <Input
+    label="Cron Expression"
     {error}
-    on:change={onChange}
-    {value}
+    on:change={updateCronExpression}
+    value={cronExpression}
     on:blur={() => (touched = true)}
     updateOnChange={false}
   />
-  {#if touched && !value}
+  {#if touched && !cronExpression}
     <Label><div class="error">Please specify a CRON expression</div></Label>
   {/if}
-  <div class="presets">
-    <Button on:click={() => (presets = !presets)}
-      >{presets ? "Hide" : "Show"} Presets</Button
-    >
-    {#if presets}
-      <Select
-        on:change={onChange}
-        value={value || "Custom"}
-        secondary
-        extraThin
-        label="Presets"
-        options={CRON_EXPRESSIONS}
-      />
-    {/if}
-  </div>
-</div>
+  {#if nextExecutions}
+    <InlineAlert
+      type="info"
+      header="Next Executions"
+      message={nextExecutions}
+    />
+  {/if}
+</Layout>
 
 <style>
-  .presets {
-    margin-top: var(--spacing-m);
+  .cron-ai-generator {
+    flex: 1;
+    position: relative;
   }
-  .block-field {
-    padding-top: var(--spacing-s);
+  .icon {
+    right: 1px;
+    bottom: 1px;
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    box-sizing: border-box;
+    border-left: 1px solid var(--spectrum-alias-border-color);
+    border-top-right-radius: var(--spectrum-alias-border-radius-regular);
+    border-bottom-right-radius: var(--spectrum-alias-border-radius-regular);
+    width: 31px;
+    color: var(--spectrum-alias-text-color);
+    background-color: var(--spectrum-global-color-gray-75);
+    transition: background-color
+    var(--spectrum-global-animation-duration-100, 130ms),
+    box-shadow var(--spectrum-global-animation-duration-100, 130ms),
+    border-color var(--spectrum-global-animation-duration-100, 130ms);
+    height: calc(var(--spectrum-alias-item-height-m) - 2px);
   }
+
+  .icon:hover {
+    cursor: pointer;
+    color: var(--spectrum-alias-text-color-hover);
+    background-color: var(--spectrum-global-color-gray-50);
+    border-color: var(--spectrum-alias-border-color-hover);
+  }
+
+  .control:not(.disabled) :global(.spectrum-Textfield-input) {
+    padding-right: 40px;
+  }
+
+
   .error {
     padding-top: var(--spacing-xs);
     color: var(--spectrum-global-color-red-500);
+  }
+
+  .pulsing-text {
+    font-size: 24px;
+    font-weight: bold;
+    /*color: #333;*/
+    animation: pulse 1.5s infinite;
+  }
+
+  @keyframes pulse {
+    0% {
+      opacity: 0.3;
+      transform: scale(1);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.05);
+    }
+    100% {
+      opacity: 0.3;
+      transform: scale(1);
+    }
   }
 </style>
