db41b4c385c98ced392c9feec20fe5a0adf8e100
Andrew Kingston <andrew@kingston.dev>
2024-10-01 10:25:32 +0100

    Add initial differences for calculation views

---
+++ packages/builder/src/components/backend/DataTable/buttons/grid/GridViewCalculationButton.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { ActionButton, Modal, ModalContent } from "@budibase/bbui"
+  import { getContext, createEventDispatcher } from "svelte"
+
+  const { datasource } = getContext("grid")
+  const dispatch = createEventDispatcher()
+
+  let modal
+</script>
+
+<ActionButton icon="WebPage" quiet on:click={modal?.show}>
+  Configure calculations
+</ActionButton>
+
+<Modal bind:this={modal}>
+  <ModalContent title="Calculations" confirmText="Save" size="L">
+    Show calculations which are based on
+  </ModalContent>
+</Modal>
