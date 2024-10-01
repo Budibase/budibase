b3afbd57be0310d7dcfcbdeeeba0b957bd143c25
Martin McKeaveney <martinmckeaveney@gmail.com>
2024-10-01 16:55:20 +0100

    longform text

--- packages/frontend-core/src/components/grid/lib/renderers.js
+++ packages/frontend-core/src/components/grid/lib/renderers.js
@@ -30,8 +30,7 @@ const TypeComponentMap = {
   [FieldType.ATTACHMENT_SINGLE]: AttachmentSingleCell,
   [FieldType.LINK]: RelationshipCell,
   [FieldType.FORMULA]: FormulaCell,
-  // TODO: fix
-  [FieldType.AI]: FormulaCell,
+  [FieldType.AI]: LongFormCell,
   [FieldType.JSON]: JSONCell,
   [FieldType.BB_REFERENCE]: BBReferenceCell,
   [FieldType.BB_REFERENCE_SINGLE]: BBReferenceSingleCell,
