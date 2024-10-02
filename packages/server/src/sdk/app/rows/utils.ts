4dd6afd4352ba774bde40764cf6d9a315d983d7b
Sam Rose <hello@samwho.dev>
2024-10-02 09:57:18 +0100

    Symbolise the special __bb_total count field name.

--- packages/server/src/sdk/app/rows/utils.ts
+++ packages/server/src/sdk/app/rows/utils.ts
@@ -22,6 +22,7 @@ import { extractViewInfoFromID, isRelationshipColumn } from "../../../db/utils"
 import { isSQL } from "../../../integrations/utils"
 import { docIds } from "@budibase/backend-core"
 import { getTableFromSource } from "../../../api/controllers/row/utils"
+import { COUNT_FIELD_NAME } from "@budibase/backend-core/src/sql/sql"
 
 const SQL_CLIENT_SOURCE_MAP: Record<SourceName, SqlClient | undefined> = {
   [SourceName.POSTGRES]: SqlClient.POSTGRES,
@@ -57,8 +58,8 @@ export function getSQLClient(datasource: Datasource): SqlClient {
 export function processRowCountResponse(
   response: DatasourcePlusQueryResponse
 ): number {
-  if (response && response.length === 1 && "__bb_total" in response[0]) {
-    const total = response[0].__bb_total
+  if (response && response.length === 1 && COUNT_FIELD_NAME in response[0]) {
+    const total = response[0][COUNT_FIELD_NAME]
     return typeof total === "number" ? total : parseInt(total)
   } else {
     throw new Error("Unable to count rows in query - no count response")
