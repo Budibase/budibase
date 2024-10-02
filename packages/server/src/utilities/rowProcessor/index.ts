e6d80707c6d513a44fab54d21d9c61a7e06b266f
Andrew Kingston <andrew@kingston.dev>
2024-10-02 10:54:17 +0100

    Cleanup

--- packages/server/src/utilities/rowProcessor/index.ts
+++ packages/server/src/utilities/rowProcessor/index.ts
@@ -134,10 +134,10 @@ async function processDefaultValues(table: Table, row: Row) {
 
   for (const [key, schema] of Object.entries(table.schema)) {
     if ("default" in schema && schema.default != null && row[key] == null) {
-      let processed = schema.default
-      if (typeof processed === "string") {
-        processed = await processString(schema.default, ctx)
-      }
+      const processed =
+        typeof schema.default === "string"
+          ? await processString(schema.default, ctx)
+          : schema.default
       try {
         row[key] = coerce(processed, schema.type)
       } catch (err: any) {
