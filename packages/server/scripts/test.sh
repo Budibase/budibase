6a36de3d26df802cc78d5005d73551a6e2fa4669
Sam Rose <hello@samwho.dev>
2024-10-08 15:03:46 +0100

    Revert test script change.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -ex
+set -e
 
 if [[ -n $CI ]]
 then
-  export NODE_OPTIONS="--max-old-space-size=16384 --no-node-snapshot $NODE_OPTIONS"
-  jest --maxWorkers=100% --forceExit --bail --ci $@
+  export NODE_OPTIONS="--max-old-space-size=4096 --no-node-snapshot $NODE_OPTIONS"
+  jest --coverage --maxWorkers=4 --forceExit --workerIdleMemoryLimit=2000MB --bail $@
 else
+  # --maxWorkers performs better in development
   export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
-  jest --maxWorkers=75% --forceExit $@
+  jest --coverage --maxWorkers=2 --forceExit $@
 fi
