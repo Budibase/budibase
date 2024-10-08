49e14d9601b5404180139244457df61d189fa171
Sam Rose <hello@samwho.dev>
2024-10-08 12:10:29 +0100

    Attempt to cache docker images, also use more workers in CI.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -4,9 +4,8 @@ set -ex
 if [[ -n $CI ]]
 then
   export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
-  jest --coverage --maxWorkers=75% --forceExit --bail --ci $@
+  jest --maxWorkers=150% --forceExit --bail --ci $@
 else
-  # --maxWorkers performs better in development
   export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
-  jest --coverage --maxWorkers=2 --forceExit $@
+  jest --maxWorkers=75% --forceExit $@
 fi
