ea9c0aa1ac6d4675b7aaf729064f1a5d7805bd82
Sam Rose <hello@samwho.dev>
2024-10-08 12:22:40 +0100

    More CI tweaking.

--- packages/server/scripts/test.sh
+++ packages/server/scripts/test.sh
@@ -3,8 +3,8 @@ set -ex
 
 if [[ -n $CI ]]
 then
-  export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
-  jest --maxWorkers=150% --forceExit --bail --ci $@
+  export NODE_OPTIONS="--max-old-space-size=16384 --no-node-snapshot $NODE_OPTIONS"
+  jest --maxWorkers=100% --forceExit --bail --ci $@
 else
   export NODE_OPTIONS="--no-node-snapshot $NODE_OPTIONS"
   jest --maxWorkers=75% --forceExit $@
