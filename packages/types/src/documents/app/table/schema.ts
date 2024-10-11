1bcd2590d3285f404e4f84c48bae8e8cf6992589
Sam Rose <hello@samwho.dev>
2024-10-11 12:28:20 +0100

    Allow BB_REFERNCE_SINGLE to have default values.

--- packages/types/src/documents/app/table/schema.ts
+++ packages/types/src/documents/app/table/schema.ts
@@ -126,6 +126,7 @@ export interface BBReferenceSingleFieldMetadata
   extends Omit<BaseFieldSchema, "subtype"> {
   type: FieldType.BB_REFERENCE_SINGLE
   subtype: Exclude<BBReferenceFieldSubType, BBReferenceFieldSubType.USERS>
+  default?: string
 }
 
 export interface AttachmentFieldMetadata extends BaseFieldSchema {
