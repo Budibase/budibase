b8c857dc5fdade4f3ae8f3d84d6759a08052e516
Sam Rose <hello@samwho.dev>
2024-10-21 13:41:28 +0100

    Fix lint.

--- packages/server/src/sdk/app/views/internal.ts
+++ packages/server/src/sdk/app/views/internal.ts
@@ -5,7 +5,6 @@ import sdk from "../../../sdk"
 import * as utils from "../../../db/utils"
 import { enrichSchema, isV2 } from "."
 import { ensureQuerySet, ensureQueryUISet } from "./utils"
-import { processTable } from "../tables/getters"
 
 export async function get(viewId: string): Promise<ViewV2> {
   const { tableId } = utils.extractViewInfoFromID(viewId)
