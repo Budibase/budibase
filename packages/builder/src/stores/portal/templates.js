7fd48dbd0fa11842582ea3b76e49d3ef57786b38
Conor Webb <conorwebb59@gmail.com>
2024-11-01 15:22:43 +0000

    v3 test data

--- packages/builder/src/stores/portal/templates.js
+++ packages/builder/src/stores/portal/templates.js
@@ -8,6 +8,18 @@ export function templatesStore() {
     subscribe,
     load: async () => {
       const templates = await API.getAppTemplates()
+      // Test data remove after the UI has been built
+      const newTemplate = {
+        name: "test",
+        category: "test",
+        v3: true,
+        description: "test test",
+        icon: "test",
+        background: "#20a3a8",
+        url: "test",
+      }
+      templates.push(newTemplate)
+      //
       set(templates)
     },
   }
