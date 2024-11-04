7b55ab61b9fa5e2329184db6e13bb936f6bb0155
mike12345567 <me@michaeldrury.co.uk>
2024-11-04 13:33:12 +0000

    Making sure all protected columns are kept during row save/patch operations.

--- packages/server/src/utilities/rowProcessor/index.ts
+++ packages/server/src/utilities/rowProcessor/index.ts
@@ -31,6 +31,8 @@ import {
 import { isExternalTableID } from "../../integrations/utils"
 import {
   helpers,
+  isExternalColumnName,
+  isInternalColumnName,
   PROTECTED_EXTERNAL_COLUMNS,
   PROTECTED_INTERNAL_COLUMNS,
 } from "@budibase/shared-core"
@@ -202,14 +204,18 @@ export async function inputProcessing(
   const clonedRow = cloneDeep(row)
   const table = await getTableFromSource(source)
 
-  const dontCleanseKeys = ["type", "_id", "_rev", "tableId"]
   for (const [key, value] of Object.entries(clonedRow)) {
     const field = table.schema[key]
     // cleanse fields that aren't in the schema
+    if (
+      !field && isExternalTableID(table._id!)
+        ? !isExternalColumnName(key)
+        : !isInternalColumnName(key)
+    ) {
+      delete clonedRow[key]
+    }
+    // field isn't found - might be a built-in column, skip over it
     if (!field) {
-      if (dontCleanseKeys.indexOf(key) === -1) {
-        delete clonedRow[key]
-      }
       continue
     }
     // remove any formula values, they are to be generated
