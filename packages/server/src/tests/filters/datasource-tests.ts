09f7d2c3909ae35f7f7000c707aa5a3202621464
Sam Rose <hello@samwho.dev>
2024-11-06 17:52:15 +0000

    Fix jest filters.

--- packages/server/src/tests/filters/datasource-tests.ts
+++
@@ -1,9 +0,0 @@
-import { DATASOURCE_TEST_FILES } from "."
-
-export default (paths: string[]) => {
-  return {
-    filtered: paths
-      .filter(path => DATASOURCE_TEST_FILES.includes(path))
-      .map(path => ({ test: path })),
-  }
-}
