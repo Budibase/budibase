fa5c004ecc129605707c112d6da14b35e2c452f8
Adria Navarro <adria@budibase.com>
2024-11-07 12:12:23 +0100

    PR comments

--- packages/backend-core/tests/core/utilities/utils/queue.ts
+++ packages/backend-core/tests/core/utilities/utils/queue.ts
@@ -4,17 +4,8 @@ import { GenericContainer, Wait } from "testcontainers"
 export async function useRealQueues() {
   const redis = await new GenericContainer("redis")
     .withExposedPorts(6379)
-    .withEnvironment({})
-    .withLabels({ "com.budibase": "true" })
-    .withReuse()
     .withWaitStrategy(
-      Wait.forSuccessfulCommand(
-        `until redis-cli  ping | grep -q PONG; do
-          echo "Waiting for Redis to be ready..."
-          sleep 1
-        done
-        echo "Redis is ready!"`
-      ).withStartupTimeout(10000)
+      Wait.forSuccessfulCommand(`redis-cli`).withStartupTimeout(10000)
     )
     .start()
 
