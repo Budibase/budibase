544e3a9da46643dd010cabd4f468eb69ec77265c
Andrew Kingston <andrew@kingston.dev>
2024-11-27 10:37:37 +0000

    Update audit logs endpoints to use TS

---
+++ packages/frontend-core/src/api/auditLogs.ts
@@ -0,0 +1,70 @@
+import {
+  SearchAuditLogsRequest,
+  SearchAuditLogsResponse,
+  DefinitionsAuditLogsResponse,
+  DownloadAuditLogsRequest,
+} from "@budibase/types"
+import { BaseAPIClient } from "./types"
+
+export interface AuditLogsEndpoints {
+  searchAuditLogs: (
+    opts: SearchAuditLogsRequest
+  ) => Promise<SearchAuditLogsResponse>
+  getEventDefinitions: () => Promise<DefinitionsAuditLogsResponse>
+  getDownloadUrl: (opts: DownloadAuditLogsRequest) => string
+}
+
+const buildOpts = (opts: SearchAuditLogsRequest) => {
+  const { bookmark, startDate, endDate, fullSearch, events, userIds, appIds } =
+    opts
+  const parsedOpts: SearchAuditLogsRequest = {}
+
+  if (bookmark) {
+    parsedOpts.bookmark = bookmark
+  }
+
+  if (opts.startDate && endDate) {
+    parsedOpts.startDate = startDate
+    parsedOpts.endDate = endDate
+  } else if (startDate && !endDate) {
+    parsedOpts.startDate = startDate
+  }
+
+  if (fullSearch) {
+    parsedOpts.fullSearch = fullSearch
+  }
+
+  if (events?.length) {
+    parsedOpts.events = events
+  }
+
+  if (userIds?.length) {
+    parsedOpts.userIds = userIds
+  }
+
+  if (appIds?.length) {
+    parsedOpts.appIds = appIds
+  }
+
+  return parsedOpts
+}
+
+export const buildAuditLogsEndpoints = (
+  API: BaseAPIClient
+): AuditLogsEndpoints => ({
+  searchAuditLogs: async opts => {
+    return await API.post({
+      url: `/api/global/auditlogs/search`,
+      body: buildOpts(opts),
+    })
+  },
+  getEventDefinitions: async () => {
+    return await API.get({
+      url: `/api/global/auditlogs/definitions`,
+    })
+  },
+  getDownloadUrl: opts => {
+    const query = encodeURIComponent(JSON.stringify(opts))
+    return `/api/global/auditlogs/download?query=${query}`
+  },
+})
