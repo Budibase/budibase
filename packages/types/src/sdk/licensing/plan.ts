525b6f84bb5d81a4b14745cb365b2b2503f4ca96
melohagan <101575380+melohagan@users.noreply.github.com>
2024-11-28 10:37:44 +0000

    Remove references to day passes (#15079)
    * Remove references to day passes
    
    * lint
    
    * Point pro to branch with no day passes code
    
    * update yarn lock

--- packages/types/src/sdk/licensing/plan.ts
+++ packages/types/src/sdk/licensing/plan.ts
@@ -38,7 +38,6 @@ export interface AvailablePrice {
 export enum PlanModel {
   PER_USER = "perUser",
   PER_CREATOR_PER_USER = "per_creator_per_user",
-  DAY_PASS = "dayPass",
 }
 
 export interface PurchasedPlan {
@@ -49,7 +48,6 @@ export interface PurchasedPlan {
 }
 
 export interface PurchasedPrice extends AvailablePrice {
-  dayPasses: number | undefined
   /** @deprecated - now at the plan level via model */
   isPerUser: boolean
 }
