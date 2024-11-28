a73165470e5ad3c93480829662dbe8d8a318d6ac
Adria Navarro <adria@budibase.com>
2024-11-28 10:31:44 +0100

    Only build sourcemap for !prod

--- packages/bbui/vite.config.js
+++ packages/bbui/vite.config.js
@@ -3,27 +3,30 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 import postcss from "postcss"
 import path from "path"
 
-export default defineConfig({
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: "src/index.js",
-      formats: ["es"],
-      fileName: "bbui.es",
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production"
+  return {
+    build: {
+      sourcemap: !isProduction,
+      lib: {
+        entry: "src/index.js",
+        formats: ["es"],
+        fileName: "bbui.es",
+      },
     },
-  },
-  plugins: [
-    svelte({
-      emitCss: true,
-    }),
-  ],
-  css: {
-    postcss,
-  },
-  resolve: {
-    alias: {
-      "@budibase/shared-core": path.resolve(__dirname, "../shared-core/src"),
-      "@budibase/types": path.resolve(__dirname, "../types/src"),
+    plugins: [
+      svelte({
+        emitCss: true,
+      }),
+    ],
+    css: {
+      postcss,
     },
-  },
+    resolve: {
+      alias: {
+        "@budibase/shared-core": path.resolve(__dirname, "../shared-core/src"),
+        "@budibase/types": path.resolve(__dirname, "../types/src"),
+      },
+    },
+  }
 })
