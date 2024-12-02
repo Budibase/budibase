1b6e6ed7db900c36b8956c4bee6404f393b67c05
Andrew Kingston <andrew@kingston.dev>
2024-12-02 15:23:48 +0000

    Type flag endpoints

--- packages/frontend-core/src/api/flags.ts
+++ packages/frontend-core/src/api/flags.ts
@@ -1,4 +1,13 @@
-export const buildFlagEndpoints = API => ({
+import { Flags, SetFlagRequest } from "@budibase/types"
+import { BaseAPIClient } from "./types"
+
+export interface FlagEndpoints {
+  getFlags: () => Promise<Flags>
+  updateFlag: (flag: string, value: any) => Promise<{ message: string }>
+  toggleUiFeature: (value: string) => Promise<{ message: string }>
+}
+
+export const buildFlagEndpoints = (API: BaseAPIClient): FlagEndpoints => ({
   /**
    * Gets the current user flags object.
    */
@@ -13,8 +22,8 @@ export const buildFlagEndpoints = API => ({
    * @param flag the flag to update
    * @param value the value to set the flag to
    */
-  updateFlag: async ({ flag, value }) => {
-    return await API.post({
+  updateFlag: async (flag, value) => {
+    return await API.post<SetFlagRequest, { message: string }>({
       url: "/api/users/flags",
       body: {
         flag,
@@ -26,7 +35,7 @@ export const buildFlagEndpoints = API => ({
    * Allows us to experimentally toggle a beta UI feature through a cookie.
    * @param value the feature to toggle
    */
-  toggleUiFeature: async ({ value }) => {
+  toggleUiFeature: async value => {
     return await API.post({
       url: `/api/beta/${value}`,
     })
