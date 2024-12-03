efcdd360e3d02b3992ae796fa8d16ecab73d4426
mike12345567 <me@michaeldrury.co.uk>
2024-12-03 17:42:31 +0000

    Routing API typing.

--- packages/server/src/utilities/routing/index.ts
+++ packages/server/src/utilities/routing/index.ts
@@ -1,24 +1,19 @@
 import { createRoutingView } from "../../db/views/staticViews"
 import { ViewName, getQueryIndex, UNICODE_MAX } from "../../db/utils"
 import { context } from "@budibase/backend-core"
-import { ScreenRouting, Document } from "@budibase/types"
+import { ScreenRoutesViewOutput } from "@budibase/types"
 
-interface ScreenRoutesView extends Document {
-  id: string
-  routing: ScreenRouting
-}
-
-export async function getRoutingInfo(): Promise<ScreenRoutesView[]> {
+export async function getRoutingInfo(): Promise<ScreenRoutesViewOutput[]> {
   const db = context.getAppDB()
   try {
-    const allRouting = await db.query<ScreenRoutesView>(
+    const allRouting = await db.query<ScreenRoutesViewOutput>(
       getQueryIndex(ViewName.ROUTING),
       {
         startkey: "",
         endkey: UNICODE_MAX,
       }
     )
-    return allRouting.rows.map(row => row.value as ScreenRoutesView)
+    return allRouting.rows.map(row => row.value)
   } catch (err: any) {
     // check if the view doesn't exist, it should for all new instances
     /* istanbul ignore next */
