a559b3392d363ad18a6d7d3e068c8bd34df678eb
mike12345567 <me@michaeldrury.co.uk>
2024-12-04 14:32:27 +0000

    Template API typing.

--- packages/types/src/api/web/template.ts
+++ packages/types/src/api/web/template.ts
@@ -1,4 +1,20 @@
-export interface FetchTemplateResponse {}
+export enum TemplateType {
+  APP = "app",
+}
+
+export interface Template {
+  background: string
+  icon: string
+  category: string
+  description: string
+  name: string
+  url: string
+  type: TemplateType
+  key: string
+  image: string
+}
+
+export type FetchTemplateResponse = Template[]
 
 export interface DownloadTemplateResponse {
   message: string
