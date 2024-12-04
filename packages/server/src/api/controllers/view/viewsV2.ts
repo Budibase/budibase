aa2d6779cde3622ca3089e0b8787fad907ba139f
mike12345567 <me@michaeldrury.co.uk>
2024-12-04 15:02:16 +0000

    Small view API typing update.

--- packages/server/src/api/controllers/view/viewsV2.ts
+++ packages/server/src/api/controllers/view/viewsV2.ts
@@ -4,7 +4,6 @@ import {
   Ctx,
   RequiredKeys,
   UpdateViewRequest,
-  ViewResponse,
   ViewResponseEnriched,
   ViewV2,
   BasicViewFieldMetadata,
@@ -15,6 +14,9 @@ import {
   ViewFetchResponseEnriched,
   CountDistinctCalculationFieldMetadata,
   CountCalculationFieldMetadata,
+  CreateViewResponse,
+  UpdateViewResponse,
+  DeleteViewResponse,
 } from "@budibase/types"
 import { builderSocket, gridSocket } from "../../../websockets"
 import { helpers } from "@budibase/shared-core"
@@ -132,7 +134,7 @@ export async function fetch(ctx: Ctx<void, ViewFetchResponseEnriched>) {
   }
 }
 
-export async function create(ctx: Ctx<CreateViewRequest, ViewResponse>) {
+export async function create(ctx: Ctx<CreateViewRequest, CreateViewResponse>) {
   const view = ctx.request.body
   const { tableId } = view
 
@@ -159,7 +161,7 @@ export async function create(ctx: Ctx<CreateViewRequest, ViewResponse>) {
   gridSocket?.emitViewUpdate(ctx, result)
 }
 
-export async function update(ctx: Ctx<UpdateViewRequest, ViewResponse>) {
+export async function update(ctx: Ctx<UpdateViewRequest, UpdateViewResponse>) {
   const view = ctx.request.body
 
   if (view.version !== 2) {
@@ -196,7 +198,7 @@ export async function update(ctx: Ctx<UpdateViewRequest, ViewResponse>) {
   gridSocket?.emitViewUpdate(ctx, result)
 }
 
-export async function remove(ctx: Ctx) {
+export async function remove(ctx: Ctx<void, void>) {
   const { viewId } = ctx.params
 
   const view = await sdk.views.remove(viewId)
