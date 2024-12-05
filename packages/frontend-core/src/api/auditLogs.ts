a0938bf1a4495bc0c52e695dda3e25ce350ee244
Andrew Kingston <andrew@kingston.dev>
2024-12-05 15:22:18 +0000

    Clean audit log endpoint code

--- packages/frontend-core/src/api/auditLogs.ts
+++ packages/frontend-core/src/api/auditLogs.ts
@@ -14,48 +14,13 @@ export interface AuditLogEndpoints {
   getDownloadUrl: (opts: DownloadAuditLogsRequest) => string
 }
 
-const buildOpts = (opts: SearchAuditLogsRequest) => {
-  const { bookmark, startDate, endDate, fullSearch, events, userIds, appIds } =
-    opts
-  const parsedOpts: SearchAuditLogsRequest = {}
-
-  if (bookmark) {
-    parsedOpts.bookmark = bookmark
-  }
-
-  if (opts.startDate && endDate) {
-    parsedOpts.startDate = startDate
-    parsedOpts.endDate = endDate
-  } else if (startDate && !endDate) {
-    parsedOpts.startDate = startDate
-  }
-
-  if (fullSearch) {
-    parsedOpts.fullSearch = fullSearch
-  }
-
-  if (events?.length) {
-    parsedOpts.events = events
-  }
-
-  if (userIds?.length) {
-    parsedOpts.userIds = userIds
-  }
-
-  if (appIds?.length) {
-    parsedOpts.appIds = appIds
-  }
-
-  return parsedOpts
-}
-
 export const buildAuditLogEndpoints = (
   API: BaseAPIClient
 ): AuditLogEndpoints => ({
   searchAuditLogs: async opts => {
     return await API.post({
       url: `/api/global/auditlogs/search`,
-      body: buildOpts(opts),
+      body: opts,
     })
   },
   getEventDefinitions: async () => {
