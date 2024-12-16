a33f709f7d7c6c865088365d3278b85cb84db05a
Adria Navarro <adria@budibase.com>
2024-12-16 15:51:17 +0100

    Fix encoding

--- packages/server/src/api/controllers/row/utils/utils.ts
+++ packages/server/src/api/controllers/row/utils/utils.ts
@@ -66,7 +66,7 @@ export function getSourceId(ctx: Ctx): { tableId: string; viewId?: string } {
     if (docIds.isViewId(sourceId)) {
       return {
         tableId: utils.extractViewInfoFromID(sourceId).tableId,
-        viewId: encodeURI(sourceId),
+        viewId: sql.utils.encodeTableId(sourceId),
       }
     }
     return { tableId: sql.utils.encodeTableId(ctx.params.sourceId) }
