6d9f09ffca8d61c4681fd092b2f3299f846a4ab9
mike12345567 <me@michaeldrury.co.uk>
2024-12-17 16:31:40 +0000

    Updating datasource and sorted integration store to be classes, that extend the BudiStore base implementation.

--- packages/builder/src/stores/builder/sortedIntegrations.ts
+++ packages/builder/src/stores/builder/sortedIntegrations.ts
@@ -3,6 +3,7 @@ import { derived } from "svelte/store"
 
 import { DatasourceTypes } from "constants/backend"
 import { UIIntegration, Integration } from "@budibase/types"
+import BudiStore from "stores/BudiStore"
 
 const getIntegrationOrder = (type: string | undefined) => {
   // if type is not known, sort to end
@@ -18,29 +19,35 @@ const getIntegrationOrder = (type: string | undefined) => {
   return type.charCodeAt(0) + 4
 }
 
-export const createSortedIntegrationsStore = () => {
-  return derived<typeof integrations, UIIntegration[]>(
-    integrations,
-    $integrations => {
-      const entries: [string, Integration][] = Object.entries($integrations)
-      const integrationsAsArray = entries.map(([name, integration]) => ({
-        name,
-        ...integration,
-      }))
+class SortedIntegrationStore extends BudiStore<UIIntegration[]> {
+  constructor() {
+    super([])
 
-      return integrationsAsArray.sort((integrationA, integrationB) => {
-        const integrationASortOrder = getIntegrationOrder(integrationA.type)
-        const integrationBSortOrder = getIntegrationOrder(integrationB.type)
-        if (integrationASortOrder === integrationBSortOrder) {
-          return integrationA.friendlyName.localeCompare(
-            integrationB.friendlyName
-          )
-        }
+    const derivedStore = derived<typeof integrations, UIIntegration[]>(
+      integrations,
+      $integrations => {
+        const entries: [string, Integration][] = Object.entries($integrations)
+        const integrationsAsArray = entries.map(([name, integration]) => ({
+          name,
+          ...integration,
+        }))
 
-        return integrationASortOrder < integrationBSortOrder ? -1 : 1
-      })
-    }
-  )
+        return integrationsAsArray.sort((integrationA, integrationB) => {
+          const integrationASortOrder = getIntegrationOrder(integrationA.type)
+          const integrationBSortOrder = getIntegrationOrder(integrationB.type)
+          if (integrationASortOrder === integrationBSortOrder) {
+            return integrationA.friendlyName.localeCompare(
+              integrationB.friendlyName
+            )
+          }
+
+          return integrationASortOrder < integrationBSortOrder ? -1 : 1
+        })
+      }
+    )
+
+    this.subscribe = derivedStore.subscribe
+  }
 }
 
-export const sortedIntegrations = createSortedIntegrationsStore()
+export const sortedIntegrations = new SortedIntegrationStore()
