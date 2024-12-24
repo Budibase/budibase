7727296aeeb14a1ee588651bbb7981013d7d116f
Adria Navarro <adria@budibase.com>
2024-12-24 12:18:49 +0100

    Fix UIFieldMutation

--- packages/types/src/ui/stores/grid/datasource.ts
+++ packages/types/src/ui/stores/grid/datasource.ts
@@ -3,6 +3,7 @@ import { UITable, UIView } from "@budibase/types"
 export type UIDatasource = UITable | UIView
 
 export interface UIFieldMutation {
-  visible: boolean
+  visible?: boolean
   readonly?: boolean
+  width?: number
 }
