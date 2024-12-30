910a56ad373c315a8d8c234fdef93cb078c82bc3
Adria Navarro <adria@budibase.com>
2024-12-30 14:25:23 +0100

    Typed enriched users

--- packages/types/src/ui/stores/grid/user.ts
+++ packages/types/src/ui/stores/grid/user.ts
@@ -4,3 +4,8 @@ export interface UIUser extends User {
   sessionId: string
   gridMetadata?: { focusedCellId?: string }
 }
+
+export interface UIEnrichedUser extends UIUser {
+  color: string
+  label: string
+}
