3b604e21781fd7f5909589acceba206a6440535a
Peter Clement <peter@budibase.com>
2024-12-30 09:27:22 +0000

    contextmenu and deployment

--- packages/builder/src/stores/builder/deployments.ts
+++ packages/builder/src/stores/builder/deployments.ts
@@ -1,11 +1,12 @@
-import { writable } from "svelte/store"
+import { writable, type Writable } from "svelte/store"
 import { API } from "api"
 import { notifications } from "@budibase/bbui"
+import { DeploymentProgressResponse } from "@budibase/types"
 
 export const createDeploymentStore = () => {
-  let store = writable([])
+  let store: Writable<DeploymentProgressResponse[]> = writable([])
 
-  const load = async () => {
+  const load = async (): Promise<void> => {
     try {
       store.set(await API.getAppDeployments())
     } catch (err) {
