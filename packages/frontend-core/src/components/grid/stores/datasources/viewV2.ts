aa07af596be8ef9bed524aab25fbccb9d40311f4
Adria Navarro <adria@budibase.com>
2024-12-31 12:24:26 +0100

    Fix types

--- packages/frontend-core/src/components/grid/stores/datasources/viewV2.ts
+++ packages/frontend-core/src/components/grid/stores/datasources/viewV2.ts
@@ -187,7 +187,7 @@ export const initialise = (context: StoreContext) => {
         }
         $fetch.update({
           sortOrder: $sort.order,
-          sortColumn: $sort.column,
+          sortColumn: $sort.column ?? undefined,
         })
       })
     )
