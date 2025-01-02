1899af919077c8ca87030c2c27517ddb4565f549
Adria Navarro <adria@budibase.com>
2025-01-02 13:33:24 +0100

    More types

--- packages/frontend-core/src/fetch/TableFetch.ts
+++ packages/frontend-core/src/fetch/TableFetch.ts
@@ -1,14 +1,33 @@
 import { get } from "svelte/store"
 import DataFetch from "./DataFetch.js"
-import { SortOrder, UITable } from "@budibase/types"
+import { SortOrder, Table, UITable } from "@budibase/types"
 
-export default class TableFetch extends DataFetch<UITable> {
+export default class TableFetch extends DataFetch<UITable, Table> {
   determineFeatureFlags() {
     return {
       supportsSearch: true,
       supportsSort: true,
       supportsPagination: true,
     }
+  }
+
+  async getDefinition(datasource: UITable | null) {
+    if (!datasource?.tableId) {
+      return null
+    }
+    try {
+      return await this.API.fetchTableDefinition(datasource.tableId)
+    } catch (error: any) {
+      this.store.update(state => ({
+        ...state,
+        error,
+      }))
+      return null
+    }
+  }
+
+  getSchema(_datasource: UITable | null, definition: Table | null) {
+    return definition?.schema
   }
 
   async getData() {
