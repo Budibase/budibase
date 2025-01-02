9523e4637af8476f66e3f5f441e48bc4f33b6717
Adria Navarro <adria@budibase.com>
2025-01-02 16:25:37 +0100

    Convert ViewFetch

---
+++ packages/frontend-core/src/fetch/ViewFetch.ts
@@ -0,0 +1,35 @@
+import { Table, View } from "@budibase/types"
+import DataFetch from "./DataFetch.js"
+
+type ViewV1 = View & { name: string }
+
+export default class ViewFetch extends DataFetch<ViewV1, Table> {
+  async getDefinition(datasource: ViewV1) {
+    if (!datasource?.tableId) {
+      return null
+    }
+    try {
+      return await this.API.fetchTableDefinition(datasource.tableId)
+    } catch (error: any) {
+      this.store.update(state => ({
+        ...state,
+        error,
+      }))
+      return null
+    }
+  }
+
+  getSchema(datasource: ViewV1, definition: Table) {
+    return definition?.views?.[datasource.name]?.schema
+  }
+
+  async getData() {
+    const { datasource } = this.options
+    try {
+      const res = await this.API.fetchViewData(datasource.name!)
+      return { rows: res || [] }
+    } catch (error) {
+      return { rows: [] }
+    }
+  }
+}
