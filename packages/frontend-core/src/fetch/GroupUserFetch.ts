5b82f0d8c933e1877f0d621116615d7f4a3c9942
Adria Navarro <adria@budibase.com>
2025-01-07 11:02:09 +0100

    Type groupUserFetch

--- packages/frontend-core/src/fetch/GroupUserFetch.ts
+++ packages/frontend-core/src/fetch/GroupUserFetch.ts
@@ -1,15 +1,33 @@
 import { get } from "svelte/store"
 import DataFetch from "./DataFetch"
 import { TableNames } from "../constants"
+import { APIClient } from "../api/types"
 
-export default class GroupUserFetch extends DataFetch {
-  constructor(opts) {
+interface GroupUserQuery {
+  groupId: string
+  emailSearch: string
+}
+
+export default class GroupUserFetch extends DataFetch<
+  any,
+  any,
+  GroupUserQuery
+> {
+  constructor(opts: {
+    API: APIClient
+    datasource: any
+    query: GroupUserQuery
+  }) {
     super({
       ...opts,
       datasource: {
         tableId: TableNames.USERS,
       },
     })
+  }
+
+  getSchema(_datasource: any, definition: any) {
+    return definition?.schema
   }
 
   determineFeatureFlags() {
@@ -28,11 +46,12 @@ export default class GroupUserFetch extends DataFetch {
 
   async getData() {
     const { query, cursor } = get(this.store)
+
     try {
       const res = await this.API.getGroupUsers({
         id: query.groupId,
         emailSearch: query.emailSearch,
-        bookmark: cursor,
+        bookmark: cursor ?? undefined,
       })
 
       return {
