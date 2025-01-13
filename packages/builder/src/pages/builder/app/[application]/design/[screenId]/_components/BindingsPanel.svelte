1e0938e93e2650e70764d309a1ba07251e415836
Andrew Kingston <andrew@kingston.dev>
2025-01-13 16:09:01 +0000

    Add dummy action buttons for new panels

---
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/_components/BindingsPanel.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { ActionButton, Modal, ModalContent } from "@budibase/bbui"
+
+  let visible = false
+  let modal
+</script>
+
+<ActionButton on:click={modal.show}>Bindings</ActionButton>
+
+<Modal bind:this={modal}>
+  <ModalContent title="Bindings" showConfirmButton={false} cancelText="Close">
+    Some awesome bindings content.
+  </ModalContent>
+</Modal>
