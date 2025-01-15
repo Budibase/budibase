a3d73ee6ce7cb159afb368c076f0016b8c1348da
Adria Navarro <adria@budibase.com>
2025-01-15 10:40:41 +0100

    Smarter datasource

--- packages/frontend-core/src/index.ts
+++ packages/frontend-core/src/index.ts
@@ -1,7 +1,11 @@
 export { createAPIClient } from "./api"
 export type { APIClient } from "./api"
 export { fetchData, DataFetchMap } from "./fetch"
-export type { DataFetchType, DataFetchOptions } from "./fetch"
+export type {
+  DataFetchType,
+  DataFetchOptions,
+  ProviderDatasource,
+} from "./fetch"
 export * as Constants from "./constants"
 export * from "./stores"
 export * from "./utils"
