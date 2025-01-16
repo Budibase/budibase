f74e2c6693a757c6eab28b2323a9d50eaa5018e6
Adria Navarro <adria@budibase.com>
2025-01-16 16:40:51 +0100

    Convert form block

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -7,9 +7,13 @@ export interface SDK {
   styleable: any
   Provider: any
   ActionTypes: typeof ActionTypes
+  fetchDatasourceSchema: any
+  generateGoldenSample: any
 }
 
 export type Component = Readable<{
   id: string
   styles: any
 }>
+
+export type Context = Readable<any>
