68374bce29126c0f38da9f030acfdc82c2ed8e60
mike12345567 <me@michaeldrury.co.uk>
2025-01-20 16:40:08 +0000

    Testing backend JS further.

--- packages/server/src/jsRunner/tests/jsRunner.spec.ts
+++ packages/server/src/jsRunner/tests/jsRunner.spec.ts
@@ -1,5 +1,9 @@
 import { validate as isValidUUID } from "uuid"
-import { processStringSync, encodeJSBinding } from "@budibase/string-templates"
+import {
+  processStringSync,
+  encodeJSBinding,
+  setTestingBackendJS,
+} from "@budibase/string-templates"
 
 import { runJsHelpersTests } from "@budibase/string-templates/test/utils"
 
@@ -7,6 +11,7 @@ import tk from "timekeeper"
 import { init } from ".."
 import TestConfiguration from "../../tests/utilities/TestConfiguration"
 
+setTestingBackendJS()
 const DATE = "2021-01-21T12:00:00"
 tk.freeze(DATE)
 
