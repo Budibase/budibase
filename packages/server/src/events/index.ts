34e6f5d2f59a60bdb1d4fd3afd69bfd678eec3db
Sam Rose <hello@samwho.dev>
2025-01-21 17:06:09 +0000

    Remove testStep endpoint and fix types.

--- packages/server/src/events/index.ts
+++ packages/server/src/events/index.ts
@@ -2,6 +2,5 @@ import BudibaseEmitter from "./BudibaseEmitter"
 
 const emitter = new BudibaseEmitter()
 
-export { NoopEmitter } from "./NoopEmitter"
 export { init } from "./docUpdates"
 export default emitter
