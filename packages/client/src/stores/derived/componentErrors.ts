e12bdc384e5195fec3978b0f622c09f1f417c677
Adria Navarro <adria@budibase.com>
2025-01-22 12:18:55 +0100

    Remove unnecessary derived

--- packages/client/src/stores/derived/componentErrors.ts
+++
@@ -1,6 +0,0 @@
-import { derived } from "svelte/store"
-import { builderStore } from "../builder.js"
-
-export const componentErrors = derived([builderStore], ([$builderStore]) => {
-  return $builderStore.componentErrors
-})
