c9feae9665a33466e9486c7ca97a2f42b067142c
Adria Navarro <adria@budibase.com>
2025-01-22 16:26:09 +0100

    Simplify derived screenComponentErrors

--- packages/builder/src/stores/builder/screenComponent.ts
+++ packages/builder/src/stores/builder/screenComponent.ts
@@ -2,79 +2,57 @@ import { derived } from "svelte/store"
 import { tables } from "./tables"
 import { selectedScreen } from "./screens"
 import { viewsV2 } from "./viewsV2"
-import { DerivedBudiStore } from "../BudiStore"
 import { findComponentsBySettingsType } from "@/helpers/screen"
 import { Screen, Table, ViewV2 } from "@budibase/types"
 
-interface BuilderScreenComponentStore {}
+export const screenComponentErrors = derived(
+  [selectedScreen, tables, viewsV2],
+  ([$selectedScreen, $tables, $viewsV2]): Record<string, string[]> => {
+    function flattenTablesAndViews(tables: Table[], views: ViewV2[]) {
+      return {
+        ...tables.reduce(
+          (list, table) => ({
+            ...list,
+            [table._id!]: table,
+          }),
+          {}
+        ),
+        ...views.reduce(
+          (list, view) => ({
+            ...list,
+            [view.id]: view,
+          }),
+          {}
+        ),
+      }
+    }
 
-interface DerivedScreenComponentStore extends BuilderScreenComponentStore {
-  errors: Record<string, string[]>
-}
+    function getInvalidDatasources(
+      screen: Screen,
+      datasources: Record<string, any>
+    ) {
+      const friendlyNameByType = {
+        table: "table",
+        view: "view",
+        viewV2: "view",
+      }
 
-export class ScreenComponentStore extends DerivedBudiStore<
-  BuilderScreenComponentStore,
-  DerivedScreenComponentStore
-> {
-  constructor() {
-    const makeDerivedStore = () => {
-      return derived(
-        [selectedScreen, tables, viewsV2],
-        ([$selectedScreen, $tables, $viewsV2]): DerivedScreenComponentStore => {
-          const datasources = flattenTablesAndViews($tables.list, $viewsV2.list)
-          return {
-            errors: getInvalidDatasources($selectedScreen, datasources),
-          }
+      const result: Record<string, string[]> = {}
+      for (const component of findComponentsBySettingsType(screen, "table")) {
+        const { resourceId, type, label } = component.dataSource
+        if (!datasources[resourceId]) {
+          const friendlyTypeName =
+            friendlyNameByType[type as keyof typeof friendlyNameByType]
+          result[component._id!] = [
+            `The ${friendlyTypeName} named "${label}" does not exist`,
+          ]
         }
-      )
+      }
+
+      return result
     }
 
-    super({}, makeDerivedStore)
+    const datasources = flattenTablesAndViews($tables.list, $viewsV2.list)
+    return getInvalidDatasources($selectedScreen, datasources)
   }
-}
-
-export const screenComponentStore = new ScreenComponentStore()
-
-function flattenTablesAndViews(tables: Table[], views: ViewV2[]) {
-  return {
-    ...tables.reduce(
-      (list, table) => ({
-        ...list,
-        [table._id!]: table,
-      }),
-      {}
-    ),
-    ...views.reduce(
-      (list, view) => ({
-        ...list,
-        [view.id]: view,
-      }),
-      {}
-    ),
-  }
-}
-
-function getInvalidDatasources(
-  screen: Screen,
-  datasources: Record<string, any>
-) {
-  const friendlyNameByType = {
-    table: "table",
-    view: "view",
-    viewV2: "view",
-  }
-
-  const result: Record<string, string[]> = {}
-  for (const component of findComponentsBySettingsType(screen, "table")) {
-    const { resourceId, type, label } = component.dataSource
-    if (!datasources[resourceId]) {
-      const friendlyTypeName =
-        friendlyNameByType[type as keyof typeof friendlyNameByType]
-      result[component._id!] = [
-        `The ${friendlyTypeName} named "${label}" does not exist`,
-      ]
-    }
-  }
-
-  return result
-}
+)
