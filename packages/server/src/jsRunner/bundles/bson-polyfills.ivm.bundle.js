9ff29c794e906202550dbce0e4621c02810bfa2c
Adria Navarro <adria@budibase.com>
2025-01-29 21:01:19 +0100

    Add TextDecoder polyfill

--- packages/server/src/jsRunner/bundles/bson-polyfills.ivm.bundle.js
+++ packages/server/src/jsRunner/bundles/bson-polyfills.ivm.bundle.js
@@ -1,6 +1,21 @@
-function atob(...args){
-    return atobCB(...args)
+function atob(...args) {
+  return atobCB(...args)
 }
-function btoa(...args){
-    return btoaCB(...args)
+function btoa(...args) {
+  return btoaCB(...args)
+}
+
+class TextDecoder {
+  constructorArgs
+
+  constructor(...constructorArgs) {
+    this.constructorArgs = constructorArgs
+  }
+
+  decode(...input) {
+    return textDecoderCb({
+      constructorArgs: this.constructorArgs,
+      functionArgs: input,
+    })
+  }
 }
