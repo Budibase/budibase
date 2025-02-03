6d984243230e02a10bd15fbeb710fd8847345b63
Adria Navarro <adria@budibase.com>
2025-02-03 10:47:50 +0100

    Show popover

---
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/_components/ScreenErrorsButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { screenComponentErrorList } from "@/stores/builder"
+  import { ActionButton, Popover } from "@budibase/bbui"
+
+  let button: any
+  let popover: any
+</script>
+
+<div bind:this={button}>
+  <ActionButton selected quiet on:click={() => popover.show()}
+    >Errors ({$screenComponentErrorList.length})</ActionButton
+  >
+</div>
+<Popover bind:this={popover} anchor={button} align={"right"}>TODO</Popover>
