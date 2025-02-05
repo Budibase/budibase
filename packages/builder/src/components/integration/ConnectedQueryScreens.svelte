bdb9cbb6ad5df82e785cdd39a184acb0a1ea2510
Peter Clement <peter@budibase.com>
2025-02-05 08:55:44 +0000

    properly reuse files

--- packages/builder/src/components/integration/ConnectedQueryScreens.svelte
+++ packages/builder/src/components/integration/ConnectedQueryScreens.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte"
-  import { List, ListItem, ActionButton } from "@budibase/bbui"
-  import DetailPopover from "@/components/common/DetailPopover.svelte"
-  import { appStore, screenStore } from "@/stores/builder"
+
+  import { screenStore } from "@/stores/builder"
+  import ScreensPopover from "@/components/common/ScreensPopover.svelte"
   import type { ScreenUsage } from "@budibase/types"
-  import { PopoverAlign } from "@budibase/types"
 
   export let sourceId: string
 
@@ -25,42 +24,10 @@
   })
 </script>
 
-<DetailPopover title="Screens" bind:this={popover} align={PopoverAlign.Left}>
-  <svelte:fragment slot="anchor" let:open>
-    <ActionButton icon="DeviceDesktop" quiet selected={open} on:click={show}>
-      Screens
-    </ActionButton>
-  </svelte:fragment>
-
-  {#if !screens.length}
-    <div class="empty-state">
-      <p>No screens are using this data.</p>
-    </div>
-  {:else}
-    <p class="description">The following screens are connected to this data.</p>
-
-    <List>
-      {#each screens as screen}
-        <ListItem
-          title={screen.url}
-          url={`/builder/app/${$appStore.appId}/design/${screen._id}`}
-          showArrow
-        />
-      {/each}
-    </List>
-  {/if}
-</DetailPopover>
-
-<style>
-  .description {
-    font-size: var(--font-size-s);
-    margin: 0 0 var(--spacing-xs) 0;
-  }
-
-  .empty-state {
-    padding: var(--spacing-m);
-    text-align: center;
-    color: var(--grey-5);
-    font-size: var(--font-size-s);
-  }
-</style>
+<ScreensPopover
+  bind:this={popover}
+  {screens}
+  icon="WebPage"
+  accentColor="#364800"
+  showCount
+/>
