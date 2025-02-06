8ff2faaa24405de727d536876108e1c7cefb608a
Andrew Kingston <andrew@kingston.dev>
2025-02-06 14:42:27 +0000

    Fix a few edge case bugs and prevent out of position flashing

--- packages/client/src/components/preview/DNDSelectionIndicators.svelte
+++ packages/client/src/components/preview/DNDSelectionIndicators.svelte
@@ -2,6 +2,11 @@
   import { isGridScreen, dndParent, dndSource, dndIsDragging } from "@/stores"
   import { DNDPlaceholderID } from "@/constants"
   import IndicatorSet from "./IndicatorSet.svelte"
+
+  // On grid screens, don't draw the indicator until we've dragged over the
+  // screen. When this happens, the dndSource props will be set as we will have
+  // attached grid metadata styles.
+  $: waitingForGrid = $isGridScreen && !$dndSource?.props
 </script>
 
 {#if $dndIsDragging}
@@ -14,14 +19,16 @@
     />
   {/if}
 
-  <IndicatorSet
-    componentId={DNDPlaceholderID}
-    color="var(--spectrum-global-color-static-green-500)"
-    zIndex={930}
-    allowResizeAnchors={false}
-    background="hsl(160, 64%, 90%)"
-    animate={!$isGridScreen}
-    text={$dndSource?.name}
-    icon={$dndSource?.icon}
-  />
+  {#if !waitingForGrid}
+    <IndicatorSet
+      componentId={DNDPlaceholderID}
+      color="var(--spectrum-global-color-static-green-500)"
+      zIndex={930}
+      allowResizeAnchors={false}
+      background="hsl(160, 64%, 90%)"
+      animate={!$isGridScreen}
+      text={$dndSource?.name}
+      icon={$dndSource?.icon}
+    />
+  {/if}
 {/if}
