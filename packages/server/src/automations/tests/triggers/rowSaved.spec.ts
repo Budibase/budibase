ba51320e64f324f56d8d9f8ce94dc30566afb9a0
Sam Rose <hello@samwho.dev>
2025-02-10 16:15:53 +0000

    Rename automation result helper.

--- packages/server/src/automations/tests/triggers/rowSaved.spec.ts
+++ packages/server/src/automations/tests/triggers/rowSaved.spec.ts
@@ -1,20 +1,22 @@
 import { createAutomationBuilder } from "../utilities/AutomationTestBuilder"
 import TestConfiguration from "../../../tests/utilities/TestConfiguration"
-import { Table } from "@budibase/types"
+import { Automation, Table } from "@budibase/types"
 import { basicTable } from "../../../tests/utilities/structures"
-import { captureAutomationRuns } from "../utilities"
+import { captureAutomationResults } from "../utilities"
 
 describe("row saved trigger", () => {
   const config = new TestConfiguration()
   let table: Table
+  let automation: Automation
 
   beforeAll(async () => {
     await config.init()
     table = await config.api.table.save(basicTable())
-    await createAutomationBuilder(config)
+    automation = await createAutomationBuilder(config)
       .onRowSaved({ tableId: table._id! })
       .serverLog({ text: "Row created!" })
       .save()
+      .then(({ automation }) => automation)
 
     await config.api.application.publish()
   })
@@ -24,12 +26,12 @@ describe("row saved trigger", () => {
   })
 
   it("should queue a Bull job when a row is created", async () => {
-    const jobs = await captureAutomationRuns(() =>
+    const results = await captureAutomationResults(automation, () =>
       config.withProdApp(() => config.api.row.save(table._id!, { name: "foo" }))
     )
 
-    expect(jobs).toHaveLength(1)
-    expect(jobs[0].data.event).toEqual(
+    expect(results).toHaveLength(1)
+    expect(results[0].data.event).toEqual(
       expect.objectContaining({
         tableId: table._id!,
         row: expect.objectContaining({ name: "foo" }),
@@ -41,12 +43,12 @@ describe("row saved trigger", () => {
     const otherTable = await config.api.table.save(basicTable())
     await config.api.application.publish()
 
-    const jobs = await captureAutomationRuns(() =>
+    const results = await captureAutomationResults(automation, () =>
       config.withProdApp(() =>
         config.api.row.save(otherTable._id!, { name: "foo" })
       )
     )
 
-    expect(jobs).toBeEmpty()
+    expect(results).toBeEmpty()
   })
 })
