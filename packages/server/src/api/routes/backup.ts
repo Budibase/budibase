b85f198fc2acb9341f17a49f1e05669edc99b80f
Martin McKeaveney <martinmckeaveney@gmail.com>
2025-02-15 19:06:21 +0000

    useAppIdFromCtx helper

--- packages/server/src/api/routes/backup.ts
+++ packages/server/src/api/routes/backup.ts
@@ -2,7 +2,6 @@ import Router from "@koa/router"
 import * as controller from "../controllers/backup"
 import authorized from "../../middleware/authorized"
 import { permissions } from "@budibase/backend-core"
-import { UserCtx } from "@budibase/types"
 import ensureTenantAppOwnership from "../../middleware/ensureTenantAppOwnership"
 
 const router: Router = new Router()
@@ -10,7 +9,7 @@ const router: Router = new Router()
 router.post(
   "/api/backups/export",
   authorized(permissions.BUILDER),
-  ensureTenantAppOwnership((ctx: UserCtx) => ctx.query.appId as string),
+  ensureTenantAppOwnership,
   controller.exportAppDump
 )
 
--- packages/server/src/middleware/ensureTenantAppOwnership.ts
+++ packages/server/src/middleware/ensureTenantAppOwnership.ts
@@ -1,16 +1,17 @@
-import { tenancy } from "@budibase/backend-core"
+import { tenancy, utils } from "@budibase/backend-core"
 import { UserCtx } from "@budibase/types"
 
-function ensureTenantAppOwnership(appIdGetter: (ctx: UserCtx) => string) {
-  return async (ctx: UserCtx, next: any) => {
-    const appId = appIdGetter(ctx)
-    const exportAppId = tenancy.getTenantIDFromAppID(appId)
-    const tenantId = tenancy.getTenantId()
-    if (exportAppId !== tenantId) {
-      ctx.throw(403, `Cannot export app from another tenant`)
-    }
-    await next()
+async function ensureTenantAppOwnership(ctx: UserCtx, next: any) {
+  const appId = await utils.getAppIdFromCtx(ctx)
+  if (!appId) {
+    ctx.throw(400, "appId must be provided")
   }
+  const exportAppId = tenancy.getTenantIDFromAppID(appId)
+  const tenantId = tenancy.getTenantId()
+  if (exportAppId !== tenantId) {
+    ctx.throw(403, `Cannot export app from another tenant`)
+  }
+  await next()
 }
 
 export default ensureTenantAppOwnership
