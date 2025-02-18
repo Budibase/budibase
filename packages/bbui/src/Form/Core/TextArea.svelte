0132a202fd5b59e07de303e50d2a46b7dc9bf9bf
Andrew Kingston <andrew@kingston.dev>
2025-02-18 11:58:51 +0000

    Add new scrollable text setting and update components to support multiline. Update binding icon styles

--- packages/bbui/src/Form/Core/TextArea.svelte
+++ packages/bbui/src/Form/Core/TextArea.svelte
@@ -1,40 +1,64 @@
-<script>
+<script lang="ts">
   import "@spectrum-css/textfield/dist/index-vars.css"
   import { createEventDispatcher } from "svelte"
 
-  export let value = ""
-  export let placeholder = null
-  export let disabled = false
-  export let readonly = false
-  export let id = null
-  export let height = null
-  export let minHeight = null
+  export let value: string | undefined = ""
+  export let placeholder: string | undefined = undefined
+  export let disabled: boolean = false
+  export let readonly: boolean = false
+  export let id: string | undefined = undefined
+  export let height: string | number | undefined = undefined
+  export let minHeight: string | number | undefined = undefined
+  export let align = null
+  export let updateOnChange: boolean = false
+
   export const getCaretPosition = () => ({
     start: textarea.selectionStart,
     end: textarea.selectionEnd,
   })
-  export let align = null
+
+  const dispatch = createEventDispatcher()
 
   let focus = false
-  let textarea
-  const dispatch = createEventDispatcher()
-  const onChange = event => {
-    dispatch("change", event.target.value)
+  let textarea: HTMLTextAreaElement
+  let scrollable = false
+
+  $: heightString = getStyleString("height", height)
+  $: minHeightString = getStyleString("min-height", minHeight)
+  $: dispatch("scrollable", scrollable)
+
+  const onBlur = () => {
     focus = false
+    updateValue()
   }
 
-  const getStyleString = (attribute, value) => {
-    if (!attribute || value == null) {
+  const onChange = () => {
+    scrollable = textarea.clientHeight < textarea.scrollHeight
+    if (!updateOnChange) {
+      return
+    }
+    updateValue()
+  }
+
+  const updateValue = () => {
+    if (readonly || disabled) {
+      return
+    }
+    dispatch("change", textarea.value)
+  }
+
+  const getStyleString = (
+    attribute: string,
+    value: string | number | undefined
+  ) => {
+    if (value == null) {
       return ""
     }
-    if (isNaN(value)) {
+    if (typeof value !== "number" || isNaN(value)) {
       return `${attribute}:${value};`
     }
     return `${attribute}:${value}px;`
   }
-
-  $: heightString = getStyleString("height", height)
-  $: minHeightString = getStyleString("min-height", minHeight)
 </script>
 
 <div
@@ -52,8 +76,10 @@
     {disabled}
     {readonly}
     {id}
+    on:input={onChange}
     on:focus={() => (focus = true)}
-    on:blur={onChange}
+    on:blur={onBlur}
+    on:blur
   >{value || ""}</textarea>
 </div>
 
