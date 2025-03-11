bd5f22b6a513bec9eb72f12ee411a5ade9a42434
Sam Rose <hello@samwho.dev>
2025-03-11 14:38:58 +0000

    Add auth to AI endpoint.

--- packages/server/src/api/routes/ai.ts
+++ packages/server/src/api/routes/ai.ts
@@ -1,8 +1,9 @@
 import Router from "@koa/router"
 import * as controller from "../controllers/ai"
+import { auth } from "@budibase/backend-core"
 
 const router: Router = new Router()
 
-router.post("/api/ai/js", controller.generateJs)
+router.post("/api/ai/js", auth.builderOrAdmin, controller.generateJs)
 
 export default router
