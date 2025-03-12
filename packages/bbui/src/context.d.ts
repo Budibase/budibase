6823401c18e7affc7713b32d491ab971595bfc48
Peter Clement <peter@budibase.com>
2025-03-12 12:58:59 +0000

    fix types

--- packages/bbui/src/context.d.ts
+++ packages/bbui/src/context.d.ts
@@ -1,8 +1,23 @@
-import { Context } from "."
-
 declare module "svelte" {
   export function getContext(key: Context.Modal): {
     hide: () => void
     cancel: () => void
   }
+  export function getContext(key: Context.Popover): {
+    show: () => void
+    hide: () => void
+  }
+  export function getContext(key: "actionMenu"): {
+    hideAll: () => void
+  }
+}
+
+export default {
+  Modal: "bbui-modal",
+  PopoverRoot: "bbui-popover-root",
+} as const
+
+export namespace Context {
+  type Modal = "bbui-modal"
+  type PopoverRoot = "bbui-popover-root"
 }
