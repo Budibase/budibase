1c6c2eb47489f6f02f531b5be62ad2828f3e4408
Adria Navarro <adria@budibase.com>
2025-03-12 12:22:45 +0100

    KeyValueBuilder to ts

--- packages/builder/src/components/integration/KeyValueBuilder.svelte
+++ packages/builder/src/components/integration/KeyValueBuilder.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import {
     Icon,
     ActionButton,
@@ -18,27 +18,26 @@
   export let defaults
   export let object = defaults || {}
   export let activity = {}
-  export let readOnly
-  export let noAddButton
-  export let name
-  export let headings = false
+  export let readOnly: boolean
+  export let noAddButton: boolean
+  export let name: string
+  export let headings: boolean = false
   export let options
   export let toggle
-  export let keyPlaceholder = "Key"
-  export let valuePlaceholder = "Value"
+  export let keyPlaceholder: string = "Key"
+  export let valuePlaceholder: string = "Value"
   export let valueHeading
   export let keyHeading
   export let tooltip
   export let menuItems
   export let showMenu = false
-  export let bindings = []
-  export let bindingDrawerLeft
-  export let allowHelpers = true
+  export let bindings: any[] = []
+  export let allowHelpers: boolean = true
   export let customButtonText = null
-  export let keyBindings = false
-  export let allowJS = false
-  export let actionButtonDisabled = false
-  export let compare = (option, value) => option === value
+  export let keyBindings: boolean = false
+  export let allowJS: boolean = false
+  export let actionButtonDisabled: boolean = false
+  export let compare = (option: string, value: string) => option === value
   export let context = null
 
   let fields = Object.entries(object || {}).map(([name, value]) => ({
@@ -52,15 +51,17 @@
     {}
   )
 
-  function buildFieldActivity(obj) {
+  function buildFieldActivity(obj: any) {
     if (!obj || typeof obj !== "object") {
       return []
     }
     const array = Array(fields.length)
     for (let [key, value] of Object.entries(obj)) {
       const field = fields.find(el => el.name === key)
-      const idx = fields.indexOf(field)
-      array[idx] = idx !== -1 ? value : true
+      if (field) {
+        const idx = fields.indexOf(field)
+        array[idx] = idx !== -1 ? value : true
+      }
     }
     return array
   }
@@ -71,7 +72,7 @@
     changed()
   }
 
-  function deleteEntry(idx) {
+  function deleteEntry(idx: number) {
     fields.splice(idx, 1)
     fieldActivity.splice(idx, 1)
     changed()
@@ -90,7 +91,7 @@
     dispatch("change", fields)
   }
 
-  function isJsonArray(value) {
+  function isJsonArray(value: any) {
     if (!value || typeof value === "string") {
       return false
     }
@@ -132,7 +133,6 @@
           value={field.name}
           {allowJS}
           {allowHelpers}
-          drawerLeft={bindingDrawerLeft}
           {context}
         />
       {:else}
@@ -159,7 +159,6 @@
           value={field.value}
           {allowJS}
           {allowHelpers}
-          drawerLeft={bindingDrawerLeft}
           {context}
         />
       {:else}
@@ -196,9 +195,6 @@
     <ActionButton
       disabled={actionButtonDisabled}
       icon="Add"
-      secondary
-      thin
-      outline
       on:click={addEntry}
     >
       {#if customButtonText}
