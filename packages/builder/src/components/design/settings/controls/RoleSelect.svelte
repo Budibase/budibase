691954aa1c078e571f7aaf6730e5267f4196a75f
Adria Navarro <adria@budibase.com>
2025-03-12 12:58:09 +0100

    More types

--- packages/builder/src/components/design/settings/controls/RoleSelect.svelte
+++ packages/builder/src/components/design/settings/controls/RoleSelect.svelte
@@ -1,11 +1,11 @@
-<script>
+<script lang="ts">
   import { Select } from "@budibase/bbui"
   import { roles } from "@/stores/builder"
 
-  export let value
-  export let error
-  export let placeholder = null
-  export let autoWidth = false
+  export let value: string
+  export let error: string | undefined = undefined
+  export let placeholder: string | undefined = undefined
+  export let autoWidth: boolean = false
 </script>
 
 <Select
