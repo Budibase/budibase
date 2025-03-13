4deb5631db9079598e95f91587adadb4e002ad8d
Adria Navarro <adria@budibase.com>
2025-03-13 15:27:43 +0100

    Initial zod validation

--- packages/builder/src/pages/builder/app/[application]/settings/oauth2/AddButton.svelte
+++ packages/builder/src/pages/builder/app/[application]/settings/oauth2/AddButton.svelte
@@ -7,9 +7,12 @@
     Divider,
     Heading,
     Input,
+    keepOpen,
     Modal,
     ModalContent,
+    notifications,
   } from "@budibase/bbui"
+  import { z, ZodType } from "zod"
 
   let modal: Modal
 
@@ -19,8 +22,32 @@
 
   let config: Partial<CreateOAuth2Config> = {}
 
+  let errors: Record<string, string> = {}
+
   $: saveOAuth2Config = async () => {
-    await oauth2.create(config)
+    const validator = z.object({
+      name: z.string().min(1),
+      url: z.string().min(1),
+      clientId: z.string().min(1),
+      clientSecret: z.string().min(1),
+    }) satisfies ZodType<CreateOAuth2Config>
+
+    const validationResult = validator.safeParse(config)
+    errors = {}
+    if (!validationResult.success) {
+      errors = Object.entries(
+        validationResult.error.formErrors.fieldErrors
+      ).reduce<Record<string, string>>((acc, [field, errors]) => {
+        if (errors[0]) {
+          acc[field] = errors[0]
+        }
+        return acc
+      }, {})
+      notifications.error("OAuth2 configuration is not valid")
+      return keepOpen
+    }
+
+    await oauth2.create(validationResult.data)
   }
 </script>
 
@@ -28,26 +55,35 @@
 <Modal bind:this={modal}>
   <ModalContent onConfirm={saveOAuth2Config}>
     <Heading size="S">Create new OAuth2 connection</Heading>
+
     <Body size="S">
       The OAuth 2 authentication below uses the Client Credentials (machine to
       machine) grant type.
     </Body>
     <Divider noGrid noMargin />
-    <Input label="Name*" placeholder="Type here..." bind:value={config.name} />
+    <Input
+      label="Name*"
+      placeholder="Type here..."
+      bind:value={config.name}
+      error={errors.name}
+    />
     <Input
       label="Service URL*"
       placeholder="E.g. www.google.com"
       bind:value={config.url}
+      error={errors.url}
     />
     <Input
       label="Client ID*"
       placeholder="Type here..."
       bind:value={config.clientId}
+      error={errors.clientId}
     />
     <Input
       label="Client secret*"
       placeholder="Type here..."
       bind:value={config.clientSecret}
+      error={errors.clientSecret}
     />
   </ModalContent>
 </Modal>
