a28310d303122f00f5ad82c068f633a5e8f9fed8
Adria Navarro <adria@budibase.com>
2025-03-13 13:56:27 +0100

    Add context menu

---
+++ packages/builder/src/pages/builder/app/[application]/settings/oauth2/MoreMenuRenderer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { ActionMenu, Icon, MenuItem } from "@budibase/bbui"
+</script>
+
+<ActionMenu align="right">
+  <div slot="control" class="control icon">
+    <Icon size="S" hoverable name="MoreSmallList" />
+  </div>
+  <MenuItem on:click={() => {}} icon="Edit">Edit</MenuItem>
+  <MenuItem on:click={() => {}} icon="Delete">Delete</MenuItem>
+</ActionMenu>
