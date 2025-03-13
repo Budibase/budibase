52312a7f6716807d63d6899aee6fbb126299e6d7
Adria Navarro <adria@budibase.com>
2025-03-13 10:56:42 +0100

    Improve error messaging

--- packages/server/src/sdk/app/oauth2/tests/utils.spec.ts
+++ packages/server/src/sdk/app/oauth2/tests/utils.spec.ts
@@ -53,21 +53,19 @@ describe("oauth2 utils", () => {
       expect(response).toEqual(expect.stringMatching(/^Bearer .+/))
     })
 
-    it("handles wrong secrets", async () => {
+    it("handles wrong urls", async () => {
       await expect(
         config.doInContext(config.appId, async () => {
           const oauthConfig = await sdk.oauth2.create({
             name: generator.guid(),
-            url: `${keycloakUrl}/realms/myrealm/protocol/openid-connect/token`,
+            url: `${keycloakUrl}/realms/wrong/protocol/openid-connect/token`,
             clientId: "my-client",
-            clientSecret: "wrong-secret",
+            clientSecret: "my-secret",
           })
 
           await generateToken(oauthConfig.id)
         })
-      ).rejects.toThrow(
-        "Error fetching oauth2 token: Invalid client or Invalid client credentials"
-      )
+      ).rejects.toThrow("Error fetching oauth2 token: Not Found")
     })
 
     it("handles wrong client ids", async () => {
@@ -78,6 +76,23 @@ describe("oauth2 utils", () => {
             url: `${keycloakUrl}/realms/myrealm/protocol/openid-connect/token`,
             clientId: "wrong-client-id",
             clientSecret: "my-secret",
+          })
+
+          await generateToken(oauthConfig.id)
+        })
+      ).rejects.toThrow(
+        "Error fetching oauth2 token: Invalid client or Invalid client credentials"
+      )
+    })
+
+    it("handles wrong secrets", async () => {
+      await expect(
+        config.doInContext(config.appId, async () => {
+          const oauthConfig = await sdk.oauth2.create({
+            name: generator.guid(),
+            url: `${keycloakUrl}/realms/myrealm/protocol/openid-connect/token`,
+            clientId: "my-client",
+            clientSecret: "wrong-secret",
           })
 
           await generateToken(oauthConfig.id)
