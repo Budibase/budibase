a252503e63aad13961c702f1c4b9be7657e02c03
Adria Navarro <adria@budibase.com>
2025-03-13 11:31:08 +0100

    Create add modal

--- packages/builder/src/pages/builder/app/[application]/settings/oauth2/index.svelte
+++ packages/builder/src/pages/builder/app/[application]/settings/oauth2/index.svelte
@@ -1,12 +1,14 @@
-<script>
-  import { Layout, Heading, Body, Divider, Button } from "@budibase/bbui"
+<script lang="ts">
+  import { Layout, Heading, Body, Divider, Button, Table } from "@budibase/bbui"
+  import { oauth2 } from "@/stores/builder"
+  import AddButton from "./AddButton.svelte"
 </script>
 
 <Layout noPadding>
   <Layout gap="XS" noPadding>
     <div class="header">
       <Heading>OAuth2</Heading>
-      <Button cta size="M">Add OAuth2</Button>
+      <AddButton />
     </div>
     <Body
       >Manage and configure OAuth 2.0 Client Credentials for secure API access.</Body
@@ -14,6 +16,8 @@
   </Layout>
   <Divider />
 </Layout>
+
+<Table data={$oauth2} />
 
 <style>
   .header {
