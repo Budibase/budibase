785e2aa58fb3077393d7a72de39e3dae2891c048
Adria Navarro <adria@budibase.com>
2025-03-17 12:29:36 +0100

    Add joi validator

--- packages/server/src/api/routes/oauth2.ts
+++ packages/server/src/api/routes/oauth2.ts
@@ -1,8 +1,22 @@
 import Router from "@koa/router"
 import { PermissionType } from "@budibase/types"
+import { middleware } from "@budibase/backend-core"
 import authorized from "../../middleware/authorized"
 
 import * as controller from "../controllers/oauth2"
+import Joi from "joi"
+
+function createOAauth2ConfigValidator() {
+  return middleware.joiValidator.body(
+    Joi.object({
+      name: Joi.string().required(),
+      url: Joi.string().required(),
+      clientId: Joi.string().required(),
+      clientSecret: Joi.string().required(),
+    }),
+    { allowUnknown: true }
+  )
+}
 
 const router: Router = new Router()
 
@@ -10,6 +24,7 @@ router.get("/api/oauth2", authorized(PermissionType.BUILDER), controller.fetch)
 router.post(
   "/api/oauth2",
   authorized(PermissionType.BUILDER),
+  createOAauth2ConfigValidator(),
   controller.create
 )
 router.put(
