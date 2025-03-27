c1ab5b8373f5091a5275d1e15c86fb469b00d81c
Peter Clement <peter@budibase.com>
2025-03-27 11:46:31 +0000

    fix apple deprecated warning:

--- packages/server/src/api/controllers/static/index.ts
+++ packages/server/src/api/controllers/static/index.ts
@@ -307,8 +307,10 @@ export const serveApp = async function (ctx: UserCtx<void, ServeAppResponse>) {
       let extraHead = ""
       if (hasPWA) {
         extraHead = `<link rel="manifest" href="${manifestUrl}">`
-        extraHead += `<meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        extraHead += `<meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content=${
+          appInfo.pwa.theme_color
+        }>
         <meta name="apple-mobile-web-app-title" content="${
           appInfo.pwa.short_name || appInfo.name
         }">`
