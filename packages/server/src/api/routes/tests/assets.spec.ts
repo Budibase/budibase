9555dec52398f7ce3e338eca2f3b670b84733b42
mike12345567 <me@michaeldrury.co.uk>
2025-03-28 16:49:05 +0000

    Test fix.

--- packages/server/src/api/routes/tests/assets.spec.ts
+++ packages/server/src/api/routes/tests/assets.spec.ts
@@ -3,21 +3,26 @@ import { join } from "path"
 import { DEV_ASSET_PATH } from "../../../utilities/fileSystem"
 import * as setup from "./utilities"
 
-const path = join(DEV_ASSET_PATH, "builder", "index.html")
-let addedIndexHtml = false
+const path = join(DEV_ASSET_PATH, "builder")
+let addedPath = false
 const config = setup.getConfig()
 
 beforeAll(async () => {
   if (!fs.existsSync(path)) {
-    fs.writeFileSync(path, "<html></html>", "utf8")
-    addedIndexHtml = true
+    addedPath = true
+    fs.mkdirSync(path)
+  }
+  const indexPath = join(path, "index.html")
+  if (!fs.existsSync(indexPath)) {
+    fs.writeFileSync(indexPath, "<html></html>", "utf8")
+    addedPath = true
   }
   await config.init()
 })
 
 afterAll(() => {
-  if (addedIndexHtml) {
-    fs.rmSync(path)
+  if (addedPath) {
+    fs.rmSync(path, { recursive: true })
   }
 })
 
