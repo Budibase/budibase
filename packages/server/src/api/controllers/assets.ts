e8ae9960b2f4742480f6f3fb17a22d96982e9114
mike12345567 <me@michaeldrury.co.uk>
2025-03-28 14:35:56 +0000

    Build fix.

--- packages/server/src/api/controllers/assets.ts
+++ packages/server/src/api/controllers/assets.ts
@@ -1,3 +1,8 @@
+import { join } from "../../utilities/centralPath"
+import { TOP_LEVEL_PATH } from "../../utilities/fileSystem"
+import { Ctx } from "@budibase/types"
+import send from "koa-send"
+
 // this is a public endpoint with no middlewares
 export const serveBuilderAssets = async function (ctx: Ctx<void, void>) {
   const builderPath = join(TOP_LEVEL_PATH, "builder")
