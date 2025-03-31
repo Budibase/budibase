462a0fd9bf525274b4e4942a3b428fa947a668cd
Adria Navarro <adria@budibase.com>
2025-03-31 12:22:11 +0200

    Handle relationships

--- packages/server/src/api/controllers/ai.ts
+++ packages/server/src/api/controllers/ai.ts
@@ -1,8 +1,11 @@
 import {
   DocumentType,
+  FieldType,
   GenerateTablesRequest,
   GenerateTablesResponse,
+  RelationshipFieldMetadata,
   SourceName,
+  Table,
   UserCtx,
 } from "@budibase/types"
 import { getLLM } from "packages/pro/src/ai"
@@ -56,26 +59,83 @@ export async function generateTables(
     )
   }
 
-  const json = JSON.parse(response!.message || "")
+  const json = JSON.parse(response!.message || "") as {
+    structure: Table[]
+    data: Record<string, any[]>
+  }
   const createdTables: GenerateTablesResponse["createdTables"] = []
+
   for (const table of json.structure) {
     const createTableCtx = {
-      request: { body: { ...table, sourceId: dsId } },
+      request: {
+        body: {
+          ...table,
+          sourceId: dsId,
+          schema: {},
+          primaryDisplay: undefined,
+        },
+      },
       user: ctx.user,
+      throw: ctx.throw,
     } as any
     await tableController.create(createTableCtx, async () => {})
     const createdTable = createTableCtx.body
     createdTables.push({ id: createdTable._id, name: createdTable.name })
+    table._id = createdTable._id
+  }
 
-    const dataToAdd = json.data?.[table.name]
-    for (const entry of dataToAdd || []) {
-      await sdk.rows.save(
-        createdTable._id,
-        {
-          ...entry,
+  for (const table of json.structure) {
+    for (const fieldKey of Object.keys(table.schema).filter(
+      f => table.schema[f].type === FieldType.LINK
+    )) {
+      const field = table.schema[fieldKey] as RelationshipFieldMetadata
+      const linkedTable = createdTables.find(t => t.name === field.tableId)
+      if (!linkedTable) {
+        throw `Table ${field.tableId} not found in the json response.`
+      }
+      field.tableId = linkedTable.id
+    }
+  }
+
+  for (const table of json.structure) {
+    const readTableCtx = {
+      params: { tableId: table._id },
+      user: ctx.user,
+      throw: ctx.throw,
+    } as any
+
+    await tableController.read(readTableCtx, async () => {})
+
+    const updateTableCtx = {
+      request: {
+        body: {
+          ...readTableCtx.body,
+          schema: {
+            ...readTableCtx.body.schema,
+            ...table.schema,
+          },
+          displayName: table.primaryDisplay,
         },
-        ctx.user._id
-      )
+      },
+      params: { tableId: table._id },
+      user: ctx.user,
+      throw: ctx.throw,
+    } as any
+    await tableController.update(updateTableCtx, async () => {})
+  }
+
+  if (addData) {
+    for (const table of json.structure) {
+      const dataToAdd = json.data?.[table.name]
+      for (const entry of dataToAdd || []) {
+        await sdk.rows.save(
+          table._id!,
+          {
+            ...entry,
+          },
+          ctx.user._id
+        )
+      }
     }
   }
 
