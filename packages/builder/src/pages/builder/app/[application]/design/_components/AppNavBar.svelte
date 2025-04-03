4bf5ed9ee060aabb15a521659dad508d85d4460f
Adria Navarro <adria@budibase.com>
2025-04-03 11:11:50 +0200

    Add navigator

---
+++ packages/builder/src/pages/builder/app/[application]/design/_components/AppNavBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { ActionButton, Tab, Tabs } from "@budibase/bbui"
+
+  $: apps = [{ name: "Default app" }]
+</script>
+
+<Tabs selected="TODO">
+  {#each apps as view}
+    <Tab title={view.name} />
+  {/each}
+  <ActionButton icon="Add">Add new app</ActionButton>
+</Tabs>
--- packages/builder/src/pages/builder/app/[application]/design/_layout.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/_layout.svelte
@@ -1,2 +1,7 @@
+<script>
+  import AppNavBar from "./_components/AppNavBar.svelte"
+</script>
+
 <!-- routify:options index=2 -->
+<AppNavBar />
 <slot />
