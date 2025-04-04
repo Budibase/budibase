1f6495c186cb8b0658a74a13f3d1488ef58dc723
Adria Navarro <adria@budibase.com>
2025-04-04 13:38:14 +0200

    Use tooling

--- packages/server/src/api/controllers/ai.ts
+++ packages/server/src/api/controllers/ai.ts
@@ -10,38 +10,18 @@ import {
   Upload,
   UserCtx,
 } from "@budibase/types"
-import { getLLM } from "packages/pro/src/ai"
+import { ai } from "@budibase/pro"
 import { context, objectStore, utils } from "@budibase/backend-core"
 import sdk from "../../sdk"
 import fs, { mkdirSync } from "fs"
 import path, { join } from "path"
-import { createHash } from "crypto"
+
 import { pipeline } from "stream"
 import { promisify } from "util"
 import fetch from "node-fetch"
 import { ObjectStoreBuckets } from "../../constants"
 
-export async function generateTables(
-  ctx: UserCtx<GenerateTablesRequest, GenerateTablesResponse>
-) {
-  const { prompt, useCached, addData } = ctx.request.body
-
-  const llm = await getLLM("gpt-4o")
-  if (!llm) {
-    ctx.throw("LLM not available")
-  }
-  llm.maxTokens = 1200
-
-  const cacheKey = `${createHash("md5").update(prompt).digest("hex")}_${
-    addData ? 1 : 0
-  }`
-
-  const response = await llm!.generateTables(
-    prompt,
-    useCached ? `${cacheKey}/latest` : "",
-    addData
-  )
-
+async function generateTablesDelegate(data: ai.GenerationStructure) {
   const count = (await sdk.datasources.fetch()).length
   const { id: dsId } = await context.getAppDB().put({
     _id: `${DocumentType.DATASOURCE}_bb_internal_${utils.newid()}`,
@@ -52,27 +32,10 @@ export async function generateTables(
     config: {},
   })
 
-  if (!useCached) {
-    const baseDir = path.join(objectStore.budibaseTempDir(), "llm-output")
-    if (!fs.existsSync(baseDir)) {
-      fs.mkdirSync(baseDir)
-    }
-    const dir = path.join(baseDir, cacheKey)
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir)
-    }
-    fs.writeFileSync(path.join(dir, "prompt.txt"), prompt)
-    fs.writeFileSync(path.join(dir, "latest.json"), JSON.stringify(response))
-    fs.writeFileSync(
-      path.join(dir, `${Date.now()}.json`),
-      JSON.stringify(response)
-    )
-  }
-
   const createdTables: GenerateTablesResponse["createdTables"] = []
   const tableIds: Record<string, string> = {}
 
-  for (const table of response.tables) {
+  for (const table of data.tables) {
     const createdTable = await sdk.tables.create({
       ...table.structure,
       sourceId: dsId,
@@ -86,7 +49,7 @@ export async function generateTables(
     tableIds[table.structure.name] = createdTable._id!
   }
 
-  for (const table of Object.values(response.tables)) {
+  for (const table of Object.values(data.tables)) {
     for (const field of table.structure.schema.filter(
       f => f.type === FieldType.LINK
     )) {
@@ -105,7 +68,7 @@ export async function generateTables(
     }
   }
 
-  for (const { structure: table } of Object.values(response.tables)) {
+  for (const { structure: table } of Object.values(data.tables)) {
     const storedTable = await sdk.tables.getTable(tableIds[table.name])
 
     await sdk.tables.update({
@@ -121,96 +84,117 @@ export async function generateTables(
     })
   }
 
-  if (addData) {
-    const createdData: Record<string, Record<string, string>> = {}
-    const toUpdateLinks: {
-      tableId: string
-      rowId: string
-      data: Record<string, { rowId: string; tableId: string }>
-    }[] = []
-    for (const table of Object.values(response.tables)) {
-      const linksOverride: Record<string, null> = {}
-      for (const field of table.structure.schema.filter(
-        f => f.type === FieldType.LINK
-      )) {
-        linksOverride[field.name] = null
-      }
+  // if (addData) {
+  //   const createdData: Record<string, Record<string, string>> = {}
+  //   const toUpdateLinks: {
+  //     tableId: string
+  //     rowId: string
+  //     data: Record<string, { rowId: string; tableId: string }>
+  //   }[] = []
+  //   for (const table of Object.values(response.tables)) {
+  //     const linksOverride: Record<string, null> = {}
+  //     for (const field of table.structure.schema.filter(
+  //       f => f.type === FieldType.LINK
+  //     )) {
+  //       linksOverride[field.name] = null
+  //     }
 
-      const attachmentColumns = table.structure.schema.filter(
-        f => f.type === FieldType.ATTACHMENT_SINGLE
-      )
+  //     const attachmentColumns = table.structure.schema.filter(
+  //       f => f.type === FieldType.ATTACHMENT_SINGLE
+  //     )
 
-      for (const entry of table.data || []) {
-        const tableId = tableIds[table.structure.name]
+  //     for (const entry of table.data || []) {
+  //       const tableId = tableIds[table.structure.name]
 
-        const attachmentData: Record<string, any> = {}
-        for (const column of attachmentColumns) {
-          const attachment = await downloadFile(
-            entry.values.find(f => f.key === column.name)!.value as string
-          )
-          attachmentData[column.name] = attachment
-        }
+  //       const attachmentData: Record<string, any> = {}
+  //       for (const column of attachmentColumns) {
+  //         const attachment = await downloadFile(
+  //           entry.values.find(f => f.key === column.name)!.value as string
+  //         )
+  //         attachmentData[column.name] = attachment
+  //       }
 
-        const createdRow = await sdk.rows.save(
-          tableId,
-          {
-            ...entry.values.reduce<Record<string, any>>((acc, v) => {
-              acc[v.key] = v.value
-              return acc
-            }, {}),
-            ...linksOverride,
-            ...attachmentData,
-            _id: undefined,
-          },
-          ctx.user._id
-        )
+  //       const createdRow = await sdk.rows.save(
+  //         tableId,
+  //         {
+  //           ...entry.values.reduce<Record<string, any>>((acc, v) => {
+  //             acc[v.key] = v.value
+  //             return acc
+  //           }, {}),
+  //           ...linksOverride,
+  //           ...attachmentData,
+  //           _id: undefined,
+  //         },
+  //         ctx.user._id
+  //       )
 
-        createdData[tableId] ??= {}
-        createdData[tableId][entry.id] = createdRow.row._id!
+  //       createdData[tableId] ??= {}
+  //       createdData[tableId][entry.id] = createdRow.row._id!
 
-        const overridenLinks = Object.keys(linksOverride).reduce<
-          Record<string, { rowId: string; tableId: string }>
-        >((acc, l) => {
-          if (entry.values.find(f => f.key === l)) {
-            acc[l] = {
-              tableId: (table.structure.schema.find(f => f.name === l) as any)
-                .tableId,
-              rowId: entry.values.find(f => f.key === l)!.value as any,
-            }
-          }
-          return acc
-        }, {})
+  //       const overridenLinks = Object.keys(linksOverride).reduce<
+  //         Record<string, { rowId: string; tableId: string }>
+  //       >((acc, l) => {
+  //         if (entry.values.find(f => f.key === l)) {
+  //           acc[l] = {
+  //             tableId: (table.structure.schema.find(f => f.name === l) as any)
+  //               .tableId,
+  //             rowId: entry.values.find(f => f.key === l)!.value as any,
+  //           }
+  //         }
+  //         return acc
+  //       }, {})
 
-        if (Object.keys(overridenLinks).length) {
-          toUpdateLinks.push({
-            tableId: createdRow.table._id!,
-            rowId: createdRow.row._id!,
-            data: overridenLinks,
-          })
-        }
-      }
-    }
+  //       if (Object.keys(overridenLinks).length) {
+  //         toUpdateLinks.push({
+  //           tableId: createdRow.table._id!,
+  //           rowId: createdRow.row._id!,
+  //           data: overridenLinks,
+  //         })
+  //       }
+  //     }
+  //   }
 
-    for (const data of toUpdateLinks) {
-      const persistedRow = await sdk.rows.find(data.tableId, data.rowId)
+  //   for (const data of toUpdateLinks) {
+  //     const persistedRow = await sdk.rows.find(data.tableId, data.rowId)
 
-      const updatedLinks = Object.keys(data.data).reduce<
-        Record<string, string>
-      >((acc, d) => {
-        acc[d] = createdData[data.data[d].tableId][data.data[d].rowId]
-        return acc
-      }, {})
+  //     const updatedLinks = Object.keys(data.data).reduce<
+  //       Record<string, string>
+  //     >((acc, d) => {
+  //       acc[d] = createdData[data.data[d].tableId][data.data[d].rowId]
+  //       return acc
+  //     }, {})
 
-      await sdk.rows.save(
-        data.tableId,
-        {
-          ...persistedRow,
-          ...updatedLinks,
-        },
-        ctx.user._id
-      )
-    }
+  //     await sdk.rows.save(
+  //       data.tableId,
+  //       {
+  //         ...persistedRow,
+  //         ...updatedLinks,
+  //       },
+  //       ctx.user._id
+  //     )
+  //   }
+  // }
+
+  return createdTables
+}
+
+export async function generateTables(
+  ctx: UserCtx<GenerateTablesRequest, GenerateTablesResponse>
+) {
+  const { prompt, useCached, addData } = ctx.request.body
+
+  const llm = await ai.getLLM("gpt-4o")
+  if (!llm) {
+    ctx.throw("LLM not available")
   }
+  llm.maxTokens = 5000
+
+  const createdTables = await llm!.generateTables(
+    prompt,
+    useCached,
+    addData,
+    generateTablesDelegate
+  )
 
   ctx.body = {
     createdTables,
