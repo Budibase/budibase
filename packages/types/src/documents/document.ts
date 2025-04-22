09bf3944e00b276ed04ac898ad50f5118a7519ab
Adria Navarro <adria@budibase.com>
2025-04-22 16:51:58 +0200

    Renames

--- packages/types/src/documents/document.ts
+++ packages/types/src/documents/document.ts
@@ -81,7 +81,7 @@ export interface Document {
   _rev?: string
   createdAt?: string | number
   updatedAt?: string
-  creationVersion?: string
+  createdVersion?: string
 }
 
 export interface AnyDocument extends Document {
