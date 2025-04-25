cfc67758bc38157a3612ba7b768319c8ebb84014
mikesealey <mikesealey@hotmail.com>
2025-04-25 07:20:00 +0100

    updates styling

--- packages/client/src/components/app/CodeGenerator.svelte
+++ packages/client/src/components/app/CodeGenerator.svelte
@@ -4,6 +4,7 @@
   import JsBarcode from "jsbarcode"
   import { createQrSvgString, createQrSvgDataUrl } from "@svelte-put/qr"
   import "@spectrum-css/vars/dist/spectrum-global.css"
+
   export let value: string
   export let codeType: "QR Code" | "Barcode"
   export let showValue: boolean
@@ -12,13 +13,15 @@
   export let size: number
   export let primColor: string
   export let invertColors: boolean
+  export let vertical: boolean
+
   const { styleable } = getContext("sdk")
   const component = getContext("component")
+
   const generateBarcode = () => {
-    console.log(size)
     let barcodeSize = size / 100
-    if (value) {
-      JsBarcode("#barcode", value, {
+    if (!!value) {
+      JsBarcode("#barcode-img", value, {
         displayValue: false, // Hide the library's built in value, optionally display it later
         width: barcodeSize,
         height: size,
@@ -39,7 +42,11 @@
   })
 </script>
 
-<div class="overall" use:styleable={$component.styles}>
+<div
+  class="overall {vertical ? 'vertical' : ''}"
+  use:styleable={$component.styles}
+  styles="border: 3px solid red; width: 100px; height: 200px;"
+>
   {#if value}
     {#if codeType === "QR Code"}
       <div class="qr-container">
@@ -58,26 +65,27 @@
       </div>
     {:else}
       <div
-        class="barcode-container"
+        id="barcode-container"
         style="background-color: {invertColors ? 'black' : 'white'}"
       >
-        <div class="logo-and-barcode">
+        <div id="logo-and-barcode" style="height: {size}px, width: 100%">
           {#if showLogo && customLogo}
             <img
+              id="custom-logo"
               src={customLogo}
               alt="logo"
               class="custom-logo"
-              style="height: {size}px;"
+              style="height: {size}px"
             />
           {/if}
           <img
-            id="barcode"
+            id="barcode-img"
             alt="barcode {value ? value : ''}"
-            class="barcode-image"
+            class="barcode-img"
           />
         </div>
         {#if showValue}
-          <div class="barcode-value">
+          <div id="barcode-value">
             <p
               style="background-color: {invertColors
                 ? 'white'
@@ -103,19 +111,6 @@
     flex-direction: row;
     justify-content: center;
   }
-  .logo-and-barcode {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  .barcode-value {
-    text-align: center;
-    padding-bottom: 5px;
-  }
-  .custom-logo {
-    padding-left: 10px;
-    transform: rotate(90);
-  }
   .qr-value {
     word-wrap: break-word;
     overflow-wrap: break-word;
@@ -125,5 +120,38 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+  #barcode-container {
+    /* border: 1px solid red; */
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    padding: 12px;
+  }
+  #logo-and-barcode {
+    /* border: 10px solid blue; */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  #custom-logo {
+    /* border: 1px solid lime; */
+    object-fit: contain;
+  }
+  #barcode-img {
+    /* border: 3px solid goldenrod; */
+    object-fit: contain;
+    max-width: 75%;
+    height: 100%;
+  }
+  #barcode-value {
+    /* border: 1px solid fuchsia; */
   }
 </style>
