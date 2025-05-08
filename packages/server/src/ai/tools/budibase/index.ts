fe24f7d60321266d0fde3776e44624f3c8bb29e6
Sam Rose <hello@samwho.dev>
2025-05-08 10:34:45 +0100

    Add a few more tools.

--- packages/server/src/ai/tools/budibase/index.ts
+++ packages/server/src/ai/tools/budibase/index.ts
@@ -1,2 +1,4 @@
 import AUTOMATION_TOOLS from "./automations"
-export default [...AUTOMATION_TOOLS]
+import TABLE_TOOLS from "./tables"
+import ROW_TOOLS from "./rows"
+export default [...AUTOMATION_TOOLS, ...TABLE_TOOLS, ...ROW_TOOLS]
