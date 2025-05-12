00bfb18aa65c026e5eac813976009f3dbd24371c
Adria Navarro <adria@budibase.com>
2025-05-12 16:23:02 +0200

    Fix merge conflicts

--- packages/builder/src/stores/builder/projectApps.ts
+++ packages/builder/src/stores/builder/projectApps.ts
@@ -1,18 +1,59 @@
-import { FetchAppPackageResponse, ProjectApp } from "@budibase/types"
-import { derived } from "svelte/store"
-import { BudiStore } from "../BudiStore"
+import { DerivedBudiStore } from "@/stores/BudiStore"
+import { Helpers } from "@budibase/bbui"
+import {
+  FetchAppPackageResponse,
+  ProjectApp,
+  UIProjectApp,
+} from "@budibase/types"
+import { derived, Readable } from "svelte/store"
+import { screenStore } from "./screens"
 
 interface ProjectAppStoreState {
   projectApps: ProjectApp[]
   loading: boolean
 }
 
-export class ProjectAppStore extends BudiStore<ProjectAppStoreState> {
+interface DerivedProjectAppStoreState {
+  projectApps: UIProjectApp[]
+}
+
+const DEFAULT_PROJECT_APP_ID = "default"
+
+export class ProjectAppStore extends DerivedBudiStore<
+  ProjectAppStoreState,
+  DerivedProjectAppStoreState
+> {
   constructor() {
-    super({
-      projectApps: [],
-      loading: true,
-    })
+    const makeDerivedStore = (store: Readable<ProjectAppStoreState>) => {
+      return derived([store, screenStore], ([$store, $screenStore]) => {
+        const projectApps = $store.projectApps.map<UIProjectApp>(projectApp => {
+          return {
+            ...projectApp,
+            screens: $screenStore.screens.filter(
+              s => (s.projectAppId || DEFAULT_PROJECT_APP_ID) === projectApp._id
+            ),
+          }
+        })
+
+        return { projectApps }
+      })
+    }
+
+    super(
+      {
+        projectApps: [
+          {
+            _id: DEFAULT_PROJECT_APP_ID,
+            urlPrefix: "/",
+            name: "Default",
+            icon: "Monitoring",
+            iconColor: "",
+          },
+        ],
+        loading: true,
+      },
+      makeDerivedStore
+    )
   }
 
   syncAppProjectApps(pkg: FetchAppPackageResponse) {
@@ -21,6 +62,36 @@ export class ProjectAppStore extends BudiStore<ProjectAppStoreState> {
       loading: false,
       projectApps: [...pkg.projectApps],
     }))
+  }
+
+  async add(projectApp: ProjectApp) {
+    this.store.update(state => {
+      state.projectApps.push({ ...projectApp, _id: Helpers.uuid() })
+      return state
+    })
+  }
+
+  async edit(projectApp: ProjectApp) {
+    this.store.update(state => {
+      const index = state.projectApps.findIndex(
+        app => app._id === projectApp._id
+      )
+      if (index === -1) {
+        throw new Error(`App not found with id "${projectApp._id}"`)
+      }
+
+      state.projectApps[index] = {
+        ...projectApp,
+      }
+      return state
+    })
+  }
+
+  async delete(_id: string | undefined, _rev: string | undefined) {
+    this.store.update(state => {
+      state.projectApps = state.projectApps.filter(app => app._id !== _id)
+      return state
+    })
   }
 }
 
