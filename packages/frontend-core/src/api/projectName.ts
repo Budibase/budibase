0a8f4ac4c14966917f70e8675e7fe5eda1e07cf3
Adria Navarro <adria@budibase.com>
2025-05-12 17:38:24 +0200

    Create frontend api

---
+++ packages/frontend-core/src/api/projectName.ts
@@ -0,0 +1,48 @@
+import {
+  InsertProjectAppRequest,
+  InsertProjectAppResponse,
+  UpdateProjectAppRequest,
+  UpdateProjectAppResponse,
+} from "@budibase/types"
+import { BaseAPIClient } from "./types"
+
+export interface ProjectAppEndpoints {
+  create: (
+    projectApp: InsertProjectAppRequest
+  ) => Promise<InsertProjectAppResponse>
+  update: (
+    projectApp: UpdateProjectAppRequest
+  ) => Promise<UpdateProjectAppResponse>
+  delete: (id: string, rev: string) => Promise<void>
+}
+
+export const buildProjectAppEndpoints = (
+  API: BaseAPIClient
+): ProjectAppEndpoints => ({
+  create: async projectApp => {
+    const result = await API.post<
+      InsertProjectAppRequest,
+      InsertProjectAppResponse
+    >({
+      url: "/api/projectApp",
+      body: projectApp,
+    })
+    return result
+  },
+  update: async projectApp => {
+    const result = await API.post<
+      UpdateProjectAppRequest,
+      UpdateProjectAppResponse
+    >({
+      url: `/api/projectApp/${projectApp._id}`,
+      body: projectApp,
+    })
+    return result
+  },
+
+  delete: async (id, rev) => {
+    return await API.delete({
+      url: `/api/screens/${id}/${rev}`,
+    })
+  },
+})
