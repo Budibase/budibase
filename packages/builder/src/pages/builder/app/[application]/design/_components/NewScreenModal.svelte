ff9d8bc0d497b4c9c8fb07185206506d34e9724d
Adria Navarro <adria@budibase.com>
2025-05-13 13:29:47 +0200

    Add new modal

---
+++ packages/builder/src/pages/builder/app/[application]/design/_components/NewScreenModal.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Modal, ModalContent } from "@budibase/bbui"
+  import NewScreen from "../_components/NewScreen/index.svelte"
+
+  let modal: Modal
+  export const show = () => modal.show()
+</script>
+
+<Modal bind:this={modal}>
+  <ModalContent title="Create new screen" size="XL">
+    <NewScreen />
+  </ModalContent>
+</Modal>
