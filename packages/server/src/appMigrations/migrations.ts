93087df445276e176fcc1021bef451c12840a310
Adria Navarro <adria@budibase.com>
2025-05-14 16:09:28 +0200

    Add flag

--- packages/server/src/appMigrations/migrations.ts
+++ packages/server/src/appMigrations/migrations.ts
@@ -1,9 +1,11 @@
 // This file should never be manually modified, use `yarn add-app-migration` in order to add a new one
 
+import { features } from "@budibase/backend-core"
 import { AppMigration } from "."
 
 import m20240604153647_initial_sqs from "./migrations/20240604153647_initial_sqs"
 import m20250514133719_project_apps from "./migrations/20250514133719_project_apps"
+import { FeatureFlag } from "@budibase/types"
 
 export const MIGRATIONS: AppMigration[] = [
   // Migrations will be executed sorted by id
@@ -14,5 +16,6 @@ export const MIGRATIONS: AppMigration[] = [
   {
     id: "20250514133719_project_apps",
     func: m20250514133719_project_apps,
+    disabled: !features.flags.isEnabled(FeatureFlag.PROJECT_APPS),
   },
 ]
