e30bfdc425ccb977f51ea87c5c6d8d95a9e675b6
Adria Navarro <adria@budibase.com>
2025-05-16 14:09:52 +0200

    Remove border on inline

--- packages/builder/src/pages/builder/app/[application]/design/new.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/new.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
-  import NewScreen from "./_components/NewScreen/index.svelte"
-  import { screenStore } from "@/stores/builder"
-  import { goto } from "@roxi/routify"
   import { onMount } from "svelte"
+  import NewScreen from "./_components/NewScreen/index.svelte"
 
   let newScreenModal: NewScreen
-
-  $: onClose = getOnClose($screenStore)
-
-  const getOnClose = ({ screens, selectedScreenId }: typeof $screenStore) => {
-    if (!screens?.length) {
-      return null
-    }
-    if (selectedScreenId) {
-      return () => {
-        $goto(`./${selectedScreenId}`)
-      }
-    }
-    return () => {
-      $goto(`./${screens[0]._id}`)
-    }
-  }
 
   onMount(() => {
     newScreenModal.show()
   })
 </script>
 
-<NewScreen {onClose} bind:this={newScreenModal} inline />
+<div class="new-screen-picker">
+  <NewScreen bind:this={newScreenModal} inline />
+</div>
+
+<style>
+  .new-screen-picker {
+    justify-items: center;
+  }
+
+  .new-screen-picker :global(.spectrum-Modal) {
+    border: none;
+    background: none;
+  }
+</style>
