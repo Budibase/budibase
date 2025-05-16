f040a31323420ab3bbc416c31baec6735c93711b
Adria Navarro <adria@budibase.com>
2025-05-16 16:28:30 +0200

    More renames

--- packages/server/src/appMigrations/migrations.ts
+++ packages/server/src/appMigrations/migrations.ts
@@ -4,7 +4,7 @@ import { features } from "@budibase/backend-core"
 import { AppMigration } from "."
 
 import m20240604153647_initial_sqs from "./migrations/20240604153647_initial_sqs"
-import m20250514133719_project_apps from "./migrations/20250514133719_workspace_apps"
+import m20250514133719_workspace_apps from "./migrations/20250514133719_workspace_apps"
 import { FeatureFlag } from "@budibase/types"
 
 export const MIGRATIONS: AppMigration[] = [
@@ -14,8 +14,8 @@ export const MIGRATIONS: AppMigration[] = [
     func: m20240604153647_initial_sqs,
   },
   {
-    id: "20250514133719_project_apps",
-    func: m20250514133719_project_apps,
+    id: "m20250514133719_workspace_apps",
+    func: m20250514133719_workspace_apps,
     // Disabling it, enabling it via env variables to enable development.
     // Using the existing flag system would require async checks and we could run to race conditions, so this keeps is simple
     disabled: !features
