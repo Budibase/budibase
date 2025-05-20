db88dc911f71b7a34da5be405acdd7b3af7d2fd4
Andrew Kingston <andrew@kingston.dev>
2025-05-20 19:40:35 +0100

    Add new top bar component

---
+++ packages/builder/src/components/common/TopBar.svelte
@@ -0,0 +1,59 @@
+<script context="module" lang="ts">
+  interface Breadcrumb {
+    text: string
+    url?: string
+  }
+</script>
+
+<script lang="ts">
+  import { Icon } from "@budibase/bbui"
+
+  export let icon: string
+  export let breadcrumbs: Breadcrumb[]
+</script>
+
+<div class="top-bar">
+  {#if icon}
+    <Icon name={icon} size="L" />
+  {/if}
+  <div class="breadcrumbs">
+    {#each breadcrumbs as breadcrumb, idx}
+      <h1>{breadcrumb.text}</h1>
+      {#if idx < breadcrumbs.length - 1}
+        <h1 class="divider">/</h1>
+      {/if}
+    {/each}
+  </div>
+  <slot name="right"></slot>
+</div>
+
+<style>
+  .top-bar {
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+    border-bottom: 1px solid var(--spectrum-global-color-gray-200);
+    padding: 0 10px 0 20px;
+  }
+  .breadcrumbs {
+    flex: 1 1 auto;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+  }
+  .breadcrumbs h1 {
+    font-size: 18px;
+    font-weight: 510;
+    color: var(--spectrum-global-color-gray-700);
+  }
+  .breadcrumbs h1:last-child {
+    color: var(--spectrum-global-color-gray-900);
+  }
+  .breadcrumbs h1.divider {
+    color: var(--spectrum-global-color-gray-400);
+  }
+</style>
--- packages/builder/src/pages/builder/app/[application]/design/[screenId]/_layout.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/[screenId]/_layout.svelte
@@ -1,51 +1,125 @@
-<script>
+<script lang="ts">
   import AppPanel from "./_components/AppPanel.svelte"
   import * as routify from "@roxi/routify"
   import { syncURLToState } from "@/helpers/urlStateSync"
-  import { screenStore, selectedScreen } from "@/stores/builder"
+  import {
+    screenStore,
+    selectedScreen,
+    appStore,
+    deploymentStore,
+  } from "@/stores/builder"
   import { onDestroy } from "svelte"
   import LeftPanel from "./_components/LeftPanel.svelte"
+  import TopBar from "@/components/common/TopBar.svelte"
+  import {
+    Body,
+    Button,
+    Icon,
+    Popover,
+    PopoverAlignment,
+    type PopoverAPI,
+  } from "@budibase/bbui"
+
+  let publishButton: HTMLElement | undefined
+  let publishSuccessPopover: PopoverAPI | undefined
 
   // Keep URL and state in sync for selected screen ID
   const stopSyncing = syncURLToState({
     urlParam: "screenId",
     stateKey: "selectedScreenId",
-    validate: id => $screenStore.screens.some(screen => screen._id === id),
+    validate: (id: string) =>
+      $screenStore.screens.some(screen => screen._id === id),
     fallbackUrl: "../../design",
     routify,
     update: screenStore.select,
     store: screenStore,
   })
 
-  onDestroy(stopSyncing)
+  const publish = async () => {
+    await deploymentStore.publishApp(false)
+    publishSuccessPopover?.show()
+  }
+
+  onDestroy(() => {
+    stopSyncing?.()
+  })
 </script>
 
 {#if $selectedScreen}
   <div class="design">
+    <TopBar
+      breadcrumbs={[{ text: "Apps" }, { text: $appStore.name }]}
+      icon="RailLeft"
+    >
+      <Button
+        cta
+        on:click={publish}
+        disabled={$deploymentStore.isPublishing}
+        bind:ref={publishButton}
+        slot="right"
+      >
+        Publish
+      </Button>
+    </TopBar>
     <div class="content">
       <LeftPanel />
       <AppPanel />
       <slot />
     </div>
   </div>
+
+  <Popover
+    anchor={publishButton}
+    bind:this={publishSuccessPopover}
+    align={PopoverAlignment.Right}
+    offset={6}
+  >
+    <div class="popover-content">
+      <Icon
+        name="CheckmarkCircle"
+        color="var(--spectrum-global-color-green-400)"
+        size="L"
+      />
+      <Body size="S">
+        App published successfully
+        <br />
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="link" on:click={deploymentStore.viewPublishedApp}>
+          View app
+        </div>
+      </Body>
+    </div>
+  </Popover>
 {/if}
 
 <style>
   .design {
     flex: 1 1 auto;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: flex-start;
     align-items: stretch;
     height: 0;
   }
-
   .content {
-    width: 100vw;
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
     align-items: stretch;
     flex: 1 1 auto;
+  }
+  .popover-content {
+    display: flex;
+    gap: var(--spacing-m);
+    padding: var(--spacing-xl);
+  }
+  .link {
+    text-decoration: underline;
+    color: var(--spectrum-global-color-gray-900);
+  }
+  .link:hover {
+    cursor: pointer;
+    filter: brightness(110%);
   }
 </style>
