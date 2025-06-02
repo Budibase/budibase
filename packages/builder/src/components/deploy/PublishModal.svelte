b3512db5da8bee35feaf1445c3a96c08d367ed40
mike12345567 <me@michaeldrury.co.uk>
2025-06-02 17:23:07 +0100

    Following designs.

--- packages/builder/src/components/deploy/PublishModal.svelte
+++ packages/builder/src/components/deploy/PublishModal.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-  import { Modal, ModalContent, Checkbox, Accordion } from "@budibase/bbui"
+  import { Modal, ModalContent, Checkbox, Accordion, Layout } from "@budibase/bbui"
   import { workspaceAppStore, automationStore } from "@/stores/builder"
   import { onMount } from "svelte"
 
   export let targetId: string
 
   let publishModal: Modal
-  let target: { type: "application" | "automation", id: string, name: string }
+  let target: { type: "app" | "automation", id: string, name: string }
   let list: Record<string, boolean> = {}
 
   $: automations = $automationStore.automations
   $: apps = $workspaceAppStore.workspaceApps
+
+  export function show() {
+    publishModal.show()
+  }
+
+  export function hide() {
+    publishModal.hide()
+  }
 
   function publish() {
 
@@ -19,7 +27,7 @@
   onMount(() => {
     const app = apps.find(app => app._id === targetId)
     if (app) {
-      target = { type: "application", id: targetId, name: app.name }
+      target = { type: "app", id: targetId, name: app.name }
     }
     const automation = automations.find(automation => automation._id === targetId)
     if (automation) {
@@ -37,8 +45,10 @@
     <span
     >Select the apps or automations you'd like to publish.</span
     >
-    <Checkbox label={target.name} />
-    <Accordion header="Publish multiple apps and automations" />
-    <Accordion header="Show everything that will be published" />
+    <Layout noPadding noGap>
+      <Checkbox text={`${target.name} ${target.type}`} />
+      <Accordion header="Publish multiple apps and automations" bold={false} />
+      <Accordion header="Show everything that will be published" bold={false} />
+    </Layout>
   </ModalContent>
 </Modal>
