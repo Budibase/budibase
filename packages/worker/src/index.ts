34c51570e780b9093de7f93307407a2fe9a1c9de
Adria Navarro <adria@budibase.com>
2025-06-03 10:51:43 +0200

    Fix closing issue

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -107,7 +107,7 @@ gracefulShutdown(server, {
   signals: "SIGINT SIGTERM",
   timeout: 30000,
   onShutdown: shutdown,
-  forceExit: !env.isTest,
+  forceExit: !env.isTest(),
   finally: () => {
     console.log("Worker service shutdown complete")
   },
@@ -116,7 +116,7 @@ gracefulShutdown(server, {
 process.on("uncaughtException", async err => {
   logging.logAlert("Uncaught exception.", err)
   await shutdown()
-  if (!env.isTest) {
+  if (!env.isTest()) {
     process.exit(1)
   }
 })
@@ -124,7 +124,7 @@ process.on("uncaughtException", async err => {
 process.on("unhandledRejection", async reason => {
   logging.logAlert("Unhandled Promise Rejection", reason as Error)
   await shutdown()
-  if (!env.isTest) {
+  if (!env.isTest()) {
     process.exit(1)
   }
 })
