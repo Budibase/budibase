51414919a4ac8fc71bfef4e9e969eff4ff582aa9
Adria Navarro <adria@budibase.com>
2025-06-05 16:00:30 +0200

    Improve

--- packages/server/src/sdk/app/workspaceApps/utils.ts
+++ packages/server/src/sdk/app/workspaceApps/utils.ts
@@ -3,21 +3,18 @@ import sdk from "../.."
 import { db } from "@budibase/backend-core"
 import { defaultAppNavigator } from "../../../constants/definitions"
 
-export async function getMatchedWorkspaceApp(fromUrl: string | undefined) {
+export async function getMatchedWorkspaceApp(fromUrl: string) {
   const app = await sdk.applications.metadata.get()
   const baseAppUrl = db.isProdAppID(app.appId)
     ? `/app/${app.url}`.replace("//", "/")
     : `/${app.appId}`
 
-  let allWorkspaceApps = await sdk.workspaceApps.fetch()
-  // Sort decending to ensure we match the most strict, removing match conflicts
-  allWorkspaceApps = allWorkspaceApps.sort((a, b) =>
-    b.urlPrefix.localeCompare(a.urlPrefix)
-  )
+  const allWorkspaceApps = await sdk.workspaceApps.fetch()
 
   function isWorkspaceAppMatch({ urlPrefix }: WorkspaceApp) {
-    return (fromUrl || "/").startsWith(
-      `${baseAppUrl}${urlPrefix}`.replace(/\/$/, "")
+    return (
+      fromUrl.replace(/\/$/, "") ===
+      `${baseAppUrl}${urlPrefix.replace(/\/$/, "")}`
     )
   }
 
