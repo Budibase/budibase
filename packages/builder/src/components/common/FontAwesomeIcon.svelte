2eb3e3cf0e84287a9c155134c862e23ddd7fe60d
Andrew Kingston <andrew@kingston.dev>
2025-06-05 16:45:07 +0100

    Remove font awesome dep, clean up multiple usages

--- packages/builder/src/components/common/FontAwesomeIcon.svelte
+++ packages/builder/src/components/common/FontAwesomeIcon.svelte
@@ -1,77 +1,37 @@
-<script context="module">
-  import { dom, library } from "@fortawesome/fontawesome-svg-core"
-  import {
-    faEnvelope,
-    faXmark,
-    faBook,
-    faPlay,
-    faLock,
-    faFileArrowUp,
-    faChevronLeft,
-    faCircleInfo,
-    faBold,
-    faItalic,
-    faHeading,
-    faQuoteLeft,
-    faListUl,
-    faListOl,
-    faLink,
-    faImage,
-    faEye,
-    faColumns,
-    faArrowsAlt,
-    faQuestionCircle,
-    faCircleCheck,
-    faGear,
-    faRectangleList,
-  } from "@fortawesome/free-solid-svg-icons"
-  import { faGithub, faDiscord } from "@fortawesome/free-brands-svg-icons"
-
-  library.add(
-    faXmark,
-    faBook,
-    faPlay,
-    faLock,
-    faGithub,
-    faDiscord,
-    faEnvelope,
-    faFileArrowUp,
-    faChevronLeft,
-    faCircleInfo,
-    faRectangleList,
-
-    // -- Required for easyMDE use in the builder.
-    faBold,
-    faItalic,
-    faHeading,
-    faQuoteLeft,
-    faListUl,
-    faListOl,
-    faLink,
-    faImage,
-    faEye,
-    faColumns,
-    faArrowsAlt,
-    faQuestionCircle,
-    // --
-
-    faCircleCheck,
-    faGear
-  )
-  dom.watch()
-</script>
-
 <script>
+  import { Icon } from "@budibase/bbui"
+
   export let name
+
+  // faXmark,
+  // faBook,
+  // faPlay,
+  // faLock,
+  // faGithub,
+  // faDiscord,
+  // faEnvelope,
+  // faFileArrowUp,
+  // faChevronLeft,
+  // faCircleInfo,
+  // faRectangleList,
+  //
+  // // -- Required for easyMDE use in the builder.
+  // faBold,
+  // faItalic,
+  // faHeading,
+  // faQuoteLeft,
+  // faListUl,
+  // faListOl,
+  // faLink,
+  // faImage,
+  // faEye,
+  // faColumns,
+  // faArrowsAlt,
+  // faQuestionCircle,
+  // // --
+  //
+  // faCircleCheck,
+  // faGear
 </script>
 
-<span>
-  <i class={name} />
-</span>
-
-<style>
-  span {
-    display: contents;
-    color: var(--spectrum-global-color-gray-900);
-  }
-</style>
+<Icon {name} />
