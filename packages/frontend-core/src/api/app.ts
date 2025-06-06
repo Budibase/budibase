2e7fec90e6b0257dd99c143d32829685cf33f2bf
Adria Navarro <adria@budibase.com>
2025-06-06 13:50:37 +0200

    Type duplicate

--- packages/frontend-core/src/api/app.ts
+++ packages/frontend-core/src/api/app.ts
@@ -150,7 +150,6 @@ export const buildAppEndpoints = (API: BaseAPIClient): AppEndpoints => ({
     return await API.post({
       url: `/api/applications/${appId}/duplicate`,
       body: app,
-      json: false,
     })
   },
 
