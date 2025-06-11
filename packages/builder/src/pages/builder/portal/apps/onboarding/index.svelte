f8df43a09fb6b3856d01c1c1bd1ff1df4cf0a7d9
Adria Navarro <adria@budibase.com>
2025-06-11 14:49:13 +0200

    Fix app creation on onboarding

--- packages/builder/src/pages/builder/portal/apps/onboarding/index.svelte
+++ packages/builder/src/pages/builder/portal/apps/onboarding/index.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { goto } from "@roxi/routify"
   import NamePanel from "./_components/NamePanel.svelte"
   import ExampleApp from "./_components/ExampleApp.svelte"
@@ -6,10 +6,11 @@
   import { SplitPage } from "@budibase/frontend-core"
   import { API } from "@/api"
   import { auth, admin } from "@/stores/portal"
+  import type { CreateAppRequest } from "@budibase/types"
 
-  let name = "My first app"
-  let url = "my-first-app"
-  let appId = null
+  let name: string = "My first app"
+  let url: string = "my-first-app"
+  let appId: string | null = null
 
   let loading = false
 
@@ -18,10 +19,11 @@
 
     // Create form data to create app
     // This is form based and not JSON
-    let data = new FormData()
-    data.append("name", name.trim())
-    data.append("url", url.trim())
-    data.append("useTemplate", false)
+    const data: CreateAppRequest = {
+      name: name.trim(),
+      url: url.trim(),
+      useTemplate: false,
+    }
 
     const createdApp = await API.createApp(data)
 
@@ -45,7 +47,7 @@
       await createApp()
 
       goToApp()
-    } catch (e) {
+    } catch (e: any) {
       loading = false
       notifications.error(e.message || "There was a problem creating your app")
     }
