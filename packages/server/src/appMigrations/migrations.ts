3012a5cb9e6ac5bece87a413caf0d4566b86d164
Adria Navarro <adria@budibase.com>
2025-06-18 18:02:07 +0200

    Migration enabled for all

--- packages/server/src/appMigrations/migrations.ts
+++ packages/server/src/appMigrations/migrations.ts
@@ -1,7 +1,5 @@
 // This file should never be manually modified, use `yarn add-app-migration` in order to add a new one
 
-import { features } from "@budibase/backend-core"
-import { FeatureFlag } from "@budibase/types"
 import { AppMigration } from "."
 
 import m20240604153647_initial_sqs from "./migrations/20240604153647_initial_sqs"
@@ -16,10 +14,5 @@ export const MIGRATIONS: AppMigration[] = [
   {
     id: "20250618162639_workspace_apps",
     func: m20250618162639_workspace_apps,
-    // Disabling it, enabling it via env variables to enable development.
-    // Using the existing flag system would require async checks and we could run to race conditions, so this keeps is simple
-    disabled: !features
-      .getEnvFlags()
-      .some(f => f.key === FeatureFlag.WORKSPACE_APPS && f.value === true),
   },
 ]
