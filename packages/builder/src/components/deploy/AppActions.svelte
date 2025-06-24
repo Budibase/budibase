1f13103b335eefd65ebb19cd751d697ca5035d1c
Andrew Kingston <andrew@kingston.dev>
2025-06-24 14:55:32 +0100

    Restore app action and flag core builder layout

---
+++ packages/builder/src/components/deploy/AppActions.svelte
@@ -0,0 +1,194 @@
+<script lang="ts">
+  import {
+    Button,
+    ActionButton,
+    StatusLight,
+    Popover,
+    PopoverAlignment,
+    Body,
+    Icon,
+  } from "@budibase/bbui"
+  import {
+    builderStore,
+    isOnlyUser,
+    appStore,
+    deploymentStore,
+    workspaceAppStore,
+    automationStore,
+  } from "@/stores/builder"
+  import { page } from "@roxi/routify"
+  import { admin, featureFlags } from "@/stores/portal"
+  import VersionModal from "@/components/deploy/VersionModal.svelte"
+  import PublishModal from "@/components/deploy/PublishModal.svelte"
+
+  type ShowUI = { show: () => void }
+
+  let versionModal: ShowUI
+  let publishModal: ShowUI
+  let showNpsSurvey = false
+  let publishButton: any
+  let publishSuccessPopover: ShowUI | undefined
+  let publishedAutomations: string[] = [],
+    publishedApps: string[] = []
+
+  $: workspaceAppsEnabled = $featureFlags.WORKSPACE_APPS
+  $: updateAvailable =
+    $appStore.upgradableVersion &&
+    $appStore.version &&
+    $appStore.upgradableVersion !== $appStore.version
+  $: inAutomations = $page.path.includes("/automation/")
+  $: inDesign = $page.path.includes("/design/")
+  $: selectedWorkspaceAppId =
+    $workspaceAppStore.selectedWorkspaceApp?._id || undefined
+  $: selectedAutomationId = $automationStore.selectedAutomationId || undefined
+
+  const publish = async () => {
+    if (workspaceAppsEnabled) {
+      publishModal.show()
+    } else {
+      await deploymentStore.publishApp()
+      publishSuccessPopover?.show()
+    }
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="action-top-nav">
+  <div class="action-buttons">
+    {#if updateAvailable && $isOnlyUser && !$admin.isDev}
+      <div class="app-action-button version" on:click={versionModal.show}>
+        <div class="app-action">
+          <ActionButton quiet>
+            <StatusLight notice />
+            Update
+          </ActionButton>
+        </div>
+      </div>
+    {/if}
+    <div class="app-action-button users">
+      <div class="app-action" id="builder-app-users-button">
+        <ActionButton
+          quiet
+          icon="users"
+          on:click={() => {
+            builderStore.showBuilderSidePanel()
+          }}
+        >
+          Users
+        </ActionButton>
+      </div>
+    </div>
+    <div class="app-action-button publish">
+      <Button
+        cta
+        on:click={publish}
+        disabled={$deploymentStore.isPublishing}
+        bind:ref={publishButton}
+      >
+        Publish
+      </Button>
+    </div>
+  </div>
+</div>
+
+{#if showNpsSurvey}
+  <div class="nps-survey" />
+{/if}
+
+<VersionModal hideIcon bind:this={versionModal} />
+{#if workspaceAppsEnabled}
+  <PublishModal
+    targetId={inDesign
+      ? selectedWorkspaceAppId
+      : inAutomations
+        ? selectedAutomationId
+        : undefined}
+    bind:this={publishModal}
+    on:success={evt => {
+      publishedAutomations = evt.detail.publishedAutomations
+      publishedApps = evt.detail.publishedApps
+      publishSuccessPopover?.show()
+    }}
+  />
+{/if}
+
+<Popover
+  anchor={publishButton}
+  bind:this={publishSuccessPopover}
+  align={PopoverAlignment.Right}
+  offset={6}
+>
+  <div class="popover-content">
+    <Icon
+      name="check-circle"
+      color="var(--spectrum-global-color-green-400)"
+      size="L"
+    />
+    <Body size="S">
+      {#if !workspaceAppsEnabled}
+        App published successfully
+        <br />
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="link" on:click={deploymentStore.viewPublishedApp}>
+          View app
+        </div>
+      {:else}
+        {#if publishedAutomations.length}
+          Automations published: {publishedAutomations.length}
+          <br />
+        {/if}
+        {#if publishedApps.length}
+          Apps published: {publishedApps.length}
+        {/if}
+      {/if}
+    </Body>
+  </div>
+</Popover>
+
+<style>
+  .action-buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+  }
+  .action-top-nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+    padding-right: var(--spacing-s);
+  }
+  .app-action-button {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding-right: var(--spacing-m);
+  }
+  .app-action-button.version :global(.spectrum-ActionButton-label) {
+    display: flex;
+    gap: var(--spectrum-actionbutton-icon-gap);
+  }
+  .app-action {
+    display: flex;
+    align-items: center;
+    gap: var(--spacing-s);
+  }
+  .popover-content {
+    display: flex;
+    gap: var(--spacing-m);
+    padding: var(--spacing-xl);
+  }
+  .link {
+    text-decoration: underline;
+    color: var(--spectrum-global-color-gray-900);
+  }
+  .link:hover {
+    cursor: pointer;
+    filter: brightness(110%);
+  }
+</style>
