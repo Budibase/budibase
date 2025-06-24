a6fa1f5f203d29bac59191b4faa67e249d90bc70
Michael Drury <me@michaeldrury.co.uk>
2025-06-24 17:02:39 +0100

    Fixing popover.

--- packages/builder/src/components/deploy/PublishModal.svelte
+++ packages/builder/src/components/deploy/PublishModal.svelte
@@ -44,9 +44,9 @@
     $workspaceDeploymentStore.workspaceApps
   )
   $: target = findTarget(targetId, apps, automations)
-  $: selectedAppNames = getSelectedNames(selectedApps, apps)
+  $: selectedAppNames = getSelectedNames(getSelectedIds(selectedApps), apps)
   $: selectedAutomationNames = getSelectedNames(
-    selectedAutomations,
+    getSelectedIds(selectedAutomations),
     automations
   )
   $: getUsedResources(
@@ -100,10 +100,9 @@
   }
 
   function getSelectedNames(
-    list: Record<string, boolean>,
+    selectedIds: string[],
     items: { _id?: string; name: string }[]
   ) {
-    const selectedIds = getSelectedIds(list)
     return items
       .filter(item => selectedIds.find(id => id === item._id))
       .map(item => item.name)
@@ -158,19 +157,18 @@
   }
 
   async function publish() {
-    const getIds = (list: Record<string, boolean>) =>
-      Object.entries(list)
-        .filter(([_, selected]) => selected)
-        .map(([id]) => id)
+    const preAutomations = automations, preApps = apps
+    const toPublishAutomations = getSelectedIds(selectedAutomations),
+      toPublishApps = getSelectedIds(selectedApps)
     await deploymentStore.publishApp({
-      automationIds: getIds(selectedAutomations),
-      workspaceAppIds: getIds(selectedApps),
+      automationIds: toPublishAutomations,
+      workspaceAppIds: toPublishApps,
     })
     const publishedAutomations = getSelectedNames(
-        selectedAutomations,
-        automations
+        toPublishAutomations,
+        preAutomations
       ),
-      publishedApps = getSelectedNames(selectedApps, apps)
+      publishedApps = getSelectedNames(toPublishApps, preApps)
     dispatcher("success", { publishedAutomations, publishedApps })
   }
 
