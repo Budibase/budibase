8d0be43eb2ccb01ee4c3225c1564d1b9b8c63f29
Andrew Kingston <andrew@kingston.dev>
2025-06-26 10:03:52 +0100

    Use real user name in side nav

--- packages/builder/src/pages/builder/app/[application]/_components/SideNav/SideNavUserSettings.svelte
+++ packages/builder/src/pages/builder/app/[application]/_components/SideNav/SideNavUserSettings.svelte
@@ -4,14 +4,33 @@
   import SideNavLink from "./SideNavLink.svelte"
   import UserDropdown from "@/pages/builder/portal/_components/UserDropdown.svelte"
   import { PopoverAlignment } from "@budibase/bbui"
+  import { type User } from "@budibase/types"
 
   export let collapsed = false
 
-  $: user = $auth.user!
+  $: user = $auth.user
+  $: name = getName(user)
+
+  const getName = (user?: User) => {
+    if (!user) {
+      return ""
+    }
+    if (user.firstName) {
+      if (user.lastName) {
+        return `${user.firstName} ${user.lastName}`
+      } else {
+        return `${user.firstName}`
+      }
+    } else {
+      return user.email
+    }
+  }
 </script>
 
-<UserDropdown align={PopoverAlignment.RightOutside} let:open>
-  <SideNavLink text="Andrew Kingston" {collapsed} forceActive={open}>
-    <UserAvatar slot="icon" size="XS" {user} showTooltip={false} />
-  </SideNavLink>
-</UserDropdown>
+{#if user}
+  <UserDropdown align={PopoverAlignment.RightOutside} let:open>
+    <SideNavLink text={name} {collapsed} forceActive={open}>
+      <UserAvatar slot="icon" size="XS" {user} showTooltip={false} />
+    </SideNavLink>
+  </UserDropdown>
+{/if}
