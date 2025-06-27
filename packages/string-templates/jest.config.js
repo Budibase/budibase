e4a99540c8be3c5d3fd2c8830d4631f7280f42ea
Andrew Kingston <andrew@kingston.dev>
2025-06-27 08:50:10 +0100

    Prune 60K from string-templates bundle by optimishing lodash usage

--- packages/string-templates/jest.config.js
+++ packages/string-templates/jest.config.js
@@ -82,7 +82,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^lodash-es$": "lodash",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
