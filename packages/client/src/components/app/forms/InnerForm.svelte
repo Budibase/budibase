6a3d3a53036de34c788af9852442217e4acea1cf
mikesealey <mikesealey@hotmail.com>
2025-06-27 16:43:22 +0100

    removes unused function from innerForm

--- packages/client/src/components/app/forms/InnerForm.svelte
+++ packages/client/src/components/app/forms/InnerForm.svelte
@@ -448,28 +448,6 @@
     }
   }
 
-  const handleScrollToField = (props: { field: FieldInfo | string }) => {
-    console.log("InnerForm.svelte", props)
-    console.log("InnerForm.svelte")
-    let field
-    if (typeof props.field === "string") {
-      field = get(getField(props.field))
-    } else {
-      field = props.field
-    }
-    const fieldId = field.fieldState.fieldId
-    const fieldElement = document.getElementById(fieldId)
-    if (fieldElement) {
-      fieldElement.focus({ preventScroll: true })
-    }
-    const label = document.querySelector<HTMLElement>(`label[for="${fieldId}"]`)
-    console.log("LABEL", label)
-    if (label) {
-      label.style.scrollMargin = "100px"
-      label.scrollIntoView({ behavior: "smooth", block: "nearest" })
-    }
-  }
-
   // Action context to pass to children
   const actions = [
     { type: ActionTypes.ValidateForm, callback: formApi.validate },
