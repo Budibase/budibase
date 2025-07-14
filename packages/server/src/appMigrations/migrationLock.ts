508e5e671a3f864b90df2ff3e7ce8beef029694e
Adria Navarro <adria@budibase.com>
2025-07-14 16:32:17 +0200

    Improve type

--- packages/server/src/appMigrations/migrationLock.ts
+++ packages/server/src/appMigrations/migrationLock.ts
@@ -1,15 +1,15 @@
 import { db, locks } from "@budibase/backend-core"
 import { LockName, LockType } from "@budibase/types"
 
-export async function doInMigrationLock(
+export async function doInMigrationLock<T>(
   appId: string,
-  fn: () => Promise<void>
-) {
+  fn: () => Promise<T>
+): Promise<T> {
   console.log(`Acquiring app migration lock for "${appId}"`)
 
   const prodAppId = db.getProdAppID(appId)
 
-  return await locks.doWithLock(
+  const { result } = await locks.doWithLock(
     {
       name: LockName.APP_MIGRATION,
       type: LockType.AUTO_EXTEND,
@@ -18,7 +18,9 @@ export async function doInMigrationLock(
     async () => {
       console.log(`Migration lock acquired for app "${prodAppId}"`)
 
-      await fn()
+      return await fn()
     }
   )
+
+  return result
 }
