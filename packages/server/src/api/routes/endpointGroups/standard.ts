4d76582005961f6bbcaaba043d58103b245ed924
Michael Drury <me@michaeldrury.co.uk>
2025-07-14 23:04:27 +0100

    Fix.

--- packages/server/src/api/routes/endpointGroups/standard.ts
+++ packages/server/src/api/routes/endpointGroups/standard.ts
@@ -2,6 +2,9 @@ import authorized from "../../../middleware/authorized"
 import { auth, permissions } from "@budibase/backend-core"
 import { customEndpointGroups } from "./custom"
 
+export const publicGroup = customEndpointGroups.group()
+publicGroup.lockMiddleware()
+
 export const globalBuilderGroup = customEndpointGroups.group(
   authorized(permissions.GLOBAL_BUILDER)
 )
@@ -19,6 +22,3 @@ creatorGroup.lockMiddleware()
 
 export const builderAdminGroup = customEndpointGroups.group(auth.builderOrAdmin)
 builderAdminGroup.lockMiddleware()
-
-export const publicGroup = customEndpointGroups.group()
-publicGroup.lockMiddleware()
