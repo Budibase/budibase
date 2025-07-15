d083053fc8da65978d27fbafc1df41994506d3d5
Dean <deanhannigan@gmail.com>
2025-07-15 09:15:12 +0100

    Refactoring for the table import and added a raw text box

--- packages/builder/src/components/backend/TableNavigator/TableDataImport.svelte
+++ packages/builder/src/components/backend/TableNavigator/TableDataImport.svelte
@@ -1,16 +1,17 @@
-<script>
-  import { Select, Icon, Layout, Label } from "@budibase/bbui"
+<script lang="ts">
+  import { Select, Icon, Layout } from "@budibase/bbui"
   import { FIELDS } from "@/constants/backend"
-  import { utils } from "@budibase/shared-core"
   import { canBeDisplayColumn } from "@budibase/frontend-core"
-  import { API } from "@/api"
-  import { parseFile } from "./utils"
+  import { createEventDispatcher } from "svelte"
+  import type { FieldSubType, Row, UIFieldSchema } from "@budibase/types"
 
-  export let rows = []
-  export let schema = {}
-  export let allValid = true
-  export let displayColumn = null
-  export let promptUpload = false
+  export let rows: Row[] = []
+  export let schema: Record<string, UIFieldSchema> = {}
+  export let displayColumn: string | null = null
+  export let validation: Record<string, boolean> | undefined = undefined
+  export let errors: Record<string, string> | undefined = undefined
+
+  const dispatch = createEventDispatcher()
 
   const typeOptions = {
     [FIELDS.STRING.type]: {
@@ -69,18 +70,18 @@
         constraints: FIELDS.LONGFORM.constraints,
       },
     },
-    user: {
+    [FIELDS.USER.type]: {
       label: "User",
-      value: "user",
+      value: FIELDS.USER.type,
       config: {
         type: FIELDS.USER.type,
         subtype: FIELDS.USER.subtype,
         constraints: FIELDS.USER.constraints,
       },
     },
-    users: {
+    [FIELDS.USERS.type]: {
       label: "Users",
-      value: "users",
+      value: FIELDS.USERS.type,
       config: {
         type: FIELDS.USERS.type,
         subtype: FIELDS.USERS.subtype,
@@ -89,137 +90,60 @@
     },
   }
 
-  let fileInput
-  let error = null
-  let fileName = null
-  let loading = false
-  let validation = {}
-  let validateHash = ""
-  let errors = {}
-  let selectedColumnTypes = {}
+  let displayColumnOptions: string[] | undefined = undefined
 
-  let rawRows = []
+  $: selectedColumnTypes = Object.entries(schema).reduce(
+    (acc, [colName, fieldConfig]) => ({
+      ...acc,
+      [colName]: fieldConfig.type,
+    }),
+    {} as Record<string, string>
+  )
 
-  $: displayColumnOptions = Object.keys(schema || {}).filter(column => {
-    return validation[column] && canBeDisplayColumn(schema[column])
-  })
+  $: processDisplayColumnOptions(schema, validation)
 
-  $: if (displayColumn && !canBeDisplayColumn(schema[displayColumn])) {
-    displayColumn = null
+  const processDisplayColumnOptions = (
+    schema: Record<string, UIFieldSchema>,
+    validation?: Record<string, boolean>
+  ) => {
+    if (!schema || !validation) return []
+
+    displayColumnOptions = Object.keys(schema || {}).filter(column => {
+      return validation[column] && canBeDisplayColumn(schema[column])
+    })
   }
 
-  $: {
-    rows = rawRows.map(row => utils.trimOtherProps(row, Object.keys(schema)))
-
-    // binding in consumer is causing double renders here
-    const newValidateHash = JSON.stringify(rows) + JSON.stringify(schema)
-    if (newValidateHash !== validateHash) {
-      validate(rows, schema)
-    }
-    validateHash = newValidateHash
-  }
-  $: openFileUpload(promptUpload, fileInput)
-
-  async function handleFile(e) {
-    loading = true
-    error = null
-    validation = {}
-
-    try {
-      const response = await parseFile(e)
-      rawRows = response.rows
-      schema = response.schema
-      fileName = response.fileName
-      selectedColumnTypes = Object.entries(response.schema).reduce(
-        (acc, [colName, fieldConfig]) => ({
-          ...acc,
-          [colName]: fieldConfig.type,
-        }),
-        {}
-      )
-    } catch (e) {
-      loading = false
-      error = e
-    }
-  }
-
-  async function validate(rows, schema) {
-    loading = true
-    try {
-      if (rows.length > 0) {
-        const response = await API.validateNewTableImport(rows, schema)
-        validation = response.schemaValidation
-        allValid = response.allValid
-        errors = response.errors
-        error = null
-      }
-    } catch (e) {
-      error = e.message
-      validation = {}
-      allValid = false
-      errors = {}
-    }
-    loading = false
-  }
-
-  const handleChange = (name, e) => {
+  const handleChange = (name: string, e: CustomEvent) => {
     const { config } = typeOptions[e.detail]
-    schema[name].type = config.type
-    schema[name].subtype = config.subtype
-    schema[name].constraints = config.constraints
+    const update = { ...schema }
+    update[name].type = config.type
+    if (config.subtype) update[name].subtype = config.subtype as FieldSubType
+    update[name].constraints = config.constraints
+    dispatch("update", {
+      schema: update,
+    })
   }
 
-  const openFileUpload = (promptUpload, fileInput) => {
-    if (promptUpload && fileInput) {
-      fileInput.click()
-    }
-  }
-
-  const deleteColumn = name => {
-    if (loading) {
-      return
-    }
-    delete schema[name]
-    schema = schema
+  const deleteColumn = (name: string) => {
+    const update = { ...schema }
+    delete update[name]
+    dispatch("update", {
+      schema: update,
+    })
   }
 </script>
 
 <Layout noPadding gap="S">
-  <Layout gap="XS" noPadding>
-    <Label grey extraSmall>
-      Create a Table from a CSV or JSON file (Optional)
-    </Label>
-    <div class="dropzone">
-      <input
-        bind:this={fileInput}
-        disabled={loading}
-        id="file-upload"
-        accept="text/csv,application/json"
-        type="file"
-        on:change={handleFile}
-      />
-      <label for="file-upload" class:uploaded={rawRows.length > 0}>
-        {#if error}
-          Error: {error}
-        {:else if fileName}
-          {fileName}
-        {:else}
-          Upload
-        {/if}
-      </label>
-    </div>
-  </Layout>
-
-  {#if rawRows.length > 0 && !error}
+  {#if rows.length > 0 && validation}
     <div>
       {#each Object.entries(schema) as [name, column]}
         <div class="field">
           <span>{column.name}</span>
           <Select
-            bind:value={selectedColumnTypes[column.name]}
+            value={selectedColumnTypes[column.name]}
             on:change={e => handleChange(name, e)}
             options={Object.values(typeOptions)}
-            placeholder={null}
+            placeholder={false}
             getOptionLabel={option => option.label}
             getOptionValue={option => option.value}
           />
@@ -232,8 +156,8 @@
               Success
             {:else}
               Failure
-              {#if errors[column.name]}
-                <Icon name="question" tooltip={errors[column.name]} />
+              {#if errors?.[column.name]}
+                <Icon name="question" tooltip={errors?.[column.name]} />
               {/if}
             {/if}
           </span>
@@ -248,7 +172,12 @@
     </div>
     <Select
       label="Display Column"
-      bind:value={displayColumn}
+      value={displayColumn}
+      on:change={e => {
+        dispatch("update", {
+          displayColumn: e.detail,
+        })
+      }}
       options={displayColumnOptions}
       sort
     />
@@ -256,49 +185,6 @@
 </Layout>
 
 <style>
-  .dropzone {
-    text-align: center;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 10px;
-    transition: all 0.3s;
-  }
-
-  input {
-    display: none;
-  }
-
-  label {
-    cursor: pointer;
-    font-weight: 600;
-    box-sizing: border-box;
-    overflow: hidden;
-    border-radius: var(--border-radius-s);
-    padding: var(--spacing-m) var(--spacing-l);
-    transition: all 0.2s ease 0s;
-    display: inline-flex;
-    text-rendering: optimizeLegibility;
-    min-width: auto;
-    outline: none;
-    font-feature-settings:
-      "case" 1,
-      "rlig" 1,
-      "calt" 0;
-    -webkit-box-align: center;
-    user-select: none;
-    flex-shrink: 0;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    background-color: var(--spectrum-global-color-gray-300);
-    font-size: var(--font-size-s);
-    line-height: normal;
-    border: var(--border-transparent);
-  }
-  .uploaded {
-    color: var(--spectrum-global-color-blue-600);
-  }
   .field {
     display: grid;
     grid-template-columns: 2fr 2fr 1fr auto;
