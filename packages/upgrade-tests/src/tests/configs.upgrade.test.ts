12364ff3710bb78c1f916683c7467c0270b7ba9c
Michael Drury <me@michaeldrury.co.uk>
2025-07-24 16:30:56 +0100

    Fixing test case change to handle recaptcha being dropped if not supported.

--- packages/upgrade-tests/src/tests/configs.upgrade.test.ts
+++ packages/upgrade-tests/src/tests/configs.upgrade.test.ts
@@ -12,14 +12,15 @@ describe("configs", () => {
   shouldNotChange.each(Object.values(ConfigType))(
     "config type: %s",
     async type => {
+      // recaptcha wasn't added until after commit deb3556 - therefore wasn't supported
+      if (type === ConfigType.RECAPTCHA && upgradeSpansCommit("deb3556")) {
+        return
+      }
       const config = await client.config.get(type)
       // In commit 191bf3a we fixed a bug where the AI config type didn't
       // always set a `type` property.
       if (type === ConfigType.AI && upgradeSpansCommit("191bf3a")) {
         config.type = ConfigType.AI
-      }
-      if (type === ConfigType.RECAPTCHA && upgradeSpansCommit("deb3556")) {
-        config.type = ConfigType.RECAPTCHA
       }
       return config
     }
