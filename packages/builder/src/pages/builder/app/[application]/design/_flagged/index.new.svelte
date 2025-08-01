a8d91f0d3d81ab9f31966835d9d375c6d5a02984
Adria Navarro <adria@budibase.com>
2025-08-01 12:18:03 +0200

    Renames

--- packages/builder/src/pages/builder/app/[application]/design/_flagged/index.new.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/_flagged/index.new.svelte
@@ -31,15 +31,15 @@
       filterValue: undefined,
     },
     {
-      label: "Published",
+      label: "On",
       filterValue: PublishResourceState.PUBLISHED,
     },
     {
-      label: "Disabled",
+      label: "Off",
       filterValue: PublishResourceState.DISABLED,
     },
     {
-      label: "Drafts",
+      label: "Pending",
       filterValue: PublishResourceState.UNPUBLISHED,
     },
   ]
