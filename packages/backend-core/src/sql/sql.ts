640f4bbf85bb89576e638101daba4a3ae24813d9
Mel O'Hagan <mel@budibase.com>
2025-08-11 22:09:39 +0100

    Fix Is In filter for text[] postgres type

--- packages/backend-core/src/sql/sql.ts
+++ packages/backend-core/src/sql/sql.ts
@@ -890,6 +890,11 @@ class InternalBuilder {
               }
             }
             return q
+          } else if (this.SPECIAL_SELECT_CASES.POSTGRES_ARRAY(schema)) {
+            return q.whereRaw(
+              `?? && '{${values.map(value => value.substring(1, value.length - 1))}}'`,
+              [this.rawQuotedIdentifier(schema!.name)]
+            )
           }
           return q.whereIn(key, values)
         },
