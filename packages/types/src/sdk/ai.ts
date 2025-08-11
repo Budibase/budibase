4992f771655e82a4dd0c2ab5b16c68441fd1510d
Peter Clement <peter@budibase.com>
2025-08-11 09:25:19 +0100

    update llm opts

--- packages/types/src/sdk/ai.ts
+++ packages/types/src/sdk/ai.ts
@@ -97,8 +97,6 @@ export interface LLMConfigOptions {
   model: string
   apiKey?: string
   maxTokens?: number
-  // New style of tokens
-  max_completion_tokens?: number
   // Primarily here for Azure OpenAI, because each customer has their own endpoint
   baseUrl?: string
 }
