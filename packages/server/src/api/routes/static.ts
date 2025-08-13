41e0fdaf1e0b9e929f521bd3fb401761fb169c37
Adria Navarro <adria@budibase.com>
2025-08-13 12:33:03 +0200

    Use route to server apple-touch-icon.png, instead of using controller

--- packages/server/src/api/routes/static.ts
+++ packages/server/src/api/routes/static.ts
@@ -14,6 +14,9 @@ const router: Router = new Router()
 addFileManagement(router)
 
 router
+  .get("/apple-touch-icon.png", async ctx => {
+    ctx.redirect("/builder/bblogo.png")
+  })
   .get("/api/assets/client", controller.serveClientLibrary)
   .get("/api/apps/:appId/manifest.json", controller.servePwaManifest)
   .post("/api/attachments/process", authorized(BUILDER), controller.uploadFile)
