0ee982d3ed31e2e988b37d8fa22d4fc5cb8046df
Michael Drury <me@michaeldrury.co.uk>
2025-08-13 14:12:11 +0100

    Using CtaNotification.

--- packages/builder/src/components/backend/DataTable/alert/grid/GridDevWarning.svelte
+++ packages/builder/src/components/backend/DataTable/alert/grid/GridDevWarning.svelte
@@ -1,49 +1,34 @@
 <script lang="ts">
   import { dataEnvironmentStore } from "@/stores/builder"
-  import { Icon } from "@budibase/bbui"
+  import CtaNotification from "@/components/common/CtaNotification.svelte"
 </script>
 
 {#if !$dataEnvironmentStore.bannerHidden}
-  <div class="dev-warning">
-    <div class="dev-warning-inner">
-      <Icon name="info" />
-      <span
-        >You're currently in Dev, so you can test things out without affecting
-        real users. Switch to Prod to view/edit live data. <a
-          href="https://docs.budibase.com/docs/dev-prod-switcher"
-          class="link">Visit the Docs ↗</a
-        ></span
-      >
-    </div>
-    <Icon
-      name="x"
-      size="S"
-      hoverable
-      weight="bold"
+  <div class="location">
+    <CtaNotification
+      icon="info"
+      buttonText="X"
       on:click={() => dataEnvironmentStore.hideBanner()}
-    />
+    >
+      <span>
+        You're currently in Dev, so you can test things out without affecting
+        real users. Switch to Prod to view/edit live data.
+        <a href="https://docs.budibase.com/docs/dev-prod-switcher" class="link">
+          Visit the Docs ↗
+        </a>
+      </span>
+    </CtaNotification>
   </div>
 {/if}
 
 <style>
-  .dev-warning {
-    bottom: 0;
+  .location {
+    position: absolute;
+    bottom: 2%;
+    left: 1%;
+    right: 1%;
     width: 98%;
-    background: rgba(75, 117, 255, 0.2);
-    border: 1px solid rgba(75, 117, 255, 0.2);
-    color: white;
-    padding: var(--spacing-m);
-    font-size: var(--font-size-m);
-    margin: 0 var(--spacing-l) var(--spacing-l) var(--spacing-l);
-    border-radius: 12px;
-    z-index: 999;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .dev-warning-inner {
-    display: flex;
-    gap: var(--spacing-m);
+    z-index: 99;
   }
   .link {
     color: var(--spectrum-global-color-blue-700);
