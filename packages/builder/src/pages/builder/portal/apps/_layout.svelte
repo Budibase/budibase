9fd547856028c069eb9eb974fa4c7ae33f4fd318
Adria Navarro <adria@budibase.com>
2025-08-18 13:12:17 +0200

    Flag banner

--- packages/builder/src/pages/builder/portal/apps/_layout.svelte
+++ packages/builder/src/pages/builder/portal/apps/_layout.svelte
@@ -7,6 +7,7 @@
     licensing,
     groups,
     auth,
+    featureFlags,
   } from "@/stores/portal"
   import { onMount } from "svelte"
   import { redirect } from "@roxi/routify"
@@ -49,17 +50,20 @@
 
 {#if loaded}
   <div class="page">
-    <HeroBanner
-      title="Workspaces are live"
-      linkTitle="Learn about workspaces"
-      linkHref="https://budibase.com/blog/updates/workspaces/"
-      color="var(--spectrum-global-color-gray-100)"
-      image="https://res.cloudinary.com/daog6scxm/image/upload/w_1200,h_800/v1628152378/1.%20Illustrations/Scene_4_web_version_izudxc.avif"
-      >Previously, Budibase centered everything around building a single app.
-      With Workspaces, that changes. Now, you can group multiple apps,
-      automations, and data sources together within a single workspace. Existing
-      apps now have their own workspace.</HeroBanner
-    >
+    {#if $featureFlags.WORKSPACE_APPS}
+      <HeroBanner
+        title="Workspaces are live"
+        linkTitle="Learn about workspaces"
+        linkHref="https://budibase.com/blog/updates/workspaces/"
+        color="var(--spectrum-global-color-gray-100)"
+        image="https://res.cloudinary.com/daog6scxm/image/upload/w_1200,h_800/v1628152378/1.%20Illustrations/Scene_4_web_version_izudxc.avif"
+      >
+        Previously, Budibase centered everything around building a single app.
+        With Workspaces, that changes. Now, you can group multiple apps,
+        automations, and data sources together within a single workspace.
+        Existing apps now have their own workspace.
+      </HeroBanner>
+    {/if}
     <slot />
   </div>
 {/if}
