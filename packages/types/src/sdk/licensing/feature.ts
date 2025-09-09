9a816ec3d1949489324f8efcbbef0e12d1e19da5
Adria Navarro <adria@budibase.com>
2025-09-09 16:30:47 +0200

    Rename features

--- packages/types/src/sdk/licensing/feature.ts
+++ packages/types/src/sdk/licensing/feature.ts
@@ -2,7 +2,7 @@ import { PlanType } from "./plan"
 
 export enum Feature {
   USER_GROUPS = "userGroups",
-  APP_BACKUPS = "appBackups",
+  WORKSPACE_BACKUPS = "appBackups",
   ENVIRONMENT_VARIABLES = "environmentVariables",
   AUDIT_LOGS = "auditLogs",
   ENFORCEABLE_SSO = "enforceableSSO",
