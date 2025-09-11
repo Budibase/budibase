0f8c666aaabb9d61bf2f2a4d17bbf89c96b64e00
Dean <deanhannigan@gmail.com>
2025-09-11 16:37:39 +0100

    Lint

--- packages/builder/src/pages/builder/workspace/[application]/_layout.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/_layout.svelte
@@ -1,19 +1,7 @@
 <script>
-  import { onMount } from "svelte"
   import WorkspaceLayout from "./_layout.workspace.svelte"
-  import { params } from "@roxi/routify"
 
   export let application
-  let currentAppId = null
-
-  $: if (currentAppId !== application) {
-    console.log({ application, currentAppId, para: $params.application })
-    currentAppId = application
-  }
-
-  onMount(() => {
-    console.log("_layout mount")
-  })
 </script>
 
 <!-- Needs to agressively re-render if the appId has changed -->
