c0e3c31775153f2cda77afb3338684cbcb909173
Adria Navarro <adria@budibase.com>
2025-09-17 10:38:39 +0200

    Check workspaceId correclty for builderOnly

--- packages/backend-core/src/middleware/builderOnly.ts
+++ packages/backend-core/src/middleware/builderOnly.ts
@@ -1,25 +1,18 @@
 import { UserCtx } from "@budibase/types"
-import { getWorkspaceId } from "../context"
-import env from "../environment"
 import { hasBuilderPermissions, isBuilder } from "../users"
+import { getAppIdFromCtx } from "../utils"
 
-type BuilderFn = (user: UserCtx["user"], appId?: string) => boolean
 export async function builderOnly(ctx: UserCtx, next: any) {
-  const appId = getWorkspaceId()
-
-  let builderFn: BuilderFn | undefined
-  if (env.isWorker() || !appId) {
-    builderFn = hasBuilderPermissions
-  } else if (env.isApps()) {
-    builderFn = isBuilder
+  if (ctx.internal) {
+    return next()
   }
 
-  if (!builderFn) {
-    throw new Error("Service name unknown - middleware inactive.")
-  }
-
-  if (!ctx.internal && !builderFn(ctx.user, appId)) {
+  const workspaceId = await getAppIdFromCtx(ctx)
+  if (!workspaceId && !hasBuilderPermissions(ctx.user)) {
+    ctx.throw(403, "Builder user only endpoint.")
+  } else if (!isBuilder(ctx.user, workspaceId)) {
     ctx.throw(403, "Builder user only endpoint.")
   }
+
   return next()
 }
--- packages/backend-core/src/middleware/builderOrAdmin.ts
+++ packages/backend-core/src/middleware/builderOrAdmin.ts
@@ -3,12 +3,11 @@ import { hasBuilderPermissions, isAdmin, isBuilder } from "../users"
 import { getAppIdFromCtx } from "../utils"
 
 export async function builderOrAdmin(ctx: UserCtx, next: any) {
-  const workspaceId = await getAppIdFromCtx(ctx)
-
   if (ctx.internal || isAdmin(ctx.user)) {
     return next()
   }
 
+  const workspaceId = await getAppIdFromCtx(ctx)
   if (!workspaceId && !hasBuilderPermissions(ctx.user)) {
     ctx.throw(403, "Admin/Builder user only endpoint.")
   } else if (!isBuilder(ctx.user, workspaceId)) {
