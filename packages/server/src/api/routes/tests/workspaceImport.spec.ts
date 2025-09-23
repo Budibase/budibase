9d9690a7df5b2e1f372c1b1d61855f76c4669dc2
Adria Navarro <adria@budibase.com>
2025-09-23 21:09:30 +0200

    Test that apps are imported

--- packages/server/src/api/routes/tests/workspaceImport.spec.ts
+++ packages/server/src/api/routes/tests/workspaceImport.spec.ts
@@ -34,10 +34,15 @@ describe("/applications/:appId/import", () => {
     expect(appPackage.navigation?.links?.length).toBe(2)
     expect(appPackage.navigation?.links?.[0].url).toBe("/blank")
     expect(appPackage.navigation?.links?.[1].url).toBe("/derp")
+
     const screens = await config.api.screen.list()
     expect(screens.length).toBe(2)
     expect(screens[0].routing.route).toBe("/derp")
     expect(screens[1].routing.route).toBe("/blank")
+
+    const { workspaceApps: apps } = await config.api.workspaceApp.fetch()
+    expect(apps.length).toBe(1)
+    expect(apps[0].name).toBe(config.getDevWorkspace().name)
 
     const fileEtags = await getAppObjectStorageEtags(appId)
     expect(fileEtags).toEqual({
@@ -62,6 +67,7 @@ describe("/applications/:appId/import", () => {
       "/review-change-requests"
     )
     expect(appPackage.navigation?.links?.[2].url).toBe("/administration")
+
     const screens = await config.api.screen.list()
     expect(screens.length).toBe(6)
     expect(screens[0].routing.route).toBe("/my-change-requests")
@@ -70,6 +76,10 @@ describe("/applications/:appId/import", () => {
     expect(screens[3].routing.route).toBe("/my-change-requests/edit/:id")
     expect(screens[4].routing.route).toBe("/administration/:id")
     expect(screens[5].routing.route).toBe("/change-request/new")
+
+    const { workspaceApps: apps } = await config.api.workspaceApp.fetch()
+    expect(apps.length).toBe(1)
+    expect(apps[0].name).toBe("Change request management")
 
     const fileEtags = await getAppObjectStorageEtags(appId)
     expect(fileEtags).toEqual({
