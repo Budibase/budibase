51be3445a9983c8f9f70661add7b485dd9c3af52
Adria Navarro <adria@budibase.com>
2025-09-23 21:56:32 +0200

    DRY

--- packages/server/src/tests/utilities/objectStore.ts
+++ packages/server/src/tests/utilities/objectStore.ts
@@ -1,35 +1,20 @@
-import { _Object } from "@aws-sdk/client-s3"
 import { db, objectStore } from "@budibase/backend-core"
-import { utils } from "@budibase/shared-core"
-
-export async function getAppObjectStorageContent(appId: string) {
-  appId = db.getProdWorkspaceID(appId)
-
-  const objects: Record<string, _Object> = {}
-  await utils.parallelForeach(
-    objectStore.listAllObjects(objectStore.ObjectStoreBuckets.APPS, appId),
-    async file => {
-      if (!file.Key) {
-        throw new Error("file.Key must be defined")
-      }
-
-      objects[file.Key.replace(new RegExp(`^${appId}/`), "")] = file
-    },
-    5
-  )
-  return objects
-}
 
 export async function getAppObjectStorageEtags(appId: string) {
-  const objects = await getAppObjectStorageContent(appId)
+  appId = db.getProdWorkspaceID(appId)
+
+  const objects = await objectStore.getAllFiles(
+    objectStore.ObjectStoreBuckets.APPS,
+    appId
+  )
 
   const fileEtags = Object.entries(objects).reduce<Record<string, string>>(
     (etags, [key, object]) => {
       if (object.ETag) {
-        etags[key] = object.ETag.replace(new RegExp('^"'), "").replace(
-          new RegExp('"$'),
+        etags[key.replace(new RegExp(`^${appId}/`), "")] = object.ETag.replace(
+          new RegExp('^"'),
           ""
-        )
+        ).replace(new RegExp('"$'), "")
       }
       return etags
     },
