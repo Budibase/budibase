993bbfbc7f36fadd2816323abc0f6ddefe8e852c
Adria Navarro <adria@budibase.com>
2025-09-24 18:19:33 +0200

    More messaging

--- packages/builder/src/pages/builder/workspace/[application]/_components/CloneResourceModal.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/_components/CloneResourceModal.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { API } from "@/api"
-  import { Modal, ModalContent } from "@budibase/bbui"
+  import { appStore } from "@/stores/builder"
+  import { appsStore } from "@/stores/portal/apps"
+  import { Modal, ModalContent, Select } from "@budibase/bbui"
+  import { sdk } from "@budibase/shared-core"
   import type { UsedResource } from "@budibase/types"
 
   export let resource: { id: string; name: string; type: "app" | "automation" }
@@ -21,14 +24,32 @@
     .then(res => {
       usedResource = res.resources
     })
+
+  $: workspaces = $appsStore.apps.filter(
+    a => a.devId !== sdk.applications.getDevAppID($appStore.appId)
+  )
 </script>
 
 <Modal bind:this={modal} on:show={onShow} on:hide>
   <ModalContent title={resource.name} {onConfirm} size="M">
-    {#if usedResource}
-      {#each usedResource as resource}
-        {resource.name}
-      {/each}
+    {#if !workspaces.length}
+      You don't have access to any other workspace
+    {:else}
+      Select the destination workspace:
+      <Select
+        options={workspaces}
+        getOptionLabel={w => w.name.trim()}
+        getOptionValue={w => w.appId}
+        getOptionIcon={() => undefined}
+      ></Select>
+
+      Are you sure to copy this {resource.type} to the new workspace?
+
+      {#if usedResource?.length}
+        {#each usedResource as resource}
+          <div>{resource.name}</div>
+        {/each}
+      {/if}
     {/if}
   </ModalContent>
 </Modal>
