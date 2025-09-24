32b77d3982d11ba7b6f9f3fb0c788392fcaf21de
Adria Navarro <adria@budibase.com>
2025-09-24 22:52:33 +0200

    Display

--- packages/builder/src/pages/builder/workspace/[application]/_components/CloneResourceModal.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/_components/CloneResourceModal.svelte
@@ -5,6 +5,7 @@
   import { Modal, ModalContent, Select } from "@budibase/bbui"
   import { sdk } from "@budibase/shared-core"
   import type { UsedResource } from "@budibase/types"
+  import { ResourceType } from "@budibase/types"
 
   export let resource: { id: string; name: string; type: "app" | "automation" }
 
@@ -15,7 +16,7 @@
 
   async function onConfirm() {}
 
-  let usedResource: UsedResource[] | undefined
+  let usedResource: UsedResource[] = []
   $: API.resource
     .searchForUsage({
       workspaceAppIds: resource.type === "app" ? [resource.id] : undefined,
@@ -25,9 +26,29 @@
       usedResource = res.resources
     })
 
+  $: usedResourceByType = usedResource?.reduce<
+    Partial<Record<ResourceType, UsedResource[]>>
+  >((acc, resource) => {
+    acc[resource.type] ??= []
+    acc[resource.type]!.push(resource)
+    return acc
+  }, {})
+
   $: workspaces = $appsStore.apps.filter(
     a => a.devId !== sdk.applications.getDevAppID($appStore.appId)
   )
+
+  function getFriendlyName(type: string): string {
+    const resourceTypeFriendlyName: Record<ResourceType, string> = {
+      [ResourceType.DATASOURCE]: "Datasources",
+      [ResourceType.TABLE]: "Tables",
+      [ResourceType.ROW_ACTION]: "Row actions",
+      [ResourceType.QUERY]: "Queries",
+      [ResourceType.AUTOMATION]: "Automations",
+    }
+
+    return resourceTypeFriendlyName[type as ResourceType]
+  }
 </script>
 
 <Modal bind:this={modal} on:show={onShow} on:hide>
@@ -46,8 +67,16 @@
       Are you sure to copy this {resource.type} to the new workspace?
 
       {#if usedResource?.length}
-        {#each usedResource as resource}
-          <div>{resource.name}</div>
+        The following resources will be copied:
+        {#each Object.entries(usedResourceByType) as [type, resources]}
+          <div>
+            <div>
+              {getFriendlyName(type)}
+            </div>
+            {#each resources as resource}
+              <div>- {resource.name}</div>
+            {/each}
+          </div>
         {/each}
       {/if}
     {/if}
