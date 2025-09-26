293af6398a05b14fbd17db249fc4f9f64b1fa43a
Adria Navarro <adria@budibase.com>
2025-09-26 10:34:03 +0200

    Use dependency directly

--- packages/client/src/utils/dependencies.ts
+++
@@ -1,8 +0,0 @@
-// These helpers rely on native dynamic imports so the ESM bundle
-// can split vendor code into separate chunks and fetch them on demand.
-// Because Rollup inlines dynamic imports for the legacy IIFE build,
-// this continues to work there (the code is simply included upfront).
-export const loadCharts = async () => (await import("apexcharts")).default
-
-export const loadQRCode = async () =>
-  (await import("html5-qrcode")).Html5Qrcode
