a611a79b3c906ee87fb6b14b84f4042ee032a7d2
Dom Barker <dom@dombarker.co.uk>
2025-10-13 19:46:08 +0100

    fix: specs
    fix: specs

--- packages/server/src/utilities/rowProcessor/map.ts
+++ packages/server/src/utilities/rowProcessor/map.ts
@@ -183,25 +183,8 @@ export const TYPE_TRANSFORM_MAP: Record<
       }
     },
   },
-  [FieldType.AI]: {
-    parse: () => {
-      throw new Error(`FieldType.AI parse is not implemented`)
-    },
-  },
-
-  [FieldType.BB_REFERENCE_SINGLE]: {
-    parse: () => {
-      throw new Error(`FieldType.BB_REFERENCE_SINGLE parse is not implemented`)
-    },
-  },
-  [FieldType.SIGNATURE_SINGLE]: {
-    parse: () => {
-      throw new Error(`FieldType.SIGNATURE_SINGLE #parse is not implemented`)
-    },
-  },
-  [FieldType.INTERNAL]: {
-    parse: () => {
-      throw new Error(`FieldType.INTERNAL is deprecated`)
-    },
-  },
+  [FieldType.AI]: {},
+  [FieldType.BB_REFERENCE_SINGLE]: {},
+  [FieldType.SIGNATURE_SINGLE]: {},
+  [FieldType.INTERNAL]: {},
 }
