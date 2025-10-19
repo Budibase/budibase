b1a7cc42ded2548f49d123258d84ea5486ecf071
Ashish Musmade <ashishmusmade45@gmail.com>
2025-10-19 16:19:59 +0530

    fix: remove unused allSelected property (resolved merge conflict)

--- packages/bbui/src/Form/Core/Picker.svelte
+++ packages/bbui/src/Form/Core/Picker.svelte
@@ -66,7 +66,6 @@
   ) => void = () => {}
   export let showSelectAll: boolean = false
   export let selectAllText: string = "Select all"
-  export let allSelected: boolean = false
   export let indeterminate: boolean = false
   export let toggleSelectAll: () => void = () => {}
 
