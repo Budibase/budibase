0406f280eabbb7732645b716a8bcd2e5919f736a
deanhannigan <deanhannigan@gmail.com>
2025-10-23 20:23:11 +0100

    Revert "Portal refactor"

--- packages/builder/src/pages/builder/workspace/[application]/index.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/index.svelte
@@ -1,12 +1,11 @@
 <script>
   import { redirect } from "@roxi/routify"
-  import { screenStore, appStore } from "@/stores/builder"
+  import { screenStore } from "@/stores/builder"
   import { onMount } from "svelte"
 
   onMount(() => {
     const { screens } = $screenStore
-    const { appId } = $appStore
-    if (!appId) return
+
     if (screens && screens.length > 0) {
       $redirect("./design")
     } else {
