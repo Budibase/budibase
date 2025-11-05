74f9ce05a97ebad4772f4c55500c828217e6627f
Mel O'Hagan <mel@budibase.com>
2025-11-05 17:08:24 +0000

    Fix openapi yaml spec import

--- packages/server/src/api/controllers/query/import/sources/base/openapi.ts
+++ packages/server/src/api/controllers/query/import/sources/base/openapi.ts
@@ -1,25 +1,52 @@
-import { ImportSource } from "."
 import SwaggerParser from "@apidevtools/swagger-parser"
+import { load as loadYaml } from "js-yaml"
+import isObject from "lodash/isObject"
 import { OpenAPI } from "openapi-types"
+import { ImportSource } from "."
 
-const yaml = require("js-yaml")
+const isYamlDocument = (loaded: unknown): loaded is OpenAPI.Document => {
+  if (isObject(loaded)) {
+    return true
+  }
+  return false
+}
+
+const prepareDocument = (raw: string): string | OpenAPI.Document => {
+  const trimmed = raw.trim()
+  if (!trimmed) {
+    throw new Error("Empty OpenAPI document")
+  }
+
+  try {
+    const yamlDocument = loadYaml(trimmed)
+    if (isYamlDocument(yamlDocument)) {
+      return yamlDocument
+    }
+  } catch (err) {
+    // fall through to allow swagger parser to attempt parsing
+  }
+
+  return raw
+}
 
 export abstract class OpenAPISource extends ImportSource {
   parseData = async (data: string): Promise<OpenAPI.Document> => {
-    let json: OpenAPI.Document
-    try {
-      json = JSON.parse(data)
-    } catch (jsonErr) {
-      // couldn't parse json
-      // try to convert yaml -> json
-      try {
-        json = yaml.load(data)
-      } catch (yamlErr) {
-        // couldn't parse yaml
-        throw new Error("Could not parse JSON or YAML")
-      }
+    const baseOptions = {
+      resolve: {
+        external: false,
+      },
     }
 
-    return SwaggerParser.validate(json, {})
+    const parsedInput = prepareDocument(data)
+    const document = await SwaggerParser.parse(parsedInput, baseOptions)
+
+    try {
+      return await SwaggerParser.validate(document)
+    } catch (err) {
+      console.log(
+        `[OpenAPI Import] Schema validation failed, continuing without validation`
+      )
+      return await SwaggerParser.dereference(document, baseOptions)
+    }
   }
 }
