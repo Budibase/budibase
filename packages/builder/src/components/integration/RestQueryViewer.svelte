9e00f606f9885c0deba1ea1f9f973116787f4977
Mel O'Hagan <mel@budibase.com>
2025-11-07 14:20:52 +0000

    show the static var value binding preview

--- packages/builder/src/components/integration/RestQueryViewer.svelte
+++ packages/builder/src/components/integration/RestQueryViewer.svelte
@@ -59,7 +59,7 @@
     requestBindings = {}
   let saveId
   let response, schema, enabledHeaders
-  let dynamicVariables, addVariableModal, varBinding, globalDynamicBindings
+  let dynamicVariables = {}, addVariableModal, varBinding, globalDynamicBindings = {}
   let restBindings = getRestBindings()
   let nestedSchemaFields = {}
   let saving
@@ -84,12 +84,23 @@
     "Datasource Static"
   )
 
+  const getBindingContext = objects => {
+    return objects.reduce((acc, current) => ({ ...acc, ...(current || {}) }), {})
+  }
+
   $: mergedBindings = [
+    ...dataSourceStaticBindings,
     ...restBindings,
     ...customRequestBindings,
     ...globalDynamicRequestBindings,
-    ...dataSourceStaticBindings,
   ]
+
+  $: bindingPreviewContext = getBindingContext([
+    requestBindings,
+    globalDynamicBindings,
+    dynamicVariables,
+    staticVariables,
+  ])
 
   $: datasourceType = datasource?.source
   $: integrationInfo = $integrations[datasourceType]
@@ -378,10 +389,10 @@
     )
 
     const prettyBindings = [
+      ...dataSourceStaticBindings,
       ...restBindings,
       ...customRequestBindings,
       ...dynamicRequestBindings,
-      ...dataSourceStaticBindings,
     ]
 
     //Parse the body here as now all bindings have been updated.
@@ -473,7 +484,7 @@
     }
     // if query doesn't have ID then its new - don't try to copy existing dynamic variables
     if (!queryId) {
-      dynamicVariables = []
+      dynamicVariables = {}
       globalDynamicBindings = getDynamicVariables(datasource)
     } else {
       dynamicVariables = getDynamicVariables(
@@ -599,10 +610,11 @@
               keyPlaceholder="Binding name"
               valuePlaceholder="Default"
               bindings={[
+                ...dataSourceStaticBindings,
                 ...restBindings,
                 ...globalDynamicRequestBindings,
-                ...dataSourceStaticBindings,
               ]}
+              context={bindingPreviewContext}
             />
           </Tab>
           <Tab title="Params">
@@ -612,6 +624,7 @@
                 defaults={breakQs}
                 headings
                 bindings={mergedBindings}
+                context={bindingPreviewContext}
                 on:change={e => {
                   let newQs = {}
                   e.detail.forEach(({ name, value }) => {
@@ -631,6 +644,7 @@
               name="header"
               headings
               bindings={mergedBindings}
+              context={bindingPreviewContext}
             />
           </Tab>
           <Tab title="Body">
