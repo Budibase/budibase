236b60e3885d5755e2162898ce117e42111acebd
Peter Clement <peter@budibase.com>
2025-12-03 10:54:18 +0000

    lint

--- packages/builder/src/pages/builder/workspace/[application]/agent/[agentId]/config.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/agent/[agentId]/config.svelte
@@ -181,7 +181,6 @@
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-
 <div class="config-wrapper">
   <TopBar
     breadcrumbs={[
