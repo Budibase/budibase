9c2dc2adaefaf4a322137a00ab1adf94ed503d4a
Peter Clement <peter@budibase.com>
2025-12-29 09:00:40 +0000

    Fix issue with external datasources dev / prod switching

--- packages/builder/src/components/backend/DataTable/buttons/grid/GridDevProdSwitcher.svelte
+++ packages/builder/src/components/backend/DataTable/buttons/grid/GridDevProdSwitcher.svelte
@@ -1,72 +1,60 @@
 <script lang="ts">
   import { Switcher, AbsTooltip, TooltipPosition } from "@budibase/bbui"
-  import { DataEnvironmentMode } from "@budibase/types"
   import {
     dataEnvironmentStore,
     workspaceDeploymentStore,
     tables,
+    datasources,
   } from "@/stores/builder"
-  import { TableNames } from "@/constants"
+  import { DataEnvironmentMode } from "@budibase/types"
   import { DB_TYPE_EXTERNAL } from "@/constants/backend"
 
   $: isDevMode = $dataEnvironmentStore.mode === DataEnvironmentMode.DEVELOPMENT
   $: tableId = $tables.selected?._id!
-  $: isBudiUsersTable = tableId === TableNames.USERS
   $: isInternal = $tables.selected?.sourceType !== DB_TYPE_EXTERNAL
   $: isDeployed =
     isInternal && $workspaceDeploymentStore.tables[tableId]?.published
-  $: hasProductionData = isInternal ? isDeployed : true
-  $: switcherDisabled = !isInternal
-  $: tooltip = isInternal
-    ? "Publish to view production data"
-    : "Dev data is only available for internal tables"
-  $: showTooltip = switcherDisabled || !hasProductionData
-  $: hideSwitcher = isBudiUsersTable
+  $: tableDatasource = $datasources.list.find(datasource => {
+    return datasource._id === $tables.selected?.sourceId
+  })
+  $: disabled = !isDeployed && !tableDatasource?.usesEnvironmentVariables
+  $: tooltip =
+    isInternal && !isDeployed
+      ? "Please publish to view production"
+      : "No production environment variables"
 
   $: switcherProps = {
     leftIcon: "wrench",
     leftText: "Dev",
     rightIcon: "pulse",
     rightText: "Prod",
-    selected: (isDevMode ? "left" : "right") as "left" | "right",
-    disabled: switcherDisabled,
-  }
-
-  $: if (isBudiUsersTable) {
-    dataEnvironmentStore.setMode(DataEnvironmentMode.DEVELOPMENT)
-  }
-
-  $: if (switcherDisabled && isDevMode && !isBudiUsersTable) {
-    dataEnvironmentStore.setMode(DataEnvironmentMode.PRODUCTION)
+    selected: (disabled && !isInternal
+      ? "right"
+      : isDevMode
+        ? "left"
+        : "right") as "left" | "right",
+    disabled,
   }
 
   const handleLeft = () =>
     dataEnvironmentStore.setMode(DataEnvironmentMode.DEVELOPMENT)
-  const handleRight = () => {
-    if (switcherDisabled) return
+  const handleRight = () =>
     dataEnvironmentStore.setMode(DataEnvironmentMode.PRODUCTION)
-  }
 </script>
 
-{#if !hideSwitcher}
-  <div class="wrapper">
-    {#if showTooltip}
-      <AbsTooltip text={tooltip} position={TooltipPosition.Left}>
-        <Switcher
-          {...switcherProps}
-          on:left={handleLeft}
-          on:right={handleRight}
-        />
-      </AbsTooltip>
-    {:else}
+<div class="wrapper">
+  {#if disabled}
+    <AbsTooltip text={tooltip} position={TooltipPosition.Left}>
       <Switcher
         {...switcherProps}
         on:left={handleLeft}
         on:right={handleRight}
       />
-    {/if}
-  </div>
-{/if}
+    </AbsTooltip>
+  {:else}
+    <Switcher {...switcherProps} on:left={handleLeft} on:right={handleRight} />
+  {/if}
+</div>
 
 <style>
   .wrapper {
