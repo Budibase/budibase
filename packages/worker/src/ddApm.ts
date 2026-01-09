7ee19089aeb35c10bf4e14a4eb6181b30d43a528
Peter Clement <peter@budibase.com>
2026-01-09 15:30:19 +0000

    we don't need the worker init

--- packages/worker/src/ddApm.ts
+++ packages/worker/src/ddApm.ts
@@ -3,9 +3,5 @@ import apm from "dd-trace"
 // enable APM if configured
 if (process.env.DD_APM_ENABLED) {
   console.log("Starting dd-trace")
-  apm.init({
-    llmobs: {
-      mlApp: process.env.DD_LLMOBS_ML_APP || "budibase",
-    },
-  })
+  apm.init()
 }
