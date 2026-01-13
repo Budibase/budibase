43555111d78f2425e4876990d1b53f4e2ac9a5b4
Adria Navarro <adria@budibase.com>
2026-01-13 17:13:04 +0100

    Caps at the largest unit

--- packages/shared-core/src/helpers/readableHelpers.ts
+++ packages/shared-core/src/helpers/readableHelpers.ts
@@ -3,7 +3,7 @@ export function formatBytes(bytes: string | number | undefined, spacer = "") {
   const byteIncrements = 1024
   let unit = 0
   let size = typeof bytes === "string" ? parseInt(bytes, 10) : bytes || 0
-  while (size >= byteIncrements && ++unit) {
+  while (size >= byteIncrements && unit < units.length - 1 && ++unit) {
     size /= byteIncrements
   }
   return `${size.toFixed(size < 10 && unit > 0 ? 1 : 0)}${spacer}${units[unit]}`
