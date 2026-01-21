7f9bc6bc1a091ab2c11dc7105c2fe709b7e97b42
mikesealey <mikesealey@hotmail.com>
2026-01-21 14:11:30 +0000

    establishes calendar

--- packages/client/src/components/app/Calendar.svelte
+++ packages/client/src/components/app/Calendar.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-  import { componentStore } from "@/stores"
   import { getContext } from "svelte"
+
+  import "@fullcalendar/core/locales-all"
+  import FullCalendar from "svelte-fullcalendar"
+  import dayGridPlugin from "@fullcalendar/daygrid"
+  import timeGridPlugin from "@fullcalendar/timegrid"
+  import listPlugin from "@fullcalendar/list"
 
   export let title: string
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
+
+  let options = {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: "dayGridMonth",
+  }
 </script>
 
 <div use:styleable={$component.styles}>
-  <h2>{title}</h2>
+  <FullCalendar {options} />
 </div>
