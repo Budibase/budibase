378f1b133ee379d1f94165720f225830967b9a2c
mikesealey <mikesealey@hotmail.com>
2026-01-21 11:59:50 +0000

    initial commit

---
+++ packages/client/src/components/app/Calendar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { componentStore } from "@/stores"
+  import { getContext } from "svelte"
+
+  export let title: string
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+</script>
+
+<div use:styleable={$component.styles}>
+  <h2>{title}</h2>
+</div>
