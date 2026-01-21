92c396cf80750bc80f17dfd4c37fbcbb3b47f55a
mikesealey <mikesealey@hotmail.com>
2026-01-21 17:07:08 +0000

    allows user to plot events on calendar

--- packages/client/src/components/app/Calendar.svelte
+++ packages/client/src/components/app/Calendar.svelte
@@ -6,15 +6,45 @@
   import dayGridPlugin from "@fullcalendar/daygrid"
   import timeGridPlugin from "@fullcalendar/timegrid"
   import listPlugin from "@fullcalendar/list"
+  import DataProvider from "./DataProvider.svelte"
 
   export let title: string
+  export let dataProvider: DataProvider
+  export let eventStart: string // can haz type?
+  export let eventEnd: string // can haz type?
+  export let eventTitle: string
+  export let eventDesc: string
+
+  console.log({ dataProvider, eventStart, eventEnd })
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 
+  const getEvents = () => {
+    return dataProvider.rows.map((row: any) => {
+      console.log({
+        title: row[eventTitle],
+        start: row[eventStart],
+        end: row[eventEnd],
+      })
+      return {
+        title: row[eventTitle],
+        start: row[eventStart],
+        end: row[eventEnd],
+      }
+    })
+  }
+
+  console.log(getEvents())
   let options = {
+    headerToolbar: {
+      start: "title",
+      center: "",
+      end: "prevYear,prev,next,nextYear",
+    },
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
     initialView: "dayGridMonth",
+    events: getEvents(),
   }
 </script>
 
