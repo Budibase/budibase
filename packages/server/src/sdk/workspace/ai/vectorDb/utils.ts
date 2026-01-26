b2e08d703cd1ec94b19ae2e77f1d43b45e30ab11
Adria Navarro <adria@budibase.com>
2026-01-26 12:13:52 +0100

    Lint

--- packages/server/src/sdk/workspace/ai/vectorDb/utils.ts
+++ packages/server/src/sdk/workspace/ai/vectorDb/utils.ts
@@ -1,6 +1,9 @@
 import type { VectorDb as VectorDbDoc } from "@budibase/types"
 import { buildPgVectorDbConfig, PgVectorDb } from "./pgVectorDb"
-import type { VectorDb as VectorDbClient, VectorDbRuntimeOptions } from "./types"
+import type {
+  VectorDb as VectorDbClient,
+  VectorDbRuntimeOptions,
+} from "./types"
 
 export const createVectorDb = (
   config: VectorDbDoc,
