b471b6a867e164c17a7373668e0733a2a1de5b69
mikesealey <mikesealey@hotmail.com>
2026-01-29 17:07:20 +0000

    passes in view text

--- packages/client/src/components/app/Calendar.svelte
+++ packages/client/src/components/app/Calendar.svelte
@@ -18,10 +18,10 @@
   export let onClick: any // Also can has type?
   export let showNavButtons: boolean
   export let showViewButtons: boolean
-  export let monthText: string
-  export let weekText: string
-  export let dayText: string
-  export let agendaText: string
+  export let monthText: string | "Month"
+  export let weekText: string | "Week"
+  export let dayText: string | "Day"
+  export let agendaText: string | "Agenda"
   export let calendarType:
     | string
     | ["dayGridMonth", "dayGridWeek", "timeGridDay", "listWeek"]
@@ -131,12 +131,23 @@
     font-size: 13px;
   }
 
+  .calendar :global(.fc-button-group .fc-button-primary) {
+    /* font-weight: var(--spectrum-button-primary-text-font-weight); */
+    font-weight: 600;
+    border: 2px solid var(--primaryColor, var(--spectrum-global-color-blue-600));
+    padding: 4 14 6 14;
+  }
+
   .calendar :global(.fc .fc-button:focus),
   .calendar :global(.fc .fc-button-primary:focus),
   .calendar
     :global(.fc .fc-button-primary:not(:disabled).fc-button-active:focus),
   .calendar :global(.fc .fc-button-primary:not(:disabled):active:focus) {
     box-shadow: none;
+    border-color: var(
+      --primaryColorHover,
+      var(--spectrum-global-color-blue-500)
+    );
   }
 
   .calendar :global(.fc-button-primary:disabled) {
