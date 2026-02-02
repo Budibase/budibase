297006b850091704e71b52bf5e57eb523c3fd5de
Adria Navarro <adria@budibase.com>
2026-02-02 16:48:00 +0100

    Use joi for validation

--- packages/server/src/api/controllers/ai/vectorDb.ts
+++ packages/server/src/api/controllers/ai/vectorDb.ts
@@ -1,4 +1,3 @@
-import { HTTPError } from "@budibase/backend-core"
 import {
   CreateVectorDbRequest,
   PASSWORD_REPLACEMENT,
@@ -6,7 +5,6 @@ import {
   UserCtx,
   VectorDb,
   VectorDbListResponse,
-  VectorDbProvider,
 } from "@budibase/types"
 import sdk from "../../../sdk"
 
@@ -31,16 +29,6 @@ export const createVectorDbConfig = async (
   ctx: UserCtx<CreateVectorDbRequest, VectorDb>
 ) => {
   const body = ctx.request.body
-  if (!body.name) {
-    throw new HTTPError("Config name is required", 400)
-  }
-  if (!body.provider) {
-    throw new HTTPError("Provider is required", 400)
-  }
-  if (!Object.values(VectorDbProvider).includes(body.provider)) {
-    throw new HTTPError(`${body.provider} is not supported`, 400)
-  }
-
   const created = await sdk.ai.vectorDb.create(body)
   ctx.body = sanitize(created)
   ctx.status = 201
@@ -50,15 +38,6 @@ export const updateVectorDbConfig = async (
   ctx: UserCtx<UpdateVectorDbRequest, VectorDb>
 ) => {
   const body = ctx.request.body
-  if (!body._id) {
-    throw new HTTPError("Config ID is required", 400)
-  }
-  if (!body._rev) {
-    throw new HTTPError("Revision is required", 400)
-  }
-  if (!Object.values(VectorDbProvider).includes(body.provider)) {
-    throw new HTTPError(`${body.provider} is not supported`, 400)
-  }
   const updated = await sdk.ai.vectorDb.update(body)
   ctx.body = sanitize(updated)
 }
@@ -67,9 +46,6 @@ export const deleteVectorDbConfig = async (
   ctx: UserCtx<void, { deleted: true }, { id: string }>
 ) => {
   const { id } = ctx.params
-  if (!id) {
-    throw new HTTPError("Config ID is required", 400)
-  }
   await sdk.ai.vectorDb.remove(id)
   ctx.body = { deleted: true }
 }
