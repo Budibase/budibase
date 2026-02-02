d91e3efd63d55c77dd4dfe732b1fc2a1ef012f09
Adria Navarro <adria@budibase.com>
2026-02-02 11:14:32 +0100

    Add vectordb provider as enum

--- packages/types/src/documents/global/vectorDb.ts
+++ packages/types/src/documents/global/vectorDb.ts
@@ -1,8 +1,12 @@
 import { Document } from "../.."
 
+export const enum VectorDbProvider {
+  PGVECTOR = "pgvector",
+}
+
 export interface VectorDb extends Document {
   name: string
-  provider: string
+  provider: VectorDbProvider
   host: string
   port: number
   database: string
