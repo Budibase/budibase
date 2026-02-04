1b12ffc59589b9dd030d7f2985cec650fc20265c
andz-bb <andrew.thompson@budibase.com>
2026-02-04 16:33:09 +0000

    use context.getWorkspaceId

--- packages/server/src/api/controllers/workspaceHome.ts
+++ packages/server/src/api/controllers/workspaceHome.ts
@@ -4,13 +4,15 @@ import {
 } from "@budibase/types"
 
 import { getWorkspaceHomeMetrics } from "../../services/workspaceHome/metrics"
+import { context } from "@budibase/backend-core"
 
 export async function metrics(
   ctx: UserCtx<void, GetWorkspaceHomeMetricsResponse>
 ) {
-  if (!ctx.appId) {
-    ctx.throw(400, "Missing appId")
+  const workspaceId = context.getWorkspaceId()
+  if (!workspaceId) {
+    ctx.throw(400, "Missing workspace context")
   }
 
-  ctx.body = await getWorkspaceHomeMetrics(ctx.appId)
+  ctx.body = await getWorkspaceHomeMetrics(workspaceId)
 }
