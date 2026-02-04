3bc8d84d7a615cba9dc6bbd5801e08f90dd9e58c
Mel O'Hagan <mel@budibase.com>
2026-02-04 17:23:00 +0000

    Fix lucene sort param typing

--- packages/backend-core/src/db/lucene.ts
+++ packages/backend-core/src/db/lucene.ts
@@ -91,9 +91,22 @@ export class QueryBuilder<T> {
     return this
   }
 
-  setSort(sort?: string) {
-    if (sort != null) {
+  setSort(sort?: SearchParams["sort"]) {
+    if (sort == null) {
+      return this
+    }
+    if (typeof sort === "string") {
       this.#sort = sort
+      return this
+    }
+    const [field, sortInfo] = Object.entries(sort)[0] || []
+    if (!field || !sortInfo) {
+      return this
+    }
+    this.#sort = field
+    this.#sortOrder = sortInfo.direction
+    if (sortInfo.type) {
+      this.#sortType = sortInfo.type
     }
     return this
   }
