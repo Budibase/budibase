b72877b2e6dc521e4a4f10364c8f44ed232c1d15
Adria Navarro <adria@budibase.com>
2026-02-09 19:51:17 +0100

    Display license modal in BBAI config

--- packages/builder/src/settings/pages/ai/configs.svelte
+++ packages/builder/src/settings/pages/ai/configs.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { aiConfigsStore } from "@/stores/portal"
+  import { admin, aiConfigsStore, licensing } from "@/stores/portal"
   import { Button, Layout, Modal, notifications } from "@budibase/bbui"
   import type { AIConfigResponse } from "@budibase/types"
   import { AIConfigType, BUDIBASE_AI_PROVIDER_ID } from "@budibase/types"
@@ -7,10 +7,15 @@
   import CustomAIConfigTile from "./CustomAIConfigTile.svelte"
   import CustomConfigModal from "./CustomConfigModal.svelte"
   import BBAIConfigModal from "./BBAIConfigModal.svelte"
+  import PortalModal from "./PortalModal.svelte"
+  import { API } from "@/api"
 
   let configModal: { show: () => void; hide: () => void }
+  let portalModal: { show: () => void; hide: () => void }
   let selectedModalConfig: AIConfigResponse | undefined
   let selectedProvider: string | undefined
+
+  let hasLicenseKey: boolean
 
   $: completionConfigs = ($aiConfigsStore.customConfigs || []).filter(
     config => config.configType === AIConfigType.COMPLETIONS
@@ -62,6 +67,15 @@
   ]
 
   function createAIConfig(provider?: string) {
+    if (
+      provider === BUDIBASE_AI_PROVIDER_ID &&
+      !$admin.cloud &&
+      !hasLicenseKey
+    ) {
+      portalModal.show()
+      return
+    }
+
     selectedModalConfig = undefined
     selectedProvider = provider
     configModal?.show()
@@ -75,6 +89,15 @@
   onMount(async () => {
     try {
       await aiConfigsStore.fetch()
+
+      const license = $licensing.license
+      const isOfflineLicense = () => license && "identifier" in license
+      if (isOfflineLicense()) {
+        hasLicenseKey = true
+      } else {
+        const licenseKeyResponse = await API.getLicenseKey()
+        hasLicenseKey = !!licenseKeyResponse?.licenseKey
+      }
     } catch {
       notifications.error("Error fetching AI settings")
     }
@@ -138,6 +161,16 @@
       }}
     />
   {/if}
+</Modal>
+
+<Modal bind:this={portalModal}>
+  <PortalModal
+    confirmHandler={() => {
+      window.open($admin.accountPortalUrl, "_blank")
+      portalModal.hide()
+    }}
+    cancelHandler={() => portalModal.hide()}
+  />
 </Modal>
 
 <style>
