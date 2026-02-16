0dcec2c8d589da02b06ba609d06368b88246ecc5
Adria Navarro <adria@budibase.com>
2026-02-16 15:27:58 +0100

    Move bbai checks away from the client

--- packages/builder/src/pages/builder/workspace/[application]/data/table/[tableId]/index.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/data/table/[tableId]/index.svelte
@@ -12,7 +12,7 @@
     deploymentStore,
     workspaceDeploymentStore,
   } from "@/stores/builder"
-  import { themeStore, admin, licensing } from "@/stores/portal"
+  import { themeStore, admin, aiStore } from "@/stores/portal"
   import { TableNames } from "@/constants"
   import { Grid, gridClipboard } from "@budibase/frontend-core"
   import type { Store as GridStore } from "@budibase/frontend-core/src/components/grid/stores"
@@ -328,7 +328,7 @@
         schemaOverrides={isUsersTable ? userSchemaOverrides : null}
         showAvatars={false}
         isCloud={$admin.cloud}
-        aiEnabled={$licensing.budibaseAIEnabled}
+        aiEnabled={$aiStore.aiEnabled}
         {buttons}
         buttonsCollapsed
         canHideColumns={false}
