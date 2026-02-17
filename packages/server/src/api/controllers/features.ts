04b022af66e6836b9beb89c700344bbae7c5ab0a
Adria Navarro <adria@budibase.com>
2026-02-17 15:06:07 +0100

    Add getflags endpoint

--- packages/server/src/api/controllers/features.ts
+++ packages/server/src/api/controllers/features.ts
@@ -1,9 +1,11 @@
 import {
+  Ctx,
+  IdentityType,
   UserCtx,
   OverrideFeatureFlagRequest,
   FeatureFlagCookie,
 } from "@budibase/types"
-import { Cookie, utils } from "@budibase/backend-core"
+import { context, Cookie, features, utils } from "@budibase/backend-core"
 
 export async function override(ctx: UserCtx<OverrideFeatureFlagRequest, void>) {
   const { flags } = ctx.request.body
@@ -23,4 +25,36 @@ export async function override(ctx: UserCtx<OverrideFeatureFlagRequest, void>) {
   utils.setCookie(ctx, cookie, Cookie.FeatureFlags)
 
   ctx.status = 200
+}
+
+interface GetCloudFeatureFlagsRequest {
+  userId?: string
+}
+
+interface GetCloudFeatureFlagsResponse {
+  flags: Record<string, boolean>
+}
+
+export async function getCloudFlags(
+  ctx: Ctx<GetCloudFeatureFlagsRequest, GetCloudFeatureFlagsResponse>
+) {
+  const { userId } = ctx.request.body
+  const tenantId = context.getTenantId()
+
+  const flagValues = userId
+    ? await context.doInIdentityContext(
+        {
+          _id: userId,
+          type: IdentityType.USER,
+          tenantId,
+        },
+        async () => {
+          return await features.all()
+        }
+      )
+    : await features.all()
+
+  ctx.body = {
+    flags: flagValues,
+  }
 }
--- packages/server/src/api/routes/features.ts
+++ packages/server/src/api/routes/features.ts
@@ -1,14 +1,25 @@
 import * as controller from "../controllers/features"
 import { validateBody } from "../../middleware/zod-validator"
-import { publicRoutes } from "./endpointGroups"
+import { endpointGroupList, publicRoutes } from "./endpointGroups"
+import { middleware } from "@budibase/pro"
 import { z } from "zod"
 
 const validator = z.object({
   flags: z.record(z.string(), z.boolean()),
 })
+const cloudFlagsValidator = z.object({
+  userId: z.string().optional(),
+})
+const licensedRoutes = endpointGroupList.group(middleware.licenseAuth)
 
 publicRoutes.patch(
   "/api/features",
   validateBody(validator),
   controller.override
+)
+
+licensedRoutes.post(
+  "/api/feature-flags",
+  validateBody(cloudFlagsValidator),
+  controller.getCloudFlags
 )
