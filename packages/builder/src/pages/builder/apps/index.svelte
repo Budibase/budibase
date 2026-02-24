dfa22d7676ab9256b90a1128b51f896991136d18
Dom Barker <dom@dombarker.co.uk>
2026-02-24 14:24:56 +0000

    fix: undo unrequired change

--- packages/builder/src/pages/builder/apps/index.svelte
+++ packages/builder/src/pages/builder/apps/index.svelte
@@ -209,7 +209,7 @@
                   {#if !chatAppsLoaded}
                     <Body size="S">Loading chat apps...</Body>
                   {:else if liveChatApps.length}
-                    {#each liveChatApps as chatApp (`${chatApp.chatAppId}_${chatApp.agentId || "chat"}`)}
+                    {#each liveChatApps as chatApp (chatApp.chatAppId)}
                       <a
                         class="app"
                         target="_blank"
