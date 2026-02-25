716490186ee32b2696705bf277dd566ca70443d5
Mel O'Hagan <mel@budibase.com>
2026-02-25 11:59:01 +0000

    Fix group user pageSize parsing and show remove-user spinner

--- packages/builder/src/settings/pages/people/groups/_components/RemoveUserTableRenderer.svelte
+++ packages/builder/src/settings/pages/people/groups/_components/RemoveUserTableRenderer.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { ActionButton } from "@budibase/bbui"
+  import { ActionButton, ProgressCircle } from "@budibase/bbui"
   import { getContext } from "svelte"
   import { auth } from "@/stores/portal/auth"
   import { sdk } from "@budibase/shared-core"
@@ -8,19 +8,44 @@
   export let row
 
   const userContext = getContext("users")
+  let removing = false
 
-  const onClick = e => {
+  const onClick = async e => {
     e.stopPropagation()
-    userContext.removeUser(value)
+    if (removing) {
+      return
+    }
+    removing = true
+    try {
+      await userContext.removeUser(value)
+    } finally {
+      removing = false
+    }
   }
 </script>
 
 {#if !row?.__skeleton}
-  <ActionButton
-    disabled={!sdk.users.isAdmin($auth.user)}
-    size="S"
-    on:click={onClick}
-  >
-    Remove
-  </ActionButton>
+  {#if removing}
+    <div class="spinner-wrap">
+      <ProgressCircle size="S" />
+    </div>
+  {:else}
+    <ActionButton
+      disabled={!sdk.users.isAdmin($auth.user)}
+      size="S"
+      on:click={onClick}
+    >
+      Remove
+    </ActionButton>
+  {/if}
 {/if}
+
+<style>
+  .spinner-wrap {
+    width: 72px;
+    height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
