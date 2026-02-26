fe3895535267b24958339869774b01b967492b1a
Adrià Navarro Redó <adria@budibase.com>
2026-02-26 14:39:23 +0100

    Migrate openai step

--- packages/server/src/automations/steps/openai.ts
+++ packages/server/src/automations/steps/openai.ts
@@ -1,31 +1,6 @@
-import { OpenAI } from "openai"
-
 import { OpenAIStepInputs, OpenAIStepOutputs } from "@budibase/types"
-import { env } from "@budibase/backend-core"
 import * as automationUtils from "../automationUtils"
-import { ai } from "@budibase/pro"
-
-/**
- * Maintains backward compatibility with automation steps created before the introduction
- * of custom configurations and Budibase AI
- * @param inputs - automation inputs from the OpenAI automation step.
- */
-async function legacyOpenAIPrompt(inputs: OpenAIStepInputs) {
-  const openai = new OpenAI({
-    apiKey: env.OPENAI_API_KEY,
-  })
-
-  const completion = await openai.chat.completions.create({
-    model: inputs.model,
-    messages: [
-      {
-        role: "user",
-        content: inputs.prompt,
-      },
-    ],
-  })
-  return completion?.choices[0]?.message?.content
-}
+import { promptWithDefaultLLM } from "./ai/llm"
 
 export async function run({
   inputs,
@@ -40,12 +15,12 @@ export async function run({
   }
 
   try {
-    let response
-    const llm = await ai.getLLM({ model: inputs.model })
-    response = llm
-      ? (await llm.prompt(inputs.prompt)).message
-      : await legacyOpenAIPrompt(inputs)
-
+    const response = await promptWithDefaultLLM([
+      {
+        role: "user",
+        content: inputs.prompt,
+      },
+    ])
     return {
       response,
       success: true,
