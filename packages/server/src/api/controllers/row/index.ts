af6c870fda15e8345e53763066395341e68ac42f
jvcalderon <jose@budibase.com>
2026-02-26 13:33:19 +0100

    Unnecessary ternary

--- packages/server/src/api/controllers/row/index.ts
+++ packages/server/src/api/controllers/row/index.ts
@@ -121,11 +121,9 @@ async function _save(
 
   // if it has an ID already then it's a patch
   if (body && body._id) {
-    return isAutomation
-      ? patch(ctx as UserCtx<PatchRowRequest, PatchRowResponse>, {
-          isAutomation: true,
-        })
-      : patch(ctx as UserCtx<PatchRowRequest, PatchRowResponse>)
+    return patch(ctx as UserCtx<PatchRowRequest, PatchRowResponse>, {
+      isAutomation,
+    })
   }
   let saveResult: { row: Row; table: Table; squashed?: Row }
   if (tableId.includes("datasource_plus")) {
