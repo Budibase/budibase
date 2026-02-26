0d51c8d3a87181866e076fbf3cc17bd32fcbe6a5
Adrià Navarro Redó <adria@budibase.com>
2026-02-26 18:04:05 +0100

    Undo packages/server/src/automations/steps/openai.ts

--- packages/server/src/automations/steps/openai.ts
+++ packages/server/src/automations/steps/openai.ts
@@ -1,6 +1,31 @@
+import { OpenAI } from "openai"
+
 import { OpenAIStepInputs, OpenAIStepOutputs } from "@budibase/types"
+import { env } from "@budibase/backend-core"
 import * as automationUtils from "../automationUtils"
-import { promptWithDefaultLLM } from "./ai/llm"
+import { ai } from "@budibase/pro"
+
+/**
+ * Maintains backward compatibility with automation steps created before the introduction
+ * of custom configurations and Budibase AI
+ * @param inputs - automation inputs from the OpenAI automation step.
+ */
+async function legacyOpenAIPrompt(inputs: OpenAIStepInputs) {
+  const openai = new OpenAI({
+    apiKey: env.OPENAI_API_KEY,
+  })
+
+  const completion = await openai.chat.completions.create({
+    model: inputs.model,
+    messages: [
+      {
+        role: "user",
+        content: inputs.prompt,
+      },
+    ],
+  })
+  return completion?.choices[0]?.message?.content
+}
 
 export async function run({
   inputs,
@@ -15,12 +40,12 @@ export async function run({
   }
 
   try {
-    const response = await promptWithDefaultLLM([
-      {
-        role: "user",
-        content: inputs.prompt,
-      },
-    ])
+    let response
+    const llm = await ai.getLLM({ model: inputs.model })
+    response = llm
+      ? (await llm.prompt(inputs.prompt)).message
+      : await legacyOpenAIPrompt(inputs)
+
     return {
       response,
       success: true,
