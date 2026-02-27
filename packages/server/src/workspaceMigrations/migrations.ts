1fcf3f59f70af1d3edb4df6b231aa4e72071c065
Adrià Navarro Redó <adria@budibase.com>
2026-02-27 14:43:44 +0100

    Add new migration

--- packages/server/src/workspaceMigrations/migrations.ts
+++ packages/server/src/workspaceMigrations/migrations.ts
@@ -5,6 +5,7 @@ import { WorkspaceMigration } from "."
 import m20240604153647_initial_sqs from "./migrations/20240604153647_initial_sqs"
 import m20250618162639_workspace_apps from "./migrations/20250618162639_workspace_apps"
 import m20250729134531_workspace_cleanups from "./migrations/20250729134531_workspace_cleanups"
+import m20260227144312_unify_ai_configs from "./migrations/20260227144312_unify_ai_configs"
 
 export const MIGRATIONS: WorkspaceMigration[] = [
   // Migrations will be executed sorted by id
@@ -19,5 +20,9 @@ export const MIGRATIONS: WorkspaceMigration[] = [
   {
     id: "20250729134531_workspace_cleanups",
     func: m20250729134531_workspace_cleanups,
+  },
+  {
+    id: "20260227144312_unify_ai_configs",
+    func: m20260227144312_unify_ai_configs,
   },
 ]
