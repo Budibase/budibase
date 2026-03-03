063e15d90686234a1e47ede392bd3381a59f620e
Adrià Navarro Redó <adria@budibase.com>
2026-03-03 15:20:33 +0100

    Remove old

--- packages/server/src/workspaceMigrations/migrations.ts
+++ packages/server/src/workspaceMigrations/migrations.ts
@@ -6,7 +6,6 @@ import m20240604153647_initial_sqs from "./migrations/20240604153647_initial_sqs
 import m20250618162639_workspace_apps from "./migrations/20250618162639_workspace_apps"
 import m20250729134531_workspace_cleanups from "./migrations/20250729134531_workspace_cleanups"
 import m20260227144312_unify_ai_configs from "./migrations/20260227144312_unify_ai_configs"
-import m20260301144039_remove_unused_ai_configs from "./migrations/20260301144039_remove_unused_ai_configs"
 
 export const MIGRATIONS: WorkspaceMigration[] = [
   // Migrations will be executed sorted by id
@@ -25,9 +24,5 @@ export const MIGRATIONS: WorkspaceMigration[] = [
   {
     id: "20260227144312_unify_ai_configs",
     func: m20260227144312_unify_ai_configs,
-  },
-  {
-    id: "20260301144039_remove_unused_ai_configs",
-    func: m20260301144039_remove_unused_ai_configs,
   },
 ]
