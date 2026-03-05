48e63fb69d14c2520933fde71923fa93590e9d1d
Adrià Navarro Redó <adria@budibase.com>
2026-03-05 19:12:27 +0100

    Remove --coverage

--- packages/backend-core/scripts/test.sh
+++ packages/backend-core/scripts/test.sh
@@ -4,8 +4,8 @@ set -e
 if [[ -n $CI ]]
 then
   # --runInBand performs better in ci where resources are limited
-  echo "jest --coverage --runInBand --forceExit $@"
-  jest --coverage --runInBand --forceExit $@
+  echo "jest --runInBand --forceExit $@"
+  jest --runInBand --forceExit $@
 else
   # --maxWorkers performs better in development
   echo "jest --coverage --forceExit --detectOpenHandles $@"
