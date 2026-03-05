e9e42b08b9cebb2afaab6368aa45db957d4be396
Adrià Navarro Redó <adria@budibase.com>
2026-03-05 12:05:57 +0100

    Add "graphql/*" to dependency

--- packages/worker/scripts/build.js
+++ packages/worker/scripts/build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const coreBuild = require("../../../scripts/build")
 
-const externals = ["deasync"]
+const externals = ["deasync", "graphql/*"]
 
 coreBuild("./src/index.ts", "./dist/index.js", { external: externals })
