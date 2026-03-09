80b27405c0245cbf0bfcf6b7e06d0c0e0dd637e4
Dean <deanhannigan@gmail.com>
2026-03-09 21:54:07 +0000

    Lint 3: The Return of the Lint

--- packages/builder/src/pages/builder/workspace/[application]/data/query/[queryId]/index.svelte
+++ packages/builder/src/pages/builder/workspace/[application]/data/query/[queryId]/index.svelte
@@ -1,26 +1,12 @@
 <script>
-  import { queries, datasources } from "@/stores/builder"
+  import { queries } from "@/stores/builder"
   import QueryViewer from "@/components/integration/QueryViewer.svelte"
-  import RestQueryViewer from "@/components/integration/RestQueryViewer.svelte"
-  import { IntegrationTypes } from "@/constants/backend"
   import { cloneDeep } from "lodash/fp"
-  import { goto } from "@roxi/routify"
 
   $: query = $queries.selected
   $: editableQuery = cloneDeep(query)
-  $: datasource = $datasources.list.find(ds => ds._id === query?.datasourceId)
-  $: isRestQuery = datasource?.source === IntegrationTypes.REST
-  $: {
-    if (query && isRestQuery) {
-      $goto(`../../../apis/query/${query._id}`)
-    }
-  }
 </script>
 
 {#if query}
-  {#if isRestQuery}
-    <RestQueryViewer queryId={$queries.selectedQueryId} />
-  {:else}
-    <QueryViewer query={editableQuery} />
-  {/if}
+  <QueryViewer query={editableQuery} />
 {/if}
