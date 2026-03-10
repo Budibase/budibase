6dd86c58e676f4112341dbe408742756c4cfcc10
Adrià Navarro Redó <adria@budibase.com>
2026-03-10 21:36:58 +0100

    Populate breadcrum

--- packages/builder/src/settings/routes.ts
+++ packages/builder/src/settings/routes.ts
@@ -13,7 +13,7 @@ import { AppMetaState } from "@/stores/builder/app"
 import { PortalAppsStore } from "@/stores/portal/apps"
 import { StoreApp } from "@/types"
 import { featureFlag } from "@/helpers"
-import { aiConfigsStore, knowledgeBaseStore } from "@/stores/portal"
+import { aiConfigsStore, knowledgeBaseStore, vectorDbStore } from "@/stores/portal"
 import { get } from "svelte/store"
 
 const getPathId = (path: string | undefined) => {
@@ -343,7 +343,17 @@ export const appRoutes = (
                   routes: [
                     {
                       path: ":configId",
-                      title: "New embedding model",
+                      title: (path: string | undefined) => {
+                        const id = getPathId(path)
+                        if (!id) {
+                          return "New embedding model"
+                        }
+                        return (
+                          get(aiConfigsStore).customConfigs.find(
+                            c => c._id === id
+                          )?.name ?? "Embedding model"
+                        )
+                      },
                       comp: Pages.get("embedding_model"),
                     },
                   ],
@@ -353,7 +363,16 @@ export const appRoutes = (
                   routes: [
                     {
                       path: ":id",
-                      title: "New vector database",
+                      title: (path: string | undefined) => {
+                        const id = getPathId(path)
+                        if (!id) {
+                          return "New vector database"
+                        }
+                        return (
+                          get(vectorDbStore).configs.find(db => db._id === id)
+                            ?.name ?? "Vector database"
+                        )
+                      },
                       comp: Pages.get("vector_database"),
                     },
                   ],
