2362e7d6b0f985c5fae4d9ccb96d0eb1a0c47f4e
Mel O'Hagan <mel@budibase.com>
2026-03-11 10:20:34 +0000

    fix(builder): allow SCIM workspace role edits

---
+++ packages/builder/src/test/mocks/MockColorPicker.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let value = ""
+  export let disabled = false
+</script>
+
+<label>
+  <span>Color</span>
+  <input aria-label="Color" bind:value {disabled} />
+</label>
