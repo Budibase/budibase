c6c5d6ddd7d3f8238320e862663bd1c6b2b5f337
Dean <deanhannigan@gmail.com>
2026-03-12 10:51:14 +0000

    Blur on enter in the url inputs

--- packages/builder/src/components/integration/CustomEndpointInput.svelte
+++ packages/builder/src/components/integration/CustomEndpointInput.svelte
@@ -67,6 +67,9 @@
         baseUrl = e.currentTarget.value
         dispatch("baseUrlChange", baseUrl)
       }}
+      on:keydown={e => {
+        if (e.key === "Enter") e.currentTarget.blur()
+      }}
     />
     {#if baseUrlOptions.length > 0}
       <div class="globe-icon">
@@ -101,6 +104,9 @@
     on:input={e => {
       path = e.currentTarget.value
       dispatch("pathChange", path)
+    }}
+    on:keydown={e => {
+      if (e.key === "Enter") e.currentTarget.blur()
     }}
     on:blur={e => {
       dispatch("pathCommit", e.currentTarget.value)
