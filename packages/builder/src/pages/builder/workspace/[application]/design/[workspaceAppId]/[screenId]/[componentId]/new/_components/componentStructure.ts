4bfbdd857cc01e43eb28a566cd09b91601c19266
andz-bb <andrew.thompson@budibase.com>
2026-05-26 17:50:27 +0100

    add url form field component

--- packages/builder/src/pages/builder/workspace/[application]/design/[workspaceAppId]/[screenId]/[componentId]/new/_components/componentStructure.ts
+++ packages/builder/src/pages/builder/workspace/[application]/design/[workspaceAppId]/[screenId]/[componentId]/new/_components/componentStructure.ts
@@ -61,6 +61,7 @@ export function getComponentStructure() {
         "formstep",
         "fieldgroup",
         "stringfield",
+        "websiteurlfield",
         "numberfield",
         "bigintfield",
         "passwordfield",
---
+++ packages/client/src/components/app/forms/URLField.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import StringField from "./StringField.svelte"
+</script>
+
+<StringField {...$$props} type="url" autocomplete="url" />
