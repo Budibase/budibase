0d54d9f49d67f41463b0ef850342429fa53b1539
andz-bb <andrew.thompson@budibase.com>
2026-05-26 18:48:40 +0100

    props type fix

--- packages/client/src/components/app/forms/URLField.svelte
+++ packages/client/src/components/app/forms/URLField.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import StringField from "./StringField.svelte"
+
+  let props = $props()
 </script>
 
-<StringField {...$$props} type="url" autocomplete="url" />
+<StringField {...props} type="url" autocomplete="url" />
