b6b6779ef5e93d19801db448c6b6e9536083b399
jvcalderon <jose@budibase.com>
2026-06-17 16:24:34 +0200

    Lint

--- packages/builder/src/components/portal/licensing/AccountLockedModal.svelte
+++ packages/builder/src/components/portal/licensing/AccountLockedModal.svelte
@@ -28,18 +28,18 @@
     <Body size="S">
       {#if lockedBy === LockReason.MIGRATION}
         You’re currently on the Budibase Free plan, which is no longer available
-        for cloud users. Your tenant has been temporarily locked. Upgrade to keep
-        building in the cloud, export your workspaces, or move to self-hosted
-        Budibase before your data is removed.
+        for cloud users. Your tenant has been temporarily locked. Upgrade to
+        keep building in the cloud, export your workspaces, or move to
+        self-hosted Budibase before your data is removed.
       {:else if lockedBy === LockReason.PAID_TO_FREE}
         Your paid Budibase subscription has expired and was not renewed. Your
-        tenant is now in a locked, limited-access state. Upgrade to keep building
-        in the cloud, export your workspaces, or move to self-hosted Budibase
-        before your data is removed.
+        tenant is now in a locked, limited-access state. Upgrade to keep
+        building in the cloud, export your workspaces, or move to self-hosted
+        Budibase before your data is removed.
       {:else if lockedBy === LockReason.FREE_TIER}
-        Your Budibase Cloud trial has ended and your tenant is temporarily locked.
-        Upgrade to keep building in the cloud, export your workspaces, or move to
-        self-hosted Budibase before your data is removed.
+        Your Budibase Cloud trial has ended and your tenant is temporarily
+        locked. Upgrade to keep building in the cloud, export your workspaces,
+        or move to self-hosted Budibase before your data is removed.
       {:else}
         Due to the Free plan user limit being exceeded, your tenant has been
         de-activated. Upgrade your plan to re-activate your tenant.
