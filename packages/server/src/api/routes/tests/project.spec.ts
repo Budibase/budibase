8d7f59e1368b2aa9ead4a6e6a5efbcd63914e9d7
andz-bb <andrew.thompson@budibase.com>
2026-06-24 12:06:46 +0100

    Apply lint fixes for project stack pre-push hook.

--- packages/server/src/api/routes/tests/project.spec.ts
+++ packages/server/src/api/routes/tests/project.spec.ts
@@ -447,9 +447,9 @@ describe("/projects", () => {
         },
       })
 
-      expect(updatedDatasource.entities![externalTable.name].projectIds).toEqual([
-        project._id
-      ])
+      expect(
+        updatedDatasource.entities![externalTable.name].projectIds
+      ).toEqual([project._id])
     })
   })
 
