d5731a843b14b3203fe2e2e4ca947a5cc0876525
Christos Alexiou <christos@budibase.com>
2026-08-19 17:10:52 +0300

    add tests

--- packages/backend-core/src/cache/tests/user.spec.ts
+++ packages/backend-core/src/cache/tests/user.spec.ts
@@ -5,6 +5,8 @@ import { getUsers } from "../user"
 import { getGlobalDB } from "../../context"
 import _ from "lodash"
 
+import * as accounts from "../../accounts"
+import { withEnv } from "../../environment"
 import * as redis from "../../redis/init"
 import { UserDB } from "../../users"
 
@@ -116,6 +118,89 @@ describe("user cache", () => {
         userIdsToRequest[2],
         userIdsToRequest[4],
       ])
+    })
+
+    describe("account portal", () => {
+      const withAccountPortal = <T>(f: () => Promise<T>) =>
+        withEnv({ SELF_HOSTED: false, DISABLE_ACCOUNT_PORTAL: "" }, f)
+
+      afterEach(() => {
+        jest.restoreAllMocks()
+      })
+
+      it("a single failing account lookup does not fail the whole batch", async () => {
+        const usersToRequest = _.sampleSize(users, 3)
+        const userIdsToRequest = usersToRequest.map(x => x._id!)
+        const failingUser = usersToRequest[1]
+        const account = structures.accounts.cloudAccount()
+
+        jest
+          .spyOn(accounts, "getAccount")
+          .mockImplementation(async (email: string) => {
+            if (email === failingUser.email) {
+              throw new Error(`Error getting account by email ${email}`)
+            }
+            return account
+          })
+
+        const results = await withAccountPortal(() =>
+          config.doInTenant(() => getUsers(userIdsToRequest))
+        )
+
+        expect(results.users).toHaveLength(3)
+        expect(_.sortBy(results.users, "_id")).toEqual(
+          _.sortBy(
+            usersToRequest.map(u => ({
+              ...u,
+              budibaseAccess: true,
+              _rev: expect.any(String),
+              ...(u._id === failingUser._id
+                ? {}
+                : { account, accountPortalAccess: true }),
+            })),
+            "_id"
+          )
+        )
+      })
+
+      it("a user with a failed account lookup is not cached", async () => {
+        const usersToRequest = _.sampleSize(users, 3)
+        const userIdsToRequest = usersToRequest.map(x => x._id!)
+        const failingUser = usersToRequest[1]
+        const account = structures.accounts.cloudAccount()
+
+        const getAccountSpy = jest
+          .spyOn(accounts, "getAccount")
+          .mockImplementation(async (email: string) => {
+            if (email === failingUser.email) {
+              throw new Error(`Error getting account by email ${email}`)
+            }
+            return account
+          })
+
+        await withAccountPortal(() =>
+          config.doInTenant(() => getUsers(userIdsToRequest))
+        )
+
+        getAccountSpy.mockResolvedValue(account)
+        jest.spyOn(UserDB, "bulkGet")
+
+        const results = await withAccountPortal(() =>
+          config.doInTenant(() => getUsers(userIdsToRequest))
+        )
+
+        expect(UserDB.bulkGet).toHaveBeenCalledTimes(1)
+        expect(UserDB.bulkGet).toHaveBeenCalledWith([failingUser._id])
+        expect(results.users).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              _id: failingUser._id,
+              account,
+              accountPortalAccess: true,
+            }),
+          ])
+        )
+      })
     })
 
     it("requesting existing and unexisting ids will return found ones", async () => {
