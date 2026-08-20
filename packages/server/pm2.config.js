456a4710ad05f3af191e365d0856060310634923
Christos Alexiou <133668576+calexiou@users.noreply.github.com>
2026-08-20 22:09:59 +0300

    Apply suggestion from @calexiou

--- packages/server/pm2.config.js
+++ packages/server/pm2.config.js
@@ -2,9 +2,6 @@ module.exports = {
   apps: [
     {
       script: "./dist/index.js",
-      // "max" counts the host's CPUs, not the container's cgroup CPU limit. On a
-      // 24 core host that forked 24 instances plus their worker farms - 79
-      // processes and 19GB of RSS - which is instant death under a pod limit.
       instances: process.env.CLUSTER_INSTANCES || "max",
       exec_mode: "cluster",
     },
