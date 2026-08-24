52f32bc3fcfc00aac70081c1cb2eb3c18dda2fc5
Christos Alexiou <christos@budibase.com>
2026-08-24 15:16:17 +0300

    fix: mirror EditWorkspaceRoleModal.svelte pattern

--- packages/builder/src/settings/pages/people/groups/_components/WorkspaceAddModal.svelte
+++ packages/builder/src/settings/pages/people/groups/_components/WorkspaceAddModal.svelte
@@ -9,10 +9,11 @@
     notifications,
   } from "@budibase/bbui"
   import GlobalRoleSelect from "@/components/common/GlobalRoleSelect.svelte"
-  import { roles } from "@/stores/builder"
+  import { API } from "@/api"
   import { workspacesStore } from "@/stores/portal/workspaces"
   import { groups } from "@/stores/portal/groups"
   import { Constants } from "@budibase/frontend-core"
+  import type { Role } from "@budibase/types"
   import GroupIcon from "./GroupIcon.svelte"
 
   export let groupId: string
@@ -27,19 +28,40 @@
       option.value === Constants.BudibaseRoles.Creator ||
       option.value === Constants.BudibaseRoles.AppUser
   )
+  const excludedRoleIds = [
+    Constants.Roles.BASIC,
+    Constants.Roles.ADMIN,
+    Constants.Roles.PUBLIC,
+    Constants.Roles.CREATOR,
+    Constants.Roles.GROUP,
+  ]
 
   let selectedWorkspaceIds: string[] = []
   let selectedRole = Constants.BudibaseRoles.AppUser
   let selectedEndUserRole = Constants.Roles.BASIC
   let workspaceSearchTerm = ""
+  let workspaceRoles: Role[] = []
+  let roleRequestId = 0
 
   $: group = $groups.find(x => x._id === groupId)
-  $: roleColorLookup = ($roles || []).reduce<
+  $: namedWorkspaceRoles = workspaceRoles.filter(
+    (role): role is Role & { _id: string } => !!role._id
+  )
+  $: roleColorLookup = namedWorkspaceRoles.reduce<
     Record<string, string | undefined>
   >((acc, role) => {
     acc[role._id] = role.uiMetadata?.color
     return acc
   }, {})
+  $: customEndUserRoleOptions = namedWorkspaceRoles
+    .filter(role => !excludedRoleIds.includes(role._id))
+    .map(role => ({
+      label: role.uiMetadata?.displayName || role.name || "Custom role",
+      value: role._id,
+      color:
+        role.uiMetadata?.color ||
+        "var(--spectrum-global-color-static-magenta-400)",
+    }))
   $: endUserRoleOptions = [
     {
       label: "Basic user",
@@ -51,6 +73,7 @@
       value: Constants.Roles.ADMIN,
       color: roleColorLookup[Constants.Roles.ADMIN],
     },
+    ...customEndUserRoleOptions,
   ]
   $: assignedWorkspaceIds = group ? groups.getGroupAppIds(group) : []
   $: workspaceOptions = Object.values(
@@ -84,12 +107,41 @@
   $: confirmDisabled =
     !selectedWorkspaceIdsForSubmit.length ||
     (selectedRole === Constants.BudibaseRoles.AppUser && !selectedEndUserRole)
+  $: singleWorkspaceId =
+    selectedWorkspaceIdsForSubmit.length === 1
+      ? selectedWorkspaceIdsForSubmit[0]
+      : null
+  $: if (singleWorkspaceId) {
+    fetchWorkspaceRoles(singleWorkspaceId)
+  } else {
+    workspaceRoles = []
+  }
+  $: if (
+    !endUserRoleOptions.find(option => option.value === selectedEndUserRole)
+  ) {
+    selectedEndUserRole = Constants.Roles.BASIC
+  }
+
+  async function fetchWorkspaceRoles(appId: string) {
+    const requestId = ++roleRequestId
+    try {
+      const response = await API.getRolesForApp(appId)
+      if (requestId === roleRequestId) {
+        workspaceRoles = response?.roles || []
+      }
+    } catch (error) {
+      if (requestId === roleRequestId) {
+        workspaceRoles = []
+      }
+    }
+  }
 
   export function reset() {
     selectedWorkspaceIds = []
     selectedRole = Constants.BudibaseRoles.AppUser
     selectedEndUserRole = Constants.Roles.BASIC
     workspaceSearchTerm = ""
+    workspaceRoles = []
   }
 
   const getWorkspaceRole = () => {
