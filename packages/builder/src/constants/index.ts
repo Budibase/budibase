5d6e7547542eda413777ed1d6600af368a7aa43d
Adria Navarro <adria@budibase.com>
2026-09-10 10:48:00 +0200

    Lint

--- packages/builder/src/constants/index.ts
+++ packages/builder/src/constants/index.ts
@@ -34,9 +34,7 @@ export const ToolBindingPrefix = {
   TOOL: "tool",
 }
 
-export const RETIRED_TOOL_BINDING_NAMESPACES = [
-  ToolBindingPrefix.ESCALATION,
-]
+export const RETIRED_TOOL_BINDING_NAMESPACES = [ToolBindingPrefix.ESCALATION]
 
 // fields on the user table that cannot be edited
 export const UNEDITABLE_USER_FIELDS = [
