af6510ed7df35d65cbd973005bc44d723d2a46c2
Christos Alexiou <christos@budibase.com>
2026-09-16 17:12:28 +0300

    new cleardebounce method

--- packages/server/src/utilities/redis.ts
+++ packages/server/src/utilities/redis.ts
@@ -97,6 +97,10 @@ export async function setDebounce(id: string, seconds: number) {
   await debounceClient.store(id, "debouncing", seconds)
 }
 
+export async function clearDebounce(id: string) {
+  await debounceClient.delete(id)
+}
+
 export async function checkTestFlag(id: string) {
   const flag = await flagClient?.get(id)
   return !!(flag && flag.testing)
