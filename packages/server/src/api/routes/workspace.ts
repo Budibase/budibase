b340cfbf3c9a71d6db817b86ec74c9973e608004
Christos Alexiou <christos@budibase.com>
2026-09-25 23:42:49 +0300

    types

--- packages/server/src/api/routes/workspace.ts
+++ packages/server/src/api/routes/workspace.ts
@@ -4,6 +4,11 @@ import * as deploymentController from "../controllers/deploy"
 import * as controller from "../controllers/workspace"
 import { builderRoutes, creatorRoutes, publicRoutes } from "./endpointGroups"
 import { applicationValidator } from "./utils/validators"
+import {
+  UserCtx,
+  CreateWorkspaceRequest,
+  CreateWorkspaceResponse,
+} from "@budibase/types"
 
 builderRoutes
   .post("/api/applications/:appId/sync", controller.sync)
@@ -25,8 +30,11 @@ builderRoutes
   .post("/api/applications/:appId/duplicate", controller.duplicateWorkspace)
   .post("/api/applications/:appId/import", controller.importToWorkspace)
 
-creatorRoutes.post("/api/applications", applicationValidator(), ctx =>
-  controller.create(ctx)
+creatorRoutes.post(
+  "/api/applications",
+  applicationValidator(),
+  (ctx: UserCtx<CreateWorkspaceRequest, CreateWorkspaceResponse, any>) =>
+    controller.create(ctx)
 )
 
 // Client only endpoints
